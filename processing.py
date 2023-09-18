e1e2d5fa028fd7dc2ae270ca4a23cf4fe06567ce
Xingnan Li <1377810972@qq.com>
2023-09-18 16:39:56 +1000

    replace the processing.py and split by the patient_id

--- processing.py
+++ processing.py
@@ -26,7 +26,7 @@ def parse_opt():
     parser.add_argument('--train_path', type=str, default=r'dataset/train', help='all train path')
     parser.add_argument('--test_path', type=str, default=r'dataset/test', help='all test path')
     parser.add_argument('--label_path', type=str, default=r'dataset/label.txt', help='label txt save path')
-    parser.add_argument('--val_size', type=float, default=0.3, help='size of val set')
+    parser.add_argument('--val_size', type=float, default=0.2, help='size of val set')
     opt = parser.parse_known_args()[0]
     return opt
 
@@ -34,6 +34,23 @@ if __name__ == '__main__':
     opt = parse_opt()
     with open(opt.label_path, 'w+', encoding='utf-8') as f:
         f.write('\n'.join(os.listdir(opt.train_path))) # label's name
+    import os
+
+    path = "./dataset/train"
+
+    class_ = os.listdir(path)
+    patients = {}
+    for i in class_:
+        imag_path = path + os.sep + i
+        a = []
+        for j in os.listdir(imag_path):
+            id = j.split("_")[0]
+            a.append(id)
+        patients[f"{i}"] = list(set(a))
+    num_ad = len(patients["AD"])
+    num_nc = len(patients["NC"])
+    print("ad" + " " + f"{num_ad}")
+    print("nc" + " " + f"{num_nc}")
 
     train_str_len = len(str(len(os.listdir(opt.train_path))))
     test_str_len = len(str(len(os.listdir(opt.test_path))))
@@ -47,20 +64,27 @@ if __name__ == '__main__':
 
     os.chdir(opt.train_path)
 
-    for i in os.listdir(os.getcwd()):
+    for index,i in enumerate(os.listdir(os.getcwd())):
+        print(i)
         base_path = os.path.join(os.getcwd(), i)
 
         base_arr = os.listdir(base_path)
-        #print(base_arr)
-        np.random.shuffle(base_arr)
-    #
+        if index == 0:
+            k = "AD"
+        else:
+            k="NC"
+
+        id = patients[k]
+
         val_path = base_path.replace('train', 'val')
         if not os.path.exists(val_path):
             os.makedirs(val_path)
-        val_need_copy = base_arr[int(len(base_arr) * (1 - opt.val_size)):
-                                 int(len(base_arr))]
-        for j in val_need_copy:
-            shutil.move(os.path.join(base_path, j), os.path.join(val_path, j))
+        val_need_copy = id[int(len(id) * (1 - opt.val_size)):
+                                 int(len(id))]
+
+        for j in base_arr:
+            if j.split("_")[0] in val_need_copy:
+                shutil.move(os.path.join(base_path, j), os.path.join(val_path, j))
 
 
 
