37b5380bb503c98457bcba545c847cea3ce0a400
Minh Tieu <s4641971@uqconnect.edu.au>
2023-09-19 15:04:14 +1000

    create template for the report code

---
+++ recognition/styleGAN_46419712/modules.py
@@ -0,0 +1,8 @@
+import torchvision
+import torch
+import torchvision.transforms as transforms
+import torch.nn as nn
+import torch.nn.functional as F
+import time
+import matplotlib.pyplot as plt
+import numpy as np
