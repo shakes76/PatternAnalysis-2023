8d9038c76a139cb2ac747820b49412f31c857e84
Kristian Norved <kristiannorved@gmail.com>
2023-09-19 15:51:54 +1000

    dataset.py now prints some very useful statistics about the given graph dataset and seperates the features and target into X and y datasets.

--- recognition/facebook-classification-s46406619/dataset.py
+++ recognition/facebook-classification-s46406619/dataset.py
@@ -1,13 +1,24 @@
 import numpy as np
 import os
+import torch
+from sklearn.model_selection import train_test_split
 
 # set working directory
 os.chdir('C:/Area-51/2023-sem2/COMP3710/PatternAnalysis-2023/recognition/facebook-classification-s46406619')
 
 # import raw data files
 facebook = np.load('facebook.npz')
-edges = facebook['edges']
-features = facebook['features'] # X dataset
-target = facebook['target'] # y dataset
+edges = torch.tensor(facebook['edges'])
+features = torch.tensor(facebook['features'])
+target = torch.tensor(facebook['target'])
 
-print('edges:', edges.shape, 'features:', features.shape, 'target:', target.shape)
+# let us get some information about this dataset
+print('number of classes:', len(np.unique(target)))
+print('number of nodes:', len(features))
+print('number of directed edges:', len(edges))
+print('number of bidirectional edges:', round(len(edges) / 2))
+
+# train test split
+X_train, y_train, X_test, y_test = train_test_split(features, target, test_size=0.2, random_state=42)
+print('X dataset shape:', X_train.shape, X_test.shape)
+print('y dataset shape:', y_train.shape, y_test.shape)
