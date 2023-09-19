a3d811809f7ed7a9d7be6b1a2acb4435bdd650c9
jdw5 <joshuadwallace1@gmail.com>
2023-09-19 15:19:24 +1000

    Image loading of the ADNI dataset

---
+++ recognition/Siamese_Joshua_Wallace/dataset.py
@@ -0,0 +1,21 @@
+import torch
+from torchvision import datasets, transforms
+
+# Define transformations
+# Note: You might want to use different transforms for training and testing datasets. For simplicity, the same transforms are used here.
+transform = transforms.Compose([
+    transforms.Resize(256),
+    transforms.ToTensor(),
+])
+
+def get_dataloaders(batch_size=32, root_dir='./AD_CN'):
+    # Create training and testing datasets
+    train_dataset = datasets.ImageFolder(root=f"{root_dir}/train", transform=transform)
+    test_dataset = datasets.ImageFolder(root=f"{root_dir}/test", transform=transform)
+    
+    # Create dataloaders
+    train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = torch.utils.data.DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+    return train_loader, test_loader
+
