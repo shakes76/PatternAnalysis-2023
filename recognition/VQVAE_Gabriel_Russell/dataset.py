ac7c2a1f5df1b37b173bf17cd314725cd4c0b9e0
Gabriel_Russell <garussell02@gmail.com>
2023-09-19 21:18:07 +1000

    Built two classes for loading in the specified folders for the train, validate and test datasets. Now able to call the OASISDataloader class to retrieve the specified dataloaders via the outlined getter functions that were implemented.

--- recognition/VQVAE_Gabriel_Russell/dataset.py
+++ recognition/VQVAE_Gabriel_Russell/dataset.py
@@ -9,3 +9,73 @@ The data is a preprocessed version of the original OASIS Brain dataset provided 
 @ID: s4640776
 
 """
+import os
+import torch
+from torchvision import transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
+from PIL import Image
+from torch.utils.data import Dataset
+
+#Get the current working directoy
+current_dir = os.getcwd()
+
+#Setting up paths for each folder required from the OASIS dataset
+OASIS_train_path = current_dir + "\keras_png_slices_train"
+OASIS_validate_path = current_dir + '\keras_png_slices_validate'
+OASIS_test_path = current_dir + '\keras_png_slices_test'
+
+#This class takes the downloaded data from a specific path, performs the required 
+#transformation to all images and returns it ready to be loaded into a dataloader
+class OASISDataset(Dataset):
+    #Define the transform as a class attribute
+
+    #Define the data transformations from imported datasets
+    #Resize all images to 256x256 pixels
+    #Convert to tensors and normalise tensor image with mean and standard deviation
+    transform = transforms.Compose([
+    transforms.Resize((256,256)),
+    transforms.ToTensor(),
+    transforms.Normalize(mean = [0.5, 0.5, 0.5], std = [0.5, 0.5, 0.5])])
+
+    def __init__(self, data_directory):
+        self.data_directory = data_directory
+        self.data = os.listdir(data_directory)
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.data_directory, self.data[idx])
+        img = Image.open(img_path)
+        img = self.transform(img)
+        return img
+
+#This class calls the OASISDataset Class in it's initialisations to set up 
+#the train, validation and test datasets.
+#Included are some getter functions for returning the specified dataloader.
+class OASISDataloader():
+
+    current_dir = os.getcwd()
+    OASIS_train_path = current_dir + '\keras_png_slices_train'
+    OASIS_validate_path = current_dir + '\keras_png_slices_validate'
+    OASIS_test_path = current_dir + '\keras_png_slices_test'
+
+    def __init__(self):
+        self.train = OASISDataset(OASIS_train_path)
+        self.validate = OASISDataset(OASIS_validate_path)
+        self.test = OASISDataset(OASIS_test_path)
+        self.batch_size = 64
+
+    def get_train(self):
+        train_dataloader =  DataLoader(self.train, batch_size = self.batch_size, shuffle = True, drop_last= True)
+        return train_dataloader
+    
+    def get_validate(self):
+        validate_dataloader =  DataLoader(self.validate, batch_size = self.batch_size, shuffle = False)
+        return validate_dataloader
+    
+    def get_test(self):
+        test_dataloader =  DataLoader(self.test, batch_size = self.batch_size, shuffle = False)
+        return test_dataloader
+
