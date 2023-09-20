4b2d67c35e03f3ef68df0615b1d29aa4b80fa658
jdw5 <joshuadwallace1@gmail.com>
2023-09-20 11:02:26 +1000

    Added ability to reduce dataset to subset for local testing

--- recognition/Siamese_Joshua_Wallace/dataset.py
+++ recognition/Siamese_Joshua_Wallace/dataset.py
@@ -1,6 +1,6 @@
 import torchvision.transforms as transforms
 from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 import random
 import torch
 
@@ -13,16 +13,23 @@ class Dataset():
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
 
-    def __init__(self, batch_size = 32, root_dir = './AD_NC') :
+    def __init__(self, batch_size = 32, root_dir = './AD_NC', fraction=1.0) :
         self.batch_size = batch_size
         self.root_dir = root_dir
+        self.fraction = fraction
         self.train_loader = None
         self.test_loader = None
     
     def load_train(self) -> None:
         train_dataset = ImageFolder(root=f"{self.root_dir}/train")
+        train_dataset = self._get_subset(train_dataset)
         siamese_train_dataset = ADNIDataset(train_dataset, transform=self.transform)
         self.train_loader = DataLoader(siamese_train_dataset, batch_size=self.batch_size, shuffle=True)
+    
+    def _get_subset(self, dataset):
+        num_samples = int(len(dataset) * self.fraction)
+        indices = random.sample(range(len(dataset)), num_samples)
+        return Subset(dataset, indices)
     
     def get_train(self) -> DataLoader :
         if self.train_unloaded() :
@@ -32,6 +39,7 @@ class Dataset():
 
     def load_test(self) -> None :
         test_dataset = ImageFolder(root=f"{self.root_dir}/test")
+        test_dataset = self._get_subset(test_dataset)
         siamese_test_dataset = ADNIDataset(test_dataset, transform=self.transform)
         self.test_loader = DataLoader(siamese_test_dataset, batch_size=self.batch_size, shuffle=True)
     
--- recognition/Siamese_Joshua_Wallace/main.py
+++ recognition/Siamese_Joshua_Wallace/main.py
@@ -7,8 +7,8 @@ from train import Train
 if __name__ == '__main__':
     net = Siamese()
     # Limit batch size, else the program will run out of memory
-    config = Config(lr=1e-3, wd=1e-5, epochs=1, batch_size=32, root_dir='./AD_NC', gpu='cuda')
-    dataset = Dataset(batch_size=config.batch_size)
+    config = Config(lr=1e-4, wd=1e-5, epochs=1, batch_size=32, root_dir='./AD_NC', gpu='cuda')
+    dataset = Dataset(batch_size=config.batch_size, fraction=0.1)
     trainer = Train(net, dataset, config)
     trainer.train()
     trainer.test()
