7ffd3b985a805a9c8a01dcbb494c3722d4a17aa6
Arvin Liu <a127000555@gmail.com>
2023-09-23 05:54:44 +0800

    add upsample

--- module.py
+++ module.py
@@ -144,3 +144,19 @@ class Downsample(nn.Module):
 
     def forward(self, x):
         return self.layer(x)
+
+
+class Upsample(nn.Module):
+    def __init__(self, in_channels, with_conv):
+        super().__init__()
+        self.with_conv = with_conv
+        if self.with_conv:
+            self.conv = torch.nn.Conv2d(in_channels, in_channels, 3, 1, 1)
+
+    def forward(self, x):
+        # Just do interpolate & convolution
+        x = torch.nn.functional.interpolate(
+            x, scale_factor=2.0, mode="nearest")
+        if self.with_conv:
+            x = self.conv(x)
+        return x
