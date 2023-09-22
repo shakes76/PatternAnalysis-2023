991a7dd7096b5ea10bc94dfb8266f26dfe627fb2
Mike Shi <dongming.shi@uqconnect.edu.au>
2023-09-23 09:56:10 +1000

    finalised dataset.py, tested on rangpur with the ADNI dataset

--- recognition/s4640878/dataset.py
+++ recognition/s4640878/dataset.py
@@ -12,55 +12,89 @@ class Dataset(data.Dataset):
     """
     def __init__(self, train=True):
 
-        self._dataset_path = "/home/groups/comp3710/"
+        """ train or test dataset """
         self._dataset_type = "train/" if train else "test/"
 
+        """ path to dataset on rangpur """
+        self._dataset_path = "/home/groups/comp3710/"
         self._dataset = "ADNI/"
         self._dataset_subdir = "AD_NC/"
 
-        self._dataset_classes = {0: "AD/", 1: "NC/"}
-
+        """ default mean and std dev """
         self._mean = (0.5, 0.5, 0.5)
         self._std_dev = (0.5, 0.5, 0.5)
 
+        """ default transforms: to pytorch tensor, normalise to mean and std dev """
         self._transform = torchvision.transforms.Compose(
             [
                 torchvision.transforms.ToTensor(),
-                #torchvision.transforms.Resize(64),
                 torchvision.transforms.Normalize(self._mean, self._std_dev),
             ]
         )
 
+        """ load image data from specified path to dataset """
         self._dataset = torchvision.datasets.ImageFolder(
-            root=f"./debug/{self._dataset_type}",
+            root=f"{self._dataset_path}{self._dataset}{self._dataset_subdir}{self._dataset_type}",
             transform=self._transform,
         )
 
-        self._train_loader = data.DataLoader(
+        """ create a pytorch dataloader for the dataset """
+        self._loader = data.DataLoader(
             dataset=self._dataset, batch_size=128, shuffle=True,
         )
 
+        """ display parameters associated with the dataset """
+        print(f"{self._dataset_type}, {len(self._loader) = }, {self._dataset.class_to_idx = }")
 
-    def train_loader(self):
-        return self._train_loader
-
+    def loader(self):
+        """
+        get loader method
+        
+        """
+        return self._loader
 
 
 def main():
+    """
+    main function: used to test the functionality of the dataloader module
+
+    """
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     print(device, flush=True)
 
-    dataset = Dataset(train=True)
-    train_loader = dataset.train_loader()
+    """ train dataset """
+    train_loader = Dataset(train=True).loader()
 
-    for images, labels in train_loader:
+    plt.figure()
+    for i, (images, labels) in enumerate(train_loader):
+        images = images.to(device)
+        labels = labels.to(device)
 
-        plt.imshow(images[4].permute(1, 2, 0))
-        plt.show()
+        if i == 0: print(f"train: {images.shape = }, {labels.shape = }, {torch.max(images) = }, {torch.min(images) = }")
+        if i >= 9: break
 
-        print(labels[4], flush=True)
+        plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].permute(1, 2, 0).cpu())
+        plt.title(labels[i], size=8)
 
-        sys.exit()
+    plt.savefig("./outputs/train.png")
+
+    """ test dataset """
+    test_loader = Dataset(train=False).loader()
+
+    plt.figure()
+    for i, (images, labels) in enumerate(test_loader):
+        images = images.to(device)
+        labels = labels.to(device)
+
+        if i == 0: print(f"train: {images.shape = }, {labels.shape = }, {torch.max(images) = }, {torch.min(images) = }")
+        if i >= 9: break
+
+        plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].permute(1, 2, 0).cpu())
+        plt.title(labels[i], size=8)
+
+    plt.savefig("./outputs/test.png")
 
 
 if __name__ == "__main__":
