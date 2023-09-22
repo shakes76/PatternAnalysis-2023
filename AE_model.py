16542bd663551af5221c99f86f6168e55807fef1
Arvin Liu <a127000555@gmail.com>
2023-09-23 06:54:40 +0800

    Complete pre-stage AE model

--- AE_model.py
+++ AE_model.py
@@ -11,7 +11,7 @@ from module import ResnetBlock, AttnBlock, Downsample, Upsample
 class Encoder(nn.Module):
     def __init__(self, *, ch, ch_mult=(1, 2, 4, 8), num_res_blocks,
                  attn_resolutions, dropout=0.0, resamp_with_conv=True, in_channels,
-                 resolution, z_channels, double_z=True):
+                 resolution, z_channels, double_z=True, **ignores):
         '''
             ch: base number of all channels, int
             ch_mult: channal number of each blocks, [int...]
@@ -90,16 +90,15 @@ class Encoder(nn.Module):
         h = self.end(h)
         return h
 
+
 class Decoder(nn.Module):
-    def __init__(self, *, ch, out_ch, ch_mult=(1,2,4,8), num_res_blocks,
+    def __init__(self, *, ch, out_ch, ch_mult=(1, 2, 4, 8), num_res_blocks,
                  attn_resolutions, dropout=0.0, resamp_with_conv=True,
-                 resolution, z_channels, give_pre_end=False, tanh_out=False):
+                 resolution, z_channels, **ignores):
         super().__init__()
         self.ch = ch
         self.num_resolutions = len(ch_mult)
         self.num_res_blocks = num_res_blocks
-        self.give_pre_end = give_pre_end
-        self.tanh_out = tanh_out
 
         # Calculate block_in & current resolution
         block_in = ch*ch_mult[-1]
@@ -108,12 +107,13 @@ class Decoder(nn.Module):
         # z to block_in
         self.conv_in = torch.nn.Conv2d(z_channels, block_in, 3, 1, 1)
 
-
         # middle
         self.mid = nn.Sequential(
-            ResnetBlock(in_channels=block_in, out_channels=block_in, dropout=dropout),
+            ResnetBlock(in_channels=block_in,
+                        out_channels=block_in, dropout=dropout),
             AttnBlock(block_in),
-            ResnetBlock(in_channels=block_in, out_channels=block_in, dropout=dropout)
+            ResnetBlock(in_channels=block_in,
+                        out_channels=block_in, dropout=dropout)
         )
 
         # upsampling
@@ -125,15 +125,15 @@ class Decoder(nn.Module):
             block_out = ch*ch_mult[i_level]
             for i_block in range(self.num_res_blocks+1):
                 blocks.append(ResnetBlock(in_channels=block_in,
-                                         out_channels=block_out,
-                                         dropout=dropout))
+                                          out_channels=block_out,
+                                          dropout=dropout))
                 if curr_res in attn_resolutions:
                     blocks.append(AttnBlock(block_out))
                 block_in = block_out
             if i_level != 0:
                 blocks.append(Upsample(block_in, resamp_with_conv))
                 curr_res = curr_res * 2
-            self.up.append(blocks) 
+            self.up.append(blocks)
 
         # end
         self.end = nn.Sequential(
@@ -153,23 +153,73 @@ class Decoder(nn.Module):
         h = self.up(h)
 
         # end
-        if self.give_pre_end:
-            return h
         h = self.end(h)
-        if self.tanh_out:
-            h = torch.tanh(h)
         return h
 
+
+class Autoencoder(nn.Module):
+    def __init__(self, *, ch=64, ch_mult=[1, 1, 2, 2, 4], num_res_blocks=2,
+                 attn_resolutions=[16], dropout=0.0, resamp_with_conv=True, resolution=256, z_channels=16, embed_dim=16):
+        super().__init__()
+        params = {
+            'ch': ch,
+            'in_channels': 1,
+            'out_ch': 1,
+            'ch_mult': ch_mult,
+            'num_res_blocks': num_res_blocks,
+            'attn_resolutions': attn_resolutions,
+            'dropout': dropout,
+            'resamp_with_conv': resamp_with_conv,
+            'resolution': resolution,
+            'z_channels': z_channels
+        }
+        self.encoder = Encoder(**params)
+        self.conv_before_reparm = torch.nn.Conv2d(2*z_channels, 2*embed_dim, 1)
+        self.conv_after_reparm = torch.nn.Conv2d(embed_dim, z_channels, 1)
+        self.decoder = Decoder(**params)
+
+    def encode(self, x):
+        h = self.encoder(x)
+        h = self.conv_before_reparm(h)
+        return h
+
+    def decode(self, z):
+        h = self.conv_after_reparm(z)
+        h = self.decoder(h)
+        return h
+
+    def reparameterization(self, z):
+        z_mean, z_logvar = torch.chunk(z, 2, dim=1)
+        z_logvar = torch.clamp(z_logvar, -30.0, 20.0)
+        z_std = torch.exp(0.5 * z_logvar)
+        z_var = torch.exp(z_logvar)
+        z = z_mean + z_std * torch.randn_like(z_mean).to(device=z_mean.device)
+
+        kl_dis = torch.pow(z_mean, 2) + z_var - 1.0 - z_logvar
+        kl_dis = 0.5 * torch.sum(kl_dis, dim=[1, 2, 3])
+
+        return z, kl_dis
+
+    def forward(self, x):
+        latent = self.encode(x)
+        z, kl_dis = self.reparameterization(latent)
+        recon = self.decode(z)
+        return recon, latent, kl_dis
+
+
 if __name__ == '__main__':
-    net = Encoder(double_z=True, z_channels=16, resolution=256, in_channels=1, ch=64, ch_mult=[
-        1, 1, 2, 2, 4], num_res_blocks=2, attn_resolutions=[16], dropout=0.0).cuda()
+    # net = Encoder(double_z=True, z_channels=16, resolution=256, in_channels=1, ch=64, ch_mult=[
+    #     1, 1, 2, 2, 4], num_res_blocks=2, attn_resolutions=[16], dropout=0.0).cuda()
 
+    # summary(net, input_size=(16, 1, 256, 256))
+    # print(net(torch.randn([16, 1, 256, 256]).cuda()).shape)
+
+    # net = Decoder(z_channels=16, resolution=256, ch=64, out_ch=1, ch_mult=[
+    #             1, 1, 2, 2, 4], num_res_blocks=2, attn_resolutions=[16], dropout=0.0).cuda()
+
+    # summary(net, input_size=(16, 16, 16, 16))
+    # print(net(torch.randn([16, 16, 16, 16]).cuda()).shape)
+
+    net = Autoencoder().cuda()
     summary(net, input_size=(16, 1, 256, 256))
-    print(net(torch.randn([16, 1, 256, 256]).cuda()).shape)
-
-    net = Decoder(z_channels=16, resolution=256, ch=64, out_ch=1, ch_mult=[
-                1, 1, 2, 2, 4], num_res_blocks=2, attn_resolutions=[16], dropout=0.0).cuda()
-
-    summary(net, input_size=(16, 16, 16, 16))
-    print(net(torch.randn([16, 16, 16, 16]).cuda()).shape)
-
+    print([out.shape for out in net(torch.randn([16, 1, 256, 256]).cuda())])
