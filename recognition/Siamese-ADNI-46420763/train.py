60d5f1d80a3dd42c094e50e6b4db3aa9e0ed7305
Aaron <>
2023-09-23 17:17:44 +1000

    FEAT prints batch and epoch accuracy in training

--- recognition/Siamese-ADNI-46420763/train.py
+++ recognition/Siamese-ADNI-46420763/train.py
@@ -1,6 +1,5 @@
 from modules import SiameseNetwork
 from dataset import * 
-from utils import ContrastiveLoss
 
 import torch
 import torch.nn as nn 
@@ -17,12 +16,12 @@ if not torch.cuda.is_available():
 
 # Training Parameters
 NUM_EPOCHS = 50
-LEARNING_RATE = 0.001
+LEARNING_RATE = 0.0001
 WEIGHT_DECAY = 0
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 
 # Model Parameters
-MODEL_LAYERS = [1, 32, 64, 128, 256]
+MODEL_LAYERS = [1, 32, 64, 128]
 
 MODEL_NAME = "ADNI-SiameseNetwork"
 DATASET_DIR = "./recognition/Siamese-ADNI-46420763/data/AD_NC"
@@ -33,7 +32,7 @@ def main():
     ######################    
     #   Retrieve Data:   #
     ######################
-    dataloader = train_dataloader(DATASET_DIR, BATCH_SIZE)
+    dataloader, valid_dataloader = train_dataloader(DATASET_DIR, BATCH_SIZE, 0.25)
     
     #########################   
     #   Initialize Model:   #
@@ -44,9 +43,8 @@ def main():
         model.load_state_dict(torch.load(MODEL_DIR, map_location=device))
 
     # Criterion and Optimizer:
-    criterion = ContrastiveLoss()
-    # criterion = nn.BCELoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY) 
+    criterion = nn.BCELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE) 
     
     ####################
     #   Train Model:   #
@@ -62,16 +60,54 @@ def main():
             labels = labels.to(device)
             
             # Forward pass
-            output1, output2, pred = model(images1, images2)
-           
-            loss = criterion(output1, output2, labels) # Contrastive Loss
-            # loss = criterion(pred, labels[:, None]) # BCE LOSS
+            pred = model(images1, images2)
+            loss = criterion(pred, labels[:, None])
             
             # Backward and optimize
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()     
-       
+            
+            pred = torch.Tensor.int(torch.round(pred)) 
+            labels = torch.Tensor.int(labels)[:, None]
+            
+            correct = (pred == labels).sum()/len(labels)
+            
+            # Print batch accuracy and loss
+            if (i % 10) == 0:
+                print("Epoch [{}/{}], Step [{}/{}]  loss: {:.5f}, acc: {:.5f}".format(
+                    epoch+1, NUM_EPOCHS, 
+                    i+1, total_step,
+                    loss.item(),
+                    correct.item()))
+
+        #######################   
+        #   Evaluate Epoch:   #
+        #######################
+        model.eval()
+        total_correct = 0
+        for i, (images1, images2, labels) in enumerate(valid_dataloader):
+            images1 = images1.to(device)
+            images2 = images2.to(device)
+            labels = labels.to(device)
+            
+            # Forward pass
+            pred = model(images1, images2)
+            
+            pred = torch.Tensor.int(torch.round(pred))
+            labels = torch.Tensor.int(labels)[:, None]
+
+            total_correct += (pred == labels).sum()
+            
+        print("Epoch [{}] Finished: val acc: {:.5f}".format(
+                    epoch+1, 
+                    total_correct/len(valid_dataloader.dataset)))
+    
+    end = time.time()
+    elapsed = end - start
+    print("Training took ", str(elapsed) + " secs or " + str(elapsed/60) + " mins in total")
+    print("END")
+    
 if __name__ == "__main__":
     main()
     
