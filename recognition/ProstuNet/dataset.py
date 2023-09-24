70b37d92e3440822e921ed37e264d923980defd6
Czek0 <czekowoski@gmail.com>
2023-09-24 12:31:57 +1000

    Data class updated with trainloader method testloader method method to opening nii file extensions init method that opens: - datafile - splits into test and train - creates trainloader testloader

--- recognition/ProstuNet/dataset.py
+++ recognition/ProstuNet/dataset.py
@@ -5,6 +5,15 @@ from tensorflow.keras import layers
 import tensorflow_datasets as tfds
 import matplotlib.pyplot as plt
 import numpy as np
+import torch
+import nibabel as nib
+import random
+import os
+import numpy as np
+from copy import deepcopy
+import matplotlib.pyplot as plt
+import glob
+from PIL import Image
 
 class Data:
     """
@@ -12,31 +21,105 @@ class Data:
 
     ...
 
-    Attributes
-    ----------
-    p : type
-        info
-        
     Methods
     -------
-    func(additional=""):
-        info about function
+    load_image_train(self, datapoint):
+        Loads image data set for training and performs preprocessing.
+
+    load_image_test(self, datapoint):
+        Loads image data set for testing and performs preprocessing.
+
+    resize(self, input_image, input_mask):
+        Resizesimages and masks to 128x128
+
+    flip(self, input_image, input_mask):
+        flips images and masks randomly.
+    
+    normalize(self, input_image, input_mask):
+        Rescaling real-valued images and masks into a compareable range 
+    
     """
 
-    def __init__(self, p):
+    def __init__(self):
         """
-        Constructs all the necessary attributes for the U-Net object.
-
-        Parameters
-        ----------
-            p : type
-                info
+        Constructs all the necessary attributes for the Data model.
         """
+        # import data here 
+        self.dataset = self.openData("v1/semantic_MRs_anon/*", 
+                      "v1/semantic_labels_anon/*")
+        # split the dataset
+        self.train, self.validation, self.test = torch.utils.data.random_split(self.dataset, [179, 16, 16])
+        # build input pipeline with data using map()
+        self.train_dataset = self.train.map(self.load_image_train, num_parallel_calls=tf.data.AUTOTUNE)
+        self.test_dataset = self.test.map(self.load_image_test, num_parallel_calls=tf.data.AUTOTUNE)
 
-        self.p = p
+    def openData(self, imagePath, maskPath):
+        self.inputs = []
+        self.masks = []
+        #retrieve path from dataset
+        for f in sorted(glob.iglob(image_path)): 
+            self.inputs.append(f)
+        for f in sorted(glob.iglob(mask_path)):
+            self.masks.append(f)
+        self.totensor = transforms.ToTensor()
+
+    def __len__(self):
+        return len(self.inputs)
+    
+    #open files
+    def __getitem__(self, idx): 
+        image_p = self.inputs[idx]
+        mask_p = self.masks[idx]
+
+        image = nib.load(image_p)
+        image = np.asarray(image.dataobj)
+
+        mask = nib.load(mask_p)
+        mask = np.asarray(mask.dataobj)
+        
+        image = self.totensor(image)
+        image = image.unsqueeze(0)
+        image = image.data
+
+        mask = self.totensor(mask)
+        mask = mask.unsqueeze(0)
+        mask = mask.data
+        
+        return image, mask
+
+
+    def load_image_train(self, datapoint):
+        ''' Loads image data set for training and performs preprocessing.
+            Augmentation is performed only on the training set.
+            Parameters:
+                datapoint: data subset for training
+            
+            Returns:
+                training data'''
+        input_image = datapoint["image"]
+        input_mask = datapoint["segmentation_mask"]
+        input_image, input_mask = self.resize(input_image, input_mask)
+        input_image, input_mask = self.augment(input_image, input_mask)
+        input_image, input_mask = self.normalize(input_image, input_mask)
+        return input_image, input_mask
+    
+    def load_image_test(self, datapoint):
+        ''' Loads image data set for testing and performs preprocessing.
+            Parameters:
+                datapoint: data subset for testing
+            
+            Returns:
+                testing data'''
+        input_image = datapoint["image"]
+        input_mask = datapoint["segmentation_mask"]
+        input_image, input_mask = self.resize(input_image, input_mask)
+        input_image, input_mask = self.normalize(input_image, input_mask)
+        return input_image, input_mask
+    
+
 
     def resize(self, input_image, input_mask):
-        ''' Resizesimages and masks to 128.
+        ''' Resizesimages and masks to 128x128.
             Parameters:
                 input_image: image
                 input_mask: mask
