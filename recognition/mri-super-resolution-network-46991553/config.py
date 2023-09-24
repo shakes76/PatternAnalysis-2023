de661713a14ee1561eacf13b9a985f3f3ecfa4bf
elijah-nelson <elijah.james.nelson@gmail.com>
2023-09-24 10:39:42 +1000

    Refactors constants into shared config file

---
+++ recognition/mri-super-resolution-network-46991553/config.py
@@ -0,0 +1,16 @@
+"""
+Hyperparameters and other meaningful constants
+"""
+
+# Factor to reduce the width and height of the images by
+dimension_reduce_factor = 2  # downsample factor of 4
+
+# Original dimensions
+original_width = 256
+original_height = 240
+
+# Directory to save images to
+image_dir = "imgs/"
+
+# Images per batch
+batch_size = 128
