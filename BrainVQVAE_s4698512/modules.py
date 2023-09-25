8a0262341270fb36a5c9ecb2bf5911208e8bb172
hugs7 <burto732@gmail.com>
2023-09-25 12:25:04 +1000

    Implemented ResBlock class

--- BrainVQVAE_s4698512/modules.py
+++ BrainVQVAE_s4698512/modules.py
@@ -17,25 +17,70 @@ import torchvision.transforms as transforms
 import numpy as np
 
 
-class VectorQuantised(nn.Module):
+class ResBlock(nn.Module):
+    """
+    Defines residual block layer
+    """
+
+    def __init__(self, dim):
+        super().__init__()
+        self.block = nn.Sequential(
+            nn.ReLU(True),
+            nn.Conv2d(dim, dim, 3, 1, 1),
+            nn.BatchNorm2d(dim),
+            nn.ReLU(True),
+            nn.Conv2d(dim, dim, 1),
+            nn.BatchNorm2d(dim)
+        )
+
+    def forward(self, x):
+        return x + self.block(x)
+
+
+class VectorQuantizedVAE(nn.Module):
     """
     Takes a tensor and quantises/discretises it
     """
 
-    def __init__(self, num_embeddings: int, embedding_dim: int, commitment_cost: int) -> None:
-        super(VectorQuantised, self).__init__()
+    def __init__(self, input_dim, dim, K=512):
+        super().__init__()
+        self.encoder = nn.Sequential(
+            nn.Conv2d(input_dim, dim, 4, 2, 1),
+            nn.BatchNorm2d(dim),
+            nn.ReLU(True),
+            nn.Conv2d(dim, dim, 4, 2, 1),
+            ResBlock(dim),
+            ResBlock(dim),
+        )
 
-        self._num_embeddings = num_embeddings
-        self._embedding_dim = embedding_dim
+        self.codebook = VQEmbedding(K, dim)
 
-        self._embedding = nn.Embedding(
-            self._num_embeddings, self._embedding_dim)
-        self._embedding.weight.data.uniform_(-1 /
-                                             self._num_embeddings, 1 / self._num_embeddings)
+        self.decoder = nn.Sequential(
+            ResBlock(dim),
+            ResBlock(dim),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(dim, dim, 4, 2, 1),
+            nn.BatchNorm2d(dim),
+            nn.ReLU(True),
+            nn.ConvTranspose2d(dim, input_dim, 4, 2, 1),
+            nn.Tanh()
+        )
 
-        self._commitment_cost = commitment_cost
+        self.apply(weights_init)
 
-    def forward(self, inputs):
-        """"""
+    def encode(self, x):
+        z_e_x = self.encoder(x)
+        latents = self.codebook(z_e_x)
+        return latents
 
-        pass
+    def decode(self, latents):
+        z_q_x = self.codebook.embedding(
+            latents).permute(0, 3, 1, 2)  # (B, D, H, W)
+        x_tilde = self.decoder(z_q_x)
+        return x_tilde
+
+    def forward(self, x):
+        z_e_x = self.encoder(x)
+        z_q_x_st, z_q_x = self.codebook.straight_through(z_e_x)
+        x_tilde = self.decoder(z_q_x_st)
+        return x_tilde, z_e_x, z_q_x
