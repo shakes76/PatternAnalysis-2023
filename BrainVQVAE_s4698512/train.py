1081dec91fa6b165f846b2a9fe662d86c085127a
hugs7 <burto732@gmail.com>
2023-09-25 23:37:29 +1000

    Implemented Training Loop Fixed issue with Hyperparameters passing into model Fixed Bug in hyperparameters channels should be 1 as grayscale adjusted pre transform for data

--- BrainVQVAE_s4698512/train.py
+++ BrainVQVAE_s4698512/train.py
@@ -17,13 +17,24 @@ from torchvision import transforms, datasets        # Will test on MNIST first
 from torchvision.utils import save_image, make_grid
 from tqdm import tqdm
 import os
+import matplotlib.pyplot as plt
 
 
 from modules import VectorQuantizedVAE
 from dataset import OASIS, ADNI
 
 
-def train(train_loader, model, optimiser, device, beta, steps):
+def train(train_loader: DataLoader, model: VectorQuantizedVAE, optimiser: torch.optim.Adam, device: torch.device, beta: int):
+    """
+    Inside of the training loop for the VQ-VAE
+    Args:
+        train_loader (DataLoader): the loader for the trainind data
+        model (VectorQuantizedVAE): the VQ-VAE Model
+        optimiser (torch.optim.Adam): The learning rate optimiser
+        device (torch.device): the device that the training runs on
+        beta (int): loss weight
+    """
+
     # Loop over the images in the data loader
     for images, _ in train_loader:
         # Move the images in the batch to the GPU
@@ -47,20 +58,46 @@ def train(train_loader, model, optimiser, device, beta, steps):
         fin_loss.backward()
 
         optimiser.step()
-        steps += 1
+    print("1 Training loop done")
 
 
-def test(test_loader, model, device, steps):
+def test(test_loader: DataLoader, model: VectorQuantizedVAE, device: torch.device):
     """
     Tests the model with testing set
 
     """
+    with torch.no_grad():
+        # Initialise losses to 0
+        recon_loss, vq_loss = 0, 0
+
+        # Loop over images in test_loader
+        for images, _ in test_loader:
+            # Move images to device
+            images = images.to(device)
+
+            # Generate samples
+            x_til, z_e_x, z_q_x = model(images)
+
+            # Add to loss using mean squared error
+            recon_loss += F.mse_loss(x_til, images)
+            vq_loss += F.mse_loss(z_q_x, z_e_x)
+
+        recon_loss /= len(test_loader)
+        vq_loss /= len(test_loader)
+
+        return recon_loss.item(), vq_loss.item()
 
 
 def generate_samples(images, model, device):
+    print(f"Generating samples on {device}")
     with torch.no_grad():
+        print(f"Passing images to {device}")
         images = images.to(device)
+        print(f"Images on {device}")
+
+        # Invoke forward pass on model
         x_tilde, _, _ = model(images)
+
     return x_tilde
 
 
@@ -81,14 +118,24 @@ def main():
 
     if dataset == "OASIS":
         # Load OASIS Dataset
+        # Greyscale data. Observe below in the transform I set num_output_channels = num_channels
         num_channels = 1
+        image_x, image_y = 256, 256
+        # Size/dimensions within latent space
+        hidden_dim = 32     # Number of neurons in each layer
+        K = 32      # Size of the codebook
+        # Dimensions of each vector within latent space
+        z_dim = 8
 
         oasis_data_path = os.path.join(".", "datasets", "OASIS")
+
         # Define a transformation to apply to the images (e.g., resizing)
         transform = transforms.Compose([
-            transforms.Resize((256, 256)),  # Adjust the size as needed
+            transforms.Resize((image_x, image_y)),  # Adjust the size as needed
+            transforms.Grayscale(num_output_channels=num_channels),
             transforms.ToTensor(),
         ])
+
         oasis = OASIS(oasis_data_path, transform=transform)
 
         train_loader = oasis.train_loader
@@ -130,63 +177,69 @@ def main():
     print(f"{dataset} Dataset Imported :)\n------\n")
 
     # Define Model and move to GPU
-    model = VectorQuantizedVAE(input_dim, hidden_dim, z_dim)
+    model = VectorQuantizedVAE(input_channels=num_channels,
+                               output_channels=num_channels, hidden_channels=hidden_dim, num_embeddings=K)
     model = model.to(device)
+    print("Model defined", model)
 
     # Optimiser as Adam
+    print("Initialising optimiser")
     optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
+    print("Optimiser initialised", optimiser)
 
     # Reconstruction Loss function. Use BCELoss
+    print("initialising Loss function")
     loss_fn = torch.nn.MSELoss(reduction="sum")
+    print("MSELoss initialised")
 
     # Train model
-    steps = 0
+    print("Retrieving first batch of images from test loader")
+    fixed_images, _ = next(iter(test_loader))
+    print(f"{len(fixed_images)} images loaded")
+    fixed_grid = make_grid(fixed_images, nrow=8, range=(-1, 1), normalize=True)
+    print("Fixed Grid computed")
+
+    # Generate the samples first once
+    reconstruction = generate_samples(fixed_images, model, device)
+    grid = make_grid(reconstruction.cpu(), nrow=8,
+                     range=(-1, 1), normalize=True)
+
+    print("Reconstruction Done")
+
+    save_filename = os.path.join(".", "ModelParams")
+    if not os.path.exists(save_filename):
+        os.makedirs(save_filename)
+    # Keep track of best loss so far and initialise to arbitrary -1
+    best_loss = -1.
+
     for epoch in range(num_epochs):
-        # use tqdm loop for progress bar
-        loop = tqdm(enumerate(train_loader))
-        for i, (x, y) in loop:
-            # Forward pass
-            # Move x to GPU
-            x = x.to(device).view(x.shape[0], input_dim)
+        train(train_loader, model, optimiser, device, beta)
+        loss, _ = test(validate_loader, model, device)
 
-            x_recon, mu, sigma = model(x)
+        print(f"Epoch [{epoch + 1}/{num_epochs}] Loss: {loss:.4f}")
 
-            # Compute Loss
+        # Save the model if it has the lowest validation loss so far
+        if (epoch == 0) or (loss < best_loss):
+            best_loss = loss
+            with open(f'{save_filename}/best.pt', 'wb') as f:
+                torch.save(model.state_dict(), f)
 
-            recon_loss = loss_fn(x_recon, x)
+        # Generate and display samples
+        with torch.no_grad():
+            reconstruction = generate_samples(fixed_images, model, device)
 
-            kl_divergence = - \
-                torch.sum(1 + torch.log(sigma.pow(2)) -
-                          mu.pow(2) - sigma.pow(2))
+        # Save the model at each epoch
+        with open(f'{save_filename}/model_{epoch + 1}.pt', 'wb') as f:
+            torch.save(model.state_dict(), f)
 
-            # Back propagation
-            loss = recon_loss + kl_divergence
-            optimiser.zero_grad()
-            loss.backward()
-            optimiser.step()
-            steps += 1
-            loop.set_postfix(loss=loss.item())
+    # Visualize the generated samples using matplotlib
+    fig, axes = plt.subplots(nrows=8, ncols=8, figsize=(12, 12))
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(reconstruction[i].cpu().numpy().squeeze(), cmap='gray')
+        ax.axis('off')
 
-    # call inference
-
-    for idx in range(10):
-        inference(idx, model, dataset)
-    # # Train
-    # train(train_loader, model, optimiser, device, beta, steps)
-
-    # # Perform Validation (mid epoch)
-    # loss, _ = test(validation_loader, model, device, steps)
-
-    # reconstruction = generate_samples(fixed_images, model, device)
-    # grid = make_grid(reconstruction.cpu(), nrow=8,
-    #                  range=(-1, 1), normalize=True)
-
-    # if (epoch == 0) or (loss < best_loss):
-    #     best_loss = loss
-    #     with open('{0}/best.pt'.format(save_filename), 'wb') as f:
-    #         torch.save(model.state_dict(), f)
-    # with open('{0}/model_{1}.pt'.format(save_filename, epoch + 1), 'wb') as f:
-    #     torch.save(model.state_dict(), f)
+    plt.tight_layout()
+    plt.show()
 
 
 def inference(digit, model, dataset):
