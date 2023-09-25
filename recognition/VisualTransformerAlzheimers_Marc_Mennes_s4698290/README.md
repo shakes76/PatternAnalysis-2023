fe82c46316848b603a2c8606803ed7dfb3ef003b
KingOfTheJuice <marcmennes@bigpond.com>
2023-09-25 14:28:17 +1000

    Added code to turn the raw data into a pytorch dataset

---
+++ recognition/VisualTransformerAlzheimers_Marc_Mennes_s4698290/README.md
@@ -0,0 +1,2 @@
+A visual transformer that identifies whether an MRI brain scan is of someone with Alzheimer's or not
+
---
+++ recognition/VisualTransformerAlzheimers_Marc_Mennes_s4698290/dataset.py
@@ -0,0 +1,47 @@
+import os
+from torchvision.io import read_image
+import torch
+
+#turns the ADNI raw data into a pytorch dataset to be used with a dataloader
+
+class ADNI(torch.utils.data.Dataset):
+
+    def __init__(self, path, transform=None, test=False):
+        self.path = path #path not ending with a /
+        self.transform = transform
+        self.isTest = test
+
+        #get a list of image names
+        if not test:
+            AD = os.listdir(self.path + "/AD_NC/train/AD")
+            NC = os.listdir(self.path + "/AD_NC/train/NC")
+        else:
+            AD = os.listdir(self.path + "/AD_NC/test/AD")
+            NC = os.listdir(self.path + "/AD_NC/test/NC")
+        
+        #map the images to indices for __getitem__
+        self.data = AD + NC
+        self.lastAD = len(AD) - 1
+
+    def __len__(self):
+        return len(self.data)
+    
+    def __getitem__(self, idx):
+        imageName = self.data[idx]
+        ADLabel = 0
+
+        #build the path to the image and find the label
+        if idx <= self.lastAD:
+            pathToImage = self.path + "/AD_NC" + ("/test" if self.isTest else "/train") + "/AD/"
+            ADLabel = 1
+        else:
+            pathToImage = self.path + "/AD_NC" + ("/test" if self.isTest else "/train") + "/NC/"
+        
+        image = read_image(pathToImage + imageName)
+
+        if self.transform is not None:
+            image = self.transform(image)
+
+        return image, ADLabel #ADLabel = 1 means alzheimer's, 0 means normal
+    
+
