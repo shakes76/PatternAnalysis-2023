35cd838fdd18b11a55bc1da9bb66c769b4c52da4
Mikhail Matveev <mish.matveev@gmail.com>
2023-09-25 18:40:57 +1000

    modified dataset.py to create both a training and a test dataloader, and also began writing the training loop in train.py

--- recognition/SubPixelCNN_4696671/dataset.py
+++ recognition/SubPixelCNN_4696671/dataset.py
@@ -9,6 +9,8 @@ import torchvision.datasets as datasets
 
 # Data Loading Parameters
 dataroot = ".\\ADNI\\AD_NC"
+test_suffix = "\\test"
+train_suffix = "\\train"
 batch_size = 128
 
 # Setup Data Transforms
@@ -16,15 +18,20 @@ transform = transforms.Compose([
     transforms.ToTensor()
 ])
 
-# Create data set from the data in the root
-dataset = datasets.ImageFolder(root=dataroot,transform=transform)
+# Create datasets from the data in the root
+train_dataset = datasets.ImageFolder(root=dataroot+train_suffix, transform=transform)
+test_dataset = datasets.ImageFolder(root=dataroot+test_suffix, transform=transform)
 
-# Create dataloader from dataset
-dataLoader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+# Create dataloaders from dataset
+train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
 # Function to get dataloader from other files
-def getDataLoader():
-    return dataLoader
+def get_train_loader():
+    return train_loader
+
+def get_test_loader():
+    return test_loader
 
 
 
--- recognition/SubPixelCNN_4696671/train.py
+++ recognition/SubPixelCNN_4696671/train.py
@@ -3,12 +3,14 @@ TRAINING LOOP
 """
 
 # Imports
-from dataset import getDataLoader
+from dataset import get_test_loader, get_train_loader
+from modules import ESPCN
 import torch
 import torchvision.utils
 import matplotlib.pyplot as plt
 import numpy as np
 import sys
+import time
 
 # PyTorch setup
 print("PyTorch Version:", torch.__version__)
@@ -16,12 +18,36 @@ print(torch.cuda.is_available())
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 sys.stdout.flush()
 
-# Vizualise some of the training data
-dataLoader = getDataLoader()
+# Get data loaders
+train_loader = get_train_loader()
+test_loader = get_test_loader()
 
-real_batch = next(iter(dataLoader))
+# Get Model
+model = ESPCN(1, 4)
+model = model.to(device)
+
+# Vizualise some of the training data
+real_batch = next(iter(train_loader))
 plt.figure(figsize=(8,8))
 plt.axis("off")
 plt.title("Training Images")
 plt.imshow(np.transpose(torchvision.utils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
 plt.show()
+
+# Training Parameters
+epochs = 10
+learning_rate = 0.001
+optimiser = torch.optim.Adam(model.parameters(), lr=learning_rate)
+scheduler = torch.optim.lr_scheduler.OneCycleLR(optimiser, 
+                                                max_lr=learning_rate, 
+                                                epochs=epochs,
+                                                steps_per_epoch=391)
+
+# Training Loop
+model.train()
+print("> Training")
+sys.stdout.flush()
+start = time.time()
+for epoch in range(epochs):
+    for i, data in enumerate(train_loader, 0):
+        pass
