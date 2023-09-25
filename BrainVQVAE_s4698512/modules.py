9f3ba1fae9ade2cccaa9adf0ff723509b27764cb
hugs7 <burto732@gmail.com>
2023-09-25 17:08:38 +1000

    Implemented VQEmbedding class for handling the codebook in the latent space

--- BrainVQVAE_s4698512/modules.py
+++ BrainVQVAE_s4698512/modules.py
@@ -14,7 +14,6 @@ import torch.nn.functional as F
 import torch.optim as optim
 
 import torchvision.transforms as transforms
-import numpy as np
 
 
 class ResBlock(nn.Module):
@@ -35,6 +34,34 @@ class ResBlock(nn.Module):
 
     def forward(self, x):
         return x + self.block(x)
+
+
+class VQEmbedding(nn.Module):
+    """
+    Codebook in the Latent Space
+    """
+
+    def __init__(self, K, D):
+        super().__init__()
+        self.embedding = nn.Embedding(K, D)
+        self.embedding.weight.data.uniform_(-1./K, 1./K)
+
+    def forward(self, z_e_x):
+        z_e_x_ = z_e_x.permute(0, 2, 3, 1).contiguous()
+        latents = vq(z_e_x_, self.embedding.weight)
+        return latents
+
+    def straight_through(self, z_e_x):
+        z_e_x_ = z_e_x.permute(0, 2, 3, 1).contiguous()
+        z_q_x_, indices = vq_st(z_e_x_, self.embedding.weight.detach())
+        z_q_x = z_q_x_.permute(0, 3, 1, 2).contiguous()
+
+        z_q_x_bar_flatten = torch.index_select(self.embedding.weight,
+                                               dim=0, index=indices)
+        z_q_x_bar_ = z_q_x_bar_flatten.view_as(z_e_x_)
+        z_q_x_bar = z_q_x_bar_.permute(0, 3, 1, 2).contiguous()
+
+        return z_q_x, z_q_x_bar
 
 
 class VectorQuantizedVAE(nn.Module):
@@ -70,7 +97,7 @@ class VectorQuantizedVAE(nn.Module):
         #     ResBlock(dim),
         # )
 
-        # self.codebook = VQEmbedding(K, dim)
+        self.codebook = VQEmbedding(K, dim)
 
         # self.decoder = nn.Sequential(
         #     ResBlock(dim),
