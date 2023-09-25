e1a3c613df4cb4acfd21e49d3136150bd233f7ab
hugs7 <burto732@gmail.com>
2023-09-25 12:15:54 +1000

    Defined init for VectorQuantised class

--- BrainVQVAE_s4698512/modules.py
+++ BrainVQVAE_s4698512/modules.py
@@ -7,3 +7,35 @@ modules.py
 Contains the source code of the components of my model
 Each component will be designated as a class
 """
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+
+import torchvision.transforms as transforms
+import numpy as np
+
+
+class VectorQuantised(nn.Module):
+    """
+    Takes a tensor and quantises/discretises it
+    """
+
+    def __init__(self, num_embeddings: int, embedding_dim: int, commitment_cost: int) -> None:
+        super(VectorQuantised, self).__init__()
+
+        self._num_embeddings = num_embeddings
+        self._embedding_dim = embedding_dim
+
+        self._embedding = nn.Embedding(
+            self._num_embeddings, self._embedding_dim)
+        self._embedding.weight.data.uniform_(-1 /
+                                             self._num_embeddings, 1 / self._num_embeddings)
+
+        self._commitment_cost = commitment_cost
+
+    def forward(self, inputs):
+        """"""
+
+        pass
