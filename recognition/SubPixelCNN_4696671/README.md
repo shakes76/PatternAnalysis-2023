18fc64e10346b3126a94b3bd8700aa7a2b9f54bc
Mikhail Matveev <mish.matveev@gmail.com>
2023-09-25 15:56:01 +1000

    Added code to load data and to create the dataloader for the model

--- recognition/SubPixelCNN_4696671/README.md
+++ recognition/SubPixelCNN_4696671/README.md
@@ -1,2 +1,3 @@
 # MRI Super-Resolution Network in PyTorch
 This is a python implementation of a MRI Super-Resolution Network that uses a Efficient Sub-Pixel CNN and is implemented in PyTorch.<br>
+It is trained on the ADNI dataset for Alzheimer's disease.
--- recognition/SubPixelCNN_4696671/dataset.py
+++ recognition/SubPixelCNN_4696671/dataset.py
@@ -1,3 +1,32 @@
 """
 LOADING AND TRANSFORMING THE DATA
 """
+
+# Imports
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import torchvision.datasets as datasets
+
+# Data Loading Parameters
+dataroot = ".\\ADNI\\AD_NC"
+batch_size = 128
+
+# Setup Data Transforms
+transform = transforms.Compose([
+    transforms.ToTensor()
+])
+
+# Create data set from the data in the root
+dataset = datasets.ImageFolder(root=dataroot,transform=transform)
+
+# Create dataloader from dataset
+dataLoader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+
+# Function to get dataloader from other files
+def getDataLoader():
+    return dataLoader
+
+
+
+
+
