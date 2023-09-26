850a3fc9552408270fc36777f8ba53cf867fc169
Alex Nguyen <alexnguyen@192-168-1-9.tpgi.com.au>
2023-09-26 10:12:44 +1000

    OASIS dataset dataloader template creation

---
+++ recognition/46429515_OASIS_brain_SD/dataset.py
@@ -0,0 +1,18 @@
+import torch
+import torchvision
+from torch.utils.data import Dataset, dataloader
+import numpy as np
+import math
+
+class OASISDataset(Dataset):
+    def __init__(self):
+        # data loading
+        pass
+    
+    def __getitem__(self, index):
+        # dataset[0]
+        pass
+    
+    def __len__(self):
+        # len(dataset)
+        pass
