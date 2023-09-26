27a848462faf0733cbf876eda9541897c18fd266
Mikhail Matveev <mish.matveev@gmail.com>
2023-09-26 20:37:31 +1000

    Added hidden layers, and expanded conv layers to have more filters

--- recognition/SubPixelCNN_4696671/modules.py
+++ recognition/SubPixelCNN_4696671/modules.py
@@ -15,9 +15,9 @@ class ESPCN(nn.Module):
 
         # Paddings are selected to ensure that output image is the same size as input for all hidden conv layers.
         # Kernel sizes selected from Figure 1 in paper.
-        self.conv1 = nn.Conv2d(in_channels, 64, kernel_size=5, padding=2)
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 32, kernel_size=3, padding=1)
+        self.conv1 = nn.Conv2d(in_channels, 128, kernel_size=5, padding=2)
+        self.conv2 = nn.Conv2d(128, 128, kernel_size=3, padding=1)
+        self.conv3 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
 
         # need to end up with r^2 filters, where r is the upscaling factor
         self.conv4 = nn.Conv2d(32, in_channels * (upscaling_factor ** 2), kernel_size=3, padding=1)
@@ -27,6 +27,12 @@ class ESPCN(nn.Module):
 
     def forward(self, x):
         x = self.conv1(x)
+        x = self.activation(x)
+
+        x = self.conv2(x)
+        x = self.activation(x)
+        
+        x = self.conv2(x)
         x = self.activation(x)
 
         x = self.conv2(x)
