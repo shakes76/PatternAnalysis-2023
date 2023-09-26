7aa15b0218f96414fb30a12fb2f842d9336ef673
Alex Nguyen <alexnguyen@192-168-1-9.tpgi.com.au>
2023-09-26 10:27:16 +1000

    Removal of custom oasis dataset class. Creation of dataloader from data folder

--- recognition/46429515_OASIS_brain_SD/dataset.py
+++ recognition/46429515_OASIS_brain_SD/dataset.py
@@ -1,18 +1,20 @@
-import torch
-import torchvision
-from torch.utils.data import Dataset, dataloader
-import numpy as np
-import math
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import DataLoader
 
-class OASISDataset(Dataset):
-    def __init__(self):
-        # data loading
-        pass
-    
-    def __getitem__(self, index):
-        # dataset[0]
-        pass
-    
-    def __len__(self):
-        # len(dataset)
-        pass
+root_path = 'data/keras_png_slices_data'
+
+# Define data transformations
+transform = transforms.Compose([transforms.Resize((224, 224)),
+                               transforms.ToTensor()])
+
+# Specifying paths to train, test and validate directories
+train_data = ImageFolder(root=f'{root_path}/keras_png_slices_train', transform=transform)
+test_data = ImageFolder(root=f'{root_path}/keras_png_slices_test', transform=transform)
+validate_data = ImageFolder(root=f'{root_path}/keras_png_slices_validate', transform=transform)
+
+# Create data loaders for each set
+batch_size = 64
+train_loader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
+test_loader = DataLoader(test_data, batch_size=batch_size)
+validate_loader = DataLoader(validate_data, batch_size=batch_size)
