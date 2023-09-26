84ceb80a7f359b236080580789097f11eb50de56
Aaron <>
2023-09-26 11:48:18 +1000

    UPDATE tweaked SNN - Decreased embedding length

--- recognition/Siamese-ADNI-46420763/modules.py
+++ recognition/Siamese-ADNI-46420763/modules.py
@@ -7,27 +7,30 @@ class ConvBlock(nn.Module):
         super(ConvBlock, self).__init__()
         self.conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.MaxPool2d(2, stride=2)
         )
         
     def forward(self, x):
         return self.conv(x)
     
 class SiameseNetwork(nn.Module):
-    def __init__(self, layers = [1, 32, 64, 128], kernel_sizes = [10, 7, 4, 4]):
+    def __init__(self, layers = [1, 64, 128, 128, 256], kernel_sizes = [10, 7, 4, 4]):
         super(SiameseNetwork, self).__init__()
         self.layers = layers
         self.blocks = nn.ModuleList([ConvBlock(layers[i], layers[i+1], kernel_size=kernel_sizes[i]) for i in range(len(layers) - 1)])
+        self.maxpool = nn.MaxPool2d(2, stride=2)
         self.linear = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(128*25*27, 512)
+            nn.Linear(256 * 8 * 7, 256)
         )
-        self.dense = nn.Linear(512, 1)
+        self.dense = nn.Linear(256, 1)
         
     def forward_once(self, x):
-        for block in self.blocks:
+        for i, block in enumerate(self.blocks):
             x = block(x)
+            if i != len(self.blocks) - 1:
+                x = self.maxpool(x)
         x = self.linear(x)
         x = F.sigmoid(x)
         return x
@@ -38,5 +41,4 @@ class SiameseNetwork(nn.Module):
         distance = torch.abs(embedding1 - embedding2)
         output = self.dense(distance)
         output = F.sigmoid(output)
-        return output
-    
+        return embedding1, embedding2, output
