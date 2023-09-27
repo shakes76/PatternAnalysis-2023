1da6d969ae10e4f1d7ecb8878026f705bd508c96
Tzu-Yu Yang <125327045+jyz523@users.noreply.github.com>
2023-09-27 18:57:49 +1000

    Update train.py

--- recognition/train.py
+++ recognition/train.py
@@ -1,1 +1,49 @@
+import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader
+from modules import get_maskrcnn_model
+from dataset import ISICDataset
+import matplotlib.pyplot as plt
+
+model = get_maskrcnn_model(num_classes=2)  # 1 class (lesion) + 1 background
+optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+
+transform = transforms.Compose([
+    transforms.Resize((256, 256)),
+    transforms.ToTensor(),
+])
+
+# path to train images
+img_train_dir = 'C:\\Users\\yangj\\Desktop\\COMP3710 Project\\ISIC-2017_Training_Data'
+# path to train masks
+mask_train_dir = 'C:\\Users\\yangj\\Desktop\\COMP3710 Project\\ISIC-2017_Training_Data'
+
+train_dataset = ISICDataset(img_dir=img_train_dir, mask_dir=mask_train_dir, transform=transform)
+train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
+
+# Define the number of epochs
+epochs = 10
+
+# Training loop
+losses = []
+for epoch in range(epochs):
+    for images, targets in train_loader:
+        optimizer.zero_grad()
+        loss_dict = model(images, targets)
+        loss = sum(loss for loss in loss_dict.values())
+
+        loss.backward()
+        optimizer.step()
+        losses.append(loss.item())
+
+
+# path to save model
+path_to_saved_model = 'C:\\Users\\yangj\\Desktop\\COMP3710 Project\\Save_Model'
+torch.save(model.state_dict(), path_to_saved_model)
+
+# Plotting losses
+plt.plot(losses)
+plt.xlabel('Iterations')
+plt.ylabel('Loss')
+plt.show()
 
