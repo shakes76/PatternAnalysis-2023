046914c6515da8fa9259e5134f265cb58ae77f7e
Tzu-Yu Yang <125327045+jyz523@users.noreply.github.com>
2023-09-27 18:56:36 +1000

    Update dataset.py

--- recognition/dataset.py
+++ recognition/dataset.py
@@ -1,1 +1,59 @@
+import torch
+from torch.utils.data import Dataset
+import matplotlib.pyplot as plt
+from matplotlib.patches import Rectangle
+import pandas as pd
+import numpy as np
+import os
+from PIL import Image
+import torchvision.transforms as T
+
+device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+
+def get_transform(train):
+    transforms = []
+    transforms.append(T.PILToTensor())
+    transforms.append(T.ConvertImageDtype(torch.float))
+    return T.Compose(transforms)
+class ISICDataset(Dataset):
+    def __init__(self, img_dir, mask_dir, transform=None):
+        self.img_dir = img_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.image_files = [f for f in os.listdir(img_dir) if f.endswith(('.jpg', '.png'))]
+
+    def __len__(self):
+        return len(self.image_files)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img_dir, self.image_files[idx])
+        mask_path = os.path.join(self.mask_dir, self.image_files[idx].replace('.jpg', '.png'))
+
+        image = Image.open(img_path).convert("RGB")
+        mask = Image.open(mask_path).convert("L")
+
+        if self.transform:
+            image = self.transform(image)
+            mask = self.transform(mask)
+
+        # Compute bounding boxes from masks
+        pos = np.where(np.array(mask) > 0)
+        xmin = np.min(pos[1])
+        xmax = np.max(pos[1])
+        ymin = np.min(pos[0])
+        ymax = np.max(pos[0])
+        boxes = torch.tensor([[xmin, ymin, xmax, ymax]], dtype=torch.float32)
+
+        # Create target dictionary
+        target = {
+            "boxes": boxes,
+            "labels": torch.ones((1,), dtype=torch.int64),
+            "masks": mask,
+            "image_id": torch.tensor([idx]),
+            "area": (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0]),
+            "iscrowd": torch.zeros((1,), dtype=torch.int64)
+
+        }
+
+        return image, target
 
