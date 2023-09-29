74575e0cc14c6038e108d03fae8dc4024fe2ec75
Rodger Xiang <rodxiang2@gmail.com>
2023-09-29 18:42:44 +1000

    refractor code into main method

--- recognition/ViT_46425067/train.py
+++ recognition/ViT_46425067/train.py
@@ -9,44 +9,6 @@ from tqdm.auto import tqdm
 #setup random seeds
 torch.manual_seed(42)
 torch.cuda.manual_seed(42)
-#device agnostic code
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-#hyperparmeters
-config = SimpleNamespace(
-    epochs=1,
-    batch_size=32,
-    img_size=(224, 224),
-    patch_size=16,
-    img_channel=1,
-    num_classes=2,
-    embed_dim=768,
-    depth=1,
-    num_heads=3,
-    mlp_ratio=4,
-    qkv_bias=True,
-    drop_prob=0.1,
-    lr=1e-3
-)
-
-#load dataloaders
-train_loader, test_load, _, _ = load_data(config.batch_size, config.img_size)
-
-#create model
-model = ViT(img_size=config.img_size[0],
-            patch_size=config.patch_size,
-            img_channels=config.img_channel,
-            num_classes=config.num_classes,
-            embed_dim=config.embed_dim,
-            depth=config.depth,
-            num_heads=config.num_heads,
-            mlp_ratio=config.mlp_ratio,
-            qkv_bias=config.qkv_bias,
-            drop_prob=config.drop_prob).to(device)
-
-#loss function + optimiser
-loss_fn = nn.CrossEntropyLoss()
-optimiser = optim.AdamW(model.parameters(), lr=config.lr)
 
 
 def train_epoch(model: nn.Module, 
@@ -57,7 +19,7 @@ def train_epoch(model: nn.Module,
     train_loss, train_acc = 0, 0
     model.train()
     
-    for batch, (X, y) in enumerate(data_loader):
+    for batch, (X, y) in enumerate(tqdm(data_loader)):
         X, y = X.to(device), y.to(device)
         y_pred = model(X)
         loss = loss_fn(y_pred, y)
@@ -119,11 +81,57 @@ def train_model(model: nn.modules,
                                             data_loader=test_loader,
                                             loss_fn=loss_fn,
                                             device=device)
-        
         # save results
         results["train_loss"].append(train_loss)
         results["train_acc"].append(train_acc)
         results["test_loss"].append(test_loss)
         results["test_acc"].append(test_acc)
-        
     return results
+
+if __name__ == "__main__":
+    #device agnostic code
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    
+    #hyperparmeters
+    config = SimpleNamespace(
+        epochs=1,
+        batch_size=32,
+        img_size=(224, 224),
+        patch_size=16,
+        img_channel=1,
+        num_classes=2,
+        embed_dim=768,
+        depth=1,
+        num_heads=3,
+        mlp_ratio=4,
+        qkv_bias=True,
+        drop_prob=0.1,
+        lr=1e-3
+    )
+    
+    #load dataloaders
+    train_loader, test_loader, _, _ = load_data(config.batch_size, config.img_size)
+    #create model
+    model = ViT(img_size=config.img_size[0],
+                patch_size=config.patch_size,
+                img_channels=config.img_channel,
+                num_classes=config.num_classes,
+                embed_dim=config.embed_dim,
+                depth=config.depth,
+                num_heads=config.num_heads,
+                mlp_ratio=config.mlp_ratio,
+                qkv_bias=config.qkv_bias,
+                drop_prob=config.drop_prob).to(device)
+    #loss function + optimiser
+    loss_fn = nn.CrossEntropyLoss()
+    optimiser = optim.AdamW(model.parameters(), lr=config.lr)
+    
+    #Train the model and store the results
+    results = train_model(model=model,
+                            train_loader=train_loader,
+                            test_loader=test_loader,
+                            optimiser=optimiser,
+                            loss_fn=loss_fn,
+                            device=device,
+                            config=config)
+    print(results)
