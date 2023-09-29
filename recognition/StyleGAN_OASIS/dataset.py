43ae8854d1a7a80967e2da92244f00316b68a511
peterdbeardsley <peter.d.beardsley@gmail.com>
2023-09-29 13:44:01 +1000

    Created a dataload that scales by image size and batch size which is required for a progressive GAN.

--- recognition/StyleGAN_OASIS/dataset.py
+++ recognition/StyleGAN_OASIS/dataset.py
@@ -2,8 +2,31 @@
 """
 File: dataset.py
 
-This is a placeholder file
+Purpose: Tools required to load and preprocess the data
 
 @author: Peter Beardsley
 """
 
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader
+
+"""
+Create a data loader:
+    image_folder    references the folder containing the classes of images
+    image_size      resize the images to this size
+    batch_size      the number of images the data loader should batch
+
+Notes: When training a progressive GAN, a dataloader is required for each
+       progressive image size. As image size increases, the batch size must
+       decrease to accommodate the increased memory requirements.
+"""
+def createDataLoader(image_folder, image_size, batch_size):
+  
+    transform = transforms.Compose([transforms.Resize((image_size, image_size))
+                                    , transforms.ToTensor()
+                                    , transforms.Normalize((0.5,), (0.5,))])
+
+    dataset = datasets.ImageFolder(root=image_folder, transform=transform)
+
+    dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+    return dataloader
