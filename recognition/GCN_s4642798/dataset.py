9e722eec54e2531a7bd2b42e5d7ed0685572b41f
William Barker <william.barker.2002@gmail.com>
2023-09-29 12:50:46 +1000

    Added self loops to edge tensor

--- recognition/GCN_s4642798/dataset.py
+++ recognition/GCN_s4642798/dataset.py
@@ -31,6 +31,13 @@ print("Sample Size: {}".format(sample_size))
 print("Number of Features: {}".format(number_features))
 print("Number of Classes: {}".format(number_classes))
 
+#adding self loops to the tensor
+self_loops = torch.eye(sample_size)
+sparse_self_loops = torch.nonzero(self_loops, as_tuple=False)
+condition = edges[:, 0] != edges[:, 1]
+edges = edges[condition]
+edges = torch.cat((edges, sparse_self_loops, sparse_self_loops), 0)
+
 # Generate training and testing mask
 train_split = 0.7
 train_size = int(train_split * sample_size)
