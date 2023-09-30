64866b2730215d560bcc298ee4e222e7dd0dac99
peterdbeardsley <peter.d.beardsley@gmail.com>
2023-09-30 14:46:43 +1000

    Added the Generator class and all its required subclasses, including the synthesis blocks and mapping network.

--- recognition/StyleGAN_OASIS/modules.py
+++ recognition/StyleGAN_OASIS/modules.py
@@ -180,8 +180,187 @@ class MappingNetwork(nn.Module):
         
         self.relu = torch.nn.LeakyReLU(0.2)
     
+    """
+    Feed forward:
+       x    The normalised output of latent z
+    """ 
     def forward(self, x):
         for fc in self.mappingNetwork:
             x = self.relu(fc(x))
             
         return x
+"""
+Adaptive Instance Normalisation PyTorch module
+     channels   Number of channels of the synthesis layer
+     w_size     The size of the Mapping Network output, w
+     
+Notes: scale and bias are derived from w by using an equaliser linear layer. This
+       is typically done by outputing twice the channels but I've opted for 
+       two seperate linear modules instead for simplicity
+"""
+class AdaIN(nn.Module):
+    def __init__(self, channels, w_size):
+        super(AdaIN, self).__init__()
+        self.instance_norm = nn.InstanceNorm2d(channels)
+        self.style_scale = LinearHe(w_size, channels)
+        self.style_bias = LinearHe(w_size, channels)
+
+    """
+    Feed forward:
+        x   The signal to feed into AdaIN
+        w   The manifold output
+    """
+    def forward(self, x, w):
+        x = self.instance_norm(x)
+        style_scale = self.style_scale(w).unsqueeze(2).unsqueeze(3)
+        style_bias = self.style_bias(w).unsqueeze(2).unsqueeze(3)
+        return style_scale * x + style_bias
+    
+"""
+The B module from the style-based generator architecture that connects the noise
+to the AdaIN.
+     channels   Number of channels of the synthesis layer
+    
+Notes: weights are initialised to zero.
+"""
+class B(nn.Module):
+    def __init__(self, channels):
+        super(B, self).__init__()
+        self.weight = nn.Parameter(torch.zeros(1, channels, 1, 1))
+
+    """
+    Feed forward:
+        noise   Generate from torch.randn((Batches, 1, img_size, img_size), device=device)
+    """
+    def forward(self, noise):
+        return self.weight * noise
+    
+"""
+The first synthesis block of the StyleGAN generator architecture. This is similar
+to the following blocks except for the constant 4x4 input.
+     ch_in  The number of channels for the constant, typically 512
+     ch_out The number of channels to output from the block
+     w_size     The size of the Mapping Network output, w
+"""
+class SynthesisInitialBlock(nn.Module):
+    def __init__(self, ch_in, ch_out, w_size):
+        super(SynthesisInitialBlock, self).__init__()
+        self.activate = nn.LeakyReLU(0.2, inplace=True)
+        self.const = nn.Parameter(torch.ones((1, ch_in, 4, 4)))
+        self.B1 = B(ch_out)
+        self.adaIN1 = AdaIN(ch_out, w_size)
+        self.conv2 = Conv2dHe(ch_out, ch_out)
+        self.B2 = B(ch_out)
+        self.adaIN2 = AdaIN(ch_out, w_size)
+ 
+    """
+    Feed Forward:
+        a   The manifold output w
+    
+    Note: x is generated from a constant 4x4
+    """
+    def forward(self, a):
+        x = self.const
+        
+        b1 = self.B1(torch.randn((x.shape[0], 1, x.shape[2], x.shape[3]), device=a.device))
+        b2 = self.B2(torch.randn((x.shape[0], 1, x.shape[2], x.shape[3]), device=a.device))
+        
+        x = x + b1
+        x = self.adaIN1(x, a)
+        x = x + b2
+        x = self.adaIN2(x, a)
+        x = self.activate(x)
+        return x
+
+"""
+The scaling synthesis blocks of the StyleGAN generator architecture. 
+     ch_in  The number of channels for the previous block
+     ch_out The number of channels to output from the block
+     w_size     The size of the Mapping Network output, w
+"""
+class SynthesisBlock(nn.Module):
+    def __init__(self, ch_in, ch_out, w_size):
+        super(SynthesisBlock, self).__init__()
+        self.activate = nn.LeakyReLU(0.2, inplace=True)
+        self.conv1 = Conv2dHe(ch_in, ch_out)
+        self.B1 = B(ch_out)
+        self.adaIN1 = AdaIN(ch_out, w_size)
+        self.conv2 = Conv2dHe(ch_out, ch_out)
+        self.B2 = B(ch_out)
+        self.adaIN2 = AdaIN(ch_out, w_size)
+
+    """
+    Feed Forward:
+        x   The output of the previous layer
+        a   The manifold output w
+    """
+    def forward(self, x, a):
+        b1 = self.B1(torch.randn((x.shape[0], 1, x.shape[2], x.shape[3]), device=x.device))
+        b2 = self.B2(torch.randn((x.shape[0], 1, x.shape[2], x.shape[3]), device=x.device))
+        
+        x = self.conv1(x)
+        x = self.activate(x + b1)
+        x = self.adaIN1(x, a)
+        x = self.activate(x + b2)
+        x = self.adaIN2(x, a)
+
+        return x
+    
+"""
+A PyTorch module implementation of the style-based generator architecture. The
+main components are:
+    RMS(z) -> Mapping Network -> w
+    Synthesis Network for layer depth d:
+        [0]: Const -> + B(Noise) -> + AdaIN(w) -> Conv + B(Noise) -> + AdaIN(w)
+        [d]: Upsample -> Conv + B(Noise) -> + AdaIN(w) -> Conv + B(Noise) -> + AdaIN(w)
+    RGB Output [d]: Conv (channels to RGB channels)
+
+Parameters:
+    z_size      Size of latent z
+    w_size      Size of manifold w
+    channels    np.array[d] for channels at layer d
+    rgb_ch      Number of RGB channels (3 for this project, 1 for grayscale)
+    alphaSched  The AlphaScheduler object for managing the progresive GAN fading
+"""
+class Generator(nn.Module):
+    def __init__(self, z_size, w_size, channels, rgb_ch, alphaSched):
+        super(Generator, self).__init__()
+        self.alphaSched = alphaSched
+        self.normalise = RMS()
+        self.mappingNetwork = MappingNetwork(z_size, w_size)
+        self.synthesisNetwork = nn.ModuleList()
+        self.rgbOutput = nn.ModuleList()
+        
+        self.synthesisNetwork.append(SynthesisInitialBlock(channels[0], channels[0], w_size))
+        self.rgbOutput.append(Conv2dHe(channels[0], rgb_ch, kernel_size=1, stride=1, padding=0))
+        
+        for d in range(len(channels) - 1):
+            ch_in = int(channels[d])
+            ch_out = int(channels[d + 1])
+            self.synthesisNetwork.append(SynthesisBlock(ch_in, ch_out, w_size))
+            self.rgbOutput.append(Conv2dHe(ch_out, rgb_ch, kernel_size=1, stride=1, padding=0))
+            
+    def forward(self, z):
+        # Normalise latent z, then pass to the mapping network
+        w = self.mappingNetwork(self.normalise(z))
+
+        # Scale depth based on the current AlphaScheduler depth
+        for depth in range(self.alphaSched.depth+1):
+            # First depth must be the SynthesisInitialBlock
+            if isinstance(self.synthesisNetwork[depth], SynthesisInitialBlock):
+                out = self.synthesisNetwork[0](w)
+            # Subsequent blocks of SynthesisBlocks
+            else:
+                upsample = F.interpolate(out, scale_factor=2, mode="bilinear")
+                out = self.synthesisNetwork[depth](upsample, w)
+        
+        # Transform from the current depth to RGB
+        rgb_out = self.rgbOutput[self.alphaSched.depth](out)        
+        
+        # If fading, combine with the upscaled RGB output of the previous depth
+        if self.alphaSched.is_fade:
+            rgb_out_previous = self.rgbOutput[self.alphaSched.depth - 1](upsample)
+            rgb_out = self.alphaSched.alpha * rgb_out + (1 - self.alphaSched.alpha) * rgb_out_previous
+        
+        # Map to a pixel value
+        return torch.tanh(rgb_out)
