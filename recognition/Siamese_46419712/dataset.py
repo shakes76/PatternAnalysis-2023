6b4d89685ef9b7b8c779a20ea09f3132943a0b8f
Minh Tieu <s4641971@uqconnect.edu.au>
2023-10-02 17:57:44 +1000

    test the data correctly load and transforms

--- recognition/Siamese_46419712/dataset.py
+++ recognition/Siamese_46419712/dataset.py
@@ -1,45 +1,63 @@
 import torchvision
 import torch
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
 
 
 def load_train_data():
-    dataset = torchvision.datasets.ImageFolder(root="/home/groups/comp3710/ADNI/train", transform=transforms.ToTensor())
-
-    # Calculate the mean and standard deviation
-    mean = torch.mean(dataset.data.float() / 255.0)
-    std = torch.std(dataset.data.float() / 255.0)
+    path="/home/groups/comp3710/ADNI/AD_NC/train"
+    # this for testing
+    path="./AD_NC/train"
 
     # Data transformation
     transform_train = transforms.Compose([
-        transforms.Resize(128),
-        transforms.RandomHorizontalFlip(),
-        transforms.RandomRotation(15),
-        transforms.CenterCrop(128),
+        transforms.Resize(105),
         transforms.ToTensor(),
-        transforms.Normalize(mean, std) # standard scaling for normalize, doesn't know much on the status of the entire dataset
     ])
 
-    trainset = torchvision.datasets.ImageFolder(root="/home/groups/comp3710/ADNI/train", transform=transform_train)
+    # transform_train = transforms.Compose([
+    #     transforms.Resize(105),
+    #     transforms.RandomHorizontalFlip(),
+    #     transforms.CenterCrop(105),
+    #     transforms.ToTensor(),
+    #     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    # ])
+
+    trainset = torchvision.datasets.ImageFolder(root=path, transform=transform_train)
     train_loader = torch.utils.data.DataLoader(trainset, batch_size=128, shuffle=True, num_workers=0)
+
+    data_iter = iter(train_loader)
+    images, _ = next(data_iter)
+
+    # Check the number of channels in the first image
+    num_channels = images.size(1)
+    print(f"Number of channels in the first image: {num_channels}")
+
 
     return train_loader
 
 def load_test_data():
+    path="/home/groups/comp3710/ADNI/AD_NC/test"
+    # this for testing
+    path="./AD_NC/test"
+    # dataset = torchvision.datasets.ImageFolder(root=path, transform=transforms.ToTensor())
 
-    dataset = torchvision.datasets.ImageFolder(root="/home/groups/comp3710/ADNI/train", transform=transforms.ToTensor())
-
-    # Calculate the mean and standard deviation
-    mean = torch.mean(dataset.data.float() / 255.0)
-    std = torch.std(dataset.data.float() / 255.0)
+    # # Calculate the mean and standard deviation
+    # mean = torch.mean(dataset.data.float() / 255.0)
+    # std = torch.std(dataset.data.float() / 255.0)
 
     # Data transformation
     transform_test = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize(mean, std) # standard scaling for normalize, doesn't know much on the status of the entire dataset
+        transforms.Normalize(0.5, 0.5) # standard scaling for normalize, doesn't know much on the status of the entire dataset
     ])
 
-    testset = torchvision.datasets.ImageFolder(root="/home/groups/comp3710/ADNI/test", transform=transform_test)
+    testset = torchvision.datasets.ImageFolder(root=path, transform=transform_test)
 
     test_loader = torch.utils.data.DataLoader(testset, batch_size=128, shuffle=True, num_workers=0)
     return test_loader
+
+if __name__ == '__main__':
+    # test dataset
+
+    load_train_data()
