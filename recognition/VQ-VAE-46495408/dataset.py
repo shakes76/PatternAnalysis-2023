0e7ebdb654c5803fd7a7e45b52541536a0e03f8c
MandalorianForce <yi.zhou6@uqconnect.edu.au>
2023-10-03 15:28:06 +1000

    Added validation and test data

--- recognition/VQ-VAE-46495408/dataset.py
+++ recognition/VQ-VAE-46495408/dataset.py
@@ -8,4 +8,20 @@ train_ds = tf.keras.utils.image_dataset_from_directory(
     image_size=(256, 256)
 )
 
-print(train_ds)
+validate_ds = tf.keras.utils.image_dataset_from_directory(
+    'recognition/VQ-VAE-46495408/keras_png_slices_data/keras_png_slices_validate',
+    label_mode=None,
+    color_mode='grayscale',
+    batch_size=128,
+    image_size=(256, 256)
+)
+
+test_ds = tf.keras.utils.image_dataset_from_directory(
+    'recognition/VQ-VAE-46495408/keras_png_slices_data/keras_png_slices_test',
+    label_mode=None,
+    color_mode='grayscale',
+    batch_size=128,
+    image_size=(256, 256)
+)
+
+print(test_ds)
