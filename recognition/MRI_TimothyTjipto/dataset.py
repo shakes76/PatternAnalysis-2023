fd85dd3e4a2f9e5136afa12e4792b28f9f964260
sv-Tjipto <113236886+sv-Tjipto@users.noreply.github.com>
2023-10-03 16:23:39 +1000

    brief change on dataset.py

--- recognition/MRI_TimothyTjipto/dataset.py
+++ recognition/MRI_TimothyTjipto/dataset.py
@@ -1,1 +1,12 @@
 '''Data loader for loading and preprocessing data'''
+
+import torch
+import numpy as np
+import matplotlib.pyplot as plt
+from torchvision import datasets,transforms
+from torch.utils.data import DataLoader
+
+# Load and return normalized data
+def load_data(path, img_size, colour):
+
+    dataset = datasets.ImageFolder(root=path, transform=transforms)
