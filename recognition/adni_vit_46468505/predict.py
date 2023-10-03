7c6f5f1a857fc93bb64cab043f0f9dfa0b94001e
Georgia Power <s4646850@student.uq.edu.au>
2023-10-04 09:05:36 +1000

    changed file paths

--- recognition/adni_vit_46468505/predict.py
+++ recognition/adni_vit_46468505/predict.py
@@ -13,7 +13,7 @@ test_dir = './recognition/adni_vit_46468505/test/'
 test_set = tf_dataset(x_test,y_test,batch_size =len(y_test))
 
 vit_classifier = create_vit_classifier()
-vit_classifier.load_weights('./recognition/adni_vit_46468505/model_checkpoints/model_256bit_acc0.7271.h5')
+vit_classifier.load_weights('./recognition/adni_vit_46468505/model_256bit_acc0.7271.h5')
 vit_classifier=model_compile(vit_classifier)
 
 loss,acc = vit_classifier.evaluate(test_set,verbose=1)
