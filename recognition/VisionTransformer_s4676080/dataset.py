9b0411e7370b913328b54fdf420778296e63ca07
Shriyan-Garg <gargshriyan23@gmail.com>
2023-10-05 02:00:56 +1000

    Added data augmentation and normalization transforms

--- recognition/VisionTransformer_s4676080/dataset.py
+++ recognition/VisionTransformer_s4676080/dataset.py
@@ -34,3 +34,20 @@ class AlzheimerDataset(Dataset):
             image = self.transform(image)                
         label = 1 if os.path.basename(os.path.dirname(image_path)) == 'AD' else 0
         return image, label
+
+# Data augmentation and normalization for training
+train_transforms = transforms.Compose([
+    transforms.RandomResizedCrop(224),
+    transforms.RandomHorizontalFlip(),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+
+# Just normalization for validation and test
+test_transforms = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize([0.485, 0.456, 0.406], [0.229, 0.224, 0.225])
+])
+    
