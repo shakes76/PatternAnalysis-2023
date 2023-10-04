026128029cd4acf92414c88703f9795a98dfa8a7
MandalorianForce <yi.zhou6@uqconnect.edu.au>
2023-10-04 21:54:14 +1000

    Define the get code indices function in VQ module

--- recognition/VQ-VAE-46495408/dataset.py
+++ recognition/VQ-VAE-46495408/dataset.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import matplotlib.pyplot as plt
 
 train_ds = tf.keras.utils.image_dataset_from_directory(
     'recognition/VQ-VAE-46495408/keras_png_slices_data/keras_png_slices_train',
@@ -27,4 +28,10 @@ test_ds = tf.keras.utils.image_dataset_from_directory(
 
 normalization_layer = tf.keras.layers.Rescaling(1./255, offset=-0.5)
 train_ds = train_ds.map(lambda x: normalization_layer(x))
-print(train_ds)
+plt.figure(figsize=(10,10))
+plt.title("Sample of images from OASIS train dataset")
+for i in range(9):
+    plt.subplot(3, 3, i+1)
+    plt.imshow(next(iter(train_ds))[i])
+    plt.axis('off')
+plt.show()
