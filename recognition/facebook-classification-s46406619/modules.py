a92361062c0b913dfde859b76cf1e3556b40fee2
Kristian Norved <kristiannorved@gmail.com>
2023-10-04 14:57:23 +1000

    Moved all existing training code in modules.py out of modules.py and into train.py

--- recognition/facebook-classification-s46406619/modules.py
+++ recognition/facebook-classification-s46406619/modules.py
@@ -16,24 +16,3 @@ class GCN(torch.nn.Module):
     
     def accuracy(self, true, predict):
         return (true == predict).sum() / len(predict)
-    
-    def train(self, data, criterion, optimizer, num_epoch):
-        for epoch in range(num_epoch):
-            optimizer.zero_grad() # clear gradients
-            h, z = self(data.X, data.edges) # forward pass
-            
-            # disregard all test labels from criterion
-            y_train = data.target
-            for i in range(len(z)):
-                if data.split_indices[i] == 1: # remove all test elements
-                    z = torch.cat([z[0:i], z[i+1:]])
-                    y_train = torch.cat([y_train[0:i], y_train[i+1:]])
-
-            loss = criterion(z, y_train) # calculate loss
-            acc = self.accuracy(y_train, z.argmax(dim=1)) # calculate accuracy
-            loss.backward() # compute gradients
-            optimizer.step() # tune parameters
-            
-            # print metrics every 10 epochs
-            if epoch % 10 == 0:
-                print(f'Epoch {epoch:>3} | Loss: {loss:.2f} | Acc: {acc*100:.2f}%')
--- recognition/facebook-classification-s46406619/train.py
+++ recognition/facebook-classification-s46406619/train.py
@@ -5,11 +5,29 @@ from dataset import *
 def run_training(lr=0.02, num_epoch=100):
     data = load_data()
     model = GCN()
-    print(model)
+    print('\n', model, '\n')
 
     optimizer = torch.optim.Adam(model.parameters(), lr=lr)
     criterion=torch.nn.CrossEntropyLoss()
 
-    model.train(data, criterion, optimizer, num_epoch)
+    for epoch in range(num_epoch):
+        optimizer.zero_grad() # clear gradients
+        h, z = model(data.X, data.edges) # forward pass
+        
+        # disregard all test labels from criterion
+        y_train = data.target
+        for i in range(len(z)):
+            if data.split_indices[i] == 1: # remove all test elements
+                z = torch.cat([z[0:i], z[i+1:]])
+                y_train = torch.cat([y_train[0:i], y_train[i+1:]])
 
-run_training()
+        loss = criterion(z, y_train) # calculate loss
+        acc = model.accuracy(y_train, z.argmax(dim=1)) # calculate accuracy
+        loss.backward() # compute gradients
+        optimizer.step() # tune parameters
+        
+        # print metrics every 5 epochs
+        if epoch % 5 == 0:
+            print(f'Epoch {epoch:>3} | Loss: {loss:.2f} | Acc: {acc*100:.2f}%')
+
+run_training(num_epoch=50)
