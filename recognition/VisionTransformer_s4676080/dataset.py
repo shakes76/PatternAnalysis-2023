07891305a58059d0d09641b4b1c117f2545af50f
Shriyan-Garg <gargshriyan23@gmail.com>
2023-10-05 01:42:15 +1000

    Loaded the required imports and defined the AlzheimerDatset class

--- recognition/VisionTransformer_s4676080/dataset.py
+++ recognition/VisionTransformer_s4676080/dataset.py
@@ -1,1 +1,12 @@
 # This is the dataset file
+
+#Importing all the required libraries
+import os
+from torchvision import transforms
+from torch.utils.data import Dataset, DataLoader
+from PIL import Image
+
+class AlzheimerDataset(Dataset):
+    def __init__(self, root_dir, transform=None, num_AD=0, num_NC=0):
+        self.root_dir = root_dir
+        self.transform = transform
