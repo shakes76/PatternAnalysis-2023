3f687f2e161b497204fe1276248b91c90a99844a
peterdbeardsley <peter.d.beardsley@gmail.com>
2023-10-04 12:16:18 +1000

    Simplified the design of AlphaScheduler by specifing epochs per depth rather than batch iterations by depth

--- recognition/StyleGAN_OASIS/modules.py
+++ recognition/StyleGAN_OASIS/modules.py
@@ -21,10 +21,6 @@ Define an alpha scheduler:
     fade_epochs     np.array of size max_depth-1 (or more) for number of epochs per
                     depth level in which to fade with the previous depth level. Index
                     0 is depth 1.
-    hold_epochs     np.array of the size max_depth-1 (or more) for number of epochs per
-                    depth level in which alpha will be held a 1 before progresing
-                    to the next depth. Index 0 is depth 1.
-    max_depth       Int describing how deep to control the fading
     batch_sizes     List of batch sizes per depth level
     data_size       Int of number of images in an epoch
     
@@ -36,59 +32,33 @@ Notes: This is a scheduler for controlling the fading factor alpha that will
        required number of epochs.
 """
 class AlphaScheduler():
-    def __init__(self, fade_epochs, hold_epochs, max_depth, batch_sizes, data_size):
+    def __init__(self, fade_epochs, batch_sizes, data_size):
         # Convert epochs into total number of iterations, AKA steps.
-        self.fade_steps = np.array([np.ceil(fade_epochs[i]*data_size/batch_sizes[i]) for i in range(len(batch_sizes))])
-        self.hold_steps = np.array([np.ceil(hold_epochs[i]*data_size/batch_sizes[i]) for i in range(len(batch_sizes))])
-        self.max_depth = max_depth
+        self.fade_steps = np.array([np.ceil(fade_epochs[i]*data_size/batch_sizes[i])+2 for i in range(len(batch_sizes))])
         # Alpha is just the inverse of the number of steps
         self.alpha_steps = 1/self.fade_steps
         self.depth = 1
         self.steps = 0
-        self.alpha = 0
-        self.is_fade = False
+        self.alpha = self.alpha_steps[0]
+        self.new_depth = False
         
     """
-    Signal the scheduler that an epoch iteration has finished. This will generally
-    increment self.alpha, finish fading and hold, or switch to the next depth.
+    Step the alpha after every batch iteration
     """
-    def step(self):
+    def stepAlpha(self):
+        self.alpha += self.alpha_steps[self.depth-1]
+        self.alpha = min(self.alpha, 1)
 
-        self.steps += 1
-        # Within a fade, so increment alpha
-        if self.steps < self.fade_steps[self.depth-1]:
-            self.alpha += self.alpha_steps[self.depth-1]
-            self.is_fade = True
-        # Fade has finished but need to keep holding at alpha = 1
-        elif self.steps < self.fade_steps[self.depth-1] + self.hold_steps[self.depth-1]:
-            self.alpha = 1
-            self.is_fade = False
-        # Fade and hold are finished, time to move to next depth
-        elif self.depth < self.max_depth:
-            self.depth += 1
-            self.alpha = 0
-            self.steps = 0
-            self.is_fade = True
+    """
+    Steh the depth after all epoch runs per depth
+    """
+    def stepDepth(self):
+        self.depth += 1
+        if self.depth-1 < len(self.alpha_steps):
+            self.alpha = self.alpha_steps[self.depth-1]
         else:
-            self.depth = self.max_depth
+            # When finished, just hold at 1
             self.alpha = 1
-    """
-    Return the current alpha value
-    """
-    def alpha(self):
-        return self.alpha
-    
-    """
-    Return the current depth level
-    """
-    def depth(self):
-        return self.depth
-    
-    """
-    Return if fading is currently scheduled (that is, 0<alpha<1)
-    """
-    def is_fade(self):
-        return self.is_fade
 
 
 
