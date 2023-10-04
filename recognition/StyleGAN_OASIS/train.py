1f0e67f714c2dcf9997351311a1a5e8a9864357f
peterdbeardsley <peter.d.beardsley@gmail.com>
2023-10-04 22:11:20 +1000

    Expanded writing of loss data to include the Wasserstein distance, gradient penality and gradient clipping losses. All losses now write into one csv file rather than multiple files.

--- recognition/StyleGAN_OASIS/train.py
+++ recognition/StyleGAN_OASIS/train.py
@@ -20,8 +20,10 @@ https://blog.paperspace.com/implementation-stylegan-from-scratch/ which get
 their code from the gradient_penalty function for WGAN-GP loss.
 
 Note: This code is very specific and crafting it independently is beyond the
-scope of this course. So with full references and acknowledgement I am using
-it as is, unaltered.
+scope of this course. So with full reference and acknowledgement I am using
+it as is, unaltered. Credit to Abd Elilah TAUIL. The source provided no
+explanation for how it works, so after my own research, I have detailed
+its inner workings in the README.md
 """
 def gradient_penalty(discriminator, real, fake, device):
     BATCH_SIZE, C, H, W = real.shape
@@ -63,14 +65,17 @@ def train_stylegan(cfg, device):
     
     
     # initialize optimizers
+    # Some StyleGAN implementations scale the Mapping Network weights by 0.01 to reduce the learning rate. I've
+    # come across this alternative approach a few times and prefer it for its explicit transparency. It specifically
+    # allocates a different learning rate for the mappingNetwork parameter.
     optimiser_generator = optim.Adam([{"params": [param for name, param in generator.named_parameters() if "mappingNetwork" not in name]},
-                            {"params": generator.mappingNetwork.parameters(), "lr": cfg['mapping_lr']}], lr=cfg['lr'], betas=(0.0, 0.99))
-    optimiser_discriminator = optim.Adam(discriminator.parameters(), lr=cfg['lr'], betas=(0.0, 0.99))
+                            {"params": generator.mappingNetwork.parameters(), "lr": cfg['mapping_lr']}], lr=cfg['lr'], betas=(cfg['beta1'], cfg['beta2']))
+    optimiser_discriminator = optim.Adam(discriminator.parameters(), lr=cfg['lr'], betas=(cfg['beta1'], cfg['beta2']))
     
     generator.train()
     discriminator.train()
     # start at step that corresponds to img size that we set in config
-    for layer_epochs in cfg['fade_epochs'][1:]:
+    for layer_epochs in cfg['fade_epochs']:
         loader = createDataLoader(cfg['image_folder'], 4 * 2 ** alphaSched.depth, cfg['batch_sizes'][alphaSched.depth-1])  
     
         for epoch in range(layer_epochs):
@@ -84,43 +89,49 @@ def train_stylegan(cfg, device):
                 discriminator_fake = discriminator(fake.detach())                           # Discriminate the fake images
                 
                 """
-                The discriminator loss calculation is not my work. The technique
-                was obtained from https://blog.paperspace.com/implementation-stylegan-from-scratch/
+                The discriminator loss calculation is not my work. I have adjusted
+                the code to improve its interperability. The technique was
+                obtained from https://blog.paperspace.com/implementation-stylegan-from-scratch/
                 with full credit to auther Abd Elilah TAUIL. In my observation, 
                 this code needed to run exactly as written or the StyleGAN fails 
-                to converge. To get this working, I had to remove the Sigmoid()
-                function from the discriminator.
+                to converge and suffers from mode collapse. To get this working, 
+                I had to remove the Sigmoid() function from the discriminator.
+                See README.md for detailed explainations.
                 """
                 gp = gradient_penalty(discriminator, real, fake, device=device)
-                loss_discriminator = (
-                    -(torch.mean(discriminator_real) - torch.mean(discriminator_fake))
-                    + cfg['lambda'] * gp
-                    + (0.001 * torch.mean(discriminator_real ** 2))
-                )
+                weighted_gradient_penalty = cfg['lambda'] * gp
+                wasserstein_distance = -(torch.mean(discriminator_real) - torch.mean(discriminator_fake))
+                gradient_clipping = (0.001 * torch.mean(discriminator_real ** 2))
+                loss_discriminator = wasserstein_distance + weighted_gradient_penalty + gradient_clipping
         
-                # 
+                # Back propogate the discriminator losses
                 discriminator.zero_grad()
                 loss_discriminator.backward()
                 optimiser_discriminator.step()
         
+                # Calculate the Generator loss
                 generator_fake = discriminator(fake)
                 loss_generator = -torch.mean(generator_fake)
         
+                # Back propogate the generator loss
                 generator.zero_grad()
                 loss_generator.backward()
                 optimiser_generator.step()
                 
-                with open(cfg['discriminator_loss_file'], "a") as f:                                              # Write out this loss
-                    np.savetxt(f, [loss_discriminator.detach().cpu()], fmt="%1.3f")
-                    
-                with open(cfg['generator_loss_file'], "a") as f:                                                  # Write out this loss
-                    np.savetxt(f, [loss_generator.detach().cpu()], fmt="%1.3f")
-                
+                # Write out batch losses for post-training analysis
+                with open(cfg['losses_file'], "a") as f:
+                    np.savetxt(f, [[weighted_gradient_penalty.detach().cpu()
+                                   , wasserstein_distance.detach().cpu()
+                                   , gradient_clipping.detach().cpu()
+                                   , loss_discriminator.detach().cpu()
+                                   , loss_generator.detach().cpu()]])
+                               
                 alphaSched.stepAlpha()
                 print('Epoch:', epoch+1,"/",layer_epochs, "   Depth:", alphaSched.depth, "   Alpha:", alphaSched.alpha)
 
     
         torch.save(generator.state_dict(), cfg['generator_model_file'].format(alphaSched.depth))
+        torch.save(discriminator.state_dict(), cfg['discriminator_model_file'].format(alphaSched.depth))
         alphaSched.stepDepth()
 
 
@@ -128,25 +139,23 @@ def train_stylegan(cfg, device):
 Train a styleGAN on the OASIS brain dataset using a predefined configuration
 """
 def train_stylegan_oasis(is_rangpur=True):
-    cfg_win = {'epochs':20, 'batch_sizes':[256,128,128,64,16,8,8], 'channels':[512, 512,256,128,64,32,16,4], 'rgb_ch':3
+    cfg_win = {'epochs':20, 'batch_sizes':[256,128,128,64,16,8,8], 'channels':[512,512,256,128,64,32,16,4], 'rgb_ch':3
             , 'fade_epochs': np.array([1, 1, 1, 1, 1, 1, 1])
             , 'depth': 6
             , 'image_folder': r'C:\Temp\keras_png_slices_data\keras_png_slices_data'
             , 'generator_model_file': 'C:\Temp\stylegan_depth_{0}.pth'
             , 'discriminator_model_file': 'C:\Temp\gan_dis_depth_{0}.pth'
-            , 'discriminator_loss_file': r'C:\Temp\losses_discriminator.csv'
-            , 'generator_loss_file': r'C:\Temp\losses_generator.csv'
+            , 'losses_file': 'C:\Temp\losses.csv'
             , 'lr':0.001, 'mapping_lr':0.00001, 'lambda':10, 'beta1': 0.0, 'beta2': 0.999, 'z_size':256, 'w_size':256, 'img_size': 256}
         
 
-    cfg_rangpur = {'epochs':20, 'batch_sizes':[256,128,128,64,16,8,8], 'channels':[512, 512,256,128,64,32,16,4], 'rgb_ch':3
+    cfg_rangpur = {'epochs':20, 'batch_sizes':[256,128,128,64,16,8,8], 'channels':[512,512,256,128,64,32,16,4], 'rgb_ch':3
             , 'fade_epochs': np.array([30, 30, 30, 30, 30, 30, 30])
             , 'depth': 6
             , 'image_folder': 'data/keras_png_slices_data'
             , 'generator_model_file': 'stylegan_depth_{0}.pth'
             , 'discriminator_model_file': 'gan_dis_depth_{0}.pth'
-            , 'discriminator_loss_file': 'losses_discriminator.csv'
-            , 'generator_loss_file': 'losses_generator.csv'
+            , 'losses_file': 'losses.csv'
             , 'lr':0.001, 'mapping_lr':0.00001, 'lambda':10, 'beta1': 0.0, 'beta2': 0.999, 'z_size':256, 'w_size':256, 'img_size': 256}
 
     if is_rangpur:
