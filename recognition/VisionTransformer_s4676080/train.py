4c40b93ed6146a561be27bbf4bfa3b89d1492307
Shriyan-Garg <gargshriyan23@gmail.com>
2023-10-05 22:23:33 +1000

    imported libraries and basic setup

--- recognition/VisionTransformer_s4676080/train.py
+++ recognition/VisionTransformer_s4676080/train.py
@@ -1,1 +1,11 @@
 # This is the train file
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+from dataset import train_loader, test_loader
+from modules import VisionTransformer
+import matplotlib.pyplot as plt
+
+# Checking for GPU
+device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
