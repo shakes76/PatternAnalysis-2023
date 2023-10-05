4d0b9a14165237f4697c8a70f186d8981022d6ef
hugs7 <burto732@gmail.com>
2023-10-05 18:24:03 +1000

    Refactored predict code into it's own function
    Added comments for what the predict function does
    
    Modularised predict function to be able to handle
    both ADNI and OASIS datasets
    
    Defined a main function in predict to initialise some basic settings

--- BrainVQVAE_s4698512/predict.py
+++ BrainVQVAE_s4698512/predict.py
@@ -13,92 +13,125 @@ import torch
 import matplotlib.pyplot as plt
 from torchvision.utils import make_grid
 from torchvision import transforms
-from dataset import OASIS
+from dataset import ADNI, OASIS
 # Replace with the actual module where your model is defined
 from modules import VectorQuantisedVAE
 from train import generate_samples
 
-# Device configuration
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Hyperparameters. These can be arbitrary as it's actually read from the save file
-num_channels = 1
+def predict(dataset: str, num_samples: int, device: torch.device, model: str = "best.pt"):
+    """
+    Takes a trained model of a dataset and generates (fake) samples.
+    Stores them in a folder and also displays a sample of the samples.
 
-# Input image resolution
-image_x, image_y = 256, 256
+    Args:
+        dataset (str): OASIS or ADNI dataset
+        num_samples (int): Number of samples to generate
+        device (torch.device): The device used to generate the samples on
+        model (str): name of the model save file used to generate samples
+    """
 
-# Size/dimensions within latent space
-hidden_dim = 32     # Number of neurons in each layer
-K = 32      # Size of the codebook
+    # Hyperparameters. These can be arbitrary as it's actually read from the save file
+    num_channels = 1
 
-# Peturbance
-noise_scale = 0.001
+    # Input image resolution
+    if dataset == "OASIS":
+        image_x, image_y = 256, 256
+    elif dataset == "ADNI":
+        image_x, image_y = 240, 256
 
-# Define model.
-model = VectorQuantisedVAE(input_channels=num_channels,
-                           output_channels=num_channels, hidden_channels=hidden_dim, num_embeddings=K)
-# Load the saved model
-model.load_state_dict(torch.load(os.path.join(".", "ModelParams", "best.pt"))
-                      )  # Update the path accordingly
-# Move model to CUDA GPU
-model = model.to(device)
-# Set the model to evaluation mode
-model.eval()
+    # Size/dimensions within latent space
+    hidden_dim = 32     # Number of neurons in each layer
+    K = 32      # Size of the codebook
 
-oasis_data_path = os.path.join(".", "datasets", "OASIS")
+    # Peturbance
+    noise_scale = 0.001
 
+    # Define model.
+    model = VectorQuantisedVAE(input_channels=num_channels,
+                               output_channels=num_channels, hidden_channels=hidden_dim, num_embeddings=K)
+    # Load the saved model
+    model.load_state_dict(torch.load(os.path.join(".", "ModelParams", "best.pt"))
+                          )  # Update the path accordingly
+    # Move model to CUDA GPU
+    model = model.to(device)
+    # Set the model to evaluation mode
+    model.eval()
 
-# Define a transformation to apply to the images (e.g., resizing)
-transform = transforms.Compose([
-    transforms.Resize((image_x, image_y)),  # Adjust the size as needed
-    transforms.Grayscale(num_output_channels=num_channels),
-    transforms.ToTensor(),
-])
+    data_path = os.path.join(".", "datasets", dataset)
 
-oasis = OASIS(oasis_data_path, transform=transform)
+    # Define a transformation to apply to the images (e.g., resizing)
+    transform = transforms.Compose([
+        transforms.Resize((image_x, image_y)),  # Adjust the size as needed
+        transforms.Grayscale(num_output_channels=num_channels),
+        transforms.ToTensor(),
+    ])
 
-# Define a data loader for generating samples
-sample_loader = oasis.test_loader
+    if dataset == "OASIS":
+        data_object = OASIS(data_path, copy=False, transform=transform)
+    elif dataset == "ADNI":
+        data_object = ADNI(data_path, copy=False,
+                           transform=transform, sampleType="AD")
 
-# Get a batch of data for generating samples
-sample_images, _ = next(iter(sample_loader))
+    # Define a data loader for generating samples
+    sample_loader = data_object.test_loader
 
+    # Get a batch of data for generating samples
+    sample_images, _ = next(iter(sample_loader))
 
-def generate_peturbed_samples(images, model, device, noise_scale=0.1):
+    def generate_peturbed_samples(images, model, device, noise_scale=0.1):
+        with torch.no_grad():
+            images = images.to(device)
+
+            # Invoke forward pass on model
+            x_tilde, _, _ = model.forward_peturb(images, noise_scale)
+
+        return x_tilde
+
+    # Generate samples
     with torch.no_grad():
-        images = images.to(device)
+        # Replace your_fixed_images with actual data
+        fake_images = generate_samples(
+            sample_images, model, device)
 
-        # Invoke forward pass on model
-        x_tilde, _, _ = model.forward_peturb(images, noise_scale)
+    # Create the generated samples directory if it doesn't exist
+    output_dir = f"./{dataset}_generated"
+    os.makedirs(output_dir, exist_ok=True)
 
-    return x_tilde
+    # Generate and save each sample
+    for i in range(len(fake_images)):
+        sample = fake_images[i].cpu().numpy().squeeze()
+        sample_filename = os.path.join(output_dir, f'fake_{i + 1:03d}.png')
+        plt.imsave(sample_filename, sample, cmap='gray')
+
+    # Visualize and save the generated samples
+    fig, axes = plt.subplots(nrows=8, ncols=8, figsize=(12, 12))
+    for i, ax in enumerate(axes.flat):
+        ax.imshow(fake_images[i].cpu().numpy().squeeze(), cmap='gray')
+        ax.axis('off')
+
+    plt.tight_layout()
+    plt.savefig(os.path.join(output_dir, 'samples.png'))  # Save the figure
+
+    # Display the generated samples
+    plt.show()
 
 
-# Generate samples
-with torch.no_grad():
-    # Replace your_fixed_images with actual data
-    fake_images = generate_samples(
-        sample_images, model, device)
+def main():
+    # Dataset selection
+    dataset = "ADNI"
 
-# Create the "OASIS_generated" directory if it doesn't exist
-output_dir = './OASIS_generated'
-os.makedirs(output_dir, exist_ok=True)
+    # Number of samples to generate
+    num_samples = 128
 
-# Generate and save each sample
-for i in range(len(fake_images)):
-    sample = fake_images[i].cpu().numpy().squeeze()
-    sample_filename = os.path.join(output_dir, f'fake_{i + 1:03d}.png')
-    plt.imsave(sample_filename, sample, cmap='gray')
+    # Device configuration
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Set the model to use the best one trained
+    model = "best.pt"
+
+    predict(dataset, num_samples, device, model)
 
 
-# Visualize and save the generated samples
-fig, axes = plt.subplots(nrows=8, ncols=8, figsize=(12, 12))
-for i, ax in enumerate(axes.flat):
-    ax.imshow(fake_images[i].cpu().numpy().squeeze(), cmap='gray')
-    ax.axis('off')
-
-plt.tight_layout()
-plt.savefig(os.path.join(output_dir, 'samples.png'))  # Save the figure
-
-# Display the generated samples
-plt.show()
+if __name__ == "__main__":
+    main()
