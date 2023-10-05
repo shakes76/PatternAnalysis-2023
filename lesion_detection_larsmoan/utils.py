17090b1ae4b3de59f5afeb1aad8524a26bd9cc98
Lars Moan <lars.ostberg.moan@gmail.com>
2023-10-05 18:13:46 +1000

    Added dataset and fixed gitignore

---
+++ lesion_detection_larsmoan/utils.py
@@ -0,0 +1,18 @@
+import os
+from pathlib import Path
+from dotenv import load_dotenv
+import pandas as pd
+
+
+# Note: you need a .env file in the root of your repository with REPO_DATA_DIR: "path" for this method to work
+def get_data_dir():
+    load_dotenv()
+    dir_name = "REPO_DATA_DIR"
+    if dir_name in os.environ:
+        path = os.getenv(dir_name)
+    else:
+        return Exception(
+            "No data directory found in the .env file or missing a .env file in the repo's root"
+        )
+    p = Path(path)
+    return p
