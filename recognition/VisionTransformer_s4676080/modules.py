5b79e3565e9275f08ca7d4189b80a946bf36579e
Shriyan-Garg <gargshriyan23@gmail.com>
2023-10-05 21:47:31 +1000

    Defned PatchEmbedding class in modules.py

--- recognition/VisionTransformer_s4676080/modules.py
+++ recognition/VisionTransformer_s4676080/modules.py
@@ -1,1 +1,15 @@
 # this is the modules file
+
+import torch
+import torch.nn as nn
+
+class PatchEmbedding(nn.Module):
+    def __init__(self, in_channels=3, patch_size=16, emb_size=768):
+        super().__init__()
+        self.patch_size = patch_size
+        self.proj = nn.Conv2d(in_channels, emb_size, kernel_size=patch_size, stride=patch_size)
+
+    def forward(self, x):
+        x = self.proj(x)
+        x = x.flatten(2).transpose(1, 2)  
+        return x
