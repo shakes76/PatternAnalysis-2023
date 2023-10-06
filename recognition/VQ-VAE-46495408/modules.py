9e43d2ea6d97b31a0e03c2baca5b9127f9bb5dd6
MandalorianForce <yi.zhou6@uqconnect.edu.au>
2023-10-06 14:20:08 +1000

    Added the decoder to the model

--- recognition/VQ-VAE-46495408/modules.py
+++ recognition/VQ-VAE-46495408/modules.py
@@ -64,3 +64,11 @@ def get_encoder(latent_dim=16):
     x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
     encoder_outputs = layers.Conv2D(latent_dim, 1, padding="same")(x)
     return keras.Model(encoder_inputs, encoder_outputs, name="encoder")
+
+def get_decoder(latent_dim=16):
+    latent_inputs = keras.Input(shape=get_encoder(latent_dim).output.shape[1:])
+    x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(
+        latent_inputs)
+    x = layers.Conv2DTranspose(32, 3, activationo="relu", strides=2, padding="same")(x)
+    decoder_outputs = layers.Conv2DTranspose(1, 3, padding="same")(x)
+    return keras.Model(latent_inputs, decoder_outputs, name="decoder")
