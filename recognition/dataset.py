a77bf2a1f913220553756f4702d92ce5d7d77bc2
FanHaoZeng <1992344969@qq.com>
2023-10-06 19:42:06 +1000

    Finalize loading functions

--- recognition/dataset.py
+++ recognition/dataset.py
@@ -54,7 +54,8 @@ def load_images(folder_path, target_size=(256, 256), extensions=[".jpg", ".png"]
 
     return np.array(resized_images)
 
-def load_training_set(test_folder_path, test_ground_truth_folder_path):
+
+def load_training_set(path):
     """
     Load the training set images and masks
 
@@ -62,13 +63,91 @@ def load_training_set(test_folder_path, test_ground_truth_folder_path):
     :return: numpy array of images and masks
     """
 
+    train_folder_path = f"{path}/ISIC2018_Task1-2_Training_Input_x2"
+    train_ground_truth_folder_path = f"{path}/ISIC2018_Task1_Training_GroundTruth_x2"
+
+    # Check if the folder_path exists
+    if not os.path.exists(train_folder_path):
+        raise FileNotFoundError(f"The folder path '{train_folder_path}' does not exist.")
+
+    if not os.path.exists(train_ground_truth_folder_path):
+        raise FileNotFoundError(f"The folder path '{train_ground_truth_folder_path}' does not exist.")
+
+    # Get list of image and mask filenames
+    image_filenames = [f for f in os.listdir(train_folder_path) if f.endswith('.jpg')]
+    mask_filenames = [f for f in os.listdir(train_ground_truth_folder_path) if f.endswith('_segmentation.png')]
+
+    # Create sets of the base names without the extensions or '_segmentation'
+    image_basenames = set([os.path.splitext(f)[0] for f in image_filenames])
+    mask_basenames = set([os.path.splitext(f)[0].replace('_segmentation', '') for f in mask_filenames])
+
+    # Check if the sets are equal, indicating a one-to-one correspondence
+    if image_basenames != mask_basenames:
+        raise ValueError("The images and masks are not in a one-to-one correspondence.")
+
+    image, mask = load_images(train_folder_path), load_images(train_ground_truth_folder_path)
+
+    print("Completed loading training set")
+    print("#" * 100)
+
+    return image, mask
+
+
+def load_val_set(path):
+    """
+    Load the training set images and masks
+
+    :param folder_path: path to folder containing images and masks
+    :return: numpy array of images and masks
+    """
+
+    val_folder_path = f"{path}/ISIC2018_Task1-2_Validation_Input"
+    val_ground_truth_folder_path = f"{path}/ISIC2018_Task1_Validation_GroundTruth"
+
+    # Check if the folder_path exists
+    if not os.path.exists(val_folder_path):
+        raise FileNotFoundError(f"The folder path '{val_folder_path}' does not exist.")
+
+    if not os.path.exists(val_ground_truth_folder_path):
+        raise FileNotFoundError(f"The folder path '{val_ground_truth_folder_path}' does not exist.")
+
+    # Get list of image and mask filenames
+    image_filenames = [f for f in os.listdir(val_folder_path) if f.endswith('.jpg')]
+    mask_filenames = [f for f in os.listdir(val_ground_truth_folder_path) if f.endswith('_segmentation.png')]
+
+    # Create sets of the base names without the extensions or '_segmentation'
+    image_basenames = set([os.path.splitext(f)[0] for f in image_filenames])
+    mask_basenames = set([os.path.splitext(f)[0].replace('_segmentation', '') for f in mask_filenames])
+
+    # Check if the sets are equal, indicating a one-to-one correspondence
+    if image_basenames != mask_basenames:
+        raise ValueError("The images and masks are not in a one-to-one correspondence.")
+
+    image, mask = load_images(val_folder_path), load_images(val_ground_truth_folder_path)
+
+    print("Completed loading validation set")
+    print("#" * 100)
+
+    return image, mask
+
+
+def load_test_set(path):
+    """
+    Load the training set images and masks
+
+    :param folder_path: path to folder containing images and masks
+    :return: numpy array of images and masks
+    """
+
+    test_folder_path = f"{path}/ISIC2018_Task1-2_Test_Input"
+    test_ground_truth_folder_path = f"{path}/ISIC2018_Task1_Test_GroundTruth"
+
     # Check if the folder_path exists
     if not os.path.exists(test_folder_path):
         raise FileNotFoundError(f"The folder path '{test_folder_path}' does not exist.")
 
     if not os.path.exists(test_ground_truth_folder_path):
         raise FileNotFoundError(f"The folder path '{test_ground_truth_folder_path}' does not exist.")
-
 
     # Get list of image and mask filenames
     image_filenames = [f for f in os.listdir(test_folder_path) if f.endswith('.jpg')]
@@ -82,8 +161,12 @@ def load_training_set(test_folder_path, test_ground_truth_folder_path):
     if image_basenames != mask_basenames:
         raise ValueError("The images and masks are not in a one-to-one correspondence.")
 
-    return load_images(test_folder_path), load_images(test_ground_truth_folder_path)
+    image, mask = load_images(test_folder_path), load_images(test_ground_truth_folder_path)
 
+    print("Completed loading test set")
+    print("#" * 100)
+
+    return image, mask
 
 # # test load_images function
 # if __name__ == '__main__':
@@ -93,15 +176,8 @@ def load_training_set(test_folder_path, test_ground_truth_folder_path):
 #     print(resized_images.shape)
 #     # output: (2594, 256, 256, 3), 2594 images of size 256x256 with 3 channels
 
-# if __name__ == '__main__':
-#     test_folder_path = "E:/comp3710/ISIC2018/ISIC2018_Task1-2_Validation_Input"
-#     test_ground_truth_folder_path = "E:/comp3710/ISIC2018/ISIC2018_Task1_Validation_GroundTruth"
-#     images, masks = load_training_set(test_folder_path, test_ground_truth_folder_path)
-#     print(images.shape)
-#     print(masks.shape)
-#     # output: (2594, 256, 256, 3), 2594 images of size 256x256 with 3 channels
-#     # output: (2594, 256, 256, 3), 2594 images of size 256x256 with 3 channels
-
-
-
-
+if __name__ == '__main__':
+    path = "E:/comp3710/ISIC2018/"
+    train_X, train_y = load_training_set(path)
+    val_X, val_y = load_val_set(path)
+    test_X, test_y = load_test_set(path)
