3c7d637a0c666edb81120937ba078beb14e876d2
FanHaoZeng <1992344969@qq.com>
2023-10-06 14:39:07 +1000

    Change to task 3

--- recognition/modules.py
+++ recognition/modules.py
@@ -1,10 +1,15 @@
-import torch
-import torch.nn as nn
+from tensorflow import keras
+from mrcnn.config import Config
+from mrcnn import model as modellib
 
-# Module structure
-class Unet(nn.Module):
-    def __init__(self):
-        super(Unet, self).__init__()
+class MaskRCNNConfig(Config):
+    NAME = "skin_lesion"
+    IMAGES_PER_GPU = 1
+    NUM_CLASSES = 1 + 1  # Background + skin lesion
+    STEPS_PER_EPOCH = 100
+    DETECTION_MIN_CONFIDENCE = 0.9
 
-    def forward(self, x):
-        return x
+def get_maskrcnn_model():
+    config = MaskRCNNConfig()
+    model = modellib.MaskRCNN(mode="training", config=config, model_dir="./")
+    return model
