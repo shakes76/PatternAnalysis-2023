e437e3964236b4094c9459511a3cc08a067c0b13
MandalorianForce <yi.zhou6@uqconnect.edu.au>
2023-10-06 14:56:13 +1000

    Finished creating the vq-vae model and created the train file

--- recognition/VQ-VAE-46495408/modules.py
+++ recognition/VQ-VAE-46495408/modules.py
@@ -55,7 +55,7 @@ class VectorQuantizer(layers.Layer):
         )
         
         # Derive the indices for minimum distances
-        encoding_indices = tf.armin(distances, axis=1)
+        encoding_indices = tf.argmin(distances, axis=1)
         return encoding_indices
     
 def get_encoder(latent_dim=16):
@@ -69,6 +69,18 @@ def get_decoder(latent_dim=16):
     latent_inputs = keras.Input(shape=get_encoder(latent_dim).output.shape[1:])
     x = layers.Conv2DTranspose(64, 3, activation="relu", strides=2, padding="same")(
         latent_inputs)
-    x = layers.Conv2DTranspose(32, 3, activationo="relu", strides=2, padding="same")(x)
+    x = layers.Conv2DTranspose(32, 3, activation="relu", strides=2, padding="same")(x)
     decoder_outputs = layers.Conv2DTranspose(1, 3, padding="same")(x)
     return keras.Model(latent_inputs, decoder_outputs, name="decoder")
+
+def get_vqvae(latent_dim=16, num_embeddings=64):
+    vq_layer = VectorQuantizer(num_embeddings, latent_dim, name="vector_quantizer")
+    encoder = get_encoder(latent_dim)
+    decoder = get_decoder(latent_dim)
+    inputs = keras.Input(shape=(28, 28, 1))
+    encoder_outputs = encoder(inputs)
+    quantized_latents = vq_layer(encoder_outputs)
+    reconstructions = decoder(quantized_latents)
+    return keras.Model(inputs, reconstructions, name="vq_vae")
+
+get_vqvae().summary()
