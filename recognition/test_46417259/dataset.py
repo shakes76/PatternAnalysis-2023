8910f50c542ae050f3aeeb1595a7f2b49c386098
Minhao Sun <141893731+minheed-s@users.noreply.github.com>
2023-10-07 17:28:23 +1000

    built PairedDataset class to handle Siamese training

--- recognition/test_46417259/dataset.py
+++ recognition/test_46417259/dataset.py
@@ -5,6 +5,7 @@ import torchvision.transforms.v2 as transforms
 import torchvision.utils as vutils
 import numpy as np
 import matplotlib.pyplot as plt
+import random
 
 # global variables
 batch_size = 128
@@ -24,6 +25,50 @@ test_transforms = transforms.Compose([
     transforms.ToTensor(),
     transforms.CenterCrop(240),
 ])
+
+class PairedDataset(torch.utils.data.Dataset):
+    def __init__(self, image_folder:dset.ImageFolder, show_debug_info:bool, random_seed=None) -> None:
+        super().__init__()
+        self.image_folder = image_folder
+        self.image_folder_size = len(self.image_folder)
+        self.debug_mode = show_debug_info
+
+        # for reproducibility
+        if random_seed is not None:
+            random.seed(random_seed)
+            torch.random.seed(random_seed)
+
+    def __len__(self) -> int:
+        return self.image_folder_size
+
+    def __getitem__(self, index: int):
+        img1, label1 = self.image_folder[index]
+        similarity = random.randint(0, 1)
+
+        match_found = False
+        while not match_found:
+            choice = random.randint(0, self.image_folder_size - 1)
+            # make sure we do not pair the same image against itself
+            if choice == index:
+                continue
+
+            img2, label2 = self.image_folder[choice]
+            if similarity == 1:
+                match_found = label1 == label2
+            else:
+                match_found = label1 != label2
+
+        # only include the filepaths in the dataset if specifically requested
+        if self.debug_mode:
+            filepath1 = self.image_folder.imgs[index][0]
+            filepath2 = self.image_folder.imgs[choice][0]
+            return img1, img2, similarity, filepath1[-20:-5], filepath2[-20:-5]
+        # otherwise save some memory
+        return img1, img2, similarity
+    
+    def showing_debug_info(self) -> bool:
+        return self.debug_mode
+
 
 def load_train() -> torch.utils.data.DataLoader:
     # load the trainset
@@ -48,10 +93,6 @@ def load_test() -> torch.utils.data.DataLoader:
                                             shuffle=True, num_workers=workers)
     
     return testloader
-
-# Decide which device we want to run on
-device = torch.device("cuda:0" if torch.cuda.is_available() else "mps")
-print("Device: ", device)
 
 def test_visualise_data(dataloader: torch.utils.data.DataLoader):
     # Plot some training images
@@ -79,6 +120,47 @@ def test_visualise_data(dataloader: torch.utils.data.DataLoader):
         plt.imshow(np.transpose(next_batch[0][i].squeeze(), (1,2,0)), cmap="gray")
     plt.show()
 
-test_visualise_data(load_train())
+def visualise_paired_data(dataset: PairedDataset):
+    if not dataset.showing_debug_info():
+        raise NotImplementedError("PairedDataset must be initialised with show_debug_info=True")
 
+    testloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                            shuffle=True, num_workers=workers)
+        
+    next_batch = next(iter(testloader))
+    print(next_batch[0][0].shape)
+    print(next_batch[0][1].shape)
+
+    cols, rows = 3, 3
+    fig, axs = plt.subplots(rows, cols * 2)
+    labels_map = {0: 'diff', 1: 'same'}
+
+    for i in range(rows):
+        for j in range(cols):
+            axs[i,j*2].imshow(np.transpose(next_batch[0][i*rows+j].squeeze(), (1,2,0)), cmap="gray")
+            axs[i,j*2+1].imshow(np.transpose(next_batch[1][i*rows+j].squeeze(), (1,2,0)), cmap="gray")
+            axs[i,j*2].set_title(f"""{labels_map[next_batch[2][i*rows+j].tolist()]}, {next_batch[3][i*rows+j]}""")
+            axs[i,j*2+1].set_title(next_batch[4][i*rows+j])
+            axs[i,j*2].axis("off")
+            axs[i,j*2+1].axis("off")
+    plt.show()
+
+def test_paired_dataset():
+    source = dset.ImageFolder(root=train_path,
+                                transform=train_transforms
+                            )
+    test = PairedDataset(source, show_debug_info=True)
+    print(len(test))
+    print(len(source))
+    visualise_paired_data(test)
+
+
+# Decide which device we want to run on
+device = torch.device("cuda:0" if torch.cuda.is_available() else "mps")
+print("Device: ", device)
+
+test_paired_dataset()
+
+# load_train()
+# test_visualise_data(load_train())
 # test_visualise_data(load_test())
