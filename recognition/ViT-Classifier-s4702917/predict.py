dc9daec2924e59bb4f1be66c51ca0a28c90fe3ed
Talia-12 <paradox.m.burgess@gmail.com>
2023-10-07 22:37:34 +1000

    setup simple evaluation script.

---
+++ recognition/ViT-Classifier-s4702917/predict.py
@@ -0,0 +1,51 @@
+import torch
+import time
+import os
+
+import dataset as ds
+
+# Initialise device
+print("PyTorch Version:", torch.__version__)
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+if (not torch.cuda.is_available()):
+	print("Warning: Cuda not available, using CPU instead.")
+
+# Parameters
+savePath = "models/mnistClassifier.pth"
+
+# Initialise Model
+model = torch.load(savePath)
+model = model.to(device)
+
+# model info
+print("Model No. of Parameters:", sum([param.nelement() for param in model.parameters()]))
+print(model)
+
+# Test the model
+print("> Testing")
+start =  time.time()
+model.eval()
+
+
+with torch.no_grad():
+	correct = 0
+	total = 0
+	
+	for images, labels in ds.trainloader:
+		images = images.to(device)
+		labels = labels.to(device)
+  
+		outputs = model(images)
+		_, predicted = torch.max(outputs.data, 1)
+	
+		total += labels.size(0)
+		correct += (predicted == labels).sum().item()
+	print("Test Accuracy: {} %".format(100 * correct / total))
+
+end = time.time()
+elapsed = end - start
+print("Testing took " + str(elapsed) + " secs")
+
+print("END")
