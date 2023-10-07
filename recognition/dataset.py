a5953cfcd8d08c3d598417ea8da4afb78ce87ef8
Tzu-Yu Yang <125327045+jyz523@users.noreply.github.com>
2023-10-07 17:25:15 +1000

    Update dataset.py
    change into doing Task1

--- recognition/dataset.py
+++ recognition/dataset.py
@@ -1,63 +1,37 @@
-import torch
 from torch.utils.data import Dataset
-import os
+from torchvision import transforms
 from PIL import Image
-import torchvision.transforms as T
-import numpy as np
-
-
-def get_transform():
-    return T.Compose([
-        T.PILToTensor(),
-        T.ConvertImageDtype(torch.float)
-    ])
+import os
 
 
 class ISICDataset(Dataset):
-    def __init__(self, img_dir, mask_dir, transform=None):
-        self.img_dir = img_dir
+    def __init__(self, image_dir, mask_dir=None, image_transform=None, mask_transform=None, img_size=(384, 512)):
+        self.img_size = img_size
+        self.image_dir = image_dir
         self.mask_dir = mask_dir
-        self.transform = transform
-        self.image_files = [f for f in os.listdir(img_dir) if f.endswith(('.jpg', '.png'))]
+        self.image_transform = image_transform
+        self.mask_transform = mask_transform
+        self.images = os.listdir(image_dir)
 
     def __len__(self):
-        return len(self.image_files)
+        return len(self.images)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.img_dir, self.image_files[idx])
-        mask_path = os.path.join(self.mask_dir, self.image_files[idx].replace('.jpg', '_segmentation.png'))
+        img_name = os.path.join(self.image_dir, self.images[idx])
+        image = Image.open(img_name).convert("RGB")
+        image = image.resize(self.img_size, Image.BILINEAR)
 
-        image = Image.open(img_path).convert("RGB")
-        mask = Image.open(mask_path).convert("L")
+        if self.mask_dir:
+            mask_name = os.path.join(self.mask_dir, self.images[idx].replace('.jpg', '_segmentation.png'))
+            mask = Image.open(mask_name).convert("L")  # Grayscale
+            mask = mask.resize(self.img_size, Image.NEAREST)
+        else:
+            mask = None
 
-        if self.transform:
-            image = self.transform(image)
-            mask = self.transform(mask)
-            mask = mask.to(dtype=torch.int64)
+        if self.image_transform:
+            image = self.image_transform(image)
+        if mask and self.mask_transform:
+            mask = self.mask_transform(mask)
 
-        # Compute bounding boxes from masks
-        pos = np.where(np.array(mask) > 0)
-        xmin = np.min(pos[1])
-        xmax = np.max(pos[1])
-        ymin = np.min(pos[0])
-        ymax = np.max(pos[0])
-        boxes = torch.tensor([[xmin, ymin, xmax, ymax]], dtype=torch.float32)
-
-        # Create target dictionary
-        target = {
-            "boxes": boxes,
-            "labels": torch.ones((1,), dtype=torch.int64), 
-            "masks": mask,
-            "image_id": torch.tensor([idx]),
-            "area": (boxes[:, 3] - boxes[:, 1]) * (boxes[:, 2] - boxes[:, 0]),
-            "iscrowd": torch.zeros((1,), dtype=torch.int64)
-        }
-
-        return image, target
-
-
-if __name__ == "__main__":
-    TRAIN_IMG_DIR = './ISIC2018_Task1-2_Training_Input'  
-    TRAIN_MASK_DIR = './ISIC2018_Task1_Training_GroundTruth'  
-
-    train_dataset = ISICDataset(img_dir=TRAIN_IMG_DIR, mask_dir=TRAIN_MASK_DIR, transform=get_transform())
+        return image, mask
+    
