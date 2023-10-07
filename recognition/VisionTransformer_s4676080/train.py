ff3c764d4a8c9532ba6a4652311f22cbcf2565c4
Shriyan-Garg <gargshriyan23@gmail.com>
2023-10-08 01:27:37 +1000

    increased epoch, train and test loop seperated

--- recognition/VisionTransformer_s4676080/train.py
+++ recognition/VisionTransformer_s4676080/train.py
@@ -17,7 +17,7 @@ optimizer = optim.Adam(model.parameters(), lr=3e-4, weight_decay=1e-4)  # Introd
 scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.7)  # Learning rate scheduler
 
 # Training loop
-num_epochs = 5
+num_epochs = 10
 train_losses = []
 test_losses = []
 
@@ -34,33 +34,33 @@ for epoch in range(num_epochs):
         loss.backward()
         optimizer.step()        
         running_loss += loss.item() * images.size(0)
-
     train_loss = running_loss / len(train_loader.dataset)
-    train_losses.append(train_loss)
-
-    # Validation
-    model.eval()
-    running_loss = 0.0
-    correct = 0
-    total = 0
-    with torch.no_grad():
-        for images, labels in test_loader:
-            images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
-            _, predicted = torch.max(outputs, 1)
-            loss = criterion(outputs, labels)
-            # print("val_labels", labels)
-            running_loss += loss.item() * images.size(0)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-    test_loss = running_loss / len(test_loader.dataset)
-    test_losses.append(test_loss)
-    accuracy = correct / total * 100
-    print(f"Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}, Accuracy: {accuracy:.2f}%")
-
-    # Update learning rate
+    train_losses.append(train_loss)    
+    print(f"Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f}")    
     scheduler.step()
 print("finished training!")
 
-# The inital accuarcy is 50% (with no hyperparameter tuning)
+# Testing loop
+model.eval()
+test_losses = []
+running_loss = 0.0
+correct = 0
+total = 0
+
+print("Testing started!")
+with torch.no_grad():
+    for images, labels in test_loader:
+        images, labels = images.to(device), labels.to(device)
+        outputs = model(images)
+        _, predicted = torch.max(outputs, 1)
+        loss = criterion(outputs, labels)
+        running_loss += loss.item() * images.size(0)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
+
+test_loss = running_loss / len(test_loader.dataset)
+test_losses.append(test_loss)
+accuracy = correct / total * 100
+print(f"Test Loss: {test_loss:.4f}, Accuracy: {accuracy:.2f}%")
+
+# The accuracy is increased to 58.40% by increasing the train and test images (no hyperparameter tuning done)
