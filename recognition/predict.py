cc1812373793729ad8762fa5f7a03b53406cc685
Tzu-Yu Yang <125327045+jyz523@users.noreply.github.com>
2023-10-07 13:21:11 +1000

    Update predict.py
    doesnt really work

--- recognition/predict.py
+++ recognition/predict.py
@@ -2,29 +2,44 @@ import torch
 from torchvision import transforms
 from modules import get_maskrcnn_model
 from PIL import Image
+import numpy as np
+import matplotlib.pyplot as plt
 
-device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+# Set up the device for GPU or CPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# Load the pre-trained model
+# Initialize and load the pre-trained model
+model_path = './Save_Model'
 model = get_maskrcnn_model(num_classes=2)
-path_to_saved_model = './Save_Model'
-model.load_state_dict(torch.load(path_to_saved_model))
-model = model.to(device)
+model.load_state_dict(torch.load(model_path, map_location=device))
+model.to(device)
 model.eval()
 
-# Image preprocessing
-transform = transforms.Compose([
-    transforms.Resize((256, 256)),
-    transforms.ToTensor(),
-])
+# Define the image transform pipeline
+def preprocess_image(img_path):
+    preprocess = transforms.Compose([
+        transforms.Resize((256, 256)),
+        transforms.ToTensor(),
+    ])
+    img = Image.open(img_path).convert("RGB")
+    return preprocess(img).unsqueeze(0)
 
-path_to_image = './ISIC2018_Task1-2_Test_Input'
-image = Image.open(path_to_image).convert("RGB")
-image = transform(image).unsqueeze(0).to(device)
+# Load and preprocess the image
+image_path = './ISIC2018_Task1-2_Test_Input'
+input_image = preprocess_image(image_path).to(device)
 
-# Make predictions
+# Perform inference
 with torch.no_grad():
-    predictions = model(image)
+    prediction = model(input_image)
 
-# Print results
-print(predictions)
+def visualize_predictions(image_tensor, predictions):
+    image_arr = image_tensor[0].cpu().numpy().transpose((1, 2, 0))
+    plt.imshow(image_arr)
+    for box in predictions[0]['boxes']:
+        plt.gca().add_patch(plt.Rectangle((box[0], box[1]), box[2] - box[0], box[3] - box[1], fill=False, color='red'))
+    plt.show()
+
+visualize_predictions(input_image, prediction)
+
+# Display prediction results
+print(prediction)
