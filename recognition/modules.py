b7cfb959baf4ce93cc18499c4256faa3835167e6
Tzu-Yu Yang <125327045+jyz523@users.noreply.github.com>
2023-10-07 13:26:15 +1000

    Update modules.py
    shorter version

--- recognition/modules.py
+++ recognition/modules.py
@@ -4,15 +4,18 @@ from torchvision.models.detection.faster_rcnn import FastRCNNPredictor
 from torchvision.models.detection.mask_rcnn import MaskRCNNPredictor
 
 
-def get_maskrcnn_model(num_classes):
-    model = maskrcnn_resnet50_fpn(pretrained=True)
+def get_maskrcnn_model(num_categories=3):
 
-    in_features_box = model.roi_heads.box_predictor.cls_score.in_features
-    box_predictor = FastRCNNPredictor(in_features_box, num_classes)
-    model.roi_heads.box_predictor = box_predictor
+    maskrcnn_model = maskrcnn_resnet50_fpn(pretrained=True)
 
-    in_features_mask = model.roi_heads.mask_predictor.conv5_mask.in_channels
-    mask_predictor = MaskRCNNPredictor(in_features_mask, 256, num_classes)
-    model.roi_heads.mask_predictor = mask_predictor
+    # Adjust the box predictor to match the number of classes
+    box_features = maskrcnn_model.roi_heads.box_predictor.cls_score.in_features
+    custom_box_predictor = FastRCNNPredictor(box_features, num_categories)
+    maskrcnn_model.roi_heads.box_predictor = custom_box_predictor
 
-    return model
+    # Adjust the mask predictor to match the number of classes
+    mask_features = maskrcnn_model.roi_heads.mask_predictor.conv5_mask.in_channels
+    custom_mask_predictor = MaskRCNNPredictor(mask_features, 256, num_categories)
+    maskrcnn_model.roi_heads.mask_predictor = custom_mask_predictor
+
+    return maskrcnn_model
