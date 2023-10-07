9a362170441af462aee0d146a5be840806187996
Shriyan-Garg <gargshriyan23@gmail.com>
2023-10-07 14:34:31 +1000

    added training and validation loop, 50% accuracy

--- recognition/VisionTransformer_s4676080/train.py
+++ recognition/VisionTransformer_s4676080/train.py
@@ -9,3 +9,58 @@ import matplotlib.pyplot as plt
 
 # Checking for GPU
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+
+# Model configurations
+model = VisionTransformer().to(device)
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=3e-4, weight_decay=1e-4)  # Introduced weight decay
+scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.7)  # Learning rate scheduler
+
+# Training loop
+num_epochs = 5
+train_losses = []
+test_losses = []
+
+print("training started!")
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for images, labels in train_loader:
+        images, labels = images.to(device), labels.to(device)
+        optimizer.zero_grad()        
+        outputs = model(images)
+        loss = criterion(outputs, labels)
+        #print(loss)
+        loss.backward()
+        optimizer.step()        
+        running_loss += loss.item() * images.size(0)
+
+    train_loss = running_loss / len(train_loader.dataset)
+    train_losses.append(train_loss)
+
+    # Validation
+    model.eval()
+    running_loss = 0.0
+    correct = 0
+    total = 0
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            loss = criterion(outputs, labels)
+            # print("val_labels", labels)
+            running_loss += loss.item() * images.size(0)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+
+    test_loss = running_loss / len(test_loader.dataset)
+    test_losses.append(test_loss)
+    accuracy = correct / total * 100
+    print(f"Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f}, Test Loss: {test_loss:.4f}, Accuracy: {accuracy:.2f}%")
+
+    # Update learning rate
+    scheduler.step()
+print("finished training!")
+
+# The inital accuarcy is 50% (with no hyperparameter tuning)
