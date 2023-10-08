b8df08784348a44334963acd04bd7d5cace8b1f9
Minh Tieu <s4641971@uqconnect.edu.au>
2023-10-08 22:07:46 +1000

    set up loss and optimizer and set up bare minimum training for Siamese model

--- recognition/Siamese_46419712/train.py
+++ recognition/Siamese_46419712/train.py
@@ -31,27 +31,73 @@ if __name__ == '__main__':
 
     # hyper parameters
     num_epochs = 20
+    learning_rate = 0.1
+
+    criterion = ContrastiveLossFunction()
+
+    # optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+    # scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=learning_rate, steps_per_epoch=total_step, epochs=num_epochs)
+
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.5) # Optimize model parameter
+
+    #Piecewise Linear Schedule
+    total_step = len(train_loader)
+    # adjust the learning rate during training -> potentially help reach convergence state faster while also ensure it does not over train
+    scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=learning_rate, steps_per_epoch=total_step, epochs=num_epochs)
     
+
+    loss_list = [] 
+    step_count = 0
+    start = time.time() #time generation
+
+    model.train()
     print("Start training")
     for epoch in range(num_epochs):
         print(f"epoch: {epoch + 1}")
         
         for i, val in enumerate(train_loader):
-            img0, img1 , label = val
-            # data, label = val
-            print(model(img0).shape)
-            print(model(img1).shape)
+            img0, img1 , labels = val
+            img0 = img0.to(device)
+            img1 = img1.to(device)
+            labels = labels.to(device)
+
+            output1 = model(img0)
+            output2 = model(img1)
+            loss = criterion(output1, output2, labels)
+
+            optimizer.zero_grad()
+
+            loss.backward()
+
+            optimizer.step()
+
+
+            # save loss for graph
+            loss_list.append(loss.item())
+            step_count += 1
+
+            if (i+1) % 100 == 0:
+                print ("Epoch [{}/{}], Step [{}/{}] Loss: {:.5f}"
+                        .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
+
+
+            scheduler.step()
 
             break
-        
         break
 
+    end = time.time() #time generation
+
     print("Finish training")
+    elapsed = end - start
+    print("Training took " + str(elapsed) + " secs of " + str(elapsed/60) + " mins in total")
 
 
     #########  TESTING SIAMASE MODEL ##########
     print("Start Testing")
-
+    
+    start = time.time()
     # evaluate the model
     model.eval() # disable drop out, batch normalization
     with torch.no_grad(): # disable gradient computation
@@ -67,8 +113,10 @@ if __name__ == '__main__':
         #     correct_predict += (predicted == labels).sum().item()
         
         # print('Test Accuracy: {} %'.format(100 * correct_predict / total_test))
+    
+    end = time.time() #time generation
 
     print("Finish Testing")
 
-    
-    pass
+    elapsed = end - start
+    print("Testing took " + str(elapsed) + " secs of " + str(elapsed/60) + " mins in total")
