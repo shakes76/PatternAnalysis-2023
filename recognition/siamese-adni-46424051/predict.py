2724fc5c9c8391b72f280fc08d5b1a43e563d184
Joseph Churchill <j.churchill@uq.net.au>
2023-10-09 01:00:33 +1000

    fixed predict file

--- recognition/siamese-adni-46424051/predict.py
+++ recognition/siamese-adni-46424051/predict.py
@@ -2,8 +2,6 @@
 
 from train import Train
 
-class Predict():
-    def __init__(self):
-        self.train = Train()
-        self.train.train()
-        self.train.test()
+train = Train()
+train.train()
+train.test()
