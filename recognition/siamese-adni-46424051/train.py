03256b3bc3263dc48a46c670d4a9d9688670650f
Joseph Churchill <j.churchill@uq.net.au>
2023-10-09 02:11:54 +1000

    edited testing stuff

--- recognition/siamese-adni-46424051/train.py
+++ recognition/siamese-adni-46424051/train.py
@@ -52,7 +52,7 @@ class Train():
                 img1, img2 = img1.cuda(), img2.cuda()
             img1, img2 = Variable(img1), Variable(img2)
             out1, out2 = self.net(img1, img2)
-            difference = torch.nn.functional.pairwise_distance(out1, out2)
+            difference = torch.nn.functional.pairwise_distance(out1, out2).item()
             if label[0] == 0:
                 label = "SAME"
             else:
