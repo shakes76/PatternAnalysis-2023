6816edf72bdf70d148fe25a678e134f6b02f4b4c
Matthew <curran.wehttam@gmail.com>
2023-10-08 15:20:41 +1000

    Added Criterion and Optimizer (Using TripletLoss) Added Training loop, set up for Siamese modifications.

--- recognition/Siamese-s46971733/train.py
+++ recognition/Siamese-s46971733/train.py
@@ -22,13 +22,46 @@ if not torch.cuda.is_available():
 
 print("\n")
 
+# Datasets and Dataloaders
 trainset = get_dataset(train=1)
 testset = get_dataset(train=0)
 
 trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=True)
 
-ResNet = Resnet().to(device)
+# Model.
+resnet = Resnet().to(device)
+
+# Optimizer
+criterion = nn.TripletMarginLoss()
+optimizer = optim.Adam(resnet.parameters(), lr=learning_rate)
+
+# Future spot for Scheduler?
 
 
+#
 
+for epoch in range(num_epochs):
+    running_loss = 0.0
+
+    for i, data in enumerate(trainloader, 0):
+        # Extract data and transfer to GPU.
+        anchor = data[0].to(device)
+
+        # Zero the gradients -- Ensuring gradients not accumulated
+        #                       across multiple training iterations.
+        optimizer.zero_grad()
+
+        # Forward Pass
+        anchor_out = resnet(anchor)
+
+        # Calculate Loss with Triplet Loss.
+        #loss = criterion(anchor_out, positive_out, negative_out)
+        # Compute gradient with respect to model.
+        #loss.backward()
+
+        # Optimizer step - Update model parameters.
+        optimizer.step()
+
+        running_loss += loss.item()
+        print(f"Epoch: {epoch+1} / {num_epochs} - Loss: {running_loss}")
