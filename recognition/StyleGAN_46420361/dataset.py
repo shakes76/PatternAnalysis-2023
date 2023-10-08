203b533ef7bc0b5b692025397c0d2a33fb837c3d
Callum Elder <cjames.02@icloud.com>
2023-10-08 22:14:55 +1000

    loaded in celeba dataset

--- recognition/StyleGAN_46420361/dataset.py
+++ recognition/StyleGAN_46420361/dataset.py
@@ -6,14 +6,21 @@ from torch.utils.data import DataLoader
 
 image_size = 64
 batch_size = 128
+path = '~/data/celeba'
 
-# transform
-transform = transforms.Compose([
-    transforms.Resize((image_size, image_size)),
-    transforms.ToTensor(),
-    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-])
 
-# data
-dataset = ImageFolder('~/data/celeba', transform=transform)
-dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)   
+def load_dataset(image_size, batch_size, path):
+    # transform
+    transform = transforms.Compose([
+        transforms.Resize((image_size, image_size)),
+        transforms.ToTensor(),
+        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
+    ])
+
+    # data
+    dataset = ImageFolder(path, transform=transform)
+    dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
+
+    return dataloader
+
+dataloader = load_dataset(image_size, batch_size, path)
