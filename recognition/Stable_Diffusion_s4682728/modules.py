534f28ec39f6ad722e79d7c643543ca02647fc7b
ray <99058662+raydesuyo@users.noreply.github.com>
2023-10-08 22:31:37 +1000

    Create template for modules.py

---
+++ recognition/Stable_Diffusion_s4682728/modules.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+
+class DiffusionProcess(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+    
+    def forward(self):
+        pass
+
+class DiffusionNetwork(nn.Module):
+    def __init__(self):
+        super().__init__()
+        pass
+    def forward(self):
+        pass
+
