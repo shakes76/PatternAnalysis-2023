1fd8d21654d90f7619bfea154351c30ca7192882
Joseph Churchill <j.churchill@uq.net.au>
2023-10-08 19:22:15 +1000

    train dataset loading

--- recognition/siamese-adni-46424051/dataset.py
+++ recognition/siamese-adni-46424051/dataset.py
@@ -1,1 +1,30 @@
 ##################################   dataset.py   ##################################
+import torch
+from torch.utils.data import Dataset
+import os
+from PIL import Image
+
+class DatasetTrain(Dataset):
+    def __init__(self, path, transforms):
+        super(DatasetTrain, self).__init__()
+        self.transforms = transforms
+        self.CN, self.AD = self.load_images(path)
+
+    def load_images(self, path):
+        CN = []
+        AD = []
+        
+        # Load from filepath
+        for filePath in os.listdir(os.path.join(path, "CN")):
+            filePath = os.path.join(path, "CN", filePath)
+            CN.append(Image.open(filePath).convert("L"))
+
+        # Load from filepath
+        for filePath in os.listdir(os.path.join(path, "AD")):
+            filePath = os.path.join(path, "AD", filePath)
+            AD.append(Image.open(filePath).convert("L"))
+
+        return CN, AD
+    
+if __name__=="__main__":
+    dataset = DatasetTrain(os.path.join(os.getcwd(), "images", "train"), None)
