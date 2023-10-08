57ef1b2a6f6f739970b6f1a67401c9a69d27e029
Matthew <curran.wehttam@gmail.com>
2023-10-08 14:43:24 +1000

    Added Imports from modules.py and dataset.py. Added hyperparameters, dataloaders and model to train.py.

--- recognition/Siamese-s46971733/train.py
+++ recognition/Siamese-s46971733/train.py
@@ -9,4 +9,26 @@ import torchvision.transforms as transforms
 import matplotlib.pyplot as plt
 import cv2
 import os
+from dataset import get_dataset
+from modules import Resnet, Resnet34
+
+num_epochs = 10
+batch_size = 64
+learning_rate = 0.001
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if not torch.cuda.is_available():
+    print("No CUDA Found. Using CPU")
+
+print("\n")
+
+trainset = get_dataset(train=1)
+testset = get_dataset(train=0)
+
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=batch_size, shuffle=True)
+testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=True)
+
+ResNet = Resnet().to(device)
+
+
 
