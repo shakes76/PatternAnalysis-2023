d77febf63187834e1983cceefc4b30d6630a0de9
Man Lik Nicholas Wong <s4668403@uqconnect.edu.au>
2023-10-08 16:19:28 +1000

    added accuracy metric

--- recognition/46684033-Siamese network/train.py
+++ recognition/46684033-Siamese network/train.py
@@ -2,40 +2,48 @@
 from dataset import load_data
 import modules
 import torch
+import torch.nn as nn
 from pytorch_metric_learning import losses
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if not torch.cuda.is_available():
     print("Warning VUDA not Found. Using CPU")
 # hyperparameters
-num_epoch = 3
+num_epoch = 20
 learning_rate = 0.001
 
 train_path = r"C:/Users/wongm/Downloads/ADNI_AD_NC_2D/AD_NC/train"
 test_path = r"C:/Users/wongm/Downloads/ADNI_AD_NC_2D/AD_NC/test"
 
 train_loader, validation_loader, test_loader = load_data(train_path, test_path)
-model = modules.ResNet18()
+model = modules.Siamese()
 model = model.to(device)
 
-loss_func = losses.TripletMarginLoss()
+criterion = nn.BCELoss()
 optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
 total_steps=len(train_loader)
 
 for epoch in range(num_epoch):
+    correct = 0
+    train_total = 0
     model.train()
     print("training starts")
-    for i, (images, labels) in enumerate(train_loader):
+    for i, ((images1,images2), labels) in enumerate(train_loader):
         optimizer.zero_grad()
-        images = images.to(device)
+        images1 = images1.to(device)
+        images2 = images2.to(device)
         labels = labels.to(device)
-        embeddings = model(images)
-        loss = loss_func(embeddings, labels)
+        output = model(images1,images2).squeeze()
+        loss = criterion(output, labels.float())
         loss.backward()
         optimizer.step()
-        if (i + 1) % 10 == 0:
-            print("Epoch [{}/{}], Step[{}/{}] Loss: {:.5f}"
-                  .format(epoch + 1, num_epoch, i + 1, total_steps, loss.item()))
+
+        pred = torch.where(output > 0.5, 1, 0)
+        correct += (pred == labels).sum().item()
+        train_total += labels.size(0)
+        if (i + 1) % 100 == 0:
+            print("Epoch [{}/{}], Step[{}/{}] Loss: {:.5f} Accuracy: {}%"
+                  .format(epoch + 1, num_epoch, i + 1, total_steps, loss.item() , 100*correct/train_total))
 
     model.eval()
 
@@ -44,14 +52,18 @@ for epoch in range(num_epoch):
     total = 0
 
     with torch.no_grad():
-        for i, (images, labels) in enumerate(train_loader):
-            images = images.to(device)
+        for i, ((images1,images2), labels) in enumerate(validation_loader):
+            images1 = images1.to(device)
+            images2 = images2.to(device)
             labels = labels.to(device)
-            embeddings = model(images)
-            val_loss = loss_func(embeddings, labels)
+            output = model(images1,images2).squeeze()
+            val_loss = criterion(output, labels.float())
+            pred = torch.where(output > 0.5, 1, 0)
+            correct += (pred == labels).sum().item()
+            total += labels.size(0)
 
     print(
-        f"Epoch [{epoch}/{num_epoch}] \
-        training_loss: {loss.item():.4f}, validation_loss: {val_loss.item():.4f}"
+        f"Epoch [{epoch+1}/{num_epoch}] \
+        training_loss: {loss.item():.4f}, validation_loss: {val_loss.item():.4f}, validation accuracy: {100*correct/total}%"
     )
-torch.save(model,r"C:\Users\wongm\Desktop\COMP3710\project\siamese.pth")
+torch.save(model,r"C:\Users\wongm\Desktop\COMP3710\project\siamesev2.pth")
