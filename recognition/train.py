2059827a5096eb4cde888c9de4cb5a27df378f11
Tzu-Yu Yang <125327045+jyz523@users.noreply.github.com>
2023-10-08 15:53:47 +1000

    Update train.py
    Remove Val only left with DSC
    Epoch2 65.19%

--- recognition/train.py
+++ recognition/train.py
@@ -5,7 +5,9 @@ from dataset import ISICDataset
 from modules import ImprovedUNet
 from torchvision import transforms
 import matplotlib.pyplot as plt
-import time
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(torch.cuda.is_available())
 
 
 def dice_loss(pred, target, smooth=1.):
@@ -17,41 +19,44 @@ def dice_loss(pred, target, smooth=1.):
     return 1 - dice
 
 
-def train():
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+def evaluate_dsc(loader, model, device):
+    model.eval()
+    all_dscs = []
+    with torch.no_grad():
+        for images, masks in loader:
+            images, masks = images.to(device), masks.to(device)
+            outputs = model(images)
+            loss = dice_loss(outputs, masks)
+            dice_coefficient = 1 - loss.item()
+            all_dscs.append(dice_coefficient)
+    return all_dscs
 
+
+def train():
     image_transform = transforms.Compose([
         transforms.ToTensor(),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
     mask_transform = transforms.Compose([transforms.ToTensor()])
 
-    # Create the entire dataset
     full_dataset = ISICDataset("ISIC2018_Task1-2_Training_Input_x2", "ISIC2018_Task1_Training_GroundTruth_x2", image_transform, mask_transform)
 
-    # Split the dataset into training and validation sets
-    train_size = int(0.8 * len(full_dataset))
-    val_size = len(full_dataset) - train_size
-    train_dataset, validation_dataset = random_split(full_dataset, [train_size, val_size])
+    train_size = int(0.85 * len(full_dataset))
+    test_size = len(full_dataset) - train_size
+    train_dataset, test_dataset = random_split(full_dataset, [train_size, test_size])
 
     train_loader = DataLoader(train_dataset, batch_size=8, shuffle=True)
-    validation_loader = DataLoader(validation_dataset, batch_size=8, shuffle=False)
+    test_loader = DataLoader(test_dataset, batch_size=8, shuffle=False)
 
     model = ImprovedUNet(in_channels=3, out_channels=1).to(device)
-    for param in model.parameters():
-        param.requires_grad = True
-
     optimizer = optim.Adam(model.parameters(), lr=0.001)
 
-    num_epochs = 1
-
+    num_epochs = 2
     train_losses = []
-    validation_losses = []
-    validation_dscs = []
-
-    start_time = time.time()
 
     for epoch in range(num_epochs):
+        print(f"--- Epoch {epoch + 1}/{num_epochs} ---")
+
         model.train()
         running_loss = 0.0
         total_samples = 0
@@ -67,62 +72,38 @@ def train():
             total_dice_coefficient += dice_coefficient
             loss.backward()
             optimizer.step()
+
             running_loss += loss.item() * images.size(0)
             total_samples += images.size(0)
 
         epoch_loss = running_loss / total_samples
         train_losses.append(epoch_loss)
-        average_dice_coefficient = total_dice_coefficient / len(
-            train_loader) 
-        print(f"Epoch {epoch + 1}/{num_epochs}, Training Loss: {epoch_loss:.4f}")
-        print(f"Epoch {epoch + 1} Average Dice Coefficient: {average_dice_coefficient:.4f}\n")
+        average_dice_coefficient = total_dice_coefficient / len(train_loader)
 
+        print(f"Training Loss: {epoch_loss:.4f}, Training Dice Coefficient: {average_dice_coefficient:.4f}")
+        print(f"Epoch {epoch + 1}/{num_epochs}")
+        print(f"Training Loss: {epoch_loss:.4f}, Training Dice Coefficient: {average_dice_coefficient:.4f}")
 
-        # Validation phase
-        model.eval()
-        val_loss = 0.0
-        total_val_dice = 0.0
-        with torch.no_grad():
-            for images, masks in validation_loader:
-                images, masks = images.to(device), masks.to(device)
-                outputs = model(images)
-                loss = dice_loss(outputs, masks)
-                val_loss += loss.item() * images.size(0)
-                dice_coefficient = 1 - loss.item()
-                total_val_dice += dice_coefficient * images.size(0)
-
-        avg_val_loss = val_loss / (len(validation_loader) * validation_loader.batch_size)
-        avg_val_dice = total_val_dice / (len(validation_loader) * validation_loader.batch_size)
-
-        validation_losses.append(avg_val_loss)
-        validation_dscs.append(avg_val_dice)
-
-        print(f"Epoch {epoch + 1} Validation Loss: {avg_val_loss:.4f}, Validation DSC: {avg_val_dice:.4f}")
-
-    end_time = time.time()
+    # Evaluate DSC for each image in the test set
+    test_dscs = evaluate_dsc(test_loader, model, device)
+    print(f"\nAverage Test DSC: {sum(test_dscs) / len(test_dscs):.4f}")
+    below_threshold = [d for d in test_dscs if d < 0.8]
+    if below_threshold:
+        print(f"{len(below_threshold)} out of {len(test_dscs)} test images have DSC below 0.8!")
+    else:
+        print("All test images have DSC above 0.8!")
 
     # Plotting
-    epochs = range(1, num_epochs + 1)
-    plt.figure(figsize=(15, 5))
-
-    plt.subplot(1, 2, 1)
-    plt.plot(epochs, train_losses, '-o', label='Training Loss')
-    plt.plot(epochs, validation_losses, '-o', label='Validation Loss')
-    plt.title('Training & Validation Loss')
+    epochs_range = range(1, len(train_losses) + 1)
+    plt.figure(figsize=(10, 5))
+    plt.plot(epochs_range, train_losses, '-o', label='Training Loss')
+    plt.title('Training Loss')
     plt.xlabel('Epochs')
     plt.ylabel('Loss')
     plt.legend()
-
-    plt.subplot(1, 2, 2)
-    plt.plot(epochs, validation_dscs, '-o')
-    plt.title('Validation DSC')
-    plt.xlabel('Epochs')
-    plt.ylabel('DSC')
-
     plt.tight_layout()
     plt.show()
 
-    print(f"Total Training Time: {end_time - start_time:.2f} seconds")
     torch.save(model.state_dict(), "model_checkpoint.pth")
 
 
