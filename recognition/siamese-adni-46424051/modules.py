9cf9d8c242e370122afd29790a8beb31c708971a
Joseph Churchill <j.churchill@uq.net.au>
2023-10-08 22:59:20 +1000

    siamese model added

--- recognition/siamese-adni-46424051/modules.py
+++ recognition/siamese-adni-46424051/modules.py
@@ -1,3 +1,33 @@
 ##################################   modules.py   ##################################
+import torch
+from torch.nn import Module, Sequential, Linear, Sigmoid, Conv2d, ReLU, MaxPool2d
 
-
+class Model(Module):
+    def __init__(self):
+        super(Model, self).__init__()
+        self.conv = Sequential(
+            Conv2d(1, 64, 10),
+            ReLU(inplace=True),
+            MaxPool2d(2),
+            Conv2d(64, 128, 7),
+            ReLU(),
+            MaxPool2d(2),
+            Conv2d(128, 128, 4),
+            ReLU(),
+            MaxPool2d(2),
+            Conv2d(128, 256, 4),
+            ReLU()
+        )
+        self.linear = Sequential(Linear(9216, 4096), Sigmoid())
+        self.out = Linear(4096, 1)
+    
+    def forward_one(self, img):
+        img = self.conv(img)
+        img = img.view(img.size()[0], -1)
+        return self.linear(img)
+        
+    def forward(self, img1, img2):
+        out1 = self.forward_one(img1)
+        out2 = self.forward_one(img2)
+        difference = torch.abs(out1 - out2)
+        return self.out(difference)
