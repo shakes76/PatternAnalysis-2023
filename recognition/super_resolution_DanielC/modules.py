7aaf607111a2caa9ec7a7a5540b55ab3dc7e7250
DHyunC <114087639+DHyunC@users.noreply.github.com>
2023-10-09 12:38:17 +1000

    Created basic SuperResolution Network per guide

--- recognition/super_resolution_DanielC/modules.py
+++ recognition/super_resolution_DanielC/modules.py
@@ -1,1 +1,21 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+class SuperResolution(nn.Module):
+    def __init__(self, upscale_factor=4, channels=1):
+        super(SuperResolution, self).__init__()
+
+        self.conv1 = nn.Conv2d(channels, 64, 5)
+        self.conv2 = nn.Conv2d(64, 64, 3)
+        self.conv3 = nn.Conv2d(64, 32, 3)
+        self.conv4 = nn.Conv2d(32, channels * (upscale_factor ** 2), 3)
+        self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
+        
+    def forward(self, x):
+        x = F.relu(self.conv1(x))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = F.pixel_shuffle(self.conv4(x))
+        return x
 
