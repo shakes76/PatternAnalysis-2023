5302563cd0c55406456568655f5359944634e10e
ray <99058662+raydesuyo@users.noreply.github.com>
2023-10-10 01:31:28 +1000

    Generate 10 images

--- recognition/Stable_Diffusion_s4682728/predict.py
+++ recognition/Stable_Diffusion_s4682728/predict.py
@@ -20,35 +20,36 @@ num_steps = 100
 diffusion_process = DiffusionProcess(betas, num_steps).to(device)
 
 # Load a sample batch of data
-batch_size = 1  # Single image
+batch_size = 10  # 10 images
 dataloader = process_dataset(batch_size=batch_size)
 sample_batch = next(iter(dataloader)).to(device)
 
-# Apply diffusion process
-diffused_sample = diffusion_process(sample_batch)
-
-# Generate image from diffused image
 with torch.no_grad():
-    output_sample = diffusion_network(diffused_sample)
+    # Apply diffusion process
+    diffused_samples = diffusion_process(sample_batch)
+    # Generate image from diffused image
+    output_samples = diffusion_network(diffused_samples)
 
 # Move data to CPU for visualization
 sample_batch = sample_batch.cpu()
-diffused_sample = diffused_sample.cpu()
-output_sample = output_sample.cpu()
+diffused_samples = diffused_samples.cpu()
+output_samples = output_samples.cpu()
 
 # Visualize
-fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+fig, axes = plt.subplots(10, 3, figsize=(15, 50))
 
-axes[0].imshow(sample_batch[0][0], cmap='gray')
-axes[0].set_title('Original Image')
+for i in range(10):
+    axes[i, 0].imshow(sample_batch[i][0], cmap='gray')
+    axes[i, 0].set_title('Original Image')
+    
+    axes[i, 1].imshow(diffused_samples[i][0], cmap='gray')
+    axes[i, 1].set_title('Diffused Image')
+    
+    axes[i, 2].imshow(output_samples[i][0], cmap='gray')
+    axes[i, 2].set_title('Generated Image')
 
-axes[1].imshow(diffused_sample[0][0], cmap='gray')
-axes[1].set_title('Diffused Image')
-
-axes[2].imshow(output_sample[0][0], cmap='gray')
-axes[2].set_title('Generated Image')
-
-for ax in axes:
-    ax.axis('off')
+for ax_row in axes:
+    for ax in ax_row:
+        ax.axis('off')
 
 plt.show()
