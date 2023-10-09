e9c2c09741cf4c3d6f8c734a16627b34046af122
ray <99058662+raydesuyo@users.noreply.github.com>
2023-10-10 02:16:30 +1000

    Modify predict.py

--- recognition/Stable_Diffusion_s4682728/predict.py
+++ recognition/Stable_Diffusion_s4682728/predict.py
@@ -4,7 +4,6 @@ import torch
 import matplotlib.pyplot as plt
 
 # Set device
-# Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
@@ -20,29 +19,30 @@ num_steps = 100
 diffusion_process = DiffusionProcess(betas, num_steps).to(device)
 
 # Load a sample batch of data
-batch_size = 10  # 10 images
+batch_size = 3
 dataloader = process_dataset(batch_size=batch_size)
 sample_batch = next(iter(dataloader)).to(device)
 
+# Apply diffusion process
+diffused_sample = diffusion_process(sample_batch)
+
+# Generate images from diffused images
 with torch.no_grad():
-    # Apply diffusion process
-    diffused_samples = diffusion_process(sample_batch)
-    # Generate image from diffused image
-    output_samples = diffusion_network(diffused_samples)
+    output_samples = diffusion_network(diffused_sample)
 
 # Move data to CPU for visualization
 sample_batch = sample_batch.cpu()
-diffused_samples = diffused_samples.cpu()
+diffused_sample = diffused_sample.cpu()
 output_samples = output_samples.cpu()
 
 # Visualize
-fig, axes = plt.subplots(10, 3, figsize=(15, 50))
+fig, axes = plt.subplots(10, 3, figsize=(15, 15))
 
-for i in range(10):
+for i in range(3):
     axes[i, 0].imshow(sample_batch[i][0], cmap='gray')
     axes[i, 0].set_title('Original Image')
     
-    axes[i, 1].imshow(diffused_samples[i][0], cmap='gray')
+    axes[i, 1].imshow(diffused_sample[i][0], cmap='gray')
     axes[i, 1].set_title('Diffused Image')
     
     axes[i, 2].imshow(output_samples[i][0], cmap='gray')
