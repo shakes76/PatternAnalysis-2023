c9624ade9b37de80d4408641584e94ccca4258ce
DHyunC <114087639+DHyunC@users.noreply.github.com>
2023-10-09 12:23:24 +1000

    added IO paths and hyper paramteres

--- recognition/super_resolution_DanielC/utils.py
+++ recognition/super_resolution_DanielC/utils.py
@@ -1,1 +1,10 @@
+# IO-paths
+AD_train_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710 Report/data/AD_NC/train/AD_train"
+NC_train_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710 Report/data/AD_NC/train/NC_train"
+AD_test_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710 Report/data/AD_NC/test/AD_test"
+NC_test_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710 Report/data/AD_NC/test/NC_test"
 
+# Hyper-parameters
+num_epochs = 35
+learning_rate = 0.001
+batch_size = 10
