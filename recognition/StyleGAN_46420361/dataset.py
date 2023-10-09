ae7582ee60c15d5bfd394c884df6aeae1fce4657
Callum Elder <cjames.02@icloud.com>
2023-10-09 22:10:00 +1000

    added horizontal flip and varying batch sizes

--- recognition/StyleGAN_46420361/dataset.py
+++ recognition/StyleGAN_46420361/dataset.py
@@ -2,26 +2,21 @@ from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
+from math import log2
 
 
-image_size = 64
-batch_size = 128
-path = '~/OASIS_data/keras_png_slices_data'
-
-
-def load_dataset(image_size, batch_size, path):
-    print('loading data...')
+def load_dataset(image_size, batch_sizes, path):
     # transform
     transform = transforms.Compose([
         transforms.Resize((image_size, image_size)),
         transforms.ToTensor(),
+        transforms.RandomHorizontalFlip(p=0.5),
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
 
     # data
+    batch_size = batch_sizes[int(log2(image_size / 4))]
     dataset = ImageFolder(path, transform=transform)
     dataloader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
 
     return dataloader, dataset
-
-dataloader, dataset = load_dataset(image_size, batch_size, path)
