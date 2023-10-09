70fd8ab639c10b3026c58d6c149e1d68ac6fadf6
Matthew <curran.wehttam@gmail.com>
2023-10-09 15:29:18 +1000

    Implemented function to visualise images, labels and predictions. - Added code to produce parameters input into the visualise function.

--- recognition/Siamese-s46971733/predict.py
+++ recognition/Siamese-s46971733/predict.py
@@ -35,3 +35,51 @@ clas_net.load_state_dict(torch.load(CLAS_PATH))
 testset = get_dataset(train=0, clas=0)
 
 testloader = torch.utils.data.DataLoader(testset, batch_size=batch_size, shuffle=True)
+def visualise_images(images, labels, predictions, num_images=4):
+    plt.figure(figsize=(12, 6))
+
+    for i in range(num_images):
+        plt.subplot(3, num_images, i * 3 + 1)
+        plt.imshow(images[i][0].cpu())
+        plt.title("Input Image")
+        plt.axis("off")
+
+        plt.subplot(3, num_images, i * 3 + 2)
+        if labels[i] == 1:
+            plt.text(0.5, 0.5, 'AD', horizontalalignment='center', verticalalignment='center')
+        else:
+            plt.text(0.5, 0.5, f'NC', horizontalalignment='center', verticalalignment='center')
+        plt.title("True Label")
+        plt.axis("off")
+
+        plt.subplot(3, num_images, i * 3 + 3)
+        if predictions[i] == 1:
+            plt.text(0.5, 0.5, 'AD', horizontalalignment='center', verticalalignment='center')
+        else:
+            plt.text(0.5, 0.5, f'NC', horizontalalignment='center', verticalalignment='center')
+        plt.title("Predicted Label")
+        plt.axis("off")
+
+    plt.tight_layout()
+    plt.show()
+
+data = next(iter(testloader))
+
+images = data[0].to(device)
+labels = data[1].to(device)
+
+outputs = resnet(images)
+clas = clas_net(outputs)
+
+m = nn.Threshold(0.5, 0)
+outthresh = m(clas)
+
+test = torch.softmax(clas, dim=1)
+
+predicted_class = test.argmax(dim=1)
+
+for i in range(batch_size):
+    print(f"[Label: {labels[i]}] Output {i} is: {clas[i]}, and OutThresh {i} is {outthresh[i]}")
+    print(f"Test {i} is {test[i]} and Predicted class is {predicted_class[i]}")
+
+visualise_images(images, labels, predicted_class, num_images=batch_size)
