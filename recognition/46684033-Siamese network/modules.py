decb4f71341afd41ebf7f1313161e0591f9220ba
Man Lik Nicholas Wong <s4668403@uqconnect.edu.au>
2023-10-10 09:34:52 +1000

    added network for triplet loss

--- recognition/46684033-Siamese network/modules.py
+++ recognition/46684033-Siamese network/modules.py
@@ -10,110 +10,14 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
-
-class BasicBlock(nn.Module):
-    expansion = 1
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(BasicBlock, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion * planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion * planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion * planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.bn2(self.conv2(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class Bottleneck(nn.Module):
-    expansion = 4
-
-    def __init__(self, in_planes, planes, stride=1):
-        super(Bottleneck, self).__init__()
-        self.conv1 = nn.Conv2d(in_planes, planes, kernel_size=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(planes)
-        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
-                               stride=stride, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(planes)
-        self.conv3 = nn.Conv2d(planes, self.expansion *
-                               planes, kernel_size=1, bias=False)
-        self.bn3 = nn.BatchNorm2d(self.expansion * planes)
-
-        self.shortcut = nn.Sequential()
-        if stride != 1 or in_planes != self.expansion * planes:
-            self.shortcut = nn.Sequential(
-                nn.Conv2d(in_planes, self.expansion * planes,
-                          kernel_size=1, stride=stride, bias=False),
-                nn.BatchNorm2d(self.expansion * planes)
-            )
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = F.relu(self.bn2(self.conv2(out)))
-        out = self.bn3(self.conv3(out))
-        out += self.shortcut(x)
-        out = F.relu(out)
-        return out
-
-
-class ResNet(nn.Module):
-    def __init__(self, block, num_blocks, num_classes=2):
-        super(ResNet, self).__init__()
-        self.in_planes = 64
-
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=7,
-                               stride=2, padding=3, bias=False)
-        self.bn1 = nn.BatchNorm2d(64)
-        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
-        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
-        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
-        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
-        self.linear = nn.Linear(512 * block.expansion, num_classes)
-
-    def _make_layer(self, block, planes, num_blocks, stride):
-        strides = [stride] + [1] * (num_blocks - 1)
-        layers = []
-        for stride in strides:
-            layers.append(block(self.in_planes, planes, stride))
-            self.in_planes = planes * block.expansion
-        return nn.Sequential(*layers)
-
-    def forward(self, x):
-        out = F.relu(self.bn1(self.conv1(x)))
-        out = self.layer1(out)
-        out = self.layer2(out)
-        out = self.layer3(out)
-        out = self.layer4(out)
-        out = F.avg_pool2d(out, 14)
-        out = out.view(out.size(0), -1)
-        out = self.linear(out)
-        return out
-
-
-def ResNet18():
-    return ResNet(BasicBlock, [2, 2, 2, 2])
-
-
 class Siamese(nn.Module):
     def __init__(self):
         super(Siamese, self).__init__()
         self.relu = nn.ReLU()
         self.sigmoid = nn.Sigmoid()
-        self.linear = nn.Linear(512, 256)
+        self.softmax = nn.Softmax(dim=0)
+        self.linear = nn.Linear(128, 64)
+        self.linear2 = nn.Linear(64, 1)
         self.conv1 = nn.Conv2d(2, 1, 1)
         # self.conv2 = nn.Conv2d(32,64,(7,7),bias=False)
         # self.conv3 = nn.Conv2d(64,128,(4,4),bias=False)
@@ -123,15 +27,18 @@ class Siamese(nn.Module):
         # self.batchNorm2 = nn.BatchNorm2d(64)
         # self.batchNorm3 = nn.BatchNorm2d(128)
         self.sequence = nn.Sequential(
-            nn.Conv2d(3, 32, (10, 10)),
+            nn.Conv2d(3, 32, (10, 10),bias=False),
+            nn.BatchNorm2d(32),
             nn.ReLU(),
             nn.MaxPool2d(4),
 
-            nn.Conv2d(32, 64, (7, 7)),
+            nn.Conv2d(32, 64, (7, 7),bias=False),
+            nn.BatchNorm2d(64),
             nn.ReLU(),
             nn.MaxPool2d(4),
 
-            nn.Conv2d(64, 128, (4, 4)),
+            nn.Conv2d(64, 128, (4, 4),bias=False),
+            nn.BatchNorm2d(128),
             nn.ReLU(),
             nn.MaxPool2d(4),
 
@@ -148,12 +55,24 @@ class Siamese(nn.Module):
         x = self.sequence(x)
         x = x.view(x.size()[0], -1)
         x = self.fc(x)
+
         return x
 
-    def forward(self, x, y):
+    def forward(self, x, y,z=None):
+        #BCELoss
+        # x = self.forward_once(x)
+        # y = self.forward_once(y)
+        # output = F.pairwise_distance(x, y, keepdim=True)
+        # output = self.sigmoid(output)
+        # return output
+
+        # contrastive Loss
         x = self.forward_once(x)
         y = self.forward_once(y)
-        #output = abs(x - y)
-        #output = self.sigmoid(output)
-
         return x,y
+
+        # #triplet loss
+        # x = self.forward_once(x)
+        # y = self.forward_once(y)
+        # z = self.forward_once(z)
+        # return x,y,z
