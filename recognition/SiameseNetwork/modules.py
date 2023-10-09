8d45f77d93e23c62d5eec9d601bbf4fa9a927aa7
CMWL4501 <112795978+CMWL4501@users.noreply.github.com>
2023-10-10 00:13:18 +1000

    build a cnn net to provide embedding vector to siamese net

--- recognition/SiameseNetwork/modules.py
+++ recognition/SiameseNetwork/modules.py
@@ -1,3 +1,41 @@
 # containing the source code of the components of your model. 
 # Each component must be implementated as a class or a function
 
+import torch.nn as nn
+
+# Build CNN network and get its embedding vector
+class CNN(nn.Module):
+    def __init__(self, out_):
+        super(CNN, self).__init__()
+        self.conv = nn.Sequential(
+            
+            nn.Conv2d(1, 32, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2), # size: 256*240 -> 128*120
+
+            nn.Conv2d(32, 64, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2), # size: 128*120 -> 64*60
+
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(kernel_size=2, stride=2), # size: 64*60 -> 32*30
+            )
+        
+        self.fc = nn.Sequential(
+            nn.Linear(128*32*30, 256),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(256, 128),
+            nn.ReLU(inplace=True),
+
+            nn.Linear(128, 2),
+            )
+        
+    def forward(self, x):
+        out = self.conv(x)
+        out = out.view(out.size()[0], -1)
+        out = self.fc(out)
+        return out
+    
+    
