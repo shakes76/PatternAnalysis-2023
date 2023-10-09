3bb2ae8ae54828d34dea032587269c53f8098368
ray <99058662+raydesuyo@users.noreply.github.com>
2023-10-10 00:51:39 +1000

    Initial implementation of diffusion network

--- recognition/Stable_Diffusion_s4682728/modules.py
+++ recognition/Stable_Diffusion_s4682728/modules.py
@@ -2,17 +2,29 @@ import torch
 import torch.nn as nn
 
 class DiffusionProcess(nn.Module):
-    def __init__(self):
-        super().__init__()
-        pass
-    
-    def forward(self):
-        pass
+    def __init__(self, beta: float, num_steps: int):
+        super(DiffusionProcess, self).__init__()
+        self.beta = beta
+        self.num_steps = num_steps
+
+    def forward(self, x):
+        # Implement the diffusion process over `num_steps` steps
+        for step in range(self.num_steps):
+            noise = torch.randn_like(x) * self.beta
+            x = x + noise
+        return x
 
 class DiffusionNetwork(nn.Module):
-    def __init__(self):
-        super().__init__()
-        pass
-    def forward(self):
-        pass
+    def __init__(self, channels: int):
+        super(DiffusionNetwork, self).__init__()
+        self.conv1 = nn.Conv2d(channels, 64, 3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(64, 128, 3, stride=2, padding=1)
+        self.deconv = nn.ConvTranspose2d(128, channels, 3, stride=2, padding=1, output_padding=1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.conv2(x)
+        x = self.deconv(x)
+        return x
+
 
