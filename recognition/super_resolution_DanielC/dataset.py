7b59f383505330cacca3a1e1a471ad05d4fccb17
DHyunC <114087639+DHyunC@users.noreply.github.com>
2023-10-09 12:22:16 +1000

    created functions to make dataloaders for each set

--- recognition/super_resolution_DanielC/dataset.py
+++ recognition/super_resolution_DanielC/dataset.py
@@ -1,1 +1,28 @@
+import torchvision.transforms as transforms
+from torchvision.datasets import ImageFolder
+from torch.utils.data import ConcatDataset
+from torch.utils.data import DataLoader
+from utils import *
 
+def generate_train_loader():
+    transform = transforms.Compose([
+                            transforms.Resize((60,64)),
+                            transforms.ToTensor(),
+                            transforms.RandomHorizontalFlip()
+                        ])
+
+    AD_dataset = ImageFolder(root=AD_train_dir, transform=transform)
+    NC_dataset = ImageFolder(root=NC_train_dir, transform=transform)
+
+    return DataLoader(ConcatDataset([AD_dataset, NC_dataset]), batch_size=batch_size, shuffle=True)
+
+def generate_test_loader():
+    transform = transforms.Compose([
+                            transforms.ToTensor(),
+                            transforms.RandomHorizontalFlip()
+                        ])
+    
+    AD_dataset = ImageFolder(root=AD_test_dir, transform=transform)
+    NC_dataset = ImageFolder(root=NC_test_dir, transform=transform)
+
+    return DataLoader(ConcatDataset([AD_dataset, NC_dataset]), batch_size=batch_size, shuffle=True)
