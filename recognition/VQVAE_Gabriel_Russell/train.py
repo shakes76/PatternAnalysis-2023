c5b37285cc4fc8f20e756a841a3eb73bee25e444
Gabriel_Russell <garussell02@gmail.com>
2023-10-09 15:05:59 +1000

    Changing how file works, now retrieving code from individual train files for VQVAE and GAN separately.

--- recognition/VQVAE_Gabriel_Russell/train.py
+++ recognition/VQVAE_Gabriel_Russell/train.py
@@ -12,63 +12,17 @@ from dataset.py. Appropriate metrics are plotted during training.
 
 from modules import *
 from dataset import *
+import train_VQVAE
 import numpy as np
 import torch
 import matplotlib.pyplot as plt
-from scipy.signal import savgol_filter
+
+train_VQVAE_model = train_VQVAE.TrainVQVAE()
+
+train_VQVAE_model.train()
+
+#Load VQVAE Model
+
+#Make encoding 
 
 
-
-
-#Initialise GPU
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-#Retrive datasets -> train, validate and test dataloaders
-load_data = OASISDataloader()
-train_data = load_data.get_train()
-validate_data = load_data.get_validate()
-test_data = load_data.get_test()
-
-#Load VQVAE model and optimizer 
-model = VQVAEModel()
-model = model.to(device)
-load_optim = Optimizer()
-optimizer = load_optim.Adam
-p = Parameters()
-
-#Calculated separately
-data_var = 2.1689048e-06
-
-
-model.train()
-reconstruction_err_vals = []
-for epochs in range(5):
-    for i in enumerate(train_data):
-        batch_num, img = i
-        img = img.to(device)
-        optimizer.zero_grad()
-
-        vec_quantizer_loss, recon, _ = model(img)
-        #Reconstruction loss is mean squared error for images
-        reconstruction_err = F.mse_loss(recon, img)/ data_var
-        total_loss = reconstruction_err + vec_quantizer_loss
-        total_loss.backward()
-
-        optimizer.step()
-
-        reconstruction_err_vals.append(reconstruction_err.item())
-        if batch_num % 20 == 0:
-            print('recon_error: %.3f' % np.mean(reconstruction_err_vals[-100:]))
-            print()
-    print("Epoch + " + str(epochs))
-train_res_recon_error_smooth = savgol_filter(reconstruction_err_vals, 375, 7)
-
-
-f = plt.figure(figsize=(16,8))
-ax = f.add_subplot(1,2,1)
-ax.plot(train_res_recon_error_smooth)
-ax.set_yscale('log')
-ax.set_title('Smoothed NMSE.')
-ax.set_xlabel('iteration')
-
-plt.savefig("reconstruction_err.png")
