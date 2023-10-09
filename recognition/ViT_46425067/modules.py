9c5102318d4aa3a1cdad40c0f589cf4cd6a39569
Rodger Xiang <rodxiang2@gmail.com>
2023-10-09 19:53:24 +1000

    removed conv2d version of creating patch embeddings

--- recognition/ViT_46425067/modules.py
+++ recognition/ViT_46425067/modules.py
@@ -11,55 +11,25 @@ import torch.nn as nn
 from einops.layers.torch import Rearrange
 
 class PatchEmbedding(nn.Module):
-    """Split images into patches
-    """
-    def __init__(self, img_size: int, patch_size:int, embed_dim=768, in_channels=1, linear_mode=True):
+    def __init__(self, patch_size:int, embed_dim:int, in_channels:int):
         """
         initialise patch embedding layer for the ViT
 
         Args:
-            img_size (int): H or W of image (should be a square number)
-            patch_size (int): H or W of patch size (should be a square number)
-            embed_dim (int, optional): Size of patch embedding stays constant across entire network. Defaults to 768.
-            in_channels (int, optional): RGB channel of image. Defaults to 1 for greyscale.
-            linear_mode (bool): whether to use linear projection method or convolution
+            patch_size (int):   H or W of patch size (should be a square number)
+            embed_dim (int):    Size of patch embedding stays constant across entire network
+            in_channels (int):  RGB channel of image
         """
         super().__init__()        
-        self.img_size = img_size
-        self.patch_size = patch_size
-        self.num_patches = (img_size // patch_size) ** 2
-        self.linear_mode = linear_mode
-        #kernel is the same size of the patch size and will never overlap since
-        self.projection = nn.Conv2d(in_channels=in_channels, 
-                                    out_channels=embed_dim,
-                                    kernel_size=self.patch_size,
-                                    stride=self.patch_size)
-        
         # embedding using linear layers
-        if self.linear_mode:
-            self.projection = nn.Sequential(
-                Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', 
-                            p1=self.patch_size, p2=self.patch_size),
-                # nn.LayerNorm(self.patch_size * self.patch_size * in_channels),
-                nn.Linear(self.patch_size * self.patch_size * in_channels, embed_dim),
-                # nn.LayerNorm(embed_dim)
-            )
+        self.projection = nn.Sequential(
+            Rearrange('b c (h p1) (w p2) -> b (h w) (p1 p2 c)', 
+                        p1=patch_size, p2=patch_size),
+            nn.Linear(patch_size * patch_size * in_channels, embed_dim),
+        )
         
     def forward(self, x):
-        """forward pass
-
-        Args:
-            x (tensor): image tensor of (batch_size, img_channels, H, W)
-        
-        Returns:
-            tensor: (batch_size, num_patches, embed_dim)
-        """
-        x = self.projection(x)
-        
-        #if not using the linear project we need to reshape the tensor after convolution
-        if not self.linear_mode:
-            x = x.flatten(2).transpose(1, 2)
-        return x
+        return self.projection(x)
     
     
 class ViT_torch(nn.Module):
@@ -71,15 +41,25 @@ class ViT_torch(nn.Module):
                     depth:int,
                     num_heads:int,
                     mlp_dim:int,
-                    drop_prob=0.1,
-                    linear_embed=False):
+                    drop_prob=0.1):
+        """Creates a visual transformer model
+
+        Args:
+            img_size (int):     size of images to be passed into the model
+            patch_size (int):   size of a image patch
+            img_channels (int): number of colour channels of the image
+            num_classes (int):  number of classes of the ADNI dataset
+            embed_dim (int):    dimension of the patch embedding
+            depth (int):        number of transformer encoder layers
+            num_heads (int):    number of attention heads for each encoder layer
+            mlp_dim (int):      number of hidden units of the feed forward layer in the encoder layers
+            drop_prob (float, optional): probability for dropout in the model. Defaults to 0.1.
+        """
         super().__init__()
         
-        self.patch_embed = PatchEmbedding(img_size=img_size,
-                                            patch_size=patch_size,
+        self.patch_embed = PatchEmbedding(patch_size=patch_size,
                                             embed_dim=embed_dim,
-                                            in_channels=img_channels,
-                                            linear_mode=linear_embed) #TODO: try changing to linear mode
+                                            in_channels=img_channels)
         #class token to determine which class the image belongs to
         self.class_token = nn.Parameter(torch.zeros(1, 1, embed_dim)) #zeros or randn
         #positional information of patches
