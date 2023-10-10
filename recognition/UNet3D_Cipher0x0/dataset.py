ae41718f0da5df9c7448f872966f4dde4311829a
Jiaxun Feng <s4694975@labs.eait.uq.edu.au>
2023-10-10 15:43:51 +1000

    Since Prostate 3D dataset is not in Torch dataset, reconstruct the dataset to fit torch dataloader

--- recognition/UNet3D_Cipher0x0/dataset.py
+++ recognition/UNet3D_Cipher0x0/dataset.py
@@ -1,20 +1,27 @@
+import glob
 import torch
 from torch.utils.data import Dataset
-from torchvision import datasets
-from torchvision.transforms import ToTensor
-import matplotlib.pyplot as plt
+from torch.utils.data import DataLoader
+import nibabel as nib
 
 
-training_data = datasets.FashionMNIST(
-    root="data",
-    train=True,
-    download=True,
-    transform=ToTensor()
-)
+class Prostate_3D(Dataset):
+    def __init__(self, data_img_dir, label_img_dir, transform=None, target_transform=None):
+        self.data_img_path = glob.glob(data_img_dir + '*.nii.gz')
+        self.label_img_path = glob.glob(label_img_dir + '*.nii.gz')
+        #self.data = nib.load(self.data_img_path).get_fdata()
+        #self.label = nib.load(self.label_img_path).get_fdata()
+        self.transform = transform
+        self.target_transform = target_transform
 
-test_data = datasets.FashionMNIST(
-    root="data",
-    train=False,
-    download=True,
-    transform=ToTensor()
-)
+    def __len__(self):
+        return len(self.label_img_path)
+
+    def __getitem__(self, idx):
+        data = torch.from_numpy(nib.load(self.data_img_path[idx]).get_fdata()).unsqueeze(0)
+        label = torch.from_numpy(nib.load(self.label_img_path[idx]).get_fdata()).unsqueeze(0)
+        if self.transform:
+            data = self.transform(data)
+        if self.target_transform:
+            label = self.target_transform(label)
+        return data, label
