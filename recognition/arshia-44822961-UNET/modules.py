a3d6997f05abb795c15dc6c6ac8598ece1a49c47
arshiaar <69202571+arshiaar@users.noreply.github.com>
2023-10-10 20:50:25 +1000

    feat: Implement Improved ModelUnet class with architecture and forward function

--- recognition/arshia-44822961-UNET/modules.py
+++ recognition/arshia-44822961-UNET/modules.py
@@ -145,3 +145,130 @@ class SoftMaxLayer(nn.Module):
     def forward(self, x):
         out = self.softmax(x)
         return out
+
+class ImprovedUnet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        # write this entire thing and then go get help. 
+        super(ImprovedUnet, self).__init__()
+
+        # because RGB colour images are used. 
+        in_channels = 3
+        out_channels = 16
+        self.conv_layer_1 = StandardConv(in_channels, out_channels,
+                                         kernel_size=3, stride=1, padding=0 )
+        self.context_layer_1 = ContextModule(in_channels,out_channels,dropout_p=0.3)
+
+        in_channels = 16 
+        out_channels = 32 
+        # so these features are allowing for more features as we go down. 
+        self.conv_stride_layer_1 = Conv2dStride2(in_channels, out_channels,kernel_size=3, padding=1)
+        
+        # confirm if input 16 or 32? 
+        self.context_layer_2 = ContextModule(in_channels,out_channels, dropout_p=0.3)
+
+        # input size changes here?
+        in_channels = 32 
+        out_channels = 64
+        self.conv_stride_layer_2 = Conv2dStride2(in_channels, out_channels,kernel_size=3, padding=1)
+        self.context_layer_3 = ContextModule(in_channels,out_channels, dropout_p=0.3)
+
+        # now next layer down 
+        in_channels = 64 
+        out_channels = 128
+        self.conv_stride_layer_3 = Conv2dStride2(in_channels, out_channels, kernel_size=3, padding=1)
+        self.context_layer_4 = ContextModule(in_channels,out_channels)
+
+        # and then next layer down again 
+        in_channels = 128
+        out_channels = 256 
+        self.conv_stride_layer_4 = Conv2dStride2(in_channels, out_channels)
+        self.context_layer_5 = ContextModule(in_channels, out_channels)
+
+        # upsample module here.
+        # input 128 and output is 256 
+        in_channels = 256
+        out_channels = 128 
+        # yes - upsampling halves the number of feature maps. 
+        self.upsample_layer_1 = UpsamplingModule(in_channels, out_channels)
+
+        # upscaling part of the image 
+        in_channels = 256 
+        out_channels = 128
+        self.localise_layer_1 = LocalisationModule(in_channels,out_channels)
+
+        in_channels = 128
+        out_channels = 64
+        self.upsample_layer_2 = UpsamplingModule(in_channels, out_channels)
+
+        in_channels = 64 
+        out_channels = 32 
+        self.localise_layer_2 = LocalisationModule(in_channels, out_channels)
+
+        in_channels = 32
+        out_channels = 16
+        self.upsample_layer_3 = UpsamplingModule(in_channels, out_channels)
+
+        in_channels = 16
+        out_channels = 32 
+        self.conv_layer_2 = StandardConv(in_channels, out_channels)
+
+        # softmax layer 
+       
+
+
+
+
+
+
+    # TODO - fix naming 
+    # VERIFY THAT CALLING THE FORWARD FUNCTION HERE MAKES SENSE 
+    def forward(self, x):
+        # downsampling is a repeat of conv layer and context module
+        # and then their element wise addition here. 
+        input_conv_out = self.conv_layer_1.forward(x)
+        context_out_1 = self.context_layer_1.forward(input)
+        # element sum into the next conv 
+        element_sum_1= input_conv_out + context_out_1 
+
+        # into first 3x3 stride 2 conv 
+        conv3_layer1 = self.conv_stride_layer_1.forward(element_sum_1)
+        context_out_2  = self.context_layer_2.forward(conv3_layer1)
+        element_sum_2 = conv3_layer1 + context_out_2
+
+        # input into the next stride 2 block 
+        conv_out = self.conv_stride_layer_2.forward(element_sum_2)
+        context_out_3 = self.context_layer_3.forward(conv_out)
+        element_sum_3 = conv_out + context_out_3 
+
+        # next downsample 
+        conv_out_2 = self.conv_stride_layer_4.forward(element_sum_3)
+        context_out_4 = self.context_layer_4.forward(conv_out_2)
+        element_sum_4 = conv_out_2 + context_out_4 
+
+        # First upsampling module. 
+        upsample_out_1 = self.upsample_layer_1.forward(element_sum_4)
+
+        # CONCAT with element sum 3 
+        concat_1 = torch.cat((element_sum_4, upsample_out_1), dim=1)
+
+        # add localisation module 
+        localisation_out_1 = self.localise_layer_1.forward(concat_1)
+        # fed directly into the upsampling layer 
+        upsample_out_2 = self.upsample_layer_2.foward(localisation_out_1)
+
+        # concat with element sum 3 here. 
+        concat_2 = torch.cat((element_sum_3, upsample_out_2))
+
+        # feed into localisation 2
+        localisation_out_2 = self.localise_layer_1.forward(concat_1)
+
+
+
+
+        
+
+
+        pass 
+
+
+        
