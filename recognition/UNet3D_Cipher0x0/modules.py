ce6957632eea7cc2c35b7e7d05655067832a95b7
Jiaxun Feng <s4694975@labs.eait.uq.edu.au>
2023-10-10 15:27:27 +1000

    Add Unet3D

---
+++ recognition/UNet3D_Cipher0x0/modules.py
@@ -0,0 +1,45 @@
+class UNet(nn.Module):
+    def __init__(self, n_channels, n_classes, width_multiplier=1, trilinear=True, use_ds_conv=False):
+        """A simple 3D Unet, adapted from a 2D Unet from https://github.com/milesial/Pytorch-UNet/tree/master/unet
+        Arguments:
+          n_channels = number of input channels; 3 for RGB, 1 for grayscale input
+          n_classes = number of output channels/classes
+          width_multiplier = how much 'wider' your UNet should be compared with a standard UNet
+                  default is 1;, meaning 32 -> 64 -> 128 -> 256 -> 512 -> 256 -> 128 -> 64 -> 32
+                  higher values increase the number of kernels pay layer, by that factor
+          trilinear = use trilinear interpolation to upsample; if false, 3D convtranspose layers will be used instead
+          use_ds_conv = if True, we use depthwise-separable convolutional layers. in my experience, this is of little help. This
+                  appears to be because with 3D data, the vast vast majority of GPU RAM is the input data/labels, not the params, so little
+                  VRAM is saved by using ds_conv, and yet performance suffers."""
+        super(UNet, self).__init__()
+        _channels = (32, 64, 128, 256, 512)
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+        self.channels = [int(c*width_multiplier) for c in _channels]
+        self.trilinear = trilinear
+        self.convtype = DepthwiseSeparableConv3d if use_ds_conv else nn.Conv3d
+
+        self.inc = DoubleConv(n_channels, self.channels[0], conv_type=self.convtype)
+        self.down1 = Down(self.channels[0], self.channels[1], conv_type=self.convtype)
+        self.down2 = Down(self.channels[1], self.channels[2], conv_type=self.convtype)
+        self.down3 = Down(self.channels[2], self.channels[3], conv_type=self.convtype)
+        factor = 2 if trilinear else 1
+        self.down4 = Down(self.channels[3], self.channels[4] // factor, conv_type=self.convtype)
+        self.up1 = Up(self.channels[4], self.channels[3] // factor, trilinear)
+        self.up2 = Up(self.channels[3], self.channels[2] // factor, trilinear)
+        self.up3 = Up(self.channels[2], self.channels[1] // factor, trilinear)
+        self.up4 = Up(self.channels[1], self.channels[0], trilinear)
+        self.outc = OutConv(self.channels[0], n_classes)
+
+    def forward(self, x):
+        x1 = self.inc(x)
+        x2 = self.down1(x1)
+        x3 = self.down2(x2)
+        x4 = self.down3(x3)
+        x5 = self.down4(x4)
+        x = self.up1(x5, x4)
+        x = self.up2(x, x3)
+        x = self.up3(x, x2)
+        x = self.up4(x, x1)
+        logits = self.outc(x)
+        return logits
