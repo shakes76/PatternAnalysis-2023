331d3a1ac8739a784c0d9a6ae9fc18df1f8b9003
jdw5 <joshuadwallace1@gmail.com>
2023-10-11 01:00:02 +1000

    Added VQ code from Pytorch source

--- recognition/Siamese_Joshua_Wallace/modules.py
+++ recognition/Siamese_Joshua_Wallace/modules.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
 class Siamese(nn.Module):
 
@@ -177,4 +178,70 @@ class Decoder(nn.Module):
         out = self.transpose2(out)
         return out
 
+class VectorQuantizer(nn.Module):
+    """
+    Discretization bottleneck part of the VQ-VAE.
 
+    Inputs:
+    - n_embeddings : number of embeddings
+    - dim_embeddings : dimension of embedding
+    - beta : commitment cost used in loss term, beta * ||z_e(x)-sg[e]||^2
+    """
+
+    def __init__(self, n_embeddings, dim_embeddings, beta):
+        super(VectorQuantizer, self).__init__()
+        self.n_embeddings = n_embeddings
+        self.dim_embeddings = dim_embeddings
+        self.beta = beta
+
+        self.embedding = nn.Embedding(self.n_embeddings, self.dim_embeddings)
+        self.embedding.weight.data.uniform_(-1.0 / self.n_embeddings, 1.0 / self.n_embeddings)
+
+    def forward(self, z):
+        """
+        Inputs the output of the encoder network z and maps it to a discrete 
+        one-hot vector that is the index of the closest embedding vector e_j
+
+        z (continuous) -> z_q (discrete)
+
+        z.shape = (batch, channel, height, width)
+
+        quantization pipeline:
+
+            1. get encoder input (B,C,H,W)
+            2. flatten input to (B*H*W,C)
+
+        """
+        # reshape z -> (batch, height, width, channel) and flatten
+        z = z.permute(0, 2, 3, 1).contiguous()
+        z_flattened = z.view(-1, self.dim_embeddings)
+        # distances from z to embeddings e_j (z - e)^2 = z^2 + e^2 - 2 e * z
+
+        d = torch.sum(z_flattened ** 2, dim=1, keepdim=True) + \
+            torch.sum(self.embedding.weight**2, dim=1) - 2 * \
+            torch.matmul(z_flattened, self.embedding.weight.t())
+
+        # find closest encodings
+        min_embeddingsncoding_indices = torch.argmin(d, dim=1).unsqueeze(1)
+        min_embeddingsncodings = torch.zeros(
+            min_embeddingsncoding_indices.shape[0], self.n_embeddings).to(device)
+        min_embeddingsncodings.scatter_(1, min_embeddingsncoding_indices, 1)
+
+        # get quantized latent vectors
+        z_q = torch.matmul(min_embeddingsncodings, self.embedding.weight).view(z.shape)
+
+        # compute loss for embedding
+        loss = torch.mean((z_q.detach()-z)**2) + self.beta * \
+            torch.mean((z_q - z.detach()) ** 2)
+
+        # preserve gradients
+        z_q = z + (z_q - z).detach()
+
+        # perplexity
+        e_mean = torch.mean(min_embeddingsncodings, dim=0)
+        perplexity = torch.exp(-torch.sum(e_mean * torch.log(e_mean + 1e-10)))
+
+        # reshape back to match original input shape
+        z_q = z_q.permute(0, 3, 1, 2).contiguous()
+
+        return loss, z_q, perplexity, min_embeddingsncodings, min_embeddingsncoding_indices
