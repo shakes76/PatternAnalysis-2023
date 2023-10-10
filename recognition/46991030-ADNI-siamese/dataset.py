73afea241b5f16cdf57ccb5d68e9ac044b4c0914
Darcy Robson <17976794+drobson03@users.noreply.github.com>
2023-10-10 10:03:29 +1000

    Add dataset loader

---
+++ recognition/46991030-ADNI-siamese/dataset.py
@@ -0,0 +1,37 @@
+import tensorflow as tf
+
+
+def load_dataset(path):
+    train_data_path = path + "/train"
+    test_data_path = path + "/test"
+
+    print(f"Loading training data from {train_data_path}")
+    train_ds = tf.keras.utils.image_dataset_from_directory(
+        train_data_path,
+        validation_split=0.2,
+        subset="training",
+        image_size=(64, 64),
+        batch_size=32,
+        seed=321,
+        color_mode="grayscale",
+    )
+
+    print(f"Loading validation data from {train_data_path}")
+    val_ds = tf.keras.utils.image_dataset_from_directory(
+        train_data_path,
+        validation_split=0.2,
+        subset="validation",
+        image_size=(64, 64),
+        batch_size=32,
+        seed=321,
+        color_mode="grayscale",
+    )
+
+    print(f"Loading test data from {test_data_path}")
+    test_ds = tf.keras.utils.image_dataset_from_directory(
+        test_data_path, image_size=(64, 64), batch_size=32, color_mode="grayscale"
+    )
+
+    print("Finished loading dataset")
+
+    return train_ds, val_ds, test_ds
