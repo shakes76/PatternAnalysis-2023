21461fd43d59e94ae48a7036c0c7df9f95c6117c
ChocomintIce1 <69633077+ChocomintIce1@users.noreply.github.com>
2023-10-11 00:05:45 +1000

    Using pre-processed dataset instead

--- dataset.py
+++ dataset.py
@@ -1,49 +1,12 @@
-import pandas as pd
+import numpy as np
 import torch
 from torch_geometric.data import Data
-import json
-import networkx as nx
-import matplotlib.pyplot as plt
 
+features = torch.Tensor(np.load('facebook_large/features.npy'))
+edges = np.rot90(np.load('facebook_large/edges.npy'), 1)
+edges = torch.Tensor(edges.copy())
+targets = torch.Tensor(np.load('facebook_large/target.npy'))
 
+test = torch.Tensor(np.array(([1,2,3], [3,4,5])))
 
-def format_features(json_file_path='facebook_large/musae_facebook_features.json'):
-    '''
-    format json -> csv
-    
-    Returns: csv
-    '''
-    df = pd.DataFrame()
-    
-    with open(json_file_path, 'r') as json_file:
-        features_dict = json.load(json_file)
-
-        for key, item in features_dict.items():
-            df1 = pd.DataFrame({'key':key, 'item': [item]})
-            df = pd.concat([df, df1], ignore_index=True)
-
-    df.to_csv('facebook_large/musae_facebook_features.csv', index=False)
-
-def format_target(csv_file_path='facebook_large/musae_facebook_target.csv'):
-    '''
-    Does something...
-    '''
-    df = pd.read_csv(csv_file_path).iloc[:, -1]
-    df.columns = ['index', 'target']
-    df.to_csv('facebook_large/musae_facebook_target1.csv', index=True)
-
-format_features()
-
-# features = pd.read_csv('facebook_large/musae_facebook_features.csv')
-# x = torch.Tensor(features.values)
-
-# edge_index = pd.read_csv('facebook_large/musae_facebook_edges.csv')
-# y = pd.read_csv('facebook_large/musae_facebook_target.csv')
-
-# data = Data(x=x, edge_index=edge_index, y=y)
-
-# print(data.is_node_attr)
-
-
-edge_list_df = pd.read_csv('facebook_large/musae_facebook_edges.csv')
-edge_list = nx.from_pandas_edgelist(edge_list_df, source='id_1', target='id_2')
+data = Data(x=features, edge_index=edges, edge_attr=None, y=targets)
