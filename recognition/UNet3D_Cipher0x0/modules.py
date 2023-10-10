b3a57a095fe6b2cf1b31684eef136eb6b22899e6
Cipher <57138168+Cipher0x0@users.noreply.github.com>
2023-10-11 00:11:02 +1000

    Fixed the bug inside func Up()

--- recognition/UNet3D_Cipher0x0/modules.py
+++ recognition/UNet3D_Cipher0x0/modules.py
@@ -98,6 +98,17 @@ class Up(nn.Module):
             self.up = nn.ConvTranspose3d(in_channels, in_channels // 2, kernel_size=2, stride=2)
             self.conv = DoubleConv(in_channels, out_channels)
 
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
+
 
 class OutConv(nn.Module):
     def __init__(self, in_channels, out_channels):
