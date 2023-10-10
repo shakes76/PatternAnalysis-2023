15aef5bb52a0c1dfabf1b7d75ac39f47540b7a36
lambdarm <141205527+lambdarm@users.noreply.github.com>
2023-10-10 22:20:00 +1000

    Added code to train.py
    Added based train() function which trains the siamese model based upon the ADNI data
    Added comments
    
    Knows bug: data from dataset.py is 1 input, need to account for triple loss (anchor, pos, neg)

--- recognition/ADNI-s4702860/train.py
+++ recognition/ADNI-s4702860/train.py
@@ -1,1 +1,29 @@
+import os
+from keras.callbacks import TensorBoard
+from dataset import load_dataset
+from modules import SiameseModel
+import matplotlib.pyplot as plt
 
+"""
+Function used for training the siamese network
+
+The data comes from dataset.py and the model comes from modules.py
+"""
+def train():
+    model = SiameseModel().model  # load the model
+    train_generator, test_generator = load_dataset() # load the data
+
+    # fit the model to the data
+    model.fit(train_generator, epochs=10, 
+                        validation_data=test_generator)
+    
+    # Save the entire model
+    model.save("ADNI_predictor.keras")
+
+
+
+def main():
+    train()
+
+if __name__=="__main__":
+    main()
