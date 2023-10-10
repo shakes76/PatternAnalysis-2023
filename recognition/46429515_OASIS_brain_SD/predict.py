11452f8b2ac77d609c9c668391c1f30b855f3bdc
Alex Nguyen <alexnguyen@s4642951.vpn.uq.edu.au>
2023-10-11 09:44:33 +1000

    Added code to predict from multiple input images

--- recognition/46429515_OASIS_brain_SD/predict.py
+++ recognition/46429515_OASIS_brain_SD/predict.py
@@ -1,3 +1,4 @@
+import os
 import module
 import utils
 import torch
@@ -13,22 +14,26 @@ model = module.UNet()
 
 # Load saved weights (pre-trained model)
 # , map_location=torch.device('cpu')
-model.load_state_dict(torch.load('best_model.pth'))
+model.load_state_dict(torch.load('best_model.pth', map_location=torch.device('cpu')))
 model.eval()
 
 preprocess = transforms.Compose([
-    transforms.Resize((224, 224)),
+    transforms.Resize((utils.IMAGE_SIZE, utils.IMAGE_SIZE)),
     transforms.ToTensor(),
-    # transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+    transforms.Normalize(mean=[0.5], std=[0.5]),
 ])
 
-# Load and preprocess image input
-input_data = Image.open('./predict_image/input_image.png')
-input_tensor = preprocess(input_data)
-input_batch = input_tensor.unsqueeze(0)
+# Load image inputs into a list
+image_paths = []
+for subdir, _, files in os.walk('./predict_image'):
+    for file in files:
+        if file.endswith(".png"):
+            image_paths.append(os.path.join(subdir, file))
+
+input_datas = [Image.open(i) for i in image_paths]
 
 # Save image for predict.py
-def save_tensor_image(image):
+def save_tensor_image(image, counter):
     reverse_transforms = transforms.Compose([
         transforms.Lambda(lambda t: (t + 1) / 2),
         transforms.Lambda(lambda t: t.permute(1, 2, 0)),
@@ -42,24 +47,32 @@ def save_tensor_image(image):
         image = image[0, :, :, :]
     
     # save image here  
-    image_path = './denoised_image.png'
+    image_path = f'./predict_output/generated_image_{counter}.png'
     img = reverse_transforms(image)
     img.save(image_path)
 
 # Perform inference
 with torch.no_grad():
+    counter = 0
     t = torch.randint(0, utils.T, (utils.BATCH_SIZE,), device=device).long()
-    output = model(input_batch, t)
-  
-    img_size = utils.IMAGE_SIZE
-    img = torch.randn((1, 1, img_size, img_size), device=device)
-    num_images = 1
-    stepsize = int(utils.T/num_images)
-
-    for i in range(0, utils.T)[::-1]:
-        t = torch.full((1,), i, device=device, dtype=torch.long)
-        img = utils.sample_timestep(model, img, t)
-        # Maintain natural range of distribution
-        img = torch.clamp(img, -1.0, 1.0)
-        if i % stepsize == 0:
-            save_tensor_image(img.detach().cpu())
+    
+    for input_data in input_datas:
+        # Preprocess input data
+        input_tensor = preprocess(input_data)
+        input_batch = input_tensor.unsqueeze(0)
+    
+        output = model(input_batch, t)
+    
+        img_size = utils.IMAGE_SIZE
+        img = torch.randn((1, 1, img_size, img_size), device=device)
+        num_images = 1
+        stepsize = int(utils.T/num_images)
+    
+        for i in range(0, utils.T)[::-1]:
+            t = torch.full((1,), i, device=device, dtype=torch.long)
+            img = utils.sample_timestep(model, img, t)
+            # Maintain natural range of distribution
+            img = torch.clamp(img, -1.0, 1.0)
+            if i % stepsize == 0:
+                save_tensor_image(img.detach().cpu(), counter)
+                counter += 1
