b445f5d6be1df55a6ee9869994d60ae6aacfd38a
ChocomintIce1 <69633077+ChocomintIce1@users.noreply.github.com>
2023-10-11 00:36:45 +1000

    Made test_train simplier

--- dataset.py
+++ dataset.py
@@ -5,6 +5,8 @@ from torch_geometric.transforms import RandomLinkSplit
 
 
 def load_data():
+    '''
+    '''
     features = torch.Tensor(np.load('facebook_large/features.npy'))
     edges = np.rot90(np.load('facebook_large/edges.npy'), 1)
     edges = torch.Tensor(edges.copy())
@@ -15,15 +17,16 @@ def load_data():
     return data
 
 def test_train():
-    features = torch.Tensor(np.load('facebook_large/features.npy'))
-    edges = np.rot90(np.load('facebook_large/edges.npy'), 1)
-    edges = torch.Tensor(edges.copy())
-    targets = torch.Tensor(np.load('facebook_large/target.npy'))
-    
-    data = Data(x=features, edge_index=edges, edge_attr=None, y=targets)
+    '''
+    Returns: train_data, val_data, test_data
+    '''
+    data = load_data()
     transform = RandomLinkSplit(is_undirected=True)
+    
     return transform(data)
 
 data = load_data()
 train_data, val_data, test_data = test_train()
-print(val_data)
+print('train: ', train_data.num_nodes)
+print('\nval: ', val_data.num_nodes)
+print('\ntest: ', test_data.num_nodes)
