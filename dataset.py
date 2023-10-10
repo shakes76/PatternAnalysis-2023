a7a54a0352084da2acd859e9c6bc7b6a0009a5f0
ChocomintIce1 <69633077+ChocomintIce1@users.noreply.github.com>
2023-10-11 00:08:34 +1000

    Created dataloader

--- dataset.py
+++ dataset.py
@@ -2,11 +2,11 @@ import numpy as np
 import torch
 from torch_geometric.data import Data
 
-features = torch.Tensor(np.load('facebook_large/features.npy'))
-edges = np.rot90(np.load('facebook_large/edges.npy'), 1)
-edges = torch.Tensor(edges.copy())
-targets = torch.Tensor(np.load('facebook_large/target.npy'))
-
-test = torch.Tensor(np.array(([1,2,3], [3,4,5])))
-
-data = Data(x=features, edge_index=edges, edge_attr=None, y=targets)
+def load_data():
+    features = torch.Tensor(np.load('facebook_large/features.npy'))
+    edges = np.rot90(np.load('facebook_large/edges.npy'), 1)
+    edges = torch.Tensor(edges.copy())
+    targets = torch.Tensor(np.load('facebook_large/target.npy'))
+    
+    data = Data(x=features, edge_index=edges, edge_attr=None, y=targets)
+    return data
