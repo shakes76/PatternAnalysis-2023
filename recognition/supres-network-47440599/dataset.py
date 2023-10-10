f7ebae018078eb228ea31c79730aec13ddef59d0
nleek0 <109025848+nleek0@users.noreply.github.com>
2023-10-10 15:13:44 +1000

    Setting up the data loader
    The data loader is set up and loads the data successfully

---
+++ recognition/supres-network-47440599/dataset.py
@@ -0,0 +1,36 @@
+import torch
+import torchvision.datasets as dset
+import torchvision.transforms as transforms
+import torchvision.utils as vutils
+import numpy as np
+import matplotlib.pyplot as plt
+
+def load_data():
+    dataroot = "./data/AD_NC/train"
+
+
+    batch_size = 1
+
+    workers = 2
+
+    device = torch.device("cpu")
+    dataset = dset.ImageFolder(root=dataroot,
+                            transform=transforms.Compose([
+                                transforms.Resize(100),
+                                transforms.ToTensor(),
+                                transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
+                            ]))
+
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
+                                            shuffle=True, num_workers=workers)
+
+    real_batch = next(iter(dataloader))
+    plt.figure(figsize=(8,8))
+    plt.axis("off")
+    plt.title("Training Images")
+    plt.imshow(np.transpose(vutils.make_grid(real_batch[0].to(device)[:64], padding=2, normalize=True).cpu(),(1,2,0)))
+    plt.show()
+
+if __name__ == "__main__":
+    load_data()
+    
