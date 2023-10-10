d960ecd14562e030932a0c6f6f7fc484cd11705b
jdw5 <joshuadwallace1@gmail.com>
2023-10-11 00:49:36 +1000

    Added encoder and decoder for pivot to VQVAE

--- recognition/Siamese_Joshua_Wallace/main.py
+++ recognition/Siamese_Joshua_Wallace/main.py
@@ -1,13 +1,13 @@
 import torch
 from modules import Siamese
 from utils import Config
-from dataset import ADNIDataset
+from dataset import Dataset
 from train import Train
 
 if __name__ == '__main__':
     net = Siamese()
     config = Config(lr=1e-4, wd=1e-5, epochs=1, batch_size=32, root_dir='./AD_NC', gpu='cuda')
-    dataset = ADNIDataset(batch_size=config.batch_size, fraction=0.1)
+    dataset = Dataset(batch_size=config.batch_size, fraction=0.1)
     print('Dataset loaded, beginning training.')
     trainer = Train(net, dataset, config)
     trainer.train()
--- recognition/Siamese_Joshua_Wallace/modules.py
+++ recognition/Siamese_Joshua_Wallace/modules.py
@@ -8,52 +8,143 @@ class Siamese(nn.Module):
     def __init__(self):
         super(Siamese, self).__init__()
 
-        self.conv1 = nn.Conv2d(3, 64, 10)
-        self.conv2 = nn.Conv2d(64, 128, 7)
-        self.conv3 = nn.Conv2d(128, 256, 4)
-        self.conv4 = nn.Conv2d(256, 512, 4)
+        self.conv = nn.Sequential(
+            nn.Conv2d(3, 64, kernel_size=10), nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, kernel_size=7), nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(128, 256, kernel_size=4), nn.ReLU(),
+            nn.MaxPool2d(2),
+            nn.Conv2d(256, 512, kernel_size=4), nn.ReLU()
+        )
 
-        self.pool1 = nn.MaxPool2d(2, stride=2)
-        self.pool2 = nn.MaxPool2d(2, stride=2)
-        self.pool3 = nn.MaxPool2d(2, stride=2)
-        self.pool4 = nn.MaxPool2d(2, stride=2)
+        self.embedding = nn.Sequential(
+            nn.Linear(128*24*24, 512),
+            nn.Linear(512, 1),
 
-        self.relu1 = nn.ReLU(inplace=True)
-        self.relu2 = nn.ReLU(inplace=True)
-        self.relu3 = nn.ReLU(inplace=True)
-        self.relu4 = nn.ReLU(inplace=True)
+            nn.ReLU()
+        )
 
-        self.fc1 = nn.Linear(128*24*24, 512)
-        self.fc2 = nn.Linear(512, 1)
+        # Classify whether the images belong to the same class or different classes
+        self.fc = nn.Sequential(
+            nn.Sigmoid()
+        )
 
-        self.sigmoid = nn.Sigmoid()
+        # self.conv1 = nn.Conv2d(3, 64, 10)
+        # self.conv2 = nn.Conv2d(64, 128, 7)
+        # self.conv3 = nn.Conv2d(128, 256, 4)
+        # self.conv4 = nn.Conv2d(256, 512, 4)
+
+        # self.pool1 = nn.MaxPool2d(2, stride=2)
+        # self.pool2 = nn.MaxPool2d(2, stride=2)
+        # self.pool3 = nn.MaxPool2d(2, stride=2)
+        # self.pool4 = nn.MaxPool2d(2, stride=2)
+
+        # self.relu1 = nn.ReLU(inplace=True)
+        # self.relu2 = nn.ReLU(inplace=True)
+        # self.relu3 = nn.ReLU(inplace=True)
+        # self.relu4 = nn.ReLU(inplace=True)
+
+        # self.fc1 = nn.Linear(128*24*24, 512)
+        # self.fc2 = nn.Linear(512, 1)
+
+        # self.sigmoid = nn.Sigmoid()
 
     def forward(self, x1, x2):
         h1 = self.sub_forward(x1)
         h2 = self.sub_forward(x2)
         diff = torch.abs(h1 - h2)
-        scores = self.fc2(diff)
+        scores = self.fc(diff)
+        
         return scores
 
     def sub_forward(self, x):
-        x = self.conv1(x)
-        x = self.pool1(x)
-        x = self.relu1(x)
-
-        x = self.conv2(x)
-        x = self.pool2(x)
-        x = self.relu2(x)
-
-        x = self.conv3(x)
-        x = self.pool3(x)
-        x = self.relu3(x)
-
-        x = self.conv4(x)
-        x = self.pool4(x)
-        x = self.relu4(x)
-
-        x = x.view(x.shape[0], -1)
-        x = self.fc1(x)
-        x = self.sigmoid(x)
+        x = self.conv(x)
+        x = self.embedding(x)
         return x
+
+class Encoder(nn.Module):
+    """
+    Encoder module for the VQ-VAE model.
+
+    The encoder consists of 2 strided convolutional layers with stride 2 and 
+    window size 4 × 4, followed by two residual 3 × 3 blocks (implemented as 
+    ReLU, 3x3 conv, ReLU, 1x1 conv), all having 256 hidden units.
+    """
+
+    def __init__(self, in_channels, n_hidden, n_residual):
+        super(Encoder, self).__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=n_hidden // 2,
+            kernel_size=4,
+            stride=2,
+            padding=1,
+        )
+
+        self.conv2 = nn.Conv2d(
+            in_channels=n_hidden // 2, 
+            out_channels=n_hidden, 
+            kernel_size=4, 
+            stride=2, 
+            padding=1
+        )
+
+        self.residual1 = None
+        self.residual2 = None
+
+        
+        self.relu1 = nn.ReLU()
+
+    def forward(self, out):
+        out = self.conv1(out)
+        out = self.relu1(out)
+        out = self.conv2(out)
+        out = self.residual1(out)
+        out = self.residual2(out)
+        return out
+    
+class Decoder(nn.Module):
+    """
+    Decoder
+    """
+    def __init__(self, in_channels, n_hidden, n_residual):
+        super(Decoder, self).__init__()
+
+        self.conv1 = nn.Conv2d(
+            in_channels=in_channels,
+            out_channels=n_hidden,
+            kernel_size=4,
+            stride=2,
+            padding=1,
+        )
+
+        self.residual1 = None
+        self.residual2 = None
+
+        self.transpose1 = nn.ConvTranspose2d(
+            in_channels=n_hidden,
+            out_channels=n_hidden // 2,
+            kernel_size=4,
+            stride=2,
+            padding=1,
+        )
+        
+        self.transpose2 = nn.ConvTranspose2d(
+            in_channels=n_hidden // 2,
+            out_channels=1,
+            kernel_size=4,
+            stride=2,
+            padding=1,
+        )
+
+    def forward(self, out):
+        out = self.conv1(out)
+        out = self.residual1(out)
+        out = self.residual2(out)
+        out = self.transpose1(out)
+        out = self.transpose2(out)
+        return out
+
 
