132d9b966a2965b9426653b6016b354ecc49926c
kappa123451 <kasper900322@gmail.com>
2023-10-10 21:10:09 +0800

    forgot push train

--- stable_diffusion.py
+++ stable_diffusion.py
@@ -7,69 +7,80 @@ import matplotlib.pyplot as plt
 from tqdm import tqdm
 from einops import rearrange, repeat, reduce
 from collections import defaultdict
+from torch.utils.data import TensorDataset, DataLoader
+from tqdm import tqdm
+
 # ==== import from this folder ==== #
-from model_VQVAE import VQVAE
-from discriminator import NLayerDiscriminator, weights_init
+from model_VAE import VQVAE
 from dataset import get_dataloader
 from util import reset_dir, weight_scheduler, compact_large_image
 from logger import Logger
+from latent_dataset import get_latent_set
+from model_diffusion import LatentDiffusionModel
+
 DEVICE = torch.device("cuda")
 print("DEVICE:", DEVICE)
 
-batch_size = 6
-dataloader = get_dataloader(mode='train_and_validate', batch_size=batch_size)
+mode = 'VQVAE'
+load_epoch = 45
 
-vae = VQVAE().to(DEVICE)
-vae = torch.load(f'model_ckpt/VQVAE/epoch_AE_50.pt')
+# Get latent set
+latent_set = torch.load(f'collected_latents/{mode}_{load_epoch}.pt')
+vae = torch.load(f'model_ckpt/{mode}/epoch_AE_{load_epoch}.pt')
 vae.eval()
-print()
 
-def get_latents(net, dataloader):
-    latents = []
-    for now_step, batch_data in tqdm(enumerate(dataloader), total=len(dataloader)):
-        raw_img, seg_img, brain_idx, z_idx = [
-            data.to(DEVICE) for data in batch_data]
-        with torch.no_grad():
-            batch_size = raw_img.shape[0]
-            latent = net.encode(raw_img)
-            latents.append(latent.detach())
-    latents = torch.cat(latents)
-    return latents
-
+# Try to normalize latents with conditions
 with torch.no_grad():
-    latents = get_latents(vae, dataloader).detach()
-from model_diffusion import LatentDiffusionModel
+    latents, z_indices = latent_set.tensors[0], latent_set.tensors[1]
+    normalized_latents = latents
+    latents_mean = torch.zeros([32, *latents.shape[1:]]).to(device=latents.device)
+    latents_std = torch.zeros_like(latents_mean)
+    for cur_z_idx in range(0, 32):
+        chosen = z_indices == cur_z_idx
+        from einops import reduce
+        latents_mean[cur_z_idx] = reduce(latents[chosen], 'n c h w -> 1 c h w', 'min')
+        latents_std[cur_z_idx] = reduce(latents[chosen], 'n c h w -> 1 c h w', 'max') - latents_mean[cur_z_idx]
+        normalized_latents[chosen] = (latents[chosen] - latents_mean[cur_z_idx]) / latents_std[cur_z_idx]
+    print(normalized_latents.shape)
+
+# Setup configs abou diffusion model
 net = LatentDiffusionModel(in_channels=8, ch=32).to(DEVICE)
-from torchinfo import summary
 criteria = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(net.parameters(), lr=0.01)
+optimizer = torch.optim.Adam(net.parameters(), lr=1e-4)
 
-from torch.utils.data import TensorDataset, DataLoader
-
-print("Latent shape:", latents.shape)
-from einops import reduce
-with torch.no_grad():
-    latents_mean = torch.mean(latents, dim=0, keepdim=True)
-    latents_std = torch.std(latents, dim=0, keepdim=True)
-    latents = (latents - latents_mean) / latents_std
-    latents = latents.detach()
-    print(latents_mean.shape, latents_std.shape)
-    # print(latents_mean, latents_std)
-dataset = TensorDataset(latents)
+# Get dataloader
+print("Latent shape:", normalized_latents.shape)
+dataset = TensorDataset(normalized_latents, z_indices)
 dataloader = DataLoader(dataset=dataset, batch_size=32, shuffle=True)
-from tqdm import tqdm
-# net = torch.load('stable_diffusion.pt')
-# net = torch.load('diffusion_MNIST.pt')
-for epochs in range(200):
+
+def test_epoch(epoch):
+    with torch.no_grad():
+        reset_dir(f'stable_diffusion_vis/epoch_{epoch}')
+        # We only sample 6 images (and each images contain 32 idx)
+        sample_n = 6
+        # Scaling
+        for cur_idx in range(sample_n):
+            cond = torch.arange(0, 32, device=DEVICE, dtype=torch.long)
+            sample_latent = net.sample_with_cond((1, 8, 16, 16), cond)
+            sample_latent = sample_latent * latents_std + latents_mean
+    
+            # quant, diff_loss, ind = vae.quantize(sample_latent)
+            sample_img = vae.decode(sample_latent, cond)
+            
+            sample_img = compact_large_image(sample_img.cpu(), HZ=4, WZ=8)
+            
+            plt.imsave(f'stable_diffusion_vis/epoch_{epoch}/{cur_idx}.png', sample_img[0] * 0.5 + 0.5, cmap='gray')
+
+for epochs in range(150):
     total_loss, total_len = 0, 0
-    for latent, in tqdm(dataloader, total=len(dataloader)):
+    for latent, zs in tqdm(dataloader, total=len(dataloader)):
         latent = latent.cuda()
         batch_size = latent.shape[0]
 
         optimizer.zero_grad()
         t = torch.randint(0, net.T, size=(batch_size, )).cuda()
         latent_noise, noise = net.get_noise(latent, t)
-        predicted_noise = net.forward(latent_noise, t)
+        predicted_noise = net.forward(latent_noise, t, zs)
 
         loss = torch.abs(noise.contiguous() - predicted_noise.contiguous())
         loss = torch.sum(loss) / batch_size
@@ -78,16 +89,8 @@ for epochs in range(200):
 
         total_loss += loss.item()
         total_len += 1
-    torch.save(net, 'stable_diffusion.pt')
+    if epochs % 10 == 0:
+        torch.save(net, f'model_ckpt/stable_diffusion/UNet_{epochs}.pt')
+        test_epoch(epochs)
     print(f"epoch {epochs:4d}, loss {total_loss / total_len:.5f}")
 
-with torch.no_grad():
-    sample_latent = net.sample((32, 8, 16, 16))
-    # Scaling
-    sample_latent = sample_latent * latents_std + latents_mean
- 
-    quant, diff_loss, (_, _, ind) = vae.quantize(sample_latent)
-    sample_img = vae.decode(quant)
-    
-    sample_img = compact_large_image(sample_img.cpu(), HZ=4, WZ=8)
-    plt.imsave('stable_diffusion.png', sample_img[0] * 0.5 + 0.5, cmap='gray')
