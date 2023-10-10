c326512c9b54fe52db57f0eda8c1f44b4197cf96
nleek0 <109025848+nleek0@users.noreply.github.com>
2023-10-10 15:39:50 +1000

    Initialised the model
    The model has been initialised in the Modules.py folder and the model has been made, but not yet tested
    The resizing
    The dataset now downscales the image by a factor of 4.

--- recognition/supres-network-47440599/dataset.py
+++ recognition/supres-network-47440599/dataset.py
@@ -8,21 +8,23 @@ import matplotlib.pyplot as plt
 def load_data():
     dataroot = "./data/AD_NC/train"
 
+    downscaling = 4
 
-    batch_size = 1
+    batch_size = 128
 
     workers = 2
 
     device = torch.device("cpu")
     dataset = dset.ImageFolder(root=dataroot,
                             transform=transforms.Compose([
-                                transforms.Resize(100),
+                                transforms.Resize((64,60)),
                                 transforms.ToTensor(),
                                 transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5)),
                             ]))
 
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
                                             shuffle=True, num_workers=workers)
+
 
     real_batch = next(iter(dataloader))
     plt.figure(figsize=(8,8))
@@ -33,4 +35,3 @@ def load_data():
 
 if __name__ == "__main__":
     load_data()
-    
