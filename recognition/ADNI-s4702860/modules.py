8051437e858c9c039ac7be5682d4edb730a2a673
lambdarm <141205527+lambdarm@users.noreply.github.com>
2023-10-10 16:34:49 +1000

    Updated modules.py
    Added layout for class Model

--- recognition/ADNI-s4702860/modules.py
+++ recognition/ADNI-s4702860/modules.py
@@ -1,1 +1,25 @@
+from tensorflow import keras
+from tensorflow.keras import layers
+from dataset import load_dataset
 
+"""
+Model class used to create a Siamese Neural Network
+"""
+class Model:
+    """
+    Initialises the model
+    """
+    def __init__(self):
+        pass
+
+    """
+    Creates the left 
+    """
+    def left_network(self):
+        pass
+
+    def right_network(self):
+        pass
+
+    def create_nodel(self):
+        pass
