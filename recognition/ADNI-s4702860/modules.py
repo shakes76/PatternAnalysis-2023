9f88386b4b1d24825ca9d6ad52020aea2530a8c8
lambdarm <141205527+lambdarm@users.noreply.github.com>
2023-10-10 22:05:16 +1000

    Updated modules.py
    Change class name to SiameseModel
    Added functionality to create_network class to create the model
    removed input shape and assumed base shape size
    updated commenting

--- recognition/ADNI-s4702860/modules.py
+++ recognition/ADNI-s4702860/modules.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D
+from tensorflow.keras.models import Sequential, Model
+from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Input
 
 """
 Model class used to create a Siamese Neural Network
@@ -8,25 +8,23 @@ Model class used to create a Siamese Neural Network
 Inspiration taken from 
 "https://github.com/schatty/siamese-networks-tf/blob/master/siamnet/models/siamese.py"
 """
-class Model:
+class SiameseModel:
     """
-    Initialises the model class
+    Initialises the model class by calling the create_network() function
     """
     def __init__(self):
-        pass
+        self.model = self.create_network()
     
     """
     Creates the base network shared by the Anchor, Positive and Negative inputs
-
-    Inputs:
-        input_shape - the size of the input shape
+    Assumes input shape is (256, 240, 3)
     
     Returns:
         base_model - the model to be used for the anchor, positive and negative input
     """
-    def base_network(self, input_shape):
-        base_model = Sequential() # Defines the base model
-        base_model.add(Conv2D(64, (3, 3), activation='relu', input_shape=input_shape))
+    def base_network(self):
+        base_model = Sequential()
+        base_model.add(Conv2D(64, (3, 3), activation='relu', input_shape=(256, 240, 3)))
         base_model.add(MaxPool2D())
         base_model.add(Conv2D(128, (3, 3), activation='relu'))
         base_model.add(MaxPool2D())
@@ -51,3 +49,29 @@ class Model:
         neg_dist = tf.reduce_sum(tf.square(anchor - negative), axis=-1)
         loss = pos_dist - neg_dist + margin
         return tf.maximum(0.0, loss)
+    
+    """
+    Class to create the siamese network. Called by  __init__ 
+
+    Outputs:
+        siamese_network - the siamese network model compilation
+    """
+    def create_network(self):
+        base_model = self.base_network()
+
+        anchor_input = Input(shape=(256, 240, 3))
+        positive_input = Input(shape=(256, 240, 3))
+        negative_input = Input(shape=(256, 240, 3))
+
+        anchor_layer = base_model(anchor_input)
+        positive_layer = base_model(positive_input)
+        negative_layer = base_model(negative_input)
+
+        # Create a dense layer for class prediction using Softmax activation.
+        prediction_layer = Dense(2, activation='softmax')(anchor_layer)
+    
+        siamese_network = Model(inputs=[anchor_input, positive_input, negative_input], outputs=prediction_layer)
+        siamese_network.compile(optimizer='adam', loss=self.triplet_loss, metrics=['accuracy'])
+
+        return siamese_network
+    
