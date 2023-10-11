ef3404d2100a70542bb4ba8c10b23ce1766ae32d
Abhinavmohindersingh <141258868+Abhinavmohindersingh@users.noreply.github.com>
2023-10-11 21:05:25 +1000

    Improved model by adding more layers and introducing BatchNormalization after each layer

--- Brain_MRI_SuperResolution/models/sub_pixel_cnn.py
+++ Brain_MRI_SuperResolution/models/sub_pixel_cnn.py
@@ -1,15 +1,32 @@
-from keras.models import Model
-from keras.layers import Input, Conv2D
-import tensorflow as tf;
-from torchvision.transforms import Lambda
+from tensorflow.keras.models import Model
+from tensorflow.keras.layers import Input, Conv2D, Lambda, BatchNormalization, Add
 
 
-def sub_pixel_cnn(scale_factor=4, channels=1):
-    inputs = Input(shape=(None, None, channels))
+def sub_pixel_cnn(input_shape):
+    inputs = Input(shape=input_shape)
 
-    # Add layers according to Efficient Sub-Pixel CNN
-    net = Conv2D(filters=channels * (scale_factor ** 2), kernel_size=3, padding='same')(inputs)
-    # Use a depth_to_space function (or similar) to shuffle and upscale.
-    outputs = Lambda(lambda x: tf.nn.depth_to_space(x, scale_factor))(net)
+    x = Conv2D(64, (5, 5), activation='relu', padding='same')(inputs)
+    x = BatchNormalization()(x)
+    x1 = Conv2D(64, (3, 3), activation='relu', padding='same')(x)
+    x1 = BatchNormalization()(x1)
+
+    # Introducing skip connection
+    x2 = Add()([x, x1])
+    x2 = Conv2D(32, (3, 3), activation='relu', padding='same')(x2)
+    x2 = BatchNormalization()(x2)
+
+    # Another layer
+    x3 = Conv2D(32, (3, 3), activation='relu', padding='same')(x2)
+    x3 = BatchNormalization()(x3)
+
+    x4 = Add()([x2, x3])
+    x4 = Conv2D(4, (3, 3), activation='relu', padding='same')(x4)
+    x4 = BatchNormalization()(x4)
+
+    def subpixel(x):
+        import tensorflow as tf
+        return tf.nn.depth_to_space(x, 2)
+
+    outputs = Lambda(subpixel)(x4)
 
     return Model(inputs=inputs, outputs=outputs)
