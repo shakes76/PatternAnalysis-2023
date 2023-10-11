3e67f36425268f20f0e9078923cf6240302fca4d
Abhinavmohindersingh <141258868+Abhinavmohindersingh@users.noreply.github.com>
2023-10-11 21:27:07 +1000

    Refactored it with functionality to save and add seperate folder for the downsampled images

--- Brain_MRI_SuperResolution/AD_NC/preprocess.py
+++ Brain_MRI_SuperResolution/AD_NC/preprocess.py
@@ -1,26 +1,27 @@
 import os
 from PIL import Image
 
-
-def downsample_images(input_folder, factor=4):
+def downsample_images(input_folder, output_folder, factor=4):
     print(f"Processing images in folder: {input_folder}")
+    if not os.path.exists(output_folder):
+        os.makedirs(output_folder)
 
-    # Counters for the total number of files and the number of files processed
     total_files = 0
     processed_files = 0
 
     for root, dirs, files in os.walk(input_folder):
         total_files += len(files)
         for file in files:
-            if file.endswith('.png'):
+            if file.endswith('.jpeg'):
                 img_path = os.path.join(root, file)
                 print(f"Processing image: {img_path}")
 
                 img = Image.open(img_path)
-
-                # Downsample the image
                 img_resized = img.resize((img.width // factor, img.height // factor), Image.ANTIALIAS)
-                img_resized.save(img_path)
+
+                # Save to the new directory
+                save_path = os.path.join(output_folder, file)
+                img_resized.save(save_path)
 
                 processed_files += 1
                 print(f"Processed {processed_files} out of {total_files}")
@@ -29,5 +30,5 @@ def downsample_images(input_folder, factor=4):
 
 
 if __name__ == "__main__":
-    downsample_images("data/ad_nc/train")
-    downsample_images("data/ad_nc/test")
+    downsample_images("train", "train_downsampled")
+    downsample_images("test", "test_downsampled")
