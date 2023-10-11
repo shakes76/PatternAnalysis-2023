fbe7c44e07c34f5da7004ad92965fa50b3e37ee6
kappa123451 <kasper900322@gmail.com>
2023-10-12 07:50:33 +0800

    Add title of subplot and fix to final version

--- recognition/46278254_stable_diffusion/logger.py
+++ recognition/46278254_stable_diffusion/logger.py
@@ -1,8 +1,7 @@
-from util import reset_dir
-import matplotlib.pyplot as plt
-import pickle
-import numpy as np
 import os
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 class Logger:
 
@@ -28,7 +27,6 @@ class Logger:
         if save:
             with open(self.file_name, "a") as fout:
                 print(f'{variable_name}|{float(variable)}', file=fout)
-            
 
     def update_dict(self, data_dict, save=True) -> None:
         # Update all vaiables then auto-save.
@@ -42,18 +40,23 @@ class Logger:
 
     def load(self, file_name) -> None:
         # Clear the dict
+        self.file_name = f"log/{file_name}"
         self.data = {}
         with open(self.file_name) as fin:
             for row in fin:
+                # The key,value pair is splited with '|'
                 variable_name, variable = row.split('|')
                 variable = float(variable)
                 self.update_value(variable_name, variable, save=False)
 
     @staticmethod
     def smooth_num(X, window=5):
+        # This function will smooth the line with a specific width window.
+        # And this will help to generate more comfortable lines.
         Y = []
         cur = 0
         for i in range(len(X)):
+            # If i is out of window, forget the value.
             if i >= window:
                 cur -= X[i - window]
             cur += X[i]
@@ -62,47 +65,62 @@ class Logger:
 
     def show_plot(self, start=None, smooth_window=5, xlabel='iteration', ylabel='loss'):
         L = len(self.data['recon_loss'])
+        # We only plot the line after 50 iterations.
         if start is None:
             start = min(L//2, 50)
 
         X = np.arange(L)[start:]
 
-        # mapping function
-        m_f = lambda v: (X[-len(v[start:]):], self.smooth_num(v, window=smooth_window)[start:])
+        # mapping function from numbers to smooth numbers
+        def m_f(v): return (X[-len(v[start:]):],
+                            self.smooth_num(v, window=smooth_window)[start:])
 
+        def my_plot(key, label, c):
+            if key in self.data:
+                plt.plot(*m_f(self.data[key]), label=label, c=c)
 
+        # Reconstruction Loss Plot
         plt.subplot(2, 2, 1)
-        plt.plot(*m_f(self.data['recon_loss']), label='Reconstruction Loss', c='blue')
+        my_plot('recon_loss', 'Reconstruction Loss', 'blue')
+        plt.title("Reconstruction Loss")
         plt.legend()
 
+        # Regularization Loss Plot
         plt.subplot(2, 2, 2)
-        plt.plot(*m_f(self.data['diff_loss']), label='regularization Loss', c='orange')
+        my_plot('diff_loss', 'Regularization Loss', 'orange')
+        plt.title("Regularization Loss")
         plt.legend()
 
+        # GAN Loss Plot
         plt.subplot(2, 2, 3)
-        plt.plot(*m_f(self.data['fake_recon_loss']), label='Reconstruction D Loss', c='blue')
-        plt.plot(*m_f(self.data['fake_sample_loss']), label='Sample D Loss', c='red')
-        plt.plot(*m_f(self.data['discriminator_loss']), label='Discriminator Toatal Loss', c='purple')
-        plt.title("Discriminator Loss")
+        my_plot('fake_recon_loss', 'Reconstruction Score', 'blue')
+        my_plot('fake_sample_loss', 'Random Sample Score', 'red')
+        my_plot('discriminator_loss', 'Discriminator Toatal Loss', 'purple')
+        plt.title("GAN part Loss")
         plt.legend()
 
+        # Weight Plot
         plt.subplot(2, 2, 4)
-        # plt.plot(*m_f(self.data['w_kld']), label='KLD loss weight', c='orange')
-        plt.plot(*m_f(self.data['w_recon']), label='Recon weight', c='blue')
-        plt.plot(*m_f(self.data['w_dis']), label='Discriminator(for Recon) weight', c='blue')
-        # plt.plot(*m_f(self.data['w_sample']), label='Discriminator(Sample) weight', c='red')
+        # Well, w_recon seems to be alwaye 1.
+        # my_plot('w_recon', 'Reconstruction weight', 'blue')
+        my_plot('w_kld', 'Regularization weight', 'orange')
+        my_plot('w_dis', 'Reconstruction GAN weight', 'blue')
+        my_plot('w_sample', 'Sample GAN weight', 'red')
         plt.legend()
         plt.title("Weight for Each Loss")
 
         plt.show()
 
     def get_range_status(self, start=0, end=None):
+        # This function is to cut the array.
+        # And the reason why this function exist is to cut the initial loss.
+        # The value of it is too high and harm the qulity of plot.
         ret = {}
         for k, v in self.data.items():
             target = v[start:end]
             ret[k] = sum(target) / len(target)
         return ret
-        
+
 
 if __name__ == '__main__':
     logger = Logger(reset=False, file_name='VAE_log.txt')
