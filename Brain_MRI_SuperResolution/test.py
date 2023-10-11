5ba370bf31fa1ddb1f36c4c4898d9e878ecd6a47
Abhinavmohindersingh <141258868+Abhinavmohindersingh@users.noreply.github.com>
2023-10-11 21:54:07 +1000

    Improving model with distributing data between validation/testing equally

--- Brain_MRI_SuperResolution/test.py
+++ Brain_MRI_SuperResolution/test.py
@@ -1,12 +1,50 @@
-from tensorflow.keras.models import load_model
-import cv2
 import numpy as np
+import cv2
+from tensorflow.keras.models import load_model
+from models import sub_pixel_cnn
 
-model = load_model('saved_models/sub_pixel_cnn_model.h5')
-print(1);
-test_image = cv2.imread('path_to_a_downsampled_test_image', cv2.IMREAD_GRAYSCALE)
-print(1);
-predicted = model.predict(np.expand_dims(np.expand_dims(test_image, axis=-1), axis=0))
-print(1);
-cv2.imwrite('super_resolved_image.png', predicted[0].squeeze())  # Squeeze to remove channel dimension
-print(1);
+print("[DEBUG] Starting the program.")
+
+test_image_path = 'AD_NC/test/AD/388206_84.jpeg'
+print(f"[DEBUG] Loading image from path: {test_image_path}")
+test_image = cv2.imread(test_image_path, cv2.IMREAD_GRAYSCALE)
+
+if test_image is None:
+    print("Error loading the image!")
+    exit()
+
+print("[DEBUG] Image loaded successfully.")
+print(f"[DEBUG] Image shape: {test_image.shape}")
+
+model_path = 'saved_models/sub_pixel_cnn_model.h5'
+print(f"[DEBUG] Loading model from path: {model_path}")
+model = load_model(model_path)
+print("[DEBUG] Model loaded successfully.")
+
+# Resize the image to the expected input size for the model
+print("[DEBUG] Resizing the image for model input.")
+test_image_downsampled = cv2.resize(test_image, (64, 60), interpolation=cv2.INTER_CUBIC)
+print(f"[DEBUG] Resized image shape: {test_image_downsampled.shape}")
+
+input_data = np.expand_dims(np.expand_dims(test_image_downsampled, axis=-1), axis=0)
+print("[DEBUG] Preparing image data for prediction.")
+print(f"[DEBUG] Input data shape: {input_data.shape}")
+
+print("[DEBUG] Running the prediction.")
+predicted_image = model.predict(input_data)
+print("[DEBUG] Prediction completed.")
+print(f"[DEBUG] Predicted image shape: {predicted_image[0].shape}")
+
+# Rescale values from 0-1 to 0-255 and convert to uint8 type for displaying/saving with OpenCV
+predicted_image_rescaled = (predicted_image[0] * 255).astype(np.uint8)
+print("[DEBUG] Rescaled predicted image for visualization.")
+
+# Display the image
+print("[DEBUG] Displaying the predicted image.")
+cv2.imshow('Predicted Image', predicted_image_rescaled)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+# Optionally, save the image
+# cv2.imwrite('path_to_save_predicted_image.png', predicted_image_rescaled)
+print("[DEBUG] Program finished successfully.")
--- Brain_MRI_SuperResolution/train.py
+++ Brain_MRI_SuperResolution/train.py
@@ -1,45 +1,57 @@
 import numpy as np
 import cv2
+from tensorflow.keras.optimizers.legacy import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint, EarlyStopping
+
 from AD_NC.dataset import load_dataset, downsample_images
 from models.sub_pixel_cnn import sub_pixel_cnn
-from tensorflow.keras.optimizers import Adam
+
 
 def resize_images(images, target_dim):
     return [cv2.resize(image, target_dim, interpolation=cv2.INTER_CUBIC) for image in images]
 
-# Load datasets
-originals = load_dataset('AD_NC/train/AD') + load_dataset('AD_NC/train/NC')
-downsampled = downsample_images(originals)
 
-print("Original images count:", len(originals))
-print("Sample original image shape:", originals[0].shape)
-print("Downsampled images count:", len(downsampled))
-print("Sample downsampled image shape:", downsampled[0].shape)
+def prepare_data():
+    originals = load_dataset('AD_NC/train/AD') + load_dataset('AD_NC/train/NC')
+    downsampled = downsample_images(originals)
 
-# Resize originals to match the expected model output size
-originals_resized = resize_images(originals, (120, 128))
-print("Resized originals count:", len(originals_resized))
-print("Sample resized original image shape:", originals_resized[0].shape)
+    print("Original images count:", len(originals))
+    print("Sample original image shape:", originals[0].shape)
+    print("Downsampled images count:", len(downsampled))
+    print("Sample downsampled image shape:", downsampled[0].shape)
 
-# Model
-input_shape = downsampled[0].shape + (1,)
-model = sub_pixel_cnn(input_shape)
-print("Model input shape:", model.input_shape)
-print("Model output shape:", model.output_shape)
+    originals_resized = resize_images(originals, (128, 120))
+    print("Resized originals count:", len(originals_resized))
+    print("Sample resized original image shape:", originals_resized[0].shape)
 
-# Optimizer and compilation
-optimizer = Adam(lr=0.001)
-model.compile(optimizer=optimizer, loss='mean_squared_error')
+    X_train = np.array(downsampled).astype(np.float32) / 255.0  # Normalizing to [0,1]
+    Y_train = np.array(originals_resized).astype(np.float32) / 255.0  # Normalizing to [0,1]
 
-# Training data preparation
-X_train = np.array(downsampled).reshape(-1, *input_shape)
-Y_train = np.array(originals_resized).reshape(-1, 120, 128, 1)
+    return X_train, Y_train
 
-print("Training data shape:", X_train.shape)
-print("Target data shape:", Y_train.shape)
 
-# Training
-model.fit(X_train, Y_train, epochs=50, batch_size=16)
+def train_model(X_train, Y_train):
+    input_shape = X_train[0].shape + (1,)  # Adding the channel dimension
+    model = sub_pixel_cnn(input_shape)
+    print("Model input shape:", model.input_shape)
+    print("Model output shape:", model.output_shape)
 
-# Save model
-model.save('saved_models/sub_pixel_cnn_model.h5')
+    optimizer = Adam(learning_rate=0.001)
+    model.compile(optimizer=optimizer, loss='mean_squared_error')
+    checkpoint = ModelCheckpoint('saved_models/sub_pixel_cnn_best_model.h5', save_best_only=True, monitor='val_loss',
+                                 mode='min')
+
+    early_stopping = EarlyStopping(monitor='val_loss', patience=5, restore_best_weights=True)
+
+    print("Training data shape:", X_train.shape)
+    print("Target data shape:", Y_train.shape)
+
+    model.fit(X_train, Y_train, validation_split=0.1, epochs=50, batch_size=16, callbacks=[checkpoint, early_stopping])
+
+    # Save the final model
+    model.save('saved_models/sub_pixel_cnn_model.keras')
+
+
+if __name__ == "__main__":
+    X_train, Y_train = prepare_data()
+    train_model(X_train, Y_train)
