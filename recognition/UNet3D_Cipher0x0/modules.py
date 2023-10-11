3291cb72f5cb8c4fb3bcabee35c4b6a1a0b026c6
Cipher <57138168+Cipher0x0@users.noreply.github.com>
2023-10-11 20:53:42 +1000

    Restructure the network

--- recognition/UNet3D_Cipher0x0/modules.py
+++ recognition/UNet3D_Cipher0x0/modules.py
@@ -3,129 +3,83 @@ import torch.nn as nn
 import torch.nn.functional as F
 
 
-class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, width_multiplier=1, trilinear=True, use_ds_conv=False):
-        """A simple 3D Unet, adapted from a 2D Unet from https://github.com/milesial/Pytorch-UNet/tree/master/unet
-        Arguments:
-          n_channels = number of input channels; 3 for RGB, 1 for grayscale input
-          n_classes = number of output channels/classes
-          width_multiplier = how much 'wider' your UNet should be compared with a standard UNet
-                  default is 1;, meaning 32 -> 64 -> 128 -> 256 -> 512 -> 256 -> 128 -> 64 -> 32
-                  higher values increase the number of kernels pay layer, by that factor
-          trilinear = use trilinear interpolation to upsample; if false, 3D convtranspose layers will be used instead
-          use_ds_conv = if True, we use depthwise-separable convolutional layers. in my experience, this is of little help. This
-                  appears to be because with 3D data, the vast vast majority of GPU RAM is the input data/labels, not the params, so little
-                  VRAM is saved by using ds_conv, and yet performance suffers."""
-        super(UNet, self).__init__()
-        _channels = (32, 64, 128, 256, 512)
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.channels = [int(c*width_multiplier) for c in _channels]
-        self.trilinear = trilinear
-        self.convtype = DepthwiseSeparableConv3d if use_ds_conv else nn.Conv3d
-
-        self.inc = DoubleConv(n_channels, self.channels[0], conv_type=self.convtype)
-        self.down1 = Down(self.channels[0], self.channels[1], conv_type=self.convtype)
-        self.down2 = Down(self.channels[1], self.channels[2], conv_type=self.convtype)
-        self.down3 = Down(self.channels[2], self.channels[3], conv_type=self.convtype)
-        factor = 2 if trilinear else 1
-        self.down4 = Down(self.channels[3], self.channels[4] // factor, conv_type=self.convtype)
-        self.up1 = Up(self.channels[4], self.channels[3] // factor, trilinear)
-        self.up2 = Up(self.channels[3], self.channels[2] // factor, trilinear)
-        self.up3 = Up(self.channels[2], self.channels[1] // factor, trilinear)
-        self.up4 = Up(self.channels[1], self.channels[0], trilinear)
-        self.outc = OutConv(self.channels[0], n_classes)
-
-    def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        logits = self.outc(x)
-        return logits
-
-
-class DoubleConv(nn.Module):
-    """(convolution => [BN] => ReLU) * 2"""
-
-    def __init__(self, in_channels, out_channels, conv_type=nn.Conv3d, mid_channels=None):
+class UNet3D(nn.Module):
+    def __init__(self):
         super().__init__()
-        if not mid_channels:
-            mid_channels = out_channels
-        self.double_conv = nn.Sequential(
-            conv_type(in_channels, mid_channels, kernel_size=3, padding=1),
-            nn.BatchNorm3d(mid_channels),
-            nn.ReLU(inplace=True),
-            conv_type(mid_channels, out_channels, kernel_size=3, padding=1),
-            nn.BatchNorm3d(out_channels),
-            nn.ReLU(inplace=True)
+        self.layer1 = nn.Sequential(
+            nn.Conv3d(1, 4, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv3d(4, 16, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv3d(16, 16, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool3d(kernel_size=2),
+            nn.ReLU(),
+            nn.BatchNorm3d(16))
+
+        self.layer2 = nn.Sequential(
+            nn.Conv3d(16, 32, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv3d(32, 64, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool3d(kernel_size=2),
+            nn.ReLU(),
+            nn.BatchNorm3d(64))
+
+        self.layer3 = nn.Sequential(
+            nn.Conv3d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv3d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool3d(kernel_size=2),
+            nn.ReLU(),
+            nn.BatchNorm3d(64))
+
+        self.layer4 = nn.Sequential(
+            nn.Conv3d(64, 64, kernel_size=3, stride=1, padding=1),
+            nn.ReLU(),
+            nn.Conv3d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.MaxPool3d(kernel_size=2),
+            nn.ReLU(),
+            nn.BatchNorm3d(128))
+
+        self.decode3 = Decoder(128, 64 + 64, 64)
+        self.decode2 = Decoder(64, 64 + 64, 64)
+        self.decode1 = Decoder(64, 16 + 16, 16)
+        self.decode0 = nn.Sequential(
+            nn.ConvTranspose3d(16, 16, kernel_size=2, stride=2),
+            nn.Conv3d(16, 16, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv3d(16, 16, kernel_size=3, padding=1),
+            nn.ReLU()
         )
+        self.conv_last = nn.Conv3d(16, 6, 1)
 
-    def forward(self, x):
-        return self.double_conv(x)
+    def forward(self, input):
+        # size in (Batch_size, channel, height, width, depth)
+        e1 = self.layer1(input)  # 16,64,128,128
+        e2 = self.layer2(e1)  # 64,32,64,64
+        e3 = self.layer3(e2)  # 64,16,32,32
+        e4 = self.layer4(e3)  # 128,8,16,16
+
+        d3 = self.decode3(e4, e3)  # 64,16,32,32
+        d2 = self.decode2(d3, e2)  # 64,32,64,64
+        d1 = self.decode1(d2, e1)  # 16,64,128,128
+        d0 = self.decode0(d1)  # 16,128,256,256
+        out = self.conv_last(d0)  # 6,128,256,256
+
+        return out
 
 
-class Down(nn.Module):
-    """Downscaling with maxpool then double conv"""
-
-    def __init__(self, in_channels, out_channels, conv_type=nn.Conv3d):
-        super().__init__()
-        self.maxpool_conv = nn.Sequential(
-            nn.MaxPool3d(2),
-            DoubleConv(in_channels, out_channels, conv_type=conv_type)
+class Decoder(nn.Module):
+    def __init__(self, in_channels, middle_channels, out_channels):
+        super(Decoder, self).__init__()
+        self.up = nn.ConvTranspose3d(in_channels, out_channels, kernel_size=2, stride=2)
+        self.conv_relu = nn.Sequential(
+            nn.Conv3d(middle_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.BatchNorm3d(out_channels)
         )
-
-    def forward(self, x):
-        return self.maxpool_conv(x)
-
-
-class Up(nn.Module):
-    """Upscaling then double conv"""
-
-    def __init__(self, in_channels, out_channels, trilinear=True):
-        super().__init__()
-
-        # if trilinear, use the normal convolutions to reduce the number of channels
-        if trilinear:
-            self.up = nn.Upsample(scale_factor=2, mode='trilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, mid_channels=in_channels // 2)
-        else:
-            self.up = nn.ConvTranspose3d(in_channels, in_channels // 2, kernel_size=2, stride=2)
-            self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1, x2):
         x1 = self.up(x1)
-        # input is CHW
-        diffY = x2.size()[2] - x1.size()[2]
-        diffX = x2.size()[3] - x1.size()[3]
-
-        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
-                        diffY // 2, diffY - diffY // 2])
-        x = torch.cat([x2, x1], dim=1)
-        return self.conv(x)
-
-
-class OutConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(OutConv, self).__init__()
-        self.conv = nn.Conv3d(in_channels, out_channels, kernel_size=1)
-
-    def forward(self, x):
-        return self.conv(x)
-
-
-class DepthwiseSeparableConv3d(nn.Module):
-    def __init__(self, nin, nout, kernel_size, padding, kernels_per_layer=1):
-        super(DepthwiseSeparableConv3d, self).__init__()
-        self.depthwise = nn.Conv3d(nin, nin * kernels_per_layer, kernel_size=kernel_size, padding=padding, groups=nin)
-        self.pointwise = nn.Conv3d(nin * kernels_per_layer, nout, kernel_size=1)
-
-    def forward(self, x):
-        out = self.depthwise(x)
-        out = self.pointwise(out)
-        return out
+        x1 = torch.cat((x1, x2), dim=1)  # add shortcut layer and input layer together
+        x1 = self.conv_relu(x1)
+        return x1
