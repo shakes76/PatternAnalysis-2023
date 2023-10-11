7468524afa4a0469e47e3549cf9701d870a38d5b
Benjamin Guy <b.guy@uq.net.au>
2023-10-11 16:54:12 +1000

    Added utils.py to temporarily store code used for testing and printing. In this case, the code used to view downsampled images and the original images from datasets.py was moved to utils.py. It is expected that utils.py will be used for debugging and testing various parts of each file but for now it is storing code. Added train and validation loops in train.py. This trains the model on the train data and validates it on the validation data.

--- recognition/46972990 - MRI super-resolution network/dataset.py
+++ recognition/46972990 - MRI super-resolution network/dataset.py
@@ -30,37 +30,3 @@ def get_test_loader():
     dataset = SuperResolutionDataset(TEST_PATH)
     data_loader = DataLoader(dataset, batch_size=BATCH_SIZE, shuffle=False)
     return data_loader
-
-# Get the train and test loaders
-train_loader, validation_loader = get_train_and_validation_loaders()
-test_loader = get_test_loader()
-
-# Extract a single batch from the train loader
-data_iter = iter(train_loader)
-downscaled_images, original_images = next(data_iter)
-
-print(original_images[0].shape)
-
-# Convert the images from tensor format back to PIL for display
-def tensor_to_PIL(tensor):
-    tensor = (tensor + 1) / 2.0
-    tensor = tensor.squeeze(0)
-    return transforms.ToPILImage()(tensor)
-
-# Plot the images (plot 5 to ensure it is working)
-num_images_to_display = 5
-
-for i in range(num_images_to_display):
-    plt.figure(figsize=(10,5))
-    
-    # Display downscaled image
-    plt.subplot(1, 2, 1)
-    plt.imshow(tensor_to_PIL(downscaled_images[i]))
-    plt.title("Downscaled Image")
-    
-    # Display original image
-    plt.subplot(1, 2, 2)
-    plt.imshow(tensor_to_PIL(original_images[i]))
-    plt.title("Original Image")
-    
-    plt.show()
--- recognition/46972990 - MRI super-resolution network/train.py
+++ recognition/46972990 - MRI super-resolution network/train.py
@@ -2,11 +2,61 @@ import torch
 import torch.nn as nn
 import torch.optim as optim
 from modules import ESPCN
+from dataset import get_train_and_validation_loaders
+import time
 
-# Create the model and load the data
+# Create the model and load training data
 model = ESPCN(upscale_factor=4, channels=1)
+train_loader, validation_loader = get_train_and_validation_loaders()
 
+# Move the model onto the GPU
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print(f"Device: {device}")
+model.to(device)
+
+# Set training parameters
 criterion = nn.MSELoss()
 optimizer = optim.Adam(model.parameters(), lr=0.001)
+num_epochs = 10
 
-num_epochs = 20
+print("Started training...")
+for epoch in range(num_epochs):
+    start_time = time.time()
+
+    # Training Loop
+    model.train()
+    running_loss = 0.0
+    for i, data in enumerate(train_loader, 0):
+        inputs, labels = data
+        inputs, labels = inputs.to(device), labels.to(device)
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        running_loss += loss.item()
+
+    train_loss = running_loss / len(train_loader)
+    print(f"Epoch {epoch+1}/{num_epochs}, Training Loss: {train_loss:.4f}", end=" - ")
+    
+    # Validation Loop
+    model.eval()  # set the model to evaluation mode
+    with torch.no_grad():
+        val_loss = 0.0
+        for i, data in enumerate(validation_loader, 0):
+            inputs, labels = data
+            inputs, labels = inputs.to(device), labels.to(device)
+            outputs = model(inputs)
+            loss = criterion(outputs, labels)
+            val_loss += loss.item()
+    val_loss = val_loss / len(validation_loader)
+    print(f"Validation Loss: {val_loss:.4f}", end=" - ")
+
+    end_time = time.time()  # End time of epoch
+    epoch_duration = end_time - start_time
+    print(f"Completed in {epoch_duration:.2f} seconds.")
+
+print("Finished training.")
+
+# Save the final model
+torch.save(model.state_dict(), 'final_model.pth')
