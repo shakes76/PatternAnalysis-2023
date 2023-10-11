3e51a2bbb252b0026b8fa37703b6f252c3aa76c2
Benjamin Guy <b.guy@uq.net.au>
2023-10-11 16:07:18 +1000

    Added a PyTorch implementation of the ESPCN model

--- recognition/46972990 - MRI super-resolution network/modules.py
+++ recognition/46972990 - MRI super-resolution network/modules.py
@@ -47,3 +47,23 @@ class SuperResolutionDataset(Dataset):
     def __getitem__(self, idx):
         orig_img, _ = self.data[idx]
         return self.down_transform(orig_img), self.orig_transform(orig_img)
+    
+class ESPCN(nn.Module):
+    """
+    Implementation adapted to PyTorch from Tensorflow from https://keras.io/examples/vision/super_resolution_sub_pixel/
+    """
+    def __init__(self, upscale_factor=4, channels=1):
+        super(ESPCN, self).__init__()
+        self.conv1 = nn.Conv2d(channels, 64, kernel_size=5, padding=2, padding_mode='reflect')
+        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1, padding_mode='reflect')
+        self.conv3 = nn.Conv2d(64, 32, kernel_size=3, padding=1, padding_mode='reflect')
+        self.conv4 = nn.Conv2d(32, channels * (upscale_factor ** 2), kernel_size=3, padding=1, padding_mode='reflect')
+        self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
+        self.relu = nn.ReLU()
+        
+    def forward(self, x):
+        x = self.relu(self.conv1(x))
+        x = self.relu(self.conv2(x))
+        x = self.relu(self.conv3(x))
+        x = self.pixel_shuffle(self.conv4(x))
+        return x
