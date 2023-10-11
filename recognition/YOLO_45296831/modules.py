fe367b408ab27529443c74dcf0f79ff6829df7dd
Riley Clark <rileyjclark14@gmail.com>
2023-10-11 11:57:12 +1000

    Added getter and predict functions to modules

---
+++ recognition/YOLO_45296831/modules.py
@@ -0,0 +1,19 @@
+import torch
+from ultralytics import YOLO
+
+def get_device():
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    if not torch.cuda.is_available():
+        print("Warning CUDA not Found. Using CPU")
+    return device
+
+def get_yolo(device, pretrained=True):
+    if pretrained:
+        model = YOLO("yolov8n.pt")
+    else:
+        model = YOLO("yolov8n.yaml")
+    model.to(device)
+    return model
+
+def detect_image(img, model):
+    model.predict(img)
