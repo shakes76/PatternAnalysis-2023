9d6002900c3d3d1c93c2021e8ca4f54b063b7056
Man Lik Nicholas Wong <s4668403@uqconnect.edu.au>
2023-10-12 07:10:11 +1000

    added predicition and test accuracy code

--- recognition/46684033-Siamese network/predict.py
+++ recognition/46684033-Siamese network/predict.py
@@ -1,7 +1,11 @@
 #predict.py
 import torch
 import dataset
-
+import torch.nn.functional as F
+import torchvision
+import torchvision.transforms as transforms
+import random
+import dataset
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if not torch.cuda.is_available():
     print("Warning VUDA not Found. Using CPU")
@@ -9,25 +13,43 @@ if not torch.cuda.is_available():
 train_path = r"C:/Users/wongm/Downloads/ADNI_AD_NC_2D/AD_NC/train"
 test_path = r"C:/Users/wongm/Downloads/ADNI_AD_NC_2D/AD_NC/test"
 
-model = torch.load(r"C:/Users/wongm/Desktop/COMP3710/project/siamesev2.pth")
+model = torch.load(r"C:/Users/wongm/Desktop/COMP3710/project/siamese_epcoh60.pth")
 model.eval()
 
-train_loader, test_loader = dataset.load_data2(train_path, test_path)
 
+
+transform = transforms.Compose([
+    # transforms.Resize((128, 128)),
+    transforms.ToTensor(),
+
+])
+train_path = r"C:/Users/wongm/Downloads/ADNI_AD_NC_2D/AD_NC/train"
+test_path = r"C:/Users/wongm/Downloads/ADNI_AD_NC_2D/AD_NC/test"
+trainset = torchvision.datasets.ImageFolder(root=train_path, transform=transform)
+testset = torchvision.datasets.ImageFolder(root=test_path, transform=transform)
+paired_testset = dataset.SiameseDatset_test(testset)
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+test_loader = torch.utils.data.DataLoader(paired_testset, batch_size=64,shuffle=True)
 correct = 0
 total = 0
 
-for trial in range(5):
-    with torch.no_grad():
-        for i, ((images1,images2), labels) in enumerate(test_loader):
-            images1 = images1.to(device)
-            images2 = images2.to(device)
-            labels = labels.to(device)
-            output = model(images1,images2).squeeze()
+with torch.no_grad():
+    for i, (test_images,pos_images,neg_images,test_labels) in enumerate(test_loader):
+        test_images = test_images.to(device)
+        pos_images = pos_images.to(device)
+        neg_images = neg_images.to(device)
+        test_labels = test_labels.to(device)
 
 
-            pred = torch.where(output > 0.5, 1, 0)
-            correct += (pred == labels).sum().item()
-            total += labels.size(0)
+        x1,y1 = model(test_images,pos_images)
+        # accuracy test
+        pos_distances = (y1 - x1).pow(2).sum(1)
 
-            print(f"test accuracy {100*correct/total}")
+        x2,y2 = model(test_images,neg_images)
+        neg_distances = (y2 - x2).pow(2).sum(1)
+
+        pred = torch.where(pos_distances < neg_distances, 1, 0)
+        correct += (pred == 1).sum().item()
+        total += test_labels.size(0)
+        print(f"progress [{i}/{len(test_loader)}]")
+    print(f"test accuracy {100*correct/total}%")
