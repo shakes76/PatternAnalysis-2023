bfa7947e035e8fadc5e0cfaa3034d62567adfe17
Caleb <calebsoonwm@gmail.com>
2023-10-11 10:19:58 +1000

    changed dataset.py to load origina + downsampled samples

--- recognition/superres_46501635/dataset.py
+++ recognition/superres_46501635/dataset.py
@@ -1,20 +1,23 @@
 import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
-from torchvision.transforms import Compose, Resize, ToTensor
 from PIL import Image
 import os
 
 class ADNIDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
+    def __init__(self, root_dir, downsample_factor=4, transform=None):
         """
         Args:
             root_dir (string): Directory with all the images.
+            downsample_factor (int): Factor by which the image will be downsampled.
             transform (callable, optional): Optional transform to be applied on a sample.
         """
         self.root_dir = root_dir
         self.transform = transform
-        self.image_files = [os.path.join(dp, f) for dp, dn, filenames in os.walk(root_dir) for f in filenames if f.endswith('.jpeg')]  # Assuming images are in .jpeg format
+        self.downsample_factor = downsample_factor
+        self.image_files = []
+        for subfolder in ['AD', 'NC']:
+            self.image_files.extend([os.path.join(dp, f) for dp, dn, filenames in os.walk(os.path.join(root_dir, subfolder)) for f in filenames if f.endswith('.jpeg')])
 
     def __len__(self):
         return len(self.image_files)
@@ -23,39 +26,32 @@ class ADNIDataset(Dataset):
         img_name = self.image_files[idx]
         image = Image.open(img_name)
 
+        # Downsample the image
+        downsampled_image = image.resize((image.width // self.downsample_factor, image.height // self.downsample_factor), Image.BICUBIC)
+
+        # Apply transforms if any
         if self.transform:
             image = self.transform(image)
+            downsampled_image = self.transform(downsampled_image)
 
-        return image
+        return downsampled_image, image
 
-def downsample_transform(factor=4):
+def image_transform():
     return transforms.Compose([
-        Resize((256 // factor, 240 // factor)),  # Assuming images are 256x240.
-        transforms.ToTensor(),
-        transforms.Normalize((0.5,), (0.5,))  # Assuming Min-Max normalization to [0,1]. Adjust if using Z-score normalization.
-    ])
-
-def original_transform():
-    return transforms.Compose([
-        Resize((256, 240)),  # Resize to a standard size
         transforms.ToTensor(),
         transforms.Normalize((0.5,), (0.5,))  # Assuming Min-Max normalization to [0,1]. Adjust if using Z-score normalization.
     ])
 
 def get_dataloaders(root_dir, batch_size=32):
     # Transforms
-    transform_original = original_transform()
-    transform_downsampled = downsample_transform()
+    transform = image_transform()
 
     # Datasets
-    original_train_dataset = ADNIDataset(os.path.join(root_dir, 'train'), transform=transform_original)
-    downsampled_train_dataset = ADNIDataset(os.path.join(root_dir, 'train'), transform=transform_downsampled)
-    
-    original_test_dataset = ADNIDataset(os.path.join(root_dir, 'test'), transform=transform_original)
-    downsampled_test_dataset = ADNIDataset(os.path.join(root_dir, 'test'), transform=transform_downsampled)
+    train_dataset = ADNIDataset(os.path.join(root_dir, 'train'), transform=transform)
+    test_dataset = ADNIDataset(os.path.join(root_dir, 'test'), transform=transform)
 
     # Data loaders
-    train_loader = DataLoader(list(zip(downsampled_train_dataset, original_train_dataset)), batch_size=batch_size, shuffle=True)
-    test_loader = DataLoader(list(zip(downsampled_test_dataset, original_test_dataset)), batch_size=batch_size, shuffle=False)
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
 
     return train_loader, test_loader
