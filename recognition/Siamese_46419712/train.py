8d64d3385040bfd515be73829c0ccb8241b20a36
MTbir <m.tieu@uqconnect.edu.au>
2023-10-11 18:23:23 +1000

    update loss plot from loss per step to loss per epoch

--- recognition/Siamese_46419712/train.py
+++ recognition/Siamese_46419712/train.py
@@ -3,9 +3,16 @@ import time
 import matplotlib.pyplot as plt
 import os
 import torch.nn as nn
+import numpy as np
 
 from modules import RawSiameseModel, ContrastiveLossFunction, BinaryModelClassifier
 from dataset import load_train_data, load_test_data, load_train_data_classifier
+
+SIAMESE_LOSS_SAVE_PATH = "siamese_loss_plot.png"
+SIAMESE_MODEL_SAVE_PATH = "siamese.pt"
+
+CLASSIFIER_LOSS_SAVE_PATH = "classifier_loss_plot.png"
+CLASSIFIER_MODEL_SAVE_PATH = "classifier.pt"
 
 def train_siamese(model, train_loader, criterion, optimizer, loss_list, scheduler):
     model.train()
@@ -62,7 +69,7 @@ def train_classifier(sModel, cModel, train_loader, criterion, optimizer, loss_li
         fv1 = model(img)
 
         output = cModel(fv1)
-        loss = criterion(output, label.unsqueeze(1))
+        loss = criterion(output, label.reshape(-1,1))
 
         optimizer.zero_grad()
         loss.backward()
@@ -75,8 +82,8 @@ def train_classifier(sModel, cModel, train_loader, criterion, optimizer, loss_li
             print (">>>>> Step [{}/{}] Loss: {:.5f}"
                     .format(i+1, len(train_loader), loss.item()))
 
-def validate_classifier(cModel, sModel, val_loader, criterion, val_loss_list):
-    pass
+# def validate_classifier(cModel, sModel, val_loader, criterion, val_loss_list):
+#     pass
 
 def test_model(model, cModel, test_loader):
     # evaluate the model
@@ -93,6 +100,11 @@ def test_model(model, cModel, test_loader):
             output = cModel(fv)
 
             _, predicted = torch.max(output.data, 1)
+            print(">>>>> Predicted")
+            print(predicted)
+
+            print(">>>>> Actual")
+            print(label)
             total_test += label.size(0)
             correct_predict += (predicted == label).sum().item()
     
@@ -104,15 +116,15 @@ def save_loss_plot(loss_list, epoch=0, train=True, siamese=True):
     """
     plt.figure(figsize=(12, 8))
     plt.plot(loss_list, label='Loss')
-    plt.xlabel('Number of Steps')
+    plt.xlabel('Epoch')
     plt.ylabel('Loss')
     if train:
         if siamese:
             plt.title(f'Siamese Losses - Epoch {epoch + 1}')
-            plt.savefig(f'siamese_loss_plot.png')
+            plt.savefig(SIAMESE_LOSS_SAVE_PATH)
         else:
             plt.title(f'Classifier Losses - Epoch {epoch + 1}')
-            plt.savefig(f'classifier_loss_plot.png')
+            plt.savefig(CLASSIFIER_LOSS_SAVE_PATH)
     else:
         if siamese:
             plt.title('Siamese Losses Validate')
@@ -124,9 +136,9 @@ def save_loss_plot(loss_list, epoch=0, train=True, siamese=True):
 
 def save_model(epochs, model, criterion, optimizer, scheduler, model_type=0):
     if model_type == 0:
-        PATH = 'siamese.pt'
+        PATH = SIAMESE_MODEL_SAVE_PATH
     else:
-        PATH = 'classifier.pt'
+        PATH = CLASSIFIER_MODEL_SAVE_PATH
 
     torch.save({
         "epochs": epochs,
@@ -140,9 +152,9 @@ def save_model(epochs, model, criterion, optimizer, scheduler, model_type=0):
 
 def load_model(model_type=0):
     if model_type == 0:
-        PATH = 'siamese.pt'
+        PATH = SIAMESE_MODEL_SAVE_PATH
     else:
-        PATH = 'classifier.pt'
+        PATH = CLASSIFIER_MODEL_SAVE_PATH
 
     if os.path.exists(PATH):
         load_model = torch.load(PATH)
@@ -184,7 +196,8 @@ if __name__ == '__main__':
     # adjust the learning rate during training -> potentially help reach convergence state faster while also ensure it does not over train
     scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=learning_rate, steps_per_epoch=total_step, epochs=num_epochs)
 
-    loss_list = [] 
+    loss_list = []
+    avg_loss_list = []
     val_loss_list = []
     start = time.time() #time generation
 
@@ -193,10 +206,16 @@ if __name__ == '__main__':
     for epoch in range(num_epochs):
         print ("Epoch [{}/{}]".format(epoch + 1, num_epochs))
         train_siamese(model, train_loader, criterion, optimizer, loss_list, scheduler)
-        save_loss_plot(loss_list, epoch) # save loss plot for siamese train
         validate_siamese(model, val_loader, criterion, val_loss_list)
         save_loss_plot(val_loss_list, train=False) # save loss plot for siamese validate
         save_model(epoch, model, criterion, optimizer, scheduler) # save model for every epoch
+
+        avg_loss_list.append(np.mean(loss_list))
+        loss_list = []
+        save_loss_plot(avg_loss_list, epoch) # save loss plot for siamese train
+
+
+    save_loss_plot(avg_loss_list, epoch)
 
 
     end = time.time() #time generation
@@ -208,14 +227,15 @@ if __name__ == '__main__':
     #########  TRAINING BINARY CLASSIFIER MODEL ########## 
     # hyper parameters for classifier
 
-    num_epochs = 20
-    learning_rate = 0.0001
-    cModel = BinaryModelClassifier(model).to(device)
+    num_epochs = 60
+    learning_rate = 0.001
+    cModel = BinaryModelClassifier().to(device)
 
     criterionB = nn.BCELoss()
-    optimizerB = torch.optim.Adam(cModel.parameters(), lr=learning_rate)
+    optimizerB = torch.optim.SGD(cModel.parameters(), lr=learning_rate)
 
     classifier_loss_list = []
+    avg_classifier_loss_list = []
 
     print("Start classifier training")
     
@@ -223,9 +243,15 @@ if __name__ == '__main__':
     # train classifier
     for epoch in range(num_epochs):
         print ("Epoch [{}/{}]".format(epoch + 1, num_epochs))
-        train_classifier(model, cModel, train_loader_classifier, criterionB, optimizerB, loss_list)
-        save_loss_plot(classifier_loss_list, epoch, siamese=False) # save loss plot for siamese train
+        train_classifier(model, cModel, train_loader_classifier, criterionB, optimizerB, classifier_loss_list)
         save_model(epoch, cModel, criterionB, optimizerB, scheduler, 1) # save model for every epoch
+
+        avg_classifier_loss_list.append(np.mean(classifier_loss_list))
+        classifier_loss_list = []
+        save_loss_plot(avg_classifier_loss_list, epoch, siamese=False) # save loss plot for siamese train
+
+    save_loss_plot(avg_classifier_loss_list, epoch)
+
 
     end = time.time() #time generation
     print("Finish classifier training")
