3e9a5cfac169aa525b0a540e89211a6547f1eb90
Awidat17 <mlamtadi1705@gmail.com>
2023-10-12 09:54:04 +1000

    "fixed file names and altered valid to truth set"

--- recognition/ISIC_I_UNet_45311716/dataset.py
+++ recognition/ISIC_I_UNet_45311716/dataset.py
@@ -7,16 +7,16 @@ if not torch.cuda.is_available():
     print("Warning CUDA not Found. Using CPU")
 
 # Path to images
-path = 'C:/Users/mlamt/OneDrive/UNI/2023/Semester 2/COMP3710/Code/COMP3710/Practicals/Prac_2/'
+path = 'C:/Users/mlamt/OneDrive/UNI/2023/Semester 2/COMP3710/Code/data/ISIC2018/'
 
 # Training dataset
-train_set = torchvision.datasets.ImageFolder(root=path+'train')
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=16, shuffle=True)
+train_set = torchvision.datasets.ImageFolder(root=path+'ISIC2018_Task1-2_Training_Input_x2')
+train_loader = torch.utils.data.DataLoader(train_set, batch_size=16, shuffle=False)
 
 # Testing dataset
-test_set = torchvision.datasets.ImageFolder(root=path+'test')
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=16, shuffle=True)
+test_set = torchvision.datasets.ImageFolder(root=path+'ISIC2018_Task1-2_Test_Input')
+test_loader = torch.utils.data.DataLoader(test_set, batch_size=16, shuffle=False)
 
-# Validation dataset
-validate_set = torchvision.datasets.ImageFolder(root=path+'validate')
-validate_loader = torch.utils.data.DataLoader(validate_set, batch_size=16, shuffle=True)
+# Truth dataset
+truth_set = torchvision.datasets.ImageFolder(root=path+'ISIC2018_Task1_Training_GroundTruth_x2')
+truth_loader = torch.utils.data.DataLoader(truth_set, batch_size=16, shuffle=False)
