89009916d325b6537e0500632189d8b53acdcda3
Cipher <57138168+Cipher0x0@users.noreply.github.com>
2023-10-11 22:13:35 +1000

    Added validation and test

--- recognition/UNet3D_Cipher0x0/train.py
+++ recognition/UNet3D_Cipher0x0/train.py
@@ -51,9 +51,13 @@ def main():
 
     # build model and optimizer
     loss_fn = nn.CrossEntropyLoss().to(device)
-    optimizer = torch.optim.Adam(model.parameters())
+    dice_loss = DiceLoss()
+    optimizer = torch.optim.Adam(unet.parameters())
 
     epoch = 30
+    loss_list = []
+    valid_dsc_list = []
+    test_dsc_list = []
 
     # train loop
     for i in range(epoch):
@@ -69,6 +73,49 @@ def main():
             loss = loss_fn(pred, mask)
             loss.backward()
             optimizer.step()
+
+        # run the model on the val set after each train loop
+        unet.eval()
+        num_batches = len(valloader)
+        val_loss = 0
+        dice_all = 0
+        with torch.no_grad():
+            for X, y in valloader:
+                X = X.unsqueeze(0)
+                X = X.float().to(device)
+                y = y.long().to(device)
+                pred = unet(X)
+                val_loss += loss_fn(pred, y).item()
+                dice_all += (1 - dice_loss(pred, y))
+        val_loss /= num_batches
+        dice_all /= num_batches
+        loss_list.append(val_loss)
+        valid_dsc_list.append(dice_all)
+        print(f"Avg loss: {val_loss:>8f}", flush=True)
+        print(f"DSC: {dice_all:>8f} \n", flush=True)
+
+        print('One Epoch Finished', flush=True)
+        torch.save(unet.state_dict(), 'net_paras.pth')
+
+        # run on test set after the train is finished
+        unet.eval()
+        num_batches = len(testloader)
+        dice_all = 0
+
+        with torch.no_grad():
+            for X, y in testloader:
+                X = X.unsqueeze(0)
+                X = X.float().to(device)
+                y = y.long().to(device)
+                pred = unet(X)
+                dice_all += (1 - dice_loss(pred, y))
+        dice_all = dice_all / num_batches
+        test_dsc_list.append(dice_all)
+        print(f"Dice: \n DSC: {dice_all:>8f} \n", flush=True)
+
+    np.save('valid_loss.npy', loss_list)
+    np.save('valid.npy', valid_dsc_list)
+    np.save('test.npy', test_dsc_list)
 
 
 if __name__ == "__main__":
