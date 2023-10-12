3a6ca182e9ef6483411c4e134cb2caa32bf3600d
MTbir <m.tieu@uqconnect.edu.au>
2023-10-12 13:50:52 +1000

    tidy up dataset code

--- recognition/Siamese_46419712/dataset.py
+++ recognition/Siamese_46419712/dataset.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 import torchvision
 import torch
 import torchvision.transforms as transforms
@@ -5,6 +6,11 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
+TRAIN_PATH = "/home/groups/comp3710/ADNI/AD_NC/train"
+TRAIN_PATH = "./AD_NC/train"
+
+TEST_PATH = "/home/groups/comp3710/ADNI/AD_NC/train"
+TEST_PATH = "./AD_NC/train"
 
 class PairedDataset(torch.utils.data.Dataset):
     
@@ -16,27 +22,16 @@ class PairedDataset(torch.utils.data.Dataset):
         img0, label0 = self.trainset[index]
 
         check_same_class = random.randint(0,1) 
-        if check_same_class:
-            while True:
-                #Look untill the same class image is found
-                img1, label1 = random.choice(self.trainset)
-                if torch.equal(img0, img1):
-                    continue
-
-                if label1 == label0:
-                    break
-        else:
-            while True:
-                #Look untill a different class image is found
-                img1, label1 = random.choice(self.trainset)
-                
-                if torch.equal(img0, img1):
-                    continue
-                
-                if label1 != label0:
-                    break
         
-        return img0, img1, torch.from_numpy(np.array([int(label0 != label1)], dtype=np.float32))
+        while True:
+            img1, label1 = random.choice(self.trainset)
+
+            if not torch.equal(img0, img1):
+
+                if label1 == label0 and check_same_class:
+                    break
+                elif label1 != label0 and not check_same_class:
+                    break
     
     def __len__(self):
         return len(self.trainset)
@@ -54,9 +49,7 @@ def split_dataset(dataset, seed=False):
     return train_set, val_set
 
 def load_train_data():
-    path="/home/groups/comp3710/ADNI/AD_NC/train"
-    # this for testing
-    path="./AD_NC/train"
+    path = TRAIN_PATH
 
     # torch.manual_seed(33) # for reproduce in the future
     # Data transformation
@@ -70,13 +63,6 @@ def load_train_data():
         transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
     ])
 
-    # transform_train = transforms.Compose([
-    #     transforms.Resize(105),
-    #     transforms.RandomHorizontalFlip(),
-    #     transforms.CenterCrop(105),
-    #     transforms.ToTensor(),
-    #     transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    # ])
     load_train_image = torchvision.datasets.ImageFolder(root=path, transform=transform_train)
 
     train_image, val_image = split_dataset(load_train_image)
@@ -90,8 +76,7 @@ def load_train_data():
     return train_loader, val_loader
 
 def load_train_data_classifier():
-    path="/home/groups/comp3710/ADNI/AD_NC/train"
-    path="./AD_NC/train"
+    path = TRAIN_PATH
 
     transform_train = transforms.Compose([
         transforms.Resize(224),
@@ -110,14 +95,7 @@ def load_train_data_classifier():
     return train_loader
 
 def load_test_data():
-    path="/home/groups/comp3710/ADNI/AD_NC/test"
-    # this for testing
-    path="./AD_NC/test"
-    # dataset = torchvision.datasets.ImageFolder(root=path, transform=transforms.ToTensor())
-
-    # # Calculate the mean and standard deviation
-    # mean = torch.mean(dataset.data.float() / 255.0)
-    # std = torch.std(dataset.data.float() / 255.0)
+    path = TEST_PATH
 
     # Data transformation
     transform_test = transforms.Compose([
