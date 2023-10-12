fdde03204816840b5ab8987dcd24daaade4e31f5
kaia-santosha <132866652+kaia-santosha@users.noreply.github.com>
2023-10-12 19:14:21 +1000

    Created function for getting feature vectors in correct form
    The features for each node are inconsistent in their quantity, thus I have made a function to convert the feature vector for each node into a bag of word vector so all nodes have equal num of features. If a word is part of a nodes feature list it will be indicated by a 1 and if its not it will be indicated by a 0

--- recognition/multi-layer_GCN_model_s4696681/dataset.py
+++ recognition/multi-layer_GCN_model_s4696681/dataset.py
@@ -1,6 +1,7 @@
 import csv
 import numpy as np
 from scipy.linalg import sqrtm
+import json
 
 """Create Adjacency matrix from the musae_facebook_edges.csv file"""
 def create_adjacency_matrix():
@@ -35,6 +36,7 @@ def create_adjacency_matrix():
     np.fill_diagonal(adjacency_matrix, 1)
     return adjacency_matrix
 
+"""Normalise adjanceny matrix. Where D is the Degree matrix"""
 def normalise_adjacency_matrix(adjacency_matrix):
     D = np.zeros_like(adjacency_matrix)
     np.fill_diagonal(D, np.asarray(adjacency_matrix.sum(axis=1)).flatten())
@@ -44,4 +46,35 @@ def normalise_adjacency_matrix(adjacency_matrix):
 
 #adjacency_normed = normalise_adjacency_matrix(create_adjacency_matrix())
 #print(adjacency_normed)
+
+
+"""Since the feature .json file has an inconsistent number of features for each node.
+ We need to make them consistent for training.
+ Hence I will create an n-dimensional bag of words feature vector for each node"""
+def create_feature_vectors():
+    # Load data from a JSON file
+    with open('../../../facebook_large\musae_facebook_features.json', 'r') as file:
+        data = json.load(file)
+
+    # Convert string keys to integers
+    data = {int(k): v for k, v in data.items()}
+
+    # Get unique features
+    all_features = set()
+    for features in data.values():
+        all_features.update(features)
+    all_features = sorted(list(all_features))
+
+    # Create n-dimensional bag of words feature vector for each node
+    feature_list = []
+    for node, features in data.items():
+        vector = [node] + [1 if feature in features else 0 for feature in all_features]
+        feature_list.append(vector)
+
+    # Convert to 2D numpy array
+    feature_vectors = np.array(feature_list, dtype=int)
+    return feature_vectors
+
+feature_vectors = create_feature_vectors()
+print(feature_vectors)
 
