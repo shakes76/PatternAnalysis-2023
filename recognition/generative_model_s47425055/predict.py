b8b6adbc9f5afafb5b8e4a73d1e520418dca1120
s4742505 <s4742505@login0.compute.eait.uq.edu.au>
2023-10-12 14:56:45 +1000

    added predict function to reconstruct image based on model parameters saved after training

--- recognition/generative_model_s47425055/predict.py
+++ recognition/generative_model_s47425055/predict.py
@@ -1,56 +1,50 @@
 # predict.py
 
 import torch
-import torchvision
+from modules import VectorQuantizedVAE, compute_ssim
+from dataset import test_loader
 import matplotlib.pyplot as plt
+import numpy as np
+from train import MODEL_PATH_TEMPLATE  # Importing the template string directly from train.py
 
-# Constants
-DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-DATASET_PATH = './OASIS'
+# Load the best epoch value
+with open("best_epoch.txt", "r") as file:
+    BEST_EPOCH = int(file.read())
 
-def save_and_display_images(images, filename, nrow=8):
-    """Saves and displays a grid of images."""
-    # Save the image
-    torchvision.utils.save_image(images, filename, nrow=nrow)
+DEVICE = torch.device('cuda')
+#MODEL_PATH = 'models/checkpoint_epoch{}_vqvae.pt'.format(BEST_EPOCH) # using the BEST_EPOCH to load the best model
+MODEL_PATH = MODEL_PATH_TEMPLATE.format(BEST_EPOCH)
 
-    # Display the image
-    grid_img = torchvision.utils.make_grid(images, nrow=nrow)
-    plt.figure(figsize=(16,8))
-    plt.imshow(grid_img.permute(1, 2, 0))
-    plt.savefig(f'{filename}_plot.png', bbox_inches='tight')
-    plt.close()
+# Load the trained model
+model = VectorQuantizedVAE(1, 256, 512).to(DEVICE)  # Assuming input_dim=1, dim=256, K=512 from your code
+model.load_state_dict(torch.load(MODEL_PATH))
+model.eval()
 
-def generate_samples(model, test_loader, epoch):
-    """Generates and saves reconstructed samples for a given epoch."""
-    model.eval()  # Set model to evaluation mode
-    x, _ = next(iter(test_loader))  # Get a batch of samples
-    x = x[:32].to(DEVICE)
-
-    # Reconstruct the images using the model
-    x_tilde, _, _ = model(x)
-    images = (torch.cat([x, x_tilde], 0).cpu().data + 1) / 2
-
-    # Save and display the reconstructed images
-    filename = f'samples3/vqvae_reconstructions_{epoch}'
-    save_and_display_images(images, filename, nrow=8)
-
-def generate_sample_from_best_model(model, test_loader, best_epoch):
-    """Generates and saves a sample using the best model from a given epoch."""
-    # Load the best model's weights
-    model.load_state_dict(torch.load(f'samples3/checkpoint_epoch{best_epoch}_vqvae.pt'))
-    model.eval()
-
-    # Get a sample from the test set
-    x, _ = next(iter(test_loader))
-    x = x[:32].to(DEVICE)
-
-    # Reconstruct the image using the model
-    x_tilde, _, _ = model(x)
-
-    # Save the reconstructed image
-    filename = 'samples3/best_model_sample'
-    save_and_display_images(x_tilde.cpu().data, filename)
+def save_image(tensor, filename):
+    img = tensor.cpu().clone().detach()
+    img = img.numpy().squeeze()
+    plt.imshow(img, cmap='gray')
+    plt.savefig(filename)
 
 
-if __name__ == "__main__":
-    
+def predict():
+    for idx, (x, _) in enumerate(test_loader):
+        x = x.to(DEVICE)
+        x_tilde, _, _ = model(x)
+
+        # Compute SSIM between original and reconstructed images
+        ssim_score = compute_ssim(x, x_tilde)
+
+        print(f"SSIM Score for batch {idx + 1}: {ssim_score:.4f}")
+        
+        # Save the reconstructed images for visualization
+        for j, image in enumerate(x_tilde):
+            save_image(image, f'samples3/reconstructed_{idx * len(x) + j + 1}.png')
+
+        # For brevity, let's break after the first batch
+        break
+
+if __name__ == '__main__':
+    predict()
+
+
