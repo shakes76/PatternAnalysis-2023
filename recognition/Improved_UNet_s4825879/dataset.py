7b821f69e35371aef0d0228bf90ded06f5eb90e3
Terym2001 <tadyrkor@gmail.com>
2023-10-12 11:50:51 +1000

    added custom pytorch dataset in file: dataset.py

---
+++ recognition/Improved_UNet_s4825879/dataset.py
@@ -0,0 +1,22 @@
+from torch.utils.data import Dataset
+import os
+
+# custom dataset
+class ISICDataset(Datset):
+    def __init__(self, img_dir, transform):
+        super(ISICDataset, self).__init__()
+        self.img_dir = img_dir
+        self.image_files = os.listdir(img_dir)
+        self.transform = transform
+
+    # function returns the length of the dataset
+    def __len__(self):
+        return len(self.image_files)
+
+    # get specific item from dataset
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.img, self.image_files[idx])
+        image = Image.open(img_path)
+        if self.transform:
+            image = self.transform(image)
+        return image
