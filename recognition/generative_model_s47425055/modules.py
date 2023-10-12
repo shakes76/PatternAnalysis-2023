fa9c5817948c2edefd8d29db3d79c0727cec3640
s4742505 <s4742505@login0.compute.eait.uq.edu.au>
2023-10-12 14:54:13 +1000

    added functions for generating samples

--- recognition/generative_model_s47425055/modules.py
+++ recognition/generative_model_s47425055/modules.py
@@ -3,9 +3,10 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-#from skimage.metrics import structural_similarity as ssim
-#import numpy as np
-
+from skimage.metrics import structural_similarity as ssim
+import numpy as np
+import matplotlib.pyplot as plt
+import torchvision
 
 
 def to_scalar(arr):
@@ -86,6 +87,49 @@ def plot_losses_and_scores():
     plt.tight_layout()
     plt.savefig('samples2/loss_ssim_plot.png', bbox_inches='tight')
     plt.close()
+def save_and_display_images(images, filename, nrow=8):
+    """Saves and displays a grid of images."""
+    # Save the image
+    torchvision.utils.save_image(images, filename, nrow=nrow)
+
+    # Display the image
+    grid_img = torchvision.utils.make_grid(images, nrow=nrow)
+    plt.figure(figsize=(16,8))
+    plt.imshow(grid_img.permute(1, 2, 0))
+    plt.savefig(f'{filename}_plot.png', bbox_inches='tight')
+    plt.close()
+
+def generate_samples(model, test_loader, epoch):
+    """Generates and saves reconstructed samples for a given epoch."""
+    model.eval()  # Set model to evaluation mode
+    x, _ = next(iter(test_loader))  # Get a batch of samples
+    x = x[:32].to(DEVICE)
+
+    # Reconstruct the images using the model
+    x_tilde, _, _ = model(x)
+    images = (torch.cat([x, x_tilde], 0).cpu().data + 1) / 2
+
+    # Save and display the reconstructed images
+    filename = f'samples3/vqvae_reconstructions_{epoch}'
+    save_and_display_images(images, filename, nrow=8)
+
+def generate_sample_from_best_model(model, test_loader, best_epoch):
+    """Generates and saves a sample using the best model from a given epoch."""
+    # Load the best model's weights
+    model.load_state_dict(torch.load(f'samples3/checkpoint_epoch{best_epoch}_vqvae.pt'))
+    model.eval()
+
+    # Get a sample from the test set
+    x, _ = next(iter(test_loader))
+    x = x[:32].to(DEVICE)
+
+    # Reconstruct the image using the model
+    x_tilde, _, _ = model(x)
+
+    # Save the reconstructed image
+    filename = 'samples3/best_model_sample'
+    save_and_display_images(x_tilde.cpu().data, filename)
+
 
 class VQEmbedding(nn.Module):
     def __init__(self, K, D):
