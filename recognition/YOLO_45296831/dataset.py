8bff40cbf671bfc6258d1d2b67856f01b3f4eca4
Riley Clark <rileyjclark14@gmail.com>
2023-10-12 21:27:01 +1000

    created new dataset.py. Added mask_to_bp function

--- recognition/YOLO_45296831/dataset.py
+++ recognition/YOLO_45296831/dataset.py
@@ -1,51 +1,31 @@
-import os
 import cv2
-import matplotlib.pyplot as plt
-import numpy as np
 
-import torch
-import torchvision
-from torch.utils.data import DataLoader, Dataset
-import torchvision.transforms as transforms
 
-data_path = "/home/groups/comp3710/ISIC2018/ISIC2018_Task1-2_Training_Input_x2/"
-batch_size = 128
+rangpur_data_path = "/home/Student/s4529683/COMP3710/"
+home_data_path = "C:/Users/clark/OneDrive/Documents/2023/COMP3710/ISIC2018_Task1_Training_GroundTruth_x2"
 
-class ISICDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
-        self.root_dir = root_dir
-        self.transform = transform
-        self.image_files = os.listdir(root_dir)
+def mask_to_bp(img_path):
+    """
+    Converts binary segmentation image into boundary polygon for training
+    
+    image_path: image to be converted
+    
+    return: list of polygon co-ordinates
+    """
+    img = cv2.imread(img_path, cv2.IMREAD_GRAYSCALE)
+    contours, _ = cv2.findContours(img, cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+    polygons = []
 
-    def __len__(self):
-        return len(self.image_files)
+    for obj in contours:
+        coords = []
 
-    def __getitem__(self, idx):
-        img_name = os.path.join(self.root_dir, self.image_files[idx])
-        image = cv2.imread(img_name)
+        for point in obj:
+            coords.append(int(point[0][0])/img.shape[0])
+            coords.append(int(point[0][1])/img.shape[1])
         
-        if self.transform:
-            image = self.transform(image)
+        polygons.append(coords)
 
-        return image
+    return polygons
 
-transform = transforms.Compose([
-    transforms.ToTensor(),
-    transforms.Grayscale(1),
-    transforms.Resize((256, 256)),
-    transforms.Normalize([0.5], [0.5])])
 
-dataset = ISICDataset(root_dir=data_path, transform=transform)
-train_loader = DataLoader(dataset=dataset, batch_size=batch_size, shuffle=True)
 
-# helper function to show an image
-def plot2x2Array(image):
-    plt.figure(figsize=(300, 300))
-    plt.imshow(image.squeeze(), cmap='gray', vmin=0, vmax=1)
-
-for i in range(5):
-    image = dataset[np.random.randint(0, len(dataset))]
-    image.numpy()
-    plot2x2Array(image)
-
-plt.savefig("image.png")
