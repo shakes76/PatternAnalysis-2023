0d638e45b5da4ee75d86a14714f03b5c851cf565
Terym2001 <tadyrkor@gmail.com>
2023-10-12 18:53:43 +1000

    change nn. classes from 3d to 2d

--- recognition/Improved_UNet_s4825879/modules.py
+++ recognition/Improved_UNet_s4825879/modules.py
@@ -8,11 +8,11 @@ class Context(nn.Module):
         self.pdrop = 0.3
         self.neagative_slope = 10**(-2)
 
-        self.instNorm = nn.InstanceNorm3d(size)
-        self.conv = nn.Conv3d(
+        self.instNorm = nn.InstanceNorm2d(size)
+        self.conv = nn.Conv2d(
             size, size, kernel_size=3
         )
-        self.dropOut = nn.Dropout3d(self.pdrop)
+        self.dropOut = nn.Dropout2d(self.pdrop)
 
     def forward(self, input):
         out = input
@@ -26,8 +26,8 @@ class Upsampling(nn.Module):
         super(Upsampling, self).__init__()
 
         self.upsample = nn.Upsample(scale_factor=2)
-        self.instNorm = nn.InstanceNorm3d(out_channels)
-        self.conv = nn.Conv3d(
+        self.instNorm = nn.InstanceNorm2d(out_channels)
+        self.conv = nn.Conv2d(
            in_channels, out_channels, kernel_size=3 
         ) 
 
@@ -39,15 +39,15 @@ class Upsampling(nn.Module):
 class Localization(nn.Module):
     def __init__(self, in_channels):
         super(Localization, self).__init__()
-
-        self.conv1 = nn.Conv3d(
+        out_channels = in_channels/2
+        self.conv1 = nn.Conv2d(
             in_channels, in_channels, kernel_size=3
         )
-        self.instNorm1 = nn.InstanceNorm3d(in_channels)
-        self.conv2 = nn.Conv3d(
-            in_channels, in_channels/2, kernel_size=1
+        self.instNorm1 = nn.InstanceNorm2d(in_channels)
+        self.conv2 = nn.Conv2d(
+            in_channels, 100, kernel_size=1
         )
-        self.instNorm2 = nn.InstanceNorm3d(in_channels/2)
+        self.instNorm2 = nn.InstanceNorm2d(in_channels/2)
         
     def forward(self, out):
         out = F.leaky_relu(self.instNorm1(self.conv1(out)), 10**(-2))
@@ -59,68 +59,68 @@ class ImpUNet(nn.Module):
         super(ImpUNet, self).__init__()
         self.negative_slope = negative_slope
         # arcitecture components
-        self.conv1 = nn.Conv3d(
+        self.conv1 = nn.Conv2d(
             in_channel, 16, kernel_size=3
         )
-        self.instNorm1 = nn.InstanceNorm3d(16)
+        self.instNorm1 = nn.InstanceNorm2d(16)
         self.context1 = Context(16)
 
-        self.conv2 = nn.Conv3d(
+        self.conv2 = nn.Conv2d(
             16, 32, kernel_size=3, stride=2
         )
-        self.instNorm2 = nn.InstanceNorm3d(32)
+        self.instNorm2 = nn.InstanceNorm2d(32)
         self.context2 = Context(32)
 
-        self.conv3 = nn.Conv3d(
+        self.conv3 = nn.Conv2d(
             32, 64, kernel_size=3, stride=2
         )
-        self.instNorm3 = nn.InstanceNorm3d(64)
+        self.instNorm3 = nn.InstanceNorm2d(64)
         self.context3 = Context(64)
         
-        self.conv4 = nn.Conv3d(
+        self.conv4 = nn.Conv2d(
             64, 128, kernel_size=3, stride=2
         )
-        self.instNorm4 = nn.InstanceNorm3d(128)
+        self.instNorm4 = nn.InstanceNorm2d(128)
         self.context4 = Context(128)
         
-        self.conv5 = nn.Conv3d(
+        self.conv5 = nn.Conv2d(
             128, 256, kernel_size=3, stride=2
         )
-        self.instNorm5 = nn.InstanceNorm3d(256)
+        self.instNorm5 = nn.InstanceNorm2d(256)
         self.context5 = Context(256)
         
         self.upsample0 = Upsampling(256, 128)
         
-        self.localize1 = Localization(256, 128)
+        self.localize1 = Localization(256)
         self.upsample1 = Upsampling(128, 64)
 
-        self.localize2 = Localization(128, 64)
+        self.localize2 = Localization(128)
         self.upsample2 = Upsampling(64, 32)
 
-        self.localize3 = Localization(64, 32)
+        self.localize3 = Localization(64)
         self.upsample3 = Upsampling(32, 16)
         
-        self.conv6 = nn.Conv3d(
+        self.conv6 = nn.Conv2d(
             32, 32, kernel_size=3
         )
-        self.instNorm6 = nn.InstanceNorm3d(32)
+        self.instNorm6 = nn.InstanceNorm2d(32)
 
         self.upscale = nn.Upsample(scale_factor=2)
         
         self.softmax = nn.Softmax(dim=3)
         # segmentation layers
-        self.seg1 = nn.Conv3d(
+        self.seg1 = nn.Conv2d(
             64, 3, kernel_size=1
         )
-        self.segNorm1 = nn.InstanceNorm3d(3)
-        self.seg2 = nn.Conv3d(
+        self.segNorm1 = nn.InstanceNorm2d(3)
+        self.seg2 = nn.Conv2d(
             32, 3, kernel_size=1
         )
-        self.segNorm2 = nn.InstanceNorm3d(3)
-        self.seg3 = nn.Conv3d(
+        self.segNorm2 = nn.InstanceNorm2d(3)
+        self.seg3 = nn.Conv2d(
             32, 3, kernel_size=1
         )
-        self.segNorm3 = nn.InstanceNorm3d(3)
+        self.segNorm3 = nn.InstanceNorm2d(3)
         
     def forward(self, out):
         # convolution layer. input 3d image output 16 channels
