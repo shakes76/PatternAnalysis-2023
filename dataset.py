a1b3cf9b6fdfb9500d7770c3dd9d4e5bbb1fe5a2
Lorenzo Policar <lorenzo_policar@yahoo.com>
2023-10-12 11:16:37 +1000

    moved data to gpu and fixed dataLoaders

--- dataset.py
+++ dataset.py
@@ -1,20 +1,22 @@
+import os
 import torch
 from torchvision import transforms, datasets
 from sklearn.model_selection import train_test_split
 
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+print("Using device:", device)
+
 # Define the data directory
-data_dir = "ADNI/"
+data_dir = "AD_NC/"
 
 def test():
     # Create the ImageFolder dataset
-    dataset = datasets.ImageFolder(root=data_dir)
-
+    dataset = datasets.ImageFolder(root=os.path.join(data_dir, "test"))
     # Iterate through the dataset and print labels
-    image, label = dataset[0]
+    image, label = dataset[4]
     print(f"Image: {image}, Label: {label}")
-    
 
-def get_loaders(data_dir):
+def get_loaders():
     # Define data transformations (resize, normalize, etc.)
     transform = transforms.Compose([
         transforms.Resize((256, 256)),  # Resize images to a consistent size
@@ -22,25 +24,26 @@ def get_loaders(data_dir):
         transforms.Normalize((0.5,), (0.5,))  # Normalize pixel values (adjust mean and std as needed)
     ])
 
-    # Create the ImageFolder dataset
-    dataset = datasets.ImageFolder(root=data_dir, transform=transform)
+    # Create the ImageFolder dataset for training
+    train_data = datasets.ImageFolder(root=os.path.join(data_dir, "train"), transform=transform)
+
+    # Create the ImageFolder dataset for testing
+    test_data = datasets.ImageFolder(root=os.path.join(data_dir, "test"), transform=transform)
 
     # Define the train-test split ratio (e.g., 80% train, 20% test)
     train_size = 0.8
-    validation_size = 0.1  # Allocate 10% of the data for validation
 
-    # Split the dataset into training, validation, and test sets
-    train_data, test_data = train_test_split(dataset, train_size=train_size, test_size=1 - train_size, shuffle=True, random_state=42)
-    validation_data, test_data = train_test_split(test_data, test_size=validation_size / (1 - train_size), shuffle=True, random_state=42)
+    # Split the training dataset into training and validation sets
+    train_data, validation_data = train_test_split(train_data, train_size=train_size, test_size=1 - train_size, shuffle=True, random_state=42)
 
     # Create data loaders
     batch_size = 64  # Adjust as needed
 
-    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True)
-    validation_loader = torch.utils.data.DataLoader(validation_data, batch_size=batch_size)
-    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size)
+    train_loader = torch.utils.data.DataLoader(train_data, batch_size=batch_size, shuffle=True, pin_memory=True, num_workers=4)
+    validation_loader = torch.utils.data.DataLoader(validation_data, batch_size=batch_size, pin_memory=True, num_workers=4)
+    test_loader = torch.utils.data.DataLoader(test_data, batch_size=batch_size, pin_memory=True, num_workers=4)
     
     return train_loader, validation_loader, test_loader
 
 if __name__ == "__main__":
-    test()
+    train_loader, validation_loader, test_loader = get_loaders()
