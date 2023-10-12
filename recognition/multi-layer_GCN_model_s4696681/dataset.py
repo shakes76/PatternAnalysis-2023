43e6321f4e495be939482cff351042533fd4cac6
kaia-santosha <132866652+kaia-santosha@users.noreply.github.com>
2023-10-12 18:01:22 +1000

    added preprocessing of dataset to produce adjacency matrix from the dataset
    I have created a function that returns an adjacency matrix that is created by analysing the edge links depicted in the musae_facebook_edges.csv

---
+++ recognition/multi-layer_GCN_model_s4696681/dataset.py
@@ -0,0 +1,39 @@
+import csv
+import numpy as np
+
+
+"""Create Adjacency matrix from the musae_facebook_edges.csv file"""
+def create_adjacency_matrix():
+    data = []
+
+    # Read the CSV data from musae_facebook_edges.csv file
+    with open('../../../facebook_large/musae_facebook_edges.csv', 'r') as file:
+        reader = csv.reader(file, delimiter=',')
+        data = list(reader)
+
+    # Determine all unique nodes, it will convert it to a list and sort it, then create a mapping of node to its index
+    nodes = set()
+    for row in data:
+        nodes.add(row[0])
+        nodes.add(row[1])
+    nodes = sorted(list(nodes), key=int)  
+    node_to_index = {node: idx for idx, node in enumerate(nodes)}
+
+    # Initialize adjacency matrix with zeros
+    matrix_size = len(nodes)
+    adjacency_matrix = np.zeros((matrix_size, matrix_size), dtype=int)
+
+    # Fill the matrix
+    for row in data:
+        i, j = node_to_index[row[0]], node_to_index[row[1]]
+        adjacency_matrix[i][j] = 1
+        # Because the links are symmetric
+        adjacency_matrix[j][i] = 1  
+
+    # Every node links to itself so the following must be done
+    np.fill_diagonal(adjacency_matrix, 1)
+
+    return adjacency_matrix
+
+adjacency_matrix = create_adjacency_matrix()
+print(adjacency_matrix)
