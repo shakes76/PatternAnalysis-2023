0dd8917b00167622b25b27de247386ec63852985
Manandeep Singh <manandeep1500@gmail.com>
2023-10-12 16:37:04 +1000

    Implement the Forward Pass Logic

--- topicRecogQues7_Manandeep_48239259/modules.py
+++ topicRecogQues7_Manandeep_48239259/modules.py
@@ -37,3 +37,17 @@ class SiameseNetwork(nn.Module):
             nn.ReLU(inplace=True),
 
             nn.Linear(500, 5))
+
+
+
+
+    def forward_once(self, x):
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2):
+        output1 = self.forward_once(input1)
+        output2 = self.forward_once(input2)
+        return output1, output2
