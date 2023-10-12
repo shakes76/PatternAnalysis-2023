ebc43a0d48046f22a1b5a4dfbcf427125e071902
lombo9 <lombospoon@gmail.com>
2023-10-12 10:37:35 +1000

    Fixed issues with the decode_block() function.

--- recognition/ISIC_UNET_S45857139/UNET.py
+++ recognition/ISIC_UNET_S45857139/UNET.py
@@ -1,15 +1,15 @@
+import torch
 import torch.nn as nn
+
+
 
 class UNET(nn.Module):
     """UNet model for image segmentation."""
-    def __init__(self):
+    def __init__(self, n_classes):
         """Initializing the channels, and the workflow for encoding and decoding."""
         super(UNET, self).__init__()
-        
-        self.defaultChannels = 3
-        self.initialChannels = 64
-        self.maxChannels = 128
-        self.endingChannels = 2
+
+        self.n_classes = n_classes
 
         def encode_block(in_channels,out_channels):
             path = nn.Sequential(
@@ -23,12 +23,13 @@ class UNET(nn.Module):
         
         def decode_block(in_channels,out_channels):
             path = nn.Sequential(
-                nn.ConvTranspose2d(in_channels,out_channels,kernel_size=3,stride=2,padding=1),
-                nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1),
+                nn.ConvTranspose2d(in_channels,out_channels,kernel_size=3,stride=1,padding=1),
+                nn.Conv2d(2*out_channels, out_channels, kernel_size=3, stride=1, padding=1),
                 nn.ReLU(),
-                nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1),
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
                 nn.ReLU(),  
             )
+            return path
 
         # The encoding path 
         self.encode1 = encode_block(3,64)
@@ -42,7 +43,7 @@ class UNET(nn.Module):
         self.decode2 = decode_block(512,256)
         self.decode3 = decode_block(256,128)
         self.decode4 = decode_block(128,64)
-        self.decode_output = decode_block(64, 2)
+        self.decode_output = decode_block(64, self.n_classes)
 
             
     def forward(self, x):
@@ -62,12 +63,16 @@ class UNET(nn.Module):
         encode5 = self.encode5(encode4)
 
         decode1 = self.decode1(encode5)
+        decode1 = torch.cat((decode1,encode4),dim=1)
         decode2 = self.decode2(decode1)
+        decode2 = torch.cat((decode2, encode3),dim=1)
         decode3 = self.decode3(decode2)
+        decode3 = torch.cat((decode3,encode2),dim=1)
         decode4 = self.decode4(decode3)
+        decode4 = torch.cat((decode4, encode1),dim=1)
         output = self.decode_output(decode4)
 
-        return output
+        return torch.sigmoid(output)
     
 
         
