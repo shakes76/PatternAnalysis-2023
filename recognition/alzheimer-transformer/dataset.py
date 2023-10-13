0116e885c26f0da69551e9dc9d91926acff4cb1c
ZezAbkin <henryabkin@gmail.com>
2023-10-13 16:40:00 +1000

    Adds patient-level train/val 80/20 split
    Adds function called get_patient_ids to allow get_alzheimer_dataloader function produce patient-level split

--- recognition/alzheimer-transformer/dataset.py
+++ recognition/alzheimer-transformer/dataset.py
@@ -1,13 +1,26 @@
 '''
 contains the data loader for loading and preprocessing the data
 '''
-
 from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader
 from torchvision import transforms
+import os
+from sklearn.model_selection import train_test_split
+from torch.utils.data import DataLoader, Subset
 
+'''
+Returns a list of unique patient IDs given the dataset path
+'''
+def get_patient_ids(data_path):
+    # Knowing that patient IDs are encoded in image filenames as 'patientID_xx.jpeg'
+    all_files = [os.path.basename(f) for dp, dn, filenames in os.walk(data_path) for f in filenames]
+    patient_ids = list(set([f.split('_')[0] for f in all_files]))
+    return patient_ids
+
+'''
+Returns the train, val, and test dataloaders for the AD_NC dataset, with a train/val split of 80/20
+'''
 def get_alzheimer_dataloader():
-    # Paths to your training and test datasets
+    # Paths to training and test datasets
     train_data_path = "./dataset/AD_NC/train"
     test_data_path = "./dataset/AD_NC/test"
 
@@ -28,13 +41,27 @@ def get_alzheimer_dataloader():
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]) 
     ])
 
+    # Get patient ids from training data path and split these with 80/20
+    patient_ids = get_patient_ids(train_data_path)
+    train_patient_ids, val_patient_ids = train_test_split(patient_ids, test_size=0.2, random_state=42)
+
     # Create datasets
     train_dataset = ImageFolder(root=train_data_path, transform=train_transforms)
     test_dataset = ImageFolder(root=test_data_path, transform=test_transforms)
 
-    # Create workers
-    # Create data loaders
-    train_loader = DataLoader(train_dataset, batch_size=32, shuffle=True, num_workers = 4)
-    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers = 4)
+    # Map ID split to the images to get patient-level split
+    train_indices = [i for i, (path, label) in enumerate(train_dataset.samples) if path.split('/')[-1].split('_')[0] in train_patient_ids]
+    val_indices = [i for i, (path, label) in enumerate(train_dataset.samples) if path.split('/')[-1].split('_')[0] in val_patient_ids]
 
-    return train_loader, test_loader
+    train_subset = Subset(train_dataset, train_indices)
+    val_subset = Subset(train_dataset, val_indices)
+
+    # Create data loaders
+    train_loader = DataLoader(train_subset, batch_size=32, shuffle=True, num_workers=4)
+    val_loader = DataLoader(val_subset, batch_size=32, shuffle=False, num_workers=4)
+    test_loader = DataLoader(test_dataset, batch_size=32, shuffle=False, num_workers=4)
+    
+    return train_loader, val_loader, test_loader
+
+
+    
