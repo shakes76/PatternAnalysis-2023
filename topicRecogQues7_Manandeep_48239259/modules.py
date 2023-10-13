52c57e9218756184c5a53c5d4336ee40d30a1378
Manandeep Singh <manandeep1500@gmail.com>
2023-10-13 13:31:28 +1000

    Add contrastive loss function

--- topicRecogQues7_Manandeep_48239259/modules.py
+++ topicRecogQues7_Manandeep_48239259/modules.py
@@ -38,9 +38,6 @@ class SiameseNetwork(nn.Module):
 
             nn.Linear(500, 5))
 
-
-
-
     def forward_once(self, x):
         output = self.cnn1(x)
         output = output.view(output.size()[0], -1)
@@ -51,3 +48,18 @@ class SiameseNetwork(nn.Module):
         output1 = self.forward_once(input1)
         output2 = self.forward_once(input2)
         return output1, output2
+
+
+class ContrastiveLoss(torch.nn.Module):
+  
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1, output2, keepdim = True)
+        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
+                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+
+
+        return loss_contrastive
