b6f832f44ceff3822df50cf913f25b1527c541c4
Manandeep Singh <manandeep1500@gmail.com>
2023-10-14 05:21:16 +1000

    Siamese Network Training and Model Saving

--- topicRecogQues7_Manandeep_48239259/train.py
+++ topicRecogQues7_Manandeep_48239259/train.py
@@ -24,3 +24,31 @@ optimizer = optim.Adam(net.parameters(),lr = 0.0005 )
 counter = []
 loss_history = []
 iteration_number= 0
+
+
+model_save_path = '/content/drive/MyDrive/siamese_network.pth'
+
+for epoch in range(Config.train_number_epochs):
+    for i, data in enumerate(train_dataloader, 0):
+        img0, img1, label = data
+        img0, img1, label = img0, img1, label
+        optimizer.zero_grad()
+        output1, output2 = net(img0, img1)
+        loss_contrastive = criterion(output1, output2, label)
+        loss_contrastive.backward()
+        optimizer.step()
+        if i % 10 == 0:
+            print("Epoch number {}\n Current loss {}\n".format(epoch, loss_contrastive.item()))
+            iteration_number += 10
+            counter.append(iteration_number)
+            loss_history.append(loss_contrastive.item())
+
+# Save the trained model to a .pth file
+torch.save(net.state_dict(), model_save_path)
+
+# Visualize loss history if needed
+plt.plot(iteration_counter, loss_history)
+plt.xlabel('No of Iteration')
+plt.ylabel('Loss')
+plt.title('Training Loss')
+plt.show()
