1244a24ef35fca97c3c9ca3175c3eb398764ed75
Caleb <calebsoonwm@gmail.com>
2023-10-13 11:38:13 +1000

    added printing of images and saving of model

--- recognition/superres_46501635/train.py
+++ recognition/superres_46501635/train.py
@@ -2,6 +2,15 @@ import torch
 import torch.optim as optim
 from modules import ESPCN
 from dataset import get_dataloaders
+import math
+from torchvision.utils import save_image
+import os
+import matplotlib.pyplot as plt
+
+
+output_dir = 'C:\\Users\\soonw\\ADNI\\ESPCN_generated_images'
+if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
 
 # Check for CUDA availability
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -25,6 +34,12 @@ optimizer = optim.Adam(model.parameters(), lr=learning_rate_initial)
 # Learning rate scheduler to adjust learning rate during training
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=5, factor=0.5, min_lr=learning_rate_final)
 
+# Function to compute PSNR
+def compute_psnr(mse_loss):
+    return 10 * math.log10(1 / mse_loss)
+
+# ... [rest of the code before the training loop]
+
 # Training loop
 for epoch in range(epochs):
     model.train()
@@ -43,6 +58,41 @@ for epoch in range(epochs):
     # Adjust learning rate
     scheduler.step(loss)
 
-    print(f"Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.4f}")
+    # Evaluation on the test set
+    model.eval()
+    avg_psnr = 0
+    with torch.no_grad():
+        for batch_idx, (LR, HR, _) in enumerate(test_loader):
+            LR, HR = LR.to(device), HR.to(device)
+            outputs = model(LR)
+            mse_loss = criterion(outputs, HR).item()
+            avg_psnr += compute_psnr(mse_loss)
+
+            # Save images every 5 epochs
+            if epoch % 5 == 0 and batch_idx == 0:  # Taking the first batch as an example
+                # Convert tensors to numpy arrays for visualization
+                downsampled_np = LR.cpu().numpy()[0][0]
+                original_np = HR.cpu().numpy()[0][0]
+                output_np = outputs.cpu().numpy()[0][0]
+
+                # Display images
+                fig, axes = plt.subplots(1, 3, figsize=(15, 5))
+                axes[0].imshow(downsampled_np, cmap='gray')
+                axes[0].set_title('Downsampled Image')
+                axes[1].imshow(output_np, cmap='gray')
+                axes[1].set_title('Upsampled by ESPCN')
+                axes[2].imshow(original_np, cmap='gray')
+                axes[2].set_title('Original Image')
+
+                for ax in axes:
+                    ax.axis('off')
+
+                plt.show()
+                
+        avg_psnr /= len(test_loader)
+        print(f"Epoch [{epoch + 1}/{epochs}], Loss: {loss.item():.4f}, Avg PSNR: {avg_psnr:.4f}")
 
 print("Training finished.")
+# Save the trained model
+save_path = 'C:\\Users\\soonw\\ADNI\\ESPCN_trained_model.pth'
+torch.save(model.state_dict(), save_path)
