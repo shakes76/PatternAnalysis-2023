d294f0ca89b8c4e68a646933a14c8cbfa5dba2ff
Samson Zhang <gniqiygnaw@gmail.com>
2023-10-13 23:38:16 +1000

    Added a function to generate dataset dictionary.

--- recognition/vqvae-46676207/utils.py
+++ recognition/vqvae-46676207/utils.py
@@ -13,6 +13,7 @@ from PIL import Image
 from torch.utils.data.sampler import SubsetRandomSampler
 import matplotlib.pyplot as plt
 from platform import node
+import os
 
 # IO Paths
 match node():                                                    # root of data dir
@@ -44,3 +45,35 @@ Z_DIM = 10          # dimension of Latent Space
 H_DIM = 1600        # dimension of Hidden Layer
 NUM_EPOCHS = 20     # number of epoch
 LR_RATE = 3e-4      # learning rate
+
+def gen_txt():
+    path = TRAIN_INPUT_PATH
+    txt_path = DATA_PATH + TRAIN_TXT
+    # path = VALID_INPUT_PATH
+    # txt_path = DATA_PATH + VALID_TXT
+    # path = TEST_INPUT_PATH
+    # txt_path = DATA_PATH + TEST_TXT
+
+    file_list = []
+    filelist = os.listdir(path) # all files (and folders) in this dir
+    count=0
+    write_file = open(txt_path, "w") # open file (readonly)
+
+    for file in os.listdir(path):   # iterate through all files
+        filename=os.path.splitext(file)[0]  # filename (without .png)
+        # filename_no_pre = os.path.splitext(file)[0][4:]
+        idx = os.path.splitext(file)[0][15:len(os.path.splitext(file)[0])-4]
+        filetype = os.path.splitext(file)[1]   # .png
+        # new_pair = os.path.join(filename + filetype + ' ' + SEG_PREFIX + filename_no_pre + filetype)
+        new_pair = os.path.join(filename + filetype + ' ' + idx)
+        file_list.append(new_pair)
+        count+=1
+
+    number_of_lines = len(file_list) # number of elements in list
+    print('file_list1:',file_list)
+    file_list.sort(key=lambda item:len(str(item)), reverse=False) # sorting
+    print('file_list:',file_list)
+    for current_line in range(number_of_lines):
+        write_file.write(file_list[current_line] + '\n')  # close file
+
+        
