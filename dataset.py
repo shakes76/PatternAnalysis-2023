8b8a46a8e0fce06ea95ddd765c5445eb5a03ee7d
noam-mendelson <noammendelson115@gmail.com>
2023-10-13 20:11:32 +1000

    Add function to extract patient IDs and split data into train/ validation sets

--- dataset.py
+++ dataset.py
@@ -23,3 +23,25 @@ all_train_images_paths_NC = get_image_paths_from_directory(train_images_paths_NC
 all_train_images_paths_AD = get_image_paths_from_directory(train_images_paths_AD)
 
 
+def extract_patient_id(image_path):
+    """Extract patient ID from image path."""
+    base_name = os.path.splitext(os.path.basename(image_path))[0] # # Extract the base filename without the extension
+    return base_name.split('_')[0] # Split by underscore and return the patient ID 
+
+## Extract unique patient IDs
+all_patient_ids_AD = list(set(extract_patient_id(path) for path in all_train_images_paths_AD))
+all_patient_ids_NC = list(set(extract_patient_id(path) for path in all_train_images_paths_NC))
+
+# Split patient IDs into training and validation sets (20%, 80% split)
+train_patient_ids_AD, val_patient_ids_AD = train_test_split(all_patient_ids_AD, test_size=0.2, random_state=42) 
+train_patient_ids_NC, val_patient_ids_NC = train_test_split(all_patient_ids_NC, test_size=0.2, random_state=42)
+
+# Map patient IDs back to image paths
+train_images_AD = [path for path in all_train_images_paths_AD if extract_patient_id(path) in train_patient_ids_AD]
+val_images_AD = [path for path in all_train_images_paths_AD if extract_patient_id(path) in val_patient_ids_AD]
+train_images_NC = [path for path in all_train_images_paths_NC if extract_patient_id(path) in train_patient_ids_NC]
+val_images_NC = [path for path in all_train_images_paths_NC if extract_patient_id(path) in val_patient_ids_NC]
+
+
+
+
