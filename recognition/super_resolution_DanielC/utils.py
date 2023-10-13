bbca64ebba7f1936a6aac63fad8564cd063fb69c
DHyunC <114087639+DHyunC@users.noreply.github.com>
2023-10-13 21:41:33 +1000

    changed resize function to allow flexibility by using variable

--- recognition/super_resolution_DanielC/utils.py
+++ recognition/super_resolution_DanielC/utils.py
@@ -1,18 +1,21 @@
 import torchvision.transforms as transforms
 import torch.nn as nn
 # IO-paths
-AD_train_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710 Report/data/AD_NC/train/AD_train"
-NC_train_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710 Report/data/AD_NC/train/NC_train"
-AD_test_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710 Report/data/AD_NC/test/AD_test"
-NC_test_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710 Report/data/AD_NC/test/NC_test"
-model_path = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710 Report/model"
+AD_train_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710_Report/data/AD_NC/train/AD_train"
+NC_train_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710_Report/data/AD_NC/train/NC_train"
+AD_test_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710_Report/data/AD_NC/test/AD_test"
+NC_test_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710_Report/data/AD_NC/test/NC_test"
+model_path = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710_Report/model/model.pt"
 
 # Hyper-parameters
-num_epochs = 10
+num_epochs = 1
 learning_rate = 0.001
 batch_size = 200
 
-def downsample_tensor(tensor):
-    transform = transforms.Compose([transforms.Resize((60, 64))])
+# Downsample-parameters 
+downsample_size = (60,64)
+
+def resize_tensor(tensor):
+    transform = transforms.Compose([transforms.Resize(downsample_size)])
 
     return transform(tensor)
