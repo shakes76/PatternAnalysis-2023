f4f47f809a125ff7d921690ba3a469c90921eef9
Shaivika Anand <shaivika.anand@gmail.com>
2023-10-13 13:29:04 +1000

    loading the preporocessed data from datasets.py and the trained model

--- recognition/TopicRecognition_Shaivika_48239192/predict.py
+++ recognition/TopicRecognition_Shaivika_48239192/predict.py
@@ -5,3 +5,9 @@ from modules import create_classifier
 from dataset import load_and_preprocess_data
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
+
+# Load and preprocess data
+Images, Labels = load_and_preprocess_data()
+
+# Load the trained model
+model = tf.keras.models.load_model('model/trained_model.h5')
