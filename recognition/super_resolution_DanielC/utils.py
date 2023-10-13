61f48d2766f03ae11fb09248505f5887d4c4bf1b
DHyunC <chsdaniel725@gmail.com>
2023-10-13 23:44:33 +1000

    changed path for desktop use and incerased epoch size

--- recognition/super_resolution_DanielC/utils.py
+++ recognition/super_resolution_DanielC/utils.py
@@ -1,14 +1,14 @@
 import torchvision.transforms as transforms
 import torch.nn as nn
 # IO-paths
-AD_train_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710_Report/data/AD_NC/train/AD_train"
-NC_train_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710_Report/data/AD_NC/train/NC_train"
-AD_test_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710_Report/data/AD_NC/test/AD_test"
-NC_test_dir = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710_Report/data/AD_NC/test/NC_test"
-model_path = "/Users/danielchoi/Documents/UQ/Y2S2/COMP3710_Report/model/model.pt"
+AD_train_dir = "F:/COMP3710/data/AD_NC/train/train_AD"
+NC_train_dir = "F:/COMP3710/data/AD_NC/train/train_NC"
+AD_test_dir = "F:/COMP3710/data/AD_NC/test/test_AD"
+NC_test_dir = "F:/COMP3710/data/AD_NC/test/test_NC"
+model_path = "F:/COMP3710/model/model.pt"
 
 # Hyper-parameters
-num_epochs = 1
+num_epochs = 10
 learning_rate = 0.001
 batch_size = 200
 
