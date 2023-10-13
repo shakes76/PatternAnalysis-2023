e64b2034a54047b91970b06128257323261e5e5d
Shaivika Anand <shaivika.anand@gmail.com>
2023-10-14 05:55:16 +1000

    finding accuracy,loading , processing and plotting the data so it tells result wheather the predicted and actual are same or not

--- recognition/TopicRecognition_Shaivika_48239192/predict.py
+++ recognition/TopicRecognition_Shaivika_48239192/predict.py
@@ -54,21 +54,46 @@ sns.heatmap(cm, annot=True, fmt='g')
 plt.xlabel("Predicted")
 plt.ylabel("True")
 plt.show()
+with open('test_accuracy.txt', 'r') as file:
+    accuracy = float(file.read())
 
-# Load and plot the training history
-history = model.history
-plt.figure(figsize=(15, 15))
-plt.subplot(221)
-plt.title('Loss')
-plt.plot(history.history['loss'], label='train')
-plt.plot(history.history['val_loss'], label='test')
-plt.legend()
-plt.subplot(222)
-plt.title('Accuracy')
-plt.plot(history.history['accuracy'], label='train')
-plt.plot(history.history['val_accuracy'], label='test')
-plt.legend()
+print(f'The accuracy of Model is: {accuracy}%')
+
+# Load and preprocess data
+Images, Labels = load_and_preprocess_data()
+
+# Split data into training and testing sets
+train_indices, test_indices = train_test_split(list(range(Images.shape[0])), train_size=0.8, test_size=0.2, shuffle=True)
+x_train = Images[train_indices]
+y_train = Labels[train_indices]
+x_test = Images[test_indices]
+y_test = Labels[test_indices]
+
+ad_indices = random.sample(np.where(y_test == 0)[0].tolist(), 5)
+cn_indices = random.sample(np.where(y_test == 1)[0].tolist(), 5)
+sample_indices = ad_indices + cn_indices
+
+sample_images = x_test[sample_indices]
+sample_labels = y_test[sample_indices]
+sample_predictions = model.predict(sample_images)
+sample_predicted_labels = np.argmax(sample_predictions, axis=1)
+
+for i, (image, actual_label, predicted_labels) in enumerate(zip(sample_images, sample_labels, sample_predicted_labels)):
+    plt.subplot(2, 5, i + 1)
+    plt.imshow(image.astype("uint8"))
+
+    if isinstance(actual_label, np.ndarray):
+        actual_label_names = [mapping_to_label[label] for label in actual_label]
+        actual_label_display = ', '.join(actual_label_names)
+    else:
+        actual_label_display = mapping_to_label[actual_label]
+
+    predicted_label_names = [mapping_to_label[predicted_labels]]
+    
+    plt.title(f"Actual: {actual_label_display}\nPredicted: {', '.join(predicted_label_names)}")
+    plt.axis("off")
 plt.show()
 
-best_accuracy = round(max(history.history["accuracy"]) * 100)
-print(f'The model has a best accuracy of {best_accuracy}%')
+
+
+
