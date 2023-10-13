9146af0658b60b320b07b79d8348593831f63103
peterdbeardsley <peter.d.beardsley@gmail.com>
2023-10-13 14:26:48 +1000

    Improved plotting of synthesis data and losses. Added plotting for real image samples.

--- recognition/StyleGAN_OASIS/predict.py
+++ recognition/StyleGAN_OASIS/predict.py
@@ -14,7 +14,6 @@ import numpy as np
 import torch
 import torchvision.utils as vutils
 from modules import Generator, AlphaScheduler
-from dataset import createDataLoader
 
 '''
 Plot the training losses for both the generator and discriminator models
@@ -30,47 +29,44 @@ def plot_losses(loss_file):
     losses = np.loadtxt(loss_file)
 
     plt.plot(losses[:,4], label="Total Discriminator Loss")    
-    plt.plot(losses[:,3], label="Total Generator Loss")
+    plt.plot(losses[:,3], label="Total Discriminator Loss")
+    plt.plot(losses[:,0], label="Weighted Gradient Penalty")
+    plt.plot(losses[:,1], label="Wasserstein Distance")
+    plt.plot(losses[:,2], label="Gradient Clipping")
+
     plt.xlabel("Batch Iteration")
     plt.ylabel("Loss")
     plt.legend()
     plt.title("GAN Losses of Generator and Discriminiator")
+    
+"""
+TODO: x
+"""
+def plot_generation(gen, n):
+    gen.eval()
+    z = torch.randn(n, gen.z_size)
+    gen_images = gen(z)
+    plt.figure(figsize=(20,20))
+    plt.axis("off")
+    plt.imshow(np.transpose(vutils.make_grid(gen_images[:n], padding=2, normalize=True).cpu(),(1,2,0)))
+    plt.title(f"Depth: {gen.alphaSched.depth}")
     plt.show()
     
-    plt.plot(losses[:,0], label="Weighted Gradient Penalty")
-    plt.plot(losses[:,1], label="Wasserstein Distance")
-    plt.plot(losses[:,2], label="Gradient Clipping")   
-    plt.xlabel("Batch Iteration")
-    plt.ylabel("Loss")
-    plt.legend()
-    plt.title("Decomposition of GAN Losses of Discriminator")
-"""
-Plot n images from synthetically generated images 
-    generator       A training generator class with weights preloaded
-    n               Number of images to dispay
-"""
-def plot_synthetic(generator, n):
-    generator.eval()
-    z = torch.randn(n, 256)
-    images = generator(z)
-    plt.figure(figsize=(16,16))
-    plt.axis("off")
-    plt.imshow(np.transpose(vutils.make_grid(images[:n], nrow=int(np.sqrt(n)), padding=2, normalize=True),(1,2,0)))
-    plt.title("Synthetically Generated Images")
-    plt.show()
     
 
-"""
-Plot n real images 
-    dataloader      A dataloader with a batch size of at least n
-    n               Number of images to dispay
-"""
-def plot_real(dataloader, n):
-    images = next(iter(dataloader))[0]
-    plt.figure(figsize=(16,16))
-    plt.axis("off")
-    plt.imshow(np.transpose(vutils.make_grid(images[:n], nrow=int(np.sqrt(n)), padding=2, normalize=True).cpu(),(1,2,0)))
-    plt.title("Real Images")
-    plt.show()   
+def run():
+    z_size = 256
+    w_size = 256
+    rgb_ch = 3
+    channels = [512, 512,256,128,64,32,16,4]
+    alphaSched = AlphaScheduler([1], [1], 1)
+    alphaSched.depth=6
+    generator = Generator(z_size, w_size, channels, rgb_ch, alphaSched, is_progressive=False)
+    generator.load_state_dict(torch.load("C:\\Temp\\stylegan_depth_6.pth"))
+    plot_generation(generator, 64, )
     
+    plot_losses(r"C:\Temp\losses_generator.csv", r"C:\Temp\losses_discriminator.csv")
 
+
+
+run()
