e0b02f21ab1f1d6e71d8a8fb1d604acd234515c8
Shaivika Anand <shaivika.anand@gmail.com>
2023-10-13 13:27:43 +1000

    importing statements for predict.py file

---
+++ recognition/TopicRecognition_Shaivika_48239192/predict.py
@@ -0,0 +1,7 @@
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+from modules import create_classifier
+from dataset import load_and_preprocess_data
+from sklearn.metrics import confusion_matrix
+import seaborn as sns
