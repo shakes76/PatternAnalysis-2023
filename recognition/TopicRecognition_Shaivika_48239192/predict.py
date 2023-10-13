cb7e5fc689a60b8b4dece122d2d76e4b83a72603
Shaivika Anand <shaivika.anand@gmail.com>
2023-10-14 05:48:56 +1000

    defined patch size, and made some other changes

--- recognition/TopicRecognition_Shaivika_48239192/predict.py
+++ recognition/TopicRecognition_Shaivika_48239192/predict.py
@@ -12,7 +12,16 @@ import os
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
 from train import model
 
+# Define the patch size
+patch_size = 6
+
+# Load an image and create the resized image
+# image_size = 128  
+# image = load_img('path_to_image.jpg', target_size=(image_size, image_size))
+# image = img_to_array(image)
+# resized_image = tf.convert_to_tensor([image])
 # Load and preprocess data
+
 Images, Labels = load_and_preprocess_data()
 
 # Load the trained model
@@ -47,5 +56,6 @@ plt.plot(history.history['accuracy'], label='train')
 plt.plot(history.history['val_accuracy'], label='test')
 plt.legend()
 plt.show()
+
 best_accuracy = round(max(history.history["accuracy"]) * 100)
 print(f'The model has a best accuracy of {best_accuracy}%')
