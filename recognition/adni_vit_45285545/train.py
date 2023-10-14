db3dcf32fa278fd86b4d6f4bbd00cf816f8545b0
Deren Teo <teo.deren@icloud.com>
2023-10-14 19:12:39 +1000

    Implemented early stopping
    Training loop returns if monitored metric (validation loss) does not
    improve for configurable (three) number of consecutive epochs.
    Training loop returns state of model after epoch with best metric.

--- recognition/adni_vit_45285545/train.py
+++ recognition/adni_vit_45285545/train.py
@@ -13,9 +13,49 @@ from tqdm import tqdm
 from dataset import create_train_dataloader, create_test_dataloader
 from modules import ViT
 
-N_EPOCHS = 20
+### UTILITIES ##################################################################
 
 strftime = lambda t: f'{int(t//3600):02}:{int((t%3600)//60):02}:{(t%3600)%60:08.5f}'
+
+def save_model(mdl: Any, timestamp: int = None) -> None:
+    '''Export the given model.'''
+    torch.save(mdl, f'adni-vit-trained-{timestamp or int(time.time())}')
+
+def load_model(timestamp: int) -> Any:
+    '''Import the model saved with the given timestamp.'''
+    return torch.load(f'adni-vit-trained-{timestamp}')
+
+class EarlyStopping:
+    '''Stop training when a monitored metric has stopped improving.'''
+    def __init__(self, mdl: Any, mode: str = 'min', min_delta: float = 0.0,
+                 patience: int = 0) -> None:
+        self.mdl = mdl
+        self.mode = mode
+        self.min_delta = min_delta
+        self.patience = patience
+        self.counter = 0
+        self.metric_best = {'min': float('inf'), 'max': 0}[mode]
+        self.mdl_timestamp = int(time.time())
+
+    def stop_training(self, metric: float) -> bool:
+        '''Returns true once the monitored metric has stopped improving.'''
+        if self.mode == 'min':
+            improved = metric < (self.metric_best - self.min_delta)
+        elif self.mode == 'max':
+            improved = metric > (self.metric_best + self.min_delta)
+        else:
+            raise ValueError('unrecognised early stopping mode')
+
+        if improved:
+            self.metric_best = metric
+            self.counter = 0
+            save_model(self.mdl, self.mdl_timestamp)
+        else:
+            self.counter += 1
+
+        return self.counter > self.patience
+
+### TRAINING ###################################################################
 
 def train_model(mdl: Any, epochs: int, device: torch.device, pg: bool = False) -> None:
     '''
@@ -26,6 +66,10 @@ def train_model(mdl: Any, epochs: int, device: torch.device, pg: bool = False) -
     train_loader, valid_loader = create_train_dataloader(val_pct=0.2)
     criterion = torch.nn.CrossEntropyLoss()
     optimizer = torch.optim.RAdam(mdl.parameters(), lr=1e-4)
+    earlystop = EarlyStopping(mdl, min_delta=1.0, patience=3)
+
+    # TODO: lr scheduler
+
     mdl.train()
     time_start = time.time()
     for epoch in range(epochs):
@@ -50,14 +94,30 @@ def train_model(mdl: Any, epochs: int, device: torch.device, pg: bool = False) -
                 labels = labels.to(device)
                 # Forward pass
                 outputs = mdl(images)
+                loss = criterion(outputs, labels)
                 _, predicted = torch.max(outputs.data, 1)
                 total += labels.size(0)
                 correct += (predicted == labels).sum().item()
 
         # Training report
         time_elapsed = time.time() - time_start
-        print(f'Epoch [{epoch+1:02}/{epochs:02}]  Loss: {loss.item():.5f}  ',
+        print(f'Epoch [{epoch+1:02}/{epochs:02}]  Val Loss: {loss.item():.5f}  ',
               f'Val Acc: {correct/total:.5f}  ({strftime(time_elapsed)})')
+
+        # Check early stopping condition
+        if earlystop.stop_training(loss.item()):
+            print(f'Stopping early, metric did not improve by more than '
+                  f'{earlystop.min_delta} for {earlystop.patience} consecutive '
+                  f'epochs')
+            break
+
+    # Return model from after epoch with lowest loss
+    if earlystop.metric_best == loss.item():
+        return mdl
+    else:
+        return load_model(earlystop.mdl_timestamp)
+
+### EVALUATION #################################################################
 
 def test_model(mdl: Any, device: torch.device, pg: bool = False) -> None:
     '''Test the given model on the ADNI test dataset.'''
@@ -81,17 +141,13 @@ def test_model(mdl: Any, device: torch.device, pg: bool = False) -> None:
         time_elapsed = time.time() - time_start
         print(f'Test accuracy: {100*correct/total:.2f}% ({strftime(time_elapsed)})')
 
-def save_model(mdl: Any) -> None:
-    '''Export the given model.'''
-    timestamp = int(time.time())
-    torch.save(mdl, f'adni-vit-trained-{timestamp}')
+### ENTRYPOINT #################################################################
 
 def main(args):
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     mdl = ViT().to(device)
-    train_model(mdl, epochs=args.epochs, device=device, pg=args.pg)
+    mdl = train_model(mdl, epochs=args.epochs, device=device, pg=args.pg)
     test_model(mdl, device=device, pg=args.pg)
-    save_model(mdl)
 
 
 if __name__ == '__main__':
