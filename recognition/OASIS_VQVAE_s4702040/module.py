6282ae4c698caf14ebd65ac8ccff9b237cd36c7b
JRSantos4742 <J.R.Santos@outlook.com>
2023-10-14 17:59:59 +1000

    Added decoder portion of unet
    Implemented decoder portion according to the graph of improved unet as described in https://arxiv.org/abs/1802.10508v1.

--- recognition/OASIS_VQVAE_s4702040/module.py
+++ recognition/OASIS_VQVAE_s4702040/module.py
@@ -66,9 +66,56 @@ class ImprovedUNet(nn.module):
         Decoder
         """
         # Level 1 localization pathway
+        # Level 1 localisation module
+        self.convD11 = nn.Conv2d(n_filter*16, n_filter*16, kernel_size=3, 
+                               stride=1, padding=1, bias=False)
+        self.batchnormD11 = nn.BatchNorm2d(n_filter*16)
+        self.convD12 = nn.Conv2d(n_filter*16, n_filter*8, kernel_size=1, 
+                               stride=1, padding=1, bias=False)
+        # Level 1 upsampling module
+        self.upsampleD1 = nn.Upsample(scale_factor=2)
+        self.convD13 = nn.Conv2d(n_filter*8, n_filter*4, kernel_size=3, 
+                               stride=1, padding=1, bias=False)
+        self.batchnormD12 = nn.BatchNorm2d(n_filter*4)
+
         # Level 2 localization pathway
+        # Level 2 localisation module
+        self.convD21 = nn.Conv2d(n_filter*8, n_filter*8, kernel_size=3, 
+                               stride=1, padding=1, bias=False)
+        self.batchnormD21 = nn.BatchNorm2d(n_filter*8)
+        self.convD22 = nn.Conv2d(n_filter*8, n_filter*4, kernel_size=1, 
+                               stride=1, padding=1, bias=False)
+        # Level 2 upsampling module
+        self.upsampleS2 = nn.Upsample(scale_factor=2)
+        self.convD23 = nn.Conv2d(n_filter*4, n_filter*2, kernel_size=3, 
+                               stride=1, padding=1, bias=False)
+        self.batchnormE22 = nn.BatchNorm2d(n_filter*2)
+        # Level 2 segmentation layer
+        self.segmentD2 = nn.Conv2d(n_filter*8, n_classes, kernel_size=1, 
+                               stride=1, padding=1, bias=False)
+
         # Level 3 localization pathway
+        # Level 3 localisation module
+        self.convD31 = nn.Conv2d(n_filter*4, n_filter*4, kernel_size=3, 
+                               stride=1, padding=1, bias=False)
+        self.batchnormD31 = nn.BatchNorm2d(n_filter*4)
+        self.convD32 = nn.Conv2d(n_filter*4, n_filter*2, kernel_size=1, 
+                               stride=1, padding=1, bias=False)
+        # Level 3 upsampling module
+        self.upsampleD3 = nn.Upsample(scale_factor=2)
+        self.convD33 = nn.Conv2d(n_filter*2, n_filter, kernel_size=3, 
+                               stride=1, padding=1, bias=False)
+        self.batchnormD32 = nn.BatchNorm2d(n_filter)
+        # Level 3 segmentation layer
+        self.segmentD3 = nn.Conv2d(n_filter*4, n_classes, kernel_size=1, 
+                               stride=1, padding=1, bias=False)
+
         # Level 4 localization pathway
+        self.convD41 = nn.Conv2d(n_filter*2, n_filter*2, kernel_size=3, 
+                               stride=1, padding=1, bias=False)
+        # Level 4 segmentation layer
+        self.segmentD4 = nn.Conv2d(n_filter*2, n_classes, kernel_size=3, 
+                               stride=1, padding=1, bias=False)
 
     def forward(self, x):
         pass
