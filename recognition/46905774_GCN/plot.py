4a60fee42c585b171bdec333081a28458304e8f7
MacBookPro <macbookpro@192-168-1-102.tpgi.com.au>
2023-10-14 22:22:43 +1000

    add embeddings_plot in plot.py

--- recognition/46905774_GCN/plot.py
+++ recognition/46905774_GCN/plot.py
@@ -1,5 +1,6 @@
 import matplotlib.pyplot as plt
 import numpy as np
+from sklearn.manifold import TSNE
 
 def plot_accuracy(train_acc, val_acc, save_path):
     plt.figure(figsize=(10, 7))
@@ -12,14 +13,23 @@ def plot_accuracy(train_acc, val_acc, save_path):
     plt.savefig(save_path)
     plt.show()
 
-
-def plot_loss(train_loss, val_loss, save_path):
+def plot_loss(train_loss, val_loss, save_path, epochs=15):
     plt.figure(figsize=(10, 7))
-    plt.plot(np.arange(1, len(train_loss) + 1), train_loss, label="Training Loss")
-    plt.plot(np.arange(1, len(val_loss) + 1), val_loss, label="Validation Loss")
+    plt.plot(np.arange(1, epochs + 1), train_loss[:epochs], label="Training Loss")
+    plt.plot(np.arange(1, epochs + 1), val_loss[:epochs], label="Validation Loss")
     plt.xlabel("Epochs")
     plt.ylabel("Loss")
     plt.title("Training and Validation Loss across Epochs")
     plt.legend(loc="upper right", fontsize="x-large")
     plt.savefig(save_path)
+    plt.show()
+
+
+def embeddings_plot(plot_filename, embeddings, point_color):
+    z = TSNE(n_components=2).fit_transform(embeddings.detach().cpu().numpy())
+
+    plt.figure(figsize=(8, 8))
+    plt.scatter(z[:, 0], z[:, 1], s=50, c=point_color)
+    filename = "{}.png".format(plot_filename)
+    plt.savefig(filename)
     plt.show()
