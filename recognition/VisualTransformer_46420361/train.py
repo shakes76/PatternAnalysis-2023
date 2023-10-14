18d89b78852c4728c43af06d9349cce005dfb2ab
Callum Elder <cjames.02@icloud.com>
2023-10-14 11:07:50 +1000

    implemented training loop

--- recognition/VisualTransformer_46420361/train.py
+++ recognition/VisualTransformer_46420361/train.py
@@ -1,23 +1,60 @@
 import torch
 from dataset import load_dataset
+from torch.optim import Adam
+from torch.nn import CrossEntropyLoss
+from tqdm import tqdm, trange
 
 
 # device config
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 if not torch.cuda.is_available():
     print("WARNING: CUDA not found, using CPU")
-
+    
 # hyperparameters
-start_image_size = 8
+N_EPOCHS = 5
+LR = 0.005
+batch_size = 128
 image_size = 64
-path = '~/OASIS_data/keras_png_slices_data'
-learning_rate = 1e-3
-batch_sizes = [256, 128, 64, 32, 16, 8]
-channels = 3
-z_dim = 256
-w_dim = 256
-in_channels = 256
-lambda_gp = 10
-progressive_epochs = [30]*len(batch_sizes)
+path = '/home/callum/AD_NC/'
 
-dataloader, dataset = load_dataset(image_size, batch_sizes, path)
+# Training loop
+def main():
+    train_loader, test_loader = load_dataset(image_size, batch_size, path)
+    optimizer = Adam(model.parameters(), lr=LR)
+    criterion = CrossEntropyLoss()
+    for epoch in trange(N_EPOCHS, desc="Training"):
+        train_loss = 0.0
+        for batch in tqdm(train_loader, desc=f"Epoch {epoch + 1} in training", leave=False):
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+            y_hat = model(x)
+            loss = criterion(y_hat, y)
+
+            train_loss += loss.detach().cpu().item() / len(train_loader)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+        print(f"Epoch {epoch + 1}/{N_EPOCHS} loss: {train_loss:.2f}")
+
+    # Test loop
+    with torch.no_grad():
+        correct, total = 0, 0
+        test_loss = 0.0
+        for batch in tqdm(test_loader, desc="Testing"):
+            x, y = batch
+            x, y = x.to(device), y.to(device)
+            y_hat = model(x)
+            loss = criterion(y_hat, y)
+            test_loss += loss.detach().cpu().item() / len(test_loader)
+
+            correct += torch.sum(torch.argmax(y_hat, dim=1) == y).detach().cpu().item()
+            total += len(x)
+        print(f"Test loss: {test_loss:.2f}")
+        print(f"Test accuracy: {correct / total * 100:.2f}%")
+        
+        
+if __name__ == '__main__':
+    main()
+        
