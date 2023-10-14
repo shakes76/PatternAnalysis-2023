99304f9503a64a55b4c7ccf684940a1d6d28ed49
WalkerJim1 <jamwalk00@gmail.com>
2023-10-15 09:51:44 +1000

    added blocks to be defined as per ImprovedUNet paper

---
+++ recognition/WALKER_45827468/modules.py
@@ -0,0 +1,25 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Context(nn.Module):
+    def __init__(self):
+        super(Context, self).__init__()
+        
+    def forward(self):
+        return
+    
+class UpSampling(nn.Module):
+    def __init__(self):
+        super(UpSampling, self).__init__()
+        
+    def forward(self):
+        return
+    
+class Localisation(nn.Module):
+    def __init__(self):
+        super(Localisation, self).__init__()
+        
+    def forward(self):
+        return
