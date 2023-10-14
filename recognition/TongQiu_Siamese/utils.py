b11b234dd43eef1424ab7d445be257c12f4b122c
Tong Qiu <tongqiu@Tongs-MacBook-Pro-2.local>
2023-10-14 15:58:08 +1000

    training directories added.

--- recognition/TongQiu_Siamese/utils.py
+++ recognition/TongQiu_Siamese/utils.py
@@ -1,7 +1,11 @@
 import torch
+from pathlib import Path
 
 
 class Config:
-    TRAIN_DIR = "/Users/tongqiu/Desktop/COMP3710_Report/AD_NC/train"
-    TEST_DIR = "/Users/tongqiu/Desktop/COMP3710_Report/AD_NC/test"
+    TRAIN_DIR = Path("/Users/tongqiu/Desktop/COMP3710_Report/AD_NC/train")
+    TEST_DIR = Path("/Users/tongqiu/Desktop/COMP3710_Report/AD_NC/test")
     DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    LOG_DIR = Path("/Users/tongqiu/Desktop/COMP3710_Report/log/SimpleCNN")
+    MODEL_DIR = '/Users/tongqiu/Desktop/COMP3710_Report/model.pth'
+
