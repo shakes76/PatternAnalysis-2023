e99f30d3faf68be476a4938f733c07fdb16888fd
lombo9 <lombospoon@gmail.com>
2023-10-14 10:13:45 +1000

    Decoding workflow fixed. All features of standard UNET now implemented.

--- recognition/ISIC_UNET_S45857139/UNET.py
+++ recognition/ISIC_UNET_S45857139/UNET.py
@@ -1,88 +1,73 @@
 import torch
 import torch.nn as nn
 
-
-
 class UNET(nn.Module):
-    """UNet model for image segmentation."""
     def __init__(self, n_classes):
-        """Initializing the channels, and the workflow for encoding and decoding."""
         super(UNET, self).__init__()
 
-        self.n_classes = n_classes
+        # Define the encoding layers
+        self.encode1 = self.conv_block(3, 64)
+        self.encode2 = self.conv_block(64, 128)
+        self.encode3 = self.conv_block(128, 256)
+        self.encode4 = self.conv_block(256, 512)
+        self.encode5 = self.conv_block(512, 1024)
 
-        def encode_block(in_channels,out_channels):
-            path = nn.Sequential(
-                nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=2, padding=1),
-                nn.ReLU(),
-                nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=2, padding=1),
-                nn.ReLU(),
-                nn.MaxPool2d(kernel_size=3, stride=2)
-            )
-            return path
-        
-        def decode_block(in_channels,out_channels):
-            path = nn.Sequential(
-                nn.ConvTranspose2d(in_channels,out_channels,kernel_size=3,stride=1,padding=1),
-                nn.Conv2d(2*out_channels, out_channels, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),
-                nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
-                nn.ReLU(),  
-            )
-            return path
+        # Define the decoding layers with upconvolutions
+        self.decode1 = self.upconv_block(1024, 512)
+        self.decode2 = self.upconv_block(512, 256)
+        self.decode3 = self.upconv_block(256, 128)
+        self.decode4 = self.upconv_block(128, 64)
 
-        # The encoding path 
-        self.encode1 = encode_block(3,64)
-        self.encode2 = encode_block(64,128)
-        self.encode3 = encode_block(128,256)
-        self.encode4 = encode_block(256,512)
-        self.encode5 = encode_block(512,1024)
+        # Define additional convolution layers to manage channel sizes after concatenation
+        self.decode_conv1 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
+        self.decode_conv2 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
+        self.decode_conv3 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
+        self.decode_conv4 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
 
-        # The decoding path
-        self.decode1 = decode_block(1024,512)
-        self.decode2 = decode_block(512,256)
-        self.decode3 = decode_block(256,128)
-        self.decode4 = decode_block(128,64)
-        self.decode_output = decode_block(64, self.n_classes)
+        # Final output layer
+        self.decode_output = nn.Conv2d(64, n_classes, kernel_size=1)
 
-            
+    def conv_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(),
+            nn.MaxPool2d(kernel_size=2, stride=2)
+        )
+
+    def upconv_block(self, in_channels, out_channels):
+        return nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
+
+    def upconv_and_concat(self, encode_feature, decode_feature, channels):
+        upconv_layer = self.upconv_block(channels*2, channels)
+        upconv = upconv_layer(decode_feature)
+        return torch.cat([upconv, encode_feature], dim=1)
+
     def forward(self, x):
-        """Passes the data through the encode and decode paths.
 
-        Parameters: 
-            x (tensor): the data as a tensor.
-
-        Returns: 
-            output (tensor): the output tensor image.
-
-        """
+        # Encode path
         encode1 = self.encode1(x)
         encode2 = self.encode2(encode1)
         encode3 = self.encode3(encode2)
         encode4 = self.encode4(encode3)
         encode5 = self.encode5(encode4)
 
-        decode1 = self.decode1(encode5)
-        decode1 = torch.cat((decode1,encode4),dim=1)
-        decode2 = self.decode2(decode1)
-        decode2 = torch.cat((decode2, encode3),dim=1)
-        decode3 = self.decode3(decode2)
-        decode3 = torch.cat((decode3,encode2),dim=1)
-        decode4 = self.decode4(decode3)
-        decode4 = torch.cat((decode4, encode1),dim=1)
+        # Decode path
+        decode1_input = self.upconv_and_concat(encode4, encode5, 512)
+        decode1 = self.decode_conv1(decode1_input)
+
+        decode2_input = self.upconv_and_concat(encode3, decode1, 256)
+        decode2 = self.decode_conv2(decode2_input)
+
+        decode3_input = self.upconv_and_concat(encode2, decode2, 128)
+        decode3 = self.decode_conv3(decode3_input)
+
+        decode4_input = self.upconv_and_concat(encode1, decode3, 64)
+        decode4 = self.decode_conv4(decode4_input)
+
         output = self.decode_output(decode4)
 
         return torch.sigmoid(output)
-    
-
-        
-
-        
-
-        
-
-        
-
-
 
 
