b31917809db09fbd77d7433fa70a2674be830c2a
Eric Ye <ericye223378@gmail.com>
2023-10-14 22:22:20 +1000

    Create low-res and high-res for train and validation data

--- recognition/SuperResolutionShanJiang/dataset.py
+++ recognition/SuperResolutionShanJiang/dataset.py
@@ -71,8 +71,8 @@ from IPython.display import display
 crop_width_size = 256
 crop_height_size = 249
 upscale_factor = 4
-input_width_size = crop_width_size // upscale_factor
 input_height_size = crop_height_size // upscale_factor
+input_width_size = crop_width_size // upscale_factor
 batch_size = 8
 data_dir = 'D:/temporary_workspace/comp3710_project/PatternAnalysis_2023_Shan_Jiang/recognition/SuperResolutionShanJiang/original/train/AD'
 
@@ -119,6 +119,58 @@ test_img_paths = sorted(
         if fname.endswith(".jpeg")
     ]
 )
+
+
+def process_input(input,input_height_size,input_width_size):
+    """ turn given image to grey scale and crop it 
+
+    Args:
+        input: image to be processed
+        input_width_size: width to be cropped into
+        input_height_size: height to be cropped into
+
+    Returns:
+        tensor: processed image
+    """
+    input = tf.image.rgb_to_yuv(input)
+    last_dimension_axis = len(input.shape) - 1
+    y, u, v = tf.split(input, 3, axis=last_dimension_axis)
+    return tf.image.resize(y, [input_height_size, input_width_size], method="area")
+
+
+def process_target(input):
+    """turn given image to grey scale
+
+    Args:
+        input: image to be processed
+
+    Returns:
+        tensor: processed image
+    """
+    input = tf.image.rgb_to_yuv(input)
+    last_dimension_axis = len(input.shape) - 1
+    y, u, v = tf.split(input, 3, axis=last_dimension_axis)
+    return y
+
+
+# Process train dataset:create low resolution images and corresponding high resolution images
+train_ds = train_ds.map(
+    lambda x: (process_input(x, input_height_size, input_width_size), process_target(x))
+)
+train_ds = train_ds.prefetch(buffer_size=32)
+
+# Process validation dataset:create low resolution images and corresponding high resolution images
+valid_ds = valid_ds.map(
+    lambda x: (process_input(x, input_height_size, input_width_size), process_target(x))
+)
+valid_ds = valid_ds.prefetch(buffer_size=32)
+
+# for batch in train_ds.take(1):
+#     for img in batch[0]:
+#         display(array_to_img(img))
+#     for img in batch[1]:
+#         display(array_to_img(img))
+
 
 
 
