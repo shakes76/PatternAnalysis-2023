a6702c4a74b0efecf659c9f400f3005ff3eb73f5
ryanlederhose <ryanlederhose@hotmail.com>
2023-10-14 19:04:51 +1000

    introduced train_model and validate_model functions

--- recognition/vision_transformer_ryanlederhose/train.py
+++ recognition/vision_transformer_ryanlederhose/train.py
@@ -22,10 +22,11 @@ class HyperParameters(object):
         self.dropout = 0.1
         self.num_classes = 2
         self.epochs = 10
-        self.lr = 2e-4
+        self.lr = 1e-3
         self.weight_decay = 3e-2
-        self.batch_size = 64
+        self.batch_size = 32
         self.dry_run = False
+        self.hidden_size = 64
 
 def check_accuracy(loader, model, device):
     '''
@@ -48,6 +49,63 @@ def check_accuracy(loader, model, device):
     model.train()
     return num_correct / num_samples
 
+def train_model(args, train_loader, epoch, device, model, criterion, optimizer):
+    '''
+    Train a specified model
+    '''
+
+    total_loss = 0.0
+    tk = tqdm(train_loader, desc="EPOCH" + "[TRAIN]" 
+                + str(epoch + 1) + "/" + str(args.epochs))
+    
+    for batch_idx, (data, targets) in enumerate(tk):
+        # Get data to cuda
+        data = data.to(device=device)
+        targets = targets.to(device=device)
+
+        # Forward propogation
+        scores = model(data)
+        loss = criterion(scores, targets)
+
+        # Back propogation
+        optimizer.zero_grad()
+        loss.backward()
+
+        # Optimizer step
+        optimizer.step()
+
+        total_loss += loss.item()
+        tk.set_postfix({"Loss": "%6f" % float(total_loss / (batch_idx + 1))})
+    
+    return total_loss
+
+def validate_model(args, val_loader, model, epoch, device, criterion):
+    '''
+    Evaluate a specified model
+    '''
+
+    model.eval()
+    total_loss = 0.0
+    num_correct = 0
+    num_samples = 0
+    tk = tqdm(val_loader, desc="EPOCH" + "[VALID]" + str(epoch + 1) + "/" + str(args.epochs))
+
+    for t, (data, targets) in enumerate(tk):
+        data, targets = data.to(device), targets.to(device)
+
+        scores = model(data)
+        _, predictions = scores.max(1)
+        num_correct += (predictions == targets).sum()
+        num_samples += predictions.size(0)
+        loss = criterion(scores, targets)
+
+        total_loss += loss.item()
+        tk.set_postfix({"Loss": "%6f" % float(total_loss / (t + 1))})
+
+    print(f"Accuracy on validation set: {num_correct / num_samples *100:.2f}")
+    model.train()
+    return total_loss / len(val_loader)
+
 
 def main():
 
@@ -59,7 +117,11 @@ def main():
     dl = DataLoader(batch_size=args.batch_size)
     train_loader = dl.get_training_loader()
     test_loader = dl.get_test_loader()
-    total_loss = 0.0
+    valid_loader = dl.get_valid_loader()
+    trainLoss = 0.0
+    trainLossList = []
+    validLoss = 0.0
+    validLossList = []
 
     model = ViT(args).to(device)
 
@@ -68,29 +130,11 @@ def main():
     model.train()
 
     for epoch in range(args.epochs):
-        tk = tqdm(train_loader, desc="EPOCH" + "[TRAIN]" 
-                  + str(epoch + 1) + "/" + str(args.epochs))
-        
-        for batch_idx, (data, targets) in enumerate(tk):
-            # Get data to cuda
-            data = data.to(device=device)
-            targets = targets.to(device=device)
+        trainLoss = train_model(args, train_loader, epoch, device, model, criterion, optimizer)
+        validLoss = validate_model(args, valid_loader, model, epoch, device, criterion)
+        trainLossList.append(trainLoss)
+        validLossList.append(validLoss)
 
-            # Forward propogation
-            scores = model(data)
-            loss = criterion(scores, targets)
-
-            # Back propogation
-            optimizer.zero_grad()
-            loss.backward()
-
-            # Optimizer step
-            optimizer.step()
-
-            total_loss += loss.item()
-            tk.set_postfix({"Loss": "%6f" % float(total_loss / (batch_idx + 1))})
-    
-    print(f"Accuracy on training set: {check_accuracy(train_loader, model, device)*100:.2f}")
     print(f"Accuracy on test set: {check_accuracy(test_loader, model, device)*100:.2f}")
 
 if __name__ == "__main__":
