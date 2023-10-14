43f38534b876684101c36284f37a8ced7de3429e
JRSantos4742 <J.R.Santos@outlook.com>
2023-10-14 22:13:28 +1000

    Created custom dataset
    Implemented custom dataset which loads the images and the masks and converts them to RGB values. Uses as a reference: https://pytorch.org/tutorials/beginner/basics/data_tutorial.html#creating-a-custom-dataset-for-your-files

--- recognition/OASIS_VQVAE_s4702040/dataset.py
+++ recognition/OASIS_VQVAE_s4702040/dataset.py
@@ -1,1 +1,25 @@
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+import os
 
+class CustomDataset(torch.utils.data.Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = os.listdir(image_dir)
+        self.masks = os.listdir(mask_dir)
+
+    def __len__(self):
+        return len(self.images)
+
+    def __getitem__(self, idx):
+        img_path = os.path.join(self.image_dir, self.images[idx])
+        image = Image.open(img_path).convert('RGB')
+        mask_path = os.path.join(self.mask_dir, self.masks[idx])
+        mask = Image.open(mask_path).convert('RGB')
+        if self.transform:
+            image = self.transform(image)
+            mask = self.transform(mask)
+        return (image, mask)
