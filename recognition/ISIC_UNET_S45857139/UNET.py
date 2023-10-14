1c3d1806448e4cd593c2c3b4704287532959becb
lombo9 <lombospoon@gmail.com>
2023-10-14 12:06:46 +1000

    Added leaky relu, dropout, instancenorm, residual block and localization module in order to alter the algorithm to suit the improve form.

--- recognition/ISIC_UNET_S45857139/UNET.py
+++ recognition/ISIC_UNET_S45857139/UNET.py
@@ -1,73 +1,92 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-class UNET(nn.Module):
+def adjust_residual(x, residual):
+    x_channels = x.shape[1]
+    residual_channels = residual.shape[1]
+
+    if x_channels != residual_channels:
+        adjustment_layer = nn.Conv2d(residual_channels,x_channels,kernel_size=1).to(x.device)
+        residual = adjustment_layer(residual)
+
+    return x + residual
+
+class ResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ResidualBlock,self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.dropout = nn.Dropout(p=0.3)
+        self.instancenorm = nn.InstanceNorm2d(out_channels)
+
+    def forward(self, x):
+        residual = x
+        x = F.leaky_relu(self.instancenorm(self.conv1(x)), negative_slope=1e-2)
+        x = self.dropout(x)
+        x = F.leaky_relu(self.instancenorm(self.conv2(x)), negative_slope=1e-2)
+        x = adjust_residual(x, residual)
+        return x
+    
+class LocalizationModule(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(LocalizationModule, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels // 2, kernel_size=1)
+
+    def forward(self, x):
+        x = F.leaky_relu(self.conv1(x), negative_slope= 1e-2)
+        x = F.leaky_relu(self.conv2(x), negative_slope = 1e-2)
+        return x
+
+class UNETImproved(nn.Module):
     def __init__(self, n_classes):
-        super(UNET, self).__init__()
+        super(UNETImproved, self).__init__()
 
-        # Define the encoding layers
-        self.encode1 = self.conv_block(3, 64)
-        self.encode2 = self.conv_block(64, 128)
-        self.encode3 = self.conv_block(128, 256)
-        self.encode4 = self.conv_block(256, 512)
-        self.encode5 = self.conv_block(512, 1024)
+        # The encoding path
+        self.encode1 = ResidualBlock(3, 64)
+        self.encode2 = ResidualBlock(64, 128)
+        self.encode3 = ResidualBlock(128, 256)
+        self.encode4 = ResidualBlock(256, 512)
+        self.encode5 = ResidualBlock(512, 1024)
 
-        # Define the decoding layers with upconvolutions
-        self.decode1 = self.upconv_block(1024, 512)
-        self.decode2 = self.upconv_block(512, 256)
-        self.decode3 = self.upconv_block(256, 128)
-        self.decode4 = self.upconv_block(128, 64)
+        # The decoding path 
+        self.decode1 = LocalizationModule(1024 + 512, 256)
+        self.decode2 = LocalizationModule(256 + 128, 128)
+        self.decode3 = LocalizationModule(128 + 64, 64)
+        self.decode4 = LocalizationModule(64 + 32, 32)
 
-        # Define additional convolution layers to manage channel sizes after concatenation
-        self.decode_conv1 = nn.Conv2d(1024, 512, kernel_size=3, padding=1)
-        self.decode_conv2 = nn.Conv2d(512, 256, kernel_size=3, padding=1)
-        self.decode_conv3 = nn.Conv2d(256, 128, kernel_size=3, padding=1)
-        self.decode_conv4 = nn.Conv2d(128, 64, kernel_size=3, padding=1)
-
-        # Final output layer
-        self.decode_output = nn.Conv2d(64, n_classes, kernel_size=1)
-
-    def conv_block(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
-            nn.ReLU(),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
-
-    def upconv_block(self, in_channels, out_channels):
-        return nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
-
-    def upconv_and_concat(self, encode_feature, decode_feature, channels):
-        upconv_layer = self.upconv_block(channels*2, channels)
-        upconv = upconv_layer(decode_feature)
-        return torch.cat([upconv, encode_feature], dim=1)
+        # # Output
+        self.final_conv = nn.Conv2d(16, n_classes, kernel_size=1)
 
     def forward(self, x):
 
-        # Encode path
-        encode1 = self.encode1(x)
-        encode2 = self.encode2(encode1)
-        encode3 = self.encode3(encode2)
-        encode4 = self.encode4(encode3)
-        encode5 = self.encode5(encode4)
+        # Encode
+        e1 = self.encode1(x)
+        e2 = self.encode2(F.max_pool2d(e1, 2))
+        e3 = self.encode3(F.max_pool2d(e2, 2))
+        e4 = self.encode4(F.max_pool2d(e3, 2))
+        e5 = self.encode5(F.max_pool2d(e4, 2))
 
-        # Decode path
-        decode1_input = self.upconv_and_concat(encode4, encode5, 512)
-        decode1 = self.decode_conv1(decode1_input)
+        # Decode
+        d1 = F.interpolate(e5, scale_factor=2, mode='nearest')
+        d1 = torch.cat([d1, e4], dim=1)
+        d1 = self.decode1(d1)
 
-        decode2_input = self.upconv_and_concat(encode3, decode1, 256)
-        decode2 = self.decode_conv2(decode2_input)
+        d2 = F.interpolate(d1, scale_factor=2, mode='nearest')
+        d2 = torch.cat([d2, e3], dim=1)
+        d2 = self.decode2(d2)
 
-        decode3_input = self.upconv_and_concat(encode2, decode2, 128)
-        decode3 = self.decode_conv3(decode3_input)
+        d3 = F.interpolate(d2, scale_factor=2, mode='nearest')
+        d3 = torch.cat([d3, e2], dim=1)
+        d3 = self.decode3(d3)
 
-        decode4_input = self.upconv_and_concat(encode1, decode3, 64)
-        decode4 = self.decode_conv4(decode4_input)
+        d4 = F.interpolate(d3, scale_factor=2, mode='nearest')
+        d4 = torch.cat([d4, e1], dim=1)
+        d4 = self.decode4(d4)
 
-        output = self.decode_output(decode4)
-
-        return torch.sigmoid(output)
-
+        # Output
+        out = self.final_conv(d4)
+        return torch.sigmoid(out)
+    
 
