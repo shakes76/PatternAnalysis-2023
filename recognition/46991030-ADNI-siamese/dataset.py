bf2ab8abb7aad7ced099173c083d85ace7acf532
Darcy Robson <17976794+drobson03@users.noreply.github.com>
2023-10-14 12:48:09 +1000

    Re-add classifier model

--- recognition/46991030-ADNI-siamese/dataset.py
+++ recognition/46991030-ADNI-siamese/dataset.py
@@ -3,9 +3,9 @@ import tensorflow as tf
 
 @tf.function
 def load_jpeg(path: str):
-    return tf.image.per_image_standardization(
-        tf.cast(tf.io.decode_jpeg(tf.io.read_file(path), channels=1), dtype=tf.float32)
-    )
+    return tf.cast(
+        tf.io.decode_jpeg(tf.io.read_file(path), channels=1), dtype=tf.float32
+    ) / tf.constant(255.0)
 
 
 def get_jpegs_at_path(path: str) -> tf.data.Dataset:
@@ -13,14 +13,21 @@ def get_jpegs_at_path(path: str) -> tf.data.Dataset:
 
 
 def create_pairs(x1: tf.data.Dataset, x2: tf.data.Dataset, label):
-    return tf.data.Dataset.zip((x1, x2)).map(
-        lambda x1, x2: (x1, x2, tf.constant(label, dtype=tf.float32))
-    )
+    return tf.data.Dataset.zip(
+        (x1.shuffle(x1.cardinality()), x2.shuffle(x2.cardinality()))
+    ).map(lambda x1, x2: (x1, x2, tf.constant(label, dtype=tf.float32)))
 
 
 def load_dataset(
     path: str,
-) -> tuple[tf.data.Dataset, tf.data.Dataset, tf.data.Dataset]:
+) -> tuple[
+    tf.data.Dataset,
+    tf.data.Dataset,
+    tf.data.Dataset,
+    tf.data.Dataset,
+    tf.data.Dataset,
+    tf.data.Dataset,
+]:
     print("Loading dataset")
 
     print("Finding training data")
@@ -55,6 +62,12 @@ def load_dataset(
         )
     )
 
+    train_AD = train_AD.map(lambda x: (x, tf.constant(0, dtype=tf.float32)))
+    train_NC = train_NC.map(lambda x: (x, tf.constant(1, dtype=tf.float32)))
+    class_train_ds = tf.data.Dataset.concatenate(train_AD, train_NC).shuffle(
+        train_AD.cardinality() + train_NC.cardinality()
+    )
+
     print("Finding testing data")
 
     test_AD = get_jpegs_at_path(f"{path}/test/AD")
@@ -87,12 +100,33 @@ def load_dataset(
         )
     )
 
+    test_AD = test_AD.map(lambda x: (x, tf.constant(0, dtype=tf.float32)))
+    test_NC = test_NC.map(lambda x: (x, tf.constant(1, dtype=tf.float32)))
+    class_test_ds = tf.data.Dataset.concatenate(test_AD, test_NC).shuffle(
+        test_AD.cardinality() + test_NC.cardinality()
+    )
+
     print("Loading images")
     train_ds = train_ds.map(lambda x1, x2, y: ((load_jpeg(x1), load_jpeg(x2)), y))
-
     test_ds = test_ds.map(lambda x1, x2, y: ((load_jpeg(x1), load_jpeg(x2)), y))
+    class_train_ds = class_train_ds.map(lambda x, y: (load_jpeg(x), y))
+    class_test_ds = class_test_ds.map(lambda x, y: (load_jpeg(x), y))
 
-    validate_ds = train_ds.take(train_ds.cardinality() // 5)
-    train_ds = train_ds.skip(train_ds.cardinality() // 5)
+    validate_ds = train_ds.shuffle(train_ds.cardinality()).take(
+        train_ds.cardinality() // 5
+    )
+    train_ds = train_ds.shuffle(train_ds.cardinality()).skip(
+        train_ds.cardinality() // 5
+    )
 
-    return train_ds.batch(32), validate_ds.batch(32), test_ds.batch(32)
+    class_validate_ds = class_train_ds.take(class_train_ds.cardinality() // 5)
+    class_train_ds = class_train_ds.skip(class_train_ds.cardinality() // 5)
+
+    return (
+        train_ds.batch(32),
+        validate_ds.batch(32),
+        test_ds.batch(32),
+        class_train_ds.batch(32),
+        class_validate_ds.batch(32),
+        class_test_ds.batch(32),
+    )
--- recognition/46991030-ADNI-siamese/train.py
+++ recognition/46991030-ADNI-siamese/train.py
@@ -17,6 +17,7 @@ model.fit(
     epochs=20,
     validation_data=validate_ds,
     verbose=1,
+    shuffle=True,
 )
 
 print("Testing SNN model")
@@ -24,3 +25,20 @@ model.evaluate(test_ds, verbose=1)
 
 print("Saving SNN model")
 model.save("snn.keras")
+
+twin = model.get_layer("sequential")
+twin.trainable = False
+
+classifier = modules.snn_classifier(twin)
+
+classifier.fit(
+    class_train_ds,
+    epochs=10,
+    validation_data=class_validate_ds,
+    verbose=1,
+    shuffle=True,
+)
+
+classifier.evaluate(class_test_ds, verbose=1)
+
+classifier.save("classifier.keras")
