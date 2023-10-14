24c31bb47ba7ad3b1cda699493d3153ab895f4e0
Eric Ye <ericye223378@gmail.com>
2023-10-14 23:50:10 +1000

    define ESPCNCallback

---
+++ recognition/SuperResolutionShanJiang/train.py
@@ -0,0 +1,36 @@
+from utils import *
+from tensorflow import keras
+from keras import layers
+from keras.utils import load_img
+import os
+import math
+
+upscale_factor = 4
+# define test data from test AD 
+test_path = 'D:/temporary_workspace/comp3710_project/PatternAnalysis_2023_Shan_Jiang/recognition/SuperResolutionShanJiang/original/test/AD'
+test_img_paths = sorted(
+    [
+        os.path.join(test_path, fname)
+        for fname in os.listdir(test_path)
+        if fname.endswith(".jpeg")
+    ]
+)
+class ESPCNCallback(keras.callbacks.Callback):
+    def __init__(self):
+        super().__init__()
+        self.test_img = get_lowres_image(load_img(test_img_paths[0]), upscale_factor)
+
+    # Store PSNR value in each epoch.
+    def on_epoch_begin(self, epoch, logs=None):
+        self.psnr = []
+
+    def on_epoch_end(self, epoch, logs=None):
+        print("Mean PSNR for epoch: %.2f" % (np.mean(self.psnr)))
+        if epoch % 20 == 0:
+            prediction = upscale_image(self.model, self.test_img)
+            # plot_results(prediction, "epoch-" + str(epoch), "prediction")
+
+    def on_test_batch_end(self, batch, logs=None):
+        self.psnr.append(10 * math.log10(1 / logs["loss"]))
+        
+
