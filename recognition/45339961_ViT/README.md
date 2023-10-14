e50d3bc8dfc8f6c2dbd61451866dbc0699bca4bf
jacques-serfontein <jben.serfontein@gmail.com>
2023-10-14 16:07:09 +1000

    Working through examples

--- recognition/45339961_ViT/README.md
+++ recognition/45339961_ViT/README.md
@@ -1,28 +1,19 @@
-# StyleGAN for the OASIS Brain Dataset
+# Classifying Alzheimer's Disease of the ADNI brain data using a Visual Transformer
 
-![Alt text](misc/architecture.png)
+# Contents
 
-## StyleGAN Architecture
-- Introduces a mapping framework alongside the generator.
-- Introduces Affine Transformation (A) and Adapative Normalisation (AdaIN).
-- Adds noise throughout the network, not just the beginning. Also scaled the noise with B.
+## Data Background
 
-![Alt text](misc/image.png)
-
-- IN extends Batch Normalisation
-- AdaIN extends IN
-
-- Started of a Progressive GAN, added in bilinear tuning -> added mapping network and styles (A) -> Remove traditional input (adds a constant tensor) -> added noist inputs (B) -> Mixing regularisation.
-
-Mixing Regularisation:
-- Generate two vectors w_1 and w_2 by passing two latent vectors z_1 and z_2.
-- Use w1 and w2 at completely randomly chosen style layers during training
-- Use this mixing with certain % of training data only
-
+## Vision Transformer Architecture
 
 ## How it works
-- 
+
+## Dependencies & Requirements
 
 ## Results
 
 ## References
+https://arxiv.org/pdf/2010.11929.pdf
+
+https://www.akshaymakes.com/blogs/vision-transformer
+
--- recognition/45339961_ViT/dataset.py
+++ recognition/45339961_ViT/dataset.py
@@ -1,1 +1,104 @@
 """ Data loader for loading and preprocessing the dataset. """
+
+import os
+import torch
+import torchvision
+from torchvision import transforms, datasets
+from torch.utils.data import DataLoader, Dataset
+import matplotlib.pyplot as plt
+from PIL import Image
+
+class ADNIDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+
+        # Get a list of subdirectories (labels) in the root directory
+        self.labels = sorted(os.listdir(root_dir))
+
+        # Create a mapping from label (subdirectory) to an integer
+        self.label_to_idx = {label: idx for idx, label in enumerate(self.labels)}
+
+        self.data = []
+        self.targets = []
+        self.names = []
+
+        # Create a dictionary to store the count of images for each class
+        self.class_counts = {label: 0 for label in self.labels}
+
+        # Iterate through subdirectories
+        for label in self.labels:
+            label_dir = os.path.join(root_dir, label)
+            
+            # Check if it's a directory
+            if not os.path.isdir(label_dir):
+                continue
+
+            # Get a list of image files in the subdirectory
+            image_files = [f for f in os.listdir(label_dir) if f.lower().endswith((".jpg", ".jpeg"))]
+
+            # Update class counts and append image paths and their corresponding labels
+            for image_file in image_files:
+                image_path = os.path.join(label_dir, image_file)
+                self.data.append(image_path)
+                self.targets.append(self.label_to_idx[label])
+                self.names.append(image_file)
+                self.class_counts[label] += 1
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, idx):
+        image_path = self.data[idx]
+        image = Image.open(image_path).convert('L')
+
+        if self.transform:
+            image = self.transform(image)
+
+        label = self.targets[idx]
+        name = self.names[idx]
+
+        return image, label, name
+
+    def get_class_counts(self):
+        return self.class_counts
+    
+    def get_labels(self):
+        return self.labels
+    
+    def get_names(self):
+        return self.names
+
+def create_dataloaders(dir, batch_size=64, transform=None, num_workers=4):
+    train_path = os.path.join(dir, "train")
+    test_path = os.path.join(dir, "test")
+
+    train_dataset = ADNIDataset(train_path, transform=transform)
+    test_dataset = ADNIDataset(test_path, transform=transform)
+
+    train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+
+    return train_loader, test_loader
+
+if __name__ == "__main__":
+    BATCH_SIZE = 32
+
+    transform = transforms.Compose([
+        transforms.CenterCrop((192, 192)),
+        # transforms.RandomRotation(degrees=90),
+        transforms.ToTensor(),
+    ])
+
+    train_loader, test_loader = create_dataloaders("D:/AD_NC", transform=transform, batch_size=BATCH_SIZE)
+    print(train_loader.dataset.get_class_counts())
+    print(test_loader.dataset.get_class_counts())
+
+    images, labels, names = next(iter(train_loader))
+    # images = images.numpy()
+    print(len(images))
+    print(images[0].shape)
+    print(names[0])
+    plt.imshow(images[0][0], cmap='gray')
+    plt.axis("off")
+    plt.show()
