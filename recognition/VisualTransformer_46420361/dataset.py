9c24b88e8018eef4a53c8651b83c4650f8a4067c
Callum Elder <cjames.02@icloud.com>
2023-10-14 13:01:13 +1000

    added grayscale

--- recognition/VisualTransformer_46420361/dataset.py
+++ recognition/VisualTransformer_46420361/dataset.py
@@ -3,15 +3,31 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 
 
+def crop_images():
+    """
+    crops the dataset images to only be of the brain (no waste)
+    """
+    return
+
+def filter_datasets():
+    """filters dataset so patient images are kept together, preventing data leakage and overfitting
+
+    Returns:
+        _type_: _description_
+    """
+    return train_dataset, test_dataset
+
 def load_dataset(image_size, batch_size, path):
     # transform
     train_transform = transforms.Compose([
         transforms.Resize((image_size, image_size)),
+        transforms.Grayscale(),
         transforms.ToTensor(),
     ])
     
     test_transform = transforms.Compose([
         transforms.Resize((image_size, image_size)),
+        transforms.Grayscale(),
         transforms.ToTensor(),
     ])
 
