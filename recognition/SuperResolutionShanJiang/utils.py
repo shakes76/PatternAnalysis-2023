02f24d0f9d68ca663d7cfb3388a511011bb955f6
Eric Ye <ericye223378@gmail.com>
2023-10-14 23:18:20 +1000

    Add utility funcions

---
+++ recognition/SuperResolutionShanJiang/utils.py
@@ -0,0 +1,53 @@
+import matplotlib.pyplot as plt
+from mpl_toolkits.axes_grid1.inset_locator import zoomed_inset_axes
+from mpl_toolkits.axes_grid1.inset_locator import mark_inset
+import PIL
+import numpy as np
+from keras.utils import img_to_array
+def get_lowres_image(img, upscale_factor):
+    """Return low-resolution image converted from given image to be fed into model
+
+    Args:
+        img: image to be fed into model
+        upscale_factor (_type_): the ratio the image is downsized by
+
+    Returns:
+        Image: low-resolution image
+    """
+    return img.resize(
+        (img.size[0] // upscale_factor, img.size[1] // upscale_factor),
+        PIL.Image.BICUBIC,
+    )
+
+
+def upscale_image(model, img):
+    """Use given model to predict high resolution version of the given image and it as RGB.
+
+    Args:
+        model: super-resolution network 
+        img (_type_): low resolution image to be converted to high resolution
+
+    Returns:
+        Image: prediction result- high resolution RGB image
+    """
+    ycbcr = img.convert("YCbCr")
+    y, cb, cr = ycbcr.split()
+    y = img_to_array(y)
+    y = y.astype("float32") / 255.0
+
+    input = np.expand_dims(y, axis=0)
+    out = model.predict(input)
+
+    out_img_y = out[0]
+    out_img_y *= 255.0
+
+    # Restore the image in RGB color space.
+    out_img_y = out_img_y.clip(0, 255)
+    out_img_y = out_img_y.reshape((np.shape(out_img_y)[0], np.shape(out_img_y)[1]))
+    out_img_y = PIL.Image.fromarray(np.uint8(out_img_y), mode="L")
+    out_img_cb = cb.resize(out_img_y.size, PIL.Image.BICUBIC)
+    out_img_cr = cr.resize(out_img_y.size, PIL.Image.BICUBIC)
+    out_img = PIL.Image.merge("YCbCr", (out_img_y, out_img_cb, out_img_cr)).convert(
+        "RGB"
+    )
+    return out_img
