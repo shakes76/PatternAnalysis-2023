ce5be66ccb753d5350331e87867ba4fb54f1ee34
Zilong Ye <ericye223378@gmail.com>
2023-10-15 01:09:57 +1000

    test model

--- recognition/easy/train.py
+++ recognition/easy/train.py
@@ -27,8 +27,8 @@ batch_size = 4
 # dir_img contains input images
 # dir_mask contains corresponding masks
 current_directory = os.getcwd()
-relative_img_path = 'recognition/easy/data/image/'
-relative_mask_path = 'recognition/easy/data/mask/'
+relative_img_path = 'recognition/easy/datasmall/image/'
+relative_mask_path = 'recognition/easy/datasmall/mask/'
 
 dir_img = Path(os.path.join(current_directory, relative_img_path))
 dir_mask = Path(os.path.join(current_directory, relative_mask_path))
@@ -74,13 +74,13 @@ print(dataset.__len__())
 dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
 
 # Training loop
-num_epochs = 10  # Adjust as needed
+num_epochs = 0  # Adjust as needed
 for epoch in range(num_epochs):
     model.train()
     running_loss = 0.0
     for i, data in enumerate(dataloader, 0):
-        if i %10 == 0:
-            print(i)
+        
+        print(i)
         inputs, masks = data
         inputs, masks = inputs.to(device), masks.to(device)
 
@@ -101,4 +101,41 @@ for epoch in range(num_epochs):
 
 print("Finished Training")
 
-# Validation can be added here to monitor model performance during training
+# testing
+relative_img_path = 'recognition/easy/testsmall/image/'
+
+
+test_image_dir = Path(os.path.join(current_directory, relative_img_path))
+output_dir = Path(current_directory)
+
+# Ensure the output directory exists
+output_dir.mkdir(parents=True, exist_ok=True)
+
+# Prepare the test dataset and data loader (similar to training)
+test_dataset = CustomDataset(image_dir=test_image_dir, mask_dir=output_dir, transform=transform)
+test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False)
+
+# Perform inference and save the results
+with torch.no_grad():
+    for i, data in enumerate(test_dataloader):
+        inputs, _ = data  # Assuming you only have test images, no masks
+
+        # Move inputs to the device (GPU/CPU) if needed
+        inputs = inputs.to(device)
+
+        # Forward pass
+        outputs = model(inputs)
+
+        # Convert model outputs to images (PIL format) if needed
+        output_images = []
+        for j in range(outputs.shape[0]):
+            output_image = outputs[j].squeeze().cpu().numpy() * 255  # Assuming the output is in the range [0, 1]
+            output_image = Image.fromarray(output_image.astype('uint8'))
+            output_images.append(output_image)
+
+        # Save the output images
+        for j, output_image in enumerate(output_images):
+            output_path = output_dir / f'result_{i * batch_size + j}.png'
+            output_image.save(output_path)
+
+print("Testing and saving results complete.")
