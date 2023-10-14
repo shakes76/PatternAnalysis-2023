3acdf97210a091740fce7589232d6bb36fabf9fd
Michael <m.hansen@uqconnect.edu.au>
2023-10-15 01:07:49 +1000

    Added function that builds model and got it to compile.

--- recognition/s4428306_UNET/modules.py
+++ recognition/s4428306_UNET/modules.py
@@ -1,5 +1,6 @@
 #Code in this section is based on code from:
 #https://pyimagesearch.com/2022/02/21/u-net-image-segmentation-in-keras/
+#TODO: Reference both papers. (Improved UNet and the segmentation layer one.)
 
 import tensorflow as tf
 from tensorflow import keras
@@ -35,7 +36,7 @@ def localization_module(x, conv_features, n_filters):
     x = layers.Conv2D(n_filters, 1, padding="same", activation="relu", kernel_initializer="he_normal")(x)
     return x
 
-#NOTE: From what I can see from the papers this just a 1x1 (or 1x1x1) convolution.
+#NOTE: From what I can see from the papers this is just a 1x1 (or 1x1x1) convolution.
 #      Should the papers be referenced somewhere?
 #      Going to define it like this for readability.
 def segmentation_layer(x, n_filters):
@@ -44,3 +45,43 @@ def segmentation_layer(x, n_filters):
     x = layers.Conv2D(n_filters, 1, padding="same", activation="relu", kernel_initializer="he_normal")(x)
     return x
 
+def build_improved_unet_model():
+    #TODO: Write specificatoin.
+    inputs = layers.Input(shape=(128,128,3))
+    #TODO: Name this path. (down?)
+    #NOTE: Again, unsure if these should be 2D or 3D.
+    x = layers.Conv2D(16, 3, padding="same", activation="relu", kernel_initializer="he_normal")(inputs)
+    c1 = context_module(x, 16)
+    x = layers.Conv2D(32, 3, padding="same", strides=2, activation="relu", kernel_initializer="he_normal")(c1)
+    c2 = context_module(x, 32)
+    x = layers.Conv2D(64, 3, padding="same", strides=2, activation="relu", kernel_initializer="he_normal")(c2)
+    c3 = context_module(x, 64)
+    x = layers.Conv2D(128, 3, padding="same", strides=2, activation="relu", kernel_initializer="he_normal")(c3)
+    c4 = context_module(x, 128)
+    x = layers.Conv2D(256, 3, padding="same", strides=2, activation="relu", kernel_initializer="he_normal")(c4)
+    x = context_module(x, 256)
+    #TODO: Name this path. (up?)
+    x = upsampling_module(x, 128)
+    x = localization_module(x, c4, 128)
+    x = upsampling_module(x, 64)
+    l1 = localization_module(x, c3, 64)
+    x = upsampling_module(l1, 32)
+    l2 = localization_module(x, c2, 32)
+    x = upsampling_module(l2, 16)
+    x = layers.concatenate([x, c1])
+    x = layers.Conv2D(32, 3, padding="same", activation="relu", kernel_initializer="he_normal")(x)
+    #NOTE: Unsure how many filters segmentation layers should have.
+    #      It looks like they all need to be the same for them to add together.
+    #      Setting them all to 1 so that the output matches the masks.
+    x = segmentation_layer(x, 1)
+    #Upscale and add segmentation layers.
+    s1 = segmentation_layer(l1, 1)
+    s1 = layers.UpSampling2D(size=(2,2))(s1)
+    s2 = segmentation_layer(l2, 1) + s1
+    s2 = layers.UpSampling2D(size=(2,2))(s2)
+    x = x + s2
+    outputs = layers.Softmax(axis=3)(x)
+    #NOTE: A different model will need to be returned once the new class is set up and training is written.
+    return tf.keras.Model(inputs, outputs, name="Improved U-Net")
+
+#TODO: Write new model and training function.
