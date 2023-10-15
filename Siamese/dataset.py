bf3a8cc7771e3e727e124a642df5d5d7d5f22b41
FanHaoZeng <1992344969@qq.com>
2023-10-16 07:40:11 +1000

    Add some new function for train val splite

--- Siamese/dataset.py
+++ Siamese/dataset.py
@@ -14,14 +14,11 @@ class ADNIDataset(Dataset):
         self.ad_path = os.path.join(data_path, 'AD')
         self.nc_path = os.path.join(data_path, 'NC')
 
-        self.ad_images = [self.transform(Image.open(os.path.join(self.ad_path, img))) for img in
-                          os.listdir(self.ad_path)]
-        self.nc_images = [self.transform(Image.open(os.path.join(self.nc_path, img))) for img in
-                          os.listdir(self.nc_path)]
+        self.ad_images_list = [Image.open(os.path.join(self.ad_path, img)) for img in os.listdir(self.ad_path)]
+        self.nc_images_list = [Image.open(os.path.join(self.nc_path, img)) for img in os.listdir(self.nc_path)]
 
-        self.ad_images = torch.stack(self.ad_images)
-        self.nc_images = torch.stack(self.nc_images)
-
+        self.ad_images = torch.stack([transforms.ToTensor()(img) for img in self.ad_images_list])
+        self.nc_images = torch.stack([transforms.ToTensor()(img) for img in self.nc_images_list])
 
     def __len__(self):
         return min(len(self.ad_images), len(self.nc_images))
@@ -39,6 +36,33 @@ class ADNIDataset(Dataset):
             label = torch.tensor(0, dtype=torch.float)
 
         return img1, img2, label
+
+
+def get_patient_ids(dataset):
+    patient_ids = set()
+    for img_path in dataset:  # 这里你需要遍历你的图像文件路径
+        patient_id = img_path.split('_')[0]  # 提取病人 ID，假设它在文件名的第一个字段
+        patient_ids.add(patient_id)
+    return list(patient_ids)
+
+
+def split_patient_ids(patient_ids, val_ratio=0.2):
+    num_val = int(len(patient_ids) * val_ratio)
+    return patient_ids[num_val:], patient_ids[:num_val]
+
+
+def get_indices_from_patient_ids(patient_ids, dataset):
+    indices = []
+    for index in range(len(dataset)):
+        img1, img2, _ = dataset[index]
+        # 提取病人 ID 的逻辑。这里假设 img1 和 img2 的文件名中包含病人 ID。
+        # 你需要根据你的具体情况进行修改。
+        patient_id1 = img1.split('_')[0]
+        patient_id2 = img2.split('_')[0]
+
+        if patient_id1 in patient_ids or patient_id2 in patient_ids:
+            indices.append(index)
+    return indices
 
 
 def get_train_dataset(data_path):
--- Siamese/train.py
+++ Siamese/train.py
@@ -3,9 +3,10 @@ import os
 import torch
 import torch.optim as optim
 from torch import nn
-from torch.utils.data import DataLoader
+from torch.utils.data import DataLoader, Subset
 from modules import SiameseNetwork
-from dataset import get_train_dataset
+from dataset import get_train_dataset, get_patient_ids, split_patient_ids, get_indices_from_patient_ids
+from sklearn.model_selection import train_test_split
 
 
 def train(model, dataloader, device, optimizer, epoch):
@@ -29,9 +30,48 @@ def train(model, dataloader, device, optimizer, epoch):
                        100. * batch_idx / len(dataloader), loss.item()))
 
 
-def validate(model, dataloader, criterion):
-    # TODO: Implement validation logic for one epoch
-    pass
+def validate(model, dataloader, device):
+    print("Starting Validation.")
+    model.eval()
+    test_loss = 0
+    correct = 0
+
+    criterion = nn.BCELoss()
+
+    with torch.no_grad():
+        for (images_1, images_2, targets) in dataloader:
+            images_1, images_2, targets = images_1.to(device), images_2.to(device), targets.to(device)
+            outputs = model(images_1, images_2).squeeze()
+            test_loss += criterion(outputs, targets).sum().item()  # sum up batch loss
+            pred = torch.where(outputs > 0.5, 1, 0)  # get the index of the max log-probability
+            correct += pred.eq(targets.view_as(pred)).sum().item()
+
+    test_loss /= len(dataloader.dataset)
+
+    print('\nVal set: Average loss: {:.4f}, Validation Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(dataloader.dataset),
+        100. * correct / len(dataloader.dataset)))
+
+    print("Finished Validation.")
+
+
+def split_dataset_by_patient(dataset):
+    patient_img_indices = {}
+    for i in range(len(dataset)):
+        _, _, img_path = dataset[i]  # 假设第三个返回值是图像路径
+        patient_id = img_path.split('_')[0]
+        if patient_id not in patient_img_indices:
+            patient_img_indices[patient_id] = []
+        patient_img_indices[patient_id].append(i)
+
+    all_patient_ids = list(patient_img_indices.keys())
+    train_patient_ids, val_patient_ids = train_test_split(all_patient_ids, test_size=0.2, random_state=42)
+
+    train_indices = [idx for patient_id in train_patient_ids for idx in patient_img_indices[patient_id]]
+    val_indices = [idx for patient_id in val_patient_ids for idx in patient_img_indices[patient_id]]
+
+    return train_indices, val_indices
+
 
 
 if __name__ == '__main__':
@@ -51,13 +91,33 @@ if __name__ == '__main__':
     train_dataloader = torch.utils.data.DataLoader(train_data, batch_size=batch_size)
     print("Data loaded.")
 
+    # # 创建完整的数据集
+    # full_dataset = get_train_dataset('E:/comp3710/AD_NC')
+    #
+    # # 获取病人 ID 并进行拆分
+    # patient_ids = get_patient_ids(full_dataset)
+    # train_patient_ids, val_patient_ids = split_patient_ids(patient_ids)
+    #
+    # # 根据病人 ID 获取数据集索引
+    # train_indices = get_indices_from_patient_ids(train_patient_ids, full_dataset)
+    # val_indices = get_indices_from_patient_ids(val_patient_ids, full_dataset)
+    #
+    # # 使用 Subset 创建训练和验证数据集
+    # train_dataset = Subset(full_dataset, train_indices)
+    # val_dataset = Subset(full_dataset, val_indices)
+    #
+    # # 创建 DataLoader
+    # train_loader = DataLoader(train_dataset, batch_size=batch_size)
+    # val_loader = DataLoader(val_dataset, batch_size=batch_size)
+
+    save_directory = "E:/PatternAnalysis-2023/results"
+    save_filename = f"siamese_network_{epochs}epochs(3).pt"
+
     print("Starting training.")
     for epoch in range(1, epochs + 1):
         train(model, train_dataloader, device, optimizer, epoch)
+        # validate(model, val_loader, device)
     print("Finished training.")
-
-    save_directory = "E:/PatternAnalysis-2023/results"
-    save_filename = f"siamese_network_{epochs}epochs(2).pt"
 
     # 创建保存目录，如果它不存在
     if not os.path.exists(save_directory):
