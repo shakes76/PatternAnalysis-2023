63d176025bbf91a594e570cb5d7b7c7b74c22522
EthanGTJones <116948347+EthanGTJones@users.noreply.github.com>
2023-10-16 05:06:59 +1000

    Created StyleGAN class in train.py
    Added StyleGAN class to train.py. In the StyleGAN class, I have added the functions: setup() and metrics()

--- recognition/OASIS brain StyleGAN - 44829531/train.py
+++ recognition/OASIS brain StyleGAN - 44829531/train.py
@@ -6,3 +6,38 @@ Student ID: 44829531
 COMP3710 OASIS brain StyleGAN project
 Semester 2, 2023
 """
+
+import os
+import tensorflow as tf
+from tensorflow import keras
+from dataset import load_data
+from modules import Generator, Discriminator
+
+LEARNING_RATE = 0.0001
+
+
+class StyleGAN(keras.Model):
+    def __init__(self):
+        super(StyleGAN, self).__init__()
+        self.generator = Generator()
+        self.discriminator = Discriminator()
+
+    def setup(self):
+        super(StyleGAN, self).setup()
+
+        # initialise the optimisers
+        self.generator_optimizer = tf.keras.optimizers.Adam(LEARNING_RATE)
+        self.discriminator_optimizer = tf.keras.optimizers.Adam(LEARNING_RATE)
+
+        # initialise the loss function
+        self.loss = tf.keras.losses.BinaryCrossentropy()
+
+        # initialise the metrics
+        self.generator_loss_metric = \
+            tf.keras.metrics.Mean(name="generator_loss")
+        self.discriminator_loss_metric = \
+            tf.keras.metrics.Mean(name="discriminator_loss")
+
+    @property
+    def metrics(self):
+        return [self.generator_loss_metric, self.discriminator_loss_metric]
