4907a4a553167a0182fab8108c3bdcd735fbbfa7
Indira Devi <indiradevi@d-i89-186-77.student.eduroam.uq.edu.au>
2023-10-15 16:53:26 +1000

    Change from using custom dataset to ImageFolder

--- recognition/ADNI_VQVAE_47670668/dataset.py
+++ recognition/ADNI_VQVAE_47670668/dataset.py
@@ -1,94 +1,50 @@
-from torch.utils.data import Dataset
 from torchvision import transforms
+from torchvision.datasets import ImageFolder
 
-import os
-from PIL import Image
-from torchvision.transforms.functional import to_pil_image
+import matplotlib.pyplot as plt
+from torchvision.utils import make_grid
 
+# Initialize constant variables for dataset creation and loading
+TRAIN_DATASET_PATH = './AD_NC/train'
+TEST_DATASET_PATH = './AD_NC/test'
 
-class ADNIDataset(Dataset):
-  """ADNI dataset"""
-
-  def __init__(self, directories, transform=None):
-        self.image_paths = []
-        self.labels = []
-
-        # Assign a label to each directory (0 for AD, 1 for NC)
-        for i, directory in enumerate(directories):
-          for filename in os.listdir(directory):
-              if filename.endswith('.jpeg'):
-                  self.image_paths.append(os.path.join(directory, filename))
-                  self.labels.append(i)
-
-        self.transform = transform
-
-  def __len__(self):
-      return len(self.image_paths)
-
-  def __getitem__(self, idx):
-      image = Image.open(self.image_paths[idx])
-      label = self.labels[idx]
-
-      if self.transform:
-        image = self.transform(image)  
-
-      return image, label
-  
-def preprocess_and_save(original_directories, preprocessed_directories):
-    for orig_dir, prep_dir in zip(original_directories, preprocessed_directories):
-        if not os.path.exists(prep_dir):
-            os.makedirs(prep_dir)
-        for filename in os.listdir(orig_dir):
-            if filename.endswith('.jpeg'):
-                # Load the image
-                image_path = os.path.join(orig_dir, filename)
-                image = Image.open(image_path)
-                
-                # Apply transformations
-                transformed_image = transform(image)
-                
-                # Save the transformed image
-                save_path = os.path.join(prep_dir, filename)
-                to_pil_image(transformed_image).save(save_path)
-
-  
+BATCH_SIZE = 32
 
 transform = transforms.Compose([
+    # Convert images to tensor
     transforms.ToTensor(),
+
+    # Rotates the image by a random angle between -15 to +15 degrees.
     transforms.RandomRotation(degrees=15),
+
+    # With a probability of 50% (p=0.5), flips the image horizontally.
     transforms.RandomHorizontalFlip(p=0.5),
+
+    # With a probability of 50% (p=0.5), flips the image vertically.
     transforms.RandomVerticalFlip(p=0.5),
+
+    # Crops the image to a size of 224x224 pixels.
     transforms.RandomResizedCrop(size=(224, 224), scale=(0.5, 1.0)),
+
+    # Randomly changes the brightness, contrast, saturation, and hue of the image.
     transforms.ColorJitter(brightness=0.1, contrast=0.1, saturation=0.1, hue=0.1),
 ])
 
+# Use image folder due to directory structure of dataset
+train_dataset = ImageFolder(TRAIN_DATASET_PATH, transform=transform)
+test_dataset = ImageFolder(TEST_DATASET_PATH, transform=transform)
 
-train_dataset_paths = ['/content/drive/MyDrive/dataset/AD_NC/train/AD',
-                       '/content/drive/MyDrive/dataset/AD_NC/train/NC'
-                    ]
-test_dataset_paths = ['/content/drive/MyDrive/dataset/AD_NC/test/AD',
-                       '/content/drive/MyDrive/dataset/AD_NC/test/NC'
-                    ]
+# Visualize first 16 training images
+def imshow(img_tensor):
+    img = img_tensor.numpy().transpose((1, 2, 0))
+    plt.imshow(img)
+    plt.axis('off')
+    plt.show()
 
+# Get a batch of images
+num_images_to_display = 16
+images, labels = zip(*[train_dataset[i] for i in range(num_images_to_display)])
 
-# Define directories to save preprocessed images
-preprocessed_train_paths = [
-    '/content/drive/MyDrive/dataset/AD_NC_preprocessed/train/AD',
-    '/content/drive/MyDrive/dataset/AD_NC_preprocessed/train/NC'
-]
-
-preprocessed_test_paths = [
-    '/content/drive/MyDrive/dataset/AD_NC_preprocessed/test/AD',
-    '/content/drive/MyDrive/dataset/AD_NC_preprocessed/test/NC'
-]
-
-# Ran once to create the directory and images
-# preprocess_and_save(train_dataset_paths, preprocessed_train_paths)
-# preprocess_and_save(test_dataset_paths, preprocessed_test_paths)
-
-
-transform = transforms.Compose([
-        transforms.ToTensor()
-])
-train_dataset = ADNIDataset(train_dataset_paths, transform=transform)
-test_dataset = ADNIDataset(test_dataset_paths, transform=transform)
+# Convert images to a grid and display
+grid = make_grid(list(images), nrow=4)
+imshow(grid)
