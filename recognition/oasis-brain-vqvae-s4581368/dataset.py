049a9c93851bde20f32614203341c87b1ed646a6
Ryan <ryan.ward17@outlook.com>
2023-10-15 16:49:24 +1000

    added residual block definition, and outlined encoder/decoder classes.

--- recognition/oasis-brain-vqvae-s4581368/dataset.py
+++ recognition/oasis-brain-vqvae-s4581368/dataset.py
@@ -1,1 +1,46 @@
 # Dataset loader for the OASIS brain dataset
+import os
+import torch
+import torchvision
+import torchvision.transforms as transforms
+from torch.utils.data import DataLoader
+import matplotlib.pyplot as plt
+# Data Parameters
+
+test_path = "./keras_png_slices_data/"
+train_path = "./keras_png_slices_data/" 
+validate_path = "./keras_png_slices_data/"
+
+
+def data_loaders(train_path, test_path, validate_path, batch_size=32):
+
+    # TODO: Include a composed transform, and split out dataset into train test and validate. For now just
+    # batch all data together
+    train_dataset = torchvision.datasets.ImageFolder(train_path, transform=transforms.ToTensor())
+    train_dataloader = DataLoader(train_dataset[2], batch_size=batch_size, shuffle=True)
+
+    # Test Dataset and DataLoader
+    test_dataset = torchvision.datasets.ImageFolder(test_path, transform=transforms.ToTensor())
+    test_dataloader = DataLoader(test_dataset, batch_size=batch_size, shuffle=True)
+
+    # Validation Dataset and DataLoader
+    validation_dataset = torchvision.datasets.ImageFolder(validate_path)
+    validation_dataloader = DataLoader(validation_dataset, batch_size=batch_size, shuffle=True)
+
+    return train_dataloader
+
+def see_data(data_loader):
+    # Display image and label.
+    train_features, train_labels = next(iter(data_loader))
+    print(f"Feature batch shape: {train_features.size()}")
+    print(f"Labels batch shape: {train_labels.size()}")
+    img = train_features[0].squeeze()
+    img = torch.transpose(img, 0, 2)
+    label = train_labels[0]
+    plt.imshow(img)
+    plt.show()
+    print(f"Label: {label}")
+
+train = data_loaders(train_path, test_path, validate_path)
+see_data(train)
+
--- recognition/oasis-brain-vqvae-s4581368/modules.py
+++ recognition/oasis-brain-vqvae-s4581368/modules.py
@@ -1,1 +1,48 @@
 # VQVAE for the OASIS Brain Dataset
+
+import torch
+
+from torch import nn
+from torch.nn import funcional as F
+
+class ResidualBlock(nn.Module):
+    """
+    Residual Block definition for the Encoder and Decoder of the VAE
+    """
+    def __init__(self, in_channels, out_channels, residual_hidden_layers):
+        super(ResidualBlock, self).__init__()
+        self._res_block = nn.Sequential(
+            nn.ReLU(True),
+            nn.Conv2d(
+                in_channels=in_channels,
+                out_channels=residual_hidden_layers,
+                kernel_size=3,
+                stride=1,
+                padding=1,
+                bias=False
+            ),
+            nn.ReLU(True),
+            nn.Conv2d(
+                in_channels=residual_hidden_layers,
+                out_channels=out_channels,
+                kernel_size=1,
+                stride=1,
+                bias=False
+            )
+        )
+    
+    def forward(self, x):
+        return x + self._res_block(x)
+
+class Enconder(nn.Module):
+    """
+    Encoder structer for VAE.
+    """
+    def __init__(self, in_channels, hidden_layers, downsampling_layers,
+                 residual_layers, residual_hidden_layers)
+
+
+class Decoder(nn.Module):
+    pass
+
+
