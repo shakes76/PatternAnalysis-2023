ade1fa954728399a51046d03cbe9c85baad02c32
siewhui2023 <csh_1020@protonmail.com>
2023-10-15 17:57:07 +1000

    Fixed saving errors in train.py

--- topic-recognition/Siamese-network/train.py
+++ topic-recognition/Siamese-network/train.py
@@ -1,23 +1,23 @@
 import matplotlib.pyplot as plt
-from modules import plot_data
 from modules import siamese_network
-from modules import classification_model
 from modules import contrastive_loss
-from dataset import load_siamese_data
-from dataset import load_classify_data
+from modules import classification_model
+from dataset import load_train_data
 from tensorflow.keras.models import load_model
 
 save_trained_snn = '/Users/jollylogan/TryTry/SNN.h5'
+save_trained_classifier = '/Users/jollylogan/TryTry/Classifier.h5'
 
 def train_and_plot_SNN(epochs=10):
     # Load Siamese model data
-    siamese_train, siamese_val = load_siamese_data()
+    siamese_train, siamese_val = load_train_data()
 
     # Create Siamese model
     model = siamese_network(128, 128)
 
     # Train the model
     siamese_fit = model.fit(siamese_train, epochs=epochs, validation_data=siamese_val)
+    model.save(save_trained_snn)
 
     # Plot the fit data
     plt.figure()
@@ -42,19 +42,20 @@ def train_and_plot_SNN(epochs=10):
 
 def train_and_plot_classifier(epochs=10):
     # Load classification model data
-    classify_train, classify_val = load_classify_data(testing=False)
+    classify_train, classify_val = load_train_data(testing=False)
 
     # Load the trained Siamese model
     siamese_model = load_model(save_trained_snn, custom_objects={'contrastive_loss': contrastive_loss})
     
     # Get the CNN layer from the siamese model
-    subnet = siamese_model.get_layer(name="cnn")
+    cnn = siamese_model.get_layer(name="cnn")
 
     # Create classification model using the subnet
-    classifier = classification_model(subnet)
+    classifier = classification_model(cnn)
 
     # Train the classification model
     classifier_fit = classifier.fit(classify_train, epochs=epochs, validation_data=classify_val)
+    classifier.save(save_trained_classifier)
 
     # Plot the fit data
     plt.figure()
@@ -69,7 +70,7 @@ def train_and_plot_classifier(epochs=10):
     plt.plot(classifier_fit.history['loss'], label='loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
-    plt.ylim([0, 1]) # Adjust as needed
+    plt.ylim([0, 1]) 
     plt.legend(loc='lower right')
 
     plt.show()
@@ -79,5 +80,6 @@ def train_and_plot_classifier(epochs=10):
 
 def train():
     siamese_model, siamese_fit = train_and_plot_SNN()
-    classifier_model, classifier_fit = train_and_plot_classifier()    
+    classifier_model, classifier_fit = train_and_plot_classifier()
+        
 train()
