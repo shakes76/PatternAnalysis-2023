35d245a7c6523e9337b4d6c85f3747781abb88c0
Deven Bhasin <d.bhasin@uqconnect.edu.au>
2023-10-15 22:37:09 +1000

    basic dataset code

--- dataset.py
+++ dataset.py
@@ -1,1 +1,14 @@
-import sklearn
+import os
+import torch
+from torchvision import transforms
+from torch.utils.data import DataLoader, Dataset
+
+class CustomDataset(Dataset):
+    def __init__(self, data_dir, transform=None):
+        return
+
+    def __len__(self):
+        pass
+
+    def __getitem__(self, index):
+        pass
--- modules.py
+++ modules.py
@@ -1,11 +1,9 @@
 import torch
 import torch.nn as nn
 
-class YourModel(nn.Module):
-    def __init__(self, input_size, num_classes):
-        super(YourModel, self).__init__()
-        # Define your model architecture here
+class UNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNet, self).__init__()
 
     def forward(self, x):
-        # Define the forward pass of your model
         pass
