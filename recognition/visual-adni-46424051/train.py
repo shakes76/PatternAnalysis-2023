2d8c3ea25de71a0c6152b696fd263a036419d123
Joseph Churchill <j.churchill@uq.net.au>
2023-10-15 15:16:06 +1000

    model

--- recognition/visual-adni-46424051/train.py
+++ recognition/visual-adni-46424051/train.py
@@ -12,9 +12,9 @@ import matplotlib.pyplot as plt
 
 class Train():
     def __init__(self):
-        self.net = Model(shape=(1, 105, 105), patches=7, hidden_dim=8, blocks=2, heads=2, out_dim=10)
+        self.net = Model(shape=(1, 28, 28), patches=7, hidden_dim=8, blocks=2, heads=2, out_dim=2)
         self.transforms = transforms.Compose([
-            transforms.Resize((105,105)),
+            transforms.Resize((28,28)),
             transforms.ToTensor()
         ])
         self.criterion = CrossEntropyLoss()
@@ -31,27 +31,26 @@ class Train():
         for epoch in range(constants.epochs):
             losses = 0.0
             for i, (x, y) in enumerate(self.trainLoader):
+                print("image: ", x)
                 if constants.cuda:
                     x, y = x.cuda(), y.cuda()
                 y1 = self.net(x)
-                loss = self.criterion(y1, y)
-                losses += loss.detach().cpu().item() / len(self.train_loader)
+                # loss = self.criterion(y1, y)
+                # losses += loss.detach().cpu().item() / len(self.trainLoader)
                 self.optimiser.zero_grad()
-                loss.backward()
-                self.optimizer.step()
-                print(f'Image at index {i} is type {y}')
+                # loss.backward()
+                self.optimiser.step()
+                print(f'Image at index {i} is type {y} and guess is type {y1}')
             print(f'Loss at epoch {epoch} is {losses}')
     
     def test(self):
         with torch.no_grad():
-            correct, total = 0.0
+            correct, total = 0.0, 0.0
             for i, (x, y) in enumerate(self.testLoader):
                 if constants.cuda:
                     x, y = x.cuda(), y.cuda()
                 y1 = self.net(x)
-                correct += torch.sum(torch.argmax(y1, 1) == y).detach().cpu().item()
+                correct += torch.sum(torch.argmax(y1, dim=0) == y).detach().cpu().item()
                 total += len(x)
+                print(f'Image at index {i} is type {y} and guess is type {y1}')
             print(f'Accuracy is {correct / total * 100}%')
-
-
-        
