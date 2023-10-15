9b3674ba9dee2a7dd65f6ac492febd30fae08e5a
EthanGTJones <116948347+EthanGTJones@users.noreply.github.com>
2023-10-15 11:41:23 +1000

    Added Hyparparameters, Input file paths and Output paths
    While working on my local machine, I will be using my local path. This will need to be updated when being trained.

--- recognition/OASIS brain StyleGAN - 44829531/predict.py
+++ recognition/OASIS brain StyleGAN - 44829531/predict.py
@@ -6,3 +6,20 @@ Student ID: 44829531
 COMP3710 OASIS brain StyleGAN project
 Semester 2, 2023
 """
+
+import os
+
+# Hyparparameters
+EPOCHS = 5
+BATCH_SIZE = 32
+
+# Inputs
+INPUT_IMAGES_PATH = "C:\\Users\\ethan\\Desktop\\COMP3710\\keras_png_slices_train"
+# INPUT_IMAGES_PATH = "/home/groups/comp3710/OASIS/keras_png_slices_train"
+
+# Results
+RESULT_IMAGE_PATH = "figures"
+RESULT_WEIGHT_PATH = "figures"
+RESULT_IMAGE_COUNT = 3
+PLOT_LOSS = True
+
