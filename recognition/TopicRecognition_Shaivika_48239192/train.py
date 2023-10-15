3ef97519e0cc42ad42ebaa711a48587de81f2da1
Shaivika Anand <shaivika.anand@gmail.com>
2023-10-15 20:58:22 +1000

    final train.py

--- recognition/TopicRecognition_Shaivika_48239192/train.py
+++ recognition/TopicRecognition_Shaivika_48239192/train.py
@@ -3,12 +3,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from modules import create_classifier
 from dataset import load_and_preprocess_data
+from sklearn.model_selection import train_test_split
+from keras.utils import to_categorical
+import keras
 
 # Load and preprocess data
 Images, Labels = load_and_preprocess_data()
 
 # Split data into training and testing sets
-train_indices, test_indices = train_test_split(list(range(Images.shape[0]), train_size=0.8, test_size=0.2, shuffle=True))
+train_indices, test_indices = train_test_split(list(range(Images.shape[0])), train_size=0.8, test_size=0.2, shuffle=True)
+
+
 x_train = Images[train_indices]
 y_train = Labels[train_indices]
 x_test = Images[test_indices]
@@ -21,6 +26,7 @@ model.compile(
     loss=keras.losses.CategoricalCrossentropy(),
     metrics=[keras.metrics.CategoricalAccuracy(name="accuracy")],
 )
+
 # Train the model
 history = model.fit(x_train, y_train, batch_size=16, epochs=30, validation_split=0.25)
 
@@ -28,8 +34,10 @@ history = model.fit(x_train, y_train, batch_size=16, epochs=30, validation_split
 loss, accuracy = model.evaluate(x_test, y_test)
 print(f"Test loss: {round(loss, 2)}")
 print(f"Test accuracy: {round(accuracy * 100, 2)} %")
+
+
 # Save the trained model
-model.save('model/trained_model.h5')
+model.save('/content/drive/MyDrive/Colab Notebooks/ADNI/model.h5')
 
 # Plot training history
 plt.plot(history.history['accuracy'], label="train_acc")
