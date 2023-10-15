2d2755d74946d1eaddff5a502266bc9cc15fd189
Deven Bhasin <d.bhasin@uqconnect.edu.au>
2023-10-15 22:26:30 +1000

    Basic UNet Modules

---
+++ modules.py
@@ -0,0 +1,11 @@
+import torch
+import torch.nn as nn
+
+class YourModel(nn.Module):
+    def __init__(self, input_size, num_classes):
+        super(YourModel, self).__init__()
+        # Define your model architecture here
+
+    def forward(self, x):
+        # Define the forward pass of your model
+        pass
