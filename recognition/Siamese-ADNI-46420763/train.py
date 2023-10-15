03975a104a0e4ee4b8927888f45a86ff8cae30de
Aaron <>
2023-10-15 11:03:20 +1000

    ADD epoch train accuracy calculation

--- recognition/Siamese-ADNI-46420763/train.py
+++ recognition/Siamese-ADNI-46420763/train.py
@@ -27,12 +27,12 @@ if not torch.cuda.is_available():
 NUM_EPOCHS = 35 # 200
 LEARNING_RATE = 0.1 # 0.00005
 WEIGHT_DECAY = 1e-5
-BATCH_SIZE = 32
+BATCH_SIZE = 16
 
 # Model Parameters
 MODEL_NAME = "ADNI-SiameseNetwork-resnet"
 DATASET_DIR = "recognition/Siamese-ADNI-46420763/data/AD_NC"
-DATASET_DIR = "/home/groups/comp3710/ADNI/AD_NC"
+# DATASET_DIR = "/home/groups/comp3710/ADNI/AD_NC"
 LOAD_MODEL = False
 MODEL_DIR = None
 
@@ -55,21 +55,16 @@ def main():
         model.load_state_dict(torch.load(MODEL_DIR, map_location=device))
 
     # Criterion and Optimizer:
-    #criterion = losses.TripletMarginLoss(embedding_regularizer = LpRegularizer())
-    #criterion = losses.TripletMarginLoss()
-    criterion = losses.TripletMarginLoss(distance = CosineSimilarity(), reducer = ThresholdReducer(high=0.3))
-    # criterion = losses.TripletMarginLoss(margin=0.2, distance = CosineSimilarity(), reducer = ThresholdReducer(high=0.3))
-    # criterion = losses.TripletMarginLoss(reducer = ThresholdReducer(high=0.3)) # NEW
-    # criterion = losses.TripletMarginLoss(reducer = ThresholdReducer(high=0.3))
-    miner = miners.MultiSimilarityMiner()
-    # miner = miners.BatchHardMiner() # NEW
-    # distance = distances.LpDistance()
-    # optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE, weight_decay=WEIGHT_DECAY) 
+    # distance = CosineSimilarity()
+    distance = distances.LpDistance()
+    criterion = losses.TripletMarginLoss(distance = distance, reducer = ThresholdReducer(high=0.3))
+    # miner = miners.MultiSimilarityMiner()
+    miner = miners.BatchHardMiner()
     optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=0.9, weight_decay=WEIGHT_DECAY)
     scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=LEARNING_RATE, steps_per_epoch=len(train_dataloader), epochs=NUM_EPOCHS)
-    # scheduler = torch.optim.lr_scheduler.CyclicLR(optimizer, max_lr=LEARNING_RATE, steps_per_epoch=len(train_dataloader), epochs=NUM_EPOCHS)
-    # scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer, T_0=, T_mult=, epochs=NUM_EPOCHS)
-
+    
+    # Logging Variables
+    
     ####################
     #   Train Model:   #
     ####################
@@ -78,6 +73,7 @@ def main():
     start = time.time()
     for epoch in range(NUM_EPOCHS):
         model.train()
+        total_correct = 0
         for i, (images, labels) in enumerate(train_dataloader):
             optimizer.zero_grad()
             
@@ -86,31 +82,46 @@ def main():
             
             # Forward pass
             embeddings = model(images)
-            hard_pairs = miner(embeddings, labels) # Finds pairs which are hard to distinguish - the negative sample is sufficiently closs to the anchor
+            # Finds pairs which are hard to distinguish - the negative sample is sufficiently closs to the anchor
+            hard_pairs = miner(embeddings, labels) 
             loss = criterion(embeddings, labels, hard_pairs)
 
+            # Calculate Accuracy:
+            positive_indices = hard_pairs[1]
+            negative_indices = hard_pairs[2]
+            
+            pos_sim = F.pairwise_distance(embeddings, embeddings[positive_indices], keepdim = True)
+            neg_sim = F.pairwise_distance(embeddings, embeddings[negative_indices], keepdim = True)
+            
+            # Find class with most similarity
+            sim = torch.stack([pos_sim, neg_sim], dim = 1)
+            pred = torch.Tensor.argmin(sim, dim = 1)
+
+            correct = (pred == 0).sum().item()
+            total_correct += correct
+                
             # Backward and optimize
             loss.backward()
             optimizer.step()     
+            scheduler.step()
             
             # Print batch accuracy and loss
             if (i % 100) == 0:
-                print("Epoch [{}/{}], Step [{}/{}]  loss: {:.5f}".format(
+                print("Epoch [{}/{}], Step [{}/{}]  loss: {:.5f} train acc: {:.2f}".format(
                     epoch+1, NUM_EPOCHS, 
                     i+1, total_step,
-                    loss.item()))
-            scheduler.step()
+                    loss.item(),
+                    correct/len(labels) * 100))
             
         #################  
         #   Validate:   #
         #################
         model.eval()
-        # random.seed(1337) # To maintain the same query across all random test samples
+        
+        # Get Class Queries
         q_class = -1
         while q_class != 0:
             AD_query, q_class = train_dataloader.dataset[random.randint(0, len(train_dataloader.dataset) - 1)]
-            
-        
         q_class = -1
         while q_class != 1:
             NC_query, q_class = train_dataloader.dataset[random.randint(0, len(train_dataloader.dataset) - 1)]
@@ -122,7 +133,6 @@ def main():
         AD_query_embedding = model(AD_query)
         NC_query_embedding = model(NC_query)
         
-        random.seed() # Get random seed
         model.eval()
         # Get random sample from train:
         total_valid_loss = 0.0
@@ -150,11 +160,7 @@ def main():
             # Find class with most similarity
             sim = torch.stack([AD_sim, NC_sim], dim = 1)
             pred = torch.Tensor.argmin(sim, dim = 1)
-            # print(sim)
-            # print(pred))
-            # print(sim)
-            # print(AD_query_batch.shape)
-            # print(embeddings.shape)
+
             correct = (labels[:, None] == pred).sum().item()
             total_correct += correct
             
