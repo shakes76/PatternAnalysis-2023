363b49441b61671b4dfe643aee6519fcd7953d05
MTbir <m.tieu@uqconnect.edu.au>
2023-10-15 22:10:28 +1000

    add load model to predict.py for later test run

--- recognition/Siamese_46419712/predict.py
+++ recognition/Siamese_46419712/predict.py
@@ -6,3 +6,21 @@ import torch.nn.functional as F
 import time
 import matplotlib.pyplot as plt
 import numpy as np
+
+# def load_model(model_type=0):
+#     if model_type == 0:
+#         PATH = SIAMESE_MODEL_SAVE_PATH
+#     else:
+#         PATH = CLASSIFIER_MODEL_SAVE_PATH
+
+#     if os.path.exists(PATH):
+#         load_model = torch.load(PATH)
+#         epoch = load_model['epochs']
+#         model = load_model['model']
+#         criterion = load_model['criterion']
+#         optimizer = load_model['optimizer']
+#         scheduler = load_model['scheduler']
+
+#         return epoch, model, criterion, optimizer, scheduler
+#     else:
+#         return None
