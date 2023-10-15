7d85f09d12329e06e4f1c3e189095a0f9fac7a54
Deven Bhasin <d.bhasin@uqconnect.edu.au>
2023-10-16 09:36:35 +1000

    Improved Unet Model

--- modules.py
+++ modules.py
@@ -1,48 +1,76 @@
 import torch
 import torch.nn as nn
+import torch.nn.functional as F
 
-class UNet(nn.Module):
+class ConvBlock(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(UNet, self).__init__()
-
-        # Contracting Path (Encoder)
-        self.encoder = nn.Sequential(
-            nn.Conv2d(in_channels, 64, kernel_size=3, padding=1),
+        super(ConvBlock, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.ReLU(inplace=True)
         )
+    
+    def forward(self, x):
+        return self.conv(x)
+    
+class UNetPlusPlus(nn.Module):
+    def __init__(self, in_channels, out_channels, num_levels=4):
+        super(UNetPlusPlus, self).__init__()
 
-        # Bottleneck
-        self.bottleneck = nn.Sequential(
-            nn.Conv2d(64, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(128, 128, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.MaxPool2d(kernel_size=2, stride=2)
-        )
+        self.num_levels = num_levels
 
-        # Expansive Path (Decoder)
-        self.decoder = nn.Sequential(
-            nn.Conv2d(128, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.ReLU(inplace=True),
-            nn.ConvTranspose2d(64, out_channels, kernel_size=2, stride=2)
-        )
+        # Define the encoder and decoder paths
+        self.encoders = nn.ModuleList()
+        self.decoders = nn.ModuleList()
+
+        for level in range(num_levels):
+            in_channels = in_channels if level == 0 else out_channels
+            out_channels *= 2
+
+            # Encoder block with convolution layers
+            encoder = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True)
+            )
+            self.encoders.append(encoder)
+
+            # Decoder block with upconvolution layers
+            decoder = nn.Sequential(
+                nn.ConvTranspose2d(out_channels, out_channels // 2, kernel_size=2, stride=2),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_channels, out_channels // 2, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True),
+                nn.Conv2d(out_channels // 2, out_channels // 2, kernel_size=3, padding=1),
+                nn.ReLU(inplace=True)
+            )
+            self.decoders.append(decoder)
+
+        # Final output convolution
+        self.final_conv = nn.Conv2d(out_channels, out_channels, kernel_size=1)
 
     def forward(self, x):
-        # Contracting Path
-        enc1 = self.encoder(x)
-        
-        # Bottleneck
-        bottle = self.bottleneck(enc1)
-        
-        # Expansive Path
-        dec1 = self.decoder(bottle)
+        features = []  # Store skip connections
 
-        return dec1
+        # Encoding path
+        for level in range(self.num_levels):
+            x = self.encoders[level](x)
+            if level < self.num_levels - 1:
+                features.append(x)  # Store skip connection
+
+        # Decoding path
+        for level in range(self.num_levels - 1, 0, -1):
+            x = self.decoders[level - 1](x)
+            x = torch.cat((x, features[level - 1]), dim=1)  # Concatenate skip connection
+            if level > 1:
+                x = self.decoders[level - 1](x)
+
+        # Final output
+        x = self.final_conv(x)
+        return x
 
 class DiceLoss(nn.Module):
     def __init__(self, smooth=1.0):
@@ -54,9 +82,3 @@ class DiceLoss(nn.Module):
         union = predicted.sum() + target.sum()
         dice = (2.0 * intersection + self.smooth) / (union + self.smooth)
         return 1 - dice  # We often minimize the loss, so return 1 - dice
-
-def dice_coefficient(predicted, target, smooth=1.0):
-    intersection = (predicted * target).sum()
-    union = predicted.sum() + target.sum()
-    dice = (2.0 * intersection + smooth) / (union + smooth)
-    return dice
