be60b782008432feb9bec4807d779fad23f442b6
ryanlederhose <ryanlederhose@hotmail.com>
2023-10-15 14:53:22 +1000

    modified validation loader to have patient-level split

--- recognition/vision_transformer_ryanlederhose/dataset.py
+++ recognition/vision_transformer_ryanlederhose/dataset.py
@@ -1,12 +1,12 @@
 from torch.utils.data import DataLoader as TorchDataLoader
 from torchvision.datasets import ImageFolder
 import torchvision.transforms as T
-from torch.utils.data import random_split
 
 class DataLoader(object):
     def __init__(self, batch_size=64) -> None:
         self.trainingFile = "AD_NC/train/"
         self.testFile = "AD_NC/test/"
+        self.validFile = "AD_NC/valid/"
         self.imageSize = 192
         self.batchSize = batch_size
         self.normalisation = (0.5, 0.5, 0.5), (0.5, 0.5, 0.5)
@@ -16,56 +16,35 @@ class DataLoader(object):
         
         self.load_training_data()
         self.load_test_data()
+        self.load_validation_data()
 
     def load_training_data(self):
         # Get training dataset from image folder
         train_images = ImageFolder(root=self.trainingFile, transform=T.Compose([T.Resize(self.imageSize), 
-                                    T.CenterCrop(self.imageSize), T.ToTensor(), T.Normalize(*self.normalisation)])
-                                    )
+                                    T.CenterCrop(self.imageSize), T.ToTensor(), T.Normalize(*self.normalisation)]))
         
         # Get data loader of training folder
-        original_loader = TorchDataLoader(train_images, batch_size=self.batchSize, 
+        self.trainloader = TorchDataLoader(train_images, batch_size=self.batchSize, 
                                  shuffle=True, num_workers=3, pin_memory=True)
-
-        # Define the split ratio or a fixed number of validation samples
-        split_ratio = 0.8
-
-        # Calculate the sizes for training and validation sets
-        total_samples = len(original_loader.dataset)
-        train_size = int(split_ratio * total_samples)
-        valid_size = total_samples - train_size
-
-        # Use the original DataLoader's dataset for splitting
-        train_dataset, valid_dataset = random_split(
-            original_loader.dataset, [train_size, valid_size]
-        )
-
-        # Create DataLoader for training dataset
-        self.trainloader = TorchDataLoader(
-            train_dataset,
-            batch_size=self.batchSize,
-            shuffle=True,  # You can shuffle the training data
-            num_workers=3  # Number of worker processes for loading data
-        )
-
-        # Create DataLoader for the validation dataset
-        self.validloader = TorchDataLoader(
-            valid_dataset,
-            batch_size=self.batchSize,
-            shuffle=False,  # Validation data shouldn't be shuffled
-            num_workers=3  # Number of worker processes for loading data
-        )
 
         
     def load_test_data(self):
-        # Get training dataset from image folder
+        # Get testing dataset from image folder
         test_images = ImageFolder(root=self.testFile, transform=T.Compose([T.Resize(self.imageSize), 
-                                    T.CenterCrop(self.imageSize), T.ToTensor(), T.Normalize(*self.normalisation)]),
-                                    )
+                                    T.CenterCrop(self.imageSize), T.ToTensor(), T.Normalize(*self.normalisation)]))
 
-        # Get training loader
+        # Get test loader
         self.testloader = TorchDataLoader(test_images, batch_size=self.batchSize, 
                                  shuffle=True, num_workers=3, pin_memory=True)
+    
+    def load_validation_data(self):
+         # Get validation dataset from image folder
+        valid_images = ImageFolder(root=self.validFile, transform=T.Compose([T.Resize(self.imageSize), 
+                                    T.CenterCrop(self.imageSize), T.ToTensor(), T.Normalize(*self.normalisation)]))
+
+        # Get validation loader
+        self.validloader = TorchDataLoader(valid_images, batch_size=self.batchSize, 
+                                 shuffle=True, num_workers=3, pin_memory=True)       
         
     def get_training_loader(self):
         return self.trainloader
@@ -75,4 +54,3 @@ class DataLoader(object):
     
     def get_valid_loader(self):
         return self.validloader
-
--- recognition/vision_transformer_ryanlederhose/train.py
+++ recognition/vision_transformer_ryanlederhose/train.py
@@ -4,6 +4,8 @@ from torch import optim
 from torch.hub import tqdm
 from dataset import DataLoader
 from modules import ViT
+from torch.optim import lr_scheduler
+import matplotlib.pyplot as plt
 
 class HyperParameters(object):
     '''
@@ -15,15 +17,15 @@ class HyperParameters(object):
 
     def __init__(self) -> None:
         self.patch_size = 16
-        self.latent_size = 768
+        self.latent_size = 128
         self.n_channels = 3
-        self.num_encoders = 16
-        self.num_heads = 12
+        self.num_encoders = 6
+        self.num_heads = 4
         self.dropout = 0.1
         self.num_classes = 2
-        self.epochs = 10
+        self.epochs = 60
         self.lr = 1e-3
-        self.weight_decay = 3e-2
+        self.weight_decay = 2e-3
         self.batch_size = 32
         self.dry_run = False
         self.hidden_size = 64
@@ -37,17 +39,18 @@ def check_accuracy(loader, model, device):
     model.eval()
 
     with torch.no_grad():
-        for x, y in loader:
-            x = x.to(device=device)
-            y = y.to(device=device)
+        for batch_idx, (data, targets) in enumerate(loader):
+            data = data.to(device=device)
+            targets = targets.to(device=device)
 
-            scores = model(x)
+            scores = model(data)
             _, predictions = scores.max(1)
-            num_correct += (predictions == y).sum()
+            num_correct += (predictions == targets).sum()
             num_samples += predictions.size(0)
 
     model.train()
-    return num_correct / num_samples
+    accuracy = num_correct / num_samples
+    return accuracy
 
 def train_model(args, train_loader, epoch, device, model, criterion, optimizer):
     '''
@@ -90,21 +93,35 @@ def validate_model(args, val_loader, model, epoch, device, criterion):
     num_samples = 0
     tk = tqdm(val_loader, desc="EPOCH" + "[VALID]" + str(epoch + 1) + "/" + str(args.epochs))
 
-    for t, (data, targets) in enumerate(tk):
-        data, targets = data.to(device), targets.to(device)
+    with torch.no_grad():
+        for t, (data, targets) in enumerate(tk):
+            data, targets = data.to(device), targets.to(device)
 
-        scores = model(data)
-        _, predictions = scores.max(1)
-        num_correct += (predictions == targets).sum()
-        num_samples += predictions.size(0)
-        loss = criterion(scores, targets)
+            scores = model(data)
+            _, predictions = scores.max(1)
+            num_correct += (predictions == targets).sum()
+            num_samples += predictions.size(0)
+            loss = criterion(scores, targets)
 
-        total_loss += loss.item()
-        tk.set_postfix({"Loss": "%6f" % float(total_loss / (t + 1))})
+            total_loss += loss.item()
+            tk.set_postfix({"Loss": "%6f" % float(total_loss / (t + 1))})
 
     print(f"Accuracy on validation set: {num_correct / num_samples *100:.2f}")
     model.train()
     return total_loss / len(val_loader)
+
+def plot_losses(train_losses, val_losses):
+    plt.plot(train_losses)
+    plt.title('Training Losses')
+    plt.xlabel('Epochs')
+    plt.ylabel('Cross Entropy Loss')
+    plt.show()
+
+    plt.plot(val_losses)
+    plt.title('Validation Losses')
+    plt.xlabel('Epochs')
+    plt.ylabel('Cross Entropy Loss')
+    plt.show()
 
 
 def main():
@@ -125,17 +142,22 @@ def main():
 
     model = ViT(args).to(device)
 
-    optimizer = optim.Adam(model.parameters(), lr=args.lr, weight_decay=args.weight_decay)
+    optimizer = optim.Adam(model.parameters(), lr=args.lr)
     criterion = nn.CrossEntropyLoss()
+    scheduler = lr_scheduler.ReduceLROnPlateau(optimizer, 'min', factor=0.8, min_lr=1e-8, 
+                                               verbose=True)
     model.train()
 
     for epoch in range(args.epochs):
         trainLoss = train_model(args, train_loader, epoch, device, model, criterion, optimizer)
         validLoss = validate_model(args, valid_loader, model, epoch, device, criterion)
+        scheduler.step(trainLoss)
         trainLossList.append(trainLoss)
         validLossList.append(validLoss)
-
+    
     print(f"Accuracy on test set: {check_accuracy(test_loader, model, device)*100:.2f}")
+    
+    plot_losses(trainLossList, validLossList)
 
 if __name__ == "__main__":
     main()
