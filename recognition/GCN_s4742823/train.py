73cb92c2aa0981dce100c1578decfa9b94354c75
tarpentine <tylercarpendale@hotmail.com>
2023-10-15 17:16:13 +1000

    Add t-SNE plotting to train.py, correctly load best model when testing in train.py, add classes array and change num_classes to reflect length of classes in train.py, import sklearn.manifold.TSNE and matplotlib.pyplot in train.py, correct in-line comment spacing and device print statement in train.py

--- recognition/GCN_s4742823/train.py
+++ recognition/GCN_s4742823/train.py
@@ -2,24 +2,27 @@ from dataset import load_data
 from modules import Model
 import torch
 from sklearn.metrics import accuracy_score
+from sklearn.manifold import TSNE
+import matplotlib.pyplot as plt
 
 print("PyTorch Version:", torch.__version__)
 
 # Device configuration
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print("Using ", str(device))
+print("Using", str(device))
 
 test_size = 0.1
 val_size = 0.1
 
 # load_data() can take a filepath, otherwise will use default filepath in method.
-data, features = load_data(test_size=test_size, val_size=val_size) # Get features to get their shape.
+data, features = load_data(test_size=test_size, val_size=val_size)  # Get features to get their shape.
 data = data.to(device)
 
 num_epochs = 500
 num_features = features.shape[1]  # 128 for default data
 hidden_dim = 64
-num_classes = 4  # politicians, governmental organizations, television shows and companies
+classes = ["Politicians", "Governmental Organisations", "Television Shows", "Companies"]
+num_classes = len(classes)
 learning_rate = 1e-2
 dropout_prob = 0.5
 
@@ -64,11 +67,24 @@ for epoch in range(num_epochs):
 
 # ----- Testing -----
 print("--- Testing ---")
+model.load_state_dict(best_model)
 model.eval()
 with torch.no_grad():
     out = model(data)
+    out_np = out.cpu().numpy()  # To be used for t-SNE
     _, predicted = torch.max(out[data.test_mask], 1)  # Get classes with the highest probablities (note that we only use test nodes).
     predicted = predicted.cpu().numpy()
     y_true = data.y[data.test_mask].cpu().numpy()
     accuracy = accuracy_score(y_true, predicted)
 print(f"Test Accuracy: {100 * accuracy:.2f}%")
+
+# Plotting t-SNE
+tsne = TSNE(n_components=2, perplexity=50)  # It was found that divergence did not converge before 50 perplexity
+transformed = tsne.fit_transform(out_np)
+
+plt.figure(figsize=(10, 8))
+for class_idx in range(num_classes):
+    plt.scatter(transformed[data.y == class_idx, 0], transformed[data.y == class_idx, 1], label=classes[class_idx])
+plt.legend()
+plt.title("t-SNE Plot")
+plt.savefig("tsne_plot.png")
