71083fc1033b477204fdeb3c1b966e635623ec79
Shaivika Anand <shaivika.anand@gmail.com>
2023-10-15 21:42:59 +1000

    edited tran.py

--- recognition/TopicRecognition_Shaivika_48239192/train.py
+++ recognition/TopicRecognition_Shaivika_48239192/train.py
@@ -13,7 +13,6 @@ Images, Labels = load_and_preprocess_data()
 # Split data into training and testing sets
 train_indices, test_indices = train_test_split(list(range(Images.shape[0])), train_size=0.8, test_size=0.2, shuffle=True)
 
-
 x_train = Images[train_indices]
 y_train = Labels[train_indices]
 x_test = Images[test_indices]
