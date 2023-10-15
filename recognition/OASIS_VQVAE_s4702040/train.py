c9e3d1d0cc4abe8b13e1abd7e5c15c3b81dac156
JRSantos4742 <J.R.Santos@outlook.com>
2023-10-15 21:40:45 +1000

    Added dice loss, added resize transform and cleaned up code
    Added dice loss function for training loop sourced from https://github.com/pytorch/pytorch/issues/1249#issuecomment-305088398. Also removed unused import libraries. Changed the printing of the loss in an epoch to every 10 steps rather than every 100 as there are only 163 total steps. Changed batch size to 16 so that it can run locally on my machine without running out of memory. Added transform to resize images to 720x720 so that the dimensions are the same across all images.

--- recognition/OASIS_VQVAE_s4702040/train.py
+++ recognition/OASIS_VQVAE_s4702040/train.py
@@ -1,13 +1,6 @@
 import torch
-from torch import nn
-from torch.nn import functional as F
-import torchvision
 import torchvision.transforms as transforms
 import time
-import numpy as np
-from PIL import Image
-import os
-import matplotlib.pyplot as plt
 from modules import ImprovedUNet
 from dataset import CustomDataset
 
@@ -19,17 +12,16 @@ if not torch.cuda.is_available():
 # Hyper-parameters
 num_epochs = 1
 learning_rate = 5e-3
-channels = 10
 
 #--------------
 #Data
-transform_train = transforms.Compose([transforms.ToTensor()])
+transform_train = transforms.Compose([transforms.ToTensor(), transforms.Resize((720, 720))])
 transform_test = transforms.Compose([transforms.ToTensor()])
 
 
 trainset = CustomDataset('C:\\Users\\JRSan\\Downloads\\ISIC2018\\ISIC2018_Task1-2_Training_Input_x2',
                          "C:\\Users\\JRSan\\Downloads\\ISIC2018\\ISIC2018_Task1_Training_GroundTruth_x2" ,transform=transform_train)
-train_loader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=16, shuffle=True)
 total_step = len(train_loader)
 
 #Training
@@ -40,8 +32,16 @@ model = model.to(device)
 print("Model No. of Parameters:", sum([param.nelement() for param in model.parameters()]))
 print(model)
 
-#criterion = nn.CrossEntropyLoss()
-criterion = nn.MSELoss()
+def dice_loss(input, target):
+    smooth = 1.
+
+    iflat = input.view(-1)
+    tflat = target.view(-1)
+    intersection = (iflat * tflat).sum()
+    
+    return 1 - ((2. * intersection + smooth) /
+              (iflat.sum() + tflat.sum() + smooth))
+
 optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate, momentum=0.9, weight_decay=5e-4)
 
 #Piecewise Linear Schedule
@@ -52,31 +52,31 @@ scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer, max_lr=learning_rate,
 # Train the model
 model.train()
 print("> Training")
+lossList = []
 start = time.time() #time generation
 for epoch in range(num_epochs):
+    lossAvg = 0
     for i, (images, masks) in enumerate(train_loader): #load a batch
         images = images.to(device)
         masks = masks.to(device)
 
-        print(images.dim())
-
-        images = images.permute(1, 0, 2, 3)
-        masks = masks.permute(1, 0, 2, 3)
-
         # Forward pass
         outputs = model(images)
-        loss = criterion(outputs, masks)
+
+        loss = dice_loss(outputs, masks)
+        lossAvg += loss
 
         # Backward and optimize
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
 
-        if (i+1) % 5 == 0:
+        if (i+1) % 10 == 0:
             print ("Epoch [{}/{}], Step [{}/{}] Loss: {:.5f}"
                     .format(epoch+1, num_epochs, i+1, total_step, loss.item()))
             
         scheduler.step()
+    print(lossAvg/total_step)
 end = time.time()
 elapsed = end - start
 print("Training took " + str(elapsed) + " secs or " + str(elapsed/60) + " mins in total") 
