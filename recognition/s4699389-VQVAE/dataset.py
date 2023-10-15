9aa71c99d6a87679a1595e64fd15cd0abad053db
ss-samaim <sam.suvichagorn@gmail.com>
2023-10-15 23:57:00 +1000

    Reimplement dataset.py to use pytorch instead of tensorflow. Made a new custom dataloader to support this change.

--- recognition/s4699389-VQVAE/dataset.py
+++ recognition/s4699389-VQVAE/dataset.py
@@ -1,53 +1,86 @@
-import tensorflow as tf
-from tensorflow import keras
-from tensorflow.keras.layers.experimental import preprocessing
+from torch.utils.data import Dataset, DataLoader
+from torchvision.transforms import transforms
+import torch.cuda
+import os
+from PIL import Image
+
+# Torch configuration
+seed = 42
+torch.manual_seed(seed)
+# torch.use_deterministic_algorithms(True)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+
+resize_size = (128, 128)
+
+# Rangpur = 0, Home PC = 1, Laptop = 2
+device_config = 1
+if device_config == 0:
+    train_path = '/home/groups/comp3710/OASIS/keras_png_slices_train/'
+    test_path = '/home/groups/comp3710/OASIS/keras_png_slices_test/'
+    validation_path = '/home/groups/comp3710/OASIS/keras_png_slices_validate/'
+elif device_config == 1:
+    train_path = 'C:/Users/PC User/Desktop/COMP3710/PatternAnalysis-2023/dataset/keras_png_slices_data/keras_png_slices_train/'
+    test_path = 'C:/Users/PC User/Desktop/COMP3710/PatternAnalysis-2023/dataset/keras_png_slices_data/keras_png_slices_test/'
+    validation_path = 'C:/Users/PC User/Desktop/COMP3710/PatternAnalysis-2023/dataset/keras_png_slices_data/keras_png_slices_validate/'
+elif device_config == 2:
+    train_path = 'C:/Users/SAM/Desktop/COMP3710/PatternAnalysis-2023/dataset/keras_png_slices_data/keras_png_slices_train/'
+    test_path = 'C:/Users/SAM/Desktop/COMP3710/PatternAnalysis-2023/dataset/keras_png_slices_data/keras_png_slices_test/'
+    validation_path = 'C:/Users/SAM/Desktop/COMP3710/PatternAnalysis-2023/dataset/keras_png_slices_data/keras_png_slices_validate/'
+
+roots = 'C:/Users/PC User/Desktop/COMP3710/PatternAnalysis-2023/dataset/keras_png_slices_data'
+
+class OASISDataLoader:
+    def __init__(self, batch_size, image_size=resize_size):
+        self.batch_size = batch_size
+        self.device_config = 1
+        if self.device_config == 0: # rangpur
+            root_dir = '/home/groups/comp3710/OASIS/'
+        elif self.device_config == 1:   # home pc
+            root_dir = 'C:/Users/PC User/Desktop/COMP3710/PatternAnalysis-2023/dataset/keras_png_slices_data/'
+        elif self.device_config == 2:   # laptop
+            root_dir = 'C:/Users/SAM/Desktop/COMP3710/PatternAnalysis-2023/dataset/keras_png_slices_data/'
+
+        train_dir = os.path.join(root_dir, "keras_png_slices_train")
+        test_dir = os.path.join(root_dir, "keras_png_slices_test")
+        validation_dir = os.path.join(root_dir, "keras_png_slices_validate")
+
+        transformer = transforms.Compose([
+            transforms.ToTensor(),
+            transforms.Normalize((0.5,), (0.5,)),
+            transforms.Resize(image_size),
+            transforms.Grayscale()
+        ])
+
+        self.train = OASISDataset(root_dir=train_dir, transform=transformer)
+        self.test = OASISDataset(root_dir=test_dir, transform=transformer)
+        self.val = OASISDataset(root_dir=validation_dir, transform=transformer)
+
+    def get_dataloaders(self):
+        train_loader = DataLoader(self.train, batch_size=self.batch_size, shuffle=True)
+        test_loader = DataLoader(self.test, batch_size=self.batch_size, shuffle=True)
+        validation_loader = DataLoader(self.val, batch_size=self.batch_size, shuffle=True)
+
+        return train_loader, test_loader, validation_loader
 
 
-# Constant Parameters
-seed = 420
-train_path = '/home/groups/comp3710/OASIS/keras_png_slices_seg_train/'
-test_path = '/home/groups/comp3710/OASIS/keras_png_slices_seg_test/'
-validation_path = '/home/groups/comp3710/OASIS/keras_png_slices_seg_validate/'
+class OASISDataset(Dataset):
+    def __init__(self, root_dir, transform):
+        self.root_dir = root_dir
+        self.file_list = os.listdir(root_dir)
+        self.transform = transform
 
+    def __len__(self):
+        return len(self.file_list)
 
-def normalise_dataset(dataset):
-    # Normalize the pixel values to be in the range [0, 1]
-    rescale = preprocessing.Rescaling(1.0 / 255.0)
-    normalised_dataset = dataset.map(lambda x, y: (rescale(x), y))
-    return normalised_dataset
+    def __getitem__(self, idx):
+        if torch.is_tensor(idx):
+            idx = idx.tolist()
 
-def load_test_data(batch_size, image_size):
-    test_ds = keras.utils.image_dataset_from_directory(
-        directory=test_path,
-        labels=None,
-        batch_size=batch_size,
-        image_size=image_size,
-        seed=seed,
-        color_mode="grayscale",
-    )
+        file_path = os.path.join(self.root_dir, self.file_list[idx])
+        image = Image.open(file_path)
 
-    return normalise_dataset(test_ds)
+        if self.transform:
+            image = self.transform(image)
 
-def load_train_data(batch_size, image_size):
-    train_ds = keras.utils.image_dataset_from_directory(
-        directory=train_path,
-        labels=None,
-        batch_size=batch_size,
-        image_size=image_size,
-        seed=seed,
-        color_mode="grayscale",
-    )
-
-    return normalise_dataset(train_ds)
-
-def load_validation_data(batch_size, image_size):
-    validation_ds = keras.utils.image_dataset_from_directory(
-        directory=validation_path,
-        labels=None,
-        batch_size=batch_size,
-        image_size=image_size,
-        seed=seed,
-        color_mode="grayscale",
-    )
-
-    return normalise_dataset(validation_ds)
+        return image
