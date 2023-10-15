c95d33947e747a478ae276e6c86295cb06675550
Deven Bhasin <d.bhasin@uqconnect.edu.au>
2023-10-15 23:27:10 +1000

    basic predict code

---
+++ predict.py
@@ -0,0 +1,16 @@
+import torch
+import torchvision.transforms as transforms
+from PIL import Image
+from modules import UNet
+
+# Load your trained model
+def load_model(model, model_path):
+    model.load_state_dict(torch.load(model_path))
+    model.eval()
+
+# Make predictions on new data
+def predict(model, input_image):
+    # Implement code to preprocess and make predictions using the model
+    pass
+
+# Visualize and print results
