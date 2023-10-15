0f706fa5a17a63f96c526fac7639f50481a87f2f
Shriyan-Garg <gargshriyan23@gmail.com>
2023-10-16 03:05:11 +1000

    Structured train.py in functions

--- recognition/VisionTransformer_s4676080/train.py
+++ recognition/VisionTransformer_s4676080/train.py
@@ -7,22 +7,15 @@ from dataset import train_loader, test_loader
 from modules import VisionTransformer
 import matplotlib.pyplot as plt
 
-# Checking for GPU
-device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
-
 # Model configurations
-model = VisionTransformer().to(device)
-criterion = nn.CrossEntropyLoss()
-optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=2e-4)  # Introduced weight decay
-scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.7)  # Learning rate scheduler
+def initialize_model():
+    model = VisionTransformer().to(device)
+    criterion = nn.CrossEntropyLoss()
+    optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=2e-4)  # Introduced weight decay
+    scheduler = optim.lr_scheduler.StepLR(optimizer, step_size=10, gamma=0.7)  # Learning rate scheduler
+    return model, criterion, optimizer, scheduler
 
-# Training loop
-num_epochs = 10
-train_losses = []
-test_losses = []
-
-print("training started!")
-for epoch in range(num_epochs):
+def train_one_epoch(model, criterion, optimizer, train_loader):
     model.train()
     running_loss = 0.0
     for images, labels in train_loader:
@@ -30,37 +23,47 @@ for epoch in range(num_epochs):
         optimizer.zero_grad()        
         outputs = model(images)
         loss = criterion(outputs, labels)
-        #print(loss)
         loss.backward()
         optimizer.step()        
         running_loss += loss.item() * images.size(0)
-    train_loss = running_loss / len(train_loader.dataset)
-    train_losses.append(train_loss)    
-    print(f"Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f}")    
-    scheduler.step()
-print("finished training!")
+    return running_loss / len(train_loader.dataset)    
 
 # Testing loop
-model.eval()
-test_losses = []
-running_loss = 0.0
-correct = 0
-total = 0
+def test_model(model, criterion, test_loader):
+    model.eval()  
+    running_loss = 0.0
+    correct = 0
+    total = 0    
+    with torch.no_grad():
+        for images, labels in test_loader:
+            images, labels = images.to(device), labels.to(device)
+            outputs = model(images)
+            _, predicted = torch.max(outputs, 1)
+            loss = criterion(outputs, labels)
+            running_loss += loss.item() * images.size(0)
+            total += labels.size(0)
+            correct += (predicted == labels).sum().item()
+    return running_loss / len(test_loader.dataset), correct / total * 100
 
-print("Testing started!")
-with torch.no_grad():
-    for images, labels in test_loader:
-        images, labels = images.to(device), labels.to(device)
-        outputs = model(images)
-        _, predicted = torch.max(outputs, 1)
-        loss = criterion(outputs, labels)
-        running_loss += loss.item() * images.size(0)
-        total += labels.size(0)
-        correct += (predicted == labels).sum().item()
+if __name__ == "__main__":
+    device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+    
+    # Initialization
+    model, criterion, optimizer, scheduler = initialize_model()
+    
+    # Training configurations
+    num_epochs = 10
+    train_losses = []
 
-test_loss = running_loss / len(test_loader.dataset)
-test_losses.append(test_loss)
-accuracy = correct / total * 100
-print(f"Test Loss: {test_loss:.4f}, Accuracy: {accuracy:.2f}%")
-
+    print("Training started!")
+    for epoch in range(num_epochs):
+        train_loss = train_one_epoch(model, criterion, optimizer, train_loader)
+        train_losses.append(train_loss)
+        print(f"Epoch {epoch+1}/{num_epochs} - Train Loss: {train_loss:.4f}")
+        scheduler.step()
+    print("Finished training!")
+    
+    print("Testing started!")
+    test_loss, accuracy = test_model(model, criterion, test_loader)
+    print(f"Test Loss: {test_loss:.4f}, Accuracy: {accuracy:.2f}%")
 # The accuracy is increased to 58.40% by increasing the train and test images (no hyperparameter tuning done)
