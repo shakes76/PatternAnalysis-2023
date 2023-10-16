36a8ac09fd4683ce75f42e1112007bc23ca2b84c
Porcupinee <htolley0@gmail.com>
2023-10-16 23:26:20 +1000

    Updated network to include ReLu and BatchNorm2ds to improve network.

--- recognition/s4744784/modules.py
+++ recognition/s4744784/modules.py
@@ -8,16 +8,31 @@ class Network(nn.Module):
     def __init__(self, upscale_factor=3, channels=1):
         super(Network, self).__init__()
         
-        self.conv1 = nn.Conv2d(channels, 64, kernel_size=5, padding=2)
-        self.conv2 = nn.Conv2d(64, 64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(64, 32, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(32, channels * (upscale_factor ** 2), kernel_size=3, padding=1)
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(channels, 64, kernel_size=5, padding=2),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU())
+        
+        self.conv3 = nn.Sequential(
+            nn.Conv2d(64, 32, kernel_size=3, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU())
+        
+        self.conv4 = nn.Sequential(
+            nn.Conv2d(32, channels * (upscale_factor ** 2), kernel_size=3, padding=1),
+            nn.ReLU())
+        
         self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
         
     def forward(self, x):
-        x = F.relu(self.conv1(x))
-        x = F.relu(self.conv2(x))
-        x = F.relu(self.conv3(x))
-        x = F.relu(self.conv4(x))
-        x = self.pixel_shuffle(x)
-        return x
+        x1 = self.conv1(x)
+        x2 = self.conv2(x1)
+        x3 = self.conv3(x2)
+        x4 = self.conv4(x3)
+        out = self.pixel_shuffle(x4)
+        return out
