c14009f7b954762c4d4fb8a759e88b0dc3bcedb7
Selma <selma.gudmundsen@gmail.com>
2023-10-17 09:40:53 +1000

    added decoder block

--- recognition/ImprovedUNet-48207351/modules.py
+++ recognition/ImprovedUNet-48207351/modules.py
@@ -1,7 +1,8 @@
 import tensorflow as tf
 from tensorflow import keras
 from keras import layers
-from keras.layers.convolutional import Input, Activation, BatchNormalization, Conv2D, MaxPooling2D
+from keras.models import Model, load_model, save_model
+from keras.layers.convolutional import Input, Activation, BatchNormalization, Conv2D, MaxPooling2D, Conv2DTranspose, concatenate
 import matplotlib.pyplot as plt
 import numpy as np
 
@@ -27,6 +28,14 @@ def encoder_block(inp, filters, padding='same', pool_stride=2,
     p = MaxPooling2D(pool_size=(2, 2), strides=pool_stride)(x)
     return x, p
 
+# Functions to build the decoder block
+def decoder_block(inp,filters,concat_layer,padding='same'):
+    # Upsample the feature maps
+    x = Conv2DTranspose(filters,(2,2),strides=(2,2),padding=padding)(inp)
+    x = concatenate([x,concat_layer]) # Concatenation/Skip conncetion with conjuagte encoder
+    x = conv_block(x,filters) # Passed into the convolution block above
+    return x
+
 class UNet2D(input, latent_dim=64, activation=None, kernel=[3, 3], channels=1, name_prefix=''):
     # encoder
     # building the first block
@@ -39,5 +48,13 @@ class UNet2D(input, latent_dim=64, activation=None, kernel=[3, 3], channels=1, n
     d4,p4 = encoder_block(p3,512) 
 
     # Middle convolution block (no max pooling) - final output will now be upsampled
-    mid = conv_block(p4,1024) #Midsection     
+    mid = conv_block(p4,1024) #Midsection   
+
+    e2 = decoder_block(mid,512,d4) #Conjugate of encoder 4
+    e3 = decoder_block(e2,256,d3) #Conjugate of encoder 3
+    e4 = decoder_block(e3,128,d2) #Conjugate of encoder 2 
+    e5 = decoder_block(e4,64,d1) #Conjugate of encoder 1
+    outputs = Conv2D(1, (1,1), activation=None)(e5) #Final Output
+    
+    ml = Model(inputs=[inputs], outputs=[outputs, o1], name='Unet')  
     
