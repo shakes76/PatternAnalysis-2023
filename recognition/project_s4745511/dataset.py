8519ddb1570b122e3289e254b9ccd50aa33bcf43
Danita Anubhuti <52007397+danitaanubhuti@users.noreply.github.com>
2023-10-16 17:30:06 +1000

    Tried a different approach to pairing in dataset.py (4 different combinations: ad_ad, ad_nc, nc_ad, nc_nc)

--- recognition/project_s4745511/dataset.py
+++ recognition/project_s4745511/dataset.py
@@ -10,40 +10,134 @@ AD_TEST_PATH = 'C://Users//Danita//OneDrive//Desktop//UQ//sem3//PatterRecognisio
 NC_TEST_PATH = 'C://Users//Danita//OneDrive//Desktop//UQ//sem3//PatterRecognision//Project3//ADNI_AD_NC_2D//AD_NC//test//NC'
 
 def load_siamese_data(batch_size=32, train_ratio=0.8):
-    # def preprocess_image(path):
-    #     image = tf.io.read_file(path)
-    #     image = tf.image.decode_jpeg(image, 1)
-    #     image = tf.image.resize(image, [128, 128])  # You can adjust the size here
-    #     return image / 255
-
     custom_ad_paths = [os.path.join(AD_PATH, path) for path in os.listdir(AD_PATH)]
     custom_nc_paths = [os.path.join(NC_PATH, path) for path in os.listdir(NC_PATH)]
 
-    num_pairs = len(custom_ad_paths) // 3  # Modify the data splitting strategy
+    num_pairs = min(len(custom_ad_paths), len(custom_nc_paths)) // 3  # Modify the data splitting strategy
 
-    custom_pair_base = custom_nc_paths[::3]
-    custom_pair_ad = custom_ad_paths[:num_pairs]
-    custom_pair_nc = custom_nc_paths[1::3][:num_pairs]
+    # Create pairs for "AD, AD" combination (positive pairs)
+    custom_pair_base_ad_ad = custom_ad_paths[::3][:num_pairs]
+    custom_pair_ad_ad = custom_ad_paths[1::3][:num_pairs]
+    custom_labels_ad_ad = np.ones([num_pairs])
 
-    random.shuffle(custom_pair_ad)
-    random.shuffle(custom_pair_nc)
+    # Create pairs for "AD, NC" combination (negative pairs)
+    # Create pairs for "NC, AD" combination (negative pairs)
+    custom_pair_base_ad_nc = custom_ad_paths[::3][:num_pairs]
+    custom_pair_ad_nc = custom_ad_paths[1::3][:num_pairs]
+    custom_pair_nc_ad = custom_nc_paths[1::3][:num_pairs]
+    custom_pair_compare_ad_nc = custom_pair_ad_nc + custom_pair_nc_ad
+    custom_labels_ad_nc = np.zeros([num_pairs])
 
-    custom_pair_compare = custom_pair_nc + custom_pair_ad
+    # Create pairs for "NC, NC" combination (positive pairs)
+    custom_pair_base_nc_nc = custom_nc_paths[::3][:num_pairs]
+    custom_pair_nc_nc = custom_nc_paths[1::3][:num_pairs]
+    custom_labels_nc_nc = np.ones([num_pairs])
 
-    custom_labels = np.concatenate([np.zeros([num_pairs]), np.ones([num_pairs])])
-    custom_labels = np.expand_dims(custom_labels, -1)
+    # Combine all the combinations
+    custom_base_ds = tf.data.Dataset.from_tensor_slices(custom_pair_base_ad_ad + custom_pair_base_ad_nc + custom_pair_base_nc_nc).map(preprocess_image)
+    custom_pair_ds = tf.data.Dataset.from_tensor_slices(custom_pair_ad_ad + custom_pair_compare_ad_nc + custom_pair_nc_nc).map(preprocess_image)
+    custom_labels_ds = tf.data.Dataset.from_tensor_slices(
+        np.concatenate([custom_labels_ad_ad, custom_labels_ad_nc, custom_labels_nc_nc])
+    )
 
-    custom_base_ds = tf.data.Dataset.from_tensor_slices(custom_pair_base).map(preprocess_image)
-    custom_pair_ds = tf.data.Dataset.from_tensor_slices(custom_pair_compare).map(preprocess_image)
-    custom_labels_ds = tf.data.Dataset.from_tensor_slices(custom_labels)
-
-    custom_dataset = tf.data.Dataset.zip(((custom_base_ds, custom_pair_ds), custom_labels_ds)).shuffle(num_pairs)
+    custom_dataset = tf.data.Dataset.zip(((custom_base_ds, custom_pair_ds), custom_labels_ds)).shuffle(len(custom_pair_base_ad_ad) + len(custom_pair_base_ad_nc) + len(custom_pair_base_nc_nc))
 
     train_num = int(round(len(custom_dataset) * train_ratio, 0))
     custom_train = custom_dataset.take(train_num)
     custom_val = custom_dataset.skip(train_num)
 
     return custom_train.batch(batch_size), custom_val.batch(batch_size)
+
+
+
+# def load_siamese_data(batch_size=32, train_ratio=0.8):
+#     custom_ad_paths = [os.path.join(AD_PATH, path) for path in os.listdir(AD_PATH)]
+#     custom_nc_paths = [os.path.join(NC_PATH, path) for path in os.listdir(NC_PATH)]
+
+#     num_pairs = len(custom_ad_paths) // 3  # Modify the data splitting strategy
+
+#     # Create pairs for "AD, NC" combination
+#     custom_pair_base = custom_nc_paths[::3]
+#     custom_pair_ad = custom_ad_paths[:num_pairs]
+#     custom_pair_nc = custom_nc_paths[1::3][:num_pairs]
+
+#     random.shuffle(custom_pair_ad)
+#     random.shuffle(custom_pair_nc)
+
+#     custom_pair_compare_ad_nc = custom_pair_ad + custom_pair_nc
+#     custom_labels_ad_nc = np.concatenate([np.zeros([num_pairs]), np.ones([num_pairs])])
+#     custom_labels_ad_nc = np.expand_dims(custom_labels_ad_nc, -1)
+
+#     # Create pairs for "NC, AD" combination
+#     custom_pair_base_nc_ad = custom_ad_paths[::3]
+#     custom_pair_ad_nc = custom_ad_paths[1::3][:num_pairs]
+#     custom_pair_nc_ad = custom_nc_paths[:num_pairs]
+
+#     random.shuffle(custom_pair_ad_nc)
+#     random.shuffle(custom_pair_nc_ad)
+
+#     custom_pair_compare_nc_ad = custom_pair_nc_ad + custom_pair_ad_nc
+#     custom_labels_nc_ad = np.concatenate([np.zeros([num_pairs]), np.ones([num_pairs])])
+#     custom_labels_nc_ad = np.expand_dims(custom_labels_nc_ad, -1)
+
+#     # Combine both datasets to include all four combinations
+#     custom_base_ds = tf.data.Dataset.from_tensor_slices(custom_pair_base + custom_pair_base_nc_ad).map(preprocess_image)
+#     custom_pair_ds = tf.data.Dataset.from_tensor_slices(custom_pair_compare_ad_nc + custom_pair_compare_nc_ad).map(preprocess_image)
+#     custom_labels_ds = tf.data.Dataset.from_tensor_slices(
+#         np.concatenate([custom_labels_ad_nc, custom_labels_nc_ad])
+#     )
+
+#     custom_dataset = tf.data.Dataset.zip(((custom_base_ds, custom_pair_ds), custom_labels_ds)).shuffle(len(custom_pair_base) + len(custom_pair_base_nc_ad))
+
+#     train_num = int(round(len(custom_dataset) * train_ratio, 0))
+#     custom_train = custom_dataset.take(train_num)
+#     custom_val = custom_dataset.skip(train_num)
+
+#     return custom_train.batch(batch_size), custom_val.batch(batch_size)
+
+
+
+# def load_siamese_data(batch_size=32, train_ratio=0.8): working code
+#     custom_ad_paths = [os.path.join(AD_PATH, path) for path in os.listdir(AD_PATH)]
+#     custom_nc_paths = [os.path.join(NC_PATH, path) for path in os.listdir(NC_PATH)]
+
+#     num_pairs = len(custom_ad_paths) // 3  # Modify the data splitting strategy
+
+#     custom_pair_base = custom_nc_paths[::3]
+#     custom_pair_ad = custom_ad_paths[:num_pairs]
+#     custom_pair_nc = custom_nc_paths[1::3][:num_pairs]
+
+#     random.shuffle(custom_pair_ad)
+#     random.shuffle(custom_pair_nc)
+
+#     custom_pair_compare_ad_nc = custom_pair_ad + custom_pair_nc
+#     custom_labels_ad_nc = np.concatenate([np.zeros([num_pairs]), np.ones([num_pairs])])
+#     custom_labels_ad_nc = np.expand_dims(custom_labels_ad_nc, -1)
+
+#     # For the other combination (NC, AD), swap the order of custom_pair_ad and custom_pair_nc
+#     custom_pair_compare_nc_ad = custom_pair_nc + custom_pair_ad
+#     custom_labels_nc_ad = np.concatenate([np.zeros([num_pairs]), np.ones([num_pairs])])
+#     custom_labels_nc_ad = np.expand_dims(custom_labels_nc_ad, -1)
+
+#     custom_base_ds = tf.data.Dataset.from_tensor_slices(custom_pair_base).map(preprocess_image)
+#     custom_pair_ds_ad_nc = tf.data.Dataset.from_tensor_slices(custom_pair_compare_ad_nc).map(preprocess_image)
+#     custom_labels_ds_ad_nc = tf.data.Dataset.from_tensor_slices(custom_labels_ad_nc)
+#     custom_dataset_ad_nc = tf.data.Dataset.zip(((custom_base_ds, custom_pair_ds_ad_nc), custom_labels_ds_ad_nc)).shuffle(num_pairs)
+
+#     # Create another dataset for the other combination (NC, AD)
+#     custom_pair_ds_nc_ad = tf.data.Dataset.from_tensor_slices(custom_pair_compare_nc_ad).map(preprocess_image)
+#     custom_labels_ds_nc_ad = tf.data.Dataset.from_tensor_slices(custom_labels_nc_ad)
+#     custom_dataset_nc_ad = tf.data.Dataset.zip(((custom_base_ds, custom_pair_ds_nc_ad), custom_labels_ds_nc_ad)).shuffle(num_pairs)
+
+#     # Combine both datasets to include all four combinations
+#     custom_dataset = custom_dataset_ad_nc.concatenate(custom_dataset_nc_ad)
+
+#     train_num = int(round(len(custom_dataset) * train_ratio, 0))
+#     custom_train = custom_dataset.take(train_num)
+#     custom_val = custom_dataset.skip(train_num)
+
+#     return custom_train.batch(batch_size), custom_val.batch(batch_size)
+
 
 # def load_classify_data(testing: bool, batch_size=32):
 #     if not testing:
