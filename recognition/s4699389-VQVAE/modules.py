8e688b8e1ff096bdee347d10998a841d0b9d47fe
Sam Suvichagorn <sam.suvichagorn@gmail.com>
2023-10-16 13:39:20 +1000

    Added VectorQuantizer class to modules.py

--- recognition/s4699389-VQVAE/modules.py
+++ recognition/s4699389-VQVAE/modules.py
@@ -1,1 +1,56 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Torch configuration
+seed = 42
+torch.manual_seed(seed)
+print(torch.__version__)
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(device)
+print("Name: ", torch.cuda.get_device_name(0))
+
+
+class VectorQuantizer(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim, commitment_loss):
+        super(VectorQuantizer, self).__init__()
+        self.embedding_dim = embedding_dim
+        self.num_embeddings = num_embeddings
+
+        # Initialize the embeddings which we will quantize.
+        self.embedding = nn.Embedding(self.num_embeddings, self.embedding_dim)
+        self.embedding.weight.data.uniform_(-1 / self.num_embeddings, 1 / self.num_embeddings)
+        self.commitment_loss = commitment_loss
+
+    def forward(self, inputs):
+        # convert inputs from BCHW -> BHWC
+        inputs = inputs.permute(0, 2, 3, 1).contiguous()
+        input_shape = inputs.shape
+
+        # Flatten input
+        flat_input = inputs.view(-1, self.embedding_dim)
+
+        # Calculate distances
+        distances = (torch.sum(flat_input ** 2, dim=1, keepdim=True)
+                     + torch.sum(self.embedding.weight ** 2, dim=1)
+                     - 2 * torch.matmul(flat_input, self.embedding.weight.t()))
+
+        # Encoding
+        encoding_indices = torch.argmin(distances, dim=1).unsqueeze(1)
+        encodings = torch.zeros(encoding_indices.shape[0], self.num_embeddings, device=inputs.device)
+        encodings.scatter_(1, encoding_indices, 1)
+
+        # Quantize and unflatten
+        quantized = torch.matmul(encodings, self.embedding.weight).view(input_shape)
+
+        # Loss
+        e_latent_loss = F.mse_loss(quantized.detach(), inputs)
+        q_latent_loss = F.mse_loss(quantized, inputs.detach())
+        loss = q_latent_loss + self.commitment_loss * e_latent_loss
+
+        quantized = inputs + (quantized - inputs).detach()
+        avg_probs = torch.mean(encodings, dim=0)
+
+        # convert quantized from BHWC -> BCHW
+        return loss, quantized.permute(0, 3, 1, 2).contiguous(), encodings, encoding_indices
 
