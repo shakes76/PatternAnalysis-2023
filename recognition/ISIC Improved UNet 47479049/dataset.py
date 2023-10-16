787b3b3c1904c14856ec0202cf1d8e60f413692e
LargePenguin034 <134495820+LargePenguin034@users.noreply.github.com>
2023-10-16 11:07:52 +1000

    completed datasetloader

---
+++ recognition/ISIC Improved UNet 47479049/dataset.py
@@ -0,0 +1,29 @@
+import os
+from PIL import Image
+from torch.utils.data import Dataset
+import numpy as np
+
+class SkinDataset(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = os.listdir(image_dir)
+
+    def __len__(self):
+        return len(self.images)
+    
+    def __getitem__(self, index):
+        img_path = os.path.join(self.image_dir, self.images[index])
+        mask_path = os.path.join(self.mask_dir, self.images[index].replace(".jpg", "_segmentation.png"))
+
+        image = np.array(Image.open(img_path).convert("RGB"))
+        mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32)
+        mask[mask == 255.0] = 1.0
+
+        if self.transform is not None:
+            augemntations = self.transform(image=image, mask=mask)
+            image = augemntations["image"]
+            mask = augemntations["mask"]
+
+        return image, mask
