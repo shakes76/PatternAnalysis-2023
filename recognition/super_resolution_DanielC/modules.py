8572abc1e67d38c5249f670d63100b108c5dad08
DHyunC <chsdaniel725@gmail.com>
2023-10-16 21:10:32 +1000

    removed staged upsampling as it didnt improve performance.

--- recognition/super_resolution_DanielC/modules.py
+++ recognition/super_resolution_DanielC/modules.py
@@ -1,3 +1,7 @@
+"""
+Reference: https://keras.io/examples/vision/super_resolution_sub_pixel/
+"""
+
 import torch.nn as nn
 import torch.nn.functional as F
 from utils import *
@@ -9,13 +13,12 @@ class SuperResolution(nn.Module):
         self.conv1 = nn.Conv2d(channels, 64, 5, padding=2)
         self.conv2 = nn.Conv2d(64, out_channels, 3, padding=1)
         self.conv3 = nn.Conv2d(out_channels, out_channels, 3, padding=1)
-        self.conv4 = nn.Conv2d(out_channels, channels * (upscale_factor ** 2), 3, padding=1)
+        self.conv4 = nn.Conv2d(out_channels, channels * (upscale_factor ** 2), 
+                               3, padding=1)
 
         #efficient sub-pixel layer
         self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
 
-        # self.pixel_shuffle1 = nn.PixelShuffle(2)
-        # self.pixel_shuffle2 = nn.PixelShuffle(2)
         
     def forward(self, x):
         x = F.leaky_relu(self.conv1(x))
@@ -23,7 +26,5 @@ class SuperResolution(nn.Module):
         x = F.leaky_relu(self.conv3(x))
         x = F.pixel_shuffle(self.conv4(x), upscale_factor=4)
 
-        # x = self.pixel_shuffle1(self.conv4(x))
-        # x = self.pixel_shuffle2(x)
         return x
 
