a27eb7457a65788522b98f64774c4e6cfad880f3
EthanGTJones <116948347+EthanGTJones@users.noreply.github.com>
2023-10-16 15:12:54 +1000

    Divided the FileSaver class into two seperate classes, SaveImage and SaveWeight
    This change was made as I was having some issues incorporating the single class into predict.py

--- recognition/OASIS brain StyleGAN - 44829531/util.py
+++ recognition/OASIS brain StyleGAN - 44829531/util.py
@@ -10,10 +10,32 @@ Semester 2, 2023
 import os
 import tensorflow as tf
 from tensorflow import keras
+import numpy as np
 
-class FileSaver(keras.callbacks.Callback):
-    def __init__(self):
+
+class SaveImage(keras.callbacks.Callback):
+
+    def __init__(self, path, image_count=5):
+        self.image_count = image_count
         directory_name = os.path.dirname(__file__)
-        self.result_image_path = os.path.join(directory_name, "figures")
-        self.result_weight_path = os.path.join(directory_name, "figures")
-        
+        self.filepath = os.path.join(directory_name, path)
+
+    def on_epoch_end(self, epoch, logs):
+        generator_inputs = self.model.get_generator_inputs()
+        output_images = self.model.generator(generator_inputs)
+        output_images *= 256
+        output_images.numpy()
+        for i in range(self.image_count):
+            img = keras.preprocessing.image.array_to_img(output_images[i])
+            img.save("{}\epoch_{}_image_{}.png".format(self.path, epoch, i))
+
+
+class SaveWeight(keras.callbacks.Callback):
+
+    def __init__(self, path):
+        directory_name = os.path.dirname(__file__)
+        self.filepath = os.path.join(directory_name, path)
+
+    def on_epoch_end(self, epoch, logs):
+        self.model.generator.save_weights("{}\epoch_{}_generator.h5".format(self.path, epoch))
+        self.model.discriminator.save_weights("{}\epoch_{}_discriminator.h5".format(self.path, epoch))
