2bb410a98da842a9ef43cbe4a500d252f7ecb293
Manandeep Singh <manandeep1500@gmail.com>
2023-10-17 04:28:28 +1000

    Load and Test the Trained Siamese Network

--- topicRecogQues7_Manandeep_48239259/predict.py
+++ topicRecogQues7_Manandeep_48239259/predict.py
@@ -1,31 +1,57 @@
 import os
+from PIL import Image
+import torchvision.transforms as transforms
+from modules import CustomSiameseNetwork
+from dataset import CustomDataset
+import torchvision.datasets as dset
+import torch
+import torchvision.datasets as datasets
+from torch.utils.data import DataLoader, Dataset
+import torchvision.transforms as transforms
+import random
+import numpy as np
+from PIL import Image
+import PIL.ImageOps
+import torch.nn.functional as F
+import torch.nn.functional as TorchFun
+import torchvision
+import matplotlib.pyplot as plt
+import numpy np
+from PIL import Image
+from torch.autograd import Variable
 
-trained_model = SiameseNetwork()
-trained_model.load_state_dict(torch.load('/content/drive/MyDrive/siamese_network.pth'))
-trained_model.eval()
+def imshow(img, text=None, should_save=False):
+    npimg = np.array(Image.open(img))
+    plt.axis("off")
+    if text:
+        plt.text(75, 8, text, style='italic', fontweight='bold',
+            bbox={'facecolor': 'white', 'alpha': 0.8, 'pad': 10})
+    plt.imshow(npimg)
+    plt.show()
 
-# Function to classify a test image based on Euclidean distance
-def classify_image(test_image_path):
-    # Load and preprocess the test image
-    test_image = Image.open('/content/drive/MyDrive/test/melanoma/ISIC_0000056.jpg')
-    test_image = test_image.convert("L")
-    test_image = transforms.Compose([transforms.Resize((100, 100)), transforms.ToTensor()])(test_image)
-    test_image = test_image.unsqueeze(0)  # Add batch dimension
+def imshow_grid(img,text=None,should_save=False):
+    npimg = img.numpy()
+    plt.axis("off")
+    if text:
+        plt.text(75, 8, text, style='italic',fontweight='bold',
+            bbox={'facecolor':'white', 'alpha':0.8, 'pad':10})
+    plt.imshow(np.transpose(npimg, (1, 2, 0))
+    plt.show()
 
-    # Prepare reference images from both classes (adjust paths accordingly)
-    reference_images_class1 = ['/content/drive/MyDrive/training/melanoma/ISIC_0000139.jpg', '/content/drive/MyDrive/training/melanoma/ISIC_0000141.jpg']  # List of reference images
-    reference_images_class2 = ['/content/drive/MyDrive/training/vascular lesion/ISIC_0024475.jpg', '/content/drive/MyDrive/training/vascular lesion/ISIC_0024662.jpg']  # List of reference images
+trained_siamese_net = CustomSiameseNetwork()
+trained_siamese_net.load_state_dict(torch.load('/content/drive/MyDrive/dataset/model.pth'))
+trained_siamese_net.eval()
 
-    # Compute Euclidean distances
-    distances_class1 = []
-    distances_class2 = []
+# Define the test dataset
+testing_dir = '/content/AD_NC/test'
+folder_dataset_test = dset.ImageFolder(root=testing_dir)
+siamese_dataset = CustomDataset(folder_dataset_test,
+                                transform=transforms.Compose([transforms.Resize((100, 100)),
+                                                              transforms.ToTensor()
+                                                              ]),
+                                should_invert=False)
 
-    for ref_image_path in reference_images_class1:
-        ref_image = Image.open(ref_image_path)
-        ref_image = ref_image.convert("L")
-        ref_image = transforms.Compose([transforms.Resize((100, 100)), transforms.ToTensor()])(ref_image)
-        ref_image = ref_image.unsqueeze(0)  # Add batch dimension
-        with torch.no_grad():
-            output1, output2 = trained_model(test_image, ref_image)
-            euclidean_distance = F.pairwise_distance(output1, output2)
-            distances_class1.append(euclidean_distance.item())
+# Define the data loader for testing
+test_dataloader = DataLoader(siamese_dataset, num_workers=6, batch_size=1, shuffle=True)
+dataiter = iter(test_dataloader)
+x0, _, _ = next(dataiter)
