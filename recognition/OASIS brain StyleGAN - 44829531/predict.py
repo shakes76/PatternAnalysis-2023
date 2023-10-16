e7397e21211ad949c8be79cbc2c5a0f3ed84f147
EthanGTJones <116948347+EthanGTJones@users.noreply.github.com>
2023-10-16 18:34:36 +1000

    Added output() and main() function to predict.py. When predict is ran,  main will be called and the files will outputs will populate the folders

--- recognition/OASIS brain StyleGAN - 44829531/predict.py
+++ recognition/OASIS brain StyleGAN - 44829531/predict.py
@@ -8,19 +8,87 @@ Semester 2, 2023
 """
 
 import os
+import tensorflow as tf
+from tensorflow import keras
+from train import StyleGAN
 
-# Hyparparameters
-EPOCHS = 5
-BATCH_SIZE = 32
+# Input paths
+INPUT_IMAGES_PATH = "~/home/groups/comp3710/OASIS/keras_png_slices_train"
+INPUT_GENERATOR_WEIGHTS_PATH = ""
+INPUT_DISCRIMINATOR_WEIGHTS_PATH = ""
 
-# Inputs
-INPUT_IMAGES_PATH = "C:\\Users\\ethan\\Desktop\\COMP3710" \
-                    "\\keras_png_slices_train "
-# INPUT_IMAGES_PATH = "/home/groups/comp3710/OASIS/keras_png_slices_train"
-
-# Results
+# Output paths
 RESULT_IMAGE_PATH = "figures"
-RESULT_WEIGHT_PATH = "figures"
-RESULT_IMAGE_COUNT = 3
+RESULT_WEIGHT_PATH = "weights"
+
+# Output parameters
+IMG_COUNT = 5
 PLOT_LOSS = True
 
+# Hyperparameters
+EPOCHS = 120
+BATCH_SIZE = 32
+
+
+def main():
+    """
+    Main function to execute the StyleGAN workflow.
+    """
+    directory_name = os.path.dirname(__file__)
+
+    # Create directories for output files
+    if RESULT_IMAGE_PATH != "":
+        filepath = os.path.join(directory_name, RESULT_IMAGE_PATH)
+        if not os.path.exists(filepath):
+            os.mkdir(filepath)
+
+    if RESULT_WEIGHT_PATH != "":
+        filepath = os.path.join(directory_name, RESULT_WEIGHT_PATH)
+        if not os.path.exists(filepath):
+            os.mkdir(filepath)
+
+    # Initialise the StyleGAN model
+    style_gan = StyleGAN(epochs=EPOCHS, batch_size=BATCH_SIZE)
+
+    # Load the pre-trained model when weights are provided
+    if INPUT_GENERATOR_WEIGHTS_PATH and INPUT_DISCRIMINATOR_WEIGHTS_PATH:
+        style_gan.built = True
+
+        generator_filepath = os.path.join(directory_name,
+                                          INPUT_GENERATOR_WEIGHTS_PATH)
+        discriminator_filepath = os.path.join(directory_name,
+                                              INPUT_DISCRIMINATOR_WEIGHTS_PATH)
+
+        style_gan.generator.load_weights(generator_filepath)
+        style_gan.discriminator.load_weights(discriminator_filepath)
+    # Train the model
+    else:
+        style_gan.train(dataset_path=INPUT_IMAGES_PATH,
+                        result_image_path=RESULT_IMAGE_PATH,
+                        image_count=IMG_COUNT,
+                        result_weight_path=RESULT_WEIGHT_PATH,
+                        plot_loss=PLOT_LOSS)
+
+    # Generate an image and display
+    output(style_gan)
+
+
+def output(style_gan):
+    """
+    Generates and displays an image using the provided StyleGAN model.
+    """
+    generator_inputs = style_gan.generator_inputs()
+
+    # Generate an image using the trained model
+    output_images = style_gan.generator(generator_inputs)
+    output_images *= 256
+    output_images.numpy()
+
+    # Convert the tensor to an image and display
+    img = keras.preprocessing.image.array_to_img(output_images[0])
+    img.show(title="Generated Image")
+
+
+# Run main() if this file is run directly
+if __name__ == "__main__":
+    main()
