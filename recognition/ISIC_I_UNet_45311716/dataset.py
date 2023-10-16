bee02792b0b4fb74a9bfec653349213d4a61c4d4
Awidat17 <mlamtadi1705@gmail.com>
2023-10-16 14:15:50 +1000

    "added single channel tranform to masks"

--- recognition/ISIC_I_UNet_45311716/dataset.py
+++ recognition/ISIC_I_UNet_45311716/dataset.py
@@ -13,6 +13,12 @@ path = 'C:/Users/mlamt/OneDrive/UNI/2023/Semester 2/COMP3710/Code/data/ISIC2018/
 data_transform = transforms.Compose([transforms.Resize((512, 512)),
                                              transforms.ToTensor()])
 
+mask_transform = transforms.Compose([transforms.Resize((512, 512)),
+                                            transforms.Grayscale(num_output_channels=1),  # Convert to single channel
+                                            transforms.ToTensor(),
+                                            transforms.Lambda(lambda x: x > 0.5)  # Threshold to 0 or 1
+                                            ])
+
 # Training dataset
 train_set = torchvision.datasets.ImageFolder(root=path+'ISIC2018_Task1-2_Training_Input_x2', transform=data_transform)
 train_loader = torch.utils.data.DataLoader(train_set, batch_size=16, shuffle=True)
@@ -22,6 +28,6 @@ test_set = torchvision.datasets.ImageFolder(root=path+'ISIC2018_Task1-2_Test_Inp
 test_loader = torch.utils.data.DataLoader(test_set, batch_size=16, shuffle=True)
 
 # Truth dataset
-truth_set = torchvision.datasets.ImageFolder(root=path+'ISIC2018_Task1_Training_GroundTruth_x2', transform=data_transform)
+truth_set = torchvision.datasets.ImageFolder(root=path+'ISIC2018_Task1_Training_GroundTruth_x2', transform=mask_transform)
 truth_loader = torch.utils.data.DataLoader(truth_set, batch_size=16, shuffle=True)
 
