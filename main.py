e0a098bd2d063cacd9da1c342f512c01ecbfb389
Paddo0 <paddo2003@gmail.com>
2023-10-16 13:31:04 +1000

    Initial commit setting up project.

---
+++ main.py
@@ -0,0 +1,9 @@
+from parameters import *
+
+
+def main():
+    pass
+
+
+if __name__ == "__main__":
+    main()
---
+++ parameters.py
@@ -0,0 +1,15 @@
+###################################
+# PARAMETER CONSTANTS FOR PROJECT #
+###################################
+
+# Number of epochs in training
+epochs = 150
+
+# Batch size for loader
+batch = 32
+
+# Learning Rate
+lr = 0.001
+
+# Image size (nxn)
+image_size = 128
