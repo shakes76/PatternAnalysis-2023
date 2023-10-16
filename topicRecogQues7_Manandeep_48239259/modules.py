e0f063b72cf3ede357d7a8c12523a47abda87d11
Manandeep Singh <manandeep1500@gmail.com>
2023-10-17 04:08:48 +1000

    Changed classname to CustomSiameseNetwork

--- topicRecogQues7_Manandeep_48239259/modules.py
+++ topicRecogQues7_Manandeep_48239259/modules.py
@@ -1,45 +1,35 @@
-import torchvision
-import torchvision.datasets as dset
-import torchvision.transforms as transforms
-from torch.utils.data import DataLoader,Dataset
-import matplotlib.pyplot as plt
-import torchvision.utils
-import numpy as np
-import random
-from PIL import Image
 import torch
-from torch.autograd import Variable
-import PIL.ImageOps
-class SiameseNetwork(nn.Module):
+import torch.nn as tnn
+import torch.nn.functional as TorchFun
+
+class CustomSiameseNetwork(tnn.Module):
     def __init__(self):
-        super(SiameseNetwork, self).__init__()
-        self.cnn1 = nn.Sequential(
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(1, 4, kernel_size=3),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(4),
-
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(4, 8, kernel_size=3),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(8),
-
-            nn.ReflectionPad2d(1),
-            nn.Conv2d(8, 8, kernel_size=3),
-            nn.ReLU(inplace=True),
-            nn.BatchNorm2d(8),
+        super(CustomSiameseNetwork, self).__init__()
+        self.cnn = tnn.Sequential(
+            tnn.ReflectionPad2d(1),
+            tnn.Conv2d(1, 4, kernel_size=3),
+            tnn.ReLU(inplace=True),
+            tnn.BatchNorm2d(4),
+            tnn.ReflectionPad2d(1),
+            tnn.Conv2d(4, 8, kernel_size=3),
+            tnn.ReLU(inplace=True),
+            tnn.BatchNorm2d(8),
+            tnn.ReflectionPad2d(1),
+            tnn.Conv2d(8, 8, kernel_size=3),
+            tnn.ReLU(inplace=True),
+            tnn.BatchNorm2d(8),
         )
-        self.fc1 = nn.Sequential(
-            nn.Linear(8 * 100 * 100, 500),
-            nn.ReLU(inplace=True),
 
-            nn.Linear(500, 500),
-            nn.ReLU(inplace=True),
-
-            nn.Linear(500, 5))
+        self.fc1 = tnn.Sequential(
+            tnn.Linear(8 * 100 * 100, 500),
+            tnn.ReLU(inplace=True),
+            tnn.Linear(500, 500),
+            tnn.ReLU(inplace=True),
+            tnn.Linear(500, 5)
+        )
 
     def forward_once(self, x):
-        output = self.cnn1(x)
+        output = self.cnn(x)
         output = output.view(output.size()[0], -1)
         output = self.fc1(output)
         return output
@@ -49,17 +39,13 @@ class SiameseNetwork(nn.Module):
         output2 = self.forward_once(input2)
         return output1, output2
 
-
 class ContrastiveLoss(torch.nn.Module):
-  
     def __init__(self, margin=2.0):
         super(ContrastiveLoss, self).__init__()
         self.margin = margin
 
     def forward(self, output1, output2, label):
-        euclidean_distance = F.pairwise_distance(output1, output2, keepdim = True)
-        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
+        euclidean_distance = TorchFun.pairwise_distance(output1, output2, keepdim=True)
+        loss_contrastive = torch.mean((1 - label) * torch.pow(euclidean_distance, 2) +
                                       (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
-
-
         return loss_contrastive
