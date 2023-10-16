791a9a457071ffabeaa4c8ef3ded7a88c9f15d01
Paddo0 <paddo2003@gmail.com>
2023-10-16 13:43:33 +1000

    Added naive ADaIN layer implementation, as required for a StyleGAN

--- main.py
+++ main.py
@@ -1,8 +1,14 @@
 from parameters import *
+from modules import *
 
 
 def main():
-    pass
+    # Example ADaIN
+    content = torch.randn(1, 3, 64, 64)
+    style = torch.randn(1, 3, 64, 64)
+
+    adain_layer = ADaIN()
+    print(adain_layer(content, style))
 
 
 if __name__ == "__main__":
--- modules.py
+++ modules.py
@@ -1,1 +1,25 @@
+import torch
+import torch.nn as nn
 
+
+class ADaIN(nn.Module):
+    def __init__(self, epsilon=1e-5):
+        super(ADaIN, self).__init__()
+        self.epsilon = epsilon
+
+    def forward(self, content, style):
+        # Calculating mean and std from content and style tensors
+        # Dim denoted as 2nd and 3rd dimension for height and width respectively
+        content_mean = content.mean(dim=[2, 3], keepdim=True)
+        content_std = content.std(dim=[2, 3], keepdim=True)
+
+        style_mean = style.mean(dim=[2, 3], keepdim=True)
+        style_std = style.std(dim=[2, 3], keepdim=True)
+
+        # Normalizing the content tensor using the statistics from content tensor
+        normalized_content = (content - content_mean) / (content_std + self.epsilon)
+
+        # Scale and shift normalized content
+        stylized_content = style_std * normalized_content + style_mean
+
+        return stylized_content
