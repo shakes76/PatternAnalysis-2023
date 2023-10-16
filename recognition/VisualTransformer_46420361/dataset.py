9478babdf7b5a99e4b271cf230185e07769092e3
Callum Elder <cjames.02@icloud.com>
2023-10-16 20:48:58 +1000

    split test data into test and validation

--- recognition/VisualTransformer_46420361/dataset.py
+++ recognition/VisualTransformer_46420361/dataset.py
@@ -1,45 +1,64 @@
 from torchvision.datasets import ImageFolder
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
+import os
+import random
 
 
-def crop_images():
-    """
-    crops the dataset images to only be of the brain
-    """
-    return
-
-def filter_datasets():
-    """filters dataset so patient images are kept together, preventing data leakage and overfitting
-
-    Returns:
-        _type_: _description_
-    """
-    return
-
-def load_dataset(image_size, crop_size, batch_size, path):
-    # transform
-    train_transform = transforms.Compose([
+def get_transform(image_size, crop_size):
+    transform = transforms.Compose([
         transforms.CenterCrop((crop_size, crop_size)),
         transforms.Resize((image_size, image_size)),
         transforms.Grayscale(),
         transforms.ToTensor(),
     ])
+    return transform
+
+
+def split_test_data(root, image_size, crop_size, test_val_ratio=0.5):
+    test_dir = os.path.join(root, "test")
+
+    # Load the test data without applying transforms
+    test_data = ImageFolder(root=test_dir)
+
+    # Extract patient IDs
+    patient_ids = set(os.path.basename(path).split('_')[0] for path, _ in test_data.samples)
+
+    # Determine the number of patients for validation and test
+    num_validation_patients = int(len(patient_ids) * test_val_ratio)
+
+    # Randomly select patient IDs for validation
+    random.seed(42)
+    patient_ids = list(set(os.path.basename(path).split('_')[0] for path, _ in test_data.samples))
     
-    test_transform = transforms.Compose([
-        transforms.CenterCrop((crop_size, crop_size)),
-        transforms.Resize((image_size, image_size)),
-        transforms.Grayscale(),
-        transforms.ToTensor(),
-    ])
+    # Split test dataset based on patient IDs
+    validation_samples = [(path, label) for path, label in test_data.samples if os.path.basename(path).split('_')[0] in patient_ids]
+    test_samples = [(path, label) for path, label in test_data.samples if os.path.basename(path).split('_')[0] not in patient_ids]
+    
+    transform = get_transform(image_size, crop_size)
 
-    # data    
-    train_dataset = ImageFolder(path, transform=train_transform)
+    # Create validation and test datasets
+    validation_dataset = ImageFolder(root=test_dir, transform=transform)
+    test_dataset = ImageFolder(root=test_dir, transform=transform)
+
+    # Overwrite samples
+    validation_dataset.samples = validation_samples
+    test_dataset.samples = test_samples
+
+    return validation_dataset, test_dataset
+
+
+def load_dataset(image_size, crop_size, batch_size, root):    
+    # transform    
+    transform = get_transform(image_size, crop_size)
+
+    # create datasets
+    train_dataset = ImageFolder(root + 'train', transform=transform)
+    validation_dataset, test_dataset = split_test_data(root, image_size, crop_size, test_val_ratio=0.5)
+    
+    # create dataloaders
     train_dataloader = DataLoader(dataset=train_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
-    
-    test_dataset = ImageFolder(path, transform=test_transform)
-    test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=True, drop_last=True)
+    test_dataloader = DataLoader(dataset=test_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
+    validation_dataloader = DataLoader(dataset=validation_dataset, batch_size=batch_size, shuffle=False, drop_last=True)
 
-    return train_dataloader, test_dataloader
-
-# KEEP PATIENTS DATA TOGETHER TO PREVENT DATA LEAKAGE AND OVERFITTING
+    return train_dataloader, test_dataloader, validation_dataloader
