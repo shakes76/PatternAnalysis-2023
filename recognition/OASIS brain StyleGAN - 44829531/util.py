eae5138d47a83c5b6545637d4e362f3ab65c56d6
EthanGTJones <116948347+EthanGTJones@users.noreply.github.com>
2023-10-16 16:45:41 +1000

    Changed self.filepath to self.path as I used used originally

--- recognition/OASIS brain StyleGAN - 44829531/util.py
+++ recognition/OASIS brain StyleGAN - 44829531/util.py
@@ -18,7 +18,7 @@ class SaveImage(keras.callbacks.Callback):
     def __init__(self, path, image_count=5):
         self.image_count = image_count
         directory_name = os.path.dirname(__file__)
-        self.filepath = os.path.join(directory_name, path)
+        self.path = os.path.join(directory_name, path)
 
     def on_epoch_end(self, epoch, logs):
         generator_inputs = self.model.get_generator_inputs()
@@ -34,7 +34,7 @@ class SaveWeight(keras.callbacks.Callback):
 
     def __init__(self, path):
         directory_name = os.path.dirname(__file__)
-        self.filepath = os.path.join(directory_name, path)
+        self.path = os.path.join(directory_name, path)
 
     def on_epoch_end(self, epoch, logs):
         self.model.generator.save_weights("{}\epoch_{}_generator.h5".format(self.path, epoch))
