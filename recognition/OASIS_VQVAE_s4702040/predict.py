4b1f1b9be886bf381fe5dbf03ddffc3929bb79f6
JRSantos4742 <J.R.Santos@outlook.com>
2023-10-16 19:44:32 +1000

    Added code for predicting the mask of a single image
    Added code which predicts the mask of an image and saves it as a png file. Uses unsqueeze to turn the 3D tensor of [channel, width, height] to a 4D tensor of [batch size, channel, width, height].

--- recognition/OASIS_VQVAE_s4702040/predict.py
+++ recognition/OASIS_VQVAE_s4702040/predict.py
@@ -1,1 +1,50 @@
 
+import torch
+import torchvision.transforms as transforms
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if not torch.cuda.is_available():
+    print("Warning CUDA not Found. Using CPU")
+
+imageTransform_test = transforms.Compose([transforms.ToTensor(),
+                                           transforms.Normalize((0.7083, 0.5821, 0.5360), (0.0969, 0.1119, 0.1261)),
+                                           transforms.Resize((1024, 672))])
+
+# File path for loading model
+filepath = "path to file"
+
+loadedModel = torch.load(filepath)
+
+# Predict mask
+print("> Predicting")
+loadedModel.eval()
+with torch.no_grad():
+    # Path to image to be predicted
+    testImagePath = "path to file"
+    testImage = Image.open(testImagePath).convert('RGB')
+
+    testImage = imageTransform_test(testImage)
+
+    testImage = testImage.to(device)
+
+    # Convert 3D tensor of [c, w, h] to 4D tensor of [n, c, w, h]
+    testImage = testImage.unsqueeze(0)
+
+    # Predict mask based in image input
+    output = loadedModel(testImage)
+
+    # Convert 4D tensor of [n, c, w, h] to 2D tensor of [w, h]
+    output = output.view(1024, 672)
+
+    # Verify dimensions of tensor
+    #print(output.size())
+
+    # Load tensor back into cpu and turn into numpy array
+    output = output.cpu().numpy()
+
+    # Save predicted mask as png greyscale
+    plt.imsave("testPredMask.png", output, cmap="gray")
