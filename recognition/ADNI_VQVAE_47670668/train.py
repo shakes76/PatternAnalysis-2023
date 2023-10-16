fc1399871747f1cb89a17949d00892a925145193
Indira Devi <indiradevi@d-i89-184-245.student.eduroam.uq.edu.au>
2023-10-16 13:17:51 +1000

    Initialize variables for training

--- recognition/ADNI_VQVAE_47670668/train.py
+++ recognition/ADNI_VQVAE_47670668/train.py
@@ -2,6 +2,7 @@ import torch
 import torch.optim as optim
 
 from dataset import train_dataloader
+from modules import *
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 
@@ -28,3 +29,39 @@ if n < 2:
 else:
     train_data_variance = M2 / (n - 1)
 
+
+# initialize all variables for training
+num_training_updates = 30000
+
+num_hiddens = 128
+num_residual_hiddens = 32
+num_residual_layers = 2
+
+embedding_dim = 64
+
+num_embeddings = 512
+
+commitment_cost = 0.25
+
+decay = 0.99
+
+learning_rate = 1e-6
+
+
+encoder = Encoder(num_hiddens, num_residual_layers, num_residual_hiddens)
+decoder = Decoder(num_hiddens, num_residual_layers, num_residual_hiddens, embedding_dim)
+
+vq_vae = VectorQuantizer(
+      embedding_dim=embedding_dim,
+      num_embeddings=num_embeddings,
+      commitment_cost=commitment_cost
+      )
+
+pre_vq_conv1 = nn.Conv2d(in_channels=num_hiddens, out_channels=embedding_dim,
+                         kernel_size=1, stride=1)
+
+model = VQVAEModel(encoder, decoder, vq_vae, pre_vq_conv1,
+                   data_variance=train_data_variance)
+
+optimizer = optim.Adam(lr=learning_rate, params=model.parameters())
+model.to(device)
