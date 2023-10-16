aa7c4e15f49508d1a73ef0368131574c79c5ec92
JRSantos4742 <J.R.Santos@outlook.com>
2023-10-16 18:54:12 +1000

    Modified to only load images/masks with "ISIC" prefix
    Modified the custom dataset to only load images/masks with "ISIC" prefix as it was noticed that the dataset on rangapur had kept the attribution text files which flagged errors when trying to run code.
    Also modified the class so that the images and masks have their own transforms. This is mainly so that the images can be normalised through a transform without affecting the masks.

--- recognition/OASIS_VQVAE_s4702040/dataset.py
+++ recognition/OASIS_VQVAE_s4702040/dataset.py
@@ -4,12 +4,16 @@ from PIL import Image
 import os
 
 class CustomDataset(torch.utils.data.Dataset):
-    def __init__(self, image_dir, mask_dir, transform=None):
+    def __init__(self, image_dir, mask_dir, imageTransform=None, maskTransform=None):
+        prefix = "ISIC"
         self.image_dir = image_dir
         self.mask_dir = mask_dir
-        self.transform = transform
+        self.imageTransform = imageTransform
+        self.maskTransform = maskTransform
         self.images = os.listdir(image_dir)
+        self.images = [image for image in self.images if image.startswith(prefix)]
         self.masks = os.listdir(mask_dir)
+        self.masks = [mask for mask in self.masks if mask.startswith(prefix)]
 
     def __len__(self):
         return len(self.images)
@@ -19,7 +23,8 @@ class CustomDataset(torch.utils.data.Dataset):
         image = Image.open(img_path).convert('RGB')
         mask_path = os.path.join(self.mask_dir, self.masks[idx])
         mask = Image.open(mask_path).convert('L')
-        if self.transform:
-            image = self.transform(image)
-            mask = self.transform(mask)
+        if self.imageTransform:
+            image = self.imageTransform(image)
+        if self.maskTransform:
+            mask = self.maskTransform(mask)
         return (image, mask)
