4376f093f8962ba2d54161cbec297f5dbf3d3118
jackc <jack.cashman@me.com>
2023-10-17 08:23:03 +1000

    Implemented VQVAE architecture: UNTESTED

--- recognition/GENERATIVE_GAN_47431748/modules.py
+++ recognition/GENERATIVE_GAN_47431748/modules.py
@@ -12,14 +12,14 @@ class VQ(layers.Layer):
     """
     Vector-Quantiser later of VQ-VAE
     """
-    def __int__(self, num_enc, enc_dim, beta=0.25, layer_name="VectorQuant"):
+    def __int__(self, num_enc, latent_dim, beta=0.25, layer_name="VQ"):
         super().__init__(layer_name=layer_name)
-        self._enc_dim = enc_dim
+        self._latent_dim = latent_dim
         self._num_enc = num_enc
         self._beta = beta
 
         runif_initialiser = tf.random_uniform_initializer()
-        enc_shape = self._enc_dim, self._num_enc
+        enc_shape = self._latent_dim, self._num_enc
         self._encoded = tf.Variable(initial_value=runif_initialiser(shape=enc_shape, dtype='float32'))
 
     def get_encoded(self):
@@ -66,22 +66,145 @@ class VQ(layers.Layer):
         return inputs + tf.stop_gradient(qtised - inputs)
 
 class Encoder(Model):
-    def __int__(self):
-        pass
+    def __int__(self, img_size=28, latent_dim=16, name='encoder'):
+        """
+        Defined Encoder for VQ-VAE
+        :return: None
+        """
+        super(Encoder, self).__init__(name=name)
+        self._latent_dim = latent_dim
+        self._img_size = img_size
+
+        self.input1 = layers.InputLayer(input_shape=self._img_size)
+        self.conv1 = layers.Conv2D(32, 3, activation='relu', strides=2, padding='same')
+        self.conv2 = layers.Conv2D(64, 3, activation='relu', strides=2, padding='same')
+        self.conv3 = layers.Conv2D(self._latent_dim, 1, padding='same')
+
 
     def call(self, inputs):
-        pass
+        """
+        Forward computation
+        :param inputs: Inputs of the layer
+        :return: Outputs of the layer
+        """
+        inputs = self.input1(inputs)
+        hidden = self.conv1(inputs)
+        hidden = self.conv2(hidden)
+        return self.conv3(hidden)
 
 class Decoder(Model):
-    def __int__(self):
-        pass
+    def __int__(self, input_dim=16, name='decoder'):
+        """
+        Defined decoder portion of VQ-VAE
+        :return: None
+        """
+        super(Decoder, self).__init__(name=name)
+        self.input1 = layers.InputLayer(input_shape=input_dim)
+        self.convt_1 = layers.Conv2DTranspose(64, 3, activation='relu', strides=2, padding='same')
+        self.convt_2 = layers.Conv2DTranspose(32, 3, activation='relu', strides=2, padding='same')
+        self.convt_3 = layers.Conv2DTranspose(1, 3, padding='same')
 
     def call(self, inputs):
-        pass
+        """
+        Forward computation for layer
+        :param inputs: Inputs to the layer
+        :return: Outputs of the layer
+        """
+        inputs = self.input1(inputs)
+        hidden = self.convt_1(inputs)
+        hidden = self.convt_2(hidden)
+        return self.convt_3(hidden)
 
 class VQVAE(Model):
-    def __int__(self):
-        pass
+    def __int__(self, tr_var, img_size=28, num_encoded=64, latent_dim=16, beta=0.25, name='vq_vae'):
+        """
+        Defines VQVAE
+        :return: None
+        """
+        super(VQVAE, self).__init__(name=name)
+        self._tr_var = tr_var
+        self._img_size = img_size
+        self._num_enc = num_encoded
+        self._latent_dim = latent_dim
+        self._beta = beta
+
+        self._encoder = Encoder(self._img_size, self._latent_dim)
+        self._vq = VQ(self._num_enc, self._latent_dim, self._beta)
+        self._decoder = Decoder(self._encoder.output.shape[1:])
+
+        self._total_loss = tf.keras.metrics.Mean(name='total_loss')
+        self._vq_loss = tf.keras.metrics.Mean(name='vq_loss')
+        self._reconstruction_loss = tf.keras.metrics.Mean(name='reconstruction_loss')
+
 
     def call(self, inputs):
-        pass
+        """
+        Forward computation handler
+        :param inputs: Inputs to the layer
+        :return: Outputs of the layer
+        """
+        enc = self._encoder(inputs)
+        quant = self._vq(enc)
+        decoded = self._decoder(quant)
+        return decoded
+
+    def train_step(self, data):
+        """
+        Performs a single iteration of training and returns loss
+        :param data: Input data
+        :return: Loss vals
+        """
+        with tf.GradientTape() as tape:
+            recon = self.vqvae(data)
+
+            recon_loss = (tf.reduce_mean((data - recon) ** 2) / self._tr_var)
+            total_loss_val = recon_loss + sum(self.get_vq().losses)
+
+        # Backprop
+        grads = tape.gradient(total_loss_val, self.trainable_variables)
+        self.optimizer.apply_gradients(zip(grads, self.trainable_variables))
+
+        # Update loss
+        self._total_loss.update_state(total_loss_val)
+        self._reconstruction_loss.update_state(recon_loss)
+        self._vq_loss.update_state(sum(self.get_vq().losses))
+
+        # Log results
+        return {
+            'loss': self._total_loss.result(),
+            'vq_loss': self._vq_loss.result(),
+            'reconstruction_loss': self._reconstruction_loss.result()
+        }
+
+    def metrics(self):
+        """
+        Return metrics
+        :return: Metrics
+        """
+        return [
+            self._total_loss,
+            self._vq_loss,
+            self._reconstruction_loss
+        ]
+
+    def get_encoder(self):
+        """
+        Returns encoder
+        :return: Encoder of the VQVAE
+        """
+        return self._encoder
+
+    def get_vq(self):
+        """
+        Returns VQ Layer
+        :return: VQ Layer
+        """
+        return self._vq
+
+    def get_decoder(self):
+        """
+        Returns decoder
+        :return: Decoder
+        """
+        return self._decoder
+
