6cc03fd2ba9de2ef24f1ea24df12b798ca88311f
Porcupinee <htolley0@gmail.com>
2023-10-17 12:10:13 +1000

    Code cleanup + reformatting

--- recognition/s4744784/train.py
+++ recognition/s4744784/train.py
@@ -7,15 +7,16 @@ from modules import Network
 import time
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
+num_epochs = 100
 
 model = Network(upscale_factor=4, channels=1).to(device)
 optimizer = optim.Adam(model.parameters(), lr=0.001)
 criterion = nn.MSELoss()
 
 
-def train(epochs=100):
+def train(epochs):
     model.train()
-    for epoch in range(epochs):
+    for epoch in range(1, epochs + 1):
         for (inputs_low_res, _), (targets_high_res, _) in dataloader:
             inputs_low_res = inputs_low_res.to(device)
             targets_high_res = targets_high_res.to(device)
@@ -26,12 +27,9 @@ def train(epochs=100):
             loss.backward()
             optimizer.step()
         
-        # Print PSNR (Peak Signal-to-Noise Ratio)
-        psnr = 10 * math.log10(1 / loss.item())
-        print(f"Epoch [{epoch + 1}/{epochs}] Loss: {loss.item()} PSNR: {psnr}")
-        
+        print(f"Epoch [{epoch + 1}/{epochs}] Loss: {loss.item()}")
+
         # Save model
-        
         if epoch % 20 == 0:
             print("Saving model...")
             torch.save(model.state_dict(), f'./espcn_epoch_{epoch}.pth')
@@ -42,7 +40,7 @@ if __name__ == '__main__':
 
     start_time = time.time()
     print("Starting training...")
-    train()
+    train(num_epochs)
     end_time = time.time()
     print("Training finished!")
     print(f"Time taken: {end_time - start_time} seconds, or {(end_time - start_time) / 60} minutes.")
