44be2d33354d1a30a336136014eabc63155a6ae9
Loksin <loksinlegis@gmail.com>
2023-10-17 20:11:36 +1000

    created testing for model and improved training

--- recognition/Siamese/train.py
+++ recognition/Siamese/train.py
@@ -21,13 +21,53 @@ if __name__ == '__main__':
     layer = "VGG2"
     in_channels = 1
     classes = 2
-    epochs = 10
+    epochs = 3
     learning_rate = 1e-5  
 
     model = md.Siamese(layers=layer, in_channels=in_channels, classes=classes).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
+    criterion = md.ContrastiveLoss()
     
-    criterion = md.loss_func
+    #start training the model
+    model.train()
+    for epoch in range(epochs):
+        print("epoch", epoch+1)
+        for count, (img1, img2, label) in enumerate(ds.trainloader):
+            #passing the images and label to device
+            img1 = img1.to(device)
+            img2 = img2.to(device)
+            label = label.to(device)
+
+            optimizer.zero_grad()
+            output1, output2 = model(img1, img2)
+            loss = criterion(output1, output2, label)
+            loss.backward()
+            optimizer.step()
+            if count % 10 == 0:
+                print(f"Epoch number {epoch+1}\n Current loss {loss.item()}\n")
+    
+    #Now test the model
+    model.eval()
+    correct = 0
+    total = 0
+    for (img1, img2, label) in ds.testloader:
+        img1 = img1.to(device)
+        img2 = img2.to(device)
+        label = label.to(device)
+        output1, output2 = model(img1, img2)
+        distance = torch.nn.functional.pairwise_distance(output1, output2)
+        pred = torch.where(distance > 0.5, 0.0, 1.0)
+        right = torch.where(label == pred, 1, 0)
+        guesses = right.size(dim=0)
+        total = total + guesses
+        correct = correct + torch.sum(right).item()
+
+    print(correct/total * 100)
+
+
+
+
+
         
     
 
