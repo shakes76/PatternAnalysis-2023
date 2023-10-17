350f23d91c96b38a016629b4cfa9bbf49ece665b
masaet <masaet@stud.ntnu.no>
2023-10-18 03:04:11 +1000

    changed custom dataset to take a list of paths as input. Created a function to sort folders into test and trainset, and return a list of paths to input in custom dataset class

--- recognition/isic178_unet_s4824209/dataset.py
+++ recognition/isic178_unet_s4824209/dataset.py
@@ -2,44 +2,66 @@ from torch.utils.data import Dataset
 import os
 from PIL import Image
 import numpy as np
+from sklearn.model_selection import train_test_split
 
 class customDataset(Dataset):
-    def __init__(self, data_path, GT_path, d_transform, g_transform):
+    def __init__(self, images, GT,transform):
         super(customDataset, self).__init__()
 
-        self.f_path = data_path
-        self.image_path = sorted(os.listdir(data_path))
+        self.images = images
+        self.GT = GT
+
+        self.transform = transform
         
-        #self.image_path.remove('LICENCE.txt')
-        #self.image_path.remove('ATTRIBUTION.txt')
-
-        self.g_path = GT_path
-        self.GT_path = sorted(os.listdir(GT_path))
-        #self.GT_path.remove('LICENCE.txt')
-        #self.GT_path.remove('ATTRIBUTION.txt')
-
-        self.data_transform = d_transform
-        self.gt_transform = g_transform
         pass
 
     def __len__(self):
-        if len(self.image_path) == len(self.GT_path):
-            return(len(self.image_path))
+        if len(self.images) == len(self.GT):
+            return(len(self.images))
         else:
             print(">>data and ground truth not same lenght")
 
     def __getitem__(self, idx):    
-        img_name = os.path.join(self.f_path, self.image_path[idx])
-        img = Image.open(img_name)
+        img = Image.open(self.images[idx])
+        GT = Image.open(self.GT[idx])
 
-        GT_name = os.path.join(self.g_path, self.GT_path[idx])
-        GT = Image.open(GT_name)
-
-        if self.data_transform:
-            img = self.data_transform(img)
-            GT = self.gt_transform(GT)
-
-
+        if self.transform:
+            img, GT = self.transform((img, GT))
+            
+            
         return img, GT
+    
 
-        
+def data_sorter(img_root, gt_root):
+    img_path = sorted(os.listdir(img_root))
+    gt_path = sorted(os.listdir(gt_root))
+
+    images_train = []
+    gt_train = []
+    images_test = []
+    gt_test = []
+
+    #create a list of indices in the data
+    indices = list(range(len(img_path)))
+
+    train_indices, test_indices = train_test_split(indices, train_size=0.8, test_size=0.2)
+
+    for idx in train_indices:
+        if img_path[idx] == 'ATTRIBUTION.txt' or img_path[idx] == 'LICENSE.txt':
+            continue
+        image = os.path.join(img_root, img_path[idx])
+        gt = os.path.join(gt_root, gt_path[idx])
+
+        images_train.append(image)
+        gt_train.append(gt)
+
+    for idx in test_indices:
+        if img_path[idx] == 'ATTRIBUTION.txt' or img_path[idx] == 'LICENSE.txt':
+            continue
+        image = os.path.join(img_root, img_path[idx])
+        gt = os.path.join(gt_root, gt_path[idx])
+
+        images_test.append(image)
+        gt_test.append(gt)
+
+    return images_train, gt_train, images_test, gt_test
