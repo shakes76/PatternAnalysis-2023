4d682a384ce0cef54c57ff625a97a12e4d8f4a85
Shriyan-Garg <gargshriyan23@gmail.com>
2023-10-18 00:51:18 +1000

    added comments and header block in train.py

--- recognition/VisionTransformer_s4676080/train.py
+++ recognition/VisionTransformer_s4676080/train.py
@@ -1,4 +1,8 @@
-# This is the train file
+"""
+train.py: Initializes, trains and test the VisionTransformer on AD 
+and NC image data. After training, the model is saved as well. It also provides visualization of training 
+and testing metrics.
+"""
 
 import torch
 import torch.nn as nn
@@ -9,14 +13,39 @@ import matplotlib.pyplot as plt
 
 # Model configurations
 def initialize_model():
+    """
+    Initialize the model, criterion, optimizer, and learning rate scheduler.
+
+    Returns:
+    - model: An instance of VisionTransformer.
+    - criterion: Cross-entropy loss for classification.
+    - optimizer: Adam optimizer.
+    - scheduler: Learning rate scheduler.
+    """
     model = VisionTransformer().to(device)
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=1e-4, weight_decay=2e-4)  
+    
+    # Implement learning rate scheduler for dynamic adjustment of learning rate
     scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=3, factor=0.7, verbose=True)
     torch.cuda.empty_cache()
     return model, criterion, optimizer, scheduler
 
 def train_one_epoch(model, criterion, optimizer, train_loader):
+    """
+    Train the model for one epoch.
+
+    Parameters:
+    - model: Model to be trained.
+    - criterion: Loss function.
+    - optimizer: Optimization algorithm.
+    - train_loader: DataLoader for the training set.
+
+    Returns:
+    - running_loss: Total loss for this epoch.
+    - correct_train: Number of correctly classified instances.
+    - total_train: Total instances in the dataset.
+    """
     model.train()
     running_loss, correct_train, total_train = 0.0, 0, 0
     for images, labels in train_loader:
@@ -28,6 +57,8 @@ def train_one_epoch(model, criterion, optimizer, train_loader):
         correct_train += (predicted_train == labels).sum().item()
         loss = criterion(outputs, labels)
         loss.backward()
+
+        # Gradient clipping to avoid large gradients
         torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
         running_loss += loss.item() * images.size(0)
@@ -36,6 +67,19 @@ def train_one_epoch(model, criterion, optimizer, train_loader):
 
 # Testing loop
 def test_model(model, criterion, test_loader):
+    """
+    Evaluate the model's performance on the test set.
+
+    Parameters:
+    - model: Model to be tested.
+    - criterion: Loss function.
+    - test_loader: DataLoader for the test set.
+
+    Returns:
+    - running_loss: Total loss for the test set.
+    - correct_test: Number of correctly classified instances.
+    - total_test: Total instances in the test dataset.
+    """
     model.eval()
     running_loss, correct_test, total_test = 0.0, 0, 0
     with torch.no_grad():
@@ -50,6 +94,15 @@ def test_model(model, criterion, test_loader):
     return running_loss, correct_test, total_test
 
 def plot_metrics(train_losses, test_losses, train_accuracies, test_accuracies):
+    """
+    Visualize training and testing metrics across epochs and save the fig.
+
+    Parameters:
+    - train_losses: List of training losses.
+    - test_losses: List of test losses.
+    - train_accuracies: List of training accuracies.
+    - test_accuracies: List of test accuracies.
+    """
     epochs = list(range(1, len(train_losses) + 1))  
     
     plt.figure(figsize=(10, 4))
@@ -88,7 +141,7 @@ if __name__ == "__main__":
     counter = 0
 
     
-    print("Training started!")
+    #print("Training started!")
     for epoch in range(1, num_epochs + 1):  # Changed the range to fix the numbering issue
         train_loss, correct_train, total_train = train_one_epoch(model, criterion, optimizer, train_loader)
         test_loss, correct_test, total_test = test_model(model, criterion, test_loader)
@@ -114,7 +167,7 @@ if __name__ == "__main__":
                 print("Early stopping triggered!")
                 break
 
-    print("Training finished!")
+    #print("Training finished!")
     plot_metrics(train_losses, test_losses, train_accuracies, test_accuracies)
     print(f"Best Training Accuracy: {max(train_accuracies):.2f}%")
     print(f"Best Test Accuracy: {max(test_accuracies):.2f}%")
