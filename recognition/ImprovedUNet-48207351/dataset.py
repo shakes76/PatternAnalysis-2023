ad9f304866e2f90c171ba241ffd746f1af74fbaf
Selma <selma.gudmundsen@gmail.com>
2023-10-18 09:40:32 +1000

    small edits for the dataloader

--- recognition/ImprovedUNet-48207351/dataset.py
+++ recognition/ImprovedUNet-48207351/dataset.py
@@ -1,28 +1,43 @@
 import os
 from torchvision import transforms
-from torchvision.datasets import ImageFolder
-from torch.utils.data import DataLoader, Dataset, random_split
+from torch.utils.data import Dataset
+from PIL import Image
+import numpy as np
+
 
 # Define the dataset root directory and transformation for data preprocessing.
 data_root = '/ISIC-2017_Training_Data'
-transform = transforms.Compose([
-    transforms.Resize((128, 128)),  # Resize the images to 128x128.
-    transforms.ToTensor(),  # Convert to a PyTorch tensor.
-])
 
-# Load the ISIC 2017 dataset.
-def load_isic_dataset(batch_size, split_ratio=(0.8, 0.1, 0.1), num_workers=4):
-    dataset = ImageFolder(root=data_root, transform=transform)
+def transform_image():
+    transform = transforms.Compose([
+        transforms.Resize((128, 128)),  # Resize the images to 128x128.
+        transforms.ToTensor(),  # Convert to a PyTorch tensor.
+    ])
+    return transform
 
-    # Split the dataset into training, validation, and test sets.
-    train_size = int(len(dataset) * split_ratio[0])
-    val_size = int(len(dataset) * split_ratio[1])
-    test_size = len(dataset) - train_size - val_size
-    train_set, val_set, test_set = random_split(dataset, [train_size, val_size, test_size])
+# Loading the data (source: https://github.com/aladdinpersson/Machine-Learning-Collection/blob/master/ML/Pytorch/image_segmentation/semantic_segmentation_unet/dataset.py)
+class ISICDataLoader(Dataset):
+    def __init__(self, image_dir, mask_dir, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
+        self.transform = transform
+        self.images = os.listdir(image_dir)
 
-    # Create DataLoader instances for each split.
-    train_loader = DataLoader(train_set, batch_size=batch_size, shuffle=True, num_workers=num_workers)
-    val_loader = DataLoader(val_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-    test_loader = DataLoader(test_set, batch_size=batch_size, shuffle=False, num_workers=num_workers)
+    def __len__(self):
+        return len(self.images)
 
-    return train_loader, val_loader, test_loader
+    def __getitem__(self, index):
+        img_path = os.path.join(self.image_dir, self.images[index])
+        mask_path = os.path.join(self.mask_dir, self.images[index].replace(".png", "_mask.gif"))
+        image = np.array(Image.open(img_path).convert("RGB"))
+        mask = np.array(Image.open(mask_path).convert("L"), dtype=np.float32)
+        mask[mask == 255.0] = 1.0
+
+        if self.transform is not None:
+            augmentations = self.transform(image=image, mask=mask)
+            image = augmentations["image"]
+            mask = augmentations["mask"]
+
+        return image, mask
+
+
--- recognition/ImprovedUNet-48207351/train.py
+++ recognition/ImprovedUNet-48207351/train.py
@@ -1,23 +1,70 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+from torch.utils.data import DataLoader, Dataset
 import numpy as np
-import modules
 
-model = modules.ImprovedUNET()
+from modules import ImprovedUNET
+from dataset import train_loader, val_loader, test_loader
 
 # Hyperparameters etc.
 LEARNING_RATE = 1e-4
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 BATCH_SIZE = 16
-NUM_EPOCHS = 3
+NUM_EPOCHS = 500
 NUM_WORKERS = 2
 IMAGE_HEIGHT = 160  # 1280 originally
 IMAGE_WIDTH = 240  # 1918 originally
 PIN_MEMORY = True
 LOAD_MODEL = False
-TRAIN_IMG_DIR = "data/train_images/"
-TRAIN_MASK_DIR = "data/train_masks/"
-VAL_IMG_DIR = "data/val_images/"
-VAL_MASK_DIR = "data/val_masks/"
+
+# Define the Dice loss function
+class DiceLoss(nn.Module):
+    def __init__(self):
+        super(DiceLoss, self).__init__()
+
+    def forward(self, output, target):
+        smooth = 1.0
+        intersection = torch.sum(output * target)
+        union = torch.sum(output) + torch.sum(target)
+        dice = (2.0 * intersection + smooth) / (union + smooth)
+        return 1.0 - dice
+    
+# Device configuration
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+if not torch.cuda.is_available():
+    print("Warning CUDA not Found. Using CPU")
+
+
+
+def train_unet_with_dice_loss(data, labels, num_epochs=300, batch_size=2, lr_init=5e-4, weight_decay=1e-5):
+    # Create a DataLoader for the dataset
+    dataloader = DataLoader(val_loader, batch_size=batch_size, shuffle=True)
+
+    # Define the loss function (Dice loss) and optimizer
+    criterion = DiceLoss()
+    optimizer = optim.Adam(ImprovedUNET.parameters(), lr=lr_init, weight_decay=weight_decay)
+
+    # Training loop
+    for epoch in range(num_epochs):
+        for batch_idx, (input_data, target) in enumerate(dataloader):
+            ImprovedUNET.train()
+            optimizer.zero_grad()
+            output = ImprovedUNET(input_data)
+
+            # Compute the Dice loss
+            loss = criterion(output, target)
+
+            # Backpropagation and optimization
+            loss.backward()
+            optimizer.step()
+
+            print(f"Epoch [{epoch}/{num_epochs}] Batch [{batch_idx}/{len(dataloader)}] Loss: {loss.item()}")
+
+if __name__ == "__main__":
+    # Load your data and labels here
+    data = np.random.randn(300, 1, 128, 128, 128)  # Replace with your actual data
+    labels = np.random.randint(0, 2, (300, 1, 128, 128, 128))  # Replace with your actual labels
+
+    train_unet_with_dice_loss(data, labels)
 
