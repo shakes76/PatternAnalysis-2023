9d2922cbf64ed021f3ba34852a1364f3770321e6
Loksin <loksinlegis@gmail.com>
2023-10-17 12:13:36 +1000

    added custom contrastive loss function

--- recognition/Siamese/module.py
+++ recognition/Siamese/module.py
@@ -8,16 +8,13 @@ import torch.utils as util_data
 import torch
 import torch.nn as nn
 
-#device configuration
-device = torch.device("cuda")
-
 def cfg(layer):
     if layer == 'VGG2' :
         return [32, 32, 'M']
 
 #define Siamese model
 class Siamese(nn.Module):
-    def __init__(self, layers, in_channels = 1, classes = 2):
+    def __init__(self, layers, in_channels, classes):
         super().__init__()
         self.in_channels = in_channels
         self.classes = classes
@@ -46,3 +43,13 @@ class Siamese(nn.Module):
         output2 = output2.view(output2.size(0), -1)
         output2 = self.classifier(output2)
         return output1, output2
+    
+def loss_func(margin, img1, img2, label):
+    if label == 1: #This means both images are in the same class
+        distance = torch.nn.functional.pairwise_distance(img1, img2)
+        return torch.mean(torch.pow(distance))
+    if label == 0: #This means both images are from different classes
+        distance = torch.nn.functional.pairwise_distance(img1, img2)
+        reverse_dist = margin - distance #reverse distance find's difference between images
+        reverse_dist = torch.clamp(reverse_dist, min=0.0)
+        return torch.mean(torch.pow(reverse_dist, 2))
