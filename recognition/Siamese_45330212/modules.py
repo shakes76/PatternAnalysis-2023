2ef38ac83a9960959f8a6b45e71d3478fe81cdb6
davidrtlake <davidrtlake@gmail.com>
2023-10-17 19:57:38 +1000

    Implemented basic Siamese Network CNN with dataloaders

--- recognition/Siamese_45330212/modules.py
+++ recognition/Siamese_45330212/modules.py
@@ -1,6 +1,6 @@
 # Contains the source code of the components of your model. Each component must be
 # implementated as a class or a function
-# Import all the necessary Library 
+# Import all the necessary libraries 
 import torchvision
 import torch.utils.data as utils
 from torchvision import datasets
@@ -29,3 +29,64 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if not torch.cuda.is_available():
     print("Warning CUDA not Found. Using CPU")
 
+class SiameseNetwork(nn.Module):
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        
+        # Setting up the Sequential of CNN Layers
+        self.cnn1 = nn.Sequential(
+            
+            nn.Conv2d(3, 96, kernel_size=11,stride=1),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
+            nn.MaxPool2d(3, stride=2),
+            
+            nn.Conv2d(96, 256, kernel_size=5,stride=1,padding=2),
+            nn.ReLU(inplace=True),
+            nn.LocalResponseNorm(5,alpha=0.0001,beta=0.75,k=2),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout(p=0.3),
+
+            nn.Conv2d(256,384 , kernel_size=3,stride=1,padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384,256 , kernel_size=3,stride=1,padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, stride=2),
+            nn.Dropout(p=0.3),
+
+        )
+        
+        # Defining the fully connected layers
+        self.fc1 = nn.Sequential(
+            nn.Linear(783, 1024),
+            nn.ReLU(inplace=True),
+            nn.Dropout(p=0.5),
+            
+            nn.Linear(1024, 128),
+            nn.ReLU(inplace=True),
+            
+            nn.Linear(128,2))
+        
+  
+    def forward_once(self, x):
+        # Forward pass 
+        output = self.cnn1(x)
+        output = output.view(output.size()[0], -1)
+        output = self.fc1(output)
+        return output
+
+    def forward(self, input1, input2):
+        # forward pass of input 1
+        output1 = self.forward_once(input1)
+        # forward pass of input 2
+        output2 = self.forward_once(input2)
+        return output1, output2
+    
+trainset = datasets.ImageFolder('C:\\Users\\david\\OneDrive\\Documents\\0NIVERSITY\\2023\\SEM2\\COMP3710\\Project\\PatternAnalysis-2023\\recognition\\Siamese_45330212\\AD_NC\\train', transform=transform_train)
+train_loader = torch.utils.data.DataLoader(trainset, batch_size=Config.train_batch_size, shuffle=True)
+
+testset = datasets.ImageFolder('C:\\Users\\david\\OneDrive\\Documents\\0NIVERSITY\\2023\\SEM2\\COMP3710\\Project\\PatternAnalysis-2023\\recognition\\Siamese_45330212\\AD_NC\\test', transform=transform_train)
+test_loader = torch.utils.data.DataLoader(testset, batch_size=Config.train_batch_size, shuffle=True)
+
+model = SiameseNetwork()
+model = model.to(device)
