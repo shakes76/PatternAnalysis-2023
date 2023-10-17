035c582d5a11a7ed1f3f55e4b0ec030a5ed56380
s4698290 <s4698290@login0.compute.eait.uq.edu.au>
2023-10-17 23:50:58 +1000

    added data augmentation, hyperparameters are set to what was used to train model, normalisation removed

--- recognition/VisualTransformerAlzheimers_Marc_Mennes_s4698290/train.py
+++ recognition/VisualTransformerAlzheimers_Marc_Mennes_s4698290/train.py
@@ -6,33 +6,44 @@ import torch
 import time
 
 CROPSIZE = 240
-PATHTODATASET = "/home/marc/Documents/PatternAnalysisReport/PatternAnalysis-2023/recognition/VisualTransformerAlzheimers_Marc_Mennes_s4698290/ADNI_AD_NC_2D"
-#PATHTODATASET = "/home/Student/s4698290/report/ADNI"
-ENCODERDENSELAYERS = [[800, 1000, 800]]*3
-LR = 0.000001
+#PATHTODATASET = "/home/marc/Documents/PatternAnalysisReport/PatternAnalysis-2023/recognition/VisualTransformerAlzheimers_Marc_Mennes_s4698290/ADNI_AD_NC_2D"
+PATHTODATASET = "/home/Student/s4698290/report/ADNI"
+ENCODERDENSELAYERS = [[512, 512, 512]]*3
+LR = 0.00001
 BATCHSIZE = 128
-EPOCHS = 40
+EPOCHS = 60
 
 #use gpu if available
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(torch.version.cuda)
 print('cuda' if torch.cuda.is_available() else 'cpu')
 
-transform = torchvision.transforms.Compose(
+#dataset mean: 31.5226 dataset std: 58.8811
+trainTransform = torchvision.transforms.Compose(
     [torchvision.transforms.CenterCrop(CROPSIZE),
-     torchvision.transforms.Lambda(lambda x: x/255), #use the format of image data between 0 and 1 not 0 and 255
-     torchvision.transforms.Normalize(0.1236, 0.2309), #normalize the image (values determined from function in dataset.py)
-     torchvision.transforms.Lambda(lambda x: x.unfold(1,CROPSIZE//10, CROPSIZE//10).unfold(2,CROPSIZE//10, CROPSIZE//10)),#split the image into 9 patches
+     torchvision.transforms.RandAugment(num_ops=4), #augment the data with random transforms
+     torchvision.transforms.Lambda(lambda x: x/255),
+     #torchvision.transforms.Normalize(31.5226/255, 58.8811/255), #normalize the image (values determined from function in dataset.py)
+     torchvision.transforms.Lambda(lambda x: x.unfold(1,CROPSIZE//4, CROPSIZE//4).unfold(2,CROPSIZE//4, CROPSIZE//4)),#split the image into 9 patches
      torchvision.transforms.Lambda(lambda x: x[0])#removes the color channel dimension as this is greyscale
     ]
 )
-
-trainData = dataset.ADNI(PATHTODATASET, transform=transform)
-validationData = dataset.ADNI(PATHTODATASET, transform=transform, validation=True)
+valTransform = torchvision.transforms.Compose(
+    [torchvision.transforms.CenterCrop(CROPSIZE),
+     torchvision.transforms.Lambda(lambda x: x/255),
+     #torchvision.transforms.Normalize(31.5226/255, 58.8811/255), #normalize the image (values determined from function in dataset.py)
+     torchvision.transforms.Lambda(lambda x: x.unfold(1,CROPSIZE//4, CROPSIZE//4).unfold(2,CROPSIZE//4, CROPSIZE//4)),#split the image into 9 patches
+     torchvision.transforms.Lambda(lambda x: x[0])#removes the color channel dimension as this is greyscale
+    ]
+)
+trainData = dataset.ADNI(PATHTODATASET, transform=trainTransform)
+validationData = dataset.ADNI(PATHTODATASET, transform=valTransform, validation=True)
 trainLoader = DataLoader(trainData, batch_size=BATCHSIZE, shuffle=True)
 validationLoader = DataLoader(validationData, batch_size=BATCHSIZE, shuffle=True)
 
-transformer = modules.ADNIConvTransformer(0, [800, 1000, 800], ENCODERDENSELAYERS).to(device)
+#transformer = modules.ADNITransformer(16, 60, 4, 0.2, [512], ENCODERDENSELAYERS).to(device)
+#transformer = modules.ADNIConvTransformer(0.2, [512], ENCODERDENSELAYERS).to(device)
+transformer = torch.load("transformer_model")
 
 loss = torch.nn.BCELoss()
 optimizer = torch.optim.Adam(transformer.parameters(), lr=LR)
@@ -49,6 +60,7 @@ print("training...")
 for epoch in range(EPOCHS):
     
     batchAccuracies = []
+    batchLoss = []
     l = 0
     for batch in trainLoader:
 
@@ -65,8 +77,9 @@ for epoch in range(EPOCHS):
         predictions = torch.round(outputs[:, 0].detach()).to(torch.uint8)
         trainAccuracy = torch.sum(predictions == labels)/(labels.size()[0])
         batchAccuracies.append(trainAccuracy)
-        trainLoss.append(l.item())
-        
+        batchLoss.append(l.item())
+    
+    trainLoss.append(sum(batchLoss)/len(batchLoss)) 
     trainAccuracies.append(sum(batchAccuracies)/len(batchAccuracies))
 
     torch.save(transformer, "transformer_model")
@@ -74,6 +87,7 @@ for epoch in range(EPOCHS):
     torch.no_grad()
     transformer.eval()
     batchAccuracies = []
+    batchLoss = []
     #run through validation set
     for batch in validationLoader:
         images, labels = batch[0].to(device), batch[1].to(device)
@@ -85,9 +99,10 @@ for epoch in range(EPOCHS):
         
         valAccuracy = torch.sum(predictions == labels)/(labels.size()[0])
         batchAccuracies.append(valAccuracy)
-        valLoss.append(l.item())
+        batchLoss.append(l.item())
     
     valAccuracies.append(sum(batchAccuracies)/len(batchAccuracies))
+    valLoss.append(sum(batchLoss)/len(batchLoss))
         
 
     torch.enable_grad()
