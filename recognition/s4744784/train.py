19d726ff07abb6ad1505bfdccf05b5efc1548565
Porcupinee <htolley0@gmail.com>
2023-10-17 15:06:42 +1000

    Refactoring training  model + code cleanup

--- recognition/s4744784/train.py
+++ recognition/s4744784/train.py
@@ -8,31 +8,38 @@ import time
 
 device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
 num_epochs = 100
+learning_rate = 0.001
 
 model = Network(upscale_factor=4, channels=1).to(device)
-optimizer = optim.Adam(model.parameters(), lr=0.001)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 criterion = nn.MSELoss()
 
 
-def train(epochs):
+def train(epochs: int):
     model.train()
+    running_loss = 0.0
+
     for epoch in range(1, epochs + 1):
-        for (inputs_low_res, _), (targets_high_res, _) in dataloader:
-            inputs_low_res = inputs_low_res.to(device)
-            targets_high_res = targets_high_res.to(device)
-            
+        for input, label in dataloader:
+
+            input.to(device)
+
             optimizer.zero_grad()
-            outputs = model(inputs_low_res)
-            loss = criterion(outputs, targets_high_res)
+
+            outputs = model(input)
+            loss = criterion(outputs, input)
+
             loss.backward()
             optimizer.step()
+
+            running_loss += loss.item()
         
-        print(f"Epoch [{epoch + 1}/{epochs}] Loss: {loss.item()}")
+        print(f"Epoch [{epoch + 1}/{epochs}] Loss: {running_loss/len(dataloader)}")
 
         # Save model
-        if epoch % 20 == 0:
-            print("Saving model...")
-            torch.save(model.state_dict(), f'./espcn_epoch_{epoch}.pth')
+        if epoch in [20, 40, 60, 80, 100]:
+            print("Saving model on epoch " + str(epoch) + "...")
+            torch.save(model.state_dict(), f'./Trained_Model.pth')
             print("Model saved!")
 
 if __name__ == '__main__':
