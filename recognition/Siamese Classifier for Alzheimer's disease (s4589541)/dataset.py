26ad5c000d0100d6c0c8ef8e10a34ed485f1dbf8
milesgardiner <68379513+milesgardiner@users.noreply.github.com>
2023-10-18 00:36:35 +1000

    Added custom transform to dataset.py
    Realised that images were being correctly converted to tensor, it just looked like it was empty due to the black pixels around the sidesof the image.
    Added the OneChannel transform to remove the extra channels as the RGB information is not needed (just brightness).

--- recognition/Siamese Classifier for Alzheimer's disease (s4589541)/dataset.py
+++ recognition/Siamese Classifier for Alzheimer's disease (s4589541)/dataset.py
@@ -1,15 +1,25 @@
 # loading and preprocessing of data
+import torch
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import matplotlib.pyplot as plt
 from utils import show_image, train_valid_split
 
-BATCH_SIZE = 4
+BATCH_SIZE = 64
+
+class OneChannel:
+    """Custom transform class to discard extra image channels."""
+    @staticmethod
+    def __call__(img_tensor):
+        return img_tensor[0]
+
 
 # setup the transforms for the images
 transform = transforms.Compose([
-    # transforms.ToTensor()
+    transforms.Resize((256, 240)),
+    transforms.ToTensor(),
+    OneChannel()
 ])
 
 # set up the datasets
@@ -26,15 +36,24 @@ test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=True, num_work
 
 if __name__ == '__main__':
     # train_valid_split()
-    data = train_set[0]
-    print(data)
-    print(len(train_set))
-    show_image(data)
+    # data = train_set[0]
+    # print(data)
+    # print(len(train_set))
+    # show_image(data)
+    # transformed = transforms.ToTensor(data)
+    # print(transformed)
 
     data = valid_set[0]
     print(data)
     print(len(valid_set))
-    show_image(data)
+    print(data[0].size())
+    # for channel in data[0]:
+        # print(channel)
+        # print(len(channel))
+        # print(torch.max(channel))
+    # show_image(data)
+    # transformed = transforms.ToTensor()(data[0])
+    # print(transformed)
 
     # print(data)
     # show_image(data)
