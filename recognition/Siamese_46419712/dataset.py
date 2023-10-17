8b676de8723e40825c44b437dbbbe0acec59266c
MTbir <m.tieu@uqconnect.edu.au>
2023-10-17 15:28:06 +1000

    fix up the error incorrect test path

--- recognition/Siamese_46419712/dataset.py
+++ recognition/Siamese_46419712/dataset.py
@@ -4,10 +4,8 @@ import torchvision.transforms as transforms
 import random
 
 TRAIN_PATH = "/home/groups/comp3710/ADNI/AD_NC/train"
-# TRAIN_PATH = "./AD_NC/train"
 
-TEST_PATH = "/home/groups/comp3710/ADNI/AD_NC/train"
-# TEST_PATH = "./AD_NC/train"
+TEST_PATH = "/home/groups/comp3710/ADNI/AD_NC/test"
 
 class ClassifierDataset(torch.utils.data.Dataset):
     """
