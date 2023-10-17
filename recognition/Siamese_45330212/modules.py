fd5dced0aa1553e545ae10498d9d68d0a0a1a9e0
davidrtlake <davidrtlake@gmail.com>
2023-10-17 23:12:22 +1000

    Implemented Custom DataSet loader to load image pairs and their labels from the folder. Also added the ContrastiveLoss function to calculate the constrastive loss when training the model. Have not added the training loop yet - soon to come.

--- recognition/Siamese_45330212/modules.py
+++ recognition/Siamese_45330212/modules.py
@@ -1,6 +1,6 @@
 # Contains the source code of the components of your model. Each component must be
 # implementated as a class or a function
-# Import all the necessary libraries 
+# Import all the necessary libraries
 import torchvision
 import torch.utils.data as utils
 from torchvision import datasets
@@ -28,6 +28,12 @@ import random
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if not torch.cuda.is_available():
     print("Warning CUDA not Found. Using CPU")
+
+class Config():
+    training_dir = "../AD_NC/train"
+    testing_dir = "../AD_NC/test"
+    train_batch_size = 8
+    train_number_epochs = 1
 
 class SiameseNetwork(nn.Module):
     def __init__(self):
@@ -58,7 +64,7 @@ class SiameseNetwork(nn.Module):
         
         # Defining the fully connected layers
         self.fc1 = nn.Sequential(
-            nn.Linear(783, 1024),
+            nn.Linear(200448, 1024),
             nn.ReLU(inplace=True),
             nn.Dropout(p=0.5),
             
@@ -82,7 +88,83 @@ class SiameseNetwork(nn.Module):
         output2 = self.forward_once(input2)
         return output1, output2
     
-trainset = datasets.ImageFolder('C:\\Users\\david\\OneDrive\\Documents\\0NIVERSITY\\2023\\SEM2\\COMP3710\\Project\\PatternAnalysis-2023\\recognition\\Siamese_45330212\\AD_NC\\train', transform=transform_train)
+class CustomSiameseNetworkDataset(Dataset):
+    def __init__(self, root_dir, transform=None):
+        self.root_dir = root_dir
+        self.transform = transform
+
+        # Create a list of image paths
+        self.image_paths = []
+        self.labels = []
+        folders = os.listdir(root_dir)
+        print("> Creating image paths")
+        c = 0
+        for i, folder1 in enumerate(folders):
+            for j, folder2 in enumerate(folders):
+                print("Folder:", folder1, folder2)
+                if i == j:
+                    label = 0  # Images from the same folder
+                else:
+                    label = 1  # Images from different folders
+
+                folder1_path = os.path.join(root_dir, folder1)
+                folder2_path = os.path.join(root_dir, folder2)
+
+                for img1 in os.listdir(folder1_path):
+                    c += 1
+                    if c % 1000 == 0:
+                        print("Count:", c)
+                    img2 = random.choice(os.listdir(folder2_path))
+                    while img1 == img2:
+                        img2 = random.choice(os.listdir(folder2_path))
+
+                img1_path = os.path.join(folder1_path, img1)
+                img2_path = os.path.join(folder2_path, img2)
+
+                self.image_paths.append((img1_path, img2_path))
+                self.labels.append(label)
+                        
+        print("< Finished creating image paths. #Images:", len(self.image_paths))
+
+    def __len__(self):
+        return len(self.image_paths)
+
+    def __getitem__(self, index):
+        print("Getting item")
+        img1_path, img2_path = self.image_paths[index]
+        img1 = Image.open(img1_path).convert("RGB")
+        img2 = Image.open(img2_path).convert("RGB")
+
+        if self.transform is not None:
+            img1 = self.transform(img1)
+            img2 = self.transform(img2)
+
+        label = torch.tensor(self.labels[index], dtype=torch.float32)
+        
+        return img1, img2, label
+    
+class ContrastiveLoss(torch.nn.Module):
+    """
+    Contrastive loss function.
+    Based on: http://yann.lecun.com/exdb/publis/pdf/hadsell-chopra-lecun-06.pdf
+    """
+
+    def __init__(self, margin=2.0):
+        super(ContrastiveLoss, self).__init__()
+        self.margin = margin
+
+    def forward(self, output1, output2, label):
+        euclidean_distance = F.pairwise_distance(output1, output2)
+        loss_contrastive = torch.mean((1-label) * torch.pow(euclidean_distance, 2) +
+                                      (label) * torch.pow(torch.clamp(self.margin - euclidean_distance, min=0.0), 2))
+        return loss_contrastive
+    
+transform_train = transforms.Compose([
+    transforms.ToTensor(),
+])
+    
+# trainset = datasets.ImageFolder('C:\\Users\\david\\OneDrive\\Documents\\0NIVERSITY\\2023\\SEM2\\COMP3710\\Project\\PatternAnalysis-2023\\recognition\\Siamese_45330212\\AD_NC\\train', transform=transform_train)
+trainset = CustomSiameseNetworkDataset(root_dir='C:\\Users\\david\\OneDrive\\Documents\\0NIVERSITY\\2023\\SEM2\\COMP3710\\Project\\PatternAnalysis-2023\\recognition\\Siamese_45330212\\AD_NC\\train', transform=transform_train)
 train_loader = torch.utils.data.DataLoader(trainset, batch_size=Config.train_batch_size, shuffle=True)
 
 testset = datasets.ImageFolder('C:\\Users\\david\\OneDrive\\Documents\\0NIVERSITY\\2023\\SEM2\\COMP3710\\Project\\PatternAnalysis-2023\\recognition\\Siamese_45330212\\AD_NC\\test', transform=transform_train)
