b245dad031abcd196f69db927399898c8e92b70f
lambdarm <141205527+lambdarm@users.noreply.github.com>
2023-10-18 00:46:08 +1000

    Updated modules.py
    Split into classifier and siamese model (unknown before, had to research)
    created create_classifier function
    updated siamese model to work with triplet_loss

--- recognition/ADNI-s4702860/modules.py
+++ recognition/ADNI-s4702860/modules.py
@@ -1,77 +1,60 @@
 import tensorflow as tf
-from tensorflow.keras.models import Sequential, Model
-from tensorflow.keras.layers import Dense, Flatten, Conv2D, MaxPool2D, Input
+from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.models import Model
 
-"""
-Model class used to create a Siamese Neural Network
-
-Inspiration taken from 
-"https://github.com/schatty/siamese-networks-tf/blob/master/siamnet/models/siamese.py"
-"""
 class SiameseModel:
-    """
-    Initialises the model class by calling the create_network() function
-    """
     def __init__(self):
         self.model = self.create_network()
-    
-    """
-    Creates the base network shared by the Anchor, Positive and Negative inputs
-    Assumes input shape is (256, 240, 3)
-    
-    Returns:
-        base_model - the model to be used for the anchor, positive and negative input
-    """
+
     def base_network(self):
-        base_model = Sequential()
-        base_model.add(Conv2D(64, (3, 3), activation='relu', input_shape=(256, 240, 3)))
-        base_model.add(MaxPool2D())
-        base_model.add(Conv2D(128, (3, 3), activation='relu'))
-        base_model.add(MaxPool2D())
-        base_model.add(Flatten())
-        base_model.add(Dense(256, activation='relu'))
-        base_model.add(Dense(128, activation='relu'))
-        return base_model
-    
-    """
-    Function used to define the triplet loss
+        input_image = Input(shape=(256, 240, 3))
+        x = Conv2D(64, (3, 3), activation='relu')(input_image)
+        x = MaxPooling2D()(x)
+        x = Conv2D(128, (3, 3), activation='relu')(x)
+        x = MaxPooling2D()(x)
+        x = Flatten()(x)
+        x = Dense(256, activation='relu')(x)
+        x = Dense(128, activation='relu')(x)
+        return Model(input_image, x)
 
-    Inputs:
-        anchor - the anchor image
-        positive - image the same class as the anchor
-        negative - image the opposite class of the anchor
-        margin - margin between positive and negative pairs, default is 0.1
-
-    triplet loss as defined by "https://en.wikipedia.org/wiki/Triplet_loss"
-    """
-    def triplet_loss(self, anchor, positive, negative, margin=0.1):
+    def triplet_loss(self, y_true, y_pred, alpha=0.2):
+        anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
         pos_dist = tf.reduce_sum(tf.square(anchor - positive), axis=-1)
         neg_dist = tf.reduce_sum(tf.square(anchor - negative), axis=-1)
-        loss = pos_dist - neg_dist + margin
-        return tf.maximum(0.0, loss)
-    
-    """
-    Class to create the siamese network. Called by  __init__ 
+        loss = tf.maximum(pos_dist - neg_dist + alpha, 0.0)
+        return tf.reduce_mean(loss)
 
-    Outputs:
-        siamese_network - the siamese network model compilation
-    """
     def create_network(self):
-        base_model = self.base_network()
-
         anchor_input = Input(shape=(256, 240, 3))
         positive_input = Input(shape=(256, 240, 3))
         negative_input = Input(shape=(256, 240, 3))
 
-        anchor_layer = base_model(anchor_input)
-        positive_layer = base_model(positive_input)
-        negative_layer = base_model(negative_input)
+        base_model = self.base_network()
 
-        # Create a dense layer for class prediction using Softmax activation.
-        prediction_layer = Dense(2, activation='softmax')(anchor_layer)
-    
-        siamese_network = Model(inputs=[anchor_input, positive_input, negative_input], outputs=prediction_layer)
-        siamese_network.compile(optimizer='adam', loss=self.triplet_loss, metrics=['accuracy'])
+        anchor_embedding = base_model(anchor_input)
+        positive_embedding = base_model(positive_input)
+        negative_embedding = base_model(negative_input)
 
+        inputs = [anchor_input, positive_input, negative_input]
+
+        # Output the embeddings as a list
+        output_embeddings = [anchor_embedding, positive_embedding, negative_embedding]
+
+        siamese_network = Model(inputs=inputs, outputs=output_embeddings)
+        siamese_network.compile(optimizer='adam', loss=self.triplet_loss)
         return siamese_network
+    
+    def create_classifier(self):
+        input_image = Input(shape=(256, 240, 3))
+
+        # Reuse the same base network architecture as the Siamese model
+        base_model = self.base_network()
+        classifier_embedding = base_model(input_image)
+
+        # Add additional layers for classification
+        classifier_output = Dense(2, activation='softmax')(classifier_embedding)
+
+        # Create the classifier model
+        classifier_model = Model(inputs=input_image, outputs=classifier_output)
+        return classifier_model
     
