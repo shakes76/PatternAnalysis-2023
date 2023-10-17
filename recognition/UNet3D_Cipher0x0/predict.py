26d4a66f66cee390dc23ef8faa086ee6952aeb01
Cipher <57138168+Cipher0x0@users.noreply.github.com>
2023-10-17 18:52:27 +1000

    Finished final version of prediction

--- recognition/UNet3D_Cipher0x0/predict.py
+++ recognition/UNet3D_Cipher0x0/predict.py
@@ -1,8 +1,9 @@
 from train import *
+import matplotlib.pyplot as plt
 
 # load the model
 unet = UNet3D()
-unet.load_state_dict(torch.load("net_paras.pth"))
+unet.load_state_dict(torch.load("net_paras.pth", map_location="cpu"))
 unet.eval()
 
 # load the data
@@ -14,10 +15,13 @@ test, _ = torch.utils.data.random_split(dataset, [1, 210])
 # predict
 for X, _ in test:
     X = X.unsqueeze(0).float().to(device)
-    pred = unet(X).argmax(1)
+    pred = unet(X).argmax(1).detach().float().numpy()
+
 
 # save the output
-ni_img = nib.Nifti1Image(pred, func.affine)
-nib.save(ni_img, 'output.nii.gz')
+ni_img = nibabel.Nifti1Image(pred, np.eye(4))
+nibabel.save(ni_img, 'output.nii.gz')
+
+
 
 
