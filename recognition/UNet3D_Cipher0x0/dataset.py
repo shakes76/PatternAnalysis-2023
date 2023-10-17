f5b599819165e7ae4b45cac04d757bdb6672271f
Cipher <57138168+Cipher0x0@users.noreply.github.com>
2023-10-17 19:50:03 +1000

    Add comment and optimize code

--- recognition/UNet3D_Cipher0x0/dataset.py
+++ recognition/UNet3D_Cipher0x0/dataset.py
@@ -2,55 +2,47 @@ import glob
 import torch
 import nibabel
 import numpy as np
-import torchio as tio
 from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
 
 
+# Dataloader for .nii.gz data
 class NiiImageLoader(DataLoader):
     def __init__(self, image_path, mask_path):
         self.inputs = []
         self.masks = []
         # retrieve path from dataset
-        for f in sorted(glob.iglob(image_path)):
-            self.inputs.append(f)
-        for f in sorted(glob.iglob(mask_path)):
-            self.masks.append(f)
-        self.to_tensor = transforms.ToTensor()
+        for input in sorted(glob.iglob(image_path)):
+            self.inputs.append(input)
+        for mask in sorted(glob.iglob(mask_path)):
+            self.masks.append(mask)
 
     def __len__(self):
         return len(self.inputs)
 
-    # open files
+    # load files
     def __getitem__(self, idx):
         image_p = self.inputs[idx]
         mask_p = self.masks[idx]
 
+        # load nii image with nibabel
         image = nibabel.load(image_p)
+        # transform as numpy array
         image = np.asarray(image.dataobj)
+        # transform as tensor
+        image = transforms.ToTensor(image).unsqueeze(0).data
 
         mask = nibabel.load(mask_p)
         mask = np.asarray(mask.dataobj)
-
-        image = self.to_tensor(image)
-        image = image.unsqueeze(0)
-        image = image.data
-
-        mask = self.to_tensor(mask)
-        mask = mask.unsqueeze(0)
-        mask = mask.data
+        mask = transforms.ToTensor(mask).unsqueeze(0).data
 
         return image, mask
 
-
-# load the dataset at local
-dataset = NiiImageLoader("semantic_MRs_anon/*",
-                         "semantic_labels_anon/*")
 
 # load the dataset in rangpur
 dataset = NiiImageLoader("/home/groups/comp3710/HipMRI_Study_open/semantic_MRs/*",
                          "/home/groups/comp3710/HipMRI_Study_open/semantic_labels_only/*")
 
 
-# split the dataset
+# split the dataset as 85% training, 7.5% validation, 7.5% testing
 trainloader, valloader, testloader = torch.utils.data.random_split(dataset, [179, 16, 16])
