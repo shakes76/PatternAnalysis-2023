a32f6c6ac7e2f1b5e2f1ff893f5be172d5698bff
lambdarm <141205527+lambdarm@users.noreply.github.com>
2023-10-17 19:00:36 +1000

    Updated dataset.py
    Added create_triplets function 
    updated commenting

--- recognition/ADNI-s4702860/dataset.py
+++ recognition/ADNI-s4702860/dataset.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+import random
 
 '''
 Function used to load ADNI datasets. 
@@ -35,13 +36,61 @@ def load_dataset():
     return train_generator, test_generator
 
 
+"""
+Python function used to create anchor, positive and negative triplets for the siamese neural network. 
+
+Input:
+    data_generator - the data_generator (either train or test)
+
+Outputs:
+    [anchor_triplets, pos_triplets, neg_triplets] - triplet array used for input into the siamese model
+"""
+def create_triplets(data_generator):
+    anchor_triplets = []
+    pos_triplets = []
+    neg_triplets = []
+
+    num_samples = 10 #len(data_generator)
+    indices = list(range(num_samples))
+
+    for _ in range(num_samples):
+        anchor_idx = random.randint(0, num_samples - 1)
+        anchor_img, anchor_label = data_generator[anchor_idx]
+        anchor_img, anchor_label = anchor_img[0], anchor_label[0]
+
+        while True:
+            pos_idx = random.randint(0, num_samples - 1)
+            pos_img, pos_label = data_generator[pos_idx]
+            pos_img, pos_label = pos_img[0], pos_label[0]
+            if pos_label.argmax() == anchor_label.argmax():
+                break
+
+        while True:
+            neg_idx = random.randint(0, num_samples - 1)
+            neg_img, neg_label = data_generator[neg_idx]
+            neg_img, neg_label = neg_img[0], neg_label[0]
+            if neg_label.argmax() != anchor_label.argmax():
+                break
+
+        anchor_triplets.append(anchor_img)
+        pos_triplets.append(pos_img)
+        neg_triplets.append(neg_img)
+
+    return [anchor_triplets, pos_triplets, neg_triplets]
+
+
+
+
+
+
+
 
 """
 Main method used to see information about the data generators
 """
 def main():
     train_generator, test_generator = load_dataset()
-
+    """
     # Print information about the training generator
     print("Training Generator:")
     print("Number of batches in training generator:", len(train_generator))
@@ -59,6 +108,13 @@ def main():
     print("Class labels and their corresponding indices:", test_generator.class_indices)
     print("Batch size:", test_generator.batch_size)
     print("Image shape:", test_generator.image_shape)
+    """
+    anchor_triplets, pos_triplets, neg_triplets = create_triplets(train_generator)
+    print(len(anchor_triplets))
+    print(len(pos_triplets))
+    print(len(neg_triplets))
+
+
 
 
 
