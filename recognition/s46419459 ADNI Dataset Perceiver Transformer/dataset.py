dc71249b15f50fa4a8a2b5da0af200f9c7140cdc
dcpais <dpaismail@gmail.com>
2023-10-17 16:58:54 +1000

    rejigged dataset loading functions

--- recognition/s46419459 ADNI Dataset Perceiver Transformer/dataset.py
+++ recognition/s46419459 ADNI Dataset Perceiver Transformer/dataset.py
@@ -1,26 +1,20 @@
 from torchvision.datasets import ImageFolder
 from torch.utils.data import DataLoader 
 
-# Defaults
-BATCH_SIZE = 64
-path = "C:\Users\dcp\Documents\OFFLINE-Projects\DATASETS\ADNI"
-# path = ...
-transforms = None
-
-def load_train_data():
-    train_dataset = ImageFolder(path + r"\train")
+def load_train_data(root_dir, batch_size, transforms = None):
+    train_dataset = ImageFolder(root_dir + r"\train")
     return DataLoader(
         train_dataset, 
-        batch_size = BATCH_SIZE, 
+        batch_size = batch_size, 
         transforms = transforms, 
         shuffle = True
     )
 
-def load_test_data():
-    test_dataset = ImageFolder(path + r"\test")
+def load_test_data(root_dir, batch_size, transforms = None):
+    test_dataset = ImageFolder(root_dir + r"\test")
     return DataLoader(
         test_dataset, 
-        batch_size = BATCH_SIZE, 
+        batch_size = batch_size, 
         transforms = transforms, 
         shuffle = True
     )
--- recognition/s46419459 ADNI Dataset Perceiver Transformer/modules.py
+++
@@ -1,9 +0,0 @@
-import torch
-import numpy as np
-import torch.nn as nn
-
-device = "cuda" if torch.cuda.is_available() else "cpu"
-
-in_features = 4
-out_features = 2
-N_layers = 6
