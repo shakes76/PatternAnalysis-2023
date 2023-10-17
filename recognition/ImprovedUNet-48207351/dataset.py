195046221771bbd8f247e1a1cefeb45e67b60eff
Selma <selma.gudmundsen@gmail.com>
2023-10-17 10:43:58 +1000

    changed from tf to pytroch and added link to zip of dataset

---
+++ recognition/ImprovedUNet-48207351/dataset.py
@@ -0,0 +1,3 @@
+
+
+# link to dataset: https://drive.google.com/file/d/1wKjoSg3QUNyN_6dqYS54OVyAr1lyQImV/view?usp=sharing
--- recognition/ImprovedUNet-48207351/modules.py
+++ recognition/ImprovedUNet-48207351/modules.py
@@ -1,57 +1,77 @@
-import tensorflow as tf
-from tensorflow import keras
-from keras import layers
-from keras.models import Model, load_model, save_model
-from keras.layers.convolutional import Input, Activation, BatchNormalization, Conv2D, MaxPooling2D, Conv2DTranspose, concatenate
-import matplotlib.pyplot as plt
+import torch
+import torch.nn as nn
+import torchvision.transforms.functional as TF
 import numpy as np
 
-# Functions to build the encoder path (source: https://towardsdatascience.com/image-segmentation-unet-and-deep-supervision-loss-using-keras-model-f21a9856750a)
-def conv_block(inp, filters, padding='same', activation='relu'):
-    """
-    Convolution block of a UNet encoder
-    """
-    x = Conv2D(filters, (3, 3), padding=padding, activation=activation)(inp)
-    x = Conv2D(filters, (3, 3), padding=padding)(x)
-    x = BatchNormalization(axis=3)(x)
-    x = Activation(activation)(x)
-    return x
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
 
+    def forward(self, x):
+        return self.conv(x)
 
-def encoder_block(inp, filters, padding='same', pool_stride=2, activation='relu'):
-    """
-    Encoder block of a UNet passes the result from the convolution block
-    above to a max pooling layer
-    """
-    x = conv_block(inp, filters, padding, activation)
-    p = MaxPooling2D(pool_size=(2, 2), strides=pool_stride)(x)
-    return x, p
+class UNET(nn.Module):
+    def __init__(
+            self, in_channels=3, out_channels=1, features=[64, 128, 256, 512],
+    ):
+        super(UNET, self).__init__()
+        self.ups = nn.ModuleList()
+        self.downs = nn.ModuleList()
+        self.pool = nn.MaxPool2d(kernel_size=2, stride=2)
 
-# Functions to build the decoder block
-def decoder_block(inp, filters, concat_layer, padding='same'):
-    # Upsample the feature maps
-    x = Conv2DTranspose(filters, (2,2), strides=(2,2), padding=padding)(inp)
-    x = concatenate([x, concat_layer]) # Concatenation/Skip conncetion with conjuagte encoder
-    x = conv_block(x, filters) # Passed into the convolution block above
-    return x
+        # Down part of UNET
+        for feature in features:
+            self.downs.append(DoubleConv(in_channels, feature))
+            in_channels = feature
 
-class UNet2D(input, latent_dim=64, activation=None, kernel=[3, 3], channels=1, name_prefix=''):
-    # Encoder
-    inputs = Input((256,256,1))
-    d1,p1 = encoder_block(inputs,64)
-    d2,p2 = encoder_block(p1,128)
-    d3,p3 = encoder_block(p2,256)
-    d4,p4 = encoder_block(p3,512) 
+        # Up part of UNET
+        for feature in reversed(features):
+            self.ups.append(
+                nn.ConvTranspose2d(
+                    feature*2, feature, kernel_size=2, stride=2,
+                )
+            )
+            self.ups.append(DoubleConv(feature*2, feature))
 
-    # Middle convolution block (no max pooling) - final output will now be upsampled
-    mid = conv_block(p4,1024) #Midsection   
+        self.bottleneck = DoubleConv(features[-1], features[-1]*2)
+        self.final_conv = nn.Conv2d(features[0], out_channels, kernel_size=1)
 
-    # Decoder
-    e2 = decoder_block(mid,512,d4) #Conjugate of encoder 4
-    e3 = decoder_block(e2,256,d3) #Conjugate of encoder 3
-    e4 = decoder_block(e3,128,d2) #Conjugate of encoder 2 
-    e5 = decoder_block(e4,64,d1) #Conjugate of encoder 1
-    outputs = Conv2D(1, (1,1), activation=None)(e5) #Final Output
-    
-    ml = Model(inputs=[inputs], outputs=[outputs, o1], name='Unet')  
-    
+    def forward(self, x):
+        skip_connections = []
+
+        for down in self.downs:
+            x = down(x)
+            skip_connections.append(x)
+            x = self.pool(x)
+
+        x = self.bottleneck(x)
+        skip_connections = skip_connections[::-1]
+
+        for idx in range(0, len(self.ups), 2):
+            x = self.ups[idx](x)
+            skip_connection = skip_connections[idx//2]
+
+            if x.shape != skip_connection.shape:
+                x = TF.resize(x, size=skip_connection.shape[2:])
+
+            concat_skip = torch.cat((skip_connection, x), dim=1)
+            x = self.ups[idx+1](concat_skip)
+
+        return self.final_conv(x)
+
+def test():
+    x = torch.randn((3, 1, 161, 161))
+    model = UNET(in_channels=1, out_channels=1)
+    preds = model(x)
+    assert preds.shape == x.shape
+
+if __name__ == "__main__":
+    test()
