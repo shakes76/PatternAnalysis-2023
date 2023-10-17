e321d4bb56491a36f4398883ff468f2167f0420f
Rhys Tyne <tynerhys@gmail.com>
2023-10-17 18:53:05 +1000

    create_data() now also makes a validation set

--- visualTransformer_s46481894/datasets.py
+++ visualTransformer_s46481894/datasets.py
@@ -3,10 +3,10 @@ import tensorflow as tf
 import os
 import numpy as np
 from keras.preprocessing.image import load_img, img_to_array
+from sklearn.model_selection import train_test_split
 
 
 # get dataset, using pre-processed ADNI dataset from blackboard
-
 
 
 def create_data():
@@ -22,28 +22,36 @@ def create_data():
 
     # image size is (240, 256, 3)
     # create training data
-    training = [] # array containing train data
+    training = []  # array containing train data
 
     for i in AD_train_path:
         image = load_img(trainDataDir + "/AD/" + i, target_size=(128, 128, 3))
         image = img_to_array(image)
-        training.append([image, 1]) # append with 1 label
+        training.append([image, 1])  # append with 1 label
 
     for i in NC_train_path:
         image = load_img(trainDataDir + "/NC/" + i, target_size=(128, 128, 3))
         image = img_to_array(image)
-        training.append([image, 0]) # append with 0 label
+        training.append([image, 0])  # append with 0 label
 
     # shuffle data to randomise?
     training_images = []
     training_labels = []
     for _ in training:
-        training_images.append(_[0]) # image
-        training_labels.append(_[1]) # label
+        training_images.append(_[0])  # image
+        training_labels.append(_[1])  # label
 
-    x_train = tf.convert_to_tensor(np.array(training_images, dtype=np.uint8))
+    # split training data into validation and training set
+    X_train, X_val, Y_train, Y_val = train_test_split(training_images,
+                                                      training_labels, train_size=0.9)
+
+    x_train = tf.convert_to_tensor(np.array(X_train, dtype=np.uint8))
     x_train = tf.cast(x_train, tf.float16) / 255.0
-    y_train = tf.convert_to_tensor(training_labels)
+    y_train = tf.convert_to_tensor(Y_train)
+
+    x_val = tf.convert_to_tensor(np.array(X_val, dtype=np.uint8))
+    x_val = tf.cast(x_val, tf.float16) / 255.0
+    y_val = tf.convert_to_tensor(Y_val)
 
     # create testing data
     testing = []  # array containing test data
@@ -69,6 +77,5 @@ def create_data():
     x_test = tf.cast(x_test, tf.float16) / 255.0
     y_test = tf.convert_to_tensor(testing_labels)
 
-    return x_train, y_train, x_test, y_test
 
-
+    return x_train, y_train, x_test, y_test, x_val, y_val
