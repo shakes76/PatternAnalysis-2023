46c9cac6cbdde34657d5cd3f0c43409a43eba50a
MarkusZuegg <markuszuegg@outlook.com>
2023-10-17 19:00:22 +1000

    Added Attentionblock

--- recognition/MarkusZuegg_Transformer/ViT_PytorchLightning/modules.py
+++ recognition/MarkusZuegg_Transformer/ViT_PytorchLightning/modules.py
@@ -1,8 +1,37 @@
 import os
 import pytorch_lightning as pl
-from pytorch_lightning import Trainer
 import torch
+import torch.nn as nn
 import torch.nn.functional as F
 import torchmetrics
 import torchvision
 
+class AttentionBlock(nn.Module):
+    def __init__(self, embed_dim, hidden_dim, num_heads, dropout=0.0):
+        """Attention Block.
+
+        Args:
+            embed_dim: Dimensionality of input and attention feature vectors
+            hidden_dim: Dimensionality of hidden layer in feed-forward network
+                         (usually 2-4x larger than embed_dim)
+            num_heads: Number of heads to use in the Multi-Head Attention block
+            dropout: Amount of dropout
+        """
+        super().__init__()
+
+        self.layer_norm_1 = nn.LayerNorm(embed_dim)
+        self.attn = nn.MultiheadAttention(embed_dim, num_heads)
+        self.layer_norm_2 = nn.LayerNorm(embed_dim)
+        self.linear = nn.Sequential(
+            nn.Linear(embed_dim, hidden_dim),
+            nn.GELU(),
+            nn.Dropout(dropout),
+            nn.Linear(hidden_dim, embed_dim),
+            nn.Dropout(dropout),
+        )
+
+    def forward(self, x):
+        inp_x = self.layer_norm_1(x)
+        x = x + self.attn(inp_x, inp_x, inp_x)[0]
+        x = x + self.linear(self.layer_norm_2(x))
+        return x
