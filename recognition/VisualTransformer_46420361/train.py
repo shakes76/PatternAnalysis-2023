7c8c90dbc13b1e3f5f64d7500b9f4cf3deaca225
Callum Elder <cjames.02@icloud.com>
2023-10-17 21:11:08 +1000

    moved main to seperate file

--- recognition/VisualTransformer_46420361/train.py
+++ recognition/VisualTransformer_46420361/train.py
@@ -80,48 +80,4 @@ def save_model(model, file):
         file (string): file name of the model to be saved (needs .pth)
     """
     torch.save(model, file)
-
-# Training loop
-def main():
-    # data variables
-    root = '/home/callum/AD_NC/'
-    image_size = 256
-    batch_size = 64
-    crop_size = 192
-    patch_size = image_size // 8
-    channels = 1
-    embedding_dims = channels * patch_size**2
-    patches = (image_size // patch_size)**2
-    num_heads = embedding_dims // 64
-    num_classes = 2
-    
-    # hyperparameters
-    epochs = 10
-    learning_rate = 0.001
-    weight_decay = 0.0001
-
-    model = create_model(image_size=image_size,
-                         in_channels=channels,
-                         patch_size=patch_size,
-                         embedding_dims=embedding_dims,
-                         num_heads=num_heads,
-                         num_classes=num_classes,
-                         patches=patches)
-    train_model(model=model,
-                root=root,
-                image_size=image_size,
-                crop_size=crop_size,
-                batch_size=batch_size,
-                learning_rate=learning_rate,
-                weight_decay=weight_decay,
-                epochs=epochs)
-    evaluate_model(model=model,
-                   root=root,
-                   image_size=image_size,
-                   crop_size=crop_size,
-                   batch_size=batch_size)
-
-        
-if __name__ == '__main__':
-    main()
         
