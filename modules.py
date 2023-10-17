caa35c528a6a5bda2c7d35752e291c35d306dd01
louisljoker <142284214+louisljoker@users.noreply.github.com>
2023-10-17 18:50:16 +1000

    Implemented U-Net model in modules.py

---
+++ modules.py
@@ -0,0 +1,59 @@
+# modules.py
+import torch
+import torch.nn as nn
+
+class UNet(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(UNet, self).__init__()
+        
+        # Contracting path
+        self.enc1 = self.conv_block(in_channels, 64)
+        self.enc2 = self.conv_block(64, 128)
+        self.enc3 = self.conv_block(128, 256)
+        self.enc4 = self.conv_block(256, 512)
+
+        self.pool = nn.MaxPool2d(kernel_size=2)
+        self.upconv3 = nn.ConvTranspose2d(512, 256, kernel_size=2, stride=2)
+        self.upconv2 = nn.ConvTranspose2d(256, 128, kernel_size=2, stride=2)
+        self.upconv1 = nn.ConvTranspose2d(128, 64, kernel_size=2, stride=2)
+
+        # Expanding path
+        self.dec3 = self.conv_block(512, 256)
+        self.dec2 = self.conv_block(256, 128)
+        self.dec1 = self.conv_block(128, 64)
+        
+        self.out_conv = nn.Conv2d(64, out_channels, kernel_size=1)
+        
+    def forward(self, x):
+        enc1 = self.enc1(x)
+        enc2 = self.enc2(self.pool(enc1))
+        enc3 = self.enc3(self.pool(enc2))
+        enc4 = self.enc4(self.pool(enc3))
+        
+        dec3 = self.upconv3(enc4)
+        dec3 = torch.cat((dec3, enc3), dim=1)
+        dec3 = self.dec3(dec3)
+        
+        dec2 = self.upconv2(dec3)
+        dec2 = torch.cat((dec2, enc2), dim=1)
+        dec2 = self.dec2(dec2)
+        
+        dec1 = self.upconv1(dec2)
+        dec1 = torch.cat((dec1, enc1), dim=1)
+        dec1 = self.dec1(dec1)
+        
+        return self.out_conv(dec1)
+
+    def conv_block(self, in_channels, out_channels):
+        return nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
+
+model = UNet(in_channels=3, out_channels=1)  # Assuming input is RGB and output is a binary mask
+print(model)
+
