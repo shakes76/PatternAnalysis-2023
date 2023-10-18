43b9bf81d06ede0c7a3bf20c1dd5705c4e88e31c
WalkerJim1 <jamwalk00@gmail.com>
2023-10-18 13:17:18 +1000

    added segmentation layers and softmax, changed norm to instance norms as per paper

--- recognition/WALKER_45827468/modules.py
+++ recognition/WALKER_45827468/modules.py
@@ -76,27 +76,27 @@ class ImprovedUNet(nn.Module):
         
         # 3x3x3 conv            3->16
         self.conv1 = nn.Conv2d(3, 16, kernel_size=3, padding=1)
-        self.bn1 = nn.BatchNorm2d(16)
+        self.in1 = nn.InstanceNorm2d(16)
         # context
         self.context1 = Context(16)
         # 3x3x3 conv stride 2   16->32
         self.conv2 = nn.Conv2d(16, 32, kernel_size=3, stride=2, padding=1)
-        self.bn2 = nn.BatchNorm2d(32)
+        self.in2 = nn.InstanceNorm2d(32)
         # context
         self.context2 = Context(32)
         # 3x3x3 conv stride 2   32->64
         self.conv3 = nn.Conv2d(32, 64, kernel_size=3, stride=2, padding=1)
-        self.bn3 = nn.BatchNorm2d(64)
+        self.in3 = nn.InstanceNorm2d(64)
         # context
         self.context3 = Context(64)
         # 3x3x3 conv stride 2   64->128
         self.conv4 = nn.Conv2d(64, 128, kernel_size=3, stride=2, padding=1)
-        self.bn4 = nn.BatchNorm2d(128)
+        self.in4 = nn.InstanceNorm2d(128)
         # context
         self.context4 = Context(128)
         # 3x3x3 conv stride 2   128->256
         self.conv5 = nn.Conv2d(128, 256, kernel_size=3, stride=2, padding=1)
-        self.bn5 = nn.BatchNorm2d(256)
+        self.in5 = nn.InstanceNorm2d(256)
         # context
         self.context5 = Context(256)
         
@@ -116,32 +116,42 @@ class ImprovedUNet(nn.Module):
         self.upsamp4 = UpSampling(32)
         # 3x3x3 conv
         self.conv6 = nn.Conv2d(2*16, 32, kernel_size=3, padding=1)
-        # segmentation ???
+        
+        # segmentation
+        self.seg1 = nn.Conv2d(64, 3, kernel_size=1)
+        self.seg2 = nn.Conv2d(32, 3, kernel_size=1)
+        self.seg3 = nn.Conv2d(32, 3, kernel_size=1)
+        self.segNorm = nn.InstanceNorm2d(3)
+        self.segRelu = nn.LeakyReLU(3)
+        self.upscale = nn.Upsample(scale_factor=2)
+        
+        # soft max
+        self.softmax = nn.Softmax(dim=3)
         
          
     def forward(self, input):
         # layer 1
-        out = self.bn1(self.conv1(input))
+        out = self.in1(self.conv1(input))
         out = self.context1(out)
         out1 = out                      # save output for concat
         
         # layer 2
-        out = self.bn2(self.conv2(out))
+        out = self.in2(self.conv2(out))
         out = self.context2(out)
         out2 = out                      # save output for concat
         
         # layer 3
-        out = self.bn3(self.conv3(out))
+        out = self.in3(self.conv3(out))
         out = self.context3(out)
         out3 = out                      # save output for concat
         
         # layer 4
-        out = self.bn4(self.conv4(out))
+        out = self.in4(self.conv4(out))
         out = self.context4(out)
         out4 = out                      # save output for concat
         
         # layer 5
-        out = self.bn5(self.conv5(out))
+        out = self.in5(self.conv5(out))
         out = self.context5(out)
         
         # layer 4
@@ -153,22 +163,28 @@ class ImprovedUNet(nn.Module):
         out = self.upsamp2(out)
         out = torch.cat((out,out3), 1)
         out = self.local2(out)
-        # seg ?
+        # seg
+        seg1 = self.upscale(self.segRelu(self.segNorm(self.seg1(out))))
         
         # layer 2
         out = self.upsamp3(out)
         out = torch.cat((out,out2), 1)
         out = self.local3(out)
-        # seg ?
+        # seg
+        seg2 = self.segRelu(self.segNorm(self.seg2(out)))
+        seg2 = torch.add(seg2, seg1)
+        seg2 = self.upscale(seg2)
         
         # layer 1
         out = self.upsamp4(out)
         out = torch.cat((out,out1), 1)
         out = self.conv6(out)
-        # seg ?
+        # seg
+        out = self.segRelu(self.segNorm(self.seg3(out)))    # seg3
         
-        # combine seg
+        out = torch.add(out, seg2)                          # combine seg layers
         
         # softmax
+        out = self.softmax(out)
         
         return out
