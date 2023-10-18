44724466aff0fc6e1fab6e0196fb3fc3d9cbe112
Darcy Robson <17976794+drobson03@users.noreply.github.com>
2023-10-18 16:14:06 +1000

    Add ReduceLROnPlateau callback for training models

--- recognition/46991030-ADNI-siamese/train.py
+++ recognition/46991030-ADNI-siamese/train.py
@@ -1,3 +1,5 @@
+import tensorflow as tf
+
 import constants
 import dataset
 import modules
@@ -20,6 +22,11 @@ model.fit(
     epochs=10,
     validation_data=validate_ds,
     verbose=1,
+    callbacks=[
+        tf.keras.callbacks.ReduceLROnPlateau(
+            monitor="val_loss", factor=0.2, patience=2, min_lr=0.0001
+        )
+    ],
 )
 
 print("Testing SNN model")
@@ -38,6 +45,11 @@ classifier.fit(
     epochs=30,
     validation_data=class_validate_ds,
     verbose=1,
+    callbacks=[
+        tf.keras.callbacks.ReduceLROnPlateau(
+            monitor="val_loss", factor=0.2, patience=5, min_lr=0.0001
+        )
+    ],
 )
 
 classifier.evaluate(class_test_ds, verbose=1)
