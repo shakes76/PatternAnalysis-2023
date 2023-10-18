0bcedf568c3edabd252ae393bdb3693b4adaa406
ray <99058662+raydesuyo@users.noreply.github.com>
2023-10-18 19:18:56 +1000

    Modify training.py

--- recognition/Stable_Diffusion_s4682728/train.py
+++ recognition/Stable_Diffusion_s4682728/train.py
@@ -1,64 +1,46 @@
 from imports import *
 from dataset import *
 from modules import *
+from utils import *
 
 # Set device
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print(device)
 
-def beta_schedule(timesteps):
-    beta_start = 0.0001
-    beta_end = 0.02
-    return torch.linspace(beta_start, beta_end, timesteps)
+model = DiffusionNetwork().to(device)
+optimizer = optim.Adam(model.parameters(), lr=1e-3)
 
-# Initialize models and dataset
-num_steps = 1000
-betas = beta_schedule(num_steps)  
-diffusion_process = DiffusionProcess(betas, num_steps).to(device)
-diffusion_network = DiffusionNetwork().to(device)
-print(diffusion_network)
-
-optimizer = optim.Adam(diffusion_network.parameters(), lr=1e-3)
-criterion = torch.nn.MSELoss().to(device)  # Example loss function
-
-batch_size = 8
+epochs = 51
+batch_size = 2
 train_dataloader = process_dataset(batch_size=batch_size, is_validation=False)
 
-# Training loop
 losses = []
-for epoch in range(99):  
-    for i, batch in enumerate(train_dataloader):
-        # Move batch to device
+model.train()
+for epoch in range(epochs):
+    for step, batch in enumerate(train_dataloader):
         batch = batch.to(device)
-        
-        # Apply diffusion process
-        diffused_batch = diffusion_process(batch)
-        
-        # Generate image from diffused image
-        output = diffusion_network(diffused_batch)
-        
-        # Compute loss
-        loss = criterion(output, batch)
-        
-        # Backpropagation
         optimizer.zero_grad()
+
+        t = torch.randint(0, timesteps, (batch_size,), device=device).long()
+
+        # Backpropagation
+        loss = get_loss(model, batch, t)
         loss.backward()
         optimizer.step()
         
         losses.append(loss.item())
         
-        if (i + 1) % 100 == 0:
-            print(f"Epoch {epoch+1}, Iteration {i+1}, Loss: {loss.item()}")
+        if (step + 1) % 100 == 0:
+            print(f"Epoch {epoch+1}, Iteration {step+1}, Loss: {loss.item()}")
 
 # Save the trained model
-torch.save(diffusion_network.state_dict(), "diffusion_network99.pth")
+torch.save(model.state_dict(), f"diffusion_network{epoch}.pth")
 
 # Plotting loss
 plt.plot(losses)
 plt.xlabel('Iteration')
 plt.ylabel('Loss')
 plt.title('Training Loss')
-plt.ylim(0, 0.04)
 
 # Save the plot
 save_dir = os.path.expanduser("~/demo_eiji/sd/plots")
