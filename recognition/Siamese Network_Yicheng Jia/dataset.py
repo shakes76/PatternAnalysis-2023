29543f2a402e03eb2e523da408918d23fcffd53f
Yicheng Jia <128900685+YichengJia@users.noreply.github.com>
2023-10-18 12:24:15 +1000

    Update dataset.py

--- recognition/Siamese Network_Yicheng Jia/dataset.py
+++ recognition/Siamese Network_Yicheng Jia/dataset.py
@@ -1,82 +1,81 @@
 import os
 import random
-
-from torch.utils.data import Dataset
-from PIL import Image
-import torchvision.transforms as transforms
 import torch
-import numpy as np
+import torchvision.transforms as transforms
+from PIL import Image
+from torch.utils.data import Dataset
 from torchvision.datasets import ImageFolder
-from transformers.image_transforms import convert_to_rgb
 
 
 class SiameseNetworkDataset(Dataset):
+    def __init__(self, root_dir):
+        super(SiameseNetworkDataset, self).__init__()
 
-    def __init__(self, root_dir, transform=None):
-        # Initialize the dataset.
-        self.imageFolderDataset = ImageFolder(root=root_dir)
-        # Apply the transformations to the images.
-        self.transform = transform
+        self.transform = transforms.ToTensor()
 
-    # Get a pair of images from the dataset.
-    def __getitem__(self, index):
+        # Load AD and NC images
+        self.AD_images_path = os.path.join(root_dir, 'AD')
+        self.NC_images_path = os.path.join(root_dir, 'NC')
 
-        # Get a random image from the dataset.
-        img0_tuple = self.imageFolderDataset.imgs[index]
+        # Get list of image filenames
+        self.AD_image_filenames = os.listdir(self.AD_images_path)
+        self.NC_image_filenames = os.listdir(self.NC_images_path)
 
-        # We need to make sure approx 50% of images are in the same class.
-        should_get_same_class = random.randint(0, 1)
+        # Load images and transform them to tensors
+        self.AD_images = [self.transform(Image.open(os.path.join(self.AD_images_path, img_filename)).convert("L")) for img_filename in self.AD_image_filenames]
+        self.NC_images = [self.transform(Image.open(os.path.join(self.NC_images_path, img_filename)).convert("L")) for img_filename in self.NC_image_filenames]
 
-        # Get another image from the dataset.
-        if should_get_same_class:
-            while True:
-                # Keep looping till the same class image is found.
-                img1_tuple = random.choice(self.imageFolderDataset.imgs)
-                # Make sure the two images are from the same class.
-                if img0_tuple[1] == img1_tuple[1]:
-                    break
-        else:
-            while True:
-                # Keep looping till the different class image is found.
-                img1_tuple = random.choice(self.imageFolderDataset.imgs)
-                # Make sure the two images are from the different classes.
-                if img0_tuple[1] != img1_tuple[1]:
-                    break
+        # Stack images into tensors
+        self.AD_images_tensor = torch.stack(self.AD_images)
+        self.NC_images_tensor = torch.stack(self.NC_images)
 
-        # Load the two images.
-        img0 = Image.open(img0_tuple[0])
-        img1 = Image.open(img1_tuple[0])
-
-        # Apply the transforms to the images.
-        if self.transform is not None:
-            img0 = self.transform(img0)
-            img1 = self.transform(img1)
-
-        # Return the two images and the label (same class or different class).
-        return img0, img1, torch.from_numpy(np.array([int(img0_tuple[1] != img1_tuple[1])], dtype=np.float32))
-
-    # Get the number of images in the dataset.
     def __len__(self):
-        return len(self.imageFolderDataset.imgs)
+        return 2 * min(len(self.AD_images_tensor), len(self.NC_images_tensor))  # 2 times for positive and negative pairs
+
+    def __getitem__(self, index):
+        if random.choice([True, False]):
+            # Positive example (both images are AD)
+            img1 = Image.open(os.path.join(self.AD_images_path, random.choice(self.AD_image_filenames))).convert("L")
+            img2 = Image.open(os.path.join(self.AD_images_path, random.choice(self.AD_image_filenames))).convert("L")
+            label = torch.tensor(1, dtype=torch.float)
+        else:
+            # Negative example (one image is AD, the other is NC)
+            img1 = Image.open(os.path.join(self.AD_images_path, random.choice(self.AD_image_filenames))).convert("L")
+            img2 = Image.open(os.path.join(self.NC_images_path, random.choice(self.NC_image_filenames))).convert("L")
+            label = torch.tensor(0, dtype=torch.float)
+
+        img1 = self.transform(img1)
+        img2 = self.transform(img2)
+
+        return img1, img2, label
 
 
-# Define the transformations to be applied on the images.
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),  # Resize the image to 224x224 pixels to fix VGG16 input size.
-    transforms.Lambda(convert_to_rgb),  # Convert the image to RGB if it is a grayscale image.
-    transforms.CenterCrop(125),  # Crop the central part of the image to focus on the brain
-    transforms.RandomResizedCrop(224, scale=(0.8, 1.2)),  # Randomly resize the cropped image
-    transforms.RandomRotation(15),  # Randomly rotate the image by 30 degrees
-    transforms.RandomHorizontalFlip(),  # Randomly flip the image horizontally
-    transforms.ColorJitter(brightness=0.2, contrast=0.2),  # Randomly change the brightness and contrast of an image
-    transforms.ToTensor()  # Convert the image to PyTorch tensor data type
-])
+def transform_process():
+    # Define the transformations to be applied on the images.
+    current_transform = transforms.Compose([
+        # transforms.Resize((105, 105)),
+        # transforms.CenterCrop(105),
+        transforms.RandomRotation(degrees=(0, 60)),  # Randomly rotate the image by 30 degrees
+        transforms.RandomHorizontalFlip(),  # Randomly flip the image horizontally
+        transforms.RandomVerticalFlip(),
+        transforms.RandomAffine(degrees=15, translate=(0.1, 0.1), scale=(0.9, 1.1)),
+        transforms.ColorJitter(brightness=0.2, contrast=0.2),  # Randomly change the brightness and contrast of an image
+        transforms.ToTensor(),  # Convert the image to PyTorch tensor data type
+        transforms.Normalize(mean=[0.485], std=[0.229]),
+        # transforms.RandomResizedCrop(224, scale=(0.8, 1.2)),  # Randomly resize the cropped image
+        # transforms.Lambda(convert_to_rgb),  # Convert the image to RGB if it is a grayscale image.
+    ])
+    return current_transform
 
 
-# Use relative paths to specify the directories.
-train_dir = ImageFolder(root=os.path.join(os.getcwd(), "AD_NC/train"))
-test_dir = ImageFolder(root=os.path.join(os.getcwd(), "AD_NC/test"))
+def transform_directory():
+    transform = transform_process()
+    # Use relative paths to specify the directories.
+    current_train_dir = ImageFolder(root=os.path.join(os.getcwd(), "AD_NC/train"))
+    current_test_dir = ImageFolder(root=os.path.join(os.getcwd(), "AD_NC/test"))
 
-# Use relative paths to specify the directories.
-train_dataset = SiameseNetworkDataset(root_dir=os.path.join(os.getcwd(), "AD_NC/train"), transform=transform)
-test_dataset = SiameseNetworkDataset(root_dir=os.path.join(os.getcwd(), "AD_NC/test"), transform=transform)
+    # Use relative paths to specify the directories.
+    current_train_dataset = SiameseNetworkDataset(root_dir=os.path.join(os.getcwd(), "AD_NC/train"))
+    current_test_dataset = SiameseNetworkDataset(root_dir=os.path.join(os.getcwd(), "AD_NC/test"))
+
+    return current_train_dir, current_test_dir, current_train_dataset, current_test_dataset
