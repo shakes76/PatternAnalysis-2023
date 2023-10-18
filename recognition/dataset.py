416a7424cf8d6f81b559a46cd9c9e84a2fba447b
Spooey <angus1252@gmail.com>
2023-10-19 00:30:10 +1000

    Changed the use of randomcrop to centercrop

--- recognition/dataset.py
+++ recognition/dataset.py
@@ -11,7 +11,7 @@ batch_size = 20
 class ADNI():
     def __init__(self, batch_size):
         self.batch_size = batch_size
-        self.transformations = transforms.Compose([transforms.Grayscale(1), transforms.RandomCrop(240), transforms.ToTensor()])
+        self.transformations = transforms.Compose([transforms.Grayscale(1), transforms.CenterCrop(240), transforms.ToTensor()])
         self.dataset = datasets.ImageFolder(path + "train", transform=self.transformations) # datasets.CelebA(root=path, download=True, transform=transformations)
         self.training_data_loader = torch.utils.data.DataLoader(self.dataset, batch_size=self.batch_size, shuffle=True)
         self.testing_data_loader = torch.utils.data.DataLoader(self.dataset, batch_size=self.batch_size, shuffle=True)
