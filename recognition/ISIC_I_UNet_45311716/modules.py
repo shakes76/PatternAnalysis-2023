186afad4d1eb493607c94cc6937acdb4405ab452
Awidat17 <mlamtadi1705@gmail.com>
2023-10-18 22:09:26 +1000

    "added couble convolution module"

--- recognition/ISIC_I_UNet_45311716/modules.py
+++ recognition/ISIC_I_UNet_45311716/modules.py
@@ -1,2 +1,21 @@
 import torch
-import torch.nn.functional as F
+import torch.nn as nn
+import torch.nn.functional as TF
+
+# Two Convelutions
+class DoubleConv(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(DoubleConv, self).__init__()
+        # sequential model
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False), # convolution operation
+            nn.BatchNorm2d(out_channels), # batch normalization
+            nn.ReLU(inplace=True), # ReLu activation function
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False), # convolution operation
+            nn.BatchNorm2d(out_channels), # batch normalization
+            nn.ReLU(inplace=True), # ReLu activation function
+        )
+
+    # performs double convolution 
+    def forward(self, x):
+        return self.double_conv(x)
