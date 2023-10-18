Fix the distance calculation and remove the sigmoid function

recognition/Siamese_classifier_shusheng_cao/modules.py
@@ -60,21 +60,16 @@ class ResNet(nn.Module):
         out = out.view(out.size(0), -1)
         return out
 
-def calc_euclidean(x1, x2):
-    return (x1 - x2).pow(2).sum(1)
-
 class SiameseNetwork(nn.Module):
 
     def __init__(self):
         super(SiameseNetwork, self).__init__()
         self.resnet = ResNet(BasicBlock, 64, [2, 2, 2, 2])
-        self.sigmoid = nn.Sigmoid()
     def forward(self, input1, input2):
         output1 = self.resnet(input1)
         output2 = self.resnet(input2)
-        distance = calc_euclidean(output2, output1)
-        out = self.sigmoid(distance)
-        return out
+        distance = nn.functional.pairwise_distance(output2, output1)
+        return distance
 
 
 class TripletLoss(nn.Module):