f9dc071afd75916ac16e3cc05c373b7848699b83
LargePenguin034 <134495820+LargePenguin034@users.noreply.github.com>
2023-10-18 15:11:15 +1000

    train confirmed working

--- recognition/ISIC Improved UNet 47479049/train.py
+++ recognition/ISIC Improved UNet 47479049/train.py
@@ -1,94 +1,136 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+import torchvision
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
-import albumentations as A
-from albumentations.pytorch import ToTensorV2
-from tqdm import tqdm
+import matplotlib.pyplot as plt
 from modules import UNet
+from Model2 import Modified3DUNet
 from dataset import SkinDataset, get_loaders
 
 # Hyperparameters
 learning_rate = 1e-4
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-batch_size = 32
-num_epochs = 100
-num_workers = 2
-image_height = 510
-image_width = 384
-train_dir = "recognition\ISIC Improved UNet 47479049\data\ISIC2018_Task1-2_Training_Input_x2"
-mask_dir = "recognition\ISIC Improved UNet 47479049\data\ISIC2018_Task1_Training_GroundTruth_x2"
+batch_size = 2
+num_epochs = 30
+num_workers = 4
+image_height = 96
+image_width = 128
+test_dir = "data\ISIC2018_Task1-2_Test_Input"
+test_out_dir = "data\ISIC2018_Task1-2_Test_Output"
+train_dir = "data\ISIC2018_Task1-2_Training_Input_x2"
+mask_dir = "data\ISIC2018_Task1_Training_GroundTruth_x2"
 
-def train(loader, model, optimizer, criterion, scaler):
-    loop = tqdm(loader)
+def train(loader, val_loader, test_loader, model, optimizer, criterion, num_epochs):
+    print("Training Start")
+    for epoch in range(num_epochs):
+        model.train()
+        total_loss = 0
 
-    for batch_idx, (data, targets) in enumerate(loop):
-        data = data.to(device)
-        targets = targets.float().unsqueeze(1).to(device=device)
+        for data, mask in loader:
+            data = data.to(device)
+            mask = mask.to(device=device)
 
-        # Forward
-        predictions = model(data)
-        loss = criterion(predictions, targets)
+            # Forward
+            predictions = model(data)
+            loss = criterion(predictions, mask)
+
+            # Backward
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            total_loss += loss.item()
+
+        avg_loss = total_loss / len(loader)
+        print(f"Epoch [{epoch + 1}/{num_epochs}] - Loss: {avg_loss:.4f}")
+
+        if epoch % 5 == 0:
+                img = next(iter(test_loader))
+                img = img.to(device)
+                img = model(img)
+                torchvision.utils.save_image(img, f"Epoch_{epoch}.png")
+
+        check_accuracy(val_loader, model, device=device)
 
 
-        # Backward
-        optimizer.zero_grad()
-        scaler.scale(loss).backward()
-        scaler.step(optimizer)
-        scaler.update()
+def check_accuracy(loader, model, device="cuda"):
+    dice_score = 0
 
-        # tqdm
-        loop.set_postfix(loss=loss.item())
+    model.eval()
+    with torch.no_grad():
+        for x, y in loader:
+            x = x.to(device)
+            y = y.to(device)
+            pred = model(x)
+            pred = (pred > 0.5).float()
+
+            dice_score += (2*(pred*y).sum()) / ((pred+y).sum()+1e-8)
+
+    print(f"Dice Score: {dice_score/len(loader)}")
+
+    model.train()
+
+
+
+def create_mask(loader, model, num_pics, device=device, folder="/"):
+    model.eval()
+
+    for idx, x in enumerate(loader):
+        if idx > num_pics:
+            break
+        x = x.to(device=device)
+        with torch.no_grad():
+            pred = model(x)
+            pred = (pred>0.5).float()
+        torchvision.utils.save_image(
+            pred, f"{folder}/pred_{idx}.png"
+        )
+        
+
+    model.train()
 
 
 
 def main():
-    train_transforms = A.Compose(
-        [
-            A.Resize(height=image_height, width=image_width),
-            A.Rotate(limit=35, p=1.0),
-            A.HorizontalFlip(p=0.5),
-            A.VerticalFlip(p=0.1),
-            A.Normalize(
-                mean=[0.0, 0.0, 0.0],
-                std=[1.0, 1.0, 1.0],
-                max_pixel_value=255.0
-            ),
-            ToTensorV2(),
-        ],
-    )
+    train_transforms = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Resize((image_height, image_width), antialias=None)
+    ])
 
-    val_transforms = A.Compose(
-        [
-            A.Resize(height=image_height, width=image_width),
-            A.Normalize(
-                mean=[0.0, 0.0, 0.0],
-                std=[1.0, 1.0, 1.0],
-                max_pixel_value=255.0
-            ),
-            ToTensorV2(),
-        ],
-    )   
+    val_transforms = transforms.Compose([
+        transforms.ToTensor(),
+    ])
 
-    model = UNet(in_channels=3, out_channels=1).to(device)
-    criterion = nn.BCEWithLogitsLoss()
+
+    model = UNet(3, 1).to(device)
+    criterion = nn.BCELoss()
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-    scaler = torch.cuda.amp.GradScaler()
 
-    train_loader = get_loaders(
+
+    train_loader, val_loader, test_loader = get_loaders(
         train_dir,
         mask_dir,
+        test_dir,
         batch_size,
         train_transforms,
         val_transforms,
     )
 
-    for epoch in range(num_epochs):
-        train(train_loader, model, optimizer, criterion, scaler)
+    #img = next(iter(test_loader))
+    #plt.imshow(img[0][0], "Greys_r")
+    #plt.show()
 
+    img = next(iter(test_loader))
+    img = img.to(device)
+    img = model(img)
+    torchvision.utils.save_image(img, "Epoch_0.png")
 
-
+    train(train_loader, val_loader, test_loader, model, optimizer, criterion, num_epochs)
+        
+    torch.save(model.state_dict(), "model")
+    create_mask(test_loader, model, 2, device=device, folder=test_out_dir)
 
 if __name__ == "__main__":
     main()
