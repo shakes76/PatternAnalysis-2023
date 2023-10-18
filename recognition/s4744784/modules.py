f6d90669c0fe8cc6a7799ec11dd71e3a3b0aa2a3
Porcupinee <htolley0@gmail.com>
2023-10-18 22:03:47 +1000

    Changing channels from 64 to 128 and implementing dropout

--- recognition/s4744784/modules.py
+++ recognition/s4744784/modules.py
@@ -6,27 +6,41 @@ import torch.nn.functional as F
 from utils import *
 
 class Network(nn.Module):
-    def __init__(self, upscale_factor=upscale_factor, channels=1):
+    def __init__(self, upscale_factor=upscale_factor, channels=1, dropout_probability=0.3):
         super(Network, self).__init__()
         
         self.conv1 = nn.Sequential(
             nn.Conv2d(channels, 64, kernel_size=5, padding=2),
             nn.BatchNorm2d(64),
-            nn.ReLU())
+            nn.ReLU(),
+            nn.Dropout(dropout_probability)
+        )
         
         self.conv2 = nn.Sequential(
-            nn.Conv2d(64, 64, kernel_size=3, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU())
+            nn.Conv2d(64, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Dropout(dropout_probability)
+        )
         
         self.conv3 = nn.Sequential(
-            nn.Conv2d(64, 32, kernel_size=3, padding=1),
-            nn.BatchNorm2d(32),
-            nn.ReLU())
+            nn.Conv2d(128, 128, kernel_size=3, padding=1),
+            nn.BatchNorm2d(128),
+            nn.ReLU(),
+            nn.Dropout(dropout_probability)
+        )
         
         self.conv4 = nn.Sequential(
-            nn.Conv2d(32, channels * (upscale_factor ** 2), kernel_size=3, padding=1),
-            nn.ReLU())
+            nn.Conv2d(128, 64, kernel_size=3, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+            nn.Dropout(dropout_probability)
+        )
+        
+        self.conv5 = nn.Sequential(
+            nn.Conv2d(64, channels * (upscale_factor ** 2), kernel_size=3, padding=1),
+            nn.ReLU()
+        )
         
         self.pixel_shuffle = nn.PixelShuffle(upscale_factor)
         
@@ -35,5 +49,6 @@ class Network(nn.Module):
         x2 = self.conv2(x1)
         x3 = self.conv3(x2)
         x4 = self.conv4(x3)
-        out = self.pixel_shuffle(x4)
+        x5 = self.conv5(x4)
+        out = self.pixel_shuffle(x5)
         return out
