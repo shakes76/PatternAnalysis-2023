53bf8a25d14f7dde57bffaf939ef33a68f84d826
Awidat17 <mlamtadi1705@gmail.com>
2023-10-19 02:55:04 +1000

    "training code added. needs testing"

--- recognition/ISIC_I_UNet_45311716/train.py
+++ recognition/ISIC_I_UNet_45311716/train.py
@@ -19,9 +19,39 @@ image_height = 512
 image_width = 512
 
 def main():
-    model = ImprovedUNet(in_channels=3, out_channels=1).to(device)
+    # Improved UNet model
+    model = ImprovedUNet(in_channels=3, out_channels=1).to(device)   
 
+    # Training data
     data = UNetData(path=path, height=image_height, width=image_width)
+    train_data = data.train_loader
+
+    # Binary class. loss function and Adam optimizer
+    loss_fn = nn.BCEWithLogitsLoss()
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+    # Gradient scaler
+    scaler = torch.cuda.amp.GradScaler()
+
+    for epoch in range(num_epochs):
+        for (image, targets) in train_data:
+            image = image.to(device=device)
+            targets = targets.float().to(device=device) 
+
+            # automatic mixed-precision training
+            with torch.cuda.amp.autocast():
+                predictions = model(image)
+                loss = loss_fn(predictions, targets) 
+
+            # backpropagation 
+            optimizer.zero_grad()
+            scaler.scale(loss).backward()
+            scaler.step(optimizer)
+            scaler.update()
+
+        print (f"Epoch [{epoch+1}/{num_epochs}], Loss: {loss.item()}")
+
+
 
 if __name__ == "__main__":
     main()
