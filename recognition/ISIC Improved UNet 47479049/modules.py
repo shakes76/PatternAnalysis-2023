a6bee97f7f2b045cd507df665a91b95235f44296
LargePenguin034 <134495820+LargePenguin034@users.noreply.github.com>
2023-10-18 15:13:07 +1000

    Changes to model however non-functioning state

--- recognition/ISIC Improved UNet 47479049/modules.py
+++ recognition/ISIC Improved UNet 47479049/modules.py
@@ -1,18 +1,15 @@
 import torch
 import torch.nn as nn
-import torchvision
-import torchvision.transforms as transforms
-import time
-import numpy as np
-import torch.nn.functional as F
 import torchvision.transforms.functional as TF
 
 
 class UNet(nn.Module):
-    def __init__(self, in_channels=1, out_channels=2, base_features=16):
+    def __init__(self, in_channels=3, out_channels=2, base_features=8):
         super(UNet, self).__init__()
 
         self.upsample = nn.Upsample(scale_factor=2, mode='nearest')
+        self.lrelu = nn.LeakyReLU()
+
 
         # Down 0
         self.initial = nn.Conv2d(in_channels, base_features, kernel_size=3, padding=1, bias=False)
@@ -35,21 +32,21 @@ class UNet(nn.Module):
         self.cm4 = self.ContexModule(base_features*16, base_features*16)
 
         # Up 0
-        self.um0 = self.UpsamplingModule(base_features*16, base_features*8)
+        self.um0 = nn.ConvTranspose2d(base_features*16, base_features*8, kernel_size=3, stride=2, padding=1, output_padding=1)
 
         # Up 1
         self.lm1 = self.LocalizationModule(base_features*16, base_features*8)
-        self.um1 = self.UpsamplingModule(base_features*8, base_features*4)
+        self.um1 =  nn.ConvTranspose2d(base_features*8, base_features*4, kernel_size=3, stride=2, padding=1, output_padding=1)
 
         # Up 2
         self.lm2 = self.LocalizationModule(base_features*8, base_features*4)
-        self.segl2 = self.SegmentationUpsample(base_features*4, out_channels)
-        self.um2 = self.UpsamplingModule(base_features*4, base_features*2)
+        self.segl2 = nn.Conv2d(base_features*4, out_channels, kernel_size=3, padding="same")
+        self.um2 =  nn.ConvTranspose2d(base_features*4, base_features*2, kernel_size=3, stride=2, padding=1, output_padding=1)
 
         # Up 3
         self.lm3 = self.LocalizationModule(base_features*4, base_features*2)
-        self.segl3 = self.SegmentationUpsample(base_features*2, out_channels)
-        self.um3 = self.UpsamplingModule(base_features*2, base_features)
+        self.segl3 = nn.Conv2d(base_features*2, out_channels, kernel_size=3, padding="same")
+        self.um3 =  nn.ConvTranspose2d(base_features*2, base_features, kernel_size=3, stride=2, padding=1, output_padding=1)
 
         # Up 4
         self.final_cov = nn.Conv2d(base_features*2, out_channels, kernel_size=3, stride=1, padding=1)
@@ -66,26 +63,14 @@ class UNet(nn.Module):
             nn.LeakyReLU(),
         )
     
-    def UpsamplingModule(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.LeakyReLU(),
-            nn.Upsample(scale_factor=2, mode='nearest'),
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.LeakyReLU(),
-        )
     
     def LocalizationModule(self, in_channels, out_channels):
         return nn.Sequential(
-            nn.LeakyReLU(),
-            nn.Conv2d(in_channels, in_channels, kernel_size=3, stride=1, padding=1, bias=False),
-            nn.LeakyReLU(),
-            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False),
-        )
-    
-    def SegmentationUpsample(self, in_channels, out_channels):
-        return nn.Sequential(
-            nn.LeakyReLU(),
             nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.LeakyReLU(),
+            nn.BatchNorm2d(out_channels),
+            nn.Conv2d(out_channels, out_channels, kernel_size=1, stride=1, padding=0, bias=False),
+            nn.LeakyReLU(),
         )
     
     
@@ -95,6 +80,7 @@ class UNet(nn.Module):
     
 
     def forward(self, x):
+
         # Down 0
         out = self.initial(x)
         residual = out
@@ -105,6 +91,7 @@ class UNet(nn.Module):
         # Down 1
         out = self.conv1(out)
         residual = out
+        self.lrelu(out)
         out = self.cm1(out)
         out += residual
         concat_1 = out
@@ -112,6 +99,7 @@ class UNet(nn.Module):
         # Down 2
         out = self.conv2(out)
         residual = out
+        self.lrelu(out)
         out = self.cm2(out)
         out += residual
         concat_2 = out
@@ -119,6 +107,7 @@ class UNet(nn.Module):
         # Down 3
         out = self.conv3(out)
         residual = out
+        self.lrelu(out)
         out = self.cm3(out)
         out += residual
         concat_3 = out
@@ -126,6 +115,7 @@ class UNet(nn.Module):
         # Down 4
         out = self.conv4(out)
         residual = out
+        self.lrelu(out)
         out = self.cm4(out)
         out += residual
 
@@ -133,15 +123,11 @@ class UNet(nn.Module):
         out = self.um0(out)
 
         # Up 1
-        if out.shape != concat_3.shape:
-            out = TF.resize(out, size=concat_3.shape[2:], antialias=False)
         out = torch.cat([out, concat_3], dim=1)
         out = self.lm1(out)
         out = self.um1(out)
 
         # Up 2
-        if out.shape != concat_2.shape:
-            out = TF.resize(out, size=concat_2.shape[2:], antialias=False)
         out = torch.cat([out, concat_2], dim=1)
         out = self.lm2(out)
         seg_2 = self.segl2(out)
@@ -149,36 +135,31 @@ class UNet(nn.Module):
         out = self.um2(out)
 
         # Up 3
-        if out.shape != concat_1.shape:
-            out = TF.resize(out, size=concat_1.shape[2:], antialias=False)
         out = torch.cat([out, concat_1], dim=1)
         out = self.lm3(out)
         seg_3 = self.segl3(out)
         out = self.um3(out)
 
         # Up 4
-        if out.shape != concat_0.shape:
-            out = TF.resize(out, size=concat_0.shape[2:], antialias=False)
         out = torch.cat([out, concat_0], dim=1)
         out = self.final_cov(out)
-
-        if seg_2.shape != seg_3.shape:
-            seg_2 = TF.resize(seg_2, size=seg_3.shape[2:], antialias=False)
+        self.lrelu(out)
 
         seg_2_add_seg_3 = seg_2+seg_3
         seg_2_add_seg_3 = self.upsample(seg_2_add_seg_3)
 
-        if out.shape != seg_2_add_seg_3.shape:
-            seg_2_add_seg_3 = TF.resize(seg_2_add_seg_3, size=out.shape[2:])
-
         out += seg_2_add_seg_3
+
 
         return self.soft_max(out)
     
 def test():
-    x =  torch.randn((3, 1, 161, 161))
+    x =  torch.randn((3, 1, 96, 128))
     model = UNet(in_channels=1, out_channels=1)
     preds = model(x)
+
+    print(x.shape)
+    print(preds.shape)
 
 
 if __name__ == "__main__":
