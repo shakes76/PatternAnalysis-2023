2aee2914685554698bbdc16ad51ed36d4a6e4573
Callum Elder <cjames.02@icloud.com>
2023-10-19 09:23:40 +1000

    reconfigured functions

--- recognition/VisualTransformer_46420361/main.py
+++ recognition/VisualTransformer_46420361/main.py
@@ -13,14 +13,24 @@ def main():
     learning_rate = 0.001
     weight_decay = 0.0001
     
-    device  = get_device()
+    # device  = get_device()
 
-    model = create_model(image_size=image_size,
-                         channels=channels,
-                         patch_size=patch_size,
-                         embedding_dims=embedding_dims,
-                         num_heads=num_heads,
-                         device=device)
+    # model = create_model(image_size=image_size,
+    #                      channels=channels,
+    #                      patch_size=patch_size,
+    #                      embedding_dims=embedding_dims,
+    #                      num_heads=num_heads,
+    #                      device=device)
+    
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    print("Using device: ", device, f"({torch.cuda.get_device_name(device)})" if torch.cuda.is_available() else "")
+    model = ViT(
+        img_size=image_size,
+        in_channels=channels,
+        patch_size=patch_size,
+        embedding_dims=embedding_dims,
+        num_heads=num_heads
+        ).to(device)
     
     train_model(model=model,
                 root=root,
