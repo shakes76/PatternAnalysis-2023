01bd9117792b6c2a2cc3549f36012a475f9b7e3f
Tong Qiu <tongqiu@Tongs-MacBook-Pro-2.local>
2023-10-19 00:51:51 +1000

    directories changed to run on rangepur.

--- recognition/TongQiu_Siamese/utils.py
+++ recognition/TongQiu_Siamese/utils.py
@@ -3,11 +3,21 @@ from pathlib import Path
 
 
 class Config:
-    TRAIN_DIR = Path("~/comp3710/report/AD_NC/train")  # Path("/Users/tongqiu/Desktop/COMP3710_Report/AD_NC/train")
-    TEST_DIR = Path("~/comp3710/report/AD_NC/rest")  # Path("/Users/tongqiu/Desktop/COMP3710_Report/AD_NC/test")
-    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"  # "mps" if torch.backends.mps.is_available() else "cpu"
-    LOG_DIR = Path("~/comp3710/report/log/Contrastive_0")  # Path("/Users/tongqiu/Desktop/COMP3710_Report/log/Contrastive_0")
-    MODEL_DIR = '~/comp3710/report/model/Contrastive_0.pth'  # '/Users/tongqiu/Desktop/COMP3710_Report/Contrastive_0.pth'
+
+    # # run on local machine
+    # TRAIN_DIR = Path("/Users/tongqiu/Desktop/COMP3710_Report/AD_NC/train")
+    # TEST_DIR = Path("/Users/tongqiu/Desktop/COMP3710_Report/AD_NC/test")
+    # DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
+    # LOG_DIR = Path("/Users/tongqiu/Desktop/COMP3710_Report/log/Contrastive_0")
+    # MODEL_DIR = "/Users/tongqiu/Desktop/COMP3710_Report/Contrastive_0.pth"
+
+    # run on Rangepur
+    TRAIN_DIR = Path("~/comp3710/report/AD_NC/train")
+    TEST_DIR = Path("~/comp3710/report/AD_NC/test")  
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"  
+    LOG_DIR = Path("~/comp3710/report/log/Contrastive_0")  
+    MODEL_DIR = "~/comp3710/report/model/Contrastive_0.pth"  
+
 
 
 
