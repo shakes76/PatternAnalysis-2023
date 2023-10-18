925b47114175b36ba4b1f5ca21b4d099d89fdf3f
Terym2001 <tadyrkor@gmail.com>
2023-10-18 10:02:19 +1000

    started commenting code

--- recognition/Improved_UNet_s4825879/train.py
+++ recognition/Improved_UNet_s4825879/train.py
@@ -1,59 +1,68 @@
 #!/home/terym/miniconda3/envs/pytorch/bin/python3
 
+# import Libraries
 import torch
-from modules import ImpUNet, DiceLoss
-from dataset import ISICDataset
-from torchvision import transforms
-from torch.utils.data import DataLoader, TensorDataset
-from torchvision.utils import save_image
-
 import sys
-
 import matplotlib.pyplot as plt
 
-BATCH_SIZE = 5
+# import function for image saving
+from torchvision.utils import save_image
+
+# import modules and loss function
+from modules import ImpUNet, DiceLoss
+
+# import dataloaders
+from dataset import train_loader, val_loader 
+
+# Macros
 LEARNING_RATE = 0.005
 NUM_EPOCH = 20 
 SPLIT_RATIO = 0.8
 
 device = ('cuda' if torch.cuda.is_available() else 'cpu')
 
-transform = transforms.Compose([transforms.ToTensor(),
-                                transforms.Resize((512, 512))])
-
-train_data = ISICDataset(img_dir="data/train_data", truth_dir="data/train_truth", transform=transform, train=True)
-train_loader = DataLoader(train_data, batch_size=BATCH_SIZE, shuffle=False)
-
+# variables containing length of dataloaders
 total_step = len(train_loader)
-
-lambda_lr = lambda epoch: LEARNING_RATE * (0.985**epoch)
+total_val_step = len(val_loader)
 
 model = ImpUNet(3).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE) 
-scheduler = torch.optim.lr_scheduler.LambdaLR(optimizer, lr_lambda=lambda_lr)
+scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=50)
 loss_fcn = DiceLoss()
 
-model.train()
+# variable for saving the global best loss
+currentBestLoss = float("inf")
+
+# ----------
+# TRAINING -
+# ----------
+
 for epoch in range(NUM_EPOCH):
     print(f"epoc : {epoch} of {NUM_EPOCH}")
+    model.train()
+    running_loss = 0.0
     for i, images in enumerate(train_loader):
         img = images[0].to(device)
         truth = images[1].to(device)
 
         # forward pass
-        outputs = model(img)
-        loss = loss_fcn(outputs, truth)
+        outputs = model(img).to(device)
+        loss = loss_fcn(outputs, truth).to(device)
         
         #backwards and optimize
         optimizer.zero_grad()
         loss.backward() 
         optimizer.step() 
 
+        running_loss += loss.item()
+
         # print status
-        if (i + 1) % 10 == 0:
+        if (i + 1) % 5 == 0:
             print("Epoch: [{}/{}], Step: [{}/{}], Loss: {:.5f}"
-                  .format(epoch+1, NUM_EPOCH, i+1, total_step, loss.item()))
+                  .format(epoch+1, NUM_EPOCH, i+1, total_step, running_loss/(i+1)))
             sys.stdout.flush()
+            
+            # save image
             outputs = outputs.round()
             saved = torch.cat((outputs, truth), dim=0)
             save_image(saved.view(-1, 1, 512, 512), f"data/prod_img/{epoch}_{i+1}_seg.png", nrow=5)
@@ -61,4 +70,37 @@ for epoch in range(NUM_EPOCH):
         # scheduler step
         scheduler.step()
 
-torch.save(model.state_dict(), "model_dict.pt")
+    # ----------------
+    #  EVALUATION    -
+    # ----------------
+
+    model.eval()
+    total = 0.0
+    for i, (img, truth) in enumerate(val_loader):
+        # compute without accumulating gradients
+        with torch.no_grad():
+          # send tensors to device
+          img = img.to(device)
+          truth = truth.to(device)
+          
+          # compute outputs
+          outputs = model(img)
+          
+          # compute loss
+          loss = loss_fcn(outputs, truth)
+          
+          # add loss to total loss
+          total += loss.item()
+          
+    # Check if new loss is better than best loss
+    if total < currentBestLoss:
+        # upddate the best loss
+        currentBestLoss = total
+        # save model
+        # this ensures that model with best loss is saved
+        torch.save(model.state_dict(), "model_dict.pt")         
+    
+    # print out the total average loss for the validation set
+    print("epoch: {}, Loss: {}".format(epoch+1, total/total_val_step))
+
+
