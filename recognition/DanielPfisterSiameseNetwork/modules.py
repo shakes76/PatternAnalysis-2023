9c026986c261a301dae496a20616864dddbc6357
UQpfister <132638480+UQpfister@users.noreply.github.com>
2023-10-18 22:58:22 +1000

    implement new testing method for evaluation of test images

--- recognition/DanielPfisterSiameseNetwork/modules.py
+++ recognition/DanielPfisterSiameseNetwork/modules.py
@@ -42,19 +42,19 @@ def siamese_network(height,width,dimension):
                                 layers.MaxPool2D(pool_size =2, strides =2, padding ='same'),
 
                                 layers.Flatten(),
-                                layers.Dense(256, activation='sigmoid'), #60 precent 1024 vgg16 128 128
+                                layers.Dense(512, activation='sigmoid'), #60 precent 1024 vgg16 128 128
                                 ])
 
     #save the features from the left and right network in two variables
     feature_vector_left_output = vgg16(left_input)
     feature_vector_right_output = vgg16(right_input)
 
-    #merge layer which claulates the distance between both networks
-    merge_layer = layers.Lambda(lambda features: tf.abs(features[0] - features[1]))([feature_vector_left_output, feature_vector_right_output])
+    # distance layer, which calculates the distance between both networks
+    distance_layer = layers.Lambda(lambda features: tf.abs(features[0] - features[1]))([feature_vector_left_output, feature_vector_left_output])
 
 
     #fully connected layers
-    output = layers.Dense(1, activation='sigmoid')(merge_layer)
+    output = layers.Dense(1, activation='sigmoid')(distance_layer)
     #create whole neural network model
     model = tf.keras.Model(inputs=[left_input, right_input], outputs=output)
 
--- recognition/DanielPfisterSiameseNetwork/predict.py
+++ recognition/DanielPfisterSiameseNetwork/predict.py
@@ -5,78 +5,130 @@ from dataset import image_list
 from dataset import load_images
 import random
 import numpy as np
+import matplotlib.pyplot as plt
 import tensorflow.keras.backend as K
+
 
 
 #define varibles
 height = 128
 width = 128
 dimension = 1
+batch_size = 32
+# %%
 
 #load the trained weights of the neural network
-model = tf.keras.saving.load_model("C:/Users/Daniel/Desktop/Studium/UQ/5.Semester/COMP3710/Assignment/LabReport/PatternAnalysis-2023/recognition/DanielPfisterSiameseNetwork/model1.h5")
+siamese_model = tf.keras.saving.load_model("C:/Users/Daniel/Desktop/Studium/UQ/5.Semester/COMP3710/Assignment/LabReport/PatternAnalysis-2023/recognition/DanielPfisterSiameseNetwork/model1.h5")
 
 # %%
+#define path test images
 path_test_images_AD = "C:/Users/Daniel/Desktop/Studium/UQ/5.Semester/COMP3710/Assignment/LabReport/Dataset/ADNI/AD_NC/test/AD/"
 path_test_images_NC = "C:/Users/Daniel/Desktop/Studium/UQ/5.Semester/COMP3710/Assignment/LabReport/Dataset/ADNI/AD_NC/test/NC/"
 
+#create test image list and count number of test images
 list_test_AD, number_test_AD = image_list(path_test_images_AD)
 list_test_NC, number_test_NC = image_list(path_test_images_NC)
 
+#create a copie of the list and shuffle only the AD2 and NC2 list
+list_test_AD2 = list_test_AD[1:number_test_AD]
+list_test_NC2 = list_test_NC[1:number_test_NC]
+temp_AD2 = list_test_AD[0]
+temp_NC2 = list_test_NC[0]
+print(list_test_AD)
+print(list_test_NC)
+print(list_test_AD2)
+print(list_test_NC2)
+random.Random(42).shuffle(list_test_AD2)
+random.Random(42).shuffle(list_test_NC2)
+list_test_AD2.append(temp_AD2)
+list_test_NC2.append(temp_NC2)
 
-test_images_AD = []
-test_images_NC = []
+print(list_test_AD)
+print(list_test_NC)
+print(list_test_AD2)
+print(list_test_NC2)
 
-#load images into list as a numpy array
-test_images_AD = load_images(path_test_images_AD, list_test_AD, number_test_AD, height, width)
-test_images_NC = load_images(path_test_images_NC, list_test_NC, number_test_NC, height, width)
+#counter variable
+number_of_correct = 0
+number_of_incorrect = 0
 
+#test loop for accuracy
+for a in range(0, number_test_AD//batch_size*batch_size, batch_size):
+  print(a)
+  #1 load 32 images
+  test_image_chunk_AD1 = list_test_AD[a:a+batch_size]
+  test_image_chunk_NC1 = list_test_NC[a:a+batch_size]
+  test_image_chunk_AD2 = list_test_AD2[a:a+batch_size]
+  test_image_chunk_NC2 = list_test_NC2[a:a+batch_size]
+  print(test_image_chunk_AD1)
+  print(test_image_chunk_NC1)
+  print(test_image_chunk_AD2)
+  print(test_image_chunk_NC2)
+  test_image_AD1 = load_images(path_test_images_AD, test_image_chunk_AD1, height, width)
+  test_image_NC1 = load_images(path_test_images_NC, test_image_chunk_NC1, height, width)
+  test_image_AD2 = load_images(path_test_images_AD, test_image_chunk_AD2, height, width)
+  test_image_NC2 = load_images(path_test_images_NC, test_image_chunk_NC2, height, width)
 
+  testimage1_list = []
+  testimage2_list = []
+  label_test_list = []
+  #2 create image pairs
+  for b in range(batch_size):
+    testimage1_list.append(test_image_AD1[b,:,:,:])
+    testimage2_list.append(test_image_AD2[b,:,:,:])
+    label_test_list.append(1)
+    testimage1_list.append(test_image_AD1[b,:,:,:])
+    testimage2_list.append(test_image_NC1[b,:,:,:])
+    label_test_list.append(0)
+    testimage1_list.append(test_image_NC1[b,:,:,:])
+    testimage2_list.append(test_image_NC2[b,:,:,:])
+    label_test_list.append(1)
+    testimage1_list.append(test_image_NC2[b,:,:,:])
+    testimage2_list.append(test_image_AD2[b,:,:,:])
+    label_test_list.append(0)
+
+  testimage1_list_array = np.array(testimage1_list)
+  testimage2_list_array = np.array(testimage2_list)
+  label_array = np.array(label_test_list)
+  #3 make prediction
+  test_pair = [testimage1_list_array, testimage2_list_array]
+  float_formatter = "{:.2f}".format
+  prediction = siamese_model.predict(test_pair, steps = 1)
+  print(np.round(prediction[:,0],3),(label_array))
+  #4 defines the classification of the image pair depending on the score
+  prediction_label = []
+  for c in prediction[:,0]:
+    if c >= 0.50: 
+        prediction_label.append(1)
+    else:
+        prediction_label.append(0)
+
+  prediction_label = np.array(prediction_label)
+  print(prediction_label)
+  text_prediction = []
+  #5 compare prediction label with correct label
+  for d in range(0,len(prediction_label)):
+      if prediction_label[d] == label_array[d]:
+        number_of_correct = number_of_correct + 1
+        text_prediction.append("correct")
+      else:
+        number_of_incorrect = number_of_incorrect + 1
+        text_prediction.append("incorrect")
+  print(number_of_correct)
+  print(number_of_incorrect)
+  print(text_prediction)
 # %%
-
-#create data with the correct shape for the neural network
-test_pair_label = []
-test_pair_image = []
-
-#creates AD and NC as well as AD and AD image pairs
-#AD and AD image pairs get the label 1
-#AD and NC image pairs get the label 0 
-for x in range(0,number_test_AD):
-    random_number_AD = random.randint(0, number_test_AD-1)
-    random_number_NC = random.randint(0, number_test_NC-1)
-    image1 = np.expand_dims(test_images_AD[x,:,:,:], 0)
-    image2 = np.expand_dims(test_images_AD[random_number_AD,:,:,:], 0)
-    con_AD = np.concatenate((image1, image2), axis=0)
-    test_pair_image.append(con_AD)
-    test_pair_label.append(1)
-    image3 = np.expand_dims(test_images_NC[random_number_NC,:,:,:], 0) #[2,32,32,1]
-    con_NC = np.concatenate((image1, image3), axis=0)
-    test_pair_image.append(con_NC)
-    test_pair_label.append(0)
-
-#creates NC and NC as well as NC and AD image pairs
-#NC and NC image pairs get the label 1
-#NC and AD image pairs get the label 0 
-for x in range(0,number_test_NC):
-    random_number_AD = random.randint(0, number_test_AD-1)
-    random_number_NC = random.randint(0, number_test_NC-1)
-    image1 = np.expand_dims(test_images_NC[x,:,:,:], 0)
-    image2 = np.expand_dims(test_images_NC[random_number_NC,:,:,:], 0)
-    con_NC = np.concatenate((image1, image2), axis=0)
-    test_pair_image.append(con_NC)
-    test_pair_label.append(1)
-    image3 = np.expand_dims(test_images_AD[random_number_AD,:,:,:], 0) #[2,32,32,1]
-    con_AD = np.concatenate((image1, image3), axis=0)
-    test_pair_image.append(con_AD)
-    test_pair_label.append(0)
-
-test_pair_image_array = np.array(test_pair_image)
-test_pair_label_array = np.array(test_pair_label)
-
-
+#calculate the accuray test result 
+accuray_test = number_of_correct/(len(list_test_AD)*4)
+print("The overall accuracy of the saimese network is %.2f."% (accuray_test))
 # %%
-
-# print the test accuracy
-metrics = model.evaluate([test_pair_image_array[:, 0, :, :], test_pair_image_array[:, 1, :, :]], test_pair_label_array)
-print('Loss of {} and Accuracy is {} %'.format(metrics[0], metrics[1] * 100))
+#plot the result of the first ten results
+for i in range(0,10):
+  titel = "The prediciton of the saimese network is %0.f and the actual label is %.0f. \nAs a result the prediciton is %s."% (prediction_label[i],label_array[i],text_prediction[i])
+  fig = plt.figure()
+  plt.suptitle(titel)
+  ax1 = fig.add_subplot(2,2,1)
+  ax1.imshow(testimage1_list_array[i,:,:,:])
+  ax2 = fig.add_subplot(2,2,2)
+  ax2.imshow(testimage2_list_array[i,:,:,:])
 # %%
