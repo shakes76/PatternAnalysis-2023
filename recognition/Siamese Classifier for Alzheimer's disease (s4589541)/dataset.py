d748478dcea2e67b6746065de4fde42fc87aa734
milesgardiner <68379513+milesgardiner@users.noreply.github.com>
2023-10-18 12:05:13 +1000

    Created a custom Dataset for Triplet network.
    Created a custom Dataset class, changing __getitem__ in order to have each sample of the dataset return a triplet of images.

--- recognition/Siamese Classifier for Alzheimer's disease (s4589541)/dataset.py
+++ recognition/Siamese Classifier for Alzheimer's disease (s4589541)/dataset.py
@@ -1,4 +1,5 @@
 # loading and preprocessing of data
+import random
 import torch
 import torchvision.datasets as datasets
 import torchvision.transforms as transforms
@@ -15,6 +16,47 @@ class OneChannel:
         return img_tensor[0]
 
 
+class TripletDataset(datasets.ImageFolder):
+    def __init__(self, root, transform=None):
+        super().__init__(root, transform=transform)
+
+    def __getitem__(self, index: int):
+        """Overrides inherited method. Returns sample image (anchor) and class, with positive and negative image.
+
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (anchor, target, positive, negative) where target is class_index of the target class.
+        """
+        # first, get the anchor image 
+        path, target = self.samples[index]
+        anchor = self.loader(path)
+
+        # now get the positive and negative images randomly
+        found_p = False
+        found_n = False
+        num_samples = len(self.samples)
+        while not (found_p and found_n):
+            rand_idx = random.randint(0, num_samples)
+            if rand_idx == index:
+                continue
+            new_path, new_target = self.samples[rand_idx]
+            if not found_p and new_target == target:
+                positive = self.loader(new_path)
+                found_p = True
+            elif not found_n and new_target != target:
+                negative = self.loader(new_path)
+                found_n = True
+
+        if self.transform is not None:
+            anchor = self.transform(anchor)
+            positive = self.transform(positive)
+            negative = self.transform(negative)
+
+        return anchor, target, positive, negative
+
+
 # setup the transforms for the images
 transform = transforms.Compose([
     transforms.Resize((256, 240)),
@@ -23,14 +65,14 @@ transform = transforms.Compose([
 ])
 
 # set up the datasets
-train_set = datasets.ImageFolder(root="data/train", transform=transform)
-valid_set = datasets.ImageFolder(root="data/valid", transform=transform)
-test_set = datasets.ImageFolder(root="data/test", transform=transform)
+# train_set = datasets.ImageFolder(root="data/train", transform=transform)
+valid_set = TripletDataset(root="data/valid", transform=transform)
+# test_set = datasets.ImageFolder(root="data/test", transform=transform)
 
 # set up the dataloaders
-train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
-valid_loader = DataLoader(valid_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
-test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+# train_loader = DataLoader(train_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+# valid_loader = DataLoader(valid_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
+# test_loader = DataLoader(test_set, batch_size=BATCH_SIZE, shuffle=True, num_workers=4)
 
 
 
