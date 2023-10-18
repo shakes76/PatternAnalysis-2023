05a9cb2ff40db0e5f81f93c97f468c732c2ef91c
IsakOrlando <isakow@stud.ntnu.no>
2023-10-18 17:04:53 +1000

    training on vgpu

--- lesion_segmentation_s4820129/queue.sh
+++ lesion_segmentation_s4820129/queue.sh
@@ -1,13 +1,13 @@
 #!/bin/bash 
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH --job-name=UMAP
+#SBATCH --job-name=UNET
 #SBATCH --cpus-per-task 1
 #SBATCH --mail-type=All
 #SBATCH --mail-user=i.wangensteen@uq.net.au
 #SBATCH -o out.txt
 #SBATCH -e err.txt
-#SBATCH --partition=test
+#SBATCH --partition=vgpu
 #SBATCH --gres=gpu:1
 
 conda activate cluster_env
