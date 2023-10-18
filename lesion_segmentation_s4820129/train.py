54a2ac10926ed57c18ef6e69aad18efe87d85531
IsakOrlando <isakow@stud.ntnu.no>
2023-10-18 11:56:47 +1000

    added printing of metrics and dice similarity coefficient for training module

---
+++ lesion_segmentation_s4820129/train.py
@@ -0,0 +1,63 @@
+from modules import ImprovedUNET
+from dataset import ISICdataset
+from utilities import get_data_from_url, train, DSC, accuracy
+import albumentations as A
+from albumentations.pytorch import ToTensorV2
+from torch.utils.data import Dataset, DataLoader
+import torch
+from torch import nn
+
+get_data_from_url('ISIC_data', '1vxd1KBIYa3zCAVONkacdclsWUAxhWLho')
+TRAIN_DIR = 'ISIC_data/train'
+TRUTH_DIR = 'ISIC_data/ground_truth'
+
+
+train_transform = A.Compose(
+        [
+            A.Resize(height=256, width=256),
+            A.Rotate(limit=35, p=1.0),
+            A.HorizontalFlip(p=0.5),
+            A.VerticalFlip(p=0.1),
+            A.Normalize(
+                mean=[0.0, 0.0, 0.0],
+                std=[1.0, 1.0, 1.0],
+                max_pixel_value=255.0,
+            ),
+            ToTensorV2(),
+        ],
+    )
+
+
+full_dataset = ISICdataset(
+        image_dir=TRAIN_DIR,
+        truth_dir=TRUTH_DIR,
+        transform=train_transform,
+    )
+
+#hyperparameters
+N_CHANNELS = 3
+N_CLASSES = 1
+BATCH_SIZE = 16
+LEARNING_RATE = 0.01
+EPOCHS = 3
+DEVICE = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+train_loader = DataLoader(
+    full_dataset,
+    batch_size=BATCH_SIZE,
+    num_workers=1,
+    pin_memory=True,
+    shuffle=True,
+)
+
+model = ImprovedUNET(N_CHANNELS, N_CLASSES)
+optimizer = torch.optim.SGD(model.parameters(),lr=LEARNING_RATE,momentum=0.1,weight_decay=5e-4)
+scheduler = torch.optim.lr_scheduler.CosineAnnealingLR(optimizer, T_max=200)
+loss_fn = nn.BCEWithLogitsLoss()
+
+model = model.to(DEVICE)
+
+for epoch in range(EPOCHS):
+    train(model, train_loader, optimizer, loss_fn, DEVICE)
+    break
+
--- lesion_segmentation_s4820129/utilities.py
+++ lesion_segmentation_s4820129/utilities.py
@@ -1,6 +1,7 @@
 import os
 import gdown
 import zipfile
+import torch
 
 def get_data_from_url(destination_dir, google_drive_id):
 
@@ -15,3 +16,44 @@ def get_data_from_url(destination_dir, google_drive_id):
 
   else:
     print('Data already loaded')
+
+def train(model, loader, optimizer, criterion, DEVICE):
+  model.train()
+  for batch_idx, (images, masks) in enumerate(loader):
+    optimizer.zero_grad()
+    masks = masks.unsqueeze(0)
+    images = images.to(device=DEVICE)
+    masks = masks.to(device=DEVICE)
+
+    preds = model(images)
+    loss = criterion(preds, masks.float())
+    print(loss.item())
+    loss.backward()
+
+    acc = accuracy(preds, masks)
+    optimizer.step()
+  
+  return preds[0], masks[0]
+
+def DSC(predictions, masks):
+  return (2 * predictions * masks).sum() / (predictions + masks).sum() + 1e-8
+
+def accuracy(model, loader, device):
+  model.eval()
+  correct = 0
+  pixels = 0
+  dice_score = 0
+  with torch.no_grad(): 
+    for _,(preds, masks) in enumerate(loader):
+      preds = preds.to(device)
+      masks = masks.to(device).unsqueeze(0)
+      preds = torch.sigmoid(preds)
+      preds = (preds > 0.5).float()
+      correct += (preds==masks).sum()
+      pixels += torch.numel(preds)
+      dice_score += DSC(preds, masks)
+  
+  print(
+    f'Accuracy: {correct / pixels*100:.2f}, Dice Similarity Coefficient: {dice_score:.2f}, Loss: {1-dice_score:.2f}'
+  )
+
