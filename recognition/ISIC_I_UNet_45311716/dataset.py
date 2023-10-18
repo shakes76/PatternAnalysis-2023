03c5950deefff82b5e60fe09c65bd332aa7cfed6
Awidat17 <mlamtadi1705@gmail.com>
2023-10-19 00:42:06 +1000

    "dataset formatted into class"

--- recognition/ISIC_I_UNet_45311716/dataset.py
+++ recognition/ISIC_I_UNet_45311716/dataset.py
@@ -7,27 +7,29 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if not torch.cuda.is_available():
     print("Warning CUDA not Found. Using CPU")
 
-# Path to images
-path = 'C:/Users/mlamt/OneDrive/UNI/2023/Semester 2/COMP3710/Code/data/ISIC2018/'
+class UNetData():
+    def __init__(self, path, height, width):
 
-data_transform = transforms.Compose([transforms.Resize((512, 512)),
-                                             transforms.ToTensor()])
+        # RGB 3 channel input images
+        data_transform = transforms.Compose([transforms.Resize((height, width)),
+                                                    transforms.ToTensor()])
 
-mask_transform = transforms.Compose([transforms.Resize((512, 512)),
-                                            transforms.Grayscale(num_output_channels=1),  # Convert to single channel
-                                            transforms.ToTensor(),
-                                            transforms.Lambda(lambda x: x > 0.5)  # Threshold to 0 or 1
-                                            ])
+        # Black and white 1 channel mask image
+        mask_transform = transforms.Compose([transforms.Resize((height, width)),
+                                                    transforms.Grayscale(num_output_channels=1),  # Convert to single channel
+                                                    transforms.ToTensor(),
+                                                    transforms.Lambda(lambda x: x > 0.5)  # Threshold to 0 or 1
+                                                    ])
 
-# Training dataset
-train_set = torchvision.datasets.ImageFolder(root=path+'ISIC2018_Task1-2_Training_Input_x2', transform=data_transform)
-train_loader = torch.utils.data.DataLoader(train_set, batch_size=16, shuffle=True)
+        # Training dataset
+        train_set = torchvision.datasets.ImageFolder(root=path+'ISIC2018_Task1-2_Training_Input_x2', transform=data_transform)
+        self.train_loader = torch.utils.data.DataLoader(train_set, batch_size=16, shuffle=True)
 
-# Testing dataset
-test_set = torchvision.datasets.ImageFolder(root=path+'ISIC2018_Task1-2_Test_Input', transform=data_transform)
-test_loader = torch.utils.data.DataLoader(test_set, batch_size=16, shuffle=True)
+        # Testing dataset
+        test_set = torchvision.datasets.ImageFolder(root=path+'ISIC2018_Task1-2_Test_Input', transform=data_transform)
+        self.test_loader = torch.utils.data.DataLoader(test_set, batch_size=16, shuffle=True)
 
-# Truth dataset
-truth_set = torchvision.datasets.ImageFolder(root=path+'ISIC2018_Task1_Training_GroundTruth_x2', transform=mask_transform)
-truth_loader = torch.utils.data.DataLoader(truth_set, batch_size=16, shuffle=True)
+        # Truth dataset
+        truth_set = torchvision.datasets.ImageFolder(root=path+'ISIC2018_Task1_Training_GroundTruth_x2', transform=mask_transform)
+        self.truth_loader = torch.utils.data.DataLoader(truth_set, batch_size=16, shuffle=True)
 
--- recognition/ISIC_I_UNet_45311716/train.py
+++ recognition/ISIC_I_UNet_45311716/train.py
@@ -9,12 +9,19 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 if not torch.cuda.is_available():
     print("Warning CUDA not Found. Using CPU")
 
+# Path to images
+path = 'C:/Users/mlamt/OneDrive/UNI/2023/Semester 2/COMP3710/Code/data/ISIC2018/'
+    
 # Hyper-parameters
 num_epochs = 1
 learning_rate = 0.1
+image_height = 512 
+image_width = 512
 
 def main():
     model = ImprovedUNet(in_channels=3, out_channels=1).to(device)
+
+    data = UNetData(path=path, height=image_height, width=image_width)
 
 if __name__ == "__main__":
     main()
