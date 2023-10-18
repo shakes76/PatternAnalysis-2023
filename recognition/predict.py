55e80250155afe29df5f84fbab6e7707aca6044d
Tzu-Yu Yang <125327045+jyz523@users.noreply.github.com>
2023-10-19 01:21:11 +1000

    Update predict.py
    fix cant print out plot error

--- recognition/predict.py
+++ recognition/predict.py
@@ -2,50 +2,83 @@ import torch
 import matplotlib.pyplot as plt
 import numpy as np
 from torch.utils.data import DataLoader
-from torchvision import transforms
-from dataset import ISICDataset
+from dataset import ISICDataset, get_transform, get_mask_transform  # Import the necessary functions
 from modules import ImprovedUNet
 
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print(f"Predict: {torch.cuda.is_available()}")
 
-def imshow(inp, title=None):
+def imshow(inp, title=None, gray=False, ax=None):
     """Imshow for Tensor."""
-    # converting tensor to numpy array and changing dimensions
-    inp = inp.numpy().transpose((1, 2, 0))  
-    mean = np.array([0.5, 0.5, 0.5])
-    std = np.array([0.5, 0.5, 0.5])
-    inp = std * inp + mean
-    inp = np.clip(inp, 0, 1)
-    plt.imshow(inp)
-    if title is not None:
-        plt.title(title)
-        # pause a bit so that plots are updated
-    plt.pause(0.001)  
+    # Convert tensor to numpy if it's a tensor
+    if isinstance(inp, torch.Tensor):
+        inp = inp.numpy()
+    if inp.shape[0] == 1:
+        inp = np.squeeze(inp, axis=0)
+    if gray:
+        cmap = 'gray'
+    else:
+        cmap = None
+    if len(inp.shape) == 3:
+        inp = inp.transpose((1, 2, 0))
+        mean = np.array([0.5, 0.5, 0.5])
+        std = np.array([0.5, 0.5, 0.5])
+        inp = std * inp + mean
+        inp = np.clip(inp, 0, 1)
+    if ax is None:
+        plt.imshow(inp, cmap=cmap)
+        if title is not None:
+            plt.title(title)
+    else:
+        ax.imshow(inp, cmap=cmap)
+        ax.set_title(title)
+    plt.pause(0.001)
+
+
+
+# Custom collate function to handle None masks
+def my_collate(batch):
+    # Filter out entries where the mask is None
+    new_batch = list(filter(lambda x: x[1] is not None, batch))
+
+    # Check if there are any valid samples in the batch
+    if len(new_batch) == 0:  # if all masks were None, we skip this batch
+        print("Warning: Empty batch encountered. All masks were None.")
+        return None  # You might want to return an identifiable value here for easier handling later.
+
+    # If there's at least one valid sample, we create a batch for processing
+    return torch.utils.data.dataloader.default_collate(new_batch)
+
+
 
 def predict():
-    transform = transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))
-    ])
+    img_transform = get_transform()
+    mask_transform = get_mask_transform()
 
-    # Initialize your dataset and data loader
-    test_dataset = ISICDataset("ISIC2018_Task1-2_Test_Input", transform=transform)
-    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False)
+    test_dataset = ISICDataset(image_dir="ISIC2018_Task1-2_Training_Input_x2",
+                               mask_dir="ISIC2018_Task1_Training_GroundTruth_x2",
+                               img_transform=img_transform,
+                               mask_transform=mask_transform,
+                               img_size=(1024, 1024))
 
-    # Load your model
+    test_loader = DataLoader(test_dataset, batch_size=1, shuffle=False, collate_fn=my_collate)
+
     model = ImprovedUNet().to(device)
-    model.load_state_dict(torch.load("model_checkpoint.pth"))
+    model.load_state_dict(torch.load("plot_checkpoint.pth"))
     model.eval()
 
-    # Number of images you want to display
     num_images_to_display = 5
 
-    # Create an iterator for your data loader
     data_iter = iter(test_loader)
 
     for _ in range(num_images_to_display):
         try:
-            images, true_masks = next(data_iter)
+            batch = next(data_iter)
+            if batch is None:
+                print("Empty batch encountered; all masks were None for this iteration.")
+                continue  # Skip this loop iteration if the batch is empty
+
+            images, true_masks = batch  # Unpack your non-empty batch here
         except StopIteration:
             break
 
@@ -55,23 +88,21 @@ def predict():
         with torch.no_grad():
             outputs = model(images)
 
-        # Convert torch tensor to numpy
         predicted_masks = outputs.data.cpu().numpy()
         true_masks = true_masks.cpu().numpy()
         images = images.cpu()
 
         for image, pred_mask, true_mask in zip(images, predicted_masks, true_masks):
-            plt.figure()
             plt.subplot(1, 3, 1)
             imshow(image, title='Original Image')
 
             plt.subplot(1, 3, 2)
-            imshow(np.squeeze(pred_mask), title='Predicted Mask')
+            imshow(np.squeeze(pred_mask), title='Predicted Mask', gray=True)
 
             plt.subplot(1, 3, 3)
-            imshow(np.squeeze(true_mask), title='True Mask')
+            imshow(np.squeeze(true_mask), title='True Mask', gray=True)
 
-            plt.show()
-
+# Main block
 if __name__ == "__main__":
     predict()
+    plt.show(block=True)
