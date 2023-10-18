d4d879eb85d66c90fe2406dd84277f59302b3d59
LukeHalby <lukehalby@gmail.com>
2023-10-18 10:04:08 +1000

    Created VQVAE encoder and decoder

--- recognition/s4641745_VQVAE_OASIS_BRAIN/models.py
+++ recognition/s4641745_VQVAE_OASIS_BRAIN/models.py
@@ -2,11 +2,8 @@ import torch as t
 import torch.nn as nn
 import torch.nn.functional as F
 
-"""
-GAN discriminator
+"""========== START UTILS =========="""
 
-model taken from COMP3710 Lab Demo 2 Part 3 (GAN) by Luke Halberstadt
-"""
 def conv(ch_in, ch_out, k_size, stride=2, pad=1, bn=True):
     layers = []
     layers.append(nn.Conv2d(ch_in, ch_out, k_size, stride, pad))
@@ -15,6 +12,37 @@ def conv(ch_in, ch_out, k_size, stride=2, pad=1, bn=True):
     return nn.Sequential(*layers)
 
 
+def deconv(ch_in, ch_out, k_size, stride=2, pad=1, bn=True):
+    layers = []
+    layers.append(nn.ConvTranspose2d(ch_in, ch_out, k_size, stride, pad))
+    if bn:
+        layers.append(nn.BatchNorm2d(ch_out))
+    return nn.Sequential(*layers)
+
+
+class Residual(nn.Module):
+    # Structure taken from Section 4.1 of Neural Discrete Representation Learning
+    def __init__(self, ch_in, ch_out_inter, ch_out_final):
+        super(Residual, self).__init__()
+        self.net = nn.Sequential(
+            nn.ReLU(),
+            nn.Conv2d(ch_in, ch_out_inter, 3, 1, 1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(ch_out_inter, ch_out_final, 1, 1, False)
+        )
+
+    def forward(self, x):
+        return x + self.net(x)
+
+
+"""========== END UTILS =========="""
+
+
+"""
+GAN discriminator
+
+model taken from COMP3710 Lab Demo 2 Part 3 (GAN) by Luke Halberstadt
+"""
 class Discriminator(nn.Module):
     def __init__(self, image_size=128, conv_dim=64):
         super(Discriminator, self).__init__()
@@ -38,14 +66,6 @@ GAN generator
 
 model taken from COMP3710 Lab Demo 2 Part 3 (GAN) by Luke Halberstadt
 """
-def deconv(ch_in, ch_out, k_size, stride=2, pad=1, bn=True):
-    layers = []
-    layers.append(nn.ConvTranspose2d(ch_in, ch_out, k_size, stride, pad))
-    if bn:
-        layers.append(nn.BatchNorm2d(ch_out))
-    return nn.Sequential(*layers)
-
-
 class Generator(nn.Module):
     def __init__(self, z_dim=256, image_size=128, conv_dim=64):
         super(Generator, self).__init__()
@@ -66,5 +86,50 @@ class Generator(nn.Module):
 
 
 """
+VQVAE Encoder
+"""
+class Encoder(nn.Module):
+    # Structure taken from Section 4.1 of Neural Discrete Representation Learning
+    def __init__(self, ch_in, num_hidden, residual_inter):
+        super(Encoder, self).__init__()
+        self.conv1 = nn.Conv2d(ch_in, num_hidden//2, 4, 2, 1)
+        self.relu = nn.ReLU()
+        self.conv2 = nn.Conv2d(num_hidden//2, num_hidden, 4, 2, 1)
+        self.residual1 = Residual(num_hidden, residual_inter, num_hidden)
+        self.residual2 = Residual(num_hidden, residual_inter, num_hidden)
 
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.relu(x)
+        x = self.conv2(x)
+        x = self.residual1(x)
+        x = self.residual2(x)
+        return x
+
+
+"""
+VQVAE Decoder
+"""
+class Decoder(nn.Module):
+    # Structure taken from Section 4.1 of Neural Discrete Representation Learning
+    def __init__(self, ch_in, num_hidden, residual_inter):
+        super(Decoder, self).__init__()
+
+        self.conv1 = nn.Conv2d(ch_in, num_hidden, 3, 1, 1)
+        self.residual1 = Residual(num_hidden, residual_inter, num_hidden)
+        self.residual2 = Residual(num_hidden, residual_inter, num_hidden)
+        self.transpose1 = nn.ConvTranspose2d(num_hidden, num_hidden // 2, 4, 2, 1)
+        self.transpose2 = nn.ConvTranspose2d(num_hidden // 2, 3, 4, 2, 1)
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.residual1(x)
+        x = self.residual2(x)
+        x = self.transpose1(x)
+        x = self.transpose2(x)
+        return x
+
+
+"""
+VQVAE Vector Quantizer
 """
