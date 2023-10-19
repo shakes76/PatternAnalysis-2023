6b576f36dfdb08ba7c5f540a3d689b12e3f0522e
Raul Movila <movilaandi@gmail.com>
2023-10-19 14:49:32 +1000

    loaded model.h5 in predict

--- recognition/predict.py
+++ recognition/predict.py
@@ -1,1 +1,19 @@
-# Predict 
+import tensorflow as tf
+from tensorflow.keras.utils import CustomObjectScope
+
+
+H = 256
+W = 256
+split = 0.2
+dataset_path = r"C:\Users\raulm\Desktop\Uni\Sem2.2023\Patterns\ISIC-2017_Training_Data"
+SCORE = []
+
+if __name__ == "__main__":
+
+    # Load the saved model from files
+    with CustomObjectScope({'dice_coef': dice_coef_predict}):
+        model = tf.keras.models.load_model("files/model.h5")
+    # Load the testing data
+    _, _, (test_x, test_y) = load_data(dataset_path, split)
+
+    
