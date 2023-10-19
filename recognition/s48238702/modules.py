0425b511d826f7f080756ca44943b822fce4f47c
Rachit Chaurasia <rachitchaurasia2701@gmail.com>
2023-10-19 22:00:26 +1000

    Resolved logic in modules.py and fixed some errors

--- recognition/s48238702/modules.py
+++ recognition/s48238702/modules.py
@@ -1,97 +1,59 @@
 import torch
-from torch.nn import Linear, ReLU, Sequential, LayerNorm, BatchNorm1d
+import torch.nn as nn
+import torch.nn.functional as F
 
-def subnetwork(height, width):
-    """ The identical subnetwork in the SNN
-
-    Returns:
-        torch.nn.Sequential: the subnetwork Model
+class SiameseNetwork(nn.Module):
     """
-    subnet = Sequential(
-        Linear(height * width, 1024),
-        ReLU(),
-        Linear(1024, 1024),
-        ReLU(),
-        Linear(1024, 1024),
-        ReLU(),
-        Linear(1024, 1024),
-        ReLU(),
-        Linear(1024, 1024),
-        ReLU(),
-        Linear(1024, 1024),
-        ReLU(),
-    )
+    Siamese Network for image similarity learning.
 
-    return subnet
-
-def distance_layer(im1_feature, im2_feature):
-    """ Layer to compute (Euclidean) difference between feature vectors
+    This network takes pairs of images and produces embeddings for them.
 
     Args:
-        im1_feature (torch.Tensor): feature vector of an image
-        im2_feature (torch.Tensor): feature vector of an image
-
-    Returns:
-        torch.Tensor: Tensor containing differences
+        None
     """
-    tensor = torch.sum(torch.square(im1_feature - im2_feature), dim=1, keepdim=True)
-    return torch.sqrt(torch.maximum(tensor, torch.tensor(1e-7)))
+    def __init__(self):
+        super(SiameseNetwork, self).__init__()
+        self.cnn = nn.Sequential(
+            nn.Conv2d(1, 64, 10),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),
+            nn.Conv2d(64, 128, 7),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2)
+        )
+        self.fc = nn.Sequential(
+            nn.Linear(128 * 24 * 24, 256),
+            nn.ReLU(inplace=True),
+            nn.Linear(256, 256),
+            nn.Linear(256, 2)
+        )
 
-def classification_model(subnet):
-    """ Build the classification Model
+    def forward_one(self, x):
+        """
+        Forward pass for a single input.
 
-    Args:
-        subnet (torch.nn.Module): the sequential layer trained in the SNN
+        Args:
+            x (Tensor): Input image tensor.
 
-    Returns:
-        torch.nn.Module: compiled model
-    """
-    image = torch.nn.Parameter(torch.randn(1, 1, 128, 128))  # Adjust input shape for grayscale image
+        Returns:
+            Tensor: Embedding for the input image.
+        """
+        x = self.cnn(x)
+        x = x.view(x.size()[0], -1)
+        x = self.fc(x)
+        return x
 
-    tensor = subnet(image)
-    tensor = BatchNorm1d(1024)(tensor)
-    out = Linear(1024, 1, activation='sigmoid')(tensor)
+    def forward(self, input1, input2):
+        """
+        Forward pass for pairs of input images.
 
-    classifier = Sequential(subnet, out)
+        Args:
+            input1 (Tensor): First input image.
+            input2 (Tensor): Second input image.
 
-    return classifier
-
-def contrastive_loss(y, y_pred):
-    """
-    Sourced from [Siamese Network with a contrastive loss](https://keras.io/examples/vision/siamese_contrastive/)
-    """
-    square = torch.square(y_pred)
-    margin = torch.square(torch.maximum(1 - y_pred, torch.zeros_like(y_pred)))
-    return torch.mean((1 - y) * square + y * margin)
-
-def siamese(height: int, width: int):
-    """ The SNN. Passes image pairs through the subnetwork,
-        and computes the distance between output vectors. 
-
-    Args:
-        height (int): height of input image
-        width (int): width of input image
-
-    Returns:
-        torch.nn.Module: compiled model
-    """
-
-    subnet = subnetwork(height, width)
-
-    image1 = torch.nn.Parameter(torch.randn(1, height * width))  # Flatten the input
-    image2 = torch.nn.Parameter(torch.randn(1, height * width))  # Flatten the input
-
-    feature1 = subnet(image1)
-    feature2 = subnet(image2)
-
-    distance = distance_layer(feature1, feature2)
-
-    # Apply layer normalization to the distance
-    tensor = LayerNorm(normalized_shape=1)(distance)
-
-    # Use torch.sigmoid to add the sigmoid activation
-    out = torch.sigmoid(torch.nn.Linear(1, 1)(tensor))
-
-    model = torch.nn.Sequential(subnet, out)
-
-    return model
+        Returns:
+            Tuple: Pair of embeddings for the input images.
+        """
+        output1 = self.forward_one(input1)
+        output2 = self.forward_one(input2)
+        return output1, output2
