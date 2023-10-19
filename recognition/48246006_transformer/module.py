92e96a078e877c2bea7bb09301813d54c6523224
saakshigupta2002 <62831255+saakshigupta2002@users.noreply.github.com>
2023-10-19 18:11:35 +1000

    Update module.py by defining the vision transformer we'll be using

--- recognition/48246006_transformer/module.py
+++ recognition/48246006_transformer/module.py
@@ -1,3 +1,15 @@
 import tensorflow as tf
-from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense, Dropout
-from tensorflow.keras.models import Model
+import tensorflow_hub as hub
+
+def transformer_model(input_shape):
+    model_url = "https://tfhub.dev/tensorflow/bert_en_uncased_L-12_H-768_A-12/2"
+    hub_layer = hub.KerasLayer(model_url, trainable=True)
+
+    input_layer = tf.keras.layers.Input(shape=input_shape)
+    x = hub_layer(input_layer)
+    x = tf.keras.layers.Dense(128, activation='relu')(x)
+    output_layer = tf.keras.layers.Dense(1, activation='sigmoid')(x)
+
+    model = tf.keras.Model(inputs=input_layer, outputs=output_layer)
+
+    return model
