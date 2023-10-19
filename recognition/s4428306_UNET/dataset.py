Center crop removed. Dice loss fixed. One-hot/softmax reintroduced.

recognition/s4428306_UNET/dataset.py
@@ -6,11 +6,7 @@ from PIL import Image
 
 #TODO: Clean up debugging notes/code.
 #      Make sure references are in order. Note use of tensorflow documentation.
-#      Change rates for adam optimizer.
-#      Rewrite train.py to be functional/non-script.
-#      Rewrite slurm script to us train.py instead of test_train.py
-#      Write prediction functions/script.
-#      Write README.
+#      Write README. Note where all files are saved in README.
 #      Make pull request.
 
 #NOTE: Expecting 2595 images in the training folders (-1 for license file) (should also be -1 for attribution file).
@@ -35,33 +31,34 @@ def loadDataFrom(directory, channels, size=128):
         data = np.zeros((numberOfImages, size, size))
     data_shape = (numberOfImages, size, size, channels)
     #data = np.zeros(data_shape)
-    for i, imageName in enumerate(os.listdir(directory)):
+    for i, imageName in enumerate(sorted(os.listdir(directory))):
         #Make sure not to load in the license file.
         if imageName != "LICENSE.txt" and imageName != "ATTRIBUTION.txt":
             imagePath = os.path.join(directory, imageName)
-            image = Image.open(imagePath)
+            #image = Image.open(imagePath)
+            #NOTE: Mention in the readme that doing this leaves some images as only skin cancer, and some as none.
+            #      If cropping isn't used mention squashed aspect ratios.
             #Center crop then resize to maintain aspect ratio.
             #Center crop algorithm from:
             #https://stackoverflow.com/questions/16646183/crop-an-image-in-the-centre-using-pil
-            crop_size = 256
-            width, height = image.size
-            left = (width - crop_size) // 2
-            top = (height - crop_size) // 2
-            right = left + crop_size
-            bottom = top + crop_size
-            image = image.crop((left, top, right, bottom)).resize((size, size))
+            #crop_size = 256
+            #width, height = image.size
+            #left = (width - crop_size) // 2
+            #top = (height - crop_size) // 2
+            #right = left + crop_size
+            #bottom = top + crop_size
+            #image = image.crop((left, top, right, bottom)).resize((size, size))
             #NOTE: Unsure if resizing should be done in this function.
             if channels > 1:
-                #data[i, :, :, :] = np.asarray(Image.open(imagePath).resize((size, size)))
-                data[i, :, :, :] = np.asarray(image)
+                data[i, :, :, :] = np.asarray(Image.open(imagePath).resize((size, size)))
+                #data[i, :, :, :] = np.asarray(image)
             else:
-                #data[i, :, :] = np.asarray(Image.open(imagePath).resize((size, size)))
-                data[i, :, :] = np.asarray(image)
+                data[i, :, :] = np.asarray(Image.open(imagePath).resize((size, size)))
+                #data[i, :, :] = np.asarray(image)
                 #data[i, :, :, :] = np.reshape(np.asarray(Image.open(imagePath).resize((size, size))), data_shape)
     #Need the extra dimension for image manipulation.
     if channels == 1:
         data = np.reshape(data, data_shape)
-    #return tf.data.Dataset.from_tensor_slices((data,))
     return tf.data.Dataset.from_tensor_slices(data)
 
 #The following is modified code from:
@@ -96,9 +93,9 @@ def normalize(image, mask):
     #image = tf.squeeze(image)
     #mask = tf.squeeze(mask)
     #Convert mask to one hot encoding.
-    #mask = tf.cast(mask, tf.int8)
-    #mask = tf.one_hot(mask, 2)
-    #mask = tf.squeeze(mask)
+    mask = tf.cast(mask, tf.int8)
+    mask = tf.one_hot(mask, 2)
+    mask = tf.squeeze(mask)
     return image, mask
 
 #Based on code from:

recognition/s4428306_UNET/predict.py
@@ -1,4 +1,80 @@
-#TODO: Convert one hot mask/prediction to image.
-#      Save images.
+from dataset import *
+from modules import *
+from train import *
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import saving
+import numpy as np
+from PIL import Image
+
+#TODO: Save images.
 #      Print graph of training history?
+#      Write specs etc.
+
+def predict_main():
+    #Load in the test batches.
+    _, _, test_batches = preprocessing()
+    #Unzip the test data to evaluate it.
+    #Code used from:
+    #https://stackoverflow.com/questions/53641920/tensorflow-tf-data-dataset-api-dataset-unzip-function
+    #test_images = test_batches.map(lambda a, b: a)
+    #test_masks = test_batches.map(lambda a, b: b)
+    #Load model and evaluate on test set.
+    with tf.keras.utils.custom_object_scope({'Dice': Dice}):
+        iunet_model = tf.keras.saving.load_model("/home/Student/s4428306/report/iunet_model.keras")
+    evaluation = iunet_model.evaluate(test_batches, batch_size=64, return_dict=True)
+    for name, value in evaluation.items():
+        print(f"{name}: {value:.4f}")
+    #Save 2 examples for inspection.
+    test_data = test_batches.unbatch()
+    test_data = list(test_data.as_numpy_iterator())
+    #TODO: Refactor this.
+    #Process first example.
+    ex1_ind = 50
+    dp1_image_og = test_data[ex1_ind][0]
+    dp1_image = dp1_image_og * 255
+    #dp1_image = dp1_image.numpy()
+    dp1_image = dp1_image.astype(np.uint8)
+    Image.fromarray(dp1_image).save("/home/Student/s4428306/report/example1_image.png")
+    dp1_mask = test_data[ex1_ind][1]
+    dp1_mask = np.argmax(dp1_mask, axis=2)
+    #dp1_mask = dp1_mask.numpy()
+    dp1_mask = dp1_mask.astype(np.uint8) * 255
+    Image.fromarray(dp1_mask).save("/home/Student/s4428306/report/example1_mask.png")
+    dp1_pred = iunet_model.call(tf.expand_dims(tf.convert_to_tensor(dp1_image_og), axis=0))
+    dp1_pred = tf.squeeze(dp1_pred)
+    print("DP1_PRED SHAPE BEFORE ARGMAX", dp1_pred.shape)
+    dp1_pred = np.argmax(dp1_pred, axis=2)
+    print("DP1_PRED SHAPE AFTER ARGMAX", dp1_pred.shape)
+    #dp1_pred = dp1_pred.numpy()
+    dp1_pred = dp1_pred.astype(np.uint8) * 255
+    Image.fromarray(dp1_pred).save("/home/Student/s4428306/report/example1_pred.png")
+    #Process second example.
+    ex2_ind = 70
+    dp2_image_og = test_data[ex2_ind][0]
+    dp2_image = dp2_image_og * 255
+    #dp2_image = dp2_image.numpy()
+    dp2_image = dp2_image.astype(np.uint8)
+    Image.fromarray(dp2_image).save("/home/Student/s4428306/report/example2_image.png")
+    dp2_mask = test_data[ex2_ind][1]
+    dp2_mask = np.argmax(dp2_mask, axis=2)
+    #dp2_mask = dp2_mask.numpy()
+    dp2_mask = dp2_mask.astype(np.uint8) * 255
+    Image.fromarray(dp2_mask).save("/home/Student/s4428306/report/example2_mask.png")
+    dp2_pred = iunet_model.call(tf.expand_dims(tf.convert_to_tensor(dp2_image_og), axis=0))
+    dp2_pred = tf.squeeze(dp2_pred)
+    dp2_pred = np.argmax(dp2_pred, axis=2)
+    #dp2_pred = dp2_pred.numpy()
+    dp2_pred = dp2_pred.astype(np.uint8) * 255
+    Image.fromarray(dp2_pred).save("/home/Student/s4428306/report/example2_pred.png")
+
+"""
+LOAD MODEL
+RUN PREDICTIONS ON TEST
+PLOT PREDICTIONS METRICS FROM TEST
+CHOOSE 2 RANDOM (IMAGE, MASK) PAIRS AND SHOW PREDICTION FOR THEM. (NEED TO CONVERT TO IMAGE.)
+"""
+
+if __name__ == "__main__":
+    predict_main()
 

recognition/s4428306_UNET/train.py
@@ -4,60 +4,86 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow import math
 from tensorflow.keras import layers
+from matplotlib import pyplot as plt
 
 #TODO: Clean up this file.
 #      Reference UNet model this is based on.
-#      Write documentation for Dice loss function.
+#      Write documentation for these functions/classes.
 
 class Dice(tf.keras.losses.Loss):
     def __init__(self, reduction=tf.keras.losses.Reduction.AUTO, name=None, k=1):
         super().__init__(reduction=reduction, name=name)
-        self.k = 1
+        self.k = k
 
     def call(self, y_true, y_pred):
-        #Number of classes.
-        #NOTE: Number of classes could be a class parameter passed to the init function.
-        #k = y_true.shape[-1]
+        #NOTE: Want to know if whole batches are being passed to this function.
+        #      If so, surely it would have complained by now, right?
+        print("Y_TRUE SHAPE", y_true.shape)
+        print("Y_PRED SHAPE", y_pred.shape)
         #Compute Dice loss.
         total = 0
         for i in range(self.k):
             #Will these be different shapes if batches are used?
-            y_true = y_true[:, :, i]
-            y_pred = y_pred[:, :, i]
-            numerator = tf.math.reduce_sum(y_true * y_pred)
-            denominator = tf.math.reduce_sum(y_true) + tf.math.reduce_sum(y_pred)
+            y_true_i = y_true[:, :, i]
+            y_pred_i = y_pred[:, :, i]
+            numerator = tf.math.reduce_sum(y_true_i * y_pred_i)
+            denominator = tf.math.reduce_sum(y_true_i) + tf.math.reduce_sum(y_pred_i)
             total += numerator / denominator
-        #NOTE: Unsure about whether this should be negative or not.
         return -1 * (2 / self.k) * total
 
-dice_loss = Dice()
+    def get_config(self):
+        config = super().get_config()
+        config.update({"k": self.k})
+        return config
 
-#!!!
-#Going to try and train it using the built in functions.
-#!!!
-#Load data.
-train_batches, val_batches, test_batches = preprocessing()
+#Plotting code taken from:
+#https://stackoverflow.com/questions/41908379/keras-plot-training-validation-and-test-set-accuracy
+def create_plots(history):
+    #Save accuracy plot.
+    plt.figure(0)
+    plt.plot(history.history['accuracy'])
+    plt.plot(history.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.savefig("/home/Student/s4428306/report/accuracy_plot.png")
+    #Save loss plot.
+    plt.figure(1)
+    plt.plot(history.history['loss'])
+    plt.plot(history.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'val'], loc='upper left')
+    plt.savefig("/home/Student/s4428306/report/loss_plot.png")
 
-#Build model.
-iunet_model = build_improved_unet_model()
-iunet_model.summary()
-iunet_model.compile(optimizer=tf.keras.optimizers.Adam(),
-#                    loss="sparse_categorical_crossentropy",
-                    loss=dice_loss,
-                    metrics="accuracy")
+def train_main():
+    #Load data.
+    train_batches, val_batches, test_batches = preprocessing()
+    #Set up training parameters.
+    BATCH_SIZE = 64
+    NUM_EPOCHS = 10
+    TRAIN_N = 1796
+    TRAIN_LENGTH = 2 * TRAIN_N
+    STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
+    #lr_schedule = tf.keras.optimizers.schedules.ExponentialDecay(0.0005, STEPS_PER_EPOCH, 0.985)
+    #NOTE: No l2 decay yet.
+    #a_opt = tf.keras.optimizers.Adam(learning_rate=lr_schedule)
+    dice_loss = Dice(k=2)
+    #Build model.
+    iunet_model = build_improved_unet_model()
+    iunet_model.summary()
+    iunet_model.compile(optimizer=tf.keras.optimizers.Adam(),
+                        loss=dice_loss,
+                        metrics="accuracy")
+    model_history = iunet_model.fit(train_batches,
+                                    epochs=NUM_EPOCHS,
+                                    steps_per_epoch=STEPS_PER_EPOCH,
+                                    validation_data=val_batches)
+    iunet_model.save("/home/Student/s4428306/report/iunet_model.keras", save_format="keras")
+    create_plots(model_history)
 
-#Train model.
-BATCH_SIZE = 64
-#NOTE: Going to make this brief for testing purposes.
-NUM_EPOCHS = 2
-TRAIN_N = 1796
-TRAIN_LENGTH = 2 * TRAIN_N
-STEPS_PER_EPOCH = TRAIN_LENGTH // BATCH_SIZE
-model_history = iunet_model.fit(train_batches,
-                                epochs=NUM_EPOCHS,
-                                steps_per_epoch=STEPS_PER_EPOCH,
-                                validation_data=val_batches)
-iunet_model.save("/home/Student/s4428306/report/iunet_model_attempt2.keras", save_format="keras")
+if __name__ == "__main__":
+    train_main()
 
-print(model_history)
-print("SUCCESS")