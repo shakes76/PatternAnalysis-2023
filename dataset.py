e6d7efe7ad1e8dd0838894ae1cc1b6685906c786
Deven Bhasin <d.bhasin@uqconnect.edu.au>
2023-10-19 13:44:06 +1000

    dataset structure changed

--- dataset.py
+++ dataset.py
@@ -1,28 +1,39 @@
+import tensorflow as tf
 import os
-import torch
-from torchvision import transforms
-from torch.utils.data import DataLoader, Dataset
-from PIL import Image
+import glob
 
-class CustomDataset(Dataset):
-    def __init__(self, data_dir, transform=None):
-        self.data_dir = data_dir
-        self.transform = transform
-        self.image_files = [f for f in os.listdir(os.path.join(data_dir, "images")) if f.endswith(".png")]
-        self.mask_files = [f for f in os.listdir(os.path.join(data_dir, "masks")) if f.endswith(".png")]
+def process_images(file_path, is_mask):
+    # Decodes the image at the given file location
+    if is_mask:
+        image = tf.image.decode_png(tf.io.read_file(file_path), channels=1)
+    else:
+        image = tf.image.decode_jpeg(tf.io.read_file(file_path), channels=3)
+    # Converts the image to float32
+    image_converted = tf.image.convert_image_dtype(image, tf.float32)
+    # Resizes the image to fit the given dimensions
+    image_resized = tf.image.resize(image_converted, size=(img_height, img_width))
+    # Normalises input image
+    if is_mask:
+        image_final = image_resized
+    else:
+        image_final = tf.cast(image_resized, tf.float32) / 255.0
+    return image_final
 
-    def __len__(self):
-        return len(self.image_files)
+def create_ds():
+    # Calculates the size of each test, train, and validation subset
+    files_ds_size = len(list(files_ds))
+    train_ds_size = int(training_split * files_ds_size)
+    val_ds_size = int(validation_split * files_ds_size)
+    test_ds_size = files_ds_size - train_ds_size - val_ds_size
+    # Prints the size of all the subsets
+    print("Training size: %d" % train_ds_size)
+    print("Validation size: %d" % val_ds_size)
+    print("Testing size: %d" % test_ds_size)
+    # Splits the dataset into test, validate, and train subsets
+    train = files_ds.take(train_ds_size)
+    val = files_ds.skip(train_ds_size).take(val_ds_size)
+    test = files_ds.skip(train_ds_size).skip(val_ds_size)
+    return train, val, test
 
-    def __getitem__(self, index):
-        image_path = os.path.join(self.data_dir, "images", self.image_files[index])
-        mask_path = os.path.join(self.data_dir, "masks", self.mask_files[index])
+img_height = img_width = 256
 
-        image = Image.open(image_path)
-        mask = Image.open(mask_path)
-
-        if self.transform:
-            image = self.transform(image)
-            mask = self.transform(mask)
-
-        return image, mask
