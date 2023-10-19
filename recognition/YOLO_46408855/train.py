0c3f26d44228ec9688ee34d9890c92ecbcb1b6e9
Charlotte <cpietsch19@gmail.com>
2023-10-20 09:50:57 +1000

    Added test loop

--- recognition/YOLO_46408855/train.py
+++ recognition/YOLO_46408855/train.py
@@ -4,7 +4,7 @@ import torch.nn.functional as F
 import time
 
 from dataset import *
-from modules import YOLO, YOLO_loss
+from modules import *
 
 
 # Device configuration
@@ -16,12 +16,12 @@ learning_rate=0.001
 image_size = 416
 batch_size = 10
 
-#data - change directories as needed
+#Train data - change directories as needed
 mask_dir = '/content/drive/MyDrive/Uni/COMP3710/ISIC-2017_Training_Part1_GroundTruth/'
 image_dir = '/content/drive/MyDrive/Uni/COMP3710/ISIC-2017_Training_Data/'
 labels = '/content/drive/MyDrive/Uni/COMP3710/ISIC-2017_Training_Part3_GroundTruth.csv'
-dataset = ISICDataset(image_dir, mask_dir, labels, image_size)
-dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=True)
+train_dataset = ISICDataset(image_dir, mask_dir, labels, image_size)
+train_dataloader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
 
 #Model
 model = YOLO()
@@ -32,7 +32,7 @@ optimizer = torch.optim.Adam(model.parameters(), learning_rate)
 criterion = YOLO_loss()
 
 #learning rate schedule, using because SGD is dumb, adam has its own learning rate
-total_step = len(dataloader)
+total_step = len(train_dataloader)
 scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer,max_lr=learning_rate,
                                                 steps_per_epoch=total_step, epochs=epochs)
 
@@ -40,7 +40,7 @@ scheduler = torch.optim.lr_scheduler.OneCycleLR(optimizer,max_lr=learning_rate,
 model.train()
 start = time.time()
 for epoch in range(epochs):
-  for i, (images, labels) in enumerate(dataloader):
+  for i, (images, labels) in enumerate(train_dataloader):
     images = images.to(device)
     labels = labels.to(device)
 
@@ -64,3 +64,40 @@ for epoch in range(epochs):
 end = time.time()
 elapsed = end - start
 print("Training took {} secs or {} mins.".format(elapsed, elapsed/60))
+
+#Test data
+mask_dir = '/content/drive/MyDrive/Uni/COMP3710/ISIC-2017_Test_v2_Part1_GroundTruth/'
+image_dir = '/content/drive/MyDrive/Uni/COMP3710/ISIC-2017_Test_v2_Data/'
+labels = '/content/drive/MyDrive/Uni/COMP3710/ISIC-2017_Test_v2_Part3_GroundTruth.csv'
+test_dataset = ISICDataset(image_dir, mask_dir, labels, 416)
+test_dataloader = DataLoader(test_dataset, batch_size, shuffle=True)
+
+#Test
+model.eval()
+torch.set_grad_enabled(True)
+start = time.time()
+total = 0
+total_step = len(test_dataloader)
+
+for i, (images, labels) in enumerate(test_dataloader):
+    images = images.to(device)
+    labels = labels.to(device)
+    outputs = model(images)
+
+    #Calculate IoU
+    for a in range(batch_size):
+      best_box = filter_boxes(outputs[a])
+      if best_box is not None:
+        best_box = torch.reshape(best_box, (1, 7))
+        iou = single_iou(best_box, labels[a,:])
+        total += iou[0]
+
+    #Keep track of average
+    average = total/(i+1)
+
+    if (i+1) % 50 == 0:
+      print("Step[{},{}] IoU average: {:.5f}".format(i+1, total_step, average))
+
+end = time.time()
+elapsed = end - start
+print("Testing took {} secs or {} mins.".format(elapsed, elapsed/60))
