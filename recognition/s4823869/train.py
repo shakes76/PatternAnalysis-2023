68f7e339f6ea944cb9147197c72ab0700e21c38d
YashMitttal <140570495+YashMitttal@users.noreply.github.com>
2023-10-20 04:37:12 +1000

    Refactored and improved the training process and model initialization for a GAN with progressive growing.

--- recognition/s4823869/train.py
+++ recognition/s4823869/train.py
@@ -1,87 +1,154 @@
 import torch
-import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from dataset import get_ttv, normalise
-from modules import build_vqvae, build_pcnn
-from torch.autograd import Variable
+from torch import optim
+from tqdm import tqdm
+import os
+from math import log2
+import matplotlib.pyplot as plt
 
-# Constants
-LATENT_DIMENSION_SIZE = 8
-NUM_EMBEDDINGS = 64
-BETA = 0.25
-VQVAE_EPOCHS = 100
-PCNN_EPOCHS = 100
-BATCH_SIZE = 128
-MODEL_PATH = "vqvae.pth"
-PCNN_PATH = "pcnn.pth"
+import modules
+import dataset
 
-def train_vqvae(train_set, train_vnce):
-    # Create VQVAE model
-    vqvae = build_vqvae(LATENT_DIMENSION_SIZE, NUM_EMBEDDINGS, BETA)
-    vqvae = vqvae.cuda()  # Move model to GPU
-    optimizer = optim.Adam(vqvae.parameters())
+# Release GPU memory
+torch.mps.empty_cache()
 
-    # Define loss function
-    criterion = nn.MSELoss()
+DEVICE = 'mps' if torch.backends.mps.is_available() else 'cpu'
+Z_DIm = 512
+W_DIM = 512
+LAMBDA_GP = 10  # coefficient of gradient penalty increased from 10 to 20, and back to 10
+BATCH_SIZES = [256, 128, 64, 32, 16, 8]
+PROGRESSIVE_EPOCHS = [10] * (len(BATCH_SIZES)-2)  # reduce the number of epoches for all resolutions from 30 to 15, and back to 30
+IN_CHANNELS = 512
+CHANNELS_IMG = 3
+LR = 1e-3
+LR_CRITIC = 5e-4
+START_TRAIN_IMG_SIZE = 4
 
-    # Training loop
-    for epoch in range(VQVAE_EPOCHS):
-        vqvae.train()
-        total_loss = 0.0
-        for data in train_set:
-            inputs = Variable(data).cuda()
-            optimizer.zero_grad()
-            outputs = vqvae(inputs)
-            loss = criterion(outputs, inputs)
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
-        print(f"Epoch {epoch + 1}, Loss: {total_loss / len(train_set)}")
 
-    # Save the trained model
-    torch.save(vqvae.state_dict(), MODEL_PATH)
-    print("VQVAE model saved")
+# Regularization on the discriminator / critic
+def gradient_penalty(critic, real, fake, alpha, train_step, device="cpu"):
+    BATCH_SIZE, C, H, W = real.shape
+    beta = torch.rand((BATCH_SIZE, 1, 1, 1)).repeat(1, C, H, W).to(device)
+    interpolated_images = real * beta + fake.detach() * (1 - beta)
+    interpolated_images.requires_grad_(True)
 
-def train_pcnn(train_set, vqvae):
-    # Create PCNN model
-    pcnn = build_pcnn(vqvae)
-    pcnn = pcnn.cuda()  # Move model to GPU
-    optimizer = optim.Adam(pcnn.parameters())
+    # Calculate critic scores
+    mixed_scores = critic(interpolated_images, alpha, train_step)
 
-    # Define loss function
-    criterion = nn.CrossEntropyLoss()
+    # Take the gradient of the scores with respect to the images
+    gradient = torch.autograd.grad(
+        inputs=interpolated_images,
+        outputs=mixed_scores,
+        grad_outputs=torch.ones_like(mixed_scores),
+        create_graph=True,
+        retain_graph=True,
+    )[0]
+    gradient = gradient.view(gradient.shape[0], -1)
+    gradient_norm = gradient.norm(2, dim=1)
+    gradient_penalty = torch.mean((gradient_norm - 1) ** 2)
+    return gradient_penalty
 
-    # Training loop
-    for epoch in range(PCNN_EPOCHS):
-        pcnn.train()
-        total_loss = 0.0
-        for data in train_set:
-            inputs = Variable(data).cuda()
-            optimizer.zero_grad()
-            logits = pcnn(inputs)
-            loss = criterion(logits, inputs)
-            loss.backward()
-            optimizer.step()
-            total_loss += loss.item()
-        print(f"Epoch {epoch + 1}, Loss: {total_loss / len(train_set)}")
 
-    # Save the trained model
-    torch.save(pcnn.state_dict(), PCNN_PATH)
-    print("PCNN model saved")
+gen_losses = []
+critic_losses = []
 
-def main():
-    # Load and preprocess the data
-    tr, _, _ = get_ttv()
-    train_set = DataLoader(normalise(tr), batch_size=BATCH_SIZE, shuffle=True)
 
-    # Train the VQVAE
-    train_vqvae(train_set, tr.var())
+# The nature of WGAN's training means that the critic's (or discriminator's) loss should ideally
+# approach zero, while the generator tries to make it as negative as possible.
+def train_fn(critic, gen, loader, dataset, step, alpha, opt_critic, opt_gen):
+    loop = tqdm(loader, leave=True)
 
-    # Train the PCNN
-    vqvae = build_vqvae(LATENT_DIMENSION_SIZE, NUM_EMBEDDINGS, BETA)
-    vqvae.load_state_dict(torch.load(MODEL_PATH))
-    train_pcnn(train_set, vqvae)
+    for batch_idx, real in enumerate(loop):
+        real = real.to(DEVICE)
+        cur_batch_size = real.shape[0]
+        noise = torch.randn(cur_batch_size, Z_DIm).to(DEVICE)  # z
+        fake = gen(noise, alpha, step)
+        critic_real = critic(real, alpha, step)
+        critic_fake = critic(fake.detach(), alpha, step)
+        gp = gradient_penalty(critic, real, fake, alpha, step, DEVICE)
+        # enhanced version of the WGAN discriminator loss
+        loss_critic = (
+                -(torch.mean(critic_real) - torch.mean(critic_fake))
+                + LAMBDA_GP * gp
+                + (0.001) * torch.mean(critic_real ** 2)
+        )
+        # store the absolute loss for plotting
+        critic_losses.append(abs(loss_critic.item()))
 
-if __name__ == "__main__":
-    main()
+        critic.zero_grad()
+        loss_critic.backward()
+        opt_critic.step()
+
+        gen_fake = critic(fake, alpha, step)
+        loss_gen = -torch.mean(gen_fake)
+        # store the absolute loss for plotting
+        gen_losses.append(abs(loss_gen.item()))
+
+        gen.zero_grad()
+        loss_gen.backward()
+        opt_gen.step()
+
+        alpha += cur_batch_size / (
+                PROGRESSIVE_EPOCHS[step] * 0.5 * len(dataset)
+        )
+        alpha = min(alpha, 1)
+
+        # display both losses during training
+        loop.set_postfix(
+            gp=gp.item(),
+            loss_critic=loss_critic.item(),
+            loss_gen=loss_gen.item()
+        )
+    return alpha
+
+
+# Model initialization
+gen = modules.Generator(Z_DIm, W_DIM, IN_CHANNELS, CHANNELS_IMG).to(DEVICE)
+critic = modules.Discriminator(IN_CHANNELS, CHANNELS_IMG).to(DEVICE)
+# Optimization
+opt_gen = optim.Adam([{'params': [param for name, param in gen.named_parameters() if 'map' not in name]},
+                      {'params': gen.map.parameters(), 'lr': 1e-5}], lr=LR, betas=(0.0, 0.99))
+opt_critic = optim.Adam(critic.parameters(), lr=LR_CRITIC,
+                        betas=(0.0, 0.99))  # change from (0.0, 0.99) to (0.5, 0.99) and back to (0.0, 0.99)
+
+# train mode
+gen.train()
+critic.train()
+
+# Progressive training
+step = int(log2(START_TRAIN_IMG_SIZE / 4))
+for num_epochs in PROGRESSIVE_EPOCHS[step:]:
+    alpha = 1e-7
+    loader, data = dataset.get_loader(4 * 2 ** step)
+    print('Current image size: ' + str(4 * 2 ** step))
+
+    for epoch in range(num_epochs):
+        print(f'Epoch [{epoch + 1}/ {num_epochs}')
+        alpha = train_fn(critic, gen, loader, data, step, alpha, opt_critic, opt_gen)
+
+    step += 1
+
+# Save the models
+torch.save(gen.state_dict(), 'OASIS_style_gan_generater.pth')
+
+# Print allocated memory
+allocated_memory = torch.cuda.memory_allocated()
+print(f"Memory allocated: {allocated_memory / (1024 ** 2):.2f} MB")
+
+
+# Plot the losses of generator and discriminater
+def plot_and_save_losses(gen_losses, critic_losses, save_path):
+    plt.figure(figsize=(10, 5))
+    plt.plot(gen_losses, label="Generator Loss", color="yellow")
+    plt.plot(critic_losses, label="Critic/Discriminator Loss", color="green")
+    plt.title("Training Losses")
+    plt.xlabel("Batch")
+    plt.ylabel("Loss")
+    plt.legend()
+
+    # Save the plot to a file
+    plt.savefig(save_path)
+    plt.close()
+
+
+save_path = os.path.join("output_images", "losses_plot.png")
+plot_and_save_losses(gen_losses, critic_losses, save_path)
