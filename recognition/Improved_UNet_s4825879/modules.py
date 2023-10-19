e63b9d67d73afb45966799aae12342b1c56a1955
Terym2001 <tadyrkor@gmail.com>
2023-10-19 19:08:07 +1000

    restructured modules.py. same functionality

--- recognition/Improved_UNet_s4825879/modules.py
+++ recognition/Improved_UNet_s4825879/modules.py
@@ -16,185 +16,152 @@ class DiceLoss(nn.Module):
         dice = (2*intersect)/(predict.sum() + target.sum())
         return 1 - dice
 
+class Conv(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size=3, stride=1, padding=1):
+        super(Conv, self).__init__()
+        negative_slope = 5*10**(-5)
+
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=kernel_size, stride=stride, padding=padding),
+            nn.BatchNorm2d(out_channels),
+            nn.LeakyReLU(negative_slope)
+        )
+
+    def forward(self, out):
+        return self.conv(out)
+
 class Context(nn.Module):
     def __init__(self, size):
         super(Context, self).__init__()
         self.pdrop = 0.3
         self.neagative_slope = 10**(-2)
 
-        self.batchNorm = nn.BatchNorm2d(size)
-        self.conv = nn.Conv2d(
-            size, size, kernel_size=3, padding=(1,1)
+        self.context = nn.Sequential(
+            Conv(size, size),
+            nn.Dropout2d(self.pdrop),
+            Conv(size, size)
         )
-        self.dropOut = nn.Dropout2d(self.pdrop)
-
+            
     def forward(self, input):
         out = input
-        out = self.conv(F.relu(self.batchNorm(out)))
-        #out = F.relu(self.batchNorm(self.conv(out)))
-        out = self.dropOut(out)
-        out = F.relu(self.batchNorm(self.conv(out)))
+        out = self.context(out)
         return torch.add(out, input)
 
 class Upsampling(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(Upsampling, self).__init__()
 
-        self.upscale = nn.Upsample(scale_factor=2)
-        self.batchNorm = nn.BatchNorm2d(out_channels)
-        self.conv = nn.Conv2d(
-           in_channels, out_channels, kernel_size=3 , padding=(1,1)
-        ) 
+        self.upsample = nn.Sequential(
+            nn.Upsample(scale_factor=2),
+            Conv(in_channels, out_channels)
+        )
 
     def forward(self, out):
-        out = self.upscale(out)
-        out = F.relu(self.batchNorm(self.conv(out)))
-        return out
+        return self.upsample(out)
         
 class Localization(nn.Module):
     def __init__(self, in_channels, out_channels):
         super(Localization, self).__init__()
-        self.conv1 = nn.Conv2d(
-            in_channels, in_channels, kernel_size=3, padding=(1,1)
+
+        self.localize = nn.Sequential(
+            Conv(in_channels, in_channels, kernel_size=3),
+            Conv(in_channels, out_channels, kernel_size=1, padding=0),
         )
-        self.batchNorm1 = nn.BatchNorm2d(in_channels)
-        self.conv2 = nn.Conv2d(
-            in_channels, out_channels, kernel_size=1
-        )
-        self.batchNorm2 = nn.BatchNorm2d(out_channels)
         
     def forward(self, out):
-        out = F.relu(self.batchNorm1(self.conv1(out)))
-        out = F.relu(self.batchNorm2(self.conv2(out))) 
-        return out
-        
+        return self.localize(out)
+
 class ImpUNet(nn.Module):
-    def __init__(self, in_channel, negative_slope=10**(-2)):
+    def __init__(self, in_channels=3):
         super(ImpUNet, self).__init__()
-        self.negative_slope = negative_slope
-        # arcitecture components
-        self.conv1 = nn.Conv2d(
-            in_channel, 16, kernel_size=3, padding=(1,1)
-        )
-        self.batchNorm1 = nn.BatchNorm2d(16)
+        
+        # ENCODING
+        # layer 1
+        self.conv1 = Conv(in_channels, 16)
         self.context1 = Context(16)
-
-        self.conv2 = nn.Conv2d(
-            16, 32, kernel_size=3, stride=2, padding=(1,1)
-        )
-        self.batchNorm2 = nn.BatchNorm2d(32)
+        
+        # layer 2
+        self.conv2 = Conv(16, 32, stride=2)
         self.context2 = Context(32)
-
-        self.conv3 = nn.Conv2d(
-            32, 64, kernel_size=3, stride=2, padding=(1,1)
-        )
-        self.batchNorm3 = nn.BatchNorm2d(64)
+        
+        # layer 3
+        self.conv3 = Conv(32, 64, stride=2)
         self.context3 = Context(64)
         
-        self.conv4 = nn.Conv2d(
-            64, 128, kernel_size=3, stride=2, padding=(1,1)
-        )
-        self.batchNorm4 = nn.BatchNorm2d(128)
+        # layer 4
+        self.conv4 = Conv(64, 128, stride=2)
         self.context4 = Context(128)
         
-        self.conv5 = nn.Conv2d(
-            128, 256, kernel_size=3, stride=2, padding=(1,1)
-        )
-        self.batchNorm5 = nn.BatchNorm2d(256)
+        # BOTTLENECK 
+        self.conv5 = Conv(128, 256, stride=2)
         self.context5 = Context(256)
+        self.upsample = Upsampling(256, 128)
         
-        self.upsample0 = Upsampling(256, 128)
-        
+        # DECODING
+        # layer 4
         self.localize1 = Localization(256, 128)
         self.upsample1 = Upsampling(128, 64)
-
+        
+        # layer 3
         self.localize2 = Localization(128, 64)
         self.upsample2 = Upsampling(64, 32)
-
+        
+        # layer 2
         self.localize3 = Localization(64, 32)
         self.upsample3 = Upsampling(32, 16)
         
-        self.conv6 = nn.Conv2d(
-            32, 32, kernel_size=3, padding=(1,1)
-        )
-        self.batchNorm6 = nn.BatchNorm2d(32)
-
+        # layer 1
+        self.conv6 = Conv(32, 32)
+        
+        # segmentation
         self.upscale = nn.Upsample(scale_factor=2)
-        
-        self.Sigmoid = nn.Sigmoid()
-        # segmentation layers
-        self.seg1 = nn.Conv2d(
-            64, 1, kernel_size=1
-        )
-        self.segNorm1 = nn.BatchNorm2d(1)
-        self.seg2 = nn.Conv2d(
-            32, 1, kernel_size=1
-        )
-        self.segNorm2 = nn.BatchNorm2d(1)
-        self.seg3 = nn.Conv2d(
-            32, 1, kernel_size=1
-        )
-        self.segNorm3 = nn.BatchNorm2d(1)
-        
+        self.seg_conv1 = Conv(64, 1, kernel_size=1, padding=0)
+        self.seg_conv2 = Conv(32, 1, kernel_size=1, padding=0)
+               
+        # final output
+        self.sigmoid = nn.Sigmoid()
+
     def forward(self, out):
-        # convolution layer. input 3d image output 16 channels
-        out = F.relu(self.batchNorm1(self.conv1(out))) 
-        # contaxt block. input/output 16 channels
+        # layer 1
+        out = self.conv1(out)
         out = self.context1(out)
-        # save information
-        layer1 = out
-        # convolution layer. input 16 channels, output 32 channels
-        out = F.relu(self.batchNorm2(self.conv2(out)))
-        # context block. input/output 32 channels
+        skip_connection1 = out
+        # layer 2
+        out = self.conv2(out)
         out = self.context2(out)
-        # save information
-        layer2 = out
-        # convolution layer. input 32 channels. output 64 channels
-        out = F.relu(self.batchNorm3(self.conv3(out))) 
-        # context block. input/output 64 channels
+        skip_connection2 = out
+        # layer 3
+        out = self.conv3(out)
         out = self.context3(out)
-        # save information
-        layer3 = out
-        # convolutional layer. input 64 channels. output 128 channels
-        out = F.relu(self.batchNorm4(self.conv4(out)))
-        # context block. input/output 128 channels
+        skip_connection3 = out
+        # layer 4
+        out = self.conv4(out)
         out = self.context4(out)
-        # save information
-        layer4 = out
-        # convolutional layer. input 128 channels. output 256 channels
-        out = F.relu(self.batchNorm5(self.conv5(out)))
-        # context block. input/output 256 channels
+        skip_connection4 = out
+        # bottleneck
+        out = self.conv5(out)
         out = self.context5(out)
-        # upsample module. input 256 channels. output 128 channels
-        out = self.upsample0(out)
-        # concatinate. 0 here is dimension
-        out = torch.cat((out, layer4), 1)
-        # localization module. input 256 channels. output 128 channels
+        out = self.upsample(out)
+        # layer 4
+        out = torch.cat((out, skip_connection4), dim=1)
         out = self.localize1(out)
-        # upsampling module. input 128 channels. output 64 channels
         out = self.upsample1(out)
-        # concatinate 
-        out = torch.cat((out, layer3), 1)
-        # localization layer. input 128 channels. output 64 channels
+        # layer 3
+        out = torch.cat((out, skip_connection3), dim=1)
         out = self.localize2(out)
-        # first segmentation layer
-        seg1 = self.upscale(F.relu(self.segNorm1(self.seg1(out))))
-        # upsampling module. input 64 channels. output 32 channels
+        seg1 = self.upscale(self.seg_conv1(out))
         out = self.upsample2(out)
-        # concatinate
-        out = torch.cat((out, layer2), 1)
-        # localization module. input 64 channels, output 32 channels
+        # layer 2
+        out = torch.cat((out, skip_connection2), dim=1)
         out = self.localize3(out)
-        # second segmentation layer
-        seg2 = self.upscale(torch.add(F.relu(self.segNorm2(self.seg2(out))), seg1))
-        # upsampling module. input 32 channels. output 16 channels
+        seg2 = self.upscale(torch.add(self.seg_conv2(out), seg1))
         out = self.upsample3(out)
-        # concatinate
-        out = torch.cat((out, layer1), 1)
-        # convolutional layer. input/output 32 channels
-        out = F.relu(self.batchNorm6(self.conv6(out)))
-        # elementwise summation of current out and seg2
-        out = torch.add(F.relu(self.segNorm3(self.seg3(out))), seg2)
-        # softmax
-        out = self.Sigmoid(out)
+        # layer 1
+        out = torch.cat((out, skip_connection1), dim=1)
+        out = self.conv6(out)
+        out = torch.add(self.seg_conv2(out), seg2)
+        
+        # sigmoid
+        out = self.sigmoid(out)
+        
         return out
