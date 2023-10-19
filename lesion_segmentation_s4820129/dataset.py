da44b7915322e5a97c4ebbbfdba6f1c9578c8a54
IsakOrlando <isakow@stud.ntnu.no>
2023-10-19 10:26:12 +1000

    fixing dataset module

--- lesion_segmentation_s4820129/dataset.py
+++ lesion_segmentation_s4820129/dataset.py
@@ -1,9 +1,25 @@
-#dataset module
 import os
 from PIL import Image
 from torch.utils.data import Dataset
-from torchvision import transforms
 import numpy as np
+from torchvision import transforms
+
+class CustomCompose(transforms.Compose):
+    def __call__(self, image, mask):
+        for t in self.transforms:
+            image, mask = t(image, mask)
+        return image, mask
+
+class CustomResize:
+    def __init__(self, size):
+        self.size = size
+
+    def __call__(self, image, mask):
+        image = transforms.Resize(self.size)(image)
+        mask = transforms.Resize(self.size)(mask)
+        return image, mask
+
+
 
 class ISICdataset(Dataset):
     def __init__(self, image_dir, truth_dir, transform=None, target_size=(256, 256)):
@@ -16,23 +32,22 @@ class ISICdataset(Dataset):
         self.images = [f for f in os.listdir(image_dir) if f.lower().endswith(('.jpg', '.jpeg', '.png', '.bmp', '.gif'))]
 
     def __getitem__(self, index):
-        img_path = os.path.join(self.image_dir, self.images[index])
-        truth_path = os.path.join(self.truth_dir, self.images[index].replace('.jpg', '_segmentation.png'))
-        
-        # Open and resize the image to the target size
-        image = Image.open(img_path).convert('RGB')
-        image = transforms.Resize(self.target_size)(image)
+      img_path = os.path.join(self.image_dir, self.images[index])
+      truth_path = os.path.join(self.truth_dir, self.images[index].replace('.jpg', '_segmentation.png'))
 
-        # Open and resize the truth mask to the target size
-        truth = Image.open(truth_path).convert('L')
-        truth = transforms.Resize(self.target_size)(truth)
+      # Open and resize the image to the target size
+      image = Image.open(img_path).convert('RGB')
+      image = transforms.Resize(self.target_size)(image)
 
-        if self.transform is not None:
-            augmentations = self.transform(image=np.array(image), mask=np.array(truth))
-            image = augmentations['image']
-            truth = augmentations['mask']
+      # Open and resize the truth mask to the target size
+      truth = Image.open(truth_path).convert('L')
+      truth = transforms.Resize(self.target_size)(truth)
 
-        return image, truth
+      if self.transform is not None:
+        image, truth = self.transform(image, truth)
+        image = transforms.ToTensor()(image)
+        truth = transforms.ToTensor()(truth)
+      return image, truth
 
     def __len__(self):
         return len(self.images)
