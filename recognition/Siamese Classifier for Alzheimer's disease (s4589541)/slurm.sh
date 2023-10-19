e7482154342dcf544242a2a9f0a56efbce4d46b8
milesgardiner <68379513+milesgardiner@users.noreply.github.com>
2023-10-20 01:49:24 +1000

    Update slurm.sh

--- recognition/Siamese Classifier for Alzheimer's disease (s4589541)/slurm.sh
+++ recognition/Siamese Classifier for Alzheimer's disease (s4589541)/slurm.sh
@@ -7,9 +7,7 @@ time=0-00:15:00
 #SBATCH --job-name="siamese"
 #SBATCH --account=s4589541
 #SBATCH --mail-user=m.gardiner@uqconnect.edu.au
-#SBATCH --mail-type=BEGIN
-#SBATCH --mail-type=END
-#SBATCH --mail-type=FAIL
+#SBATCH --mail-type=All
 #SBATCH —output=output_dir/%j.out
 conda activate /home/Student/s4589541/miniconda3/envs/venv
 python /home/Student/s4589541/comp3710/report/PatternAnalysis-2023/recognition/Siamese\ Classifier\ for\ Alzheimer\'s\ disease\ \(s4589541\)/train.py
