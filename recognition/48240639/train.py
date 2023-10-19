e4527324fc2a2197b64f4b73cd98e069dd248a47
Aniket Gupta <aniketgupta1707@gmail.com>
2023-10-20 04:28:16 +1000

    Made the initial training model for the project

--- recognition/48240639/train.py
+++ recognition/48240639/train.py
@@ -1,50 +1,72 @@
 
 """
 Created on Wednesday October 18 
+Siamese Network Training Script
 
-This script is dedicated to the complete process of training, validating, testing, and saving the VQVAE model.
-The model itself is imported from modules.py, and the data loader is sourced from dataset.py.
-Throughout the training process, relevant metrics are plotted to monitor progress effectively.
+This script is used to train a Siamese Network model on a dataset, with support for validation.
+It includes training and validation loops, model saving, and TensorBoard logging.
 
 @author: Aniket Gupta 
 @ID: s4824063
 
 """
+from modules import SiameseNN
+from dataset import get_training
+import os
+from torch.utils.tensorboard import SummaryWriter
 import torch
+from torch.utils.data import DataLoader, Subset
 import torch.optim as optim
-import torch.nn.functional as F
+from torch import nn
 
-def train_model(model, train_loader, optimizer, criterion):
+def train(model, dataloader, device, optimizer, epoch):
     model.train()
-    for batch in train_loader:
-        data, target = batch['data'], batch['label']
+    criterion = nn.BCELoss()
+    correct = 0
+    total_samples = 0
+
+    for batch_idx, (images_1, images_2, targets) in enumerate(dataloader):
+        images_1, images_2, targets = images_1.to(device), images_2.to(device), targets.to(device)
         optimizer.zero_grad()
-        output = model(data)
-        loss = criterion(output, target)
+        outputs = model(images_1, images_2).squeeze()
+        loss = criterion(outputs, targets)
         loss.backward()
         optimizer.step()
 
-def validate_model(model, val_loader, criterion):
+        pred = torch.where(outputs > 0.5, torch.tensor(1.0, device=device), torch.tensor(0.0, device=device))
+        correct += pred.eq(targets.view_as(pred)).sum().item()
+        total_samples += len(targets)
+
+        if batch_idx % 10 == 0:
+            accuracy = 100. * correct / total_samples
+
+            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}\tAccuracy: {:.2f}%'.format(
+                epoch, total_samples, len(dataloader.dataset),
+                100. * batch_idx / len(dataloader), loss.item(), accuracy))
+
+            writer.add_scalar('Training Loss', loss.item(), epoch * len(dataloader) + batch_idx)
+            writer.add_scalar('Training Accuracy', accuracy, epoch * len(dataloader) + batch_idx)
+
+def validate(model, dataloader, device):
+    print("Starting Validation.")
     model.eval()
-    val_loss = 0
+    test_loss = 0
     correct = 0
+    criterion = nn.BCELoss()
+
     with torch.no_grad():
-        for batch in val_loader:
-            data, target = batch['data'], batch['label']
-            output = model(data)
-            val_loss += criterion(output, target).item()
-            pred = output.argmax(dim=1, keepdim=True)
-            correct += pred.eq(target.view_as(pred)).sum().item()
+        for (images_1, images_2, targets) in dataloader:
+            images_1, images_2, targets = images_1.to(device), images_2.to(device), targets.to(device)
+            outputs = model(images_1, images_2).squeeze()
+            test_loss += criterion(outputs, targets).sum().item()
+            pred = torch.where(outputs > 0.5, 1, 0)
+            correct += pred.eq(targets.view_as(pred)).sum().item()
 
-    val_loss /= len(val_loader.dataset)
-    accuracy = correct / len(val_loader.dataset)
-    print('Validation set: Average loss: {:.4f}, Accuracy: {}/{} ({:.2f}%)'.format(
-        val_loss, correct, len(val_loader.dataset), 100. * accuracy))
+    test_loss /= len(dataloader.dataset)
 
-def test_model(model, test_loader):
-    model.eval()
-    # Testing loop logic
+    print('\nVal set: Average loss: {:.4f}, Validation Accuracy: {}/{} ({:.0f}%)\n'.format(
+        test_loss, correct, len(dataloader.dataset),
+        100. * correct / len(dataloader.dataset)))
 
-def save_model(model, filepath):
-    torch.save(model.state_dict(), filepath)
-    print("Model saved successfully.")
+    print("Finished Validation.")
+
