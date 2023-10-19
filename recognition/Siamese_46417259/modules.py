ea2dcd0d6120beabd5962c551943fafb7e85f1bb
Minhao Sun <141893731+minheed-s@users.noreply.github.com>
2023-10-19 14:19:17 +1000

    Revert "implemented VGGTwin, an alternative approach to Siamese backbone. Pending testing"
    This reverts commit c723c890d0ad2e4adb4b087b49ed435c1ac0cc59.
    
    reverted after testing, VGGTwin did not deliver significant improvements in classification accuracy

--- recognition/Siamese_46417259/modules.py
+++ recognition/Siamese_46417259/modules.py
@@ -33,46 +33,11 @@ class SiameseTwin(nn.Module):
         # out = F.sigmoid(self.fc(out))
         return out
     
-cfg = {
-    'VGG9': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M'],
-    'VGG11': [64, 'M', 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    'VGG13': [64, 64, 'M', 128, 128, 'M', 256, 256, 'M', 512, 512, 'M', 512, 512, 'M'],
-    'VGG16': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 'M', 512, 512, 512, 'M', 512, 512, 512, 'M'],
-    'VGG19': [64, 64, 'M', 128, 128, 'M', 256, 256, 256, 256, 'M', 512, 512, 512, 512, 'M', 512, 512, 512, 512, 'M'],
-}
-
-class VGGTwin(nn.Module):
-    def __init__(self, vgg_name, in_channels):
-        super(VGGTwin, self).__init__()
-        self.in_channels = in_channels
-        self.features = self._make_layers(cfg[vgg_name], self.in_channels)
-        self.fc = nn.Linear(25088, 1024)
-
-    def forward(self, x): #forward pass of the model
-        out = self.features(x)
-        out = out.view(out.size(0), -1) #view as 1D
-        out = self.fc(out)
-        return out
-
-    def _make_layers(self, cfg, in_channels):
-        layers = []
-        for x in cfg:
-            if x == 'M':
-                layers += [nn.MaxPool2d(kernel_size=2, stride=2)]
-            else:
-                layers += [nn.Conv2d(in_channels, x, kernel_size=3, padding=1),
-                           nn.BatchNorm2d(x),
-                           nn.ReLU(inplace=True)]
-                in_channels = x
-        layers += [nn.AvgPool2d(kernel_size=1, stride=1)]
-        return nn.Sequential(*layers)
-
 class SiameseNeuralNet(nn.Module):
     def __init__(self) -> None:
         super(SiameseNeuralNet, self).__init__()
 
-        # self.backbone = SiameseTwin()
-        self.backbone = VGGTwin('VGG16', 3)
+        self.backbone = SiameseTwin()
         # self.fc = nn.Linear(4096, 1)
 
     def forward(self, x1, x2):
@@ -118,9 +83,9 @@ class SimpleMLP(nn.Module):
         super(SimpleMLP, self).__init__()
         
         self.mlp = nn.Sequential(
-            # nn.Linear(4096, 1024),
+            nn.Linear(4096, 1024),
             # nn.BatchNorm1d(1024),
-            # nn.ReLU(),
+            nn.ReLU(),
             nn.Linear(1024, 128),
             # nn.BatchNorm1d(128),
             nn.ReLU(),
@@ -189,21 +154,13 @@ def test_mlp():
     # loss = F.binary_cross_entropy(torch.sigmoid(input), target)
     # print(loss)
 
-def test_VGG_twin():
-    test = VGGTwin('VGG16', 3)
-    print(test)
-
-    input = torch.rand(2, 3, 240, 240)
-    x = test(input)
-    print(x.shape)
 
 
 
 if __name__ == "__main__":
-    # test_one_twin()
+    test_one_twin()
     # test_entire_net()
     # test_mlp()
-    test_VGG_twin()
 
 
 
