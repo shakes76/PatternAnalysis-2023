8f287e247c27a215223c22be1b04249ad25686bd
Abhinavmohindersingh <141258868+Abhinavmohindersingh@users.noreply.github.com>
2023-10-20 09:53:33 +1000

    Removed debug statements and refactored the code

--- Brain_MRI_SuperResolution/utils/data_loader.py
+++ Brain_MRI_SuperResolution/utils/data_loader.py
@@ -1,9 +1,6 @@
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-import os
-import numpy as np
 from tensorflow.keras.preprocessing import image_dataset_from_directory
 import os
-import tensorflow as tf;
 import numpy as np
 from PIL import Image, ImageOps
 from PIL import ImageEnhance
@@ -35,7 +32,6 @@ def downsample_image(img, factor=4):
 
 def process_images(input_folder, output_folder, factor=4, display_sample=True):
     """Process and downsample the images in the given input folder and save them to the output folder."""
-    print(f"Processing images in folder: {input_folder}")
 
     # Create output directory if not exists
     if not os.path.exists(output_folder):
@@ -66,7 +62,7 @@ def process_images(input_folder, output_folder, factor=4, display_sample=True):
                 except Exception as e:
                     print(f"Error processing {img_path}: {e}")
 
-    print(f"Completed processing images in folder: {input_folder}")
+
 
 
 if __name__ == "__main__":
@@ -87,17 +83,10 @@ def load_images_from_category(base_dir, data_type, category, target_size=(100, 1
     images = []
     category_dir = os.path.join(base_dir, data_type, category)
 
-    # Print the directory we're attempting to load from
-    print("Loading images from directory:", category_dir)
-
     for root, _, files in os.walk(category_dir):
         for file in files:
             if file.endswith(('.jpg', '.jpeg', '.png')):
                 image_path = os.path.join(root, file)
-
-                # Print the path of the image we're attempting to load
-                print("Loading image:", image_path)
-
                 image = load_img(image_path, target_size=target_size, color_mode='grayscale')
                 image_arr = img_to_array(image) / 255.0
                 images.append(image_arr)
@@ -105,88 +94,4 @@ def load_images_from_category(base_dir, data_type, category, target_size=(100, 1
                 # Print files that were skipped due to file type mismatch
                 print("Skipped file (not an image or unsupported format):", file)
 
-    # Print the total number of images loaded
-    print(f"Total {category} images loaded:", len(images))
-
     return np.array(images)
-
-# def load_data():
-#     crop_size = 300
-#     upscale_factor = 3
-#     input_size = crop_size // upscale_factor
-#     batch_size = 8
-#
-#     # --------- Load Datasets ---------
-#     print(f"Attempting to load from: {train_dir}")  # Add this for diagnostics
-#
-#     train_ds = image_dataset_from_directory(
-#         train_dir,
-#         batch_size=batch_size,
-#         image_size=(crop_size, crop_size),
-#         validation_split=0.2,
-#         subset="training",
-#         seed=1337,
-#         label_mode=None
-#     )
-#
-#     print(f"Found {len(train_ds)} batches of training data.")
-#
-#     valid_ds = image_dataset_from_directory(
-#         test_dir, batch_size=batch_size, image_size=(crop_size, crop_size),
-#         validation_split=0.2, subset="validation", seed=1337, label_mode=None)
-#
-#     # --------- Scaling Function ---------
-#     def scaling(input_image):
-#         return input_image / 255.0
-#
-#     # Apply scaling to datasets
-#     train_ds = train_ds.map(scaling)
-#     valid_ds = valid_ds.map(scaling)
-#
-#     # --------- Input and Target Processing Functions ---------
-#     def process_input(input, input_size, upscale_factor):
-#         input = tf.image.rgb_to_yuv(input)
-#         y, u, v = tf.split(input, 3, axis=-1)
-#         return tf.image.resize(y, [input_size, input_size], method="area")
-#
-#     def process_target(input):
-#         input = tf.image.rgb_to_yuv(input)
-#         y, u, v = tf.split(input, 3, axis=-1)
-#         return y
-#
-#     # Preprocess datasets by applying the above functions
-#     def preprocess_datasets(train_ds, valid_ds, input_size, upscale_factor):
-#         train_ds = train_ds.map(lambda x: (process_input(x, input_size, upscale_factor), process_target(x)))
-#         train_ds = train_ds.prefetch(buffer_size=32)
-#
-#         valid_ds = valid_ds.map(lambda x: (process_input(x, input_size, upscale_factor), process_target(x)))
-#         valid_ds = valid_ds.prefetch(buffer_size=32)
-#
-#         return train_ds, valid_ds
-#
-#     # Apply preprocessing to datasets
-#     train_ds, valid_ds = preprocess_datasets(train_ds, valid_ds, input_size, upscale_factor)
-#
-#     return train_ds, valid_ds
-#
-#
-# if __name__ == '__main__':
-#     train_ds, valid_ds = load_data()
-#
-#
-#     # --------- Visualization Function ---------
-#     def visualize_samples(train_ds):
-#         print("Visualizing a few sample training images:")
-#         for batch in train_ds.take(1):
-#             for img in batch[0]:
-#                 array_to_img(img).show()
-#             for img in batch[1]:
-#                 array_to_img(img).show()
-#
-#
-#     visualize_samples(train_ds)
-#
-#     # --------- Prepare Test Image Paths ---------
-#     test_img_paths = sorted(
-#         [os.path.join(test_dir, fname) for fname in os.listdir(test_dir) if fname.endswith(".jpeg")])
-#     print(f"Found {len(test_img_paths)} test images.")
