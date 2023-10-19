9fbbd368b51e419c71e1aa3ef139dd8aa3d6474c
Tong Qiu <tongqiu@s4560219.vpn.uq.edu.au>
2023-10-19 21:24:24 +1000

    model for triplet loss added.

--- recognition/TongQiu_Siamese/modules.py
+++ recognition/TongQiu_Siamese/modules.py
@@ -37,13 +37,41 @@ class Baseline_Contrastive(nn.Module):
 
 
 class Baseline_Triplet(nn.Module):
-    pass
+    def __init__(self):
+        super(Baseline_Triplet, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(1, 64, 10),  # 64@231x247
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),  # 64@115x123
+            nn.Conv2d(64, 128, 7),  # 128@109x117
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),  # 128@54x58
+            nn.Conv2d(128, 128, 4),  # 128@51x55
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(2),  # 128@25x27
+            nn.Conv2d(128, 256, 4),  # 256@22x24
+            nn.ReLU(inplace=True),
+        )
+        self.liner = nn.Sequential(nn.Linear(135168, 4096), nn.Sigmoid())  # nn.Linear(9216, 4096)
+
+    def sub_forward(self, x):
+        x = self.conv(x)
+        x = x.view(x.size()[0], -1)
+        x = self.liner(x)
+        return x
+
+    def forward(self, anchor, positive, negative):
+        anchor = self.sub_forward(anchor)
+        positive = self.sub_forward(positive)
+        negative = self.sub_forward(negative)
+        return anchor, positive, negative
 
 
 # test if the model works properly
 if __name__ == '__main__':
-    model = Baseline_Contrastive()
+    model = Baseline_Triplet()
     test_tensor = torch.ones(3, 1, 240, 256)
-    output = model(test_tensor, test_tensor)
+    output = model(test_tensor, test_tensor, test_tensor)
     print(output[0].shape)
     print(output[1].shape)
+    print(output[2].shape)
