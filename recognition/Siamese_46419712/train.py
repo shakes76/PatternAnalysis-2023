2c9e79b8214ac0f6840e51edb30be23e04d6543a
MTbir <m.tieu@uqconnect.edu.au>
2023-10-19 22:38:22 +1000

    implement t-sne to view the data dissimilarity of siamese model, also since the model is overfitting, save model at the last epoch instead of based on the lowest loss

--- recognition/Siamese_46419712/train.py
+++ recognition/Siamese_46419712/train.py
@@ -4,18 +4,21 @@ import matplotlib.pyplot as plt
 import os
 import torch.nn as nn
 import numpy as np
-
+from sklearn.manifold import TSNE
+import utils
 import random
 from modules import RawSiameseModel, ContrastiveLossFunction, BinaryModelClassifier
 from dataset import LoadData
 
-SIAMESE_LOSS_SAVE_PATH = "/home/Student/s4641971/project/result/siamese_loss_plot.png"
-SIAMESE_MODEL_SAVE_PATH = "/home/Student/s4641971/project/result/siamese.pt"
+from predict import random_test_model, save_plot_image
 
-CLASSIFIER_LOSS_SAVE_PATH = "/home/Student/s4641971/project/result/classifier_loss_plot.png"
-CLASSIFIER_MODEL_SAVE_PATH = "/home/Student/s4641971/project/result/classifier.pt"
+SIAMESE_LOSS_SAVE_PATH = utils.siamese_loss
+SIAMESE_MODEL_SAVE_PATH = utils.siamese
 
-VALIDATE_ACCURACY_SAVE_PATH = "/home/Student/s4641971/project/result/Accuracy_plot.png"
+CLASSIFIER_LOSS_SAVE_PATH = utils.classifer_loss
+CLASSIFIER_MODEL_SAVE_PATH = utils.classifier
+
+VALIDATE_ACCURACY_SAVE_PATH = utils.train_accuracy
 
 def train_siamese(model, train_loader, criterion, optimizer, loss_list):
     model.train()
@@ -64,10 +67,22 @@ def validate_siamese(model, val_loader, criterion, val_loss_list):
                 print (">>>>> Step [{}/{}] Validate Loss: {:.5f}"
                         .format(i+1, len(val_loader), loss.item()))
 
+def plot_tsne(fv, label, name, file):
+    fv = fv.cpu().detach().numpy()
+    label = label.cpu().detach().numpy()
+    tsne = TSNE(n_components=2, random_state=42)
+    val_tsne = tsne.fit_transform(fv)
+    plt.scatter(val_tsne[:, 0], val_tsne[:, 1], c=label)
+    plt.title(name)
+    plt.savefig(file)
+    plt.close()
+
 def train_classifier(sModel, cModel, train_loader, criterion, optimizer, loss_list):
     sModel.eval() # siamese
     cModel.train() # classifier
 
+    correct_predict = 0
+    total_test = 0
     for i, val in enumerate(train_loader):
         img, label = val
         img = img.to(device)
@@ -75,8 +90,14 @@ def train_classifier(sModel, cModel, train_loader, criterion, optimizer, loss_li
         optimizer.zero_grad()
 
         fv1 = sModel(img)
+        plot_tsne(fv1, label, "t-SNE train", utils.tsne_train)
         output = cModel(fv1)
         loss = criterion(output.view(-1), label)
+
+        # train test
+        predicted = (output.view(-1) > 0.5).float()
+        total_test += label.size(0)
+        correct_predict += (predicted == label).sum().item()
 
         loss.backward()
         optimizer.step()
@@ -87,7 +108,8 @@ def train_classifier(sModel, cModel, train_loader, criterion, optimizer, loss_li
             print (">>>>> Step [{}/{}] Loss: {:.5f}"
                     .format(i+1, len(train_loader), loss.item()))
     
-    return cModel
+    accuracy = 100 * correct_predict / total_test
+    return cModel, accuracy
 
 def validate_classifier(sModel, cModel, val_loader, criterion, val_loss_list):
     sModel.eval()
@@ -102,6 +124,7 @@ def validate_classifier(sModel, cModel, val_loader, criterion, val_loss_list):
 
             fv = sModel(img) 
             output = cModel(fv)
+            plot_tsne(output, label, "t-SNE validate", utils.tsne_validate)
             output = output.view(-1)
             loss = criterion(output, label)
             val_loss_list.append(loss.item())
@@ -130,15 +153,11 @@ def test_model(model, cModel, test_loader):
             label = label.to(device).float()
 
             fv = model(img)
+            plot_tsne(fv, label, "t-SNE test", utils.tsne_test)
             output = cModel(fv)
             output = output.view(-1)
 
             predicted = (output > 0.5).float()
-            # print(">>>>> Predicted")
-            # print(predicted)
-
-            # print(">>>>> Actual")
-            # print(label)
             total_test += label.size(0)
             correct_predict += (predicted == label).sum().item()
     
@@ -164,12 +183,14 @@ def save_loss_plot(loss_list, val_loss_list, epoch=0, siamese=True):
 
     plt.close()
 
-def save_val_accuracy_plot(accuracy_list, epoch=0):
+def save_val_accuracy_plot(accuracy_list, train_accuracy_list, epoch=0):
     """
         Save accuracy list during validate for classifier
     """
     plt.figure(figsize=(12, 8))
     plt.plot(accuracy_list, label="Accuracy")
+    plt.plot(train_accuracy_list, label="Train Accuracy")
+    plt.legend()
     plt.xlabel('Epoch')
     plt.ylabel('Accuracy %')
     plt.title(f"Classifier validate accuracy -> Total epoch {epoch + 1}")
@@ -213,7 +234,7 @@ def execute_sTrain(train_loader, val_loader):
     model = RawSiameseModel().to(device)
 
     # hyper-parameters
-    num_epochs = 10
+    num_epochs = 20
     learning_rate = 0.0001
 
     criterion = ContrastiveLossFunction()
@@ -230,6 +251,7 @@ def execute_sTrain(train_loader, val_loader):
     # save model based on validate
     best_val_loss = 100 # should not be 100
     best_model = None
+
 
     start = time.time() #time generation
     print("Start training")
@@ -260,6 +282,9 @@ def execute_sTrain(train_loader, val_loader):
         val_loss_list = []
         save_loss_plot(avg_loss_list, avg_val_loss, epoch)
 
+    # test save the last model
+    save_model(epoch, best_model, criterion, optimizer.state_dict())
+
     end = time.time() #time generation
 
     print("Finish training")
@@ -273,11 +298,11 @@ def execute_cTrain(sModel, train_loader_classifier, val_loader_classifier):
     cModel = BinaryModelClassifier().to(device)
 
     # hyper parameters for classifier
-    num_epochs = 50
+    num_epochs = 30
     learning_rate = 0.001
 
     criterion = nn.BCELoss()
-    optimizer = torch.optim.Adam(cModel.parameters(), lr=learning_rate)
+    optimizer = torch.optim.Adam(cModel.parameters(), lr=learning_rate, betas=(0.5, 0.999))
 
     # training
     classifier_loss_list = []
@@ -289,10 +314,14 @@ def execute_cTrain(sModel, train_loader_classifier, val_loader_classifier):
 
     # accuracy
     accuracy_list = []
+    train_accuracy_list = []
 
     # save model based on validate
-    best_val_loss = 100 # should not be 100
+    # best_val_loss = 100 # should not be 100
     best_model = None
+
+    # save model based on accuracy
+    best_acc = 0
 
     print("Start classifier training")
     
@@ -300,33 +329,37 @@ def execute_cTrain(sModel, train_loader_classifier, val_loader_classifier):
     # train classifier
     for epoch in range(num_epochs):
         print ("Epoch [{}/{}]".format(epoch + 1, num_epochs))
-        cModel = train_classifier(sModel, cModel, train_loader_classifier, criterion, optimizer, classifier_loss_list)
+        cModel, t_accuracy = train_classifier(sModel, cModel, train_loader_classifier, criterion, optimizer, classifier_loss_list)
         accuracy = validate_classifier(sModel, cModel, val_loader_classifier, criterion, classifier_val_loss_list)
         
         accuracy_list.append(accuracy)
+        train_accuracy_list.append(t_accuracy)
         
         current_classifier_val_loss = np.mean(classifier_val_loss_list)
 
         avg_classifier_loss_list.append(np.mean(classifier_loss_list))
         avg_classifier_val_loss.append(current_classifier_val_loss)
-
+        
         if best_model is None:
             best_model = cModel.state_dict()
-            best_val_loss = current_classifier_val_loss
-            print(f"Current best validate loss is {best_val_loss} at epoch {epoch + 1}")
+            best_acc = accuracy
+            print(f"Current best accuracy is {best_acc} at epoch {epoch + 1}")
             save_model(epoch, best_model, criterion, optimizer.state_dict(), 1)
-        elif current_classifier_val_loss < best_val_loss:
+        elif accuracy > best_acc:
             best_model = cModel.state_dict()
-            print(f"The previous best validate loss is {best_val_loss}")
-            best_val_loss = current_classifier_val_loss
-            print(f"Current best validate loss is {best_val_loss} at epoch {epoch + 1}")
+            print(f"The previous best accuracy is {best_acc}")
+            best_acc = accuracy
+            print(f"Current best validate accuracy is {best_acc} at epoch {epoch + 1}")
             save_model(epoch, best_model, criterion, optimizer.state_dict(), 1)
         
         classifier_loss_list = []
         classifier_val_loss_list = []
 
         save_loss_plot(avg_classifier_loss_list, avg_classifier_val_loss, epoch, siamese=False)
-        save_val_accuracy_plot(accuracy_list, epoch)
+        save_val_accuracy_plot(accuracy_list, train_accuracy_list, epoch)
+    
+    # test save the last model
+    save_model(epoch, best_model, criterion, optimizer.state_dict(), 1)
 
     end = time.time() #time generation
     print("Finish classifier training")
@@ -338,8 +371,8 @@ def execute_cTrain(sModel, train_loader_classifier, val_loader_classifier):
 
 if __name__ == '__main__':
     os.environ["CUBLAS_WORKSPACE_CONFIG"] = ":4096:8"
-    random.seed(60)
-    torch.manual_seed(60)
+    random.seed(42)
+    torch.manual_seed(42)
     torch.use_deterministic_algorithms(True)
 
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
@@ -386,3 +419,8 @@ if __name__ == '__main__':
     print("Finish Testing")
     elapsed = end - start
     print("Testing took " + str(elapsed) + " secs of " + str(elapsed/60) + " mins in total")
+
+    #########  TESTING MODEL ##########
+    img, label, predicted = random_test_model(device, siamese_model, classifier_model, test_loader)
+
+    save_plot_image(img, label, predicted)
