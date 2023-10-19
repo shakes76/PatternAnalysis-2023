7ee838f6e28c3dd0513c50151516cf45f695cc23
louisljoker <142284214+louisljoker@users.noreply.github.com>
2023-10-19 20:20:37 +1000

    Update utils.py

--- utils.py
+++ utils.py
@@ -3,8 +3,6 @@ from PIL import Image
 import matplotlib.pyplot as plt
 
 
-
-
 def dice_coefficient(predicted, target, epsilon=1e-6):
     # Flatten the tensors
     predicted = predicted.view(-1).float()
@@ -16,18 +14,21 @@ def dice_coefficient(predicted, target, epsilon=1e-6):
     return dice.item()
 
 
-def display_images_from_directory(directory_path):
+def display_images_from_directory(directory_path, max_images=10, extensions=['.jpg', '.jpeg', '.png']):
     # List all files in the directory
     files = os.listdir(directory_path)
 
-    # Filter out the files that are JPEG images
-    jpg_files = [f for f in files if f.lower().endswith('.jpg')]
+    # Filter out the files that are images with the specified extensions
+    image_files = [f for f in files if any(f.lower().endswith(ext) for ext in extensions)]
 
-    for jpg_file in jpg_files:
+    for idx, image_file in enumerate(image_files):
+        if idx >= max_images:
+            break
         # Construct the full image path
-        image_path = os.path.join(directory_path, jpg_file)
+        image_path = os.path.join(directory_path, image_file)
 
         # Open and display the image
         image = Image.open(image_path)
         image.show()
+
 
