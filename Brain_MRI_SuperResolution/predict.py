7dedff81a1479d57afe3b9018a80b92e2a024b89
Abhinavmohindersingh <141258868+Abhinavmohindersingh@users.noreply.github.com>
2023-10-19 17:49:57 +1000

    Added PSNR metrics for better evaluation of the model

--- Brain_MRI_SuperResolution/predict.py
+++ Brain_MRI_SuperResolution/predict.py
@@ -1,67 +1,65 @@
-import numpy as np
-import cv2
-from tensorflow.keras.models import load_model
-from models import sub_pixel_cnn
+import matplotlib.pyplot as plt
+import tensorflow as tf
 
-print("[DEBUG] Starting the program.")
+# Constants
+HEIGHT = 256
+WIDTH = 256
+DOWNSCALE_FACTOR = 4
 
-test_image_path = 'AD_NC/test/AD/388206_87.jpeg'
-print(f"[DEBUG] Loading image from path: {test_image_path}")
-test_image = cv2.imread(test_image_path, cv2.IMREAD_GRAYSCALE)
+import tensorflow as tf
 
-if test_image is None:
-    print("Error loading the image!")
-    exit()
+def psnr_metric(y_true, y_pred):
+    return tf.image.psnr(y_true, y_pred, max_val=1.0)
 
-print("[DEBUG] Image loaded successfully.")
-print(f"[DEBUG] Image shape: {test_image.shape}")
+def predict(model, image):
+    """Use the model to predict the image from the lowres image and plot results"""
+    input = tf.expand_dims(image, axis=0)
+    output = model.predict(input)
+    plt.imshow(output[0] / 255.0)
 
+    # plt.imshow(output[0] / 255.0, cmap='gray')
+
+    plt.title("Upscaled Image")
+    plt.axis('off')
+    plt.show()
+
+
+def displayPredictions(model, test_image_path):
+    """Function used to display the Original image, the low resolution image
+        and the upscaled image the model has predicted"""
+
+    # Load the image
+    image = tf.io.read_file(test_image_path)
+    image = tf.io.decode_jpeg(image, channels=1)
+    original = tf.image.resize(image, [HEIGHT, WIDTH])
+    downscaled = tf.image.resize(original, [100, 100], method=tf.image.ResizeMethod.BILINEAR)
+
+    plt.figure(figsize=(10, 5))
+
+    plt.subplot(1, 2, 1)
+    plt.imshow(original.numpy().squeeze(), cmap='gray')
+    plt.title("Original Image")
+    plt.axis('off')
+
+    plt.subplot(1, 2, 2)
+    plt.imshow(downscaled.numpy().squeeze(), cmap='gray')
+    plt.title("Downscaled Image")
+    plt.axis('off')
+    plt.tight_layout()
+    plt.show()
+
+    predict(model, downscaled)
+
+
+# Load model and image
 model_path = 'saved_models/sub_pixel_cnn_model.h5'
-print(f"[DEBUG] Loading model from path: {model_path}")
-model = load_model(model_path)
-print("[DEBUG] Model loaded successfully.")
+model = tf.keras.models.load_model(
+    model_path,
+    custom_objects={
+        "LeakyReLU": tf.keras.layers.LeakyReLU,
+        "psnr_metric": psnr_metric
+    }
+)
+test_image_path = 'AD_NC/test/AD/388206_87.jpeg'
 
-# Resize the image to the expected input size for the model
-print("[DEBUG] Resizing the image for model input.")
-test_image_downsampled = cv2.resize(test_image, (64, 60), interpolation=cv2.INTER_CUBIC)
-print(f"[DEBUG] Resized image shape: {test_image_downsampled.shape}")
-
-input_data = np.expand_dims(np.expand_dims(test_image_downsampled, axis=-1), axis=0)
-print("[DEBUG] Preparing image data for prediction.")
-print(f"[DEBUG] Input data shape: {input_data.shape}")
-
-print("[DEBUG] Running the prediction.")
-predicted_image = model.predict(input_data)
-print("[DEBUG] Prediction completed.")
-print(f"[DEBUG] Predicted image shape: {predicted_image[0].shape}")
-
-# Rescale values from 0-1 to 0-255 and convert to uint8 type for displaying/saving with OpenCV
-predicted_image_rescaled = (predicted_image[0] * 255).astype(np.uint8)
-print("[DEBUG] Rescaled predicted image for visualization.")
-
-# # Display the image
-# print("[DEBUG] Displaying the predicted image.")
-# cv2.imshow('Predicted Image', predicted_image_rescaled)
-# cv2.waitKey(0)
-# cv2.destroyAllWindows()
-
-# Display the images side by side for comparison
-# Ensure all images have the same height before concatenation
-height = test_image.shape[0]
-width_ratio_downsampled = test_image_downsampled.shape[1] / test_image_downsampled.shape[0]
-width_ratio_predicted = predicted_image_rescaled.shape[1] / predicted_image_rescaled.shape[0]
-
-
-test_image_downsampled_resized = cv2.resize(test_image_downsampled, (test_image_downsampled.shape[1] * height // test_image_downsampled.shape[0], height))
-predicted_image_rescaled_resized = cv2.resize(predicted_image_rescaled, (predicted_image_rescaled.shape[1] * height // predicted_image_rescaled.shape[0], height))
-
-
-concatenated_output = np.hstack((test_image, test_image_downsampled_resized, predicted_image_rescaled_resized))
-cv2.imshow('Original | Downsampled | Predicted', concatenated_output)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-
-
-# Optionally, save the image
-# cv2.imwrite('path_to_save_predicted_image.png', predicted_image_rescaled)
-print("[DEBUG] Program finished successfully.")
+displayPredictions(model, test_image_path)
