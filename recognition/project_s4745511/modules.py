290eef889ecd3c19f5197cd7d3da51bcd6fed06d
Danita Anubhuti <52007397+danitaanubhuti@users.noreply.github.com>
2023-10-19 21:06:45 +1000

    Modified classifier and siamese model structure (modules.py)

--- recognition/project_s4745511/modules.py
+++ recognition/project_s4745511/modules.py
@@ -1,124 +1,76 @@
-import os
-from xmlrpc.client import Boolean
 import tensorflow as tf
-import keras as k
-import numpy as np
-import random
+import keras.layers as kl
+import keras.backend as kb
 
-# GLOBAL_PATH = 'C:\\Users\\Wom\\Desktop\\COMP3710\\ADNI_AD_NC_2D\\'
 
-AD_PATH = "/content/extracted/AD_NC/train/AD"
-CN_PATH = "/content/extracted/AD_NC/train/NC"
+def create_siamese_network(height: int, width: int):
 
-AD_TEST_PATH = "/content/extracted/AD_NC/test/AD"
-CN_TEST_PATH = "/content/extracted/AD_NC/test/NC"
+    def create_distance_layer(im1_feature, im2_feature):
+      tensor = kb.sum(kb.square(im1_feature - im2_feature), axis=1, keepdims=True)
+      return kb.sqrt(kb.maximum(tensor, kb.epsilon()))
 
-import tensorflow as tf
-from tensorflow.keras import layers, models
+    # Create a shared neural network (subnetwork)
+    subnet = create_cnn_network(height, width)
 
-def cnn_network(height, width): 
-    """ The modified subnetwork using CNN
+    # Define inputs for two images
+    image1_input = kl.Input((height, width, 1))
+    image2_input = kl.Input((height, width, 1))
 
-    Args:
-        height (int): Height of the input image
-        width (int): Width of the input image
+    # Pass the inputs through the shared subnetwork
+    feature1 = subnet(image1_input)
+    feature2 = subnet(image2_input)
 
-    Returns:
-        tf.keras.Model: The modified subnetwork Model
-    """
-    input = kl.Input(shape=(height, width, 1))
-    conv1 = kl.Conv2D(32, (3, 3), activation='relu', padding='same')(input)
+    # Compute the distance between the feature vectors
+    distance = create_distance_layer(feature1, feature2)
+    normalized_distance = kl.BatchNormalization()(distance)
+
+    # Output layer for similarity prediction
+    output = kl.Dense(units=1, activation='sigmoid')(normalized_distance)
+
+    # Create the Siamese model
+    siamese_model = tf.keras.models.Model([image1_input, image2_input], output)
+
+    # Define the optimizer and compile the model with the contrastive loss
+    optimizer = tf.optimizers.Adam(learning_rate=0.0001)
+    siamese_model.compile(loss=create_contrastive_loss, metrics=['accuracy'], optimizer=optimizer)
+
+    return siamese_model
+
+def create_cnn_network(height, width):
+    # Define the input layer
+    input_layer = kl.Input(shape=(height, width, 1))
+
+    # Convolutional and pooling layers for feature extraction
+    conv1 = kl.Conv2D(32, (3, 3), activation='relu', padding='same')(input_layer)
     pool1 = kl.MaxPooling2D((2, 2))(conv1)
     conv2 = kl.Conv2D(64, (3, 3), activation='relu', padding='same')(pool1)
     pool2 = kl.MaxPooling2D((2, 2))(conv2)
     conv3 = kl.Conv2D(128, (3, 3), activation='relu', padding='same')(pool2)
     pool3 = kl.MaxPooling2D((2, 2))(conv3)
+
+    # Flatten the output
     flat = kl.Flatten()(pool3)
+
+    # Fully connected layers
     dense1 = kl.Dense(512, activation='relu', kernel_regularizer='l2')(flat)
     dense2 = kl.Dense(512, activation='relu', kernel_regularizer='l2')(dense1)
 
-    subnet = k.Model(inputs=input, outputs=dense2, name='subnet')
-
+    # Create and return the neural network model
+    subnet = tf.keras.models.Model(inputs=input_layer, outputs=dense2, name='subnet')
     return subnet
 
-
-def distance_layer(im1_feature, im2_feature):
-    """ Layer to compute (euclidean) difference between feature vectors
-
-    Args:
-        im1_feature (tensor): feature vector of an image
-        im2_feature (tensor): feature vector of an image
-
-    Returns:
-        tensor: Tensor containing differences
-    """
-    tensor = kb.sum(kb.square(im1_feature - im2_feature), axis=1, keepdims=True)
-    return kb.sqrt(kb.maximum(tensor, kb.epsilon())) 
-
-
-def classification_model(subnet):
-    """ Build the classification Model with a CNN-based subnetwork
-
-    Args:
-        subnet (layer): the modified subnetwork with convolutional layers
-
-    Returns:
-        model: compiled model
-    """
-    image = kl.Input((128, 128, 1))
-    tensor = subnet(image)
-    tensor = kl.BatchNormalization()(tensor)
-    out = kl.Dense(units=1, activation='sigmoid')(tensor)
-
-    classifier = Model([image], out)
-
-    opt = tf.optimizers.Adam(learning_rate=0.0001)
-
-    classifier.compile(loss='binary_crossentropy', metrics=['accuracy'], optimizer=opt)
-
+def create_classification_model(subnet):
+    image_input = kl.Input((128, 128, 1))
+    feature = subnet(image_input)
+    feature = kl.BatchNormalization()(feature)
+    output_layer = kl.Dense(units=1, activation='sigmoid')(feature)
+    classifier = tf.keras.models.Model([image_input], output_layer)
+    optimizer = tf.optimizers.Adam(learning_rate=0.0001)
+    classifier.compile(loss='binary_crossentropy', metrics=['accuracy'], optimizer=optimizer)
     return classifier
 
-
-def contrastive_loss(y, y_pred):
-    """
-    Sourced from [Siameses Network with a contrastive loss](https://keras.io/examples/vision/siamese_contrastive/)
-    """
+# Define the contrastive loss function
+def create_contrastive_loss(y, y_pred):
     square = tf.math.square(y_pred)
-    margin = tf.math.square(tf.math.maximum(1 - (y_pred), 0))
+    margin = tf.math.square(tf.math.maximum(1 - y_pred, 0))
     return tf.math.reduce_mean((1 - y) * square + (y) * margin)
-
-
-def siamese(height: int, width: int):
-    """ The SNN. Passes image pairs through the subnetwork,
-        and computes distance between output vectors. 
-
-    Args:
-        height (int): height of input image
-        width (int): width of input image
-
-    Returns:
-        Model: compiled model
-    """
-
-    subnet = cnn_network(height, width)
-
-
-    image1 = kl.Input((height, width, 1))
-    image2 = kl.Input((height, width, 1))
-
-    feature1 = subnet(image1)
-    feature2 = subnet(image2)
-
-    distance = distance_layer(feature1, feature2)
-
-    # Classification
-    tensor = kl.BatchNormalization()(distance)
-    out = kl.Dense(units = 1, activation='sigmoid')(tensor)
-
-    model = Model([image1, image2], out)
-
-    opt = tf.optimizers.Adam(learning_rate=0.0001)
-
-    model.compile(loss=contrastive_loss, metrics=['accuracy'],optimizer=opt)
-
-    return model
