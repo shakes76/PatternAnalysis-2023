50cdb1e0e0bc83253fae707b78465df490b83e6e
WILL-PC\Will <wpcruick@gmail.com>
2023-10-20 02:57:13 +1000

    added context module which makes downwards steps

--- recognition/UNET-image-segnementation-William-cruickshank/modules.py
+++ recognition/UNET-image-segnementation-William-cruickshank/modules.py
@@ -1,2 +1,24 @@
 import torch
 import torch.nn as nn
+import torchvision.transforms.functional as TF
+
+
+class ContextModule(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(ContextModule, self).__init__()
+        self.conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, 3, 2, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(0.3),
+            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Dropout2d(0.3),
+        )
+
+    def forward(self, x):
+        return self.conv(x)
