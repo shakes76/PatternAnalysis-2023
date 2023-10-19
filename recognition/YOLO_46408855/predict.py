703f8b84af4ad56edff4879f48fd2e87516402bc
Charlotte <cpietsch19@gmail.com>
2023-10-20 09:12:26 +1000

    small adjustments

--- recognition/YOLO_46408855/predict.py
+++ recognition/YOLO_46408855/predict.py
@@ -17,34 +17,21 @@ def plot_boxes(image, boxes):
     ax.imshow(image.cpu().permute(1, 2, 0))
 
     if boxes is not None:
-        vector = boxes.cpu()
-        # Create a Rectangle patch
-        rect = patches.Rectangle((vector[0] - vector[2]/2, vector[1] - vector[2]/2), vector[2], vector[3], linewidth=1, edgecolor='r', facecolor='none')
-        #Get label
-        if vector[5] > vector[6]:
-            label = 'melanoma'
-        else:
-            label = 'seborrheic keratosis'
+      vector = boxes.cpu()
+      # Create a Rectangle patch
+      rect = patches.Rectangle((vector[0] - vector[2]/2, vector[1] - vector[2]/2), vector[2], vector[3], linewidth=1, edgecolor='r', facecolor='none')
+      #Get label
+      if vector[5] > vector[6]:
+        label = 'melanoma'
+      else:
+        label = 'seborrheic keratosis'
 
-        # Add the patch to the Axes
-        ax.add_patch(rect)
-        #Add labels
-        plt.text(vector[0] - vector[2]/2, vector[1] - vector[2]/2, label, bbox=dict(facecolor='red', alpha=0.5))
+      # Add the patch to the Axes
+      ax.add_patch(rect)
+      #Add labels
+      plt.text(vector[0] - vector[2]/2, vector[1] - vector[2]/2, label, bbox=dict(facecolor='red', alpha=0.5))
 
     plt.show()
-
-def filter_boxes(pred):
-  """
-  Returns boxes that have detected something
-  """
-  best_box = None
-  highest_conf = 0.8
-  for i in range(pred.size(1)):
-    box = pred[0][i]
-    if box[4] >= highest_conf:
-      best_box = box
-      highest_conf = box[4]
-  return best_box
 
 def predict(path):
     """
@@ -59,5 +46,5 @@ def predict(path):
     image = torch.reshape(image, (1,3,416,416))
     image = image.to(device)
     pred = model(image)
-    pred = filter_boxes(pred)
-    plot_boxes(image, pred)
+    box = filter_boxes(pred[0])
+    plot_boxes(image, box)
