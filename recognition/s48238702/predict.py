d5b587686cdce711bf5c3ce5f09924274768258b
Rachit Chaurasia <rachitchaurasia2701@gmail.com>
2023-10-20 03:18:32 +1000

    Added predict.py But has low accuracy, it uses euclidean distance for prediction

---
+++ recognition/s48238702/predict.py
@@ -0,0 +1,38 @@
+import torch
+from torch.utils.data import DataLoader
+from torch.nn import functional as F
+from modules import SiameseNetwork 
+from dataset import load_classify_data
+
+# Load pretrained Siamese network
+model = SiameseNetwork()
+model.load_state_dict(torch.load('SNN.pth', map_location=torch.device('cpu')))
+model.eval()
+
+# Load test data
+test_loader = load_classify_data(testing=True, batch_size=32) 
+
+threshold = 0.5  
+
+def predict(model, test_loader, threshold):
+
+    model.eval()
+    correct = 0
+    total = 0
+    
+    with torch.no_grad():
+        for img1, img2, labels in test_loader:
+            
+            output1, output2 = model(img1, img2)  
+            
+            euclidean_distance = F.pairwise_distance(output1, output2)
+            
+            predictions = (euclidean_distance < threshold).int()
+            
+            correct += (predictions == labels).sum()
+            total += len(labels)
+            
+    accuracy = correct/total
+    print(f'Accuracy: {accuracy:.2%}')
+        
+predict(model, test_loader, threshold)
