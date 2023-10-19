2755ea831223d0320c7b9b805907578fef28b587
louisljoker <142284214+louisljoker@users.noreply.github.com>
2023-10-19 20:20:19 +1000

    Update train.py

--- train.py
+++ train.py
@@ -6,7 +6,17 @@ from modules import UNet
 from torch.utils.tensorboard import SummaryWriter
 import gc
 from utils import dice_coefficient
+import matplotlib.pyplot as plt
 
+# Function to save plots
+def save_plot(data, filename, y_label):
+    plt.figure(figsize=(10,6))
+    plt.plot(data)
+    plt.title(y_label)
+    plt.ylabel(y_label)
+    plt.xlabel('Epoch')
+    plt.savefig(filename)
+    plt.close()
 
 # Hyperparameters
 batch_size = 16
@@ -19,13 +29,25 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
 transform = transforms.Compose([transforms.Resize((128, 128)), transforms.ToTensor()])
 
+
+# Set your paths
+train_image_dir = 'C:\\Users\\25060\\Desktop\\ISIC-2017_Training_Data'
+
+transform = transforms.Compose([
+    transforms.Resize((128, 128)),
+    transforms.ToTensor()
+])
+
+# Use the path for training dataset
+dataset = ISICDataset(image_dir=train_image_dir, transform=transform)
+
 # Split dataset into training and validation sets
-dataset = ISICDataset(root_dir='data', transform=transform)
+dataset = ISICDataset(image_dir='data', transform=transform)
 train_len = int((1-validation_split) * len(dataset))
 valid_len = len(dataset) - train_len
 train_dataset, valid_dataset = random_split(dataset, [train_len, valid_len])
 
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+train_loader = DataLoader(dataset, batch_size=16, shuffle=True)
 valid_loader = DataLoader(valid_dataset, batch_size=batch_size, shuffle=False)
 
 model = UNet(in_channels=3, out_channels=1).to(device)
@@ -40,12 +62,15 @@ writer = SummaryWriter()
 
 best_val_loss = float('inf')
 
-# Training loop
-model.train()
+# Lists to store losses and dice scores
+train_losses = []
+valid_losses = []
+valid_dices = []
 
+# Training loop
 for epoch in range(epochs):
     epoch_loss = 0
-    model.train()  # Ensure the model is in training mode
+    model.train()
     for batch_idx, (data, target) in enumerate(train_loader):
         data, target = data.to(device), target.to(device)
         
@@ -60,9 +85,9 @@ for epoch in range(epochs):
         epoch_loss += loss.item()
 
     # Validation loop
-    model.eval()  # Set the model to evaluation mode
+    model.eval()
     val_loss = 0
-    val_dice = 0  
+    val_dice = 0
     with torch.no_grad():
         for data, target in valid_loader:
             data, target = data.to(device), target.to(device)
@@ -70,29 +95,37 @@ for epoch in range(epochs):
             loss = criterion(output, target)
             val_loss += loss.item()
 
-    # Calculate and accumulate the Dice score
-    output = torch.sigmoid(output) 
-    predicted_mask = (output > 0.5).float()
-    dice_score = dice_coefficient(predicted_mask, target)
-    val_dice += dice_score
-    
-    # Compute average Dice score for the entire validation set
-    val_dice /= len(valid_loader) 
+            # Calculate Dice score for each batch
+            output = torch.sigmoid(output) 
+            predicted_mask = (output > 0.5).float()
+            dice_score = dice_coefficient(predicted_mask, target)
+            val_dice += dice_score
 
-    
+ # Calculate average loss and dice scores
+    train_losses.append(epoch_loss / len(train_loader))
+    valid_losses.append(val_loss / len(valid_loader))
+    valid_dices.append(val_dice / len(valid_loader))
+
     # Update learning rate
-    scheduler.step(val_loss_avg)
+    scheduler.step(val_loss/len(valid_loader))
 
-    # Log losses to TensorBoard
-    writer.add_scalar('Loss/Training', train_loss_avg, epoch)
-    writer.add_scalar('Loss/Validation', val_loss_avg, epoch)
+    # Log losses and dice score to TensorBoard
+    writer.add_scalar('Loss/Training', epoch_loss/len(train_loader), epoch)
+    writer.add_scalar('Loss/Validation', val_loss/len(valid_loader), epoch)
+    writer.add_scalar('Dice Score/Validation', val_dice/len(valid_loader), epoch)
 
     # Print epoch statistics
-    print(f"Epoch {epoch+1}/{epochs}, Training Loss: {epoch_loss/len(train_loader)}, Validation Loss: {val_loss/len(valid_loader)}, Validation Dice: {val_dice}") 
+    print(f"Epoch {epoch+1}/{epochs}, Training Loss: {epoch_loss/len(train_loader)}, Validation Loss: {val_loss/len(valid_loader)}, Validation Dice: {val_dice/len(valid_loader)}") 
+
     # Save the model with the best validation loss
-    if val_loss_avg < best_val_loss:
-        best_val_loss = val_loss_avg
+    if (val_loss/len(valid_loader)) < best_val_loss:
+        best_val_loss = val_loss/len(valid_loader)
         torch.save(model.state_dict(), 'best_model.pth')
+
+    # Save plots
+    save_plot(train_losses, 'Traning_Loss.png', 'Training Loss')
+    save_plot(valid_losses, 'Validation_Loss.png', 'Validation Loss')
+    save_plot(valid_dices, 'Validation_Dice.png', 'Validation Dice Score')
 
     # Optional: Garbage collection
     gc.collect()
