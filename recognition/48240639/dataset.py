d99d8cab9ba428d53c9062c0fc6a38ca5b4bdc50
Aniket Gupta <aniketgupta1707@gmail.com>
2023-10-20 03:34:12 +1000

    Added Relevant comments to the dataset.py

--- recognition/48240639/dataset.py
+++ recognition/48240639/dataset.py
@@ -11,60 +11,55 @@ functions to get train and test datasets from a specified data path.
 
 """
 
-
-import os
-import torch
-from PIL import Image
-from torch.utils.data import Dataset
 from torchvision import transforms
+import os
+from PIL import Image
+import torch
+from torch.utils.data import Dataset
 
 
+# Define a custom dataset class for SiameseADNIDataset
 class SiameseADNIDataset(Dataset):
-    def __init__(self, data_path):
+    def __init__(self, data_path, dataset_type):
         super(SiameseADNIDataset, self).__init__()
-
+    # Define a transformation to convert images to tensors
         self.transform = transforms.ToTensor()
-
-        # Load AD and NC images
-        self.ad_path = os.path.join(data_path, 'AD')
-        self.nc_path = os.path.join(data_path, 'NC')
-
-        # Load images
-        self.ad_images = [self.transform(Image.open(os.path.join(self.ad_path, img))) for img in
-                          os.listdir(self.ad_path)]
-        self.nc_images = [self.transform(Image.open(os.path.join(self.nc_path, img))) for img in
-                          os.listdir(self.nc_path)]
-
-        # Stack images into tensors
+    # Create paths to AD and NC image directories
+        ad_directory = os.path.join(data_path, dataset_type, 'AD')
+        nc_directory = os.path.join(data_path, dataset_type, 'NC')
+    # Load and transform AD images
+        self.ad_images = [self.load_and_transform_image(os.path.join(ad_directory, img)) for img in os.listdir(ad_directory)]
+    # Load and transform NC images
+        self.nc_images = [self.load_and_transform_image(os.path.join(nc_directory, img)) for img in os.listdir(nc_directory)]
+    # Stack AD and NC images into tensors
         self.ad_images = torch.stack(self.ad_images)
         self.nc_images = torch.stack(self.nc_images)
-
+    # Function to load and transform an image
+    def load_and_transform_image(self, img_path):
+        img = Image.open(img_path)
+        return self.transform(img)
+    # Function to get the length of the dataset
     def __len__(self):
-        # Return the length of the smaller dataset
         return min(len(self.ad_images), len(self.nc_images))
-
+    # Function to get items (image pairs and labels) from the dataset
     def __getitem__(self, index):
+        img1 = self.ad_images[index % len(self.ad_images)]
         if index % 2 == 0:
-            # Positive example (both images are AD)
-            img1 = self.ad_images[index % len(self.ad_images)] # Get the image at the current index
-            img2 = self.ad_images[(index + 1) % len(self.ad_images)] # Get the next image
-            label = torch.tensor(1, dtype=torch.float) # Set the label to 1
+            img2 = self.ad_images[(index + 1) % len(self.ad_images)]
+            label = torch.tensor(1, dtype=torch.float)
         else:
-            # Negative example (one image is AD, the other is NC)
-            img1 = self.ad_images[index % len(self.ad_images)] # Get the image of ad at the current index
-            img2 = self.nc_images[index % len(self.nc_images)] # Get the image of nc at the current index
-            label = torch.tensor(0, dtype=torch.float) # Set the label to 0
+            img2 = self.nc_images[index % len(self.nc_images)]
+            label = torch.tensor(0, dtype=torch.float)
 
         return img1, img2, label
 
-
+# Function to create a dataset
+def create_dataset(data_path, dataset_type):
+    dataset = SiameseADNIDataset(data_path, dataset_type)
+    return dataset
+# Function to get the training dataset
 def get_training(data_path):
-    # Get the training dataset
-    train_dataset = SiameseADNIDataset(os.path.join(data_path, 'train'))
-    return train_dataset
-
-
+    return create_dataset(data_path, 'train')
+# Function to get the testing dataset
 def get_testing(data_path):
-    # Get the test dataset
-    test_dataset = SiameseADNIDataset(os.path.join(data_path, 'test'))
-    return test_dataset
+    return create_dataset(data_path, 'test')
