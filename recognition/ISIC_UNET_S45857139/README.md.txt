48ed830dda5a1ce0cbf3d9e1adda6c1e06141fbd
lombo9 <lombospoon@gmail.com>
2023-10-20 02:25:19 +1000

    Added documentation to the dataset.py script.

--- recognition/ISIC_UNET_S45857139/README.md.txt
+++ recognition/ISIC_UNET_S45857139/README.md.txt
@@ -3,21 +3,23 @@
 
 A typical UNET algorithm is characterised by an encoding path that captures the context of an input image, and a decoding path that restores the spatial details, with skip 
 connections between the two paths to improve performance. My implementation of the improved UNET (inspired by https://arxiv.org/abs/1802.10508v1) makes use of residual blocks 
-instead of standard convolutions, and a localization module for decoding, which consists of a combination of a 3x3 and a 1x1 convolution. It also uses a dropout, and a leaky 
-ReLU activation rather than a standard ReLU.
+instead of standard convolutions.The residual block consists of two convolution layers followed by instance normalization. The output is then added to the residual to form the final output.
+The improved UNet also features a localization module for the decoding path, which consists of a 3x3 convolution followed by a 1x1 convolution which reduces the number of channels by half.
+
+
 
 The task was to segment the ISIC dataset with the improved UNet, with all labels having a minimum dice similarity coefficient of 0.8 on the test set. The dataset consists of 
-skin lesions as images, and then segmented mask images. The 
+skin lesions as images, and corresponding segmented mask images. The algorithm will learn to accurately predict the segmented mask for an unseen skin lesion image.
 
+In the dataset, the training set size is set to 80% of the dataset, whereas the validation set is set to the remaining 20% of the dataset. There is no seperate test set. Having
+a larger training set size is beneficial because it results in better model generalization and reduces overfitting.
 
+The algorithm was trained on the rangpur HPC at UQ for a total of 10 epochs. Here are the results.
 
+Dependencies/Libraries:
+	- PyTorch
+	- Python
+	- PIL
+	- os
 
-
-
-- Dependencies required
-	- versions
-	- reproducibiltiy of results
-
-- Example outputs 
-- Plots
-
+	
--- recognition/ISIC_UNET_S45857139/dataset.py
+++ recognition/ISIC_UNET_S45857139/dataset.py
@@ -1,45 +1,70 @@
 import os
-import torch
 from torch.utils.data import Dataset, DataLoader
 from torchvision import transforms
 from PIL import Image
-import matplotlib.pyplot as plt
-
 
 class ISICDataset(Dataset):
-    def __init__(self, root_dir, transform=None):
-        self.root_dir = root_dir
+    """
+    Custom dataset for loading ISIC skin lesion images and their corresponding masks.
+
+    Args:
+        image_dir (str): Path to the directory with image files.
+        mask_dir (str): Path to the directory with mask files.
+        transform (callable, optional): Optional transform to be applied on both image and mask. Defaults to None.
+    """
+    def __init__(self, image_dir, mask_dir, transform=None):
+        self.image_dir = image_dir
+        self.mask_dir = mask_dir
         self.transform = transform
-        self.image_files = [f for f in os.listdir(root_dir) if 'superpixels' not in f]
+        # Exclude files with '_segmentation.png' in their names from image files list
+        self.image_files = [f for f in os.listdir(image_dir) if '_segmentation.png' not in f]
         
     def __len__(self):
+        """Returns the number of items in the dataset."""
         return len(self.image_files)
         
     def __getitem__(self, idx):
-        img_name = os.path.join(self.root_dir, self.image_files[idx])
-        mask_name = os.path.join(self.root_dir, self.image_files[idx].replace('.jpg', '_superpixels.png'))
+        """
+        Fetches the image and mask at the specified index.
+        
+        Args:
+            idx (int): Index of the item to retrieve.
+
+        Returns:
+            tuple: (image, mask) pair.
+        """
+        img_name = os.path.join(self.image_dir, self.image_files[idx])
+        mask_name = os.path.join(self.mask_dir, self.image_files[idx].replace('.jpg', '_segmentation.png'))
         
         image = Image.open(img_name)
-        mask = Image.open(mask_name).convert('L')  
+        mask = Image.open(mask_name)  
 
         if self.transform:
             image = self.transform(image)
             mask = self.transform(mask)
+        
         return image, mask
 
+def get_isic_dataloader(image_dir, mask_dir, batch_size=32, shuffle=True, num_workers=4):
+    """
+    Helper function to create a DataLoader for the ISIC dataset.
 
-def get_isic_dataloader(root_dir, batch_size=32, shuffle=True, num_workers=4):
+    Args:
+        image_dir (str): Path to the directory with image files.
+        mask_dir (str): Path to the directory with mask files.
+        batch_size (int, optional): Number of samples per batch. Defaults to 32.
+        shuffle (bool, optional): Whether to shuffle the dataset. Defaults to True.
+        num_workers (int, optional): Number of subprocesses to use for data loading. Defaults to 4.
+
+    Returns:
+        DataLoader: DataLoader object for the ISIC dataset.
+    """
     transform = transforms.Compose([
         transforms.Resize((128, 128)),
         transforms.ToTensor()
     ])
     
-    dataset = ISICDataset(root_dir=root_dir, transform=transform)
+    dataset = ISICDataset(image_dir=image_dir, mask_dir=mask_dir, transform=transform)
     dataloader = DataLoader(dataset, batch_size=batch_size, shuffle=shuffle, num_workers=num_workers)
-    return dataloader
-
-
-#if __name__ == '__main__':
-    #root_dir = r'C:\Users\lombo\Desktop\3710_report\ISIC-2017_Training_Data\ISIC-2017_Training_Data'
-    #dataloader = get_isic_dataloader(root_dir)
     
+    return dataloader
