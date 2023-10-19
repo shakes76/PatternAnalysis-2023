39df8a6fd6fa719175463d2f02e84b68371254bb
Wolf-Syndrome <wolfhunter7521@gmail.com>
2023-10-19 23:11:50 +1000

    Improved model to function

--- recognition/Mask RCNN s4698053/modules.py
+++ recognition/Mask RCNN s4698053/modules.py
@@ -1,6 +1,11 @@
 from torchvision.models.detection import maskrcnn_resnet50_fpn_v2, MaskRCNN_ResNet50_FPN_V2_Weights
+import torch.nn as nn
+from math import log
 
 # https://pytorch.org/vision/main/models/generated/torchvision.models.detection.maskrcnn_resnet50_fpn_v2.html
 # Mask RCNN with a ResNet-50-FPN backbone
-model = maskrcnn_resnet50_fpn_v2(weights=MaskRCNN_ResNet50_FPN_V2_Weights.DEFAULT,
-                                 num_classes=3)
+
+def getModel(num_classes=3) -> maskrcnn_resnet50_fpn_v2:
+    model = maskrcnn_resnet50_fpn_v2(num_classes=num_classes)
+
+    return model
