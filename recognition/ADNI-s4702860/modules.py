32b53553bd1d657fc0c81ed5dbd5d1b1424f4fe2
lambdarm <141205527+lambdarm@users.noreply.github.com>
2023-10-20 09:34:22 +1000

    updated modules.py
    Updated commenting to modules.py

--- recognition/ADNI-s4702860/modules.py
+++ recognition/ADNI-s4702860/modules.py
@@ -3,12 +3,28 @@ from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, Flatten, Dense
 from tensorflow.keras.models import Model
 import tensorflow_addons as tfa
 
-class SiameseModel:
-    def __init__(self):
-        self.model = self.create_network()
+"""
+Class to handle both the siamese model and the classifier. 
 
+Architecture (input linking) was influenced by the video 
+below for a 2 input siamese neural network
+https://youtu.be/DGJyh5dK4hU?si=2LxFEx1aOVHJEFBu
+"""
+class Model():
+    """
+    Initialises the Model class
+    """
+    def __init__(self):
+        pass
+
+    """
+    Creates the base model architecture that will be used for the siamese architecture
+
+    outputs:
+        model - the base network used for the layers of the siamese model
+    """
     def base_network(self):
-        input_image = Input(shape=(256, 240, 3))
+        input_image = Input(shape=(256, 240, 3)) # know shape of images
         x = Conv2D(64, (3, 3), activation='relu')(input_image)
         x = MaxPooling2D()(x)
         x = Conv2D(128, (3, 3), activation='relu')(x)
@@ -16,32 +32,41 @@ class SiameseModel:
         x = Flatten()(x)
         x = Dense(256, activation='relu')(x)
         x = Dense(128, activation='relu')(x)
-        return Model(input_image, x)
+        model = Model(input_image, x)
+        return model
 
-    def triplet_loss(self, y_true, y_pred, alpha=0.2):
-        anchor, positive, negative = y_pred[0], y_pred[1], y_pred[2]
-        pos_dist = tf.reduce_sum(tf.square(anchor - positive), axis=-1)
-        neg_dist = tf.reduce_sum(tf.square(anchor - negative), axis=-1)
-        loss = tf.maximum(pos_dist - neg_dist + alpha, 0.0)
-        return tf.reduce_mean(loss)
+    """
+    Function that creates a siamese neural network with three inputs.
+    The model has anchor (neutral class), positive (same class as anchor), 
+    negative (opposite class as anchor), and returns the embeddings of the anchor. 
 
-    def create_network(self):
+    Outputs:
+        siamese_network = A compiled siamese network ready for training
+    """
+    def create_siamese_network(self):
+        # create the input layers
         anchor_input = Input(shape=(256, 240, 3))
         positive_input = Input(shape=(256, 240, 3))
         negative_input = Input(shape=(256, 240, 3))
 
+        # create same model for inputs to share weights
         base_model = self.base_network()
-
         anchor_embedding = base_model(anchor_input)
         positive_embedding = base_model(positive_input)
         negative_embedding = base_model(negative_input)
 
+        # define model architecture
         inputs = [anchor_input, positive_input, negative_input]
-
         siamese_network = Model(inputs=inputs, outputs=anchor_embedding)
         siamese_network.compile(optimizer='adam', loss=tfa.losses.TripletSemiHardLoss())
         return siamese_network
     
+    """
+    Function that creates a classifier based on the siamese embeddings.
+
+    Outputs:
+        classifier_model - A compiled siamese network ready for training.
+    """
     def create_classifier(self, base_model):
         anchor_embedding_input = Input(shape=(128,))  # Assuming the embedding size is 128
         x = Dense(64, activation='relu')(anchor_embedding_input)
