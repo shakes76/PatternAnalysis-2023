c080a9c10c9ec0fac5f569942322a8c7210e856d
Aniket Gupta <aniketgupta1707@gmail.com>
2023-10-20 01:00:47 +1000

    Implemented a Siamese Network model for image similarity comparison using a modified ResNet-18 backbone. The model includes custom weight initialization and feature processing layers for improved performance.

--- recognition/48240639/modules.py
+++ recognition/48240639/modules.py
@@ -11,38 +11,57 @@ grayscale images. It includes custom weight initialization and forward pass meth
 @ID: s4824063
 
 """
-
 import torch
 import torch.nn as nn
-import torchvision
+import torchvision.models as models
 
-
-class SiameseNetwork(nn.Module):
+class SiameseNN(nn.Module):
     def __init__(self):
-        super(SiameseNetwork, self).__init__()
-        # get resnet model
-        self.resnet = torchvision.models.resnet18(weights=None)
+        super(SiameseNN, self).__init()
+        self.backbone = models.resnet18(pretrained=False)
+        self.backbone.conv1 = nn.Conv2d(1, 64, kernel_size=7, stride=2, padding=3, bias=False)
+        self.fc_in_features = self.backbone.fc.in_features
+        self.backbone = torch.nn.Sequential(*(list(self.backbone.children())[:-1]))
 
-        # over-write the first conv layer to be able to read ADNI images
-        # as resnet18 reads (3,x,x) where 3 is RGB channels
-        # whereas ADNI has (1,x,x) where 1 is a gray-scale channel
-        self.resnet.conv1 = nn.Conv2d(1, 64, kernel_size=(7, 7), stride=(2, 2), padding=(3, 3), bias=False)
-        self.fc_in_features = self.resnet.fc.in_features
-
-        # remove the last layer of resnet18 (linear layer which is before avgpool layer)
-        self.resnet = torch.nn.Sequential(*(list(self.resnet.children())[:-1]))
-
-        # add linear layers to compare between the features of the two images
-        self.fc = nn.Sequential(
-            nn.Linear(self.fc_in_features * 2, 256),
+        # Additional hidden layers for further feature processing
+        self.hidden_layers = nn.Sequential(
+            nn.Linear(self.fc_in_features, 128),
             nn.ReLU(inplace=True),
-            nn.Linear(256, 1),
+            nn.Linear(128, 64),
+            nn.ReLU(inplace=True)
         )
 
-        self.sigmoid = nn.Sigmoid()
+        self.fc_layers = nn.Sequential(
+            nn.Linear(64 * 2, 32),
+            nn.ReLU(inplace=True),
+            nn.Linear(32, 1)
+        )
+        
+        self.sigmoid_activation = nn.Sigmoid()
+        self.backbone.apply(self.initialize_weights)
+        self.hidden_layers.apply(self.initialize_weights)
+        self.fc_layers.apply(self.initialize_weights)
 
-        # initialize the weights
-        self.resnet.apply(self.init_weights)
-        self.fc.apply(self.init_weights)
+    def initialize_weights(self, m):
+        if isinstance(m, nn.Linear):
+            torch.nn.init.xavier_uniform_(m.weight)
+            m.bias.data.fill_(0.01)
 
-  
+    def forward_one(self, x):
+        features = self.backbone(x)
+        flattened_features = features.view(features.size()[0], -1)
+        return flattened_features
+
+    def forward(self, input1, input2):
+        features1 = self.forward_one(input1)
+        features2 = self.forward_one(input2)
+
+        # Additional feature processing
+        features1 = self.hidden_layers(features1)
+        features2 = self.hidden_layers(features2)
+        
+        concatenated_features = torch.cat((features1, features2), 1)
+        final_output = self.fc_layers(concatenated_features)
+        final_output = self.sigmoid_activation(final_output)
+        return final_output
+
