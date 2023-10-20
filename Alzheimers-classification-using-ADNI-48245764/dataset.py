74f61f90fb2cc7b4885d6ec9ab04dc72a195352b
JivalX <kjival@gmail.com>
2023-10-20 10:42:41 +1000

    updated dataset.py  with partition and plot functions

--- Alzheimers-classification-using-ADNI-48245764/dataset.py
+++ Alzheimers-classification-using-ADNI-48245764/dataset.py
@@ -11,7 +11,7 @@ EPOCHS=50
 
 
 dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    'Alzheimers-classification-using-ADNI-48245764/AD_NC',
+    'AD_NC/train',
     seed=123,
     shuffle=True,
     image_size=(IMAGE_SIZE,IMAGE_SIZE),
@@ -23,3 +23,79 @@ class_names = dataset.class_names
 class_names
 
 
+len(dataset)
+
+
+for image_batch, labels_batch in dataset.take(1):
+    print(image_batch.shape)
+    print(labels_batch.numpy())
+    
+    
+
+plt.figure(figsize=(10, 10))
+for image_batch, labels_batch in dataset.take(1):
+    for i in range(12):
+        ax = plt.subplot(3, 4, i + 1)
+        plt.imshow(image_batch[i].numpy().astype("uint8"))
+        plt.title(class_names[labels_batch[i]])
+        plt.axis("off")
+        
+        
+
+train_size = 0.8
+len(dataset)*train_size
+
+
+train_ds = dataset.take(54)
+len(train_ds)
+
+
+test_ds = dataset.skip(54)
+len(test_ds)
+
+
+val_size=0.1
+len(dataset)*val_size
+
+
+val_ds = test_ds.take(6)
+len(val_ds)
+
+
+test_ds = test_ds.skip(6)
+len(test_ds)
+
+
+def get_dataset_partitions_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+    assert (train_split + test_split + val_split) == 1
+    
+    ds_size = len(ds)
+    
+    if shuffle:
+        ds = ds.shuffle(shuffle_size, seed=12)
+    
+    train_size = int(train_split * ds_size)
+    val_size = int(val_split * ds_size)
+    
+    train_ds = ds.take(train_size)    
+    val_ds = ds.skip(train_size).take(val_size)
+    test_ds = ds.skip(train_size).skip(val_size)
+    
+    return train_ds, val_ds, test_ds
+
+
+train_ds, val_ds, test_ds = get_dataset_partitions_tf(dataset)
+
+
+len(train_ds)
+
+
+len(val_ds)
+
+
+len(test_ds)
+
+
+train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+test_ds = test_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
