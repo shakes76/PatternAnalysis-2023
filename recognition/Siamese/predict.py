974fbc73a462f902168e5ddfee041b7504710779
Loksin <loksinlegis@gmail.com>
2023-10-20 13:14:04 +1000

    Added viualisations of training and testing

--- recognition/Siamese/predict.py
+++ recognition/Siamese/predict.py
@@ -11,13 +11,27 @@ import random
 from PIL import Image
 import module as md
 import train as tr
+import dataset as ds
 
 if __name__ == '__main__':
+
+    loader = DataLoader(ds.siamese_train,
+                        shuffle=True,
+                        batch_size=8)
+    
+    image1, image2, label = next(iter(loader))
+    concatenated = torch.cat((image1, image2),0)
+
+    img = torchvision.utils.make_grid(concatenated).numpy()
+    plt.imshow(np.transpose(img, (1, 2, 0)))
+    plt.show()
+    print(label.numpy().reshape(-1))
+
     #device configuration
     device = torch.device("cuda")
 
     #Define parameters for model
-    layer = "VGG16"
+    layer = "VGG13"
     in_channels = 1
     classes = 2
     epochs = 5
@@ -26,6 +40,13 @@ if __name__ == '__main__':
     model = md.Siamese(layers=layer, in_channels=in_channels, classes=classes).to(device)
     optimizer = torch.optim.Adam(model.parameters(), lr = learning_rate)
 
-    tr.model_train(model, optimizer, epochs)
+    loss = []
+    counter = []
+
+    tr.model_train(model, optimizer, epochs, loss, counter)
+    print(loss)
+    print(counter)
+    plt.plot(counter, loss)
+    plt.show()
     tr.model_test(model)
 
