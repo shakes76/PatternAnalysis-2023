7d40b27616a13de0ed97885a617e2b99c65de96e
Loksin <loksinlegis@gmail.com>
2023-10-20 13:15:24 +1000

    did some image contrast to try improve performance of model

--- recognition/Siamese/dataset.py
+++ recognition/Siamese/dataset.py
@@ -47,20 +47,20 @@ class Siamese_dataset(Dataset):
         img0_Image = self.transform(img0_Image)
         img1_Image = self.transform(img1_Image)
 
-        img0_Image = F.adjust_contrast(img0_Image, 4)
-        img1_Image = F.adjust_contrast(img1_Image, 4)
+        img0_Image = F.adjust_contrast(img0_Image, 3)
+        img1_Image = F.adjust_contrast(img1_Image, 3)
 
         return img0_Image, img1_Image, torch.tensor(same_class, dtype=torch.float)
     
 train_path = "C:\\Users\\Asus\\Desktop\\AD_NC\\train"
 test_path = "C:\\Users\\Asus\\Desktop\\AD_NC\\test"
-train_size = 10
-test_size = 6
+train_size = 20
+test_size = 14
 
 training_dataset = datasets.ImageFolder(root=train_path)
 testing_dataset = datasets.ImageFolder(root=test_path)
 
-transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (1.0,))])
+transform = transforms.Compose([transforms.ToTensor(), transforms.CenterCrop(150), transforms.Normalize([0.5], [0.5])])
 siamese_train = Siamese_dataset(imageFolder=training_dataset, transform=transform)
 siamese_test = Siamese_dataset(imageFolder=testing_dataset, transform=transform)
 
