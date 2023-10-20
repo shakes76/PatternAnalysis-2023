30064957cbd616a3331843bfefd37d74200f2d57
Selma <selma.gudmundsen@gmail.com>
2023-10-20 10:41:02 +1000

    removed unused module class

--- recognition/ImprovedUNet-48207351/modules.py
+++ recognition/ImprovedUNet-48207351/modules.py
@@ -4,21 +4,6 @@ import torch.nn as nn
 import torch.nn.functional as F
 import torchvision.transforms.functional as TF
 import numpy as np
-
-class DoubleConv(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(DoubleConv, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, 3, 1, 1, bias=False),
-            nn.BatchNorm2d(out_channels),
-            nn.ReLU(inplace=True),
-        )
-
-    def forward(self, x):
-        return self.conv(x)
     
 class ContextModule(nn.Module):
     def __init__(self, in_channels, out_channels, pdrop=0.3):
