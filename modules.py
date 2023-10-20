30ff61a49cd80a7f5f164e33ddeb1b5cb7e06724
sainty0 <saintyrod@gmail.com>
2023-10-20 12:22:16 +1000

    Created block class in modules

---
+++ modules.py
@@ -0,0 +1,16 @@
+import torch
+import torch.nn as nn
+
+class Block(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(Block, self).__init__()
+        self.conv1 = nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1)
+        self.activation = nn.SiLU()
+
+    def forward(self, x):
+        x = self.conv1(x)
+        x = self.activation(x)
+        x = self.conv2(x)
+        x = self.activation(x)
+        return x
