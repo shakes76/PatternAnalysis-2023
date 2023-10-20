57ea860026371490924a3c58800a0179c0143b2b
Tord Gunnarsli <tordsg@stud.ntnu.no>
2023-10-20 21:02:19 +1000

    Splitted the data into training and test

--- recognition/Tord_Improved_UNet/dataset.py
+++ recognition/Tord_Improved_UNet/dataset.py
@@ -3,42 +3,43 @@ import torch
 from torchvision import transforms
 from torchvision.datasets.vision import VisionDataset
 from PIL import Image
-import torch.nn as nn
 
 
 #dataset
 class TrainingDataset(VisionDataset):
-    def __init__(self, root, data_transform=None, target_transform=None):
+    def __init__(self, root, data_transform=None, target_transform=None, test=False):
         super(TrainingDataset, self).__init__(root, transforms=None, transform=data_transform, target_transform=target_transform)
         self.data_transform = data_transform
         self.target_transform = target_transform
         self.images = []
         self.masks = []
-        
-        
-        
-        if(os.path.exists('~/Patternanalysis-2023/recognition/Tord_Improved_UNet/tensorsets/images.pt')):
-            self.images = torch.load('~/Patternanalysis-2023/recognition/Tord_Improved_UNet/tensorsets/images.pt')
-            self.masks = torch.load('~/Patternanalysis-2023/recognition/Tord_Improved_UNet/tensorsets/masks.pt')
+
+        if(os.path.exists(os.path.join(root, 'tensorsets/image.pt'))):
+            if(test):
+                self.images = torch.load(os.path.join(root, 'tensorsets/test_images.pt'))
+                self.masks = torch.load(os.path.join(root, 'tensorsets/test_masks.pt'))
+                return
+            self.images = torch.load(os.path.join(root, 'tensorsets/images.pt'))
+            self.masks = torch.load(os.path.join(root, 'tensorsets/masks.pt'))
             return
-        
+    
         data_folder = os.path.join(root, 'ISIC2018_Task1-2_Training_Input_x2')
         mask_folder = os.path.join(root, 'ISIC2018_Task1_Training_GroundTruth_x2')
-        i = 0
-        for image_name in os.listdir(data_folder):
-            i+=1
-            mask_name = image_name.replace('.jpg', '_segmentation.png')  # Assumes naming conventions for masks
-            image = Image.open(os.path.join(data_folder, image_name))
-            
-            image2 = transforms.ToTensor()(image)
-            
-            mask = Image.open(os.path.join(mask_folder, mask_name))
+        
+        for i in range(500):
+            image_name = "ISIC_{:07d}.jpg".format(i)
+            mask_name = image_name.replace('.jpg', '_segmentation.png')
+            try:
+                image = Image.open(os.path.join(data_folder, image_name))
+                mask = Image.open(os.path.join(mask_folder, mask_name))
+            except:
+                continue
             image = self.data_transform(image)
             mask = self.target_transform(mask)
             self.images.append(image)
             self.masks.append(mask)
-        torch.save(self.images, '~/Patternanalysis-2023/recognition/Tord_Improved_UNet/tensorsets/images.pt')
-        torch.save(self.masks, '~/Patternanalysis-2023/recognition/Tord_Improved_UNet/tensorsets/masks.pt')
+        torch.save(self.images, 'recognition/Tord_Improved_UNet/tensorsets/test_images.pt')
+        torch.save(self.masks, 'recognition/Tord_Improved_UNet/tensorsets/test_masks.pt')
             
     def __len__(self):
         return len(self.images)
@@ -49,30 +50,41 @@ class TrainingDataset(VisionDataset):
 
         return image_path, mask_path
 
+class CustomNormalize(object):
+    def __init__(self, mean, std):
+        self.mean = mean
+        self.std = std
+
+    def __call__(self, tensor):
+        # Normalize each modality independently
+        for i in range(tensor.shape[0]):
+            tensor[i] = (tensor[i] - self.mean[i]) / self.std[i]
+        return tensor
+    
+mean = [0.7084, 0.5822, 0.5361]
+std = [0.0948, 0.1099, 0.1240]
+
 # Example usage:
 data_transform = transforms.Compose([
     transforms.Resize((128, 128)),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    CustomNormalize(mean, std)
+    
 ])
 
 target_transform = transforms.Compose([
     transforms.Resize((128, 128)),
-    transforms.ToTensor()
+    transforms.ToTensor(),
+    CustomNormalize(mean, std)
 ])
 
 @staticmethod
-def load():
-    return TrainingDataset(root='~/Patternanalysis-2023/recognition/Tord_Improved_UNet/data', data_transform=data_transform, target_transform=target_transform)
+def load_data():
+    return TrainingDataset(root='recognition/Tord_Improved_UNet/data', data_transform=data_transform, target_transform=target_transform)
 
-class DiceLoss(nn.Module):
-    def __init__(self, smooth=1.0):
-        super(DiceLoss, self).__init__()
-        self.smooth = smooth
+@staticmethod
+def load_test():
+    return TrainingDataset(root='recognition/Tord_Improved_UNet/data', data_transform=data_transform, target_transform=target_transform, test=True)
 
-    def forward(self, prediction, target):
-        intersection = (prediction * target).sum()
-        prediction_sum = prediction.sum()
-        target_sum = target.sum()
-        dice = (2.0 * intersection + self.smooth) / (prediction_sum + target_sum + self.smooth)
-        dice_loss = 1 - dice
-        return dice_loss
+    
+
