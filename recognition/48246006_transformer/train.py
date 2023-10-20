4ade2fa399b559a8f93d36169b35eebd9c53f496
saakshigupta2002 <62831255+saakshigupta2002@users.noreply.github.com>
2023-10-20 08:07:47 +0530

    Updated train.py on the basis of new module using hyperparameters

--- recognition/48246006_transformer/train.py
+++ recognition/48246006_transformer/train.py
@@ -1,21 +1,36 @@
 import tensorflow as tf
-from module import transformer_model
+from module import build_vision_transformer
 from dataset import load_and_preprocess_data
 import matplotlib.pyplot as plt
 from sklearn.metrics import confusion_matrix
 import seaborn as sns
 import numpy as np
+from parameters import *
 
 # Define hyperparameters
-input_shape = (224, 224, 3)  # Adjust image size as needed
+input_shape = (128, 128, 3)  # Adjust image size as needed
 epochs = 3
 model_save_path = 'alzheimer_model.h5'
 
 # Load and preprocess the data
-train_data_gen, test_data_gen = load_and_preprocess_data(img_height=224, img_width=224, batch_size=16)
+train_data_gen, test_data_gen = load_and_preprocess_data(img_height=128, img_width=128, batch_size=16)
 
 # Build the transformer model
-model = transformer_model(input_shape)
+# Build the transformer model
+#model = transformer_model(input_shape)
+model = build_vision_transformer(
+        INPUT_SHAPE,
+        IMAGE_SIZE,
+        PATCH_SIZE,
+        NUM_PATCHES,
+        ATTENTION_HEADS,
+        PROJECTION_DIM,
+        HIDDEN_UNITS,
+        DROPOUT_RATE,
+        TRANSFORMER_LAYERS,
+        MLP_HEAD_UNITS
+    )
+
 
 # Compile the model
 model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
