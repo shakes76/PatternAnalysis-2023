c33e73d5828081f2c059d8a9fddd91eaf2e2f7d9
RagGul <95918733+RagGul@users.noreply.github.com>
2023-10-20 22:22:22 +1000

    Implement training pipeline for a Convolutional Neural Network (CNN) model on the AD/NC brain MRI dataset. This includes data loading and preprocessing from separate AD and NC directories, creation of a sequential CNN model, implementation of the training loop with 10 epochs, and visualization of training and validation accuracy/loss curves using matplotlib. The model architecture consists of two convolutional layers with max pooling, followed by a flattening layer and two dense layers. The model is compiled with the Adam optimizer and binary cross-entropy loss. Training results demonstrate convergence with expected fluctuations in loss and accuracy over the epochs. This commit finalizes a basic CNN approach for AD/NC classification, serving as an initial step for further model enhancements and optimization.

--- recognition/s4824575_ADNI/train.py
+++ recognition/s4824575_ADNI/train.py
@@ -1,25 +1,23 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from tensorflow.keras.models import Sequential 
+from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Conv2D, MaxPooling2D, Flatten
+from sklearn.model_selection import train_test_split
 
-DATA_PATH = 'AD/'
+DATA_PATH_AD = '/Users/raghavendrasinghgulia/PatternAnalysis-2023/recognition/s4824575_ADNI/AD_NC/train/AD'
+DATA_PATH_NC = '/Users/raghavendrasinghgulia/PatternAnalysis-2023/recognition/s4824575_ADNI/AD_NC/train/NC'
 
 from tensorflow.keras.preprocessing.image import load_img, img_to_array
-
-# ...
 
 def load_data():
     data = []
     labels = []
 
-    for category in os.listdir(DATA_PATH):
-        category_path = os.path.join(DATA_PATH, category)
-        
-        for img in os.listdir(category_path):
-            img_path = os.path.join(category_path, img)
-            image = load_img(img_path, target_size=(150, 150))  # Resize the image to match the expected input size
+    for category, path in [('AD', DATA_PATH_AD), ('NC', DATA_PATH_NC)]:
+        for img in os.listdir(path):
+            img_path = os.path.join(path, img)
+            image = load_img(img_path, target_size=(150, 150))
             image = img_to_array(image)
             data.append(image)
             labels.append(0 if category == 'AD' else 1)
@@ -30,34 +28,24 @@ def load_data():
     return data, labels
 
 
- 
-
 def create_model():
+    model = Sequential()
+    model.add(Conv2D(32, 3, activation='relu', input_shape=(150, 150, 3)))
+    model.add(MaxPooling2D())
+    model.add(Conv2D(64, 3, activation='relu'))
+    model.add(MaxPooling2D())
+    model.add(Flatten())
+    model.add(Dense(128, activation='relu'))
+    model.add(Dense(1, activation='sigmoid'))
 
-  model = Sequential()
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
 
-  model.add(Conv2D(32, 3, activation='relu', input_shape=(150,150,3)))
-  model.add(MaxPooling2D())
-
-  model.add(Conv2D(64, 3, activation='relu'))
-  model.add(MaxPooling2D())  
-
-  model.add(Flatten())
-  model.add(Dense(128, activation='relu'))
-
-  model.add(Dense(1, activation='sigmoid'))
-
-  model.compile(optimizer='adam',
-                loss='binary_crossentropy',
-                metrics=['accuracy'])
-
-  return model
+    return model
 
 data, labels = load_data()
 
 model = create_model()
 
-# Training the model and plotting the results
 history = model.fit(data, labels, epochs=10, validation_split=0.2)
 
 # Plotting the results
@@ -70,7 +58,7 @@ plt.ylabel('Accuracy')
 plt.legend(loc='lower right')
 
 plt.subplot(1, 2, 2)
-plt.plot(history.history['loss'], label='Training Loss')  
+plt.plot(history.history['loss'], label='Training Loss')
 plt.plot(history.history['val_loss'], label='Validation Loss')
 plt.title('Training and Validation Loss')
 plt.xlabel('Epoch')
