777490e5d33a81fc589fa162e18f290fef7fc21c
Raul Movila <movilaandi@gmail.com>
2023-10-20 15:57:48 +1000

    added path parsing and cleaned up

--- recognition/predict.py
+++ recognition/predict.py
@@ -6,25 +6,22 @@ import tensorflow as tf
 from tensorflow.keras.utils import CustomObjectScope
 from sklearn.metrics import accuracy_score, precision_score
 from dataset import load_data
-from utils import dice_coef_predict, read_image_predict, read_mask_predict, save_results
+from utils import dice_coef, read_image_predict, read_mask_predict, save_results, parse_args
 
 # Inisalise values
-H = 256
-W = 256
 split = 0.2
-dataset_path = r"C:\Users\raulm\Desktop\Uni\Sem2.2023\Patterns\ISIC-2017_Training_Data"
-
+SCORE = []
 
 def predict_mask(model, test_x, test_y):
-    SCORE = []
+    
     for x, y in tqdm(zip(test_x, test_y), total=len(test_x)):
         # Grab the img name and set img path
         name = x.split("/")[-1]
         name = os.path.basename(name)
         save_image_path = os.path.join("results", name)
 
-        ori_x, x = read_image_predict(x)
-        ori_y, y = read_mask_predict(y)
+        image, x = read_image_predict(x)
+        mask, y = read_mask_predict(y)
 
         # Start model predicton
         y_pred = model.predict(x)[0] > 0.5
@@ -32,22 +29,24 @@ def predict_mask(model, test_x, test_y):
         y_pred = y_pred.astype(np.int32)
 
         # Save results to the img path
-        save_results(ori_x, ori_y, y_pred, save_image_path)
+        save_results(image, mask, y_pred, save_image_path)
 
         # Flatten to calculate the dice accuracy and precision
         y = y.flatten()
         y_pred = y_pred.flatten()
         acc_value = accuracy_score(y, y_pred)
         precision_value = precision_score(y, y_pred, labels=[0, 1], average="binary")
-        dice_value = dice_coef_predict(y, y_pred)
+        dice_value = dice_coef(y, y_pred)
         
         # Add the scored to the Score list
         SCORE.append([name, acc_value, precision_value, dice_value])
 
 if __name__ == "__main__":
+    args = parse_args()
+    dataset_path = args.path
 
     # Load the saved model from files
-    with CustomObjectScope({'dice_coef': dice_coef_predict}):
+    with CustomObjectScope({'dice_coef': dice_coef}):
         model = tf.keras.models.load_model("files/model.h5")
     # Load the testing data
     _, _, (test_x, test_y) = load_data(dataset_path, split)
