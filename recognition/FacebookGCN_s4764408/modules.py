4a41d2fd3a8f50128059dd4c100951f4404b975c
Joel Adhika <joeladhika@gmail.com>
2023-10-20 19:23:17 +1000

    Add layer to modules.py and add working train.py

--- recognition/FacebookGCN_s4764408/modules.py
+++ recognition/FacebookGCN_s4764408/modules.py
@@ -7,7 +7,8 @@ class GCN(torch.nn.Module):
     def __init__(self, num_features, hidden_channels, num_classes):
         super(GCN, self).__init__()
         self.conv1 = GCNConv(num_features, hidden_channels)
-        self.conv2 = GCNConv(hidden_channels, num_classes)
+        self.conv2 = GCNConv(hidden_channels, hidden_channels)
+        self.conv3 = GCNConv(hidden_channels, num_classes)
 
     def forward(self, x, edge_index):
         x = self.conv1(x, edge_index)
@@ -15,4 +16,8 @@ class GCN(torch.nn.Module):
         x = F.dropout(x, p=0.5, training=self.training)
 
         x = self.conv2(x, edge_index)
+        x = F.relu(x)
+        x = F.dropout(x, p=0.5, training=self.training)
+
+        x = self.conv3(x, edge_index)
         return F.log_softmax(x, dim=1)
