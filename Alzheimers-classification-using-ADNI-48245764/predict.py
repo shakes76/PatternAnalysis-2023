510eaa62af0537d153b65e6f9c1ffc828f35bf66
JivalX <kjival@gmail.com>
2023-10-20 10:12:36 +1000

    created predict.py file with plot functions

---
+++ Alzheimers-classification-using-ADNI-48245764/predict.py
@@ -0,0 +1,39 @@
+import numpy as np
+for images_batch, labels_batch in test_ds.take(1):
+    
+    first_image = images_batch[0].numpy().astype('uint8')
+    first_label = labels_batch[0].numpy()
+    
+    print("first image to predict")
+    plt.imshow(first_image)
+    print("actual label:",class_names[first_label])
+    
+    batch_prediction = model.predict(images_batch)
+    print("predicted label:",class_names[np.argmax(batch_prediction[0])])
+
+
+def predict(model, img):
+    img_array = tf.keras.preprocessing.image.img_to_array(images[i].numpy())
+    img_array = tf.expand_dims(img_array, 0)
+
+    predictions = model.predict(img_array)
+
+    predicted_class = class_names[np.argmax(predictions[0])]
+    confidence = round(100 * (np.max(predictions[0])), 2)
+    return predicted_class, confidence
+
+
+plt.figure(figsize=(15, 15))
+for images, labels in test_ds.take(1):
+    for i in range(9):
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        
+        predicted_class, confidence = predict(model, images[i].numpy())
+        actual_class = class_names[labels[i]] 
+        
+        plt.title(f"Actual: {actual_class},\n Predicted: {predicted_class}.\n Confidence: {confidence}%")
+        
+        plt.axis("off")
+        
+
