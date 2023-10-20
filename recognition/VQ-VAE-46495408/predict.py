24ce1ea4d6ec51c40df959b38a2fa7a2d9233ed2
MandalorianForce <yi.zhou6@uqconnect.edu.au>
2023-10-20 11:32:21 +1000

    Code cleaning

--- recognition/VQ-VAE-46495408/predict.py
+++ recognition/VQ-VAE-46495408/predict.py
@@ -9,14 +9,7 @@ vqvae_trainer = VQVAETrainer(0.03525, latent_dim=32, num_embeddings=128)
 vqvae_trainer.load_weights('recognition/VQ-VAE-46495408/checkpoint/vqvae_ckpt')
 pixel_cnn = get_pixel_cnn((64, 64), 128)
 pixel_cnn.load_weights('recognition/VQ-VAE-46495408/checkpoint/pixelcnn_ckpt')
-'''
-# Create a mini sampler model
-inputs = layers.Input(shape=pixel_cnn.input_shape[1:])
-outputs= pixel_cnn(inputs, training=False)
-categorical_layer = tfp.layers.DistributionLambda(tfp.distributions.Categorical)
-outputs = categorical_layer(outputs)
-sampler = keras.Model(inputs, outputs)
-'''
+
 # Create a prior to generate images
 batch = 10
 priors = np.zeros(shape=(batch,) + (pixel_cnn.input_shape)[1:])
