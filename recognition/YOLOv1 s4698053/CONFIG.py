7d2bc55b023fd155254fc993ae3e472ab1b91518
Wolf-Syndrome <wolfhunter7521@gmail.com>
2023-10-20 14:58:12 +1000

    Changed folder name, whoops MASKRcnn -> YOLOv1

--- recognition/YOLOv1 s4698053/CONFIG.py
+++ recognition/YOLOv1 s4698053/CONFIG.py
@@ -14,9 +14,9 @@ LOAD_MODEL_FILE = "model.pytorch"
 
 #--------------
 # Data information
-classify_file = './recognition/Mask RCNN s4698053/.tmp/ISIC-2017_Training_Part3_GroundTruth.csv'
-mask_dir = './recognition/Mask RCNN s4698053/.tmp/ISIC-2017_Training_Part1_GroundTruth/'
-photo_dir = './recognition/Mask RCNN s4698053/.tmp/ISIC-2017_Training_Data/'
+classify_file = './recognition/YOLOv1 s4698053/.tmp/ISIC-2017_Training_Part3_GroundTruth.csv'
+mask_dir = './recognition/YOLOv1 s4698053/.tmp/ISIC-2017_Training_Part1_GroundTruth/'
+photo_dir = './recognition/YOLOv1 s4698053/.tmp/ISIC-2017_Training_Data/'
 train_size = 0.8
 test_size = 0.2
 image_size = (448, 448)
