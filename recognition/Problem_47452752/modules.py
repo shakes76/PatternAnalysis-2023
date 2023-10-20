82802d563520f342d909d21247eb36d2e5cec32d
Noah Smyth <noah.smyth@hotmail.com>
2023-10-21 04:41:25 +1000

    Setup the UNet modules according to the paper

--- recognition/Problem_47452752/modules.py
+++ recognition/Problem_47452752/modules.py
@@ -1,1 +1,19 @@
-# containing the source code of the components of your model. Each component must be implementated as a class or a function
+"""
+Contains the source code of the components in my model. Each component is implemented as a class or function. 
+"""
+
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ContextModule(nn.Module):
+    pass
+
+
+class LocalizationModule(nn.Module):
+    pass
+
+
+class UNet(nn.Module):
+    pass
