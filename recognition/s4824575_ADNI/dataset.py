3c872970d4abeded1572245dcad0e3c651d6a261
RagGul <95918733+RagGul@users.noreply.github.com>
2023-10-20 21:41:51 +1000

    Implemented data loading, preprocessing, and dataset splitting for the Alzheimer's Disease and Normal Control classification task
    Description:
    - Added functionality for loading image data from the specified directory and preparing it for training and testing.
    - Resized the images to a standard size, converted them to arrays, and normalized the data.
    - Split the dataset into training and testing sets using the train_test_split function from scikit-learn.
    - Converted the data into TensorFlow datasets and applied shuffling and batching for efficient model training.
    
    File Changes:
    - dataset.py: Included data loading, preprocessing, and dataset splitting functionality for the Alzheimer's Disease and Normal Control classification task

--- recognition/s4824575_ADNI/dataset.py
+++ recognition/s4824575_ADNI/dataset.py
@@ -1,25 +1,47 @@
+import os
+import numpy as np
 import tensorflow as tf
+from sklearn.model_selection import train_test_split
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
 
-# Example data loading and preprocessing logic
-# Replace this with your actual data loading and preprocessing logic
+
+
+DATA_PATH = '/Users/raghavendrasinghgulia/PatternAnalysis-2023/recognition/s4824575_ADNI/AD_NC/train'
+
 def load_data():
-    (x_train, y_train), (x_test, y_test) = tf.keras.datasets.mnist.load_data()
-    # Normalize the data and perform any necessary preprocessing
-    x_train = x_train / 255.0
-    x_test = x_test / 255.0
-    return (x_train, y_train), (x_test, y_test)
+  data = []
+  labels = []
 
-if __name__ == "__main__":
-    # Data loading
-    (x_train, y_train), (x_test, y_test) = load_data()
-    train_dataset = tf.data.Dataset.from_tensor_slices((x_train, y_train))
-    train_dataset = train_dataset.shuffle(buffer_size=1024).batch(32)
-    test_dataset = tf.data.Dataset.from_tensor_slices((x_test, y_test)).batch(32)
+  for category in os.listdir(DATA_PATH):
+    category_path = os.path.join(DATA_PATH, category)
+    
+    for img in os.listdir(category_path):
+      img_path = os.path.join(category_path, img)  
+      image = load_img(img_path, target_size=(150,150))
+      image = img_to_array(image)
+      data.append(image)
+      labels.append(0 if category == 'AD' else 1)
 
-    # Rest of your code here
-    # ...
+  data = np.array(data)
+  labels = np.array(labels)
 
-    # Example usage of the data
-    for (x, y) in train_dataset:
-        # Do something with the data batches
-        print(f"X shape: {x.shape}, Y shape: {y.shape}")
+  return data, labels
+
+if __name__ == '__main__':
+
+  # Load data
+  data, labels = load_data()
+
+  # Normalize 
+  data = data/255.
+
+  # Split into train/test
+  x_train, x_test, y_train, y_test = train_test_split(data, labels)
+
+  # Convert to TensorFlow datasets
+  train_dataset = tf.data.Dataset.from_tensor_slices((x_train,y_train)) 
+  test_dataset = tf.data.Dataset.from_tensor_slices((x_test,y_test))
+
+  # Shuffle and batch datasets
+  train_dataset = train_dataset.shuffle(buffer_size=1024).batch(32)
+  test_dataset = test_dataset.batch(32)
