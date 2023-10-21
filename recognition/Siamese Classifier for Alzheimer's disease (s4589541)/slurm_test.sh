844647e6ab8586b6626757083406f96cb1926c6d
milesgardiner <68379513+milesgardiner@users.noreply.github.com>
2023-10-21 17:45:01 +1000

    Update slurm scripts

--- recognition/Siamese Classifier for Alzheimer's disease (s4589541)/slurm_test.sh
+++ recognition/Siamese Classifier for Alzheimer's disease (s4589541)/slurm_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-time=0-00:45:00
+#SBATCH --time=0-00:45:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
 #SBATCH --gres=gpu:1
 #SBATCH --partition=vgpu
-#SBATCH --job-name="siamese"
+#SBATCH --job-name="s4589541-siamese-test"
 #SBATCH --account=s4589541
 #SBATCH --mail-user=m.gardiner@uq.net.au
 #SBATCH --mail-type=BEGIN
