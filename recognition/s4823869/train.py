b146009107485f90fd39795ab7ddb512c2136b25
YashMitttal <140570495+YashMitttal@users.noreply.github.com>
2023-10-22 03:25:18 +1000

    Updated comments in train.py

--- recognition/s4823869/train.py
+++ recognition/s4823869/train.py
@@ -29,30 +29,30 @@ torch.mps.empty_cache()
 DEVICE = 'mps' if torch.backends.mps.is_available() else 'cpu'
 
 # Constants
-Z_DIM = 512
-W_DIM = 512
-LAMBDA_GP = 10
+latent_dim = 512
+style_dim = 512
+gradient_penalty_weight = 10
 BATCH_SIZES = [256, 128, 64, 32, 16, 8]
-PROGRESSIVE_EPOCHS = [1] * (len(BATCH_SIZES)-4)
-IN_CHANNELS = 512
-CHANNELS_IMG = 3
-LR_GEN = 1e-3
-LR_CRITIC = 5e-4
-START_TRAIN_IMG_SIZE = 4
+progressive_epochs = [1] * (len(BATCH_SIZES)-5)
+input_channels = 512
+image_channels = 3
+generator_learning_rate = 1e-3
+discriminator_learning_rate = 5e-4
+initial_image_size = 4
 
-gen_losses = []  # List to store generator losses
-critic_losses = []  # List to store critic losses
+generator_losses = []  # List to store generator losses
+discriminator_losses = []  # List to store discriminator losses
 
 # Function to calculate gradient penalty
-def calculate_gradient_penalty(critic, real, fake, alpha, step, device):
+def calculate_gradient_penalty(discriminator, real_images, fake_images, interpolation_alpha, step, device):
     """
     Calculate the gradient penalty for enforcing the Lipschitz constraint.
 
     Args:
-        critic (nn.Module): The discriminator (critic) network.
-        real (torch.Tensor): Real images.
-        fake (torch.Tensor): Generated fake images.
-        alpha (float): A random value for the interpolation.
+        discriminator (nn.Module): The discriminator (discriminator) network.
+        real_images (torch.Tensor): real_images images.
+        fake_images (torch.Tensor): Generated fake_images images.
+        interpolation_alpha (float): A random value for the interpolation.
         step (int): The current step in progressive training.
         device (str): Device for computation ('mps' or 'cpu').
 
@@ -60,12 +60,12 @@ def calculate_gradient_penalty(critic, real, fake, alpha, step, device):
         torch.Tensor: The gradient penalty.
 
     """
-    batch_size, _, _, _ = real.shape
+    batch_size, _, _, _ = real_images.shape
     beta = torch.rand((batch_size, 1, 1, 1)).to(device)
-    interpolated_images = real * beta + fake.detach() * (1 - beta)
+    interpolated_images = real_images * beta + fake_images.detach() * (1 - beta)
     interpolated_images.requires_grad_(True)
 
-    mixed_scores = critic(interpolated_images, alpha, step)
+    mixed_scores = discriminator(interpolated_images, interpolation_alpha, step)
 
     gradients = torch.autograd.grad(
         inputs=interpolated_images,
@@ -79,106 +79,106 @@ def calculate_gradient_penalty(critic, real, fake, alpha, step, device):
     return gradient_penalty
 
 # Train function
-def train(critic, gen, loader, step, alpha, opt_critic, opt_gen):
+def train_discriminator_and_generator(discriminator, gen, loader, step, interpolation_alpha, opt_discriminator, opt_gen):
     """
-    Training loop for both the discriminator (critic) and the generator.
+    Training loop for both the discriminator (discriminator) and the generator.
 
     Args:
-        critic (nn.Module): The discriminator (critic) network.
+        discriminator (nn.Module): The discriminator (discriminator) network.
         gen (nn.Module): The generator network.
         loader (DataLoader): DataLoader for training data.
         step (int): The current step in progressive training.
-        alpha (float): A value for controlling the fading of layers.
-        opt_critic (optim.Optimizer): Optimizer for the critic.
+        interpolation_alpha (float): A value for controlling the fading of layers.
+        opt_discriminator (optim.Optimizer): Optimizer for the discriminator.
         opt_gen (optim.Optimizer): Optimizer for the generator.
 
     Returns:
-        float: Updated alpha value for layer fading.
+        float: Updated interpolation_alpha value for layer fading.
 
     """
-    for batch_idx, real in enumerate(loader):
-        real = real.to(DEVICE)
-        cur_batch_size = real.shape[0]
-        noise = torch.randn(cur_batch_size, Z_DIM).to(DEVICE)
-        fake = gen(noise, alpha, step)
+    for batch_idx, real_images in enumerate(loader):
+        real_images = real_images.to(DEVICE)
+        cur_batch_size = real_images.shape[0]
+        noise = torch.randn(cur_batch_size, latent_dim).to(DEVICE)
+        fake_images = gen(noise, interpolation_alpha, step)
 
-        critic_real = critic(real, alpha, step)  # Pass step and alpha to the discriminator
-        critic_fake = critic(fake.detach(), alpha, step)  # Pass step and alpha to the discriminator
-        gp = calculate_gradient_penalty(critic, real, fake, alpha, step, DEVICE)
+        discriminator_real_images = discriminator(real_images, interpolation_alpha, step)  # Pass step and interpolation_alpha to the discriminator
+        discriminator_fake_images = discriminator(fake_images.detach(), interpolation_alpha, step)  # Pass step and interpolation_alpha to the discriminator
+        gp = calculate_gradient_penalty(discriminator, real_images, fake_images, interpolation_alpha, step, DEVICE)
         # Pass step to the gradient penalty function
 
-        loss_critic = (
-                -(torch.mean(critic_real) - torch.mean(critic_fake))
-                + LAMBDA_GP * gp
-                + (0.001) * torch.mean(critic_real ** 2)
+        loss_discriminator = (
+                -(torch.mean(discriminator_real_images) - torch.mean(discriminator_fake_images))
+                + gradient_penalty_weight * gp
+                + (0.001) * torch.mean(discriminator_real_images ** 2)
         )
-        critic_losses.append(abs(loss_critic.item()))
-        critic.zero_grad()
-        loss_critic.backward()
-        opt_critic.step()
+        discriminator_losses.append(abs(loss_discriminator.item()))
+        discriminator.zero_grad()
+        loss_discriminator.backward()
+        opt_discriminator.step()
 
-        gen_fake = critic(fake, alpha, step)  # Pass step and alpha to the discriminator
-        loss_gen = -torch.mean(gen_fake)
-        gen_losses.append(abs(loss_gen.item()))
+        gen_fake_images = discriminator(fake_images, interpolation_alpha, step)  # Pass step and interpolation_alpha to the discriminator
+        loss_gen = -torch.mean(gen_fake_images)
+        generator_losses.append(abs(loss_gen.item()))
         gen.zero_grad()
         loss_gen.backward()
         opt_gen.step()
 
-        alpha += cur_batch_size / (PROGRESSIVE_EPOCHS[step] * 0.5 * len(loader.dataset))
-        alpha = min(alpha, 1)
+        interpolation_alpha += cur_batch_size / (progressive_epochs[step] * 0.5 * len(loader.dataset))
+        interpolation_alpha = min(interpolation_alpha, 1)
 
         print(
                 f"Epoch [{current_epoch_label}/{total_epochs_per_step}] Batch Sample [{batch_idx + 1}/{len(loader)}] loss_gen: {loss_gen:.4f}"
             )
 
-    return alpha
+    return interpolation_alpha
 
 # Model initialization
-gen = modules.Generator(Z_DIM, W_DIM, IN_CHANNELS, CHANNELS_IMG).to(DEVICE)
-critic = modules.Discriminator(IN_CHANNELS, CHANNELS_IMG).to(DEVICE)
+gen = modules.Generator(latent_dim, style_dim, input_channels, image_channels).to(DEVICE)
+discriminator = modules.Discriminator(input_channels, image_channels).to(DEVICE)
 
 # Optimizers
 gen_params = [{'params': [param for name, param in gen.named_parameters() if 'map' not in name]}]
 gen_params += [{'params': gen.style_mapping.parameters(), 'lr': 1e-5}]
-opt_gen = optim.Adam(gen_params, lr=LR_GEN, betas=(0.0, 0.99))
-opt_critic = optim.Adam(critic.parameters(), lr=LR_CRITIC, betas=(0.0, 0.99))
+opt_gen = optim.Adam(gen_params, lr=generator_learning_rate, betas=(0.0, 0.99))
+opt_discriminator = optim.Adam(discriminator.parameters(), lr=discriminator_learning_rate, betas=(0.0, 0.99))
 
 # Set training mode
 gen.train()
-critic.train()
+discriminator.train()
 
 # Progressive training
-step = int(np.log2(START_TRAIN_IMG_SIZE / 4))
+step = int(np.log2(initial_image_size / 4))
 epoch_losses = []
 
-for num_epochs in PROGRESSIVE_EPOCHS[step:]:
-    alpha = 1e-7
+for num_epochs in progressive_epochs[step:]:
+    interpolation_alpha = 1e-7
     loader, data = dataset.get_data_loader(4 * 2 ** step)
     current_step_label = step + 1
-    total_steps = len(PROGRESSIVE_EPOCHS)
+    total_steps = len(progressive_epochs)
     print('Present image size: ' + str(4 * 2 ** step))
     print(f'Currently Running for Batch {current_step_label}/{total_steps}')
 
     for epoch in range(num_epochs):
         current_epoch_label = epoch + 1
-        total_epochs_per_step = PROGRESSIVE_EPOCHS[step]
+        total_epochs_per_step = progressive_epochs[step]
         print("Starting next epoch\n")
-        train(critic, gen, loader, step, alpha, opt_critic, opt_gen)
+        train_discriminator_and_generator(discriminator, gen, loader, step, interpolation_alpha, opt_discriminator, opt_gen)
 
-    epoch_loss_gen = sum(gen_losses) / len(gen_losses)
+    epoch_loss_gen = sum(generator_losses) / len(generator_losses)
     epoch_losses.append(epoch_loss_gen)
 
     step += 1
-    gen_losses = []
+    generator_losses = []
 
 # Save the models
 torch.save(gen.state_dict(), 'Generator.pth')
 
-# Plot the losses of generator and critic
-def plot_losses(epoch_losses, critic_losses):
+# Plot the losses of generator and discriminator
+def plot_losses(epoch_losses, discriminator_losses):
     plt.figure(figsize=(10, 5))
     plt.plot(epoch_losses, label="Generator's Loss per Epoch", color="yellow")
-    plt.plot(critic_losses, label="Critic's Loss per Batch", color="green")
+    plt.plot(discriminator_losses, label="discriminator's Loss per Batch", color="green")
     plt.title("Loss Plot")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
@@ -186,4 +186,4 @@ def plot_losses(epoch_losses, critic_losses):
     plt.savefig(save_path)
     plt.show()
 save_path = os.path.join("output_images", "LossPlot.png")
-plot_losses(epoch_losses, critic_losses)
+plot_losses(epoch_losses, discriminator_losses)
