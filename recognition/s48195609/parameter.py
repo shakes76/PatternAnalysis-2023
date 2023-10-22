2b33110cafac2681ff6fabbe476b5c6f09d355d2
atharvagupta2003 <atharvagupta2003@gmail.com>
2023-10-22 14:38:56 +1000

    updated parameter

--- recognition/s48195609/parameter.py
+++ recognition/s48195609/parameter.py
@@ -1,29 +1,23 @@
 """
 parameters.py
 
-Hyperparameters for the visual transformer.
+Parameters for the Perceiver transformer.
 
 Author: Atharva Gupta
 Date Created: 17-10-2023
 """
 
-# Hyperparameters
-IMAGE_SIZE = 56  # Reduce image size
-PATCH_SIZE = 8
-BATCH_SIZE = 16  # Reduce batch size
-PROJECTION_DIM = 256  # Reduce projection dimension
-LEARNING_RATE = 0.001  # Adjust learning rate accordingly
-NUM_HEADS = 4  # Reduce the number of heads
-DROPOUT_RATE = 0.1  # Adjust dropout rate
-NUM_LAYERS = 4  # Reduce the number of layers
-WEIGHT_DECAY = 0.0001
-EPOCHS = 10
-MLP_HEAD_UNITS = [128, 64]  # Reduce hidden units
-LOCAL_WINDOW_SIZE = 8  # You can adjust this value as needed
-DATA_LOAD_PATH = "C:/Users/hp/Desktop/comp3710/PatternAnalysis-2023/recognition/s48195609/AD_NC"
-MODEL_SAVE_PATH = "C:/Users/hp/Desktop/comp3710/PatternAnalysis-2023/recognition/vision_transformer"
+# Data path and training parameters
+DATA_PATH = "C:/Users/hp/Desktop/comp3710/PatternAnalysis-2023/recognition/s48195609/AD_NC"
+EPOCHS = 10  # Number of training epochs
+BATCH_SIZE = 16  # Batch size for training
+LEARNING_RATE = 0.001  # Learning rate for the optimizer
 
-##### AUTOMATICALLY CALCULATED #####
-INPUT_SHAPE = (3, IMAGE_SIZE, IMAGE_SIZE)  # Channels first for PyTorch
-HIDDEN_UNITS = [PROJECTION_DIM * 2, PROJECTION_DIM]
-NUM_PATCHES = (IMAGE_SIZE // PATCH_SIZE) ** 2
+# Model parameters
+NUM_LATENTS = 32  # Number of latent units
+DIM_LATENTS = 128  # Dimension of the latent units
+NUM_CROSS_ATTENDS = 1  # Number of cross-attention layers
+DEPTH_LATENT_TRANSFORMER = 4  # Depth of the latent transformer
+
+# Model save path
+MODEL_PATH = 'PatternAnalysis-2023/perceiver_transformer'  # Path to save the trained model
