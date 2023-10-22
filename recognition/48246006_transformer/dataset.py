b4ed08d690be1dc49dc0f2be1a5e68f12ab6adda
saakshigupta2002 <62831255+saakshigupta2002@users.noreply.github.com>
2023-10-23 00:05:07 +1000

    added more descriptive comments

--- recognition/48246006_transformer/dataset.py
+++ recognition/48246006_transformer/dataset.py
@@ -2,36 +2,40 @@ import tensorflow as tf
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
 def load_and_preprocess_data(img_height, img_width, batch_size):
-    train_data_dir = '/Users/saakshigupta/Desktop/code/AD_NC/train'  # Update this path
-    test_data_dir = '/Users/saakshigupta/Desktop/code/AD_NC/test'    # Update this path
+    # Define the paths to the training and test data directories
+    train_data_dir = '/Users/saakshigupta/Desktop/code/AD_NC/train'  
+    test_data_dir = '/Users/saakshigupta/Desktop/code/AD_NC/test'    
+    num_classes = 2  # Number of classes in your dataset (e.g., 2 for binary classification)
 
-    num_classes = 2  # Number of classes
-
-    # Data preprocessing for training set
-    train_datagen = ImageDataGenerator(
-        rescale=1.0 / 255.0,
-        rotation_range=20,
-        width_shift_range=0.2,
-        height_shift_range=0.2,
-        horizontal_flip=True,
-        fill_mode='nearest'
+    # Data preprocessing for the training set
+     train_datagen = ImageDataGenerator(
+        rescale=1.0 / 255.0,  # Rescale pixel values to [0, 1]
+        rotation_range=20,  # Rotate images up to 20 degrees
+        width_shift_range=0.2,  # Shift image width by up to 20%
+        height_shift_range=0.2,  # Shift image height by up to 20%
+        horizontal_flip=True,  # Flip images horizontally
+        fill_mode='nearest'  # Fill mode for new pixels
     )
 
+    # Create a generator for the training data
     train_data_gen = train_datagen.flow_from_directory(
         train_data_dir,
         target_size=(img_height, img_width),
         batch_size=batch_size,
-        class_mode='binary',  # Change to 'categorical'
+        class_mode='binary',  # 'binary' or 'categorical'
     )
 
-    # Data preprocessing for the test set
-    test_datagen = ImageDataGenerator(rescale=1.0 / 255.0)
 
-    test_data_gen = test_datagen.flow_from_directory(
+    # Data preprocessing for the test set
+    test_datagen = ImageDataGenerator(rescale=1.0 / 255.0)  # Only rescale pixel values for the test set
+
+    # Create a generator for the test data
+     test_data_gen = test_datagen.flow_from_directory(
         test_data_dir,
         target_size=(img_height, img_width),
         batch_size=batch_size,
-        class_mode='binary',  # Change to 'categorical'
+        class_mode='binary',  
     )
 
+    # Return the data generators for training and testing
     return train_data_gen, test_data_gen
