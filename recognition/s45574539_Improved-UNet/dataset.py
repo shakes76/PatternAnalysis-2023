59a1da3c8ab66332e5e5dc94d2e0efa77c194b66
kmindc <112996381+kmindc@users.noreply.github.com>
2023-10-23 06:51:52 +1000

    Update dataset.py cleaned up and added comments to make code more readable

--- recognition/s45574539_Improved-UNet/dataset.py
+++ recognition/s45574539_Improved-UNet/dataset.py
@@ -1,11 +1,8 @@
 import tensorflow as tf
-import matplotlib.pyplot as plt
-from tensorflow.image import resize, decode_jpeg, decode_png
-from tensorflow.io import read_file
 import glob
 from sklearn.utils import shuffle
 
-# Define paths
+# Define paths to access saved dataset
 DATA_PATH = 'C:/Users/keefe/Documents/COMP3710/Task1/DATA/'
 TRAIN_PATH = '/ISIC2018_Task1-2_Training_Input_x2/'
 MASK_PATH = '/ISIC2018_Task1_Training_GroundTruth_x2/'
@@ -16,6 +13,12 @@ test_val_split_size = 389
 
 
 def pre_process(image, mask_image):
+    """
+    Function to pre-process the images and save into tensors. Resizes and normalises the given image and mask.
+    :param image: image to pre-process
+    :param mask_image: mask of the given image to pre-process
+    :return: pre-processed image and mask (as tensorflow elements)
+    """
     img = tf.io.read_file(image)
     img = tf.image.decode_jpeg(img, channels=1)
     img = tf.image.resize(img, (256, 256))
@@ -25,11 +28,15 @@ def pre_process(image, mask_image):
     mask = tf.image.decode_png(mask, channels=1)
     mask = tf.image.resize(mask, (256, 256))
     mask = mask == [0, 255]
-    
+
     return img, mask
 
 
 def load_data():
+    """
+    Function to load the dataset and split into Train, Test, Validation sets.
+    :return: Train, Test, Validation datasets.
+    """
     # Load data from Dataset
     img_paths = sorted(glob.glob(DATA_PATH + TRAIN_PATH + '/*.jpg'))
     mask_paths = sorted(glob.glob(DATA_PATH + MASK_PATH + '/*.png'))
@@ -47,7 +54,7 @@ def load_data():
     val_img = img_paths[train_split_size + test_val_split_size:]
     val_mask = mask_paths[train_split_size + test_val_split_size:]
 
-    # Load into tensorflow dataset
+    # Load the image and mask sets into tensorflow datasets
     train_data = tf.data.Dataset.from_tensor_slices((train_img, train_mask))
     test_data = tf.data.Dataset.from_tensor_slices((test_img, test_mask))
     val_data = tf.data.Dataset.from_tensor_slices((val_img, val_mask))
