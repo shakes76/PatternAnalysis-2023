597515be53cc71d8d6f390c326f6d92d695d39cc
Noah Smyth <noah.smyth@hotmail.com>
2023-10-22 13:36:43 +1000

    Implemented Encoder, changed UNet accordingly

--- recognition/Problem_47452752/modules.py
+++ recognition/Problem_47452752/modules.py
@@ -8,13 +8,32 @@ import torch.nn.functional as F
 
 
 class Encoder(nn.Module):
-    def __init__(self, in_channels, out_channels):
-        # leakyReLU, convolution, normalization
-        pass
+    """
+    Encoder that computes the activations in the context pathway. This class behaves as the 'context module' from the paper.
+    Each Encoder module is a pre-activation residual block with two 3x3x3 convolutional layers and a dropout layer (p = 0.3) in between.
+    Instance normalization and leaky ReLU is used throughout the network.
+    """
+
+    def __init__(self, in_channels):
+        # We use leakyReLU with negative slope 1e-2
+        self.relu = nn.LeakyReLU(negative_slope=1e-2, inplace=True)
+        # 3x3x3 convolutional layer that preserves the input size
+        self.conv = nn.Conv3d(
+            in_channels, in_channels, kernel_size=3, stride=1, padding=1
+        )
+        # Dropout layer with p_dropout = 0.3
+        self.dropout = nn.Dropout(p=0.3)
+        # Normalize the batch with instance normalization
+        self.norm = nn.InstanceNorm3d()
 
     def forward(self, x):
-        # element-wise summation of pre and post processed features
-        pass
+        shortcut = x
+
+        residual = self.relu(self.norm(x))
+        residual = self.dropout(residual)
+        residual = self.relu(self.norm(x))
+
+        return residual + shortcut
 
 
 class Decoder(nn.Module):
@@ -22,7 +41,7 @@ class Decoder(nn.Module):
         pass
 
     def forward(self, x, skip_connection):
-        # halve the in_features, concativate with skip features, then halve again
+        # halve the in_features, concativate with skip features
         pass
 
 
@@ -31,11 +50,11 @@ class UNet(nn.Module):
         super(UNet, self).__init__()
 
         # Context Pathway (encoders)
-        self.enc1 = Encoder(in_channels, 16)
-        self.enc2 = Encoder(32, 32)
-        self.enc3 = Encoder(64, 64)
-        self.enc4 = Encoder(128, 128)
-        self.enc5 = Encoder(256, 256)
+        self.enc1 = Encoder(in_channels)
+        self.enc2 = Encoder(32)
+        self.enc3 = Encoder(64)
+        self.enc4 = Encoder(128)
+        self.enc5 = Encoder(256)
 
         # Convolutions for downsampling
         self.down1 = nn.Conv3d(in_channels, 16, kernel_size=3, stride=1, padding=1)
@@ -45,18 +64,21 @@ class UNet(nn.Module):
         self.down5 = nn.Conv3d(128, 256, kernel_size=3, stride=2, padding=1)
 
         # Localization Pathway
-        self.up1 = Decoder(256, 64)
-        self.up2 = Decoder(64, 64)
-        self.up3 = Decoder(128, 128)
+        self.up1 = Decoder(256, 128)
+        self.up2 = Decoder(128, 64)
+        self.up3 = Decoder(64, 32)
+        self.up4 = Decoder(32, 16)
 
         # Final output layer
         self.out_conv = nn.Conv3d(16, out_channels, kernel_size=1)
 
     def forward(self, x):
         # Context Pathway
-        enc1 = self.enc1(x)
-        enc2 = self.enc2(self.down1(enc1))
-        enc3 = self.enc3(self.down2(enc2))
+        enc1 = self.enc1(self.down1(x))
+        enc2 = self.enc2(self.down2(enc1))
+        enc3 = self.enc3(self.down3(enc2))
+        enc4 = self.enc4(self.down1(enc3))
+        enc5 = self.enc5(self.down2(enc4))
 
         # Localization Pathway with skip connections (decoding)
         dec1 = self.up1(enc3, enc2)
