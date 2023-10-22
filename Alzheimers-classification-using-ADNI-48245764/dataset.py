b5dbb512f94ea70383619de1f752ff7e3252373a
JivalX <104201585+JivalX@users.noreply.github.com>
2023-10-23 09:13:39 +1000

    changed dataset.py for better local running and updated it

--- Alzheimers-classification-using-ADNI-48245764/dataset.py
+++ Alzheimers-classification-using-ADNI-48245764/dataset.py
@@ -4,11 +4,9 @@ import os
 from keras.preprocessing.image import img_to_array, load_img
 from sklearn.model_selection import train_test_split
 
-
 def load_and_preprocess_data():
     
     training_images = 'AD_NC/train'
-    
     testing_images = 'AD_NC/test'
 
     AD_train_files = os.listdir(os.path.join(training_images, 'AD'))
@@ -16,81 +14,71 @@ def load_and_preprocess_data():
     NC_train_files = os.listdir(os.path.join(training_images, 'NC'))
     NC_test_files = os.listdir(os.path.join(testing_images, 'NC'))
 
-
     training_data = [] 
 
-    for filename in AD_train_files:
+    i = 0
+    while i < len(AD_train_files):
+        filename = AD_train_files[i]
         image = load_img(os.path.join(training_images, 'AD', filename), target_size=(140, 140, 3))
         image = img_to_array(image)
-        training_data.append([image, 1])  
+        training_data.append([image, 1])
+        i += 1
 
-    for filename in NC_train_files:
+    i = 0
+    while i < len(NC_train_files):
+        filename = NC_train_files[i]
         image = load_img(os.path.join(training_images, 'NC', filename), target_size=(140, 140, 3))
         image = img_to_array(image)
-        training_data.append([image, 0])  
-
+        training_data.append([image, 0])
+        i += 1
 
     training_images = []
     training_labels = []
-    
-    
-    for _ in training_data:
-        training_images.append(_[0])  
-        training_labels.append(_[1])  
-        
-        
 
+    i = 0
+    while i < len(training_data):
+        training_images.append(training_data[i][0])
+        training_labels.append(training_data[i][1])
+        i += 1
 
+    trainset, valid, trainsetlabel, valid_label = train_test_split(training_images, training_labels, train_size=0.80, shuffle=True)
 
-train_size = 0.8
-len(dataset)*train_size
+    train_images = tf.convert_to_tensor(np.array(trainset, dtype=np.uint8))
+    train_images = tf.cast(train_images, tf.float16) / 255.0
+    train_labels = tf.convert_to_tensor(trainsetlabel)
 
-train_ds = dataset.take(54)
-len(train_ds)
+    validation_images = tf.convert_to_tensor(np.array(valid, dtype=np.uint8))
+    validation_images = tf.cast(validation_images, tf.float16) / 255.0
+    validation_labels = tf.convert_to_tensor(valid_label)
 
-test_ds = dataset.skip(54)
-len(test_ds)
+    testing_data = []
 
-val_size=0.1
-len(dataset)*val_size
+    i = 0
+    while i < len(AD_test_files):
+        filename = AD_test_files[i]
+        image = load_img(os.path.join(testing_images, 'AD', filename), target_size=(140, 140, 3))
+        image = img_to_array(image)
+        testing_data.append([image, 1])
+        i += 1
 
-val_ds = test_ds.take(6)
-len(val_ds)
+    i = 0
+    while i < len(NC_test_files):
+        filename = NC_test_files[i]
+        image = load_img(os.path.join(testing_images, 'NC', filename), target_size=(140, 140, 3))
+        image = img_to_array(image)
+        testing_data.append([image, 0])
+        i += 1
 
-test_ds = test_ds.skip(6)
-len(test_ds)
+    testing_images = []
+    testing_labels = []
+    i = 0
+    while i < len(testing_data):
+        testing_images.append(testing_data[i][0])
+        testing_labels.append(testing_data[i][1])
+        i += 1
 
+    test_images = tf.convert_to_tensor(np.array(testing_images, dtype=np.uint8))
+    test_images = tf.cast(test_images, tf.float16) / 255.0
+    test_labels = tf.convert_to_tensor(testing_labels)
 
-# def get_dataset_partitions_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
-#     assert (train_split + test_split + val_split) == 1
-    
-#     ds_size = len(ds)
-    
-#     if shuffle:
-#         ds = ds.shuffle(shuffle_size, seed=12)
-    
-#     train_size = int(train_split * ds_size)
-#     val_size = int(val_split * ds_size)
-    
-#     train_ds = ds.take(train_size)    
-#     val_ds = ds.skip(train_size).take(val_size)
-#     test_ds = ds.skip(train_size).skip(val_size)
-    
-#     return train_ds, val_ds, test_ds
-
-
-# train_ds, val_ds, test_ds = get_dataset_partitions_tf(dataset)
-
-
-# len(train_ds)
-
-
-# len(val_ds)
-
-
-# len(test_ds)
-
-
-# train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
-# val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
-# test_ds = test_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+    return train_images, test_images, validation_images, train_labels, test_labels, validation_labels
