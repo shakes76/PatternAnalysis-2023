bfec211b93a41b0bbbbce2da036a8903a68e8aaa
Tong Qiu <tongqiu@s4560219.vpn.uq.edu.au>
2023-10-23 04:19:31 +1000

    ok to run on rangpur

--- recognition/TongQiu_Siamese/utils.py
+++ recognition/TongQiu_Siamese/utils.py
@@ -4,21 +4,21 @@ from pathlib import Path
 
 class Config:
 
-    # # run on local machine
-    TRAIN_DIR = Path("/Users/tongqiu/Desktop/COMP3710_Report/AD_NC/train")
-    TEST_DIR = Path("/Users/tongqiu/Desktop/COMP3710_Report/AD_NC/test")
-    DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
-    LOG_DIR = Path("/Users/tongqiu/Desktop/COMP3710_Report/log")
-    MODEL_DIR_CONTRASTIVE = "/Users/tongqiu/Desktop/COMP3710_Report/Contrastive.pth"
-    MODEL_DIR_TRIPLET = "/Users/tongqiu/Desktop/COMP3710_Report/Triplet.pth"
+    # # # run on local machine
+    # TRAIN_DIR = Path("/Users/tongqiu/Desktop/COMP3710_Report/AD_NC/train")
+    # TEST_DIR = Path("/Users/tongqiu/Desktop/COMP3710_Report/AD_NC/test")
+    # DEVICE = "mps" if torch.backends.mps.is_available() else "cpu"
+    # LOG_DIR = Path("/Users/tongqiu/Desktop/COMP3710_Report/log")
+    # MODEL_DIR_CONTRASTIVE = "/Users/tongqiu/Desktop/COMP3710_Report/Contrastive.pth"
+    # MODEL_DIR_TRIPLET = "/Users/tongqiu/Desktop/COMP3710_Report/Triplet.pth"
 
-    # # run on Rangpur
-    # TRAIN_DIR = Path("./AD_NC/train")
-    # TEST_DIR = Path("./AD_NC/test")
-    # DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
-    # LOG_DIR_CONTRASTIVE = Path("./log")
-    # MODEL_DIR_CONTRASTIVE = "./model/Contrastive.pth"
-    # MODEL_DIR_TRIPLET = "./model/Triplet.pth"
+    # run on Rangpur
+    TRAIN_DIR = Path("./AD_NC/train")
+    TEST_DIR = Path("./AD_NC/test")
+    DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
+    LOG_DIR_CONTRASTIVE = Path("./log")
+    MODEL_DIR_CONTRASTIVE = "./model/Contrastive.pth"
+    MODEL_DIR_TRIPLET = "./model/Triplet.pth"
 
     # # colab
     # TRAIN_DIR = Path("/content/drive/MyDrive/Colab_Notebooks/COMP_3710/Siamese/AD_NC/train")
