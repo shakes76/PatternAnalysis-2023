d3d55216abf198d8b7b4b855762081dce44b692c
kmindc <112996381+kmindc@users.noreply.github.com>
2023-10-23 08:20:24 +1000

    Update train.py implemented function to plot dice similarity results from training and added comments to code

--- recognition/s45574539_Improved-UNet/train.py
+++ recognition/s45574539_Improved-UNet/train.py
@@ -1,11 +1,13 @@
 import dataset
 import modules
 import tensorflow as tf
+import matplotlib.pyplot as plt
 import tensorflow.keras.backend as be
 
 
 def dice_similarity(y_true, y_pred):
     """
+    Function to calculate the dice similarity coefficient.
     Based on code from https://notebook.community/cshallue/models/samples/outreach/blogs/segmentation_blogpost/image_segmentation
     (From section: Defining custom metrics and loss functions)
     """
@@ -17,12 +19,40 @@ def dice_similarity(y_true, y_pred):
 
 
 def train_model():
+    # Use data loader from dataset.py
     train_data, test_data, val_data = dataset.load_data()
+
+    # Use model from modules.py
     model = modules.improved_unet()
+
+    # Compile improved UNet model
     model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', dice_similarity])
     history = model.fit(train_data.batch(10), validation_data=val_data.batch(10), epochs=10)
+
+    # Plot dice similarity of the model
+    plot_dice_similarity(history)
+
+    # evaluate on test dataset
     model.evaluate(test_data.batch(10))
 
 
+def plot_dice_similarity(history):
+    """
+    Plots the dice similarity coefficient readings for the trained model.
+    :param history: history of the trained model
+    :return: image / plot of dice similarity
+    """
+    plt.figure(figsize=(12, 6))
+    plt.plot(history.history['dice_similarity'], label='Training')
+    plt.plot(history.history['val_dice_similarity'], label='Validation')
+    plt.legend()
+    plt.title('Dice Similarity Coefficient Test vs Validation')
+    plt.xlabel('# of Epochs')
+    plt.ylabel('Dice Similarity Coefficient')
+    plt.savefig('dice_similarity.png')
+    plt.show()
+
+
+# run main function
 if __name__ == "__main__":
     train_model()
