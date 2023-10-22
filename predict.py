40cc8fdc407971d7da7e96cb3054bf5a8aeb33a2
SamWolfenden <141794077+SamWolfenden@users.noreply.github.com>
2023-10-22 22:53:07 +1000

    Update predict.py
    Correctly works now using tensorflow.

--- predict.py
+++ predict.py
@@ -1,60 +1,49 @@
-import torch
-import os
-from torchvision import transforms
-from PIL import Image
-from modules import UNet  # Import your UNet class or module
+import numpy as np
+import cv2
+import tensorflow as tf
+from train import load_data, create_dir, dice_coef
 
-# Create an instance of your model (replace YourModelClass with your actual model class)
-model = UNet(3, 1)  # Instantiate the model with the same architecture as in your training script
+H = 256
+W = 256
 
-# Load the saved model state dictionary
-state_dict = torch.load(r'saved model directory here')
+def read_image(path):
+    x = cv2.imread(path, cv2.IMREAD_COLOR)
+    x = cv2.resize(x, (W, H))
+    x = x/255.0
+    x = x.astype(np.float32)
+    x = np.expand_dims(x, axis=0)
+    return x
 
-# Load the state dictionary into your model
-model.load_state_dict(state_dict)
+def read_mask(path):
+    x = cv2.imread(path, cv2.IMREAD_GRAYSCALE)
+    x = cv2.resize(x, (W, H))
+    x = x/255.0
+    x = x.astype(np.int32)
+    return x
 
-# Set the model in evaluation mode
-model.eval()
+if __name__ == "__main__":
+    # Set random seeds for reproducibility
+    np.random.seed(1)
+    tf.random.set_seed(1)
+    create_dir("Predictions")
 
-# Define the test data directory
-test_data_dir = r'testing data directory here'
+    with tf.keras.utils.CustomObjectScope({'dice_coef': dice_coef}):
+        model = tf.keras.models.load_model("files/UNet.h5")
 
-# Define the output directory for saving the predictions
-output_dir = r'location of output here'
+    dataset_path = "path to data here (folder both the images and masks are in)"
+    (train_x, train_y), (valid_x, valid_y), (test_x, test_y) = load_data(dataset_path)
 
-# Create the output directory if it doesn't exist
-if not os.path.exists(output_dir):
-    os.makedirs(output_dir)
+    dice_scores = []
 
-# Define a transformation to apply to the test images (make sure it matches your training data preprocessing)
-transform = transforms.Compose([transforms.ToTensor()])
+    for x, y in zip(test_x, test_y):
+        x = read_image(x)
+        y = read_mask(y)
 
-# List all image files in the test data directory
-test_files = [file for file in os.listdir(test_data_dir) if file.endswith('.jpg')]
+        y_pred = model.predict(x)[0] > 0.5
+        y_pred = y_pred.astype(np.int32)
 
-# Make predictions for each test image
-for idx, file in enumerate(test_files):
-    # Load the test image
-    image = Image.open(os.path.join(test_data_dir, file))
+        dice = dice_coef(y, y_pred)
+        dice_scores.append(dice)
 
-    # Apply the same transformation used during training
-    image = transform(image).unsqueeze(0)  # Add a batch dimension
-
-    # Make predictions
-    with torch.no_grad():
-        predicted_segmentation = model(image)
-
-    # Convert the predicted segmentation to a format suitable for saving (e.g., numpy array)
-    predicted_segmentation = predicted_segmentation[0, 0].cpu().numpy()  # Assuming output has a single channel
-
-    # Save the predicted segmentation to the output directory
-    output_filename = os.path.splitext(file)[0] + '_segmentation.png'
-    output_path = os.path.join(output_dir, output_filename)
-    # Save the predicted segmentation using your preferred method (e.g., PIL, OpenCV)
-    # Example using PIL:
-    Image.fromarray((predicted_segmentation * 255).astype('uint8')).save(output_path)
-
-    # Print the progress
-    #print(f"Processed {idx + 1} out of {len(test_files)} images")
-
-print("Predictions have been saved to the output directory:", output_dir)
+    mean_dice = np.mean(dice_scores)
+    print(f"Mean Dice Coefficient: {mean_dice:.2f}")
