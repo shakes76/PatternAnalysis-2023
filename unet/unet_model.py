5a15fd175b3c476686293269339a7221603b8ae1
FrostNov4 <97688822+FrostNov4@users.noreply.github.com>
2023-10-23 00:19:20 +1000

    Model updated. Add ContextModule

--- unet/unet_model.py
+++ unet/unet_model.py
@@ -3,46 +3,47 @@
 from .unet_parts import *
 
 
-class UNet(nn.Module):
-    def __init__(self, n_channels, n_classes, bilinear=False):
-        super(UNet, self).__init__()
-        self.n_channels = n_channels
-        self.n_classes = n_classes
-        self.bilinear = bilinear
 
-        self.inc = (DoubleConv(n_channels, 64))
-        self.down1 = (Down(64, 128))
-        self.down2 = (Down(128, 256))
-        self.down3 = (Down(256, 512))
-        factor = 2 if bilinear else 1
-        self.down4 = (Down(512, 1024 // factor))
-        self.up1 = (Up(1024, 512 // factor, bilinear))
-        self.up2 = (Up(512, 256 // factor, bilinear))
-        self.up3 = (Up(256, 128 // factor, bilinear))
-        self.up4 = (Up(128, 64, bilinear))
-        self.outc = (OutConv(64, n_classes))
+class ContextModule(nn.Module):
+    """
+    Context Module: Consists of two convolutional layers for feature extraction and a dropout layer
+    for regularization, aimed at capturing and preserving the context information in the features.
+    """
+    def __init__(self, in_channels, out_channels):
+        """
+        Initialize the Context Module.
 
-    def forward(self, x):
-        x1 = self.inc(x)
-        x2 = self.down1(x1)
-        x3 = self.down2(x2)
-        x4 = self.down3(x3)
-        x5 = self.down4(x4)
-        x = self.up1(x5, x4)
-        x = self.up2(x, x3)
-        x = self.up3(x, x2)
-        x = self.up4(x, x1)
-        logits = self.outc(x)
-        return logits
+        Parameters:
+        - in_channels (int): Number of input channels.
+        - out_channels (int): Number of output channels.
+        """
+        super(ContextModule, self).__init__()
+        # 2 3x3 convolution layer followed by instance normalization and leaky ReLU activation
+        self.conv1 = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+        self.conv2 = nn.Sequential(
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+        )
+        # Dropout layer to prevent overfitting
+        self.dropout = nn.Dropout2d(p=0.3)
 
-    def use_checkpointing(self):
-        self.inc = torch.utils.checkpoint(self.inc)
-        self.down1 = torch.utils.checkpoint(self.down1)
-        self.down2 = torch.utils.checkpoint(self.down2)
-        self.down3 = torch.utils.checkpoint(self.down3)
-        self.down4 = torch.utils.checkpoint(self.down4)
-        self.up1 = torch.utils.checkpoint(self.up1)
-        self.up2 = torch.utils.checkpoint(self.up2)
-        self.up3 = torch.utils.checkpoint(self.up3)
-        self.up4 = torch.utils.checkpoint(self.up4)
-        self.outc = torch.utils.checkpoint(self.outc)
+        def forward(self, x):
+            """
+            Forward pass through the context module. Input is put through 2 3x3 stride 1 convolutions with a dropout
+            layer in between
+
+            Parameters:
+            - x (Tensor): The input tensor.
+
+            Returns:
+            - Tensor: The output tensor after passing through the context module.
+            """
+            x = self.conv1(x)
+            x = self.dropout(x)
+            x = self.conv2(x)
+            return x
