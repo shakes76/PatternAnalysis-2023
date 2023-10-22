4a3350fc2cb62fd63c7350b0cbdaec024aeb5504
kaia-santosha <132866652+kaia-santosha@users.noreply.github.com>
2023-10-22 15:48:22 +1000

    modified model architecture to incorporate techniques learnt in Model Exhibition CON session
    Though my previous model had good performance, I wanted to experiment and hopefully settle on a new model architecture that better incorporates the techniques learned in the model exhibition CON session

--- recognition/multi-layer_GCN_model_s4696681/modules.py
+++ recognition/multi-layer_GCN_model_s4696681/modules.py
@@ -1,24 +1,24 @@
-
 import torch
-import torch.nn as nn
+from torch.nn import Module, Parameter
 import torch.nn.functional as F
 
-"""A single layer of a Graph Convolutional Network that aggregates information from neighboring 
-    nodes and applies a linear transformation followed by a ReLU activation."""
-class GCNLayer(nn.Module):
+'''updated GCN layer trying to incorporate techniques learnt from model expo CON session'''
+class GCNLayer(Module):
     def __init__(self, in_features, out_features):
         super(GCNLayer, self).__init__()
-        self.fc = nn.Linear(in_features, out_features)
-        
-    def forward(self, x, adj):
-        h = torch.mm(adj, x)
-        h = self.fc(h)
-        return F.relu(h)
+        self.in_features = in_features
+        self.out_features = out_features
+        self.weight = Parameter(torch.FloatTensor(in_features, out_features))
+        torch.nn.init.xavier_uniform_(self.weight) 
 
+    def forward(self, features, adj, active=True):
+        support = torch.mm(features, self.weight)
+        output = torch.spmm(adj, support)
+        if active:
+            output = F.relu(output)
+        return output
 
-
-"""A two-layer Graph Convolutional Network model that processes node features using adjacency information, applies
-    dropout for regularization, and outputs log softmax values, while also providing an option to retrieve node embeddings from the first layer."""
+'''GCN represents the entire Graph Convolutional Network consisting of two GCNLayer'''
 class GCN(nn.Module):
     def __init__(self, in_features, hidden_features, out_features):
         super(GCN, self).__init__()
@@ -26,13 +26,12 @@ class GCN(nn.Module):
         self.layer2 = GCNLayer(hidden_features, out_features)
         
     def forward(self, x, adj):
-         # Store embeddings from the first layer (used for TSNE visualisation)
-        self.embeddings = self.layer1(x, adj) 
-
+        # Use the first GNNLayer with activation function (ReLU)
+        self.embeddings = self.layer1(x, adj, active=True) 
         h = F.dropout(self.embeddings, p=0.5, training=self.training)
-        h = self.layer2(h, adj)
+        # Use the second GNNLayer without activation function
+        h = self.layer2(h, adj, active=False) 
         return F.log_softmax(h, dim=1)
     
-    #  Retrieves the node embeddings produced after the first convolutional layer. (Used for TSNE visualisation)
     def get_embeddings(self):
         return self.embeddings
