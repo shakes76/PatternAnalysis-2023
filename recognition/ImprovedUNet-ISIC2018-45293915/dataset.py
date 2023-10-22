36a0de03ff317be87fef64aeed5e7d7d5d00c1f9
riley-ball <riley-ball@outlook.com>
2023-10-22 10:42:04 +1000

    gpu working on ubuntu

--- recognition/ImprovedUNet-ISIC2018-45293915/dataset.py
+++ recognition/ImprovedUNet-ISIC2018-45293915/dataset.py
@@ -1,54 +1,47 @@
 import tensorflow as tf
 import glob
-import cv2
-import numpy as np
 
 class SegmentationDataset(tf.data.Dataset):
-    def __init__(self, imageDir, maskDir, image_size, cache=False):
-        # store the image and mask filepaths, and augmentation transforms
-        self.cache = cache
-        self.imagePaths = sorted(glob.glob(imageDir + "/*"))
-        self.maskPaths = sorted(glob.glob(maskDir + "/*"))
-        self.image_size = image_size
+    def _generator(image_paths, mask_paths, image_size):
+        for image_path, mask_path in zip(image_paths, mask_paths):
+            image = tf.io.read_file(image_path)
+            image = tf.image.decode_jpeg(image, channels=3)
+            image = tf.image.resize(image, image_size)
+            image = image / 255.0  # Normalize
 
-        # Create a dataset from the list of file paths
-        self.dataset = tf.data.Dataset.from_tensor_slices((self.imagePaths, self.maskPaths))
-        self.dataset = self.dataset.map(self._load_image_and_mask, num_parallel_calls=tf.data.experimental.AUTOTUNE)
+            mask = tf.io.read_file(mask_path)
+            mask = tf.image.decode_png(mask, channels=1)  # Assuming mask is in PNG format
+            mask = tf.image.resize(mask, image_size)
+            mask = mask / 255.0  # Normalize
 
-        if self.cache:
-            self.dataset = self.dataset.cache()
+            yield image, mask
 
-    def _load_image_and_mask(self, image_path, mask_path):
-        image = tf.io.read_file(image_path)
-        image = tf.image.decode_jpeg(image, channels=3)
-        image = tf.image.resize(image, self.image_size)
-        image = image / 255.0  # Normalize
+    def __new__(cls, image_dir, mask_dir, image_size, cache=False):
+        image_paths = sorted(glob.glob(image_dir + "/*"))
+        mask_paths = sorted(glob.glob(mask_dir + "/*"))
 
-        mask = tf.io.read_file(mask_path)
-        mask = tf.image.decode_jpeg(mask, channels=1)
-        mask = tf.image.resize(mask, self.image_size)
-        mask = mask / 255.0  # Normalize
+        # Assume that the number of images and masks are the same
+        dataset = tf.data.Dataset.from_generator(
+            cls._generator,
+            output_signature=(
+                tf.TensorSpec(shape=(image_size[0], image_size[1], 3), dtype=tf.float32),
+                tf.TensorSpec(shape=(image_size[0], image_size[1], 1), dtype=tf.float32)
+            ),
+            args=(image_paths, mask_paths, image_size)
+        )
 
-        return image, mask
+        if cache:
+            dataset = dataset.cache()
 
-    def __call__(self, batch_size=32, shuffle=False):
-        if shuffle:
-            self.dataset = self.dataset.shuffle(buffer_size=1000)
-        self.dataset = self.dataset.batch(batch_size)
-        self.dataset = self.dataset.prefetch(buffer_size=tf.data.experimental.AUTOTUNE)
-        return self.dataset
+        return dataset
 
-def get_dataloaders(batch_size=32):
-    train_dataset = SegmentationDataset("/datasets/ISIC2018_Task1-2_Training_Input",
-                                        "/datasets/ISIC2018_Task1-2_Training_Input_GroundTruth",
-                                        (572, 572), cache=True) 
+def get_dataloaders(batch_size=32, image_size=(572, 572)):
+    train_dataset = SegmentationDataset("/datasets/ISIC2018_Task1-2_Training_Input", "/datasets/ISIC2018_Task1-2_Training_Input_GroundTruth", image_size, cache=True)
+    test_dataset = SegmentationDataset("/datasets/ISIC2018_Task1-2_Test_Input", "/datasets/ISIC2018_Task1-2_Test_Input_GroundTruth", image_size, cache=True)
+    valid_dataset = SegmentationDataset("/datasets/ISIC2018_Task1-2_Validation_Input", "/datasets/ISIC2018_Task1-2_Validation_Input_GroundTruth", image_size, cache=True)
 
-    test_dataset = SegmentationDataset("/datasets/ISIC2018_Task1-2_Test_Input",
-                                       "/datasets/ISIC2018_Task1-2_Test_Input_GroundTruth",
-                                       (572, 572), cache=True)
+    train_dataloader = train_dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    test_dataloader = test_dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
+    valid_dataloader = valid_dataset.batch(batch_size).prefetch(tf.data.AUTOTUNE)
 
-    valid_dataset = SegmentationDataset("/datasets/ISIC2018_Task1-2_Training_Input",
-                                        "/datasets/ISIC2018_Task1-2_Training_Input_GroundTruth",
-                                        (572, 572), cache=True)
-
-    return train_dataset(batch_size), test_dataset(batch_size), valid_dataset(batch_size)
+    return train_dataloader, test_dataloader, valid_dataloader
--- recognition/ImprovedUNet-ISIC2018-45293915/train.py
+++ recognition/ImprovedUNet-ISIC2018-45293915/train.py
@@ -1,3 +1,17 @@
 from dataset import get_dataloaders
+import tensorflow as tf
+print("Num GPUs Available: ", len(tf.config.experimental.list_physical_devices('GPU')))
+print(tf.__version__)
 
-train_dataloader, test_dataloader, valid_dataloader = get_dataloaders()
+try:
+    train_dataloader, test_dataloader, valid_dataloader = get_dataloaders()
+
+    print("aaaaa", train_dataloader)
+    
+    if not train_dataloader:
+        print("Error: train_dataloader is empty")
+
+    # 
+
+except Exception as e:
+    print("An error occurred: ", str(e))
