968e0ebc9b45a9b2c98f3faff479db01b936c270
atharvagupta2003 <atharvagupta2003@gmail.com>
2023-10-22 14:51:56 +1000

    new testing according to new model

--- recognition/s48195609/test.py
+++ recognition/s48195609/test.py
@@ -1,53 +1,43 @@
-# test.py
+"""
+test.py
 
+Code for testing, and loading the saved model.
+
+Author: Atharva Gupta
+Date Created: 20-10-2023
+"""
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import matplotlib.pyplot as plt
-from datasplit import load_data  # Modify your data loading function import here
-from modules import build_vision_transformer
-from parameter import MODEL_SAVE_PATH, INPUT_SHAPE, IMAGE_SIZE, PATCH_SIZE, NUM_PATCHES, NUM_HEADS, PROJECTION_DIM, HIDDEN_UNITS, DROPOUT_RATE, NUM_LAYERS, MLP_HEAD_UNITS
+from modules import CustomPerceiver
+from datasplit import test_loader
+from parameter import *
 
-def predict(model, test_data):
-    model.eval()
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    model.to(device)
+# Define the class labels
+classes = ('AD', 'NC')
 
-    y_true = []
-    y_pred = []
+# Create an instance of the CustomPerceiver model with the specified parameters
+net = CustomPerceiver(NUM_LATENTS, DIM_LATENTS, DEPTH_LATENT_TRANSFORMER, NUM_CROSS_ATTENDS)
 
-    with torch.no_grad():
-        for data in test_data:
-            inputs, labels = data
-            inputs, labels = inputs.to(device), labels.to(device)
+# Load the pre-trained model weights
+net.load_state_dict(torch.load(MODEL_PATH))
 
-            outputs = model(inputs)
-            predicted = torch.round(torch.sigmoid(outputs))
-            y_true.append(labels.cpu().numpy())
-            y_pred.append(predicted.cpu().numpy())
+correct = 0
+total = 0
 
-    y_true = torch.cat(y_true).squeeze().numpy()
-    y_pred = torch.cat(y_pred).squeeze().numpy()
+# Iterate through the test data loader
+with torch.no_grad():
+    for i, data in enumerate(test_loader(DATA_PATH, 4), 0):
+        images, labels = data
+        # Calculate outputs by running images through the network
+        outputs = net(images)
+        # The class with the highest energy is chosen as the prediction
+        _, predicted = torch.max(outputs.data, 1)
+        total += labels.size(0)
+        correct += (predicted == labels).sum().item()
 
-    # Calculate confusion matrix
-    from sklearn.metrics import confusion_matrix
-    import seaborn as sns
+        if i == 50:  # Limiting the number of iterations for illustration
+            break
 
-    cm = confusion_matrix(y_true, y_pred)
-    plt.figure(figsize=(6, 5))
-    sns.heatmap(cm, annot=True, fmt="d", cmap="Blues")
-    plt.xlabel('Predicted')
-    plt.ylabel('Actual')
-    plt.title('Confusion Matrix')
-    plt.show()
-
-if __name__ == '__main__':
-    # Modify your data loading function to accommodate the test data split
-    test = load_data()  # This assumes that load_data properly splits the test set
-
-    # Create and load the model
-    model = build_vision_transformer(INPUT_SHAPE, IMAGE_SIZE, PATCH_SIZE, NUM_PATCHES, NUM_HEADS, PROJECTION_DIM, HIDDEN_UNITS, DROPOUT_RATE, NUM_LAYERS, MLP_HEAD_UNITS)
-    model.load_state_dict(torch.load(MODEL_SAVE_PATH))
-    
-    # Call the predict function to evaluate and display the confusion matrix
-    predict(model, test)
+# Calculate and print the accuracy of the network on the test images
+accuracy = 100 * correct // total
+print(f'Accuracy of the network on the test images: {accuracy} %')
