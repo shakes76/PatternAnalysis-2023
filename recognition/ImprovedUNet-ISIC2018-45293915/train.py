beb188c9d51cd9501f076884cee2ab66500e6116
riley-ball <riley-ball@outlook.com>
2023-10-22 17:03:39 +1000

    added timestr variable for saving images and modified to save to output folder instead of plotting

--- recognition/ImprovedUNet-ISIC2018-45293915/train.py
+++ recognition/ImprovedUNet-ISIC2018-45293915/train.py
@@ -5,12 +5,18 @@ import numpy as np
 import matplotlib.pyplot as plt
 from itertools import islice
 import math
+from keras.callbacks import Callback
+import time
 
 import modules as layers
 from dataset import pre_process_data
 
+# string modifier for saving output files based on time
+timestr = time.strftime("%Y%m%d-%H%M%S")
+output_dir = "output"
+
 # Constants related to training
-EPOCHS = 10
+EPOCHS = 5
 LEARNING_RATE = 0.0005
 BATCH_SIZE = 2  # set the batch_size
 IMAGE_HEIGHT = 512  # the height input images are scaled to
@@ -20,7 +26,19 @@ STEPS_PER_EPOCH_TRAIN = math.floor(2076 / BATCH_SIZE)
 STEPS_PER_EPOCH_TEST = math.floor(519 / BATCH_SIZE)
 NUMBER_SHOW_TEST_PREDICTIONS = 3
 
-# Define your dice_coefficient, dice_loss, and other functions here
+# Define a callback to calculate Dice coefficient after each epoch
+class DiceCoefficientCallback(Callback):
+    def __init__(self, test_gen, steps_per_epoch_test):
+        self.test_gen = test_gen
+        self.steps_per_epoch_test = steps_per_epoch_test
+        self.dice_coefficients = []
+
+    def on_epoch_end(self, epoch, logs=None):
+        test_loss, test_accuracy, test_dice = \
+            self.model.evaluate(self.test_gen, steps=self.steps_per_epoch_test, verbose=0, use_multiprocessing=False)
+        self.dice_coefficients.append(test_dice)
+        print(f"Epoch {epoch + 1} - Test Dice Coefficient: {test_dice:.4f}")
+
 
 # Plot the accuracy and loss curves of model training.
 def plot_accuracy_loss(track):
@@ -30,7 +48,17 @@ def plot_accuracy_loss(track):
     plt.title('Loss & Accuracy Curves')
     plt.xlabel('Epoch')
     plt.legend(['Accuracy', 'Loss'])
-    plt.show()
+    
+    # Generate a unique filename based on the current date and time
+    now = datetime.datetime.now()
+    timestr = now.strftime("%Y%m%d-%H%M%S")
+    filename = os.path.join(output_dir, f"accuracy_loss_plot_{timestr}.png")
+    
+    # Save the plot to the output folder
+    plt.savefig(filename, bbox_inches='tight', pad_inches=0.1)
+    plt.close()
+
+    print(f"Accuracy and loss plot saved as '{filename}'.")
 
 
 # Metric for how similar two sets (prediction vs truth) are.
@@ -73,10 +101,8 @@ def train_model_check_accuracy(train_gen, test_gen):
 
 # Test and visualize model predictions with a set amount of test inputs.
 def test_visualise_model_predictions(model, test_gen):
-    print(model)
-    print(test_gen)
     test_range = np.arange(0, stop=NUMBER_SHOW_TEST_PREDICTIONS, step=1)
-    figure, axes = plt.subplots(NUMBER_SHOW_TEST_PREDICTIONS, 3)
+    
     for i in test_range:
         current = next(islice(test_gen, i, None))
         image_input = current[0]  # Image tensor
@@ -87,17 +113,33 @@ def test_visualise_model_predictions(model, test_gen):
         probabilities = keras.preprocessing.image.img_to_array(test_pred)
         test_dice = dice_coefficient(truth, test_pred, axis=None)
 
-        axes[i][0].title.set_text('Input')
-        axes[i][0].imshow(original, vmin=0.0, vmax=1.0)
-        axes[i][0].set_axis_off()
-        axes[i][1].title.set_text('Output (DSC: ' + str(test_dice.numpy()) + ")")
-        axes[i][1].imshow(probabilities, cmap='gray', vmin=0.0, vmax=1.0)
-        axes[i][1].set_axis_off()
-        axes[i][2].title.set_text('Ground Truth')
-        axes[i][2].imshow(truth, cmap='gray', vmin=0.0, vmax=1.0)
-        axes[i][2].set_axis_off()
-    plt.axis('off')
-    plt.show()
+        # Create a unique filename for each visualization
+        filename = os.path.join(output_dir, f"visualization_{i}_{timestr}.png")
+
+        # Create a subplot for the visualization
+        figure, axes = plt.subplots(1, 3)
+        
+        # Plot and save the input image
+        axes[0].title.set_text('Input')
+        axes[0].imshow(original, vmin=0.0, vmax=1.0)
+        axes[0].set_axis_off()
+        
+        # Plot and save the model's output
+        axes[1].title.set_text('Output (DSC: ' + str(test_dice.numpy()) + ")")
+        axes[1].imshow(probabilities, cmap='gray', vmin=0.0, vmax=1.0)
+        axes[1].set_axis_off()
+        
+        # Plot and save the ground truth
+        axes[2].title.set_text('Ground Truth')
+        axes[2].imshow(truth, cmap='gray', vmin=0.0, vmax=1.0)
+        axes[2].set_axis_off()
+
+        # Save the visualization to the output folder
+        plt.axis('off')
+        plt.savefig(filename, bbox_inches='tight', pad_inches=0.1)
+        plt.close()
+
+    print("Visualizations saved in the 'output' folder.")
 
 
 # Run the test driver.
@@ -111,6 +153,17 @@ def main():
     model.save("my_model.keras")
     print("\nVISUALISING PREDICTIONS")
     test_visualise_model_predictions(model, test_gen)
+
+    # Initialize the callback for Dice coefficient
+    dice_coefficient_callback = DiceCoefficientCallback(test_gen, STEPS_PER_EPOCH_TEST)
+    
+    print("\nPLOTTING DICE COEFFICIENT")
+    plt.figure()
+    plt.plot(dice_coefficient_callback.dice_coefficients)
+    plt.title('Dice Coefficient')
+    plt.xlabel('Epoch')
+    plt.ylabel('Dice Coefficient')
+    plt.show()
 
     print("COMPLETED")
     return 0
