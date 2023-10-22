fb0c859d5c1ea46cb335ce01cfad95a84545e455
JivalX <104201585+JivalX@users.noreply.github.com>
2023-10-23 09:45:38 +1000

    added documenation to file

--- Alzheimers-classification-using-ADNI-48245764/predict.py
+++ Alzheimers-classification-using-ADNI-48245764/predict.py
@@ -4,10 +4,22 @@ import matplotlib.pyplot as plt
 import os
 
 def main():
+    """
+    Main function to train a model, plot accuracy and loss, and save the plots as images.
+
+    This function loads a neural network model using modules.transformer(),
+    trains the model using train.run_model(), and plots the accuracy and loss
+    during training. The resulting plots are saved as images in the same directory.
+
+    Note:
+        - The 'accuracy_plot.png' image will contain the accuracy plot.
+        - The 'loss_plot.png' image will contain the loss plot.
+    """
+
     model = modules.transformer()
     performance = train.run_model(model)
 
-    # Plot accuracy
+    # Plot Accuracy
     plt.plot(performance.performance['accuracy'])
     plt.plot(performance.performance['val_accuracy'])
     plt.title("Accuracy Plot")
@@ -17,13 +29,12 @@ def main():
     plt.savefig(accuracy_plot_path)
     plt.show()
 
-    # Plot loss
+    # Plot Loss
     plt.plot(performance.performance['loss']) 
     plt.plot(performance.performance['val_loss'])
     plt.title("Model Loss")
     plt.xlabel("Epoch")
     plt.ylabel("Loss")
-    # Save loss plot as an image
     loss_plot_path = os.path.join(os.path.dirname(__file__), "loss_plot.png")
     plt.savefig(loss_plot_path)
     plt.show()
