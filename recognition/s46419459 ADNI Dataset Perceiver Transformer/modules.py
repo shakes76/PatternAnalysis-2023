8b96d1d5abca97919505557e182180a517cf37f9
dcpais <dpaismail@gmail.com>
2023-10-22 22:54:09 +1000

    Perceiver implemented. Forward method unfinished

--- recognition/s46419459 ADNI Dataset Perceiver Transformer/modules.py
+++ recognition/s46419459 ADNI Dataset Perceiver Transformer/modules.py
@@ -26,10 +26,10 @@ class PositionalEncoding(nn.Module):
         return self.dropout(x)
 
 
-class CrossAtttention(nn.Module):
+class CrossAttention(nn.Module):
 
     def __init__(self, dim_latent, heads):
-        super(CrossAtttention, self).__init__()
+        super(CrossAttention, self).__init__()
         self.attention = nn.MultiheadAttention(dim_latent, heads)
         self.linear = nn.Linear(dim_latent, dim_latent)
 
@@ -42,7 +42,7 @@ class CrossAtttention(nn.Module):
 class PerceiverBlock(nn.Module):
 
     def __init__(self, dim_latent, heads, transformer_depth):
-        self.cross_attention = CrossAtttention(dim_latent, heads)
+        self.cross_attention = CrossAttention(dim_latent, heads)
         self.transformer = nn.Transformer(
             d_model = dim_latent, 
             nhead = heads,
@@ -50,18 +50,57 @@ class PerceiverBlock(nn.Module):
             num_decoder_layers = transformer_depth
         )
 
-    def forward(self, x):
-        out = self.cross_attention(x)
+    def forward(self, x, y):
+        out = self.cross_attention(x, y)
         out = self.transformer(out)
         return out
     
 
-class Classifer(nn.Module):
+class Classifier(nn.Module):
 
     def __init__(self, dim_latent, n_classes):
+        super(Classifier, self).__init__()
         self.output_layer = nn.Linear(dim_latent, n_classes)
 
     def forward(self, x):
         averaged = torch.mean(x, dim = 2)
         out = self.output_layer(averaged)
         return out
+    
+
+class Perceiver(nn.Module):
+    """
+    Architecture and layout of Perceiver adapted from source:
+    https://medium.com/@curttigges/the-annotated-perceiver-74752113eefb
+
+    The perceiver model constructed here has a few modifications. Additionally,
+    we use the pytorch transformer model as it 
+    """
+
+    def __init__(self, dim_latent, heads, transformer_depth, num_blocks, n_classes):
+        super(Perceiver, self).__init__()
+
+        self.pe = PositionalEncoding(240 * 240)
+
+        latent = torch.zeros((dim_latent, 1, 1))
+        torch.nn.init.trunc_normal(latent, mean = 0, std = 0.02, a = -2, b = 2)
+        self.latent = nn.Parameter(latent)
+
+        self.blocks = nn.ModuleList(
+            [PerceiverBlock(dim_latent, heads, transformer_depth) for _ in range(num_blocks)]
+        )
+        self.classifier = Classifier(dim_latent, n_classes)
+
+    def forward(self, x):
+        batch_size = x.shape[0]
+        
+        out = self.latent.reshape((-1, batch_size, -1))
+        embedded = self.pe(x)
+
+        for block in self.blocks:
+            out = block(embedded, out)
+
+        
+
+
+
