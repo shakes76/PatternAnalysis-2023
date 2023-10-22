47d97c5d292678a8992bed578999fe257cbda665
kmindc <112996381+kmindc@users.noreply.github.com>
2023-10-22 22:00:49 +1000

    Update train.py implemented train_model function to train the model and record accuracy / loss / dice similarity coefficient

--- recognition/s45574539_Improved-UNet/train.py
+++ recognition/s45574539_Improved-UNet/train.py
@@ -1,11 +1,12 @@
 import dataset
 import modules
+import tensorflow as tf
 import tensorflow.keras.backend as be
 
 
 def dice_similarity(y_true, y_pred):
     """
-    Used / based on code from https://notebook.community/cshallue/models/samples/outreach/blogs/segmentation_blogpost/image_segmentation
+    Based on code from https://notebook.community/cshallue/models/samples/outreach/blogs/segmentation_blogpost/image_segmentation
     (From section: Defining custom metrics and loss functions)
     """
     smooth = 1
@@ -13,3 +14,15 @@ def dice_similarity(y_true, y_pred):
     y_pred_f = be.flatten(y_pred)
     intersection = be.sum(y_true_f * y_pred_f)
     return (2.0 * intersection + smooth) / (be.sum(y_true_f) + be.sum(y_pred_f) + smooth)
+
+
+def train_model():
+    train_data, test_data, val_data = dataset.load_data()
+    model = modules.improved_unet()
+    model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy', dice_similarity])
+    history = model.fit(train_data.batch(10), validation_data=val_data.batch(10), epochs=10)
+    model.evaluate(test_data.batch(10))
+
+
+if __name__ == "__main__":
+    train_model()
