3bfdf1c889cbbd83ff6a0939677a061fb13aa63b
JivalX <104201585+JivalX@users.noreply.github.com>
2023-10-23 09:08:43 +1000

    Update dataset for local running instead of colab

--- Alzheimers-classification-using-ADNI-48245764/dataset.py
+++ Alzheimers-classification-using-ADNI-48245764/dataset.py
@@ -1,95 +1,96 @@
 import tensorflow as tf
-from tensorflow.keras import models, layers
-import matplotlib.pyplot as plt
-from IPython.display import HTML
+import numpy as np
+import os
+from keras.preprocessing.image import img_to_array, load_img
+from sklearn.model_selection import train_test_split
 
 
-dataset = tf.keras.preprocessing.image_dataset_from_directory(
-    'AD_NC/train',
-    seed=123,
-    shuffle=True,
-    image_size=(IMAGE_SIZE,IMAGE_SIZE),
-    batch_size=BATCH_SIZE
-)
+def load_and_preprocess_data():
+    
+    training_images = 'AD_NC/train'
+    
+    testing_images = 'AD_NC/test'
+
+    AD_train_files = os.listdir(os.path.join(training_images, 'AD'))
+    AD_test_files = os.listdir(os.path.join(testing_images, 'AD'))
+    NC_train_files = os.listdir(os.path.join(training_images, 'NC'))
+    NC_test_files = os.listdir(os.path.join(testing_images, 'NC'))
 
 
-class_names = dataset.class_names
-class_names
+    training_data = [] 
+
+    for filename in AD_train_files:
+        image = load_img(os.path.join(training_images, 'AD', filename), target_size=(140, 140, 3))
+        image = img_to_array(image)
+        training_data.append([image, 1])  
+
+    for filename in NC_train_files:
+        image = load_img(os.path.join(training_images, 'NC', filename), target_size=(140, 140, 3))
+        image = img_to_array(image)
+        training_data.append([image, 0])  
 
 
-len(dataset)
-
-
-for image_batch, labels_batch in dataset.take(1):
-    print(image_batch.shape)
-    print(labels_batch.numpy())
+    training_images = []
+    training_labels = []
     
     
+    for _ in training_data:
+        training_images.append(_[0])  
+        training_labels.append(_[1])  
+        
+        
 
-plt.figure(figsize=(10, 10))
-for image_batch, labels_batch in dataset.take(1):
-    for i in range(12):
-        ax = plt.subplot(3, 4, i + 1)
-        plt.imshow(image_batch[i].numpy().astype("uint8"))
-        plt.title(class_names[labels_batch[i]])
-        plt.axis("off")
-        
-        
+
 
 train_size = 0.8
 len(dataset)*train_size
 
-
 train_ds = dataset.take(54)
 len(train_ds)
-
 
 test_ds = dataset.skip(54)
 len(test_ds)
 
-
 val_size=0.1
 len(dataset)*val_size
 
-
 val_ds = test_ds.take(6)
 len(val_ds)
-
 
 test_ds = test_ds.skip(6)
 len(test_ds)
 
 
-def get_dataset_partitions_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
-    assert (train_split + test_split + val_split) == 1
+# def get_dataset_partitions_tf(ds, train_split=0.8, val_split=0.1, test_split=0.1, shuffle=True, shuffle_size=10000):
+#     assert (train_split + test_split + val_split) == 1
     
-    ds_size = len(ds)
+#     ds_size = len(ds)
     
-    if shuffle:
-        ds = ds.shuffle(shuffle_size, seed=12)
+#     if shuffle:
+#         ds = ds.shuffle(shuffle_size, seed=12)
     
-    train_size = int(train_split * ds_size)
-    val_size = int(val_split * ds_size)
+#     train_size = int(train_split * ds_size)
+#     val_size = int(val_split * ds_size)
     
-    train_ds = ds.take(train_size)    
-    val_ds = ds.skip(train_size).take(val_size)
-    test_ds = ds.skip(train_size).skip(val_size)
+#     train_ds = ds.take(train_size)    
+#     val_ds = ds.skip(train_size).take(val_size)
+#     test_ds = ds.skip(train_size).skip(val_size)
     
-    return train_ds, val_ds, test_ds
+#     return train_ds, val_ds, test_ds
 
 
-train_ds, val_ds, test_ds = get_dataset_partitions_tf(dataset)
+# train_ds, val_ds, test_ds = get_dataset_partitions_tf(dataset)
 
 
-len(train_ds)
+# len(train_ds)
 
 
-len(val_ds)
+# len(val_ds)
 
 
-len(test_ds)
+# len(test_ds)
 
 
-train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
-val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
-test_ds = test_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+# train_ds = train_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+# val_ds = val_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
+# test_ds = test_ds.cache().shuffle(1000).prefetch(buffer_size=tf.data.AUTOTUNE)
