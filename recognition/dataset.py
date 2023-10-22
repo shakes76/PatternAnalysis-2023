a3c5ea19f2e2af8a3f6382d81031ebf410f47f54
Tzu-Yu Yang <125327045+jyz523@users.noreply.github.com>
2023-10-22 18:44:25 +1000

    Update dataset.py

--- recognition/dataset.py
+++ recognition/dataset.py
@@ -1,61 +1,67 @@
-import os
-import torch
-from torch.utils.data import Dataset
-from PIL import Image
-from torchvision import transforms
+import os  # For interacting with the operating system.
+import torch  # Import PyTorch for deep learning functionalities.
+from torch.utils.data import Dataset  # Abstract class representing a dataset.
+from PIL import Image  # Python Imaging Library for opening, manipulating, and saving images.
+from torchvision import transforms  # Common image transformations.
 
-
+# Determine if CUDA (GPU support) is available, use it, otherwise default to CPU.
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-print(f"Dataset: {torch.cuda.is_available()}")
+print(f"Dataset: {torch.cuda.is_available()}")  # Display whether CUDA is available.
 
+# Define a custom dataset class, inheriting from the torch.utils.data.Dataset class.
 class ISICDataset(Dataset):
     def __init__(self, image_dir, mask_dir=None, img_transform=None, mask_transform=None, img_size=None):
-        self.img_size = img_size
-        self.image_dir = image_dir
-        self.mask_dir = mask_dir
-        self.images = os.listdir(image_dir)
-        self.img_transform = img_transform
-        self.mask_transform = mask_transform
+        # Initialization method for the dataset object.
+        self.img_size = img_size  # Resize images to this size.
+        self.image_dir = image_dir  # Directory containing the images.
+        self.mask_dir = mask_dir  # Optional: directory containing the masks.
+        self.images = os.listdir(image_dir)  # List of filenames in the image directory.
+        self.img_transform = img_transform  # Transformations to apply to the images.
+        self.mask_transform = mask_transform  # Transformations to apply to the masks.
 
     def __len__(self):
+        # Return the total number of items in the dataset.
         return len(self.images)
 
     def __getitem__(self, idx):
-        img_path = os.path.join(self.image_dir, self.images[idx])
-        image = Image.open(img_path).convert("RGB")
-        image = image.resize(self.img_size, Image.BILINEAR)
+        # Retrieve an item (image and optionally a mask) by index.
+        img_path = os.path.join(self.image_dir, self.images[idx])  # Path to the image.
+        image = Image.open(img_path).convert("RGB")  # Open and convert the image to RGB.
+        image = image.resize(self.img_size, Image.BILINEAR)  # Resize the image if size is specified.
 
-        mask = None  # We start by assuming there is no mask
-        if self.mask_dir:  # If a mask directory is provided
-            mask_name = self.images[idx].replace('.jpg', '_segmentation.png')
-            mask_path = os.path.join(self.mask_dir, mask_name)
+        mask = None  # We start by assuming there is no mask.
+        if self.mask_dir:  # If a mask directory is provided.
+            mask_name = self.images[idx].replace('.jpg', '_segmentation.png')  # Mask filename.
+            mask_path = os.path.join(self.mask_dir, mask_name)  # Path to the mask.
 
-            if os.path.exists(mask_path):  # Check if the mask file exists
-                mask = Image.open(mask_path).convert("L")
-                mask = mask.resize(self.img_size, Image.NEAREST)
+            if os.path.exists(mask_path):  # Check if the mask file exists.
+                mask = Image.open(mask_path).convert("L")  # Open and convert the mask to greyscale.
+                mask = mask.resize(self.img_size, Image.NEAREST)  # Resize the mask if size is specified.
             else:
-                print(f"File not found: {mask_path}")
+                print(f"File not found: {mask_path}")  # Log a message if the mask file is not found.
 
-                # Apply transformations if specified
+        # Apply transformations if specified.
         if self.img_transform:
-            image = self.img_transform(image)
+            image = self.img_transform(image)  # Apply image transformations.
 
-        if mask is not None and self.mask_transform:  # Only apply mask transform if mask is present
-            mask = self.mask_transform(mask)
+        if mask is not None and self.mask_transform:  # Only apply mask transform if mask is present.
+            mask = self.mask_transform(mask)  # Apply mask transformations.
 
-        return image, mask
+        return image, mask  # Return the processed image and mask.
 
 def get_mask_transform():
-    # Masks usually don't require normalization, but they still need to be tensors
+    # Function to define and return the transformations to apply to the mask.
+    # Mask usually don't require normalization, but they still need to be tensors.
     return transforms.Compose([
-        transforms.ToTensor()
+        transforms.ToTensor()  # Convert the mask to a PyTorch tensor.
     ])
 
-
-# Define the transformation function
+# Define the transformation function for images.
 def get_transform():
+    # Define and return the transformations to apply to the images
+    # Includes normalization, and the values are standard for pre-trained models.
     return transforms.Compose([
-        transforms.ToTensor(),
+        transforms.ToTensor(),  # Convert the image to a PyTorch tensor.
+        # Normalize the tensor with the standard mean and standard deviation of the ImageNet dataset.
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
     ])
-
