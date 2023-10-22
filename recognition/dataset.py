3f56b12876651160f111e648997e9205ab6b0067
Tzu-Yu Yang <125327045+jyz523@users.noreply.github.com>
2023-10-22 19:51:13 +1000

    Update dataset.py
    fix comment

--- recognition/dataset.py
+++ recognition/dataset.py
@@ -1,5 +1,6 @@
 import os  # For interacting with the operating system.
 import torch  # Import PyTorch for deep learning functionalities.
+
 from torch.utils.data import Dataset  # Abstract class representing a dataset.
 from PIL import Image  # Python Imaging Library for opening, manipulating, and saving images.
 from torchvision import transforms  # Common image transformations.
@@ -8,8 +9,9 @@ from torchvision import transforms  # Common image transformations.
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print(f"Dataset: {torch.cuda.is_available()}")  # Display whether CUDA is available.
 
-# Define a custom dataset class, inheriting from the torch.utils.data.Dataset class.
+
 class ISICDataset(Dataset):
+    """Define a custom dataset class from the torch.utils.data.Dataset class"""
     def __init__(self, image_dir, mask_dir=None, img_transform=None, mask_transform=None, img_size=None):
         # Initialization method for the dataset object.
         self.img_size = img_size  # Resize images to this size.
@@ -20,11 +22,12 @@ class ISICDataset(Dataset):
         self.mask_transform = mask_transform  # Transformations to apply to the masks.
 
     def __len__(self):
-        # Return the total number of items in the dataset.
+        """Return the total number of items in the dataset."""
+        
         return len(self.images)
 
     def __getitem__(self, idx):
-        # Retrieve an item (image and optionally a mask) by index.
+        """Retrieve image and mask."""
         img_path = os.path.join(self.image_dir, self.images[idx])  # Path to the image.
         image = Image.open(img_path).convert("RGB")  # Open and convert the image to RGB.
         image = image.resize(self.img_size, Image.BILINEAR)  # Resize the image if size is specified.
@@ -49,17 +52,18 @@ class ISICDataset(Dataset):
 
         return image, mask  # Return the processed image and mask.
 
+
 def get_mask_transform():
-    # Function to define and return the transformations to apply to the mask.
-    # Mask usually don't require normalization, but they still need to be tensors.
+    """Define and return the transformations to apply to the mask"""
+    
     return transforms.Compose([
         transforms.ToTensor()  # Convert the mask to a PyTorch tensor.
     ])
 
-# Define the transformation function for images.
+
 def get_transform():
-    # Define and return the transformations to apply to the images
-    # Includes normalization, and the values are standard for pre-trained models.
+    """Define and return the transformations to apply to the images"""
+    
     return transforms.Compose([
         transforms.ToTensor(),  # Convert the image to a PyTorch tensor.
         # Normalize the tensor with the standard mean and standard deviation of the ImageNet dataset.
