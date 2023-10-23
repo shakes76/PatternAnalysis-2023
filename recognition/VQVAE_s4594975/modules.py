f33f7f1b8eb694e215eb0f8a829a5895061be858
pragun <prgun383@gmail.com>
2023-10-23 10:03:36 +1000

    Added structure for VQVAE and Pixelcnn - models

---
+++ recognition/VQVAE_s4594975/modules.py
@@ -0,0 +1,21 @@
+import tensorflow as tf
+from tensorflow import keras
+from tensorflow.keras import layers
+
+class VectorQuantizer(layers.Layer):
+    def __init__(self, num_embeddings, embedding_dim, beta=0.25, **kwargs):
+        
+
+    def call(self, input):
+       
+    def get_indices(self, input):
+
+
+class VAE(model)       
+    
+class pixelcnn()
+    def __init__(self) -> None:
+    def call(self, input)
+             
+class residualblock()
+class convlayer()             
