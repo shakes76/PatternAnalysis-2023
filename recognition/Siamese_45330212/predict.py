45d70605ab73b6c56239c22770a34a00524b403f
davidrtlake <davidrtlake@gmail.com>
2023-10-23 11:44:21 +1000

    updated predict to include a testing run on the trained models.

--- recognition/Siamese_45330212/predict.py
+++ recognition/Siamese_45330212/predict.py
@@ -1,6 +1,10 @@
 # Shows example usage of my trained model. Print out any results and / or provide visualisations where applicable
 from modules import *
 from dataset import *
+import time
+import torch
+import torch.nn as nn
+import time
 
 def save_plot(iteration, loss, epoch, title):# for showing loss value changed with iter
     plt.plot(iteration, loss)
@@ -22,3 +26,43 @@ def show_plot(iteration, loss, title):# for showing loss value changed with iter
     plt.xlabel('Iterations')
     plt.ylabel('Loss')
     plt.show()
+
+def runtest():
+    classification_model = BinaryClassifier()
+    classification_model.load_state_dict(torch.load("/home/Student/s4533021/classification_model.pt"))
+    classification_model = classification_model.to(device)
+    model = ResNet18()
+    model.load_state_dict(torch.load("/home/Student/s4533021/siamese_model2.pt"))
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    # Warning message if CUDA is not available
+    if not torch.cuda.is_available():
+        print("Warning CUDA not Found. Using CPU")
+    else:
+        print("Using CUDA.")
+    # --------------
+    # Test the model
+    print("> Testing")
+    start = time.time() #time generation
+    classification_model.eval()
+    model.eval()
+    with torch.no_grad():
+        correct = 0
+        total = 0
+        for img, label in test_loader:
+            img, label = img.to(device) , label.to(device)
+            embeddings = model.forward_once(img)
+            output = classification_model(embeddings)
+            _, predicted = torch.max(output.data, 1)
+            total += label.size(0)
+            correct += (predicted == label).sum().item()
+        if total != 0:
+            print('Test Accuracy: {} %'.format(100 * correct / total))
+        else:
+            print('Total is 0', correct, total)
+    end = time.time()
+    elapsed = end - start
+    print("Testing took " + str(elapsed) + " secs or " + str(elapsed/60) + " mins in total")
+
+    print('END')
