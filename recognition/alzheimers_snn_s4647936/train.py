cc2038f52a25433fc21882a41cc451315c3837c6
Vicky <trannvickyy@gmail.com>
2023-10-23 14:30:15 +1000

    Integrated the training loop for the Siamese Network into train.py. Added visualization of sample images post-training.

--- recognition/alzheimers_snn_s4647936/train.py
+++ recognition/alzheimers_snn_s4647936/train.py
@@ -2,7 +2,10 @@ import os
 import torch
 import matplotlib.pyplot as plt
 from dataset import TripletDataset 
+from modules import SiameseNetwork, TripletLoss
 from torchvision import transforms
+import torch.optim as optim
+
 
 # Transformations for images
 transform = transforms.Compose([
@@ -14,33 +17,76 @@ transform = transforms.Compose([
 train_dataset = TripletDataset(root_dir="/home/Student/s4647936/PatternAnalysis-2023/recognition/alzheimers_snn_s4647936/AD_NC", mode='train', transform=transform)
 test_dataset = TripletDataset(root_dir="/home/Student/s4647936/PatternAnalysis-2023/recognition/alzheimers_snn_s4647936/AD_NC", mode='test', transform=transform)
 
+# Parameters
+learning_rate = 0.001
+num_epochs = 10
+batch_size = 32
+
+# GPU availability
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+# Initialise the Siamese Network and Triplet Loss
+model = SiameseNetwork().to(device)
+criterion = TripletLoss(margin=1.0).to(device)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+
+# DataLoader setup
+train_loader = torch.utils.data.DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+
+# Training loop
+losses = []
+
+for epoch in range(num_epochs):
+    model.train()
+    running_loss = 0.0
+    for batch_idx, (anchor, positive, negative) in enumerate(train_loader):
+        anchor, positive, negative = anchor.to(device), positive.to(device), negative.to(device)
+
+        # Zero the parameter gradients
+        optimizer.zero_grad()
+
+        # Forward pass
+        anchor_out, positive_out = model(anchor, positive)
+        _, negative_out = model(anchor, negative)
+
+        loss = criterion(anchor_out, positive_out, negative_out)
+        loss.backward()
+        optimizer.step()
+
+        running_loss += loss.item()
+
+    epoch_loss = running_loss / len(train_loader)
+    losses.append(epoch_loss)
+    print(f"Epoch {epoch+1}/{num_epochs}, Loss: {epoch_loss:.4f}")
+
+print("Finished Training")
+
 # Test to see number of images
-print(len(train_dataset)) # 21520
-print(len(test_dataset)) # 9000
+# print(len(train_dataset)) # 17200
+# print(len(test_dataset)) # 1820
 
-# Get a sample triplet from the training dataset
-anchor, positive, negative = train_dataset[0]
+# Save the loss curve
+plt.figure()
+plt.plot(losses)
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.title('Training Loss Curve')
+plt.savefig('loss_curve.png')
 
-# Now check for NaN and Inf values
-print(torch.isnan(anchor).any())
-print(torch.isinf(anchor).any())
-
-# Function to display images
-def imshow(img):
+# Function to save images
+def save_image(img, filename):
     img = img.numpy().transpose((1, 2, 0))
     
     # Convert to float and normalize if necessary
     if img.max() > 1:
         img = img.astype(float) / 255
         
+    plt.figure()
     plt.imshow(img)
-    plt.show(block=True)
+    plt.axis('off')  # Hide axes
+    plt.savefig(filename)
 
-# Display the images
-imshow(anchor)
-imshow(positive)
-imshow(negative)
-
-# Example to save the image
-plt.savefig('sample_image.png')
-
+# Save sample images after training
+save_image(anchor, 'anchor_sample.png')
+save_image(positive, 'positive_sample.png')
+save_image(negative, 'negative_sample.png')
