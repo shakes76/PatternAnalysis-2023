e255345cce25548ad36e86abb06b3fdc7d0f95b1
pragun <prgun383@gmail.com>
2023-10-23 11:03:37 +1000

    Added encoder for the VQVAE

--- recognition/VQVAE_s4594975/modules.py
+++ recognition/VQVAE_s4594975/modules.py
@@ -40,6 +40,17 @@ class VectorQuantizer(layers.Layer):
             + tf.reduce_sum(self.embeddings ** 2, axis=0)
             - 2 * similarity
         )
+        indices = tf.argmin(distances, axis=1)
+        return indices
+
+def get_encoder(latent_dim=16):
+    encoder_inputs = keras.Input(shape=(80, 80, 1))
+    x = layers.Conv2D(32, 3, activation="relu", strides=2, padding="same")(
+        encoder_inputs
+    )
+    x = layers.Conv2D(64, 3, activation="relu", strides=2, padding="same")(x)
+    encoder_outputs = layers.Conv2D(latent_dim, 1, padding="same")(x)
+    return keras.Model(encoder_inputs, encoder_outputs, name="encoder")
 
 
 '''
