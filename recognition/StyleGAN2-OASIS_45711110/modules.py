f59ededa26bd8ce509e98fbe94137eec00774e8d
4vir4l <iaviral.sh@gmail.com>
2023-10-24 17:42:31 +1000

    implemented perceptual path length normalization

--- recognition/StyleGAN2-OASIS_45711110/modules.py
+++ recognition/StyleGAN2-OASIS_45711110/modules.py
@@ -244,3 +244,43 @@ class EqualizedConv2d(nn.Module):
 
     def forward(self, x: torch.Tensor):
         return F.conv2d(x, self.weight(), bias=self.bias, padding=self.padding)
+    
+class PathLengthPenalty(nn.Module):
+
+    def __init__(self, beta):
+        super().__init__()
+
+        self.beta = beta
+        self.steps = nn.Parameter(torch.tensor(0.), requires_grad=False)
+
+        self.exp_sum_a = nn.Parameter(torch.tensor(0.), requires_grad=False)
+
+    def forward(self, w, x):
+
+        device = x.device
+        image_size = x.shape[2] * x.shape[3]
+        y = torch.randn(x.shape, device=device)
+
+        output = (x * y).sum() / sqrt(image_size)
+        sqrt(image_size)
+
+        gradients, *_ = torch.autograd.grad(outputs=output,
+                                            inputs=w,
+                                            grad_outputs=torch.ones(output.shape, device=device),
+                                            create_graph=True)
+
+        norm = (gradients ** 2).sum(dim=2).mean(dim=1).sqrt()
+
+        if self.steps > 0:
+
+            a = self.exp_sum_a / (1 - self.beta ** self.steps)
+
+            loss = torch.mean((norm - a) ** 2)
+        else:
+            loss = norm.new_tensor(0)
+
+        mean = norm.mean().detach()
+        self.exp_sum_a.mul_(self.beta).add_(mean, alpha=1 - self.beta)
+        self.steps.add_(1.)
+
+        return loss
