fc7ba99047fe1b4d46368cfd867dff187c0084b2
4vir4l <iaviral.sh@gmail.com>
2023-10-24 13:47:38 +1000

    module: Implemented Mapping Network

--- recognition/StyleGAN2-OASIS_45711110/modules.py
+++ recognition/StyleGAN2-OASIS_45711110/modules.py
@@ -1,1 +1,54 @@
-"Source code of the model components"
+'''Source code of the model components'''
+
+import torch
+from torch import nn
+import torch.nn.functional as F
+from math import sqrt #math sqrt is about 7 times faster than numpy sqrt
+import numpy as np
+
+# Mapping network with the 8 Equalized Linear layer. Uses ReLU as activation fn for each layer.
+class MappingNetwork(nn.Module):
+    def __init__(self, z_dim, w_dim):
+        super().__init__()
+        self.mapping = nn.Sequential(
+            EqualizedLinear(z_dim, w_dim),
+            nn.ReLU(),
+            EqualizedLinear(z_dim, w_dim),
+            nn.ReLU(),
+            EqualizedLinear(z_dim, w_dim),
+            nn.ReLU(),
+            EqualizedLinear(z_dim, w_dim),
+            nn.ReLU(),
+            EqualizedLinear(z_dim, w_dim),
+            nn.ReLU(),
+            EqualizedLinear(z_dim, w_dim),
+            nn.ReLU(),
+            EqualizedLinear(z_dim, w_dim),
+            nn.ReLU(),
+            EqualizedLinear(z_dim, w_dim)
+        )
+    
+    def forward(self, x):
+        x = x / torch.sqrt(torch.mean(x ** 2, dim=1, keepdim=True) + 1e-8)  # for PixelNorm 
+        return self.mapping(x)
+
+# Define the EqualizedLinear class
+class EqualizedLinear(nn.Module):
+    def __init__(self, in_features, out_features, bias=0.):
+        super().__init__()
+        # Define a linear layer with equalized learning rate
+        self.weight = EqualizedWeight([out_features, in_features])
+        self.bias = nn.Parameter(torch.ones(out_features) * bias)
+
+    def forward(self, x: torch.Tensor):
+        return F.linear(x, self.weight(), bias=self.bias)
+
+# Weight equalization layer
+class EqualizedWeight(nn.Module):
+    def __init__(self, shape):
+        super().__init__()
+        self.c = 1 / sqrt(np.prod(shape[1:]))
+        self.weight = nn.Parameter(torch.randn(shape))
+
+    def forward(self):
+        return self.weight * self.c
