7e3381299f648d4cc004a1a49f2bbace3b341504
ConnorArmstrong <connor.armstrong@pm.me>
2023-10-24 20:52:47 +1000

    refactoring monolith

--- recognition/vq-vae_s47036219/modules.py
+++ recognition/vq-vae_s47036219/modules.py
@@ -26,98 +26,107 @@ device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Running on: ", device)
 
 class ResidualBlock(nn.Module):
-    def __init__(self, in_channels):
+    def __init__(self, in_channels, out_channels, intermediate_channels=None):
         super(ResidualBlock, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(in_channels, in_channels, 3, padding=1),
-            nn.BatchNorm2d(in_channels),
+
+        if not intermediate_channels:
+            intermediate_channels = in_channels // 2
+
+        self._residual_block = nn.Sequential(
             nn.ReLU(),
-            nn.Conv2d(in_channels, in_channels, 3, padding=1),
-            nn.BatchNorm2d(in_channels)
+            nn.Conv2d(in_channels, intermediate_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.ReLU(),
+            nn.Conv2d(intermediate_channels, out_channels, kernel_size=1, stride=1, bias=False)
         )
 
     def forward(self, x):
-        return x + self.conv(x)
+        return x + self._residual_block(x)
 
-# VQ-VAE Components
-# VQ-VAE Components
+
 class Encoder(nn.Module):
     def __init__(self):
         super(Encoder, self).__init__()
-        self.conv = nn.Sequential(
-            nn.Conv2d(1, 64, 4, stride=2, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Conv2d(64, 128, 4, stride=2, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Conv2d(128, 256, 4, stride=2, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.Conv2d(256, 512, 4, stride=2, padding=1),
-            nn.BatchNorm2d(512),
-            nn.ReLU(),
-            nn.Dropout(0.2)
+        
+        self.layers = nn.Sequential(
+            nn.Conv2d(1, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64)
         )
 
     def forward(self, x):
-        x = self.conv(x)
-        #print(f"Encoder output shape: {x.shape}")
-        return x
+        out = self.layers(x)
+        return out
+
+
+class VectorQuantizer(nn.Module):
+    def __init__(self, num_embeddings, embedding_dim):
+        super(VectorQuantizer, self).__init__()
+        
+        self.num_embeddings = num_embeddings  # Save as an instance variable
+        self.embedding = nn.Embedding(self.num_embeddings, embedding_dim)
+        self.embedding.weight.data.uniform_(-1./self.num_embeddings, 1./self.num_embeddings)
+
+    def forward(self, x):
+        batch_size, channels, height, width = x.shape
+        x_flat = x.permute(0, 2, 3, 1).contiguous().view(-1, channels)
+
+        # Now x_flat is [batch_size * height * width, channels]
+        
+        # Calculate distances
+        distances = ((x_flat.unsqueeze(1) - self.embedding.weight.unsqueeze(0)) ** 2).sum(-1)
+        
+        # Find the closest embeddings
+        _, indices = distances.min(1)
+        encodings = torch.zeros_like(distances).scatter_(1, indices.unsqueeze(1), 1)
+        
+        # Quantize the input image
+        quantized = self.embedding(indices)
+        
+        # Reshape the quantized tensor to the same shape as the input
+        quantized = quantized.view(batch_size, height, width, channels).permute(0, 3, 1, 2)
+        
+        return quantized
 
 class Decoder(nn.Module):
     def __init__(self):
         super(Decoder, self).__init__()
-        self.deconv = nn.Sequential(
-            nn.ConvTranspose2d(512, 256, 4, stride=2, padding=1),
-            nn.BatchNorm2d(256),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.ConvTranspose2d(256, 128, 4, stride=2, padding=1),
-            nn.BatchNorm2d(128),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.ConvTranspose2d(128, 64, 4, stride=2, padding=1),
-            nn.BatchNorm2d(64),
-            nn.ReLU(),
-            nn.Dropout(0.2),
-            nn.ConvTranspose2d(64, 1, 4, stride=2, padding=1),
-            nn.Sigmoid()
+
+        self.layers = nn.Sequential(
+            nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64),
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.ReLU(inplace=True),
+            nn.ConvTranspose2d(32, 1, kernel_size=4, stride=2, padding=1)
         )
 
     def forward(self, x):
-        x = self.deconv(x)
-        #print(f"Decoder output shape: {x.shape}")
-        return x
+        return self.layers(x)
 
-class VectorQuantizer(nn.Module):
-    def __init__(self, codebook_size=512, code_dim=512):
-        super(VectorQuantizer, self).__init__()
-        self.codebook = nn.Parameter(torch.randn(codebook_size, code_dim).cuda())
-        
-    def forward(self, z):
-        # z shape: [batch, num_vectors_per_batch, dim]
-        # codebook shape: [num_codebook_entries, dim]
-        
-        # Calculate distances
-        dist = ((z.unsqueeze(2) - self.codebook.unsqueeze(0).unsqueeze(0)) ** 2).sum(-1)
-        
-        # Find the closest codebook entry for each z vector
-        _, indices = dist.min(-1)
-        
-        # Fetch the corresponding codebook vectors
-        # The shape is the same as z ([batch, num_vectors_per_batch, dim])
-        z_q = self.codebook[indices, :]
 
-        return z_q
+class VQVAE(nn.Module):
+    def __init__(self, num_embeddings=512, embedding_dim=64):
+        super(VQVAE, self).__init__()
+
+        self.encoder = Encoder()
+        self.conv1 = nn.Conv2d(64, embedding_dim, kernel_size=1, stride=1)
+        self.vector_quantizer = VectorQuantizer(num_embeddings, embedding_dim)
+        self.decoder = Decoder()
+
+    def forward(self, x):
+        enc = self.encoder(x)
+        enc = self.conv1(enc)
+        quantized = self.vector_quantizer(enc)
+        
+        dec = self.decoder(quantized)
+        return dec
 
 # Hyperparameters
 learning_rate = 1e-3
 batch_size = 32
-num_epochs = 75
+num_epochs = 20
 codebook_size = 512
 
 # Weight for L2 and SSIM in final loss
@@ -125,7 +134,7 @@ l2_weight = 0
 ssim_weight = 1
 
 # Constants for early stopping
-patience = 75
+patience = 10
 best_val_loss = float('inf')
 counter = 0
 
@@ -147,29 +156,19 @@ val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
 
 # Models and Optimizers
-encoder = Encoder().to(device)  # Move to device
-decoder = Decoder().to(device)  # Move to device
-vector_quantizer = VectorQuantizer().to(device)  # Move to device
+model = VQVAE(codebook_size).to(device)
 
-optimizer = optim.Adam(list(encoder.parameters()) + list(decoder.parameters()), lr=learning_rate)
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=2, verbose=True)
+optimizer = optim.Adam(model.parameters(), lr=learning_rate)
+scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=3, verbose=True)
 
 # Training Loop
 for epoch in range(num_epochs):
     for i, (img, _) in enumerate(train_loader):
         img = img.to(device)  # Move to device
 
-        # Forward pass
-        z = encoder(img)
-        batch_size, _, H, W = z.shape
-
-        z = z.permute(0, 2, 3, 1).contiguous().view(batch_size, H * W, -1)
-        z_q = vector_quantizer(z)
-        z_q = z_q.view(batch_size, H, W, 512).permute(0, 3, 1, 2).contiguous()
-
-        # Decoder
-        recon = decoder(z_q)
-
+        # Forward pass through the entire model
+        recon = model(img)
+        
         # Calculate L2 loss
         l2_loss = ((recon - img) ** 2).sum()
 
@@ -180,11 +179,11 @@ for epoch in range(num_epochs):
         loss = l2_weight * l2_loss + ssim_weight * ssim_loss
 
         optimizer.zero_grad()
-        loss.backward(retain_graph=True)
+        loss.backward()
         optimizer.step()
 
-        # Update codebook
-        vector_quantizer.codebook.data = 0.99 * vector_quantizer.codebook.data + 0.01 * z.detach().mean(0).mean(0)
+        # Assuming you'd still want to update the codebook, though this needs to be integrated with the VQ process
+        #model.vector_quantizer.embedding.weight.data = 0.99 * model.vector_quantizer.embedding.weight.data + 0.01 * enc.detach().mean(0).mean(0)
 
     # Validation phase
     val_losses = []
@@ -216,65 +215,24 @@ for epoch in range(num_epochs):
     avg_val_loss = sum(val_losses) / len(val_losses)
     print(f"Epoch [{epoch+1}/{num_epochs}], Training Loss: {loss.item():.4f}, Validation Loss: {avg_val_loss:.4f}")
 
+    # Update learning rate
+    scheduler.step(avg_val_loss)
+    # Print current learning rate
+    current_lr = optimizer.param_groups[0]['lr']
+    print(f"Current Learning Rate: {current_lr}")
+
     # Early Stopping
     if avg_val_loss < best_val_loss:
         best_val_loss = avg_val_loss
-        counter = 0
+        counter = 0  # Reset counter when validation loss decreases
     else:
         counter += 1
         if counter >= patience:
             print(f"Early stopping at epoch {epoch+1}")
             break
 
+
 # Save Models
-torch.save(encoder.state_dict(), 'encoder.pth')
-torch.save(decoder.state_dict(), 'decoder.pth')
-torch.save(vector_quantizer.state_dict(), 'vectorquantizer.pth')
+torch.save(model.state_dict(), 'vqvae.pth')
 
 
-from skimage.metrics import structural_similarity as ssim
-import matplotlib.pyplot as plt
-import numpy as np
-import cv2  # Import OpenCV
-import torch
-
-# Assuming your existing PyTorch models are already loaded
-# encoder, decoder, vector_quantizer, and codebook
-
-encoder.eval()  # Set the encoder model to evaluation mode
-decoder.eval()  # Set the decoder model to evaluation mode
-vector_quantizer.eval()  # Set the vector quantizer to evaluation mode
-
-with torch.no_grad():  # Turn off gradients for the upcoming operations
-    for img, _ in train_loader:  # Loop through batches in the data loader
-        img = img.cuda()  # Move the images to the GPU
-        z = encoder(img)  # Encode the images to latent space
-
-        batch_size, _, H, W = z.shape
-        z = z.permute(0, 2, 3, 1).contiguous().view(batch_size, H * W, -1)
-        
-        z_q = vector_quantizer(z)
-        z_q = z_q.view(batch_size, H, W, 512).permute(0, 3, 1, 2).contiguous()
-        
-        recon = decoder(z_q)  # Decode the quantized vectors
-
-        # Calculate SSIM
-        original_img = img.cpu().numpy().squeeze(1)  # Convert to numpy and remove color channel dimension
-        reconstructed_img = recon.cpu().numpy().squeeze(1)  # Convert to numpy and remove color channel dimension
-        
-        ssim_val = ssim(original_img[0], reconstructed_img[0], data_range=reconstructed_img.max() - reconstructed_img.min())  # Calculate SSIM
-
-        print(f'SSIM: {ssim_val}')  # Output SSIM value
-
-        # Output images
-        plt.subplot(1, 2, 1)
-        plt.title('Original')
-        plt.imshow(original_img[0], cmap='gray')  # Show original image
-        
-        plt.subplot(1, 2, 2)
-        plt.title('Reconstructed')
-        plt.imshow(reconstructed_img[0], cmap='gray')  # Show reconstructed image
-
-        plt.show()  # Show the plot
-
-        break  # Exit the loop after one iteration for demonstration
