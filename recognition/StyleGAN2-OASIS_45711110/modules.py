36aca40d70693393952603afd519c6e7e2631f41
4vir4l <iaviral.sh@gmail.com>
2023-10-24 17:32:19 +1000

    added class: discriminator block

--- recognition/StyleGAN2-OASIS_45711110/modules.py
+++ recognition/StyleGAN2-OASIS_45711110/modules.py
@@ -204,4 +204,32 @@ class Discriminator(nn.Module):
         x = x.reshape(x.shape[0], -1)
         return self.final(x)
     
-#TODO: discriminatorBlock and EqualizedConv2d
+class DiscriminatorBlock(nn.Module):
+
+    def __init__(self, in_features, out_features):
+        super().__init__()
+        self.residual = nn.Sequential(nn.AvgPool2d(kernel_size=2, stride=2), # down sampling using avg pool
+                                      EqualizedConv2d(in_features, out_features, kernel_size=1))
+
+        self.block = nn.Sequential(
+            EqualizedConv2d(in_features, in_features, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.2, True),
+            EqualizedConv2d(in_features, out_features, kernel_size=3, padding=1),
+            nn.LeakyReLU(0.2, True),
+        )
+
+        self.down_sample = nn.AvgPool2d(
+            kernel_size=2, stride=2
+        )  # down sampling using avg pool
+
+        self.scale = 1 / sqrt(2)
+
+    def forward(self, x):
+        residual = self.residual(x)
+
+        x = self.block(x)
+        x = self.down_sample(x)
+
+        return (x + residual) * self.scale
+    
+#TODO: qualizedConv2d
