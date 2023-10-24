45cfe11bdfdf28db65441134cb1d68e87d14310d
4vir4l <iaviral.sh@gmail.com>
2023-10-24 15:55:01 +1000

    added class: styleBlock

--- recognition/StyleGAN2-OASIS_45711110/modules.py
+++ recognition/StyleGAN2-OASIS_45711110/modules.py
@@ -100,4 +100,23 @@ class GeneratorBlock(nn.Module):
 
         return x, rgb
     
-#TODO: styleblock, toRGB
+class StyleBlock(nn.Module):
+
+    def __init__(self, W_DIM, in_features, out_features):
+        super().__init__()
+
+        self.to_style = EqualizedLinear(W_DIM, in_features, bias=1.0)
+        self.conv = Conv2dWeightModulate(in_features, out_features, kernel_size=3)
+        self.scale_noise = nn.Parameter(torch.zeros(1))
+        self.bias = nn.Parameter(torch.zeros(out_features))
+
+        self.activation = nn.LeakyReLU(0.2, True)
+
+    def forward(self, x, w, noise):
+        s = self.to_style(w)
+        x = self.conv(x, s)
+        if noise is not None:
+            x = x + self.scale_noise[None, :, None, None] * noise
+        return self.activation(x + self.bias[None, :, None, None])
+    
+#TODO: toRGB
