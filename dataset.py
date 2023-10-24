99733ad9bc6fadf27b13e235b4264f9b7a2a6fb1
HaadiQureshi <141606798+HaadiQureshi@users.noreply.github.com>
2023-10-24 16:05:28 +1000

    dataset.py
    Set-up data loading pipelines for training, validation, and testing

---
+++ dataset.py
@@ -0,0 +1,13 @@
+from torchvision import datasets, transforms
+from torch.utils.data import DataLoader, random_split
+
+
+transform = transforms.Compose([transforms.Grayscale(num_output_channels=1), transforms.ToTensor()])
+dataset_train = datasets.ImageFolder('../ADNI_AD_NC_2D/AD_NC/train/', transform=transform)
+dataloader_train = DataLoader(dataset_train, batch_size=32, shuffle=True)
+dataset_test = datasets.ImageFolder('../ADNI_AD_NC_2D/AD_NC/test/', transform=transform)
+dataset_test, dataset_val = random_split(dataset_test, [0.7, 0.3])
+dataloader_test = DataLoader(dataset_test, batch_size=32, shuffle=True)
+
+def returnDataloaders():
+    return dataloader_train, dataloader_test, dataloader_val
