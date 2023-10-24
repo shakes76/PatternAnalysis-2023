ce5117b75216dc3f3be534829be7ad9ceaaf379b
ConnorArmstrong <connor.armstrong@pm.me>
2023-10-24 22:04:12 +1000

    modules refactor

--- recognition/vq-vae_s47036219/README.md
+++ recognition/vq-vae_s47036219/README.md
@@ -4,6 +4,8 @@
 
 
 # Project:
+
+## The Vector Quantized Variational Autoencoder
 The goal of this task was to implement a Vector Quantized Variational Autoencoder (henceforth referred to as a VQ-VAE). The VQ-VAE is an extension of a typical variational autoencoder that handles discrete latent representation learning - which is where the model learns to represent data in a form where the latent variables take on distinct discrete values, rather than a continuous range. This is done by the model passing the encoders output through a vector quantisation layer, mapping the continuous encodings to the closest vector in the embedding spacve. This makes the VQ-VAE very effective at managing discrete structured data and image reconstruction/generation.
 
 
--- recognition/vq-vae_s47036219/modules.py
+++ recognition/vq-vae_s47036219/modules.py
@@ -1,29 +1,5 @@
 import torch
 import torch.nn as nn
-import torch.optim as optim
-from torch.utils.data import DataLoader
-from torchvision import datasets, transforms
-import pytorch_ssim
-
-
-def ssim(img1, img2, C1=0.01**2, C2=0.03**2):
-    mu1 = img1.mean(dim=[2, 3], keepdim=True)
-    mu2 = img2.mean(dim=[2, 3], keepdim=True)
-    
-    sigma1_sq = (img1 - mu1).pow(2).mean(dim=[2, 3], keepdim=True)
-    sigma2_sq = (img2 - mu2).pow(2).mean(dim=[2, 3], keepdim=True)
-    sigma12 = ((img1 - mu1)*(img2 - mu2)).mean(dim=[2, 3], keepdim=True)
-    
-    ssim_n = (2*mu1*mu2 + C1) * (2*sigma12 + C2)
-    ssim_d = (mu1.pow(2) + mu2.pow(2) + C1) * (sigma1_sq + sigma2_sq + C2)
-
-    ssim_val = ssim_n / ssim_d
-
-    return ssim_val.mean()
-
-# Check for CUDA availability and set the device accordingly
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-print("Running on: ", device)
 
 class ResidualBlock(nn.Module):
     def __init__(self, in_channels, out_channels, intermediate_channels=None):
@@ -49,8 +25,15 @@ class Encoder(nn.Module):
         
         self.layers = nn.Sequential(
             nn.Conv2d(1, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
             nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+
             nn.Conv2d(32, 64, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+
             ResidualBlock(64, 64),
             ResidualBlock(64, 64)
         )
@@ -58,7 +41,6 @@ class Encoder(nn.Module):
     def forward(self, x):
         out = self.layers(x)
         return out
-
 
 class VectorQuantizer(nn.Module):
     def __init__(self, num_embeddings, embedding_dim):
@@ -95,16 +77,23 @@ class Decoder(nn.Module):
 
         self.layers = nn.Sequential(
             nn.Conv2d(64, 64, kernel_size=3, stride=1, padding=1),
-            ResidualBlock(64, 64),
-            ResidualBlock(64, 64),
-            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(64),
             nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64),
+
+            nn.ConvTranspose2d(64, 32, kernel_size=4, stride=2, padding=1),
+            nn.BatchNorm2d(32),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+
             nn.ConvTranspose2d(32, 1, kernel_size=4, stride=2, padding=1)
         )
 
     def forward(self, x):
         return self.layers(x)
-
 
 class VQVAE(nn.Module):
     def __init__(self, num_embeddings=512, embedding_dim=64):
@@ -122,164 +111,19 @@ class VQVAE(nn.Module):
         
         dec = self.decoder(quantized)
         return dec
+    
+    
+def ssim(img1, img2, C1=0.01**2, C2=0.03**2):
+    mu1 = img1.mean(dim=[2, 3], keepdim=True)
+    mu2 = img2.mean(dim=[2, 3], keepdim=True)
+    
+    sigma1_sq = (img1 - mu1).pow(2).mean(dim=[2, 3], keepdim=True)
+    sigma2_sq = (img2 - mu2).pow(2).mean(dim=[2, 3], keepdim=True)
+    sigma12 = ((img1 - mu1)*(img2 - mu2)).mean(dim=[2, 3], keepdim=True)
+    
+    ssim_n = (2*mu1*mu2 + C1) * (2*sigma12 + C2)
+    ssim_d = (mu1.pow(2) + mu2.pow(2) + C1) * (sigma1_sq + sigma2_sq + C2)
 
-# Hyperparameters
-learning_rate = 1e-3
-batch_size = 32
-num_epochs = 40
-codebook_size = 512
+    ssim_val = ssim_n / ssim_d
 
-# Weight for L2 and SSIM in final loss
-l2_weight = 0.05
-ssim_weight = 1
-
-# Constants for early stopping
-patience = 12
-best_val_loss = float('inf')
-counter = 0
-
-# Data Loaders
-transform = transforms.Compose([
-    transforms.Grayscale(num_output_channels=1),
-    transforms.Resize((64, 64)),
-    transforms.ToTensor(),
-    #transforms.Normalize(mean=[0.5], std=[0.5])  
-])
-
-train_dataset = datasets.ImageFolder(root='C:/Users/Connor/Documents/comp3710/dataset/ADNI/AD_NC/train', transform=transform)
-train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
-
-# Validation DataLoader
-val_dataset = datasets.ImageFolder(root='C:/Users/Connor/Documents/comp3710/dataset/ADNI/AD_NC/test', transform=transform)
-val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
-
-
-# Models and Optimizers
-model = VQVAE(codebook_size).to(device)
-
-optimizer = optim.Adam(model.parameters(), lr=learning_rate)
-scheduler = optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min', patience=3, verbose=True)
-
-# Training Loop
-for epoch in range(num_epochs):
-    for i, (img, _) in enumerate(train_loader):
-        model.train()
-        img = img.to(device)  # Move to device
-
-        # Forward pass through the entire model
-        recon = model(img)
-        
-        # Calculate L2 loss
-        l2_loss = ((recon - img) ** 2).sum()
-
-        # Calculate SSIM loss
-        ssim_loss = 1 - ssim(img, recon)
-
-        # Final Loss
-        loss = l2_weight * l2_loss + ssim_weight * ssim_loss
-
-        optimizer.zero_grad()
-        loss.backward()
-        optimizer.step()
-
-        # Assuming you'd still want to update the codebook, though this needs to be integrated with the VQ process
-        #model.vector_quantizer.embedding.weight.data = 0.99 * model.vector_quantizer.embedding.weight.data + 0.01 * enc.detach().mean(0).mean(0)
-
-    # Validation phase
-    val_losses = []
-    model.eval()
-    with torch.no_grad():
-        for i, (img, _) in enumerate(val_loader):
-            img = img.to(device)
-            
-            # Validation forward pass
-            z = encoder(img)
-            batch_size, _, H, W = z.shape
-
-            z = z.permute(0, 2, 3, 1).contiguous().view(batch_size, H * W, -1)
-            z_q = vector_quantizer(z)
-            z_q = z_q.view(batch_size, H, W, 512).permute(0, 3, 1, 2).contiguous()
-
-            # Decoder
-            recon = decoder(z_q)
-
-            # Validation losses
-            l2_loss = ((recon - img) ** 2).sum()
-            ssim_loss = 1 - ssim(img, recon)
-            loss = l2_weight * l2_loss + ssim_weight * ssim_loss
-
-            val_losses.append(loss.item())
-
-    avg_val_loss = sum(val_losses) / len(val_losses)
-    print(f"Epoch [{epoch+1}/{num_epochs}], Training Loss: {loss.item():.4f}, Validation Loss: {avg_val_loss:.4f}")
-
-    # Update learning rate
-    scheduler.step(avg_val_loss)
-    # Print current learning rate
-    current_lr = optimizer.param_groups[0]['lr']
-    print(f"Current Learning Rate: {current_lr}")
-
-    # Early Stopping
-    if avg_val_loss < best_val_loss:
-        best_val_loss = avg_val_loss
-        counter = 0  # Reset counter when validation loss decreases
-    else:
-        counter += 1
-        if counter >= patience:
-            print(f"Early stopping at epoch {epoch+1}")
-            break
-
-
-# Save Models
-torch.save(model.state_dict(), 'vqvae.pth')
-
-import matplotlib.pyplot as plt
-import numpy as np
-import cv2  # Import OpenCV
-import torch
-import random
-
-# Assuming your existing PyTorch models are already loaded
-# encoder, decoder, vector_quantizer, and codebook
-model.load_state_dict(torch.load('vqvae.pth'))
-val_losses = []
-model.eval()
-
-with torch.no_grad():
-    for i, (img, _) in enumerate(val_loader):
-        img = img.to(device)
-        
-        # Validation forward pass
-        z = model.encoder(img)
-        z = model.conv1(z)
-        z_q = model.vector_quantizer(z)
-        recon = model.decoder(z_q)
-
-        # Validation losses
-        l2_loss = ((recon - img) ** 2).sum()
-        print(img.dtype, recon.dtype)
-        ssim_loss = 1 - ssim(img, recon)
-        loss = l2_weight * l2_loss + ssim_weight * ssim_loss
-
-        val_losses.append(loss.item())
-        
-        # Calculate SSIM
-        ssim_val = ssim(img, recon).item()  # We already have a PyTorch-based SSIM function
-        print(f'SSIM: {ssim_val}')  # Output SSIM value
-
-        # Assuming the images are single channel and the channel dimension is at the second position
-        original_img = img.cpu().numpy().squeeze(1)
-        reconstructed_img = recon.cpu().numpy().squeeze(1)
-
-        # Output images (for first image in the batch)
-        plt.subplot(1, 2, 1)
-        plt.title('Original')
-        plt.imshow(original_img[0], cmap='gray')
-        
-        plt.subplot(1, 2, 2)
-        plt.title('Reconstructed')
-        plt.imshow(reconstructed_img[0], cmap='gray')
-        
-        plt.show()
-        
-        break  # Exit the loop after one iteration for demonstration
+    return ssim_val.mean()
