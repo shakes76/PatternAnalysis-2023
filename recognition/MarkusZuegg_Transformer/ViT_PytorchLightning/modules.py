3f1720efc816381d685905d9fbfe3517d52fb1b0
Markus Zuegg <markuszuegg@outlook.com>
2023-10-25 02:06:43 +1000

    added comments and cleaned code

--- recognition/MarkusZuegg_Transformer/ViT_PytorchLightning/modules.py
+++ recognition/MarkusZuegg_Transformer/ViT_PytorchLightning/modules.py
@@ -1,9 +1,10 @@
+"""
+#! make a file header
+"""
 import pytorch_lightning as pl
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchmetrics
-import torchvision
 from dataset import img_to_patch
 
 class AttentionBlock(nn.Module):
@@ -13,7 +14,6 @@ class AttentionBlock(nn.Module):
         Args:
             embed_dim: Dimensionality of input and attention feature vectors
             hidden_dim: Dimensionality of hidden layer in feed-forward network
-                         (usually 2-4x larger than embed_dim)
             num_heads: Number of heads to use in the Multi-Head Attention block
             dropout: Amount of dropout
         """
@@ -45,20 +45,18 @@ class VisionEncoder(nn.Module):
                 patch_size,
                 num_patches,
                 dropout=0.0,):
-        """Vision Transformer.
+        """Vision Encoder.
 
         Args:
-            embed_dim: Dimensionality of the input feature vectors to the Transformer
-            hidden_dim: Dimensionality of the hidden layer in the feed-forward networks
-                         within the Transformer
+            embed_dim: Dimensionality of the input feature vectors
+            hidden_dim: Dimensionality of the hidden layer
             num_channels: Number of channels of the input
             num_heads: Number of heads to use in the Multi-Head Attention block
             num_layers: Number of layers to use in the Encoder
             num_classes: Number of classes to predict
             patch_size: Number of pixels that the patches have per dimension
             num_patches: Maximum number of patches an image can have
-            dropout: Amount of dropout to apply in the feed-forward network and
-                      on the input encoding
+            dropout: Amount of dropout to apply in the feed-forward network
         """
         super().__init__()
 
@@ -67,16 +65,10 @@ class VisionEncoder(nn.Module):
         self.hidden_dim = hidden_dim
         self.num_heads = num_heads
         self.num_layers = num_layers
-
-        #! could be an issue
         self.num_dropout = dropout
 
         # Layers/Networks
         self.input_layer = nn.Linear(num_channels * (patch_size**2), embed_dim)
-
-        # self.transformer = nn.Sequential(
-        #     *(AttentionBlock(embed_dim, hidden_dim, num_heads, dropout=dropout) for _ in range(num_layers)))
-
         self.encoder = self._make_layers()
         self.mlp_head = nn.Sequential(nn.LayerNorm(embed_dim), nn.Linear(embed_dim, num_classes))
         self.dropout = nn.Dropout(dropout)
@@ -101,6 +93,7 @@ class VisionEncoder(nn.Module):
         B, T, _ = x.shape
         x = self.input_layer(x)
 
+        #!maybe make positional encoding to sin and cos func
         # Add CLS token and positional encoding
         cls_token = self.cls_token.repeat(B, 1, 1)
         x = torch.cat([cls_token, x], dim=1)
