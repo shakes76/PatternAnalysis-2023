b8a29e9244f79a17fc3d14e335750088c3e5281a
4vir4l <iaviral.sh@gmail.com>
2023-10-24 17:15:26 +1000

    added class: conv2d weightmodulate

--- recognition/StyleGAN2-OASIS_45711110/modules.py
+++ recognition/StyleGAN2-OASIS_45711110/modules.py
@@ -136,3 +136,36 @@ class ToRGB(nn.Module):
         return self.activation(x + self.bias[None, :, None, None])
 
 #TODO: Conv2dWeightModulate
+class Conv2dWeightModulate(nn.Module):
+
+    def __init__(self, in_features, out_features, kernel_size,
+                 demodulate = True, eps = 1e-8):
+
+        super().__init__()
+        self.out_features = out_features
+        self.demodulate = demodulate
+        self.padding = (kernel_size - 1) // 2
+
+        self.weight = EqualizedWeight([out_features, in_features, kernel_size, kernel_size])
+        self.eps = eps
+
+    def forward(self, x, s):
+
+        b, _, h, w = x.shape
+
+        s = s[:, None, :, None, None]
+        weights = self.weight()[None, :, :, :, :]
+        weights = weights * s
+
+        if self.demodulate:
+            sigma_inv = torch.rsqrt((weights ** 2).sum(dim=(2, 3, 4), keepdim=True) + self.eps)
+            weights = weights * sigma_inv
+
+        x = x.reshape(1, -1, h, w)
+
+        _, _, *ws = weights.shape
+        weights = weights.reshape(b * self.out_features, *ws)
+
+        x = F.conv2d(x, weights, padding=self.padding, groups=b)
+
+        return x.reshape(-1, self.out_features, h, w)
