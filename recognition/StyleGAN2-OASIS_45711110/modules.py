dff7a4dac2879ff704e951705155b10a11e90fce
4vir4l <iaviral.sh@gmail.com>
2023-10-24 17:39:30 +1000

    added class: EqualizedConv2d

--- recognition/StyleGAN2-OASIS_45711110/modules.py
+++ recognition/StyleGAN2-OASIS_45711110/modules.py
@@ -232,4 +232,15 @@ class DiscriminatorBlock(nn.Module):
 
         return (x + residual) * self.scale
     
-#TODO: qualizedConv2d
+class EqualizedConv2d(nn.Module):
+
+    def __init__(self, in_features, out_features,
+                 kernel_size, padding = 0):
+
+        super().__init__()
+        self.padding = padding
+        self.weight = EqualizedWeight([out_features, in_features, kernel_size, kernel_size])
+        self.bias = nn.Parameter(torch.ones(out_features))
+
+    def forward(self, x: torch.Tensor):
+        return F.conv2d(x, self.weight(), bias=self.bias, padding=self.padding)
