2440a3a202c99bb4751de5c5315cf123488eb721
4vir4l <iaviral.sh@gmail.com>
2023-10-24 15:45:32 +1000

    added class: generatorBlock

--- recognition/StyleGAN2-OASIS_45711110/modules.py
+++ recognition/StyleGAN2-OASIS_45711110/modules.py
@@ -82,3 +82,22 @@ class Generator(nn.Module):
             rgb = F.interpolate(rgb, scale_factor=2, mode="bilinear") + rgb_new
 
         return torch.tanh(rgb)
+
+class GeneratorBlock(nn.Module):
+
+    def __init__(self, W_DIM, in_features, out_features):
+        super().__init__()
+
+        self.style_block1 = StyleBlock(W_DIM, in_features, out_features)
+        self.style_block2 = StyleBlock(W_DIM, out_features, out_features)
+
+        self.to_rgb = ToRGB(W_DIM, out_features)
+
+    def forward(self, x, w, noise):
+        x = self.style_block1(x, w, noise[0])
+        x = self.style_block2(x, w, noise[1])
+        rgb = self.to_rgb(x, w)
+
+        return x, rgb
+    
+#TODO: styleblock, toRGB
