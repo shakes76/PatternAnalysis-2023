5d1d40b51476567f8d3e16975401f49f02353be3
4vir4l <iaviral.sh@gmail.com>
2023-10-24 15:40:40 +1000

    added class: generator module

--- recognition/StyleGAN2-OASIS_45711110/modules.py
+++ recognition/StyleGAN2-OASIS_45711110/modules.py
@@ -52,3 +52,33 @@ class EqualizedWeight(nn.Module):
 
     def forward(self):
         return self.weight * self.c
+
+class Generator(nn.Module):
+
+    def __init__(self, log_resolution, W_DIM, n_features = 32, max_features = 256):
+        super().__init__()
+
+        features = [min(max_features, n_features * (2 ** i)) for i in range(log_resolution - 2, -1, -1)]
+        self.n_blocks = len(features)
+
+        self.initial_constant = nn.Parameter(torch.randn((1, features[0], 4, 4)))
+
+        self.style_block = StyleBlock(W_DIM, features[0], features[0])
+        self.to_rgb = ToRGB(W_DIM, features[0])
+
+        blocks = [GeneratorBlock(W_DIM, features[i - 1], features[i]) for i in range(1, self.n_blocks)]
+        self.blocks = nn.ModuleList(blocks)
+
+    def forward(self, w, input_noise):
+        batch_size = w.shape[1]
+
+        x = self.initial_constant.expand(batch_size, -1, -1, -1)
+        x = self.style_block(x, w[0], input_noise[0][1])
+        rgb = self.to_rgb(x, w[0])
+
+        for i in range(1, self.n_blocks):
+            x = F.interpolate(x, scale_factor=2, mode="bilinear")
+            x, rgb_new = self.blocks[i - 1](x, w[i], input_noise[i])
+            rgb = F.interpolate(rgb, scale_factor=2, mode="bilinear") + rgb_new
+
+        return torch.tanh(rgb)
