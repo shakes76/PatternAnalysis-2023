4e1b5d252381d67203fd3850d8364c342201234a
4vir4l <iaviral.sh@gmail.com>
2023-10-24 17:27:12 +1000

    implemented Discriminator

--- recognition/StyleGAN2-OASIS_45711110/modules.py
+++ recognition/StyleGAN2-OASIS_45711110/modules.py
@@ -135,7 +135,6 @@ class ToRGB(nn.Module):
         x = self.conv(x, style)
         return self.activation(x + self.bias[None, :, None, None])
 
-#TODO: Conv2dWeightModulate
 class Conv2dWeightModulate(nn.Module):
 
     def __init__(self, in_features, out_features, kernel_size,
@@ -169,3 +168,40 @@ class Conv2dWeightModulate(nn.Module):
         x = F.conv2d(x, weights, padding=self.padding, groups=b)
 
         return x.reshape(-1, self.out_features, h, w)
+    
+class Discriminator(nn.Module):
+
+    def __init__(self, log_resolution, n_features = 64, max_features = 256):
+        super().__init__()
+
+        features = [min(max_features, n_features * (2 ** i)) for i in range(log_resolution - 1)]
+
+        self.from_rgb = nn.Sequential(
+            EqualizedConv2d(3, n_features, 1),
+            nn.LeakyReLU(0.2, True),
+        )
+        n_blocks = len(features) - 1
+        blocks = [DiscriminatorBlock(features[i], features[i + 1]) for i in range(n_blocks)]
+        self.blocks = nn.Sequential(*blocks)
+
+        final_features = features[-1] + 1
+        self.conv = EqualizedConv2d(final_features, final_features, 3)
+        self.final = EqualizedLinear(2 * 2 * final_features, 1)
+
+    def minibatch_std(self, x):
+        batch_statistics = (
+            torch.std(x, dim=0).mean().repeat(x.shape[0], 1, x.shape[2], x.shape[3])
+        )
+        return torch.cat([x, batch_statistics], dim=1)
+
+    def forward(self, x):
+
+        x = self.from_rgb(x)
+        x = self.blocks(x)
+
+        x = self.minibatch_std(x)
+        x = self.conv(x)
+        x = x.reshape(x.shape[0], -1)
+        return self.final(x)
+    
+#TODO: discriminatorBlock and EqualizedConv2d
