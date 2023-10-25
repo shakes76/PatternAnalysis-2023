243796bd3375c007d41b7e93d64f0f06fe3963fd
Yicheng Jia <128900685+YichengJia@users.noreply.github.com>
2023-10-26 00:49:13 +1000

    Update predict.py

--- recognition/Siamese Network_Yicheng Jia/predict.py
+++ recognition/Siamese Network_Yicheng Jia/predict.py
@@ -1,6 +1,7 @@
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
+from tqdm import tqdm
 
 import dataset
 from modules import SiameseNetwork
@@ -13,11 +14,7 @@ def write_to_file(file_path, content):
 
 def check_CUDA():
     # Check if CUDA is available and set the device accordingly
-    if torch.cuda.is_available():
-        current_device = torch.device("cuda")
-        torch.cuda.empty_cache()
-    else:
-        current_device = torch.device("cpu")
+    current_device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     return current_device
 
 
@@ -30,30 +27,31 @@ def read_from_checkpoint(current_model_pth: str):
     return current_checkpoint, current_training_epoch
 
 
-def predict_progress(current_device,  dataloader):
-    predicted_loss = 0.0
+def predict_progress(network, current_device,  dataloader):
+    network.eval()
+    predicted_total_loss = 0.0
     predicted_accuracy = 0.0
     predicted_total_labels = 0.0
     print("Start testing!")
-    for index, image_data in enumerate(dataloader):
-        img_0, img_1, current_image_label = image_data
-        img_0, img_1, current_image_label = img_0.to(current_device), img_1.to(current_device), current_image_label.to(current_device)
+    # Progress bar
+    for index, image_data in tqdm(enumerate(dataloader), total=len(dataloader)):
+        img_0, img_1, image_label = image_data
+        img_0, img_1, image_label = img_0.to(current_device), img_1.to(current_device), image_label.to(current_device)
         with torch.no_grad():
-            outputs = net(img_0, img_1)
-            outputs = outputs.squeeze()
-            loss_values = criterion(outputs, current_image_label)
+            predicted_total_outputs = net(img_0, img_1).squeeze()
+            predicted_loss = criterion(predicted_total_outputs, image_label)
 
-        predicted_label = (outputs > 0.5).float()
-        predicted_total_labels += current_image_label.size(0)
-        predicted_accuracy += (predicted_label == current_image_label).sum().item()
-        predicted_loss += loss_values.item()
+            predicted_label = torch.where(predicted_total_outputs > 0.5, torch.tensor(1, device=device), torch.tensor(0, device=device))
+            predicted_total_labels += len(image_label)
+            predicted_accuracy += predicted_label.eq(image_label.view_as(predicted_label)).sum().item()
+            predicted_total_loss += predicted_loss.item()
 
     print("Testing finished, printing results...")
 
-    test_average_loss = predicted_loss / len(dataloader)
+    predict_average_loss = predicted_total_loss / len(dataloader)
     accuracy = predicted_accuracy / predicted_total_labels * 100
 
-    print(f"Test_Average_Loss:{test_average_loss:.4f},Test_Accuracy:{accuracy:.4f}")
+    print(f"Test_Average_Loss:{predict_average_loss:.4f},Test_Accuracy:{accuracy:.4f}")
 
 
 if __name__ == '__main__':
@@ -63,13 +61,12 @@ if __name__ == '__main__':
 
     # Define the loss function
     criterion = nn.BCELoss()
-    net.eval()
-
-    # Create data loaders for training and testing datasets AFTER re-instantiating train_dataset
-    current_train_dir, test_dir, current_train_dataset, test_dataset = dataset.transform_directory()
-    test_dataloader = DataLoader(test_dataset, shuffle=True, num_workers=4, batch_size=32)
+    ROOT_DIR = "AD_NC"
+    # Create data loaders for training and testing datasets
+    current_train_dataset, test_dataset = dataset.get_datasets(ROOT_DIR)
+    test_dataloader = DataLoader(test_dataset, shuffle=True, num_workers=4, batch_size=256)
 
     # read the model from the checkpoint of the previous module
-    checkpoint, current_epoch = read_from_checkpoint('module_8.pth') # Please usethe pth file you prefre after running train.py.
+    checkpoint, current_epoch = read_from_checkpoint('module_best_test_accuracy.pth')
 
-    predict_progress(device, test_dataloader)
+    predict_progress(net, device, test_dataloader)
