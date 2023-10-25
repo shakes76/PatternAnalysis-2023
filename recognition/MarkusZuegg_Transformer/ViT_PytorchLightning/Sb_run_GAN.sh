771b31c600b19c3d8faf710c2cd969288c143c20
Markus Zuegg <markuszuegg@outlook.com>
2023-10-26 03:11:35 +1000

    sbatch script

---
+++ recognition/MarkusZuegg_Transformer/ViT_PytorchLightning/Sb_run_GAN.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+#SBATCH --time=0-03:00:00
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=1
+#SBATCH --cpus-per-task 4
+#SBATCH --gres=gpu:1
+#SBATCH --partition=vgpu
+#SBATCH --job-name="ViT"
+# SBATCH --mail-user=s4744924@student.uq.edu.au
+# SBATCH --mail-type=BEGIN
+#SBATCH --mail-type=END
+#SBATCH --mail-type=FAIL
+#SBATCH --output=x%j.out
+#SBATCH --error=x%j.err
+
+module load cuda
+conda activate pytorch2
+
+srun python train.py
