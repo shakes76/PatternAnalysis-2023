5fcdbbf9830f0f013f8811b70c3c40cd899311c9
MarkusZuegg <markuszuegg@outlook.com>
2023-10-27 04:10:42 +1000

    n tasks per cpu = 1

--- recognition/MarkusZuegg_Transformer/ViT_PytorchLightning/Sb_run_ViT.sh
+++ recognition/MarkusZuegg_Transformer/ViT_PytorchLightning/Sb_run_ViT.sh
@@ -3,7 +3,7 @@
 #SBATCH --time=0-05:00:00
 #SBATCH --nodes=1
 #SBATCH --ntasks-per-node=1
-#SBATCH --cpus-per-task 4
+#SBATCH --cpus-per-task 1
 #SBATCH --gres=gpu:1
 #SBATCH --partition=vgpu
 #SBATCH --job-name="ViT"
