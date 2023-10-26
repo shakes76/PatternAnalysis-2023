401d15586675400054f8562adf9ae103e31400ac
pragun <prgun383@gmail.com>
2023-10-27 03:28:41 +1000

    Added code for validating pcnn, plotting quantizations and generating samples

--- recognition/VQVAE_s4594975/predict.py
+++ recognition/VQVAE_s4594975/predict.py
@@ -2,6 +2,8 @@ from modules import *
 from train import *
 from dataset import *
 import tensorflow as tf
+import numpy as np
+import tensorflow_probability 
 from tensorflow import keras
 from keras.models import load_model
 from tensorflow.image import convert_image_dtype, ssim
@@ -10,14 +12,18 @@ from matplotlib.pyplot import suptitle, imshow, subplot, axis, show, cm, title
 custom_objects = {
     "VectorQuantizer": VectorQuantizer
 }
-custom_objects_pcnn = {"PixelConvolution": PixelLayer, "ResidualBlock": ResidualBlock}
-
 with keras.utils.custom_object_scope(custom_objects):
-    vqvae = load_model("/Users/pc/Documents/COMP3710/VQVAE.h5")
+    vqvae = load_model("VQVAE.h5")
 
-with keras.utils.custom_object_scope(custom_objects_pcnn):
-    pcnn = load_model("/Users/pc/Documents/COMP3710/PCNN.h5") 
+custom_objects = {
+    "PixelConvolution": PixelLayer,
+    "ResidualBlock": ResidualBlock
+    }
+#with keras.utils.custom_object_scope(custom_objects):
+#    pcnn = load_model("PCNN.h5") 
 
+select = np.random.choice(len(x_test_scaled), 10)
+images =  x_test_scaled[select]
 
 def calculate_ssim(images, recons):
     sum = 0
@@ -40,15 +46,67 @@ def calculate_ssim(images, recons):
     print(sum / len(recons))
 
 def test_vqvae():
-    select = np.random.choice(len(x_test_scaled), 1000)
-    images =  x_test_scaled[select]
     reconstructed = vqvae.predict(images)
     ssim = calculate_ssim(images, reconstructed)
     return 0
 
+def show_codes(codebook):
+    
+    for i in range(len(images)):
+    
+        plt.subplot(1, 2, 1)
+        plt.imshow(images[i].squeeze() + 0.5, cmap=plt.cm.gray)
+        plt.title("Original")
+        plt.axis("off")
+
+        plt.subplot(1, 2, 2)
+        plt.imshow(codebook[i], cmap=plt.cm.gray)
+        plt.title("Code")
+        plt.axis("off")
+        plt.show()
+
+def generate(quantizer, priors, output_enco):
+    pretrained_embeddings = quantizer.embeddings
+    priors_ohe = tf.one_hot(priors.astype("int32"), vqvae.num_embeddings).numpy()
+    quantized = tf.matmul(priors_ohe.astype("float32"), pretrained_embeddings, transpose_b=True)
+    quantized = tf.reshape(quantized, (-1, *(output_enco.shape[1:])))
+
+    # Generate novel images.
+    decoder = vqvae.get_layer("decoder")
+    generated_samples = decoder.predict(quantized)
+
+    for i in range(10):
+        plt.subplot(1, 2, 1)
+        plt.imshow(priors[i], cmap = plt.cm.gray)
+        plt.title("Code")
+        plt.axis("off")
+
+        plt.subplot(1, 2, 2)
+        plt.imshow(generated_samples[i].squeeze() + 0.5, cmap = plt.cm.gray)
+        plt.title("Generated Sample")
+        plt.axis("off")
+        plt.show()
+
 def test_pcnn():
+    priors	= np.zeros(shape = (10,) + (pcnn.input_shape)[1:])
+    batch, rows, cols = priors.shape
+
+    for row in range(rows):
+      for col in range(cols):
+          logits = pcnn.predict(priors)
+          sampler = tensorflow_probability.distributions.Categorical(logits)
+          probs = sampler.sample()
+          priors[:, row, col] = probs[:, row, col]
+
+    encoder	= vqvae.get_layer("encoder")
+    quantizer = vqvae.get_layer("quantizer")
+    output_enco	= encoder.predict(x_test_scaled)
+    codebook = quantizer.get_code_indices(output_enco.reshape(-1, output_enco.shape[-1]))
+    codebook = codebook.numpy().reshape(output_enco.shape[:-1])
+    show_codes(codebook)
+    generate(quantizer, priors, output_enco)
     return 0
 
 if __name__ == "__main__":
     test_vqvae()
-    test_pcnn()
+    #test_pcnn()
