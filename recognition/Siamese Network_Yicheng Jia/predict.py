7166ed1db5b59b29f6437a066836a9410f31970a
Yicheng Jia <128900685+YichengJia@users.noreply.github.com>
2023-10-26 23:15:08 +1000

    Finalise predict.py

--- recognition/Siamese Network_Yicheng Jia/predict.py
+++ recognition/Siamese Network_Yicheng Jia/predict.py
@@ -3,7 +3,7 @@
     Author: Yicheng Jia
     Date created: 27/09/2023
     Date last modified: 26/10/2023
-    Python Version: 3.10.12
+    Python Version: 3.11.04
 """
 
 
@@ -14,11 +14,6 @@ from tqdm import tqdm
 
 import dataset
 from modules import SiameseNetwork
-
-
-def write_to_file(file_path, content):
-    with open(file_path, 'a') as file:
-        file.write(content + '\n')
 
 
 def check_CUDA():
@@ -36,31 +31,40 @@ def read_from_checkpoint(current_model_pth: str):
     return current_checkpoint, current_training_epoch
 
 
-def predict_progress(network, current_device,  dataloader):
-    network.eval()
-    predicted_total_loss = 0.0
-    predicted_accuracy = 0.0
-    predicted_total_labels = 0.0
-    print("Start testing!")
-    # Progress bar
-    for index, image_data in tqdm(enumerate(dataloader), total=len(dataloader)):
-        img_0, img_1, image_label = image_data
-        img_0, img_1, image_label = img_0.to(current_device), img_1.to(current_device), image_label.to(current_device)
-        with torch.no_grad():
-            predicted_total_outputs = net(img_0, img_1).squeeze()
-            predicted_loss = criterion(predicted_total_outputs, image_label)
+def predict_progress(predicted_module, current_device, predicted_loop_dataloader):
+    # Predict Loop
+    predicted_module.eval()
 
-            predicted_label = torch.where(predicted_total_outputs > 0.5, torch.tensor(1, device=device), torch.tensor(0, device=device))
+    # Initial values
+    predicted_total_losses = 0.0
+    predicted_correct_numbers = 0.0
+    predicted_total_labels = 0.0
+    print("Start predicting!")
+
+    # Loss function
+    criterion = nn.BCELoss()
+
+    # Progress bar
+    with torch.no_grad():
+        for index, image_data in tqdm(enumerate(predicted_loop_dataloader), total=len(predicted_loop_dataloader)):
+            img_0, img_1, image_label = image_data  # Get the images and labels from the data
+            img_0, img_1, image_label = img_0.to(current_device), img_1.to(current_device), image_label.to(current_device)  # Move the images and labels to the appropriate device
+
+            predicted_total_outputs = predicted_module(img_0, img_1).squeeze()  # Adjust the size of the output to match the target
+            predicted_loss = criterion(predicted_total_outputs, image_label)  # Get the loss
+
+            # Calculate the accuracy
+            predicted_label = torch.where(predicted_total_outputs > 0.5, torch.tensor(1, device=current_device), torch.tensor(0, device=current_device))
             predicted_total_labels += len(image_label)
-            predicted_accuracy += predicted_label.eq(image_label.view_as(predicted_label)).sum().item()
-            predicted_total_loss += predicted_loss.item()
+            predicted_correct_numbers += predicted_label.eq(image_label.view_as(predicted_label)).sum().item()
+            predicted_total_losses += predicted_loss.item()
 
     print("Testing finished, printing results...")
 
-    predict_average_loss = predicted_total_loss / len(dataloader)
-    accuracy = predicted_accuracy / predicted_total_labels * 100
+    predict_average_loss = predicted_total_losses / len(predicted_loop_dataloader)
+    accuracy = 100. * predicted_correct_numbers / predicted_total_labels
 
-    print(f"Test_Average_Loss:{predict_average_loss:.4f},Test_Accuracy:{accuracy:.4f}")
+    print(f"Test_Average_Loss:{predict_average_loss:.4f},Test_Accuracy:{accuracy:.4f}%")
 
 
 if __name__ == '__main__':
@@ -68,11 +72,9 @@ if __name__ == '__main__':
     device = check_CUDA()
     net = SiameseNetwork().to(device)
 
-    # Define the loss function
-    criterion = nn.BCELoss()
     ROOT_DIR = "AD_NC"
     # Create data loaders for training and testing datasets
-    current_train_dataset, test_dataset = dataset.get_datasets(ROOT_DIR)
+    current_train_dataset, test_dataset, _ = dataset.get_datasets(ROOT_DIR)
     test_dataloader = DataLoader(test_dataset, shuffle=True, num_workers=4, batch_size=256)
 
     # read the model from the checkpoint of the previous module
