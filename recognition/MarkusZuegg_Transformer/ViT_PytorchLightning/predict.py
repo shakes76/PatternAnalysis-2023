b09f97065ccf1a5c7d6b8b5e48f2028d3c41e4e5
MarkusZuegg <markuszuegg@outlook.com>
2023-10-27 06:16:10 +1000

    fixed simple mis named variable

--- recognition/MarkusZuegg_Transformer/ViT_PytorchLightning/predict.py
+++ recognition/MarkusZuegg_Transformer/ViT_PytorchLightning/predict.py
@@ -8,15 +8,14 @@ from train import ViT
 # Set up dir of checkpoint
 root_dir = './checkpoints/'
 # Enter file name below:
-file_name = 'ViT_ckpt'
+file_name = 'ViT.ckpt'
 
 def main():
     checkpoint = root_dir + file_name
+    print(checkpoint)
 
     # Initialise trainer
-    trainer = pl.Trainer(max_epochs=1,
-                        accelerator='gpu',
-                        devices=1)
+    trainer = pl.Trainer(max_epochs=1)
     
     # Initialise dataset
     ADNI = ADNIDataModule(batch_size=32, num_workers=0, image_size=[256,240])
@@ -26,5 +25,6 @@ def main():
     
     # Test model
     trainer.test(model, ADNI)
+    print('Test done')
 
 if __name__ == '__main__': main()
