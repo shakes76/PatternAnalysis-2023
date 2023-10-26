a0d80de9b808b4ed7125b69e48861514ef98bb7f
Noah Smyth <noah.smyth@hotmail.com>
2023-10-27 00:56:15 +1000

    Gave the problem directory a meaningful name

---
+++ recognition/UNet_Segmentation_s4745275/slurm.sh
@@ -0,0 +1,13 @@
+#!/bin/bash 
+#SBATCH --nodes=1 
+#SBATCH --ntasks=1 
+#SBATCH --gres=gpu:1 
+#SBATCH --partition=vgpu 
+#SBATCH --job-name="COMP3710 Report"
+#SBATCH --mail-user=s4745275@student.uq.edu.au
+#SBATCH --mail-type=ALL 
+#SBATCH -e test_err.txt
+#SBATCH -o test_out.txt
+
+source /home/Student/s4745275/miniconda/bin/activate /home/Student/s4745275/my_demo_environment
+python train.py
