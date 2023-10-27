5f8814c0430eb10d50a76d3d906420149d317dc2
4vir4l <iaviral.sh@gmail.com>
2023-10-27 18:16:49 +1000

    implementated training fn

--- recognition/StyleGAN2-OASIS_45711110/train.py
+++ recognition/StyleGAN2-OASIS_45711110/train.py
@@ -1,1 +1,63 @@
 "Source code for training, validating, testing and saving the model"
+
+import torch
+import tqdm
+
+from utils import get_w, get_noise, gradient_penalty
+import tqdm
+
+from config import device, lambda_gp
+from modules import MappingNetwork
+from utils import get_w, get_noise, gradient_penalty
+
+def train_fn(
+    critic,
+    gen,
+    path_length_penalty,
+    loader,
+    opt_critic,
+    opt_gen,
+    opt_mapping_network,
+):
+    loop = tqdm(loader, leave=True)
+
+    for batch_idx, (real, _) in enumerate(loop):
+        real = real.to(device)
+        cur_batch_size = real.shape[0]
+
+        w     = get_w(cur_batch_size)
+        noise = get_noise(cur_batch_size)
+        with torch.cuda.amp.autocast():
+            fake = gen(w, noise)
+            critic_fake = critic(fake.detach())
+            
+            critic_real = critic(real)
+            gp = gradient_penalty(critic, real, fake, device=device)
+            loss_critic = (
+                -(torch.mean(critic_real) - torch.mean(critic_fake))
+                + lambda_gp * gp
+                + (0.001 * torch.mean(critic_real ** 2))
+            )
+
+        critic.zero_grad()
+        loss_critic.backward()
+        opt_critic.step()
+
+        gen_fake = critic(fake)
+        loss_gen = -torch.mean(gen_fake)
+
+        if batch_idx % 16 == 0:
+            plp = path_length_penalty(w, fake)
+            if not torch.isnan(plp):
+                loss_gen = loss_gen + plp
+
+        MappingNetwork.zero_grad()
+        gen.zero_grad()
+        loss_gen.backward()
+        opt_gen.step()
+        opt_mapping_network.step()
+
+        loop.set_postfix(
+            gp=gp.item(),
+            loss_critic=loss_critic.item(),
+        )
