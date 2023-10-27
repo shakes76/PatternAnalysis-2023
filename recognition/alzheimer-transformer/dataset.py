223a19cd425711aa1bee662401a20247ff396306
ZezAbkin <henryabkin@gmail.com>
2023-10-27 18:52:47 +1000

    Adds better comments and references

--- recognition/alzheimer-transformer/dataset.py
+++ recognition/alzheimer-transformer/dataset.py
@@ -1,14 +1,12 @@
 '''
-dataset.py: contains the data loader for loading and preprocessing the data
+contains the data loader for loading and preprocessing the data
 '''
-
 from torchvision.datasets import ImageFolder
 from torchvision import transforms
 import os
 from sklearn.model_selection import train_test_split
 from torch.utils.data import DataLoader, Subset, SubsetRandomSampler
 import torch
-import matplotlib.pyplot as plt
 
 '''
 Returns a list of unique patient IDs given the dataset path
@@ -38,7 +36,7 @@ def compute_mean_std(loader):
 '''
 Returns the train, val, and test dataloaders for the AD_NC dataset, with a train/val split of 80/20
 '''
-def get_alzheimer_dataloader(batch_size:int=32, img_size:int=224, path:str="/content/ADNC-Dataset/AD_NC"):
+def get_alzheimer_dataloader(batch_size:int=32, img_size:int=224, path:str="/home/groups/comp3710/ADNI/AD_NC"):
     train_data_path = path+"/train"
     test_data_path = path+"/test"
 
@@ -54,11 +52,10 @@ def get_alzheimer_dataloader(batch_size:int=32, img_size:int=224, path:str="/con
     sampler = SubsetRandomSampler(torch.randperm(len(pre_train_dataset))[:n_samples])
     sample_loader = DataLoader(pre_train_dataset, batch_size=batch_size, sampler=sampler)
 
-    # Compute mean and std for 1000 samples in dataset
+    # Compute mean and std for the sample dataset
     mean, std = compute_mean_std(sample_loader)
 
-    # transformers with normalization as calculated above
-    # Random rotations and random sized crop added
+    # Transformers WITH normalization
     train_transforms = transforms.Compose([
         transforms.Resize((img_size, img_size)),
         transforms.RandomRotation(10),  # Random rotations in range [-10, 10]
@@ -66,28 +63,28 @@ def get_alzheimer_dataloader(batch_size:int=32, img_size:int=224, path:str="/con
         transforms.ToTensor(),
         transforms.Normalize(mean=mean, std=std)
     ])
+
     test_transforms = transforms.Compose([
         transforms.Resize((img_size, img_size)),
         transforms.ToTensor(),
         transforms.Normalize(mean=mean, std=std)
     ])
 
-    # get patient ids from training data path and split these with 80/20
+    # Get patient ids from training data path and split these with 80/20
     patient_ids = get_patient_ids(train_data_path)
     train_patient_ids, val_patient_ids = train_test_split(patient_ids, test_size=0.2, random_state=42)
 
-    # create datasets
+    # Create datasets
     train_dataset = ImageFolder(root=train_data_path, transform=train_transforms)
     test_dataset = ImageFolder(root=test_data_path, transform=test_transforms)
 
-    # map ID split to the images to get patient-level split
+    # Map ID split to the images to get patient-level split
     train_indices = [i for i, (path, label) in enumerate(train_dataset.samples) if path.split('/')[-1].split('_')[0] in train_patient_ids]
     val_indices = [i for i, (path, label) in enumerate(train_dataset.samples) if path.split('/')[-1].split('_')[0] in val_patient_ids]
 
     train_subset = Subset(train_dataset, train_indices)
     val_subset = Subset(train_dataset, val_indices)
-
-    # create data loaders
+    # Create data loaders
     train_loader = DataLoader(train_subset, batch_size=batch_size, shuffle=True, num_workers=4)
     val_loader = DataLoader(val_subset, batch_size=batch_size, shuffle=False, num_workers=4)
     test_loader = DataLoader(test_dataset, batch_size=batch_size, shuffle=False, num_workers=4)
--- recognition/alzheimer-transformer/modules.py
+++ recognition/alzheimer-transformer/modules.py
@@ -5,15 +5,11 @@ implementated as a class or a function
 
 import torch
 import torch.nn as nn
-import torch.optim as optim
-import torchvision
-import matplotlib.pyplot as plt
-from torchinfo import summary
 
 class PatchEmbedding(nn.Module):
     """
     Takes a 2D image and turns it into an input for the tranformer by flattening it
-    into a 1D sequence learnable embedding vector as described in the paper 
+    into a 1D sequence learnable embedding vector as described in (Dosovitskiy et al., 2020)
 
     Args:
         in_channels (int): Number of colour channels. In the case of the ADNC, there are 3 (RGB)
@@ -21,7 +17,7 @@ class PatchEmbedding(nn.Module):
 
         patch_size (int): Size of patches (pixels) that the image will be converted into
             -> default = 16
-        
+
         embedding_dim (int): Size of embedding vector for each patch
             -> default = 768
     """
@@ -39,11 +35,10 @@ class PatchEmbedding(nn.Module):
                                  stride=patch_size,
                                  padding=0)
 
-        # Flattens the patch feature maps to 1D 
+        # Flattens the patch feature maps to 1D
         # Only flatten the feature map dimension
         self.flatten = nn.Flatten(start_dim=2, end_dim=3)
 
-    # Forward Method
     def forward(self, x):
         x_patched = self.patcher(x)
         x_flattened = self.flatten(x_patched)
@@ -51,10 +46,10 @@ class PatchEmbedding(nn.Module):
 
 
 
-"""
-
-"""
 class ViT(nn.Module):
+    """
+    The main class for the Vision Transformer model
+    """
     def __init__(self,
                  model_type:str='base',
                  num_classes:int=2,
@@ -62,27 +57,28 @@ class ViT(nn.Module):
                  num_channels:int=3,
                  patch_size:int=16,
                  dropout:float=0.1):
-        
+
         super().__init__()
 
+        #3 types of model sizes according to (Dosovitskiy et al., 2020)
         if model_type == 'base':
             n_layers = 12
             hidden_D = 768
             mlp_size = 3072
             n_heads = 12
-        
+
         elif model_type == 'large':
             n_layers = 24
             hidden_D = 1024
             mlp_size = 4096
             n_heads = 16
-        
+
         elif model_type == 'huge':
             n_layers = 32
             hidden_D = 1280
             mlp_size = 5120
             n_heads = 16
-        
+
         else:
             raise Exception("Model type must be one of: base, large, huge")
 
@@ -99,23 +95,23 @@ class ViT(nn.Module):
         # CLASS TOKEN #
         #-------------#
         self.class_token = nn.Parameter(torch.randn(1, 1, hidden_D), requires_grad=True)
-        
+
         #----------------------#
         # POSITIONAL EMBEDDING #
         #----------------------#
         num_patches = (img_size//patch_size)**2
         self.positional_embedding = nn.Parameter(torch.randn(1, num_patches+1, hidden_D))
-        
+
         #------------------------------------#
         # DROPOUT AFTER POSITIONAL EMBEDDING #
         #------------------------------------#
-        # according to appendix B.1 in the paper, dropout is used directly after adding positional to patch embeddings
+        # according to appendix B.1 in (Dosovitskiy et al., 2020), dropout is used directly after adding positional to patch embeddings
         self.embedding_dropout = nn.Dropout(p=dropout)
 
         #-----------------------------#
         #  TRANSFORMER ENCODER STACK
         #-----------------------------#
-        # this is to resemble the transformer enoder in figure 1 of the paper
+        # this is to resemble the transformer enoder in figure 1 of (Dosovitskiy et al., 2020)
 
         self.transformer_encoder = nn.TransformerEncoder(
             encoder_layer=nn.TransformerEncoderLayer(
@@ -123,17 +119,17 @@ class ViT(nn.Module):
                 nhead=n_heads,
                 dim_feedforward=mlp_size,
                 dropout=dropout,
-                activation='gelu', # as per the paper
+                activation='gelu', # as per (Dosovitskiy et al., 2020)
                 batch_first=True, # since the batch dimension is first in patch_embedding
-                norm_first=True), # according to the tranformer encoder diagram in figure 1 of the paper,
+                norm_first=True), # according to the tranformer encoder diagram in figure 1 of (Dosovitskiy et al., 2020),
             num_layers=n_layers)
-        
+
         #----------#
         # MLP HEAD #
         #----------#
 
         self.mlp_head = nn.Sequential(
-            nn.LayerNorm(normalized_shape=hidden_D), # from eq.4 of paper
+            nn.LayerNorm(normalized_shape=hidden_D), # from eq.4 of (Dosovitskiy et al., 2020)
             nn.Linear(in_features=hidden_D,
                       out_features=num_classes)
         )
@@ -141,14 +137,14 @@ class ViT(nn.Module):
 
     def forward(self, x):
         batch_size = x.shape[0]
-        
+
         # make patch embedding
         x = self.patch_embedding(x)
 
         # stretch class token across whole batch size
         class_token = self.class_token.expand(batch_size, -1, -1)
 
-        # concat class token to make it part of patch embedding (see X_class in eq.1)
+        # concat class token to make it part of patch embedding (see X_class in eq.1 of Dosovitskiy et al., 2020)
         x = torch.cat((class_token, x), dim=1)
 
         # add positional embedding to x
@@ -160,7 +156,7 @@ class ViT(nn.Module):
         # input into trasnformer encoder stack
         x = self.transformer_encoder(x)
 
-        # input only the 0th index through to the head 
+        # input only the 0th index through to the head
         x = self.mlp_head(x[:, 0])
 
         return x
