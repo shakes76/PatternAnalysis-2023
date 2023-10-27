f10c0a935620d70745acd854e551f97d12d08310
dcpais <dpaismail@gmail.com>
2023-10-28 04:35:29 +1000

    added predicting random images to model in predict.py

---
+++ recognition/s46419459 ADNI Dataset Perceiver Transformer/predict.py
@@ -0,0 +1,76 @@
+from torchvision import transforms
+import torch.nn.functional as F
+import matplotlib.pyplot as plt
+from matplotlib import cycler
+
+from PIL import Image
+import os
+import random as rand
+
+from modules import *
+
+#path = r"C:\Users\dcp\Documents\OFFLINE-Projects\DATASETS\ADNI"  # Laptop Path
+path = r"C:\Users\deepp\Documents\Offline Projects\ML Datasets\ADNI" # PC path
+
+# Model Parameters
+image_shape = (240, 240)
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+d_latent = 128
+embed_dim = 64
+transformer_depth = 1
+num_heads = 1
+n_perceiver_blocks = 1
+num_epochs = 3
+batch_size = 5
+n_classes = 2
+lr = 0.005
+
+if __name__ == "__main__":
+    
+    # Setup image transforms to feed into model
+    transforms = transforms.Compose([
+        transforms.CenterCrop(240), 
+        transforms.ToTensor(),
+        transforms.Grayscale(),
+        transforms.Lambda(lambda x: torch.flatten(x, start_dim = 1))
+    ])
+
+    # Load model 
+    model = Perceiver(
+        d_latent,
+        embed_dim,
+        num_heads,
+        transformer_depth,
+        n_perceiver_blocks,
+        n_classes,
+        batch_size
+    ).to(device = device)
+    model.load_state_dict(torch.load("./model.pth"))
+    model.eval()
+
+    # Open a random NC image and try predict it with model
+    nc_image = Image.open(rand.choice(os.listdir(path + "\\test\\NC\\")))
+    nc_tensor = transforms(nc_image).unsqueeze(0).to(device)
+    nc_output = model(nc_tensor)
+    nc_probs = F.softmax(nc_output, dim = 1)
+    nc_class = torch.argmax(nc_output, dim = 1).item()
+
+    # Open a random NC image and try predict it with model
+    ad_image = Image.open(rand.choice(os.listdir(path + "\\test\\AD\\")))
+    ad_tensor = transforms(ad_image).unsqueeze(0).to(device)
+    ad_output = model(nc_tensor)
+    ad_probs = F.softmax(nc_output, dim = 1)
+    ad_class = torch.argmax(nc_output, dim = 1).item()
+
+    # Display the images and their prediction probabilities
+    fig, axs = plt.subplots(ncols = 2)
+    plt.axis('off')
+
+    # Plotting the NC Image
+    axs[0].imshow(nc_image)
+    axs[0].title(f"NC Brain scan predicted with prob: {nc_probs[0][0]:.4f}")
+
+    # Plotting the AD Image
+    axs[1].imshow(ad_image)
+    axs[1].title(f"AD Brain scan predicted with prob: {ad_probs[0][0]:.4f}")
+    plt.show()
