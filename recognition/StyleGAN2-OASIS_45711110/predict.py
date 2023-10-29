f9d9a7c1248794bb4fb53f6b0c140d560818acbf
4vir4l <iaviral.sh@gmail.com>
2023-10-30 04:01:34 +1000

    train.py - predict.py: moved the generate eg and plot fn

--- recognition/StyleGAN2-OASIS_45711110/predict.py
+++ recognition/StyleGAN2-OASIS_45711110/predict.py
@@ -1,19 +1,45 @@
-"example usage of the trained model"
+#example usage of the trained model
 
 import torch
 import os
-from config import *
+import train
+
+import matplotlib.pyplot as plt
 from torchvision.utils import save_image
 
+'''
+Generate Imagees using the generator.
+Images are saved in separate epoch folder with 100 images each
+
+Epoch intervals is sent as parameter while the number of imgs and path is hard coded below.
 '''
 def generate_examples(gen, epoch, n=100):
     
     for i in range(n):
         with torch.no_grad():
-            w     = get_w(1)
-            noise = get_noise(1)
+            w     = train.get_w(1)
+            noise = train.get_noise(1)
             img = gen(w, noise)
             if not os.path.exists(f'saved_examples/epoch{epoch}'):
                 os.makedirs(f'saved_examples/epoch{epoch}')
             save_image(img*0.5+0.5, f"saved_examples/epoch{epoch}/img_{i}.png")
+
 '''
+Plot a 10x5 graph of the Generator and Discriminator loss during training over iteration
+'''
+def plot_loss(G_Loss, D_Loss):
+    plt.figure(figsize=(10,5))
+    plt.title("Generator Loss During Training")
+    plt.plot(G_Loss, label="G", color="blue")
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.savefig('gen_loss.png')
+
+    plt.figure(figsize=(10,5))
+    plt.title("Discriminator Loss During Training")
+    plt.plot(D_Loss, label="D", color="red")
+    plt.xlabel("Iterations")
+    plt.ylabel("Loss")
+    plt.legend()
+    plt.savefig('disc_loss.png')
--- recognition/StyleGAN2-OASIS_45711110/train.py
+++ recognition/StyleGAN2-OASIS_45711110/train.py
@@ -7,13 +7,14 @@ from tqdm import tqdm
 from torchvision.utils import save_image
 import matplotlib.pyplot as plt
 
+import predict
 from config import *
 from dataset import get_data
 from modules import *
 
 '''
 This is an regularization penalty 
-We try to reduce the L2 norm of gradients of the discriminator with respect to images.
+:We try to reduce the L2 norm of gradients of the discriminator with respect to images.
 '''
 def gradient_penalty(critic, real, fake,device="cpu"):
     BATCH_SIZE, C, H, W = real.shape
@@ -74,22 +75,6 @@ def get_noise(batch_size):
 
     return noise
 
-'''
-Generate Imagees using the generator.
-Images are saved in separate epoch folder with 100 images each
-
-Epoch intervals is sent as parameter while the number of imgs and path is hard coded below.
-'''
-def generate_examples(gen, epoch, n=100):
-    
-    for i in range(n):
-        with torch.no_grad():
-            w     = get_w(1)
-            noise = get_noise(1)
-            img = gen(w, noise)
-            if not os.path.exists(f'saved_examples/epoch{epoch}'):
-                os.makedirs(f'saved_examples/epoch{epoch}')
-            save_image(img*0.5+0.5, f"saved_examples/epoch{epoch}/img_{i}.png")
 
 '''
 Main training loop
@@ -180,7 +165,7 @@ device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 print('Device: ', device)
 
 # Module initilization
-loader              = get_data(LOCAL, log_resolution, batch_size)
+loader              = get_data(DATA, log_resolution, batch_size)
 
 gen                 = Generator(log_resolution, w_dim).to(device)
 critic              = Discriminator(log_resolution).to(device)
@@ -219,23 +204,6 @@ for epoch in range(epochs):
 
     # Save generator's fake image on every 50th epoch
     if epoch % 50 == 0:
-    	generate_examples(gen, epoch)
+    	predict.generate_examples(gen, epoch)
 
-'''
-Plot a 10x5 graph of the Generator and Discriminator loss during training over iteration
-'''
-plt.figure(figsize=(10,5))
-plt.title("Generator Loss During Training")
-plt.plot(G_Loss, label="G", color="blue")
-plt.xlabel("Iterations")
-plt.ylabel("Loss")
-plt.legend()
-plt.savefig('gen_loss.png')
-
-plt.figure(figsize=(10,5))
-plt.title("Discriminator Loss During Training")
-plt.plot(D_Loss, label="D", color="red")
-plt.xlabel("Iterations")
-plt.ylabel("Loss")
-plt.legend()
-plt.savefig('disc_loss.png')
+predict.plot_loss(G_Loss, D_Loss)
