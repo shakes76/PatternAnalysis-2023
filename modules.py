d5ba11b982c734abd898609ad34c1bcf99dc8a56
Sam Wolfenden <s.wolfenden@uqconnect.edu.au>
2023-11-12 10:53:54 +1000

    Fixing feedback issues. Added Dropoff layer.

--- modules.py
+++ modules.py
@@ -1,38 +1,39 @@
-from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input
+from tensorflow.keras.layers import Conv2D, BatchNormalization, Activation, MaxPool2D, Conv2DTranspose, Concatenate, Input, Dropout
 from tensorflow.keras.models import Model
 
-def conv_block(inputs, num_filters):
+def conv_block(inputs, num_filters, dropout_rate=0.3):
     x = Conv2D(num_filters, 3, padding="same", activation='relu')(inputs)
+    x = Dropout(dropout_rate)(x)
     x = Conv2D(num_filters, 3, padding="same", activation='relu')(x)
     return x
 
-def encoder_block(inputs, num_filters):
-    x = conv_block(inputs, num_filters)
+def encoder_block(inputs, num_filters, dropout_rate=0.3):
+    x = conv_block(inputs, num_filters, dropout_rate)
     p = MaxPool2D((2, 2))(x)
     return x, p
 
-def decoder_block(inputs, skip_features, num_filters):
+def decoder_block(inputs, skip_features, num_filters, dropout_rate=0.3):
     x = Conv2DTranspose(num_filters, (2, 2), strides=2, padding="same")(inputs)
     x = Concatenate()([x, skip_features])
-    x = conv_block(x, num_filters)
+    x = conv_block(x, num_filters, dropout_rate)
     return x
 
-def build_unet(input_shape):
+def build_unet(input_shape, dropout_rate=0.3):
     inputs = Input(input_shape)
 
     """ Encoder """
-    s1, p1 = encoder_block(inputs, 64)
-    s2, p2 = encoder_block(p1, 128)
-    s3, p3 = encoder_block(p2, 256)
-    s4, p4 = encoder_block(p3, 512)
+    s1, p1 = encoder_block(inputs, 64, dropout_rate)
+    s2, p2 = encoder_block(p1, 128, dropout_rate)
+    s3, p3 = encoder_block(p2, 256, dropout_rate)
+    s4, p4 = encoder_block(p3, 512, dropout_rate)
 
-    b1 = conv_block(p4, 1024)
+    b1 = conv_block(p4, 1024, dropout_rate)
 
     """ Decoder """
-    d1 = decoder_block(b1, s4, 512)
-    d2 = decoder_block(d1, s3, 256)
-    d3 = decoder_block(d2, s2, 128)
-    d4 = decoder_block(d3, s1, 64)
+    d1 = decoder_block(b1, s4, 512, dropout_rate)
+    d2 = decoder_block(d1, s3, 256, dropout_rate)
+    d3 = decoder_block(d2, s2, 128, dropout_rate)
+    d4 = decoder_block(d3, s1, 64, dropout_rate)
 
     """ Outputs """
     outputs = Conv2D(1, 1, padding="same", activation="sigmoid")(d4)
