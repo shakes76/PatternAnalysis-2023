458c7e6073213bb7ec0c8e8a75e3672e076c5b3b
louisljoker <142284214+louisljoker@users.noreply.github.com>
2023-11-17 21:28:32 +1000

    Update utils.py

--- utils.py
+++ utils.py
@@ -4,6 +4,17 @@ import matplotlib.pyplot as plt
 
 
 def dice_coefficient(predicted, target, epsilon=1e-6):
+    """
+    Calculate the Dice Coefficient for two tensors.
+
+    Args:
+        predicted (tensor): Predicted tensor.
+        target (tensor): Ground truth tensor.
+        epsilon (float): Small constant for numerical stability.
+
+    Returns:
+        float: Dice Coefficient.
+    """
     # Flatten the tensors
     predicted = predicted.view(-1).float()
     target = target.view(-1).float()
@@ -15,11 +26,17 @@ def dice_coefficient(predicted, target, epsilon=1e-6):
 
 
 def display_images_from_directory(directory_path, max_images=10, extensions=['.jpg', '.jpeg', '.png']):
+    if not os.path.exists(directory_path):
+        raise FileNotFoundError(f"Directory {directory_path} does not exist.")
+    
     # List all files in the directory
     files = os.listdir(directory_path)
 
     # Filter out the files that are images with the specified extensions
     image_files = [f for f in files if any(f.lower().endswith(ext) for ext in extensions)]
+
+    if not image_files:
+        raise ValueError(f"No image files found in the directory {directory_path}.")
 
     for idx, image_file in enumerate(image_files):
         if idx >= max_images:
@@ -28,7 +45,11 @@ def display_images_from_directory(directory_path, max_images=10, extensions=['.j
         image_path = os.path.join(directory_path, image_file)
 
         # Open and display the image
-        image = Image.open(image_path)
-        image.show()
+        try:
+            image = Image.open(image_path)
+            image.show()
+        except Exception as e:
+            print(f"Error opening {image_path}: {e}")
+
 
 
