f692e76baa6b98761105eeffa6ae91b3b5406899
louisljoker <142284214+louisljoker@users.noreply.github.com>
2023-11-17 21:30:20 +1000

    Update train.py

--- train.py
+++ train.py
@@ -1,48 +1,42 @@
 import torch
-from torch.utils.data import DataLoader, random_split
+import argparse
+from torch.utils.data import DataLoader
 import torchvision.transforms as transforms
-from dataset import ISICDataset
-from modules import UNet
-from torch.utils.tensorboard import SummaryWriter
-import gc
-from utils import dice_coefficient
-import matplotlib.pyplot as plt
+from dataset import ISICDataset  # Assuming this is your dataset class
+from modules import UNet  # Assuming this is your model class
+from utils import dice_coefficient  # If you use this in your training loop
 
-# Function to save plots
-def show_plot(data, y_label):
-    plt.figure(figsize=(10,6))
-    plt.plot(data)
-    plt.title(y_label)
-    plt.ylabel(y_label)
-    plt.xlabel('Epoch')
-    plt.show()
-    plt.close()
+# Parsing command-line arguments
+parser = argparse.ArgumentParser(description='Training script for the U-Net model.')
+parser.add_argument('--train-dir', type=str, required=True, help='Path to the training data directory.')
+parser.add_argument('--valid-dir', type=str, required=True, help='Path to the validation data directory.')
+parser.add_argument('--epochs', type=int, default=10, help='Number of training epochs.')
+parser.add_argument('--batch-size', type=int, default=16, help='Batch size for training.')
+parser.add_argument('--learning-rate', type=float, default=0.001, help='Learning rate.')
+args = parser.parse_args()
+
+# Dataset and DataLoader setup
+transform = transforms.Compose([transforms.Resize((128, 128)), transforms.ToTensor()])
+
+train_dataset = ISICDataset(args.train_dir, transform=transform)
+valid_dataset = ISICDataset(args.valid_dir, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=args.batch_size, shuffle=True)
+valid_loader = DataLoader(valid_dataset, batch_size=args.batch_size, shuffle=False)
 
 # Device setup
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 
-# Dataset setup
-train_image_dir = 'C:\\Users\\25060\\Desktop\\ISIC-2017_Training_Data'
-valid_image_dir = 'C:\\Users\\25060\\Desktop\\ISIC-2017_Validation_Data'
-transform = transforms.Compose([transforms.Resize((128, 128)), transforms.ToTensor()])
-
-train_dataset = ISICDataset(train_image_dir, transform=transform)
-valid_dataset = ISICDataset(valid_image_dir, transform=transform)
-
-train_loader = DataLoader(train_dataset, batch_size=16, shuffle=True)
-valid_loader = DataLoader(valid_dataset, batch_size=16, shuffle=False)
-
 # Model, Loss, Optimizer setup
 model = UNet(in_channels=3, out_channels=1).to(device)
 criterion = torch.nn.BCEWithLogitsLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
+optimizer = torch.optim.Adam(model.parameters(), lr=args.learning_rate)
 
 # Training Loop
-epochs = 10
 train_losses = []
 valid_losses = []
 
-for epoch in range(epochs):
+for epoch in range(args.epochs):
     model.train()
     epoch_loss = 0
     for data, target in train_loader:
@@ -54,6 +48,8 @@ for epoch in range(epochs):
         optimizer.step()
         epoch_loss += loss.item()
 
+    train_losses.append(epoch_loss)
+
     # Validation
     model.eval()
     val_loss = 0
@@ -64,20 +60,12 @@ for epoch in range(epochs):
             loss = criterion(output, target)
             val_loss += loss.item()
 
-    # Store average losses for plotting
-    train_losses.append(epoch_loss / len(train_loader))
-    valid_losses.append(val_loss / len(valid_loader))
+    valid_losses.append(val_loss)
 
-    # Print epoch statistics
-    print(f"Epoch {epoch+1}/{epochs}, Training Loss: {epoch_loss/len(train_loader)}, Validation Loss: {val_loss/len(valid_loader)}")
+    # Optional: Print epoch statistics, calculate Dice coefficient, etc.
 
-# Plotting losses
-show_plot(train_losses, 'Training Loss')
-show_plot(valid_losses, 'Validation Loss')
+    # Clear unused memory
+    torch.cuda.empty_cache()
 
-    # Optional: Garbage collection
-    gc.collect()
-    if torch.cuda.is_available():
-        torch.cuda.empty_cache()
-
-writer.close()  # Close the TensorBoard writer
+# Save the model
+torch.save(model.state_dict(), 'model.pth')
